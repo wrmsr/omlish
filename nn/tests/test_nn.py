db46c71c35c49ddf645105de13a20b6399afb450
wrmsr <timwilloney@gmail.com>
2023-05-07 15:05:47 -0700

    --

--- nn/tests/test_nn.py
+++ nn/tests/test_nn.py
@@ -1,6 +1,15 @@
+import math
+
+
 class Shape(tuple):
-    pass
+    @property
+    def dim(self) -> int:
+        return math.prod(self)
 
 
+class Strides(tuple):
+    def offset(self):
 def test_nn():
-    pass
+    sh = Shape((1, 2, 3))
+    print(sh)
+    print(sh.dim)
