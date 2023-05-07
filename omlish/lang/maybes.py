7f9e323aa7037d0d1837f0d161055260e0085057
wrmsr <timwilloney@gmail.com>
2023-05-07 09:56:03 -0700

    --

--- omlish/lang/maybes.py
+++ omlish/lang/maybes.py
@@ -3,6 +3,7 @@ import typing as ta
 
 
 T = ta.TypeVar('T')
+U = ta.TypeVar('U')
 
 
 class ValueNotPresentException(BaseException):
@@ -24,6 +25,34 @@ class Maybe(abc.ABC, ta.Generic[T]):
     def __iter__(self) -> ta.Iterator[T]:
         raise NotImplementedError
 
+    @abc.abstractmethod
+    def if_present(self, consumer: ta.Callable[[T], None]) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def filter(self, predicate: ta.Callable[[T], bool]) -> 'Maybe[T]':
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def map(self, mapper: ta.Callable[[T], U]) -> 'Maybe[U]':
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def flat_map(self, mapper: ta.Callable[[T], 'Maybe[U]']) -> 'Maybe[U]':
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def or_else(self, other: T) -> 'Maybe[T]':
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def or_else_get(self, supplier: ta.Callable[[], T]) -> 'Maybe[T]':
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def or_else_raise(self, exception_supplier: ta.Callable[[], Exception]) -> 'Maybe[T]':
+        raise NotImplementedError
+
 
 class _Maybe(Maybe[T], tuple):
 
@@ -41,6 +70,35 @@ class _Maybe(Maybe[T], tuple):
 
     locals()['__iter__'] = tuple.__iter__
 
+    def filter(self, predicate: ta.Callable[[T], bool]) -> Maybe[T]:
+        return self if self and predicate(self[0]) else _empty
+
+    def map(self, mapper: ta.Callable[[T], U]) -> Maybe[U]:
+        if self:
+            value = mapper(self[0])
+            if value is not None:
+                return just(value)
+        return _empty
+
+    def flat_map(self, mapper: ta.Callable[[T], Maybe[U]]) -> Maybe[U]:
+        if self:
+            value = mapper(self[0])
+            if not isinstance(value, Maybe):
+                raise TypeError(value)
+            return value
+        return _empty
+
+    def or_else(self, other: T) -> Maybe[T]:
+        return self if self else just(other)
+
+    def or_else_get(self, supplier: ta.Callable[[], T]) -> Maybe[T]:
+        return self if self else just(supplier())
+
+    def or_else_raise(self, exception_supplier: ta.Callable[[], Exception]) -> Maybe[T]:
+        if self:
+            return self
+        raise exception_supplier()
+
 
 def just(v: T) -> Maybe[T]:
     return tuple.__new__(_Maybe, (v,))
@@ -56,4 +114,4 @@ def empty() -> Maybe[T]:
 def maybe(o: ta.Optional[T]) -> Maybe[T]:
     if o is None:
         return _empty
-    return tuple.__new__(_Maybe, (o,))
+    return just(o)
