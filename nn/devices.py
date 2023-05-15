fd6470601823faaaceb918a476c034e020a8a2b9
wrmsr <timwilloney@gmail.com>
2023-05-15 14:44:42 -0700

    --

--- nn/devices.py
+++ nn/devices.py
@@ -2,11 +2,13 @@ import abc
 import operator
 import typing as ta
 
+from omlish import check
 from omlish import lang
 import numpy as np
 
 from .ops import BinaryOp
 from .ops import MovementOp
+from .ops import ReduceOp
 from .raw import RawCpuBuffer
 
 if ta.TYPE_CHECKING:
@@ -31,6 +33,11 @@ class CpuDevice(Device):
 DEFAULT_DEVICE: Device = CpuDevice()
 
 
+def shape_to_axis(old_shape: ta.Sequence[int], new_shape: ta.Sequence[int]) -> ta.Sequence[int]:
+    check.arg(len(old_shape) == len(new_shape), 'reduce shapes must have same dimensions')
+    return tuple(i for i, (a, b) in enumerate(zip(old_shape, new_shape)) if a != b)
+
+
 @lang.cached_nullary
 def numpy_interpreter() -> 'eval_.Interpreter':
     return eval_.Interpreter(
@@ -38,6 +45,12 @@ def numpy_interpreter() -> 'eval_.Interpreter':
         {
             BinaryOp.ADD: operator.add,
             BinaryOp.MUL: operator.mul,
+
+            ReduceOp.SUM: lambda x, new_shape: (
+                x.sum(shape_to_axis(x.shape, new_shape), keepdims=True)
+                if tuple(x.shape) != tuple(new_shape) else x[:]
+            ),
+
             MovementOp.EXPAND: np.broadcast_to,
         },
     )
