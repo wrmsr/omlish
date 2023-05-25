772af7657b52a6e5db258851b0261f53c02cff54
wrmsr <timwilloney@gmail.com>
2023-05-25 16:01:35 -0700

    --

--- nn/tests/test_opencl.py
+++ nn/tests/test_opencl.py
@@ -4,6 +4,7 @@ from omlish import lang
 from omlish.dev import pytest as ptu
 import numpy as np
 
+
 if ta.TYPE_CHECKING:
     import pyopencl as cl
 else:
@@ -36,13 +37,45 @@ def test_opencl():
     item_size = 4
     device = 0
     x = np.arange(8, dtype=np.float32)
+    x2 = np.zeros_like(x)
 
-    buf = cl.Buffer(cl_ctx, cl.mem_flags.READ_WRITE, size * item_size)
+    print(x)
 
-    cl.enqueue_copy(cl_queue[device], buf, x, is_blocking=False)
+    cl_buf = cl.Buffer(cl_ctx, cl.mem_flags.READ_WRITE, size * item_size)
+    cl_buf2 = cl.Buffer(cl_ctx, cl.mem_flags.READ_WRITE, size * item_size)
 
-    # e = self.clprg(
-    #     cl_queue[device], global_size, local_size, *cl_bufs
-    # )
+    cl.enqueue_copy(cl_queue[device], cl_buf, x, is_blocking=False)
 
-    cl.enqueue_copy(cl_queue[buf.device], x, buf, is_blocking=True)
+    ##
+
+    name = 'E_8'
+    prg = """
+__kernel void E_8(__global float* data0, const __global float* data1) {
+{ int gidx0 = get_global_id(0);  /* 8 */
+  float val1_0 = data1[gidx0];
+  float val2_0 = 2.0f;
+  float alu0 = (val1_0*val2_0);
+  data0[gidx0] = alu0;
+} /* global */
+}
+"""
+
+    cl_prg = cl.Program(cl_ctx, prg)
+    _cl_bin = cl_prg.build()
+    cl_bin = getattr(_cl_bin, name)
+
+    global_size = [8]
+    local_size = None
+    cl_bufs = [cl_buf2, cl_buf]
+
+    cl_ev: cl.Event = cl_bin(
+        cl_queue[device],
+        global_size,
+        local_size,
+        *cl_bufs
+    )
+    cl_ev.wait()
+
+    cl.enqueue_copy(cl_queue[device], x2, cl_buf2, is_blocking=True)
+
+    print(x2)
