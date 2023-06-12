30ff2b726bb120aad5302515521b5603263bfc06
wrmsr <timwilloney@gmail.com>
2023-06-11 20:40:40 -0700

    --

--- omlish/dispatch/tests/test_dispatch.py
+++ omlish/dispatch/tests/test_dispatch.py
@@ -86,3 +86,12 @@ def test_method_mro():
     assert C().f(None) == 'A:object'
     assert C().f(1) == 'B:int'
     assert C().f('') == 'B:str'
+
+    # class D(B):
+    #     @A.f.register
+    #     def f_str(self, x: str):
+    #         return 'D:' + super().f(x)
+    #
+    # assert D().f(None) == 'A:object'
+    # assert D().f(1) == 'B:int'
+    # assert D().f('') == 'D:B:str'
