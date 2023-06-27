ee490bb82558c316f5dc600dcb1f7a1376ffb07c
wrmsr <timwilloney@gmail.com>
2023-06-27 16:46:46 -0700

    --

--- nn/devices.py
+++ nn/devices.py
@@ -30,7 +30,8 @@ class Device(lang.Abstract):
 class CpuDevice(Device):
     @property
     def evaluator(self) -> 'evaluators.Evaluator':
-        return numpy_interpreter()
+        from . evaluators import NumpyInterpreter
+        return NumpyInterpreter()
 
     def make_raw_buffer(self, obj: ta.Any) -> RawCpuBuffer:
         return RawCpuBuffer.from_cpu(obj)  # noqa
@@ -46,42 +47,3 @@ def default_device() -> Device:
     from .opencl import OpenclDevice
     return OpenclDevice()
     # return cpu_device()
-
-
-def shape_to_axis(old_shape: ta.Sequence[int], new_shape: ta.Sequence[int]) -> ta.Sequence[int]:
-    check.arg(len(old_shape) == len(new_shape), 'reduce shapes must have same dimensions')
-    return tuple(i for i, (a, b) in enumerate(zip(old_shape, new_shape)) if a != b)
-
-
-@lang.cached_nullary
-def numpy_interpreter() -> 'evaluators.Interpreter':
-    return evaluators.Interpreter(
-        RawCpuBuffer,
-        {
-            BinaryOp.ADD: operator.add,
-            BinaryOp.SUB: operator.sub,
-            BinaryOp.MUL: operator.mul,
-            BinaryOp.DIV: operator.truediv,
-
-            ReduceOp.SUM: lambda x, new_shape: (
-                x.sum(shape_to_axis(x.shape, new_shape), keepdims=True)
-                if tuple(x.shape) != tuple(new_shape) else x[:]
-            ),
-
-            ReduceOp.MAX: lambda x, new_shape: (
-                (x.amax if hasattr(x, 'amax') else x.max)(shape_to_axis(x.shape, new_shape), keepdims=True)
-                if tuple(x.shape) != tuple(new_shape) else x[:]
-            ),
-
-            BinaryOp.MAX: np.maximum,
-            BinaryOp.CMP_EQ: lambda x, y: (x == y).astype(np.float32),  # noqa
-
-            UnaryOp.EXP2: np.exp2,
-            UnaryOp.LOG2: np.log2,
-
-            MovementOp.EXPAND: np.broadcast_to,
-            MovementOp.RESHAPE: lambda x, arg: x.reshape(arg),
-            MovementOp.PERMUTE: lambda x, order: x.transpose(order),
-            MovementOp.PAD: np.pad,
-        },
-    )
