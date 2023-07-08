1cf1b15fced2f2e6c3cb6164f496ff1ddce15bfb
wrmsr <timwilloney@gmail.com>
2023-07-07 17:00:06 -0700

    --

--- _attic/omnibus/graphs/dot/parsing/_antlr/__init__.py
+++
@@ -1,4 +0,0 @@
-from .DotLexer import DotLexer  # noqa
-from .DotListener import DotListener  # noqa
-from .DotParser import DotParser  # noqa
-from .DotVisitor import DotVisitor  # noqa
--- nn/dot.py
+++ nn/dot.py
@@ -1,1 +1,15 @@
-class
+import typing as ta
+
+from omlish import dispatch
+from omlish.graphs import dot
+
+
+class DotGen:
+    def __init__(self) -> None:
+        super().__init__()
+        self._items: ta.List[dot.Item] = []
+
+    @dispatch.method
+    def gen(self, obj: ta.Any) -> None:
+        raise TypeError(obj)
+
