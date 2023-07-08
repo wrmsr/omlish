09e180980a7193de289b4ace9adabbd2fd0e1ad8
wrmsr <timwilloney@gmail.com>
2023-07-08 13:35:40 -0700

    --

--- omlish/collections/tests/test_treapmap.py
+++ omlish/collections/tests/test_treapmap.py
@@ -10,11 +10,11 @@ def test_treapmap():
     for i in range(32):
         m = m.with_(i, str(i))
 
-    m = m.with_(52, "hello")
-    m = m.with_(53, "world")
-    m = m.with_(52, "Hello")
+    m = m.with_(52, 'hello')
+    m = m.with_(53, 'world')
+    m = m.with_(52, 'Hello')
     m = m.with_(54, "I'm")
-    m = m.with_(55, "here")
+    m = m.with_(55, 'here')
 
     print(m)
 
