ad11a5be6b9536ad1662698705f46beb7bbfd4f7
wrmsr <timwilloney@gmail.com>
2023-07-12 12:18:57 -0700

    --

--- x/marshal/base.py
+++ x/marshal/base.py
@@ -1,30 +1,36 @@
-"""
-TODO:
- -
-"""
 import abc
 import dataclasses as dc
-import threading
 import typing as ta
 
 from .factories import Factory
+from .registries import RegistryItem
 from .specs import Spec
 from .values import Value
 
 
-# @dc.dataclass(frozen=True)
-# class SetType(RegistryItem):
-#     marshaler: 'Marshaler'
+class Marshaler(abc.ABC):
+    @abc.abstractmethod
+    def __call__(self, ctx: 'MarshalContext', o: ta.Any) -> Value:
+        raise NotImplementedError
 
 
-# class Manager:
-#     pass
+@dc.dataclass(frozen=True)
+class FuncMarshaler(Marshaler):
+    fn: ta.Callable[['MarshalContext', ta.Any], Value]
+
+    def __call__(self, ctx: 'MarshalContext', o: ta.Any) -> Value:
+        return self.fn(ctx, o)
 
 
-Marshaler = ta.Callable[['MarshalContext', ta.Any], Value]
 MarshalerFactory = Factory['MarshalContext', Spec, Marshaler]
 
 
 class MarshalContext:
     def make(self, spec: Spec) -> Marshaler:
         raise NotImplementedError
+
+
+@dc.dataclass(frozen=True)
+class SetType(RegistryItem):
+    marshaler: ta.Optional[Marshaler] = None
+    marshaler_factory: ta.Optional[MarshalerFactory] = None
