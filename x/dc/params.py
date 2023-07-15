cb9b67ec3c59fe61832ccdcae3c8937aeab24cb6
wrmsr <timwilloney@gmail.com>
2023-07-15 11:25:25 -0700

    --

---
+++ x/dc/params.py
@@ -0,0 +1,29 @@
+"""
+Field:
+    'name',
+    'type',
+    'default',
+    'default_factory',
+    'repr',
+    'hash',
+    'init',
+    'compare',
+    'metadata',
+    'kw_only',
+    '_field_type',
+
+
+Class:
+    'init',
+    'repr',
+    'eq',
+    'order',
+    'unsafe_hash',
+    'frozen',
+    'match_args',
+    'kw_only',
+    'slots',
+    'weakref_slot',
+
+
+"""
