830423a11e171b1b0f77d973b84d343e76c72749
wrmsr <timwilloney@gmail.com>
2023-07-17 17:53:28 -0700

    --

--- tg/tests/test_convert.py
+++ tg/tests/test_convert.py
@@ -3,10 +3,12 @@ from tinygrad.tensor import Tensor as TgTensor
 from nn.tensor import Tensor
 
 from ..convert import convert_tg
+from ..dot import open_tg_dot
 
 
 def test_ops():
     tgx = TgTensor([1, 2, 3]) + 2
+    open_tg_dot(tgx)
     # print(tgx.numpy())
 
     x = Tensor([1, 2, 3]) + 2
