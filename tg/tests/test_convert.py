eab33276ec3928a32e3185e2ed773a7e33c415a1
wrmsr <timwilloney@gmail.com>
2023-07-17 17:41:48 -0700

    --

--- tg/tests/test_convert.py
+++ tg/tests/test_convert.py
@@ -7,9 +7,9 @@ from ..convert import convert_tg
 
 def test_ops():
     tgx = TgTensor([1, 2, 3]) + 2
-    print(tgx.numpy())
+    # print(tgx.numpy())
 
     x = Tensor([1, 2, 3]) + 2
-    print(x.numpy())
+    # print(x.numpy())
 
     print(convert_tg(tgx.lazydata))
