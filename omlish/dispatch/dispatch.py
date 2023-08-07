b89ade58657386f7f444e6ea1039c18e3bd11c36
wrmsr <timwilloney@gmail.com>
2023-08-07 13:58:59 -0700

    --

--- omlish/dispatch/dispatch.py
+++ omlish/dispatch/dispatch.py
@@ -143,51 +143,51 @@ class Dispatcher(ta.Generic[T]):
 ##
 
 
-class _Dispatcher(ta.Generic[T]):
-    def __init__(self) -> None:
-        super().__init__()
-
-        self._impls_by_arg_cls: dict[type, T] = {}
-        self._dispatch_cache: dict[ta.Any, T] = {}
-
-        def cache_remove(k, self_ref=weakref.ref(self)):
-            if (ref_self := self_ref()) is not None:
-                cache = ref_self._dispatch_cache  # noqa
-                try:
-                    del cache[k]
-                except KeyError:
-                    pass
-
-        self._cache_remove = cache_remove
-
-        self._cache_token: ta.Any = None
-
-    def register(self, impl: U, cls_col: ta.Iterable[type]) -> U:
-        for cls in cls_col:
-            self._impls_by_arg_cls[cls] = impl  # type: ignore
-
-            if self._cache_token is None and hasattr(cls, '__abstractmethods__'):
-                self._cache_token = abc.get_cache_token()
-
-        self._dispatch_cache.clear()
-        return impl
-
-    def dispatch(self, cls: type) -> ta.Optional[T]:
-        if self._cache_token is not None and (current_token := abc.get_cache_token()) != self._cache_token:
-            self._dispatch_cache.clear()
-            self._cache_token = current_token
-
-        cls_ref = weakref.ref(cls)
-        try:
-            return self._dispatch_cache[cls_ref]
-        except KeyError:
-            pass
-        del cls_ref
-
-        try:
-            impl = self._impls_by_arg_cls[cls]
-        except KeyError:
-            impl = find_impl(cls, self._impls_by_arg_cls)  # type: ignore
-
-        self._dispatch_cache[weakref.ref(cls, self._cache_remove)] = impl
-        return impl
+# class _Dispatcher(ta.Generic[T]):
+#     def __init__(self) -> None:
+#         super().__init__()
+#
+#         self._impls_by_arg_cls: dict[type, T] = {}
+#         self._dispatch_cache: dict[ta.Any, T] = {}
+#
+#         def cache_remove(k, self_ref=weakref.ref(self)):
+#             if (ref_self := self_ref()) is not None:
+#                 cache = ref_self._dispatch_cache  # noqa
+#                 try:
+#                     del cache[k]
+#                 except KeyError:
+#                     pass
+#
+#         self._cache_remove = cache_remove
+#
+#         self._cache_token: ta.Any = None
+#
+#     def register(self, impl: U, cls_col: ta.Iterable[type]) -> U:
+#         for cls in cls_col:
+#             self._impls_by_arg_cls[cls] = impl  # type: ignore
+#
+#             if self._cache_token is None and hasattr(cls, '__abstractmethods__'):
+#                 self._cache_token = abc.get_cache_token()
+#
+#         self._dispatch_cache.clear()
+#         return impl
+#
+#     def dispatch(self, cls: type) -> ta.Optional[T]:
+#         if self._cache_token is not None and (current_token := abc.get_cache_token()) != self._cache_token:
+#             self._dispatch_cache.clear()
+#             self._cache_token = current_token
+#
+#         cls_ref = weakref.ref(cls)
+#         try:
+#             return self._dispatch_cache[cls_ref]
+#         except KeyError:
+#             pass
+#         del cls_ref
+#
+#         try:
+#             impl = self._impls_by_arg_cls[cls]
+#         except KeyError:
+#             impl = find_impl(cls, self._impls_by_arg_cls)  # type: ignore
+#
+#         self._dispatch_cache[weakref.ref(cls, self._cache_remove)] = impl
+#         return impl
