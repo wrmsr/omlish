2c49cdee3616085289368ca01e2e39679475a3ec
wrmsr <timwilloney@gmail.com>
2023-08-10 11:51:44 -0700

    --

--- tg/tests/test_simple2.py
+++ tg/tests/test_simple2.py
@@ -16,3 +16,13 @@ class TestSimple(unittest.TestCase):
         i = ((x * 0.420) * (y * 0.69)) + (z * 0.123)
         j = i.sum()
         print(j.numpy())
+
+    def test_matmul(self):
+        sz = 1024
+        np.random.seed(0)
+        a = 0.0
+        b = 1.0
+        x, y = [Tensor(np.random.random(size=(sz, sz)).astype(np.float32) + a) * b for _ in range(2)]
+        i = x @ y
+        j = i.sum()
+        print(j.numpy())
