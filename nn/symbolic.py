10a9587564eec8229cd0f97886153a4767d81f8a
wrmsr <timwilloney@gmail.com>
2023-08-23 15:00:05 -0700

    --

--- nn/symbolic.py
+++ nn/symbolic.py
@@ -105,7 +105,7 @@ class Sym(lang.Abstract, lang.Sealed):
 
     def vars(self) -> ta.Iterator['Var']:
         return
-        yield  # noqa
+        yield  # type: ignore  # noqa
 
     @cached.property
     def key(self) -> str:
