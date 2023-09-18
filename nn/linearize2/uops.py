aa2b4f954ee5ab95ca95734af7f1f05c65b957f4
wrmsr <timwilloney@gmail.com>
2023-09-18 14:11:32 -0700

    --

--- nn/linearize2/uops.py
+++ nn/linearize2/uops.py
@@ -14,3 +14,90 @@ WMMA = auto()
 CAST = auto()
 GEP = auto()  # noqa: E702
 """
+import abc
+import typing as ta
+
+from omlish import dataclasses as dc
+from omlish import lang
+
+from .. import ops
+from .. import symbolic as sym
+from ..dtypes import Dtype
+from ..dtypes import Float32
+from ..scalars import Scalar
+
+
+@dc.dataclass(frozen=True)
+class Uop(lang.Sealed, lang.Abstract):
+    dtype: ta.Optional[Dtype]
+    vin: ta.Sequence['Uop']
+
+
+@dc.dataclass(frozen=True)
+class Loop(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class End(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Special(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class DefineGlobal(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class DefineLocal(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class DefineAcc(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Load(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Store(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Const(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Barrier(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Alu(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Wmma(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Cast(Uop, lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Gep(Uop, lang.Final):
+    pass
