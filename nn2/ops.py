56e45c25bc6a3e62068a08f1c3c8f63aef261489
wrmsr <timwilloney@gmail.com>
2023-09-29 13:05:46 -0700

    --

--- nn2/ops.py
+++ nn2/ops.py
@@ -1,95 +1,33 @@
+"""
+NOTE: MOD, CMPLT don't have to be implemented on vectors, just scalars
+NOTE: rdna3 only has RECIP and not DIV. DIV and POW are on the chopping block
+"""
 from __future__ import annotations
 
-import enum
 import typing as ta
+
+from omlish import lang
 
 if ta.TYPE_CHECKING:
     from .lazy import LazyBuffer
 
 
-# these are the llops your accelerator must implement, along with toCpu
-# the Enum class doesn't work with mypy, this is static. sorry it's ugly
-# NOTE: MOD, CMPLT don't have to be implemented on vectors, just scalars
-# NOTE: rdna3 only has RECIP and not DIV. DIV and POW are on the chopping block
-class UnaryOps(enum.Enum):
-    NOOP = enum.auto()
-    EXP2 = enum.auto()
-    LOG2 = enum.auto()
-    CAST = enum.auto()
-    SIN = enum.auto()
-    SQRT = enum.auto()
-    RECIP = enum.auto()
-    NEG = enum.auto()  # noqa: E702
-
-
-class BinaryOps(enum.Enum):
-    ADD = enum.auto()
-    SUB = enum.auto()
-    MUL = enum.auto()
-    DIV = enum.auto()
-    MAX = enum.auto()
-    MOD = enum.auto()
-    CMPLT = enum.auto()  # noqa: E702
-
-
-class TernaryOps(enum.Enum):
-    MULACC = enum.auto()
-    WHERE = enum.auto()  # noqa: E702
-
-
-class ReduceOps(enum.Enum):
-    SUM = enum.auto()
-    MAX = enum.auto()  # noqa: E702
-
-
-class BufferOps(enum.Enum):
-    MEM = enum.auto()
-    CONST = enum.auto()  # noqa: E702
-
-
-# Ops below this line are not allowed in ASTs
-class MovementOps(enum.Enum):
-    RESHAPE = enum.auto()
-    PERMUTE = enum.auto()
-    EXPAND = enum.auto()
-    PAD = enum.auto()
-    SHRINK = enum.auto()
-    STRIDE = enum.auto()
-    AS_STRIDED = enum.auto()  # noqa: E702
-
-
-class LoadOps(enum.Enum):
-    EMPTY = enum.auto()
-    RAND = enum.auto()
-    CONST = enum.auto()
-    FROM = enum.auto()
-    CONTIGUOUS = enum.auto()
-    CUSTOM = enum.auto()  # noqa: E702
-
-
-Op = ta.Union[UnaryOps, BinaryOps, ReduceOps, MovementOps, LoadOps, TernaryOps, BufferOps]
-OpType = ta.Union[
-    type[UnaryOps],
-    type[BinaryOps],
-    type[ReduceOps],
-    type[MovementOps],
-    type[LoadOps],
-    type[TernaryOps],
-    type[BufferOps],
-]
-
-
-class LazyOp:
-    __slots__ = "op", "src", "arg", "buffers", "__weakref__"
-    op: Op
+class LazyOp(lang.Abstract):
     src: tuple[ta.Union[LazyOp, LazyBuffer], ...]
     arg: ta.Any
     buffers: tuple[LazyBuffer, ...]
 
     def __init__(
-        self, op: Op, src: tuple[ta.Union[LazyOp, LazyBuffer], ...], arg: ta.Any = None
-    ):
-        self.op, self.src, self.arg, self.buffers = op, src, arg, ()
+            self,
+            src: tuple[ta.Union[LazyOp, LazyBuffer], ...],
+            arg: ta.Any = None,
+    ) -> None:
+        super().__init__()
+
+        self.src = src
+        self.arg = arg
+        self.buffers = ()
+
         # NOTE: the linearizer's key function maps the buffers to ints, and LOCAL_BUFFER is used. we don't care about
         # buffers in these cases
         try:
@@ -98,24 +36,24 @@ class LazyOp:
         except AttributeError:
             self.buffers = ()
 
-    def __repr__(self):
-        return f"LazyOp(op={self.op}, src={self.src}, arg={self.arg})"
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(src={self.src}, arg={self.arg})"
 
     def __eq__(self, __value: object) -> bool:
         return (
             isinstance(__value, LazyOp)
-            and self.op is __value.op
+            and type(self) is type(__value)
             and self.src == __value.src
             and self.arg == __value.arg
         )
 
     def __hash__(self) -> int:
-        return hash((self.op, self.src, self.arg))
+        return hash((type(self), self.src, self.arg))
 
     @property
     def key(self):
         return (
-            self.op,
+            type(self),
             tuple(map(lambda x: getattr(x, "key", x), self.src)),
             getattr(self.arg, "key", self.arg),
         )
@@ -123,15 +61,16 @@ class LazyOp:
     def map_buffers(
         self, real_srcs: ta.Mapping[LazyBuffer, ta.Union[LazyBuffer, LazyOp]]
     ) -> LazyOp:
-        return LazyOp(
-            self.op, tuple([y.map_buffers(real_srcs) for y in self.src]), self.arg
+        return type(self)(
+            tuple([y.map_buffers(real_srcs) for y in self.src]), self.arg
         )
 
     def get_lazyops(self) -> list[LazyOp]:
         return [self] + [item for x in self.src for item in x.get_lazyops()]
 
     def replace_with_movement_ops(
-        self: LazyOp, ops: list[tuple[MovementOps, tuple[ta.Any, ...]]]
+        self: LazyOp,
+            ops: list[tuple[MovementOps, tuple[ta.Any, ...]]]
     ) -> "LazyBuffer":
         assert self.op in BinaryOps or self.op in UnaryOps or self.op in TernaryOps
         srcs = [z.replace_with_movement_ops(ops) for z in self.src]
@@ -178,3 +117,188 @@ class LazyOp:
 
     def stride(self, _):
         raise NotImplementedError
+
+
+##
+
+
+class UnaryOp(LazyOp, lang.Abstract):
+    pass
+
+
+class Nop(UnaryOp, lang.Final):
+    pass
+
+
+class Exp2(UnaryOp, lang.Final):
+    pass
+
+
+class Log2(UnaryOp, lang.Final):
+    pass
+
+
+class Cast(UnaryOp, lang.Final):
+    pass
+
+
+class Sin(UnaryOp, lang.Final):
+    pass
+
+
+class Sqrt(UnaryOp, lang.Final):
+    pass
+
+
+class Recip(UnaryOp, lang.Final):
+    pass
+
+
+class Neg(UnaryOp, lang.Final):
+    pass
+
+
+##
+
+
+class BinaryOp(LazyOp, lang.Abstract):
+    pass
+
+
+class Add(BinaryOp, lang.Final):
+    pass
+
+
+class Sub(BinaryOp, lang.Final):
+    pass
+
+
+class Mul(BinaryOp, lang.Final):
+    pass
+
+
+class Div(BinaryOp, lang.Final):
+    pass
+
+
+class Max2(BinaryOp, lang.Final):
+    pass
+
+
+class Mod(BinaryOp, lang.Final):
+    pass
+
+
+class CmpLt(BinaryOp, lang.Final):
+    pass
+
+
+##
+
+
+class TernaryOp(LazyOp, lang.Abstract):
+    pass
+
+
+class MulAcc(TernaryOp, lang.Final):
+    pass
+
+
+class Where(TernaryOp, lang.Final):
+    pass
+
+
+##
+
+
+class ReduceOp(LazyOp, lang.Abstract):
+    pass
+
+
+class Sum(ReduceOp, lang.Final):
+    pass
+
+
+class Max(ReduceOp, lang.Final):
+    pass
+
+
+##
+
+
+class BufferOp(LazyOp, lang.Abstract):
+    pass
+
+
+class Mem(BufferOp, lang.Final):
+    pass
+
+
+class Const(BufferOp, lang.Final):
+    pass
+
+
+##
+
+
+class MovementOp(LazyOp, lang.Abstract):
+    pass
+
+
+class Reshape(MovementOp, lang.Final):
+    pass
+
+
+class Permute(MovementOp, lang.Final):
+    pass
+
+
+class Expand(MovementOp, lang.Final):
+    pass
+
+
+class Pad(MovementOp, lang.Final):
+    pass
+
+
+class Shrink(MovementOp, lang.Final):
+    pass
+
+
+class Restride(MovementOp, lang.Final):
+    pass
+
+
+class AsStrided(MovementOp, lang.Final):
+    pass
+
+
+##
+
+
+class LoadOp(LazyOp, lang.Abstract):
+    pass
+
+
+class Empty(LoadOp, lang.Final):
+    pass
+
+
+class Rand(LoadOp, lang.Final):
+    pass
+
+
+class LoadConst(LoadOp, lang.Final):
+    pass
+
+
+class From(LoadOp, lang.Final):
+    pass
+
+
+class Contiguous(LoadOp, lang.Final):
+    pass
+
+
+class Custom(LoadOp, lang.Final):
+    pass
