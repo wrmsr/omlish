abd0b005641de3a682593dea6165a4ab20b2e07b
wrmsr <timwilloney@gmail.com>
2023-11-02 17:12:27 -0700

    --
    Former-commit-id: 614e09e47bf3f64812a642e918c3e1a24d87424a

--- nn/execution.py
+++ nn/execution.py
@@ -560,52 +560,52 @@ class Compiled(ASTExecutor):
                 if BEAM >= 1 and not vars_from_ast(ast):
                     lins = [(("tc" if used_tensor_cores else "hc"), k)]
 
-                # allocate a scratch buffer if output buffer is also input
-                test_rawbuffers = [type(rawbuffers[0])(rawbuffers[0].size, rawbuffers[0].dtype), *rawbuffers[1:]] \
-                    if rawbuffers[0] in rawbuffers[1:] else rawbuffers
-                kb = Linearizer(ast, self.linearizer_opts)
-                kb.required_optimizations()
-                from .features.search import beam_search
-                from .features.search import time_linearizer
+                    # allocate a scratch buffer if output buffer is also input
+                    test_rawbuffers = [type(rawbuffers[0])(rawbuffers[0].size, rawbuffers[0].dtype), *rawbuffers[1:]] \
+                        if rawbuffers[0] in rawbuffers[1:] else rawbuffers
+                    kb = Linearizer(ast, self.linearizer_opts)
+                    kb.required_optimizations()
+                    from .features.search import beam_search
+                    from .features.search import time_linearizer
 
-                lins.append((
-                    f"beam{BEAM.value}",
-                    beam_search(
-                        kb,
-                        test_rawbuffers,
-                        BEAM.value,
-                        bool(getenv("BEAM_ESTIMATE", 1)),
-                    ),
-                ))
-
-                if used_tensor_cores:
-                    lins.append(("hc", Linearizer(ast, self.linearizer_opts)))
-                    lins[-1][1].hand_coded_optimizations()
-                timed = sorted(
-                    [
-                        (
-                            nm,
-                            tk,
-                            time_linearizer(
-                                tk,
-                                test_rawbuffers,
-                                allow_test_size=False,
-                                disable_cache=True,
-                                clear_l2=True,
-                            ),
-                        )
-                        for nm, tk in lins
-                    ],
-                    key=lambda x: x[2],
-                )
-
-                if DEBUG >= 1:
-                    print("  <  ".join(
-                        f"{nm:6s} : {lin.colored_shape(30, dense=True)} : {tm * 1e6:8.2f} us"
-                        for nm, lin, tm in timed
+                    lins.append((
+                        f"beam{BEAM.value}",
+                        beam_search(
+                            kb,
+                            test_rawbuffers,
+                            BEAM.value,
+                            bool(getenv("BEAM_ESTIMATE", 1)),
+                        ),
                     ))
 
-                k = timed[0][1]
+                    if used_tensor_cores:
+                        lins.append(("hc", Linearizer(ast, self.linearizer_opts)))
+                        lins[-1][1].hand_coded_optimizations()
+                    timed = sorted(
+                        [
+                            (
+                                nm,
+                                tk,
+                                time_linearizer(
+                                    tk,
+                                    test_rawbuffers,
+                                    allow_test_size=False,
+                                    disable_cache=True,
+                                    clear_l2=True,
+                                ),
+                            )
+                            for nm, tk in lins
+                        ],
+                        key=lambda x: x[2],
+                    )
+
+                    if DEBUG >= 1:
+                        print("  <  ".join(
+                            f"{nm:6s} : {lin.colored_shape(30, dense=True)} : {tm * 1e6:8.2f} us"
+                            for nm, lin, tm in timed
+                        ))
+
+                    k = timed[0][1]
 
             return self.to_program(k)
 
