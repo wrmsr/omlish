b2ed59135e6732c605dbe225c722cec5391a07d3
wrmsr <timwilloney@gmail.com>
2023-11-09 13:21:01 -0800

    --
    Former-commit-id: 7ef3b2f466a894a16554d5d23f444e10a964d179

--- omlish/marshal/dataclasses.py
+++ omlish/marshal/dataclasses.py
@@ -19,6 +19,9 @@ from .base import UnmarshalerFactory
 from .values import Value
 
 
+##
+
+
 @dc.dataclass(frozen=True)
 class Field:
     name: str | None = None
@@ -51,6 +54,7 @@ def get_field_infos(ty: type, opts: col.TypeMap[Option] = col.TypeMap()) -> ta.S
         kw = dict(
             field=field,
             type=type_hints[field.name],
+            metadata=None,
 
             marshal_name=field.name,
 
@@ -74,54 +78,53 @@ def get_field_infos(ty: type, opts: col.TypeMap[Option] = col.TypeMap()) -> ta.S
     return ret
 
 
+##
+
+
 @dc.dataclass(frozen=True)
 class DataclassMarshaler(Marshaler):
-    fields: ta.Sequence[tuple[str, Marshaler, str]]
+    fields: ta.Sequence[tuple[FieldInfo, Marshaler]]
 
     def marshal(self, ctx: MarshalContext, o: ta.Any) -> Value:
-        return {k: m.marshal(ctx, getattr(o, a)) for a, m, k in self.fields}
+        return {
+            fi.marshal_name: m.marshal(ctx, getattr(o, fi.field.name))
+            for fi, m in self.fields
+        }
 
 
 class DataclassMarshalerFactory(MarshalerFactory):
     def __call__(self, ctx: MarshalContext, rty: rfl.Type) -> ta.Optional[Marshaler]:
         if isinstance(rty, type) and dc.is_dataclass(rty):
-            fields: list[tuple[str, Marshaler, str]] = []
-            th = ta.get_type_hints(rty)
-            for fld in dc.fields(rty):
-                fty = th[fld.name]
-                m = ctx.make(fty)
-                k = fld.name
-                if (mdf := fld.metadata.get(Field)) is not None:
-                    if mdf.name is not None:
-                        k = mdf.name
-                fields.append((fld.name, m, k))
+            fields = [
+                (fi, ctx.make(fi.type))
+                for fi in get_field_infos(rty, ctx.options)
+            ]
             return DataclassMarshaler(fields)
         return None
+
+
+##
 
 
 @dc.dataclass(frozen=True)
 class DataclassUnmarshaler(Unmarshaler):
     cls: type
-    fields: ta.Sequence[tuple[str, Unmarshaler, str]]
+    fields: ta.Sequence[tuple[FieldInfo, Unmarshaler]]
 
     def unmarshal(self, ctx: UnmarshalContext, v: Value) -> ta.Any:
         ma = check.isinstance(v, collections.abc.Mapping)
-        return self.cls(**{a: u.unmarshal(ctx, ma[k]) for k, u, a in self.fields})  # type: ignore
+        return self.cls(**{  # type: ignore
+            fi.field.name: u.unmarshal(ctx, ma[fi.unmarshal_name])
+            for fi, u in self.fields
+        })
 
 
 class DataclassUnmarshalerFactory(UnmarshalerFactory):
     def __call__(self, ctx: UnmarshalContext, rty: rfl.Type) -> ta.Optional[Unmarshaler]:
         if isinstance(rty, type) and dc.is_dataclass(rty):
-            get_field_infos(rty, ctx.options)
-            fields: list[tuple[str, Unmarshaler, str]] = []
-            th = ta.get_type_hints(rty)
-            for fld in dc.fields(rty):
-                fty = th[fld.name]
-                u = ctx.make(fty)
-                k = fld.name
-                if (mdf := fld.metadata.get(Field)) is not None:
-                    if mdf.name is not None:
-                        k = mdf.name
-                fields.append((k, u, fld.name))
+            fields = [
+                (fi, ctx.make(fi.type))
+                for fi in get_field_infos(rty, ctx.options)
+            ]
             return DataclassUnmarshaler(rty, fields)
         return None
