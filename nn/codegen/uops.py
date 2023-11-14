7a34224b1b2a56e582db695e82f26a0eb6cf699e
wrmsr <timwilloney@gmail.com>
2023-11-14 11:25:09 -0800

    --

--- nn/codegen/uops.py
+++ nn/codegen/uops.py
@@ -8,7 +8,7 @@ from omlish import lang
 from ..dtypes import DType
 
 
-@dc.dataclass(frozen=True, eq=False)
+@dc.dataclass(eq=False)
 class UOp(lang.Abstract):
     dtype: ta.Optional[DType]
     vin: tuple[UOp, ...]
@@ -26,81 +26,81 @@ class UOp(lang.Abstract):
 # bottom ones are asm only
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Loop(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class If(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class End(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Special(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class DefineGlobal(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class DefineLocal(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class DefineAcc(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Load(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Store(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Const(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Barrier(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Phi(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Alu(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Wmma(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Cast(UOp, lang.Final):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False, repr=False)
+@dc.dataclass(eq=False, repr=False)
 class Gep(UOp, lang.Final):
     pass
