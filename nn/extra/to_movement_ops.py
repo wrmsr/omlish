b86134671ec71644693a8968c4f2b02af27a26eb
wrmsr <timwilloney@gmail.com>
2023-11-17 15:00:06 -0800

    --
    Former-commit-id: ea6c97e0393817df4019f734a50fba2cc9206561

--- nn/extra/to_movement_ops.py
+++ nn/extra/to_movement_ops.py
@@ -1,7 +1,10 @@
+import itertools
+
 from .. import ops
 from ..helpers import prod
 from ..shape.shapetracker import ShapeTracker
 from ..shape.view import View
+from ..shape.symbolic import sym_infer
 
 
 inf = float('inf')
@@ -22,54 +25,36 @@ def get_buffer_size(shape, strides, offset, mask):
     return real_offset + sum((s - 1) * st for s, st in zip(real_real_shape, strides)) + 1
 
 
-def flatten_view(view: View):
-    real_real_shape, strides, real_offset = get_real_view(view.shape, view.strides, view.offset, view.mask)
+def st_equivalent(st1: ShapeTracker, st2: ShapeTracker):
+    if (idxs1 := st1.expr_idxs()) == (idxs2 := st2.expr_idxs()):
+        return True
 
-    def sort_by_strides(shape, strides):
-        return (
-            sorted(zip(shape, strides), key=lambda k: (k[1], -k[0]), reverse=True),
-            sorted(range(len(strides)), key=lambda k: (strides[k], - real_real_shape[k]), reverse=True),
-        )
+    idx1, valid1 = idxs1
+    idx2, valid2 = idxs2
+    # always invalid
+    if valid1 == 0 and valid2 == 0:
+        return True
 
-    ordered_shape_strides, _ = sort_by_strides(real_real_shape, strides)
-    ordered_shape_strides = [list(s) for s in ordered_shape_strides]
+    var1 = set(idx1.vars() + valid1.vars())
+    var2 = set(idx2.vars() + valid2.vars())
+    # Maybe there are cases that vars are different yet the sts are the same?
+    if var1 != var2:
+        return False
 
-    if strides:
-        i = 0
-        while i < len(ordered_shape_strides):
-            if (
-                    i < len(ordered_shape_strides) - 1
-                    and ordered_shape_strides[i][1] == ordered_shape_strides[i + 1][0] * ordered_shape_strides[i + 1][1]
-            ):
-                ordered_shape_strides[i + 1][0] = ordered_shape_strides[i][0] * ordered_shape_strides[i + 1][0]
-            else:
-                i += 1
+    # brute force over the vars range
+    vs = list(var1)
+    for i, ranges in enumerate(itertools.product(*[range(v.min, v.max + 1) for v in vs])):
+        if i > 1000:
+            print("WARNING: did not search all possible combinations")
+            # not happening for now
+            break
 
-        flat_shape = [shape_stride[0] for shape_stride in ordered_shape_strides]
-        flat_strides = [shape_stride[1] for shape_stride in ordered_shape_strides]
-        return (flat_shape, flat_strides, real_offset)
+        var_vals = {k: v for k, v in zip(vs, ranges)}
+        r1 = sym_infer(idx1, var_vals) if sym_infer(valid1, var_vals) else 0
+        r2 = sym_infer(idx2, var_vals) if sym_infer(valid2, var_vals) else 0
+        if r1 != r2:
+            return False
 
-    return (real_real_shape, view.strides, real_offset)
-
-
-def views_equivalent(v1: View, v2: View) -> bool:
-    return v1 == v2 or flatten_view(v1) == flatten_view(v2)
-
-
-def st_equivalent(st: ShapeTracker, st_rebuilt: ShapeTracker):
-    views = list(st.views)
-    rebuilt_views = list(st_rebuilt.views)
-    i = 0
-    while i < len(views):
-        view, rebuilt_view = views[i], rebuilt_views[i]
-        if view == rebuilt_view:
-            i += 1
-            continue
-        elif view.shape == rebuilt_view.shape:
-            i += 1
-        # hack to skip expands for overlapped strides
-        else:
-            rebuilt_views.pop(i)
     return True
 
 
@@ -112,14 +97,7 @@ def test_rebuild(st: ShapeTracker):
             raise Exception("invalid mop")
 
     rebuilt_st = rebuilt_st.simplify()
-    if len(st.views) != len(rebuilt_st.views):
-        if not set(st.views).issubset(set(rebuilt_st.views)):
-            assert st_equivalent(st, rebuilt_st)
-
-    else:
-        for v1, v2 in zip(st.views, rebuilt_st.views):
-            assert views_equivalent(v1, v2), f"{v1} not equivalent to {v2}"
-
+    assert st_equivalent(st, rebuilt_st)
     last_v1 = st.views[-1]
     last_v2 = rebuilt_st.views[-1]
     assert last_v1.shape == last_v2.shape, f"{last_v1.shape} != {last_v2.shape}"
