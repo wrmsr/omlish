72792aeda340cf0e38926dbd92cdece05d24b4e8
wrmsr <timwilloney@gmail.com>
2023-12-13 14:27:54 -0800

    --
    Former-commit-id: 9b4ea3b36483398e0c6c9fb9797bc77c05cef456

--- tg/conftest.py
+++
@@ -1,5 +0,0 @@
-from omlish.dev import pytest as ptu
-
-
-def pytest_addhooks(pluginmanager):
-    ptu.plugins.addhooks(pluginmanager)
--- tg/update.bash
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cd tinygrad && git reset --hard HEAD && git pull origin master
