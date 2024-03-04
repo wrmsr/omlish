06befd02156e112338095e919c192f5cc42c182d
wrmsr <timwilloney@gmail.com>
2024-03-04 15:26:15 -0800

    --

--- x/antlr_cpp/py.py
+++ x/antlr_cpp/py.py
@@ -1,9 +1,40 @@
+"""
+(barf says: hi \n\nxarf says:  xi\n<EOF> (barf says: hi \n (barf  barf  ) (says:  says :  ) (hi  hi  ) \n) (\n \n) (xarf says:  xi\n (xarf  xarf  ) (says:  says :  ) ( xi   xi) \n) <EOF>)
+(barf says: hi \n (barf  barf  ) (says:  says :  ) (hi  hi  ) \n)
+(barf  barf  )
+(says:  says :  )
+(hi  hi  )
+(\n \n)
+(xarf says:  xi\n (xarf  xarf  ) (says:  says :  ) ( xi   xi) \n)
+(xarf  xarf  )
+(says:  says :  )
+( xi   xi)
+
+[]
+[18]
+[25 18]
+[26 18]
+[27 18]
+[18]
+[18]
+[25 18]
+[26 18]
+[27 18]
+"""
 import typing as ta
 
 import antlr4
 
 from ._py.ChatLexer import ChatLexer
 from ._py.ChatParser import ChatParser
+from ._py.ChatVisitor import ChatVisitor
+
+
+class ChatVisitorImpl(ChatVisitor):
+
+    def visitChildren(self, node):
+        print(node)
+        return super().visitChildren(node)
 
 
 def create_parser(buf: str) -> ChatParser:
@@ -23,7 +54,7 @@ def _main():
 
     parser = create_parser(buf)
 
-    print(parser)
+    ChatVisitorImpl().visit(parser.chat())
 
 
 if __name__ == '__main__':
