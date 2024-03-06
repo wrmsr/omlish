1d7cc2333e9bb490ed8bfbae206912977c3b942a
wrmsr <timwilloney@gmail.com>
2024-03-06 15:06:18 -0800

    --
    Former-commit-id: 835b3dd97672cc6831d6dac79f1208372e34f6fe

--- x/ai4b/14_embeddings_0.py
+++ x/ai4b/14_embeddings_0.py
@@ -67,8 +67,8 @@ def _main() -> None:
 
         net.train()
 
-        total_loss = torch.tensor(0.)
-        acc = torch.tensor(0)
+        total_loss = torch.tensor(0.).to(device)
+        acc = torch.tensor(0).to(device)
         count = 0
         i = 0
 
@@ -99,76 +99,78 @@ def _main() -> None:
 
         return total_loss.item() / count, acc.item() / count
 
-    train_epoch(net, train_loader, lr=1, epoch_size=25000)
+    # while True:
+    #     train_epoch(net, train_loader, lr=1, epoch_size=25000)
 
     ##
 
-    # class EmbedClassifier(torch.nn.Module):
-    #     def __init__(self, vocab_size, embed_dim, num_class):
-    #         super().__init__()
-    #         self.embedding = torch.nn.EmbeddingBag(vocab_size, embed_dim)
-    #         self.fc = torch.nn.Linear(embed_dim, num_class)
-    #
-    #     def forward(self, text, off):
-    #         x = self.embedding(text, off)
-    #         return self.fc(x)
-    #
-    # def offsetify(b):
-    #     # first, compute data tensor from all sequences
-    #     x = [torch.tensor(encode(t[1])) for t in b]
-    #     # now, compute the offsets by accumulating the tensor of sequence lengths
-    #     o = [0] + [len(t) for t in x]
-    #     o = torch.tensor(o[:-1]).cumsum(dim=0)
-    #     return (
-    #         torch.LongTensor([t[0] - 1 for t in b]),  # labels
-    #         torch.cat(x),  # text
-    #         o
-    #     )
-    #
-    # train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, collate_fn=offsetify, shuffle=True)
-    #
-    # net = EmbedClassifier(vocab_size, 32, len(classes)).to(device)
-    #
-    # def train_epoch_emb(
-    #         net,
-    #         dataloader,
-    #         lr=0.01,
-    #         optimizer=None,
-    #         loss_fn=torch.nn.CrossEntropyLoss(),
-    #         epoch_size=None,
-    #         report_freq=200,
-    # ):
-    #     optimizer = optimizer or torch.optim.Adam(net.parameters(), lr=lr)
-    #     loss_fn = loss_fn.to(device)
-    #     net.train()
-    #
-    #     total_loss, acc, count, i = 0, 0, 0, 0
-    #     for labels, text, off in dataloader:
-    #         labels = labels.to(device)
-    #         text = text.to(device)
-    #         off = off.to(device)
-    #
-    #         optimizer.zero_grad()
-    #         labels, text, off = labels.to(device), text.to(device), off.to(device)
-    #         out = net(text, off)
-    #         loss = loss_fn(out, labels)  # cross_entropy(out,labels)
-    #         loss.backward()
-    #         optimizer.step()
-    #         total_loss += loss
-    #         _, predicted = torch.max(out, 1)
-    #         acc += (predicted == labels).sum()
-    #         count += len(labels)
-    #
-    #         i += 1
-    #         if i % report_freq == 0:
-    #             print(f"{count}: acc={acc.item() / count}")
-    #
-    #         if epoch_size and count > epoch_size:
-    #             break
-    #
-    #     return total_loss.item() / count, acc.item() / count
-    #
-    # train_epoch_emb(net, train_loader, lr=4, epoch_size=25000)
+    class EmbedClassifier(torch.nn.Module):
+        def __init__(self, vocab_size, embed_dim, num_class):
+            super().__init__()
+            self.embedding = torch.nn.EmbeddingBag(vocab_size, embed_dim)
+            self.fc = torch.nn.Linear(embed_dim, num_class)
+
+        def forward(self, text, off):
+            x = self.embedding(text, off)
+            return self.fc(x)
+
+    def offsetify(b):
+        # first, compute data tensor from all sequences
+        x = [torch.tensor(encode(t[1])) for t in b]
+        # now, compute the offsets by accumulating the tensor of sequence lengths
+        o = [0] + [len(t) for t in x]
+        o = torch.tensor(o[:-1]).cumsum(dim=0)
+        return (
+            torch.LongTensor([t[0] - 1 for t in b]),  # labels
+            torch.cat(x),  # text
+            o
+        )
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, collate_fn=offsetify, shuffle=True)
+
+    net = EmbedClassifier(vocab_size, 32, len(classes)).to(device)
+
+    def train_epoch_emb(
+            net,
+            dataloader,
+            lr=0.01,
+            optimizer=None,
+            loss_fn=torch.nn.CrossEntropyLoss(),
+            epoch_size=None,
+            report_freq=200,
+    ):
+        optimizer = optimizer or torch.optim.Adam(net.parameters(), lr=lr)
+        loss_fn = loss_fn.to(device)
+        net.train()
+
+        total_loss, acc, count, i = 0, 0, 0, 0
+        for labels, text, off in dataloader:
+            labels = labels.to(device)
+            text = text.to(device)
+            off = off.to(device)
+
+            optimizer.zero_grad()
+            labels, text, off = labels.to(device), text.to(device), off.to(device)
+            out = net(text, off)
+            loss = loss_fn(out, labels)  # cross_entropy(out,labels)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss
+            _, predicted = torch.max(out, 1)
+            acc += (predicted == labels).sum()
+            count += len(labels)
+
+            i += 1
+            if i % report_freq == 0:
+                print(f"{count}: acc={acc.item() / count}")
+
+            if epoch_size and count > epoch_size:
+                break
+
+        return total_loss.item() / count, acc.item() / count
+
+    while True:
+        train_epoch_emb(net, train_loader, lr=4, epoch_size=25000)
 
 
 if __name__ == '__main__':
