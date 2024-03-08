265a5ef5db255efe181b4777940cc51dcade5dee
wrmsr <timwilloney@gmail.com>
2024-03-08 15:53:48 -0800

    --

--- x/runpod/cli.py
+++ x/runpod/cli.py
@@ -5,6 +5,11 @@ curl -s --request POST \
   --header 'content-type: application/json' \
   --url "https://api.runpod.io/graphql?api_key=$RUNPOD_API_KEY" \
   --data '{"query": "query Pods { myself { pods { id } } }"}'
+
+curl --request POST \
+  --header 'content-type: application/json' \
+  --url "https://api.runpod.io/graphql?api_key=$RUNPOD_API_KEY" \
+  --data '{"query": "mutation { podStop(input: { id: \"hcbt5m4crrm1wu\" } ) } { id }" }'
 """
 
 
