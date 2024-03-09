98dadea823d55cf95bef992df293c0e12ad89ade
wrmsr <timwilloney@gmail.com>
2024-03-08 16:01:08 -0800

    --

--- x/llama_fs/lfs.py
+++ x/llama_fs/lfs.py
@@ -185,3 +185,20 @@ def train(model, optimizer, scheduler=None, config=MASTER_CONFIG, print_logs=Fal
 
 
 train(model, optimizer, print_logs=True)
+
+
+##
+
+
+def generate(model, config=MASTER_CONFIG, max_new_tokens=30):
+    idx = torch.zeros(5, 1).long()
+    for _ in range(max_new_tokens):
+        # call the model
+        logits = model(idx[:, -config['context_window']:])
+        last_time_step_logits = logits[:, -1, :]  # all the batches (1), last time step, all the logits
+        p = F.softmax(last_time_step_logits, dim=-1)  # softmax to get probabilities
+        idx_next = torch.multinomial(p, num_samples=1)  # sample from the distribution to get the next token
+        idx = torch.cat([idx, idx_next], dim=-1)  # append to the sequence
+    return [decode(x) for x in idx.tolist()]
+
+print(generate(model))
