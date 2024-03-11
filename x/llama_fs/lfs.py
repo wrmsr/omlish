103cfab1a9c101ff1d02ff31853a58580cc83aeb
wrmsr <timwilloney@gmail.com>
2024-03-11 15:29:22 -0700

    --

--- x/llama_fs/lfs.py
+++ x/llama_fs/lfs.py
@@ -11,10 +11,11 @@ import numpy as np
 import pandas as pd
 import torch
 
-from .data import vocab
 from .data import dataset
-from .data import get_batches
 from .data import decode
+from .data import get_batches
+from .data import vocab
+from .generate import generate
 from .train import train
 
 
@@ -28,12 +29,7 @@ MASTER_CONFIG = {
 }
 
 
-xs, ys = get_batches(
-    dataset(),
-    'train',
-    MASTER_CONFIG['batch_size'],
-    MASTER_CONFIG['context_window'],
-)
+xs, ys = get_batches(dataset(), 'train', MASTER_CONFIG)
 
 pprint([(decode(xs[i].tolist()), decode(ys[i].tolist())) for i in range(len(xs))])
 
@@ -72,12 +68,7 @@ MASTER_CONFIG.update({
 
 model = SimpleBrokenModel(MASTER_CONFIG)
 
-xs, ys = get_batches(
-    dataset(),
-    'train',
-    MASTER_CONFIG['batch_size'],
-    MASTER_CONFIG['context_window'],
-)
+xs, ys = get_batches(dataset(), 'train', MASTER_CONFIG)
 
 logits, loss = model(xs, ys)
 
@@ -98,25 +89,13 @@ optimizer = torch.optim.Adam(
 )
 
 
-
 train(model, optimizer, config=MASTER_CONFIG, print_logs=True)
 
 
 ##
 
 
-def generate(model, config=MASTER_CONFIG, max_new_tokens=30):
-    idx = torch.zeros(5, 1).long()
-    for _ in range(max_new_tokens):
-        # call the model
-        logits = model(idx[:, -config['context_window']:])
-        last_time_step_logits = logits[:, -1, :]  # all the batches (1), last time step, all the logits
-        p = F.softmax(last_time_step_logits, dim=-1)  # softmax to get probabilities
-        idx_next = torch.multinomial(p, num_samples=1)  # sample from the distribution to get the next token
-        idx = torch.cat([idx, idx_next], dim=-1)  # append to the sequence
-    return [decode(x) for x in idx.tolist()]
-
-print(generate(model))
+print(generate(model, MASTER_CONFIG))
 
 
 ##
