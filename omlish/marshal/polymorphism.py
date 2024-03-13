5d16760420bd793696325fa9ae48f5280fa3648a
wrmsr <timwilloney@gmail.com>
2024-03-12 17:32:29 -0700

    --

--- omlish/marshal/polymorphism.py
+++ omlish/marshal/polymorphism.py
@@ -7,6 +7,15 @@ TODO:
 import dataclasses as dc
 import typing as ta
 
+from .. import reflect as rfl
+from .base import MarshalContext
+from .base import Marshaler
+from .base import MarshalerFactory
+from .base import UnmarshalContext
+from .base import Unmarshaler
+from .base import UnmarshalerFactory
+from .values import Value
+
 
 @dc.dataclass(frozen=True)
 class Impl:
@@ -35,3 +44,27 @@ class Polymorphism:
             by_tag[i.tag] = i
             for a in i.alts:
                 by_tag[a] = i
+
+
+class PolymorphismMarshaler(Marshaler):
+    m: ta.Mapping[type, tuple[str, Marshaler]]
+
+    def marshal(self, ctx: MarshalContext, o: ta.Optional[ta.Any]) -> Value:
+        raise NotImplementedError
+
+
+class PolymorphismMarshalerFactory(MarshalerFactory):
+    def __call__(self, ctx: MarshalContext, rty: rfl.Type) -> ta.Optional[Marshaler]:
+        raise NotImplementedError
+
+
+class PolymorphismUnmarshaler(Unmarshaler):
+    m: ta.Mapping[str, Unmarshaler]
+
+    def unmarshal(self, ctx: UnmarshalContext, v: Value) -> ta.Optional[ta.Any]:
+        raise NotImplementedError
+
+
+class PolymorphismUnmarshalerFactory(UnmarshalerFactory):
+    def __call__(self, ctx: UnmarshalContext, rty: rfl.Type) -> ta.Optional[Unmarshaler]:
+        raise NotImplementedError
