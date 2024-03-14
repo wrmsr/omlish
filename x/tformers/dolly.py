36664fc286938009e67ed97477dbdfc87d0b96a2
wrmsr <timwilloney@gmail.com>
2024-03-13 17:15:27 -0700

    --
    Former-commit-id: 7e825a87da45a7071ddf50d3850e80cc8871206d

---
+++ x/tformers/dolly.py
@@ -0,0 +1,20 @@
+"""
+https://github.com/databrickslabs/dolly
+"""
+import torch
+import transformers
+
+
+def _main():
+    instruct_pipeline = transformers.pipeline(
+        model="databricks/dolly-v2-12b",
+        torch_dtype=torch.float16,
+        trust_remote_code=True,
+        device_map="auto",
+    )
+
+    print(instruct_pipeline("Explain to me the difference between nuclear fission and fusion."))
+
+
+if __name__ == '__main__':
+    _main()
