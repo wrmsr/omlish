a11ccb556990ee2ef6f8068ed21cc5d9b7588a16
wrmsr <timwilloney@gmail.com>
2024-03-18 14:36:22 -0700

    --
    Former-commit-id: 7548adf46ba3258468f5151b033b2717343f485f

--- dataplays/dp20240315_torch_tuts/b_nn_tutorial_5.py
+++ dataplays/dp20240315_torch_tuts/b_nn_tutorial_5.py
@@ -1,41 +1,41 @@
-import math
-from matplotlib import pyplot
-import numpy as np
-import torch
-import torch.nn.functional as F
-from torch import optim
-from torch import nn
-import pickle
 import gzip
-from pathlib import Path
+import pathlib
+import pickle
+
+from matplotlib import pyplot  # noqa
+import numpy as np
 import requests
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torch.utils.data
 
 
-DATA_PATH = Path("data")
-PATH = DATA_PATH / "mnist"
+def load_data() -> tuple[np.ndarray, ...]:
+    data_path = pathlib.Path('data')
+    path = data_path / 'mnist'
 
-PATH.mkdir(parents=True, exist_ok=True)
+    path.mkdir(parents=True, exist_ok=True)
 
-URL = "https://github.com/pytorch/tutorials/raw/main/_static/"
-FILENAME = "mnist.pkl.gz"
+    url = 'https://github.com/pytorch/tutorials/raw/main/_static/'
+    filename = 'mnist.pkl.gz'
 
-if not (PATH / FILENAME).exists():
-    content = requests.get(URL + FILENAME).content
-    (PATH / FILENAME).open("wb").write(content)
+    if not (path / filename).exists():
+        content = requests.get(url + filename).content
+        (path / filename).open('wb').write(content)
 
-with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
-    (x_train, y_train), (x_valid, y_valid), _ = pickle.load(f, encoding="latin-1")
+    with gzip.open((path / filename).as_posix(), 'rb') as f:
+        (x_train, y_train), (x_valid, y_valid), _ = pickle.load(f, encoding='latin-1')
 
-x_train, y_train, x_valid, y_valid = map(torch.tensor, (x_train, y_train, x_valid, y_valid))
-n, c = x_train.shape
+    return x_train, y_train, x_valid, y_valid
 
-bs = 64  # batch size
 
-loss_func = F.cross_entropy
+def get_data(train_ds, valid_ds, bs):
+    return (
+        torch.utils.data.DataLoader(train_ds, batch_size=bs, shuffle=True),
+        torch.utils.data.DataLoader(valid_ds, batch_size=bs * 2),
+    )
 
-train_ds = torch.utils.data.TensorDataset(x_train, y_train)
-valid_ds = torch.utils.data.TensorDataset(x_valid, y_valid)
 
 def loss_batch(model, loss_func, xb, yb, opt=None):
     loss = loss_func(model(xb), yb)
@@ -64,56 +64,27 @@ def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
         print(epoch, val_loss)
 
 
-def get_data(train_ds, valid_ds, bs):
-    return (
-        torch.utils.data.DataLoader(train_ds, batch_size=bs, shuffle=True),
-        torch.utils.data.DataLoader(valid_ds, batch_size=bs * 2),
-    )
-
-
-# class Mnist_CNN(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
-#         self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
-#         self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
-#
-#     def forward(self, xb):
-#         xb = xb.view(-1, 1, 28, 28)
-#         xb = F.relu(self.conv1(xb))
-#         xb = F.relu(self.conv2(xb))
-#         xb = F.relu(self.conv3(xb))
-#         xb = F.avg_pool2d(xb, 4)
-#         return xb.view(-1, xb.size(1))
-
-
-class Lambda(nn.Module):
-    def __init__(self, func):
+class Mnist_CNN(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.func = func
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
 
-    def forward(self, x):
-        return self.func(x)
+    def forward(
+            self,
+            xb,  # B x 1 x 28 x 28
+    ):
+        xb = F.relu(self.conv1(xb))
+        xb = F.relu(self.conv2(xb))
+        xb = F.relu(self.conv3(xb))
+        xb = F.adaptive_avg_pool2d(xb, 1)
+        return xb.view(xb.size(0), -1)
 
-
-model = nn.Sequential(
-    nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
-    nn.ReLU(),
-    nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1),
-    nn.ReLU(),
-    nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1),
-    nn.ReLU(),
-    nn.AdaptiveAvgPool2d(1),
-    Lambda(lambda x: x.view(x.size(0), -1)),
-)
-
-dev = torch.device("mps") # if torch.cuda.is_available() else torch.device("cpu")
-
-def preprocess(x, y):
-    return x.view(-1, 1, 28, 28).to(dev), y.to(dev)
 
 class WrappedDataLoader:
     def __init__(self, dl, func):
+        super().__init__()
         self.dl = dl
         self.func = func
 
@@ -125,14 +96,32 @@ class WrappedDataLoader:
             yield (self.func(*b))
 
 
-train_dl, valid_dl = get_data(train_ds, valid_ds, bs)
-train_dl = WrappedDataLoader(train_dl, preprocess)
-valid_dl = WrappedDataLoader(valid_dl, preprocess)
+def _main():
+    x_train, y_train, x_valid, y_valid = map(torch.tensor, load_data())
+    train_ds = torch.utils.data.TensorDataset(x_train, y_train)
+    valid_ds = torch.utils.data.TensorDataset(x_valid, y_valid)
 
-model.to(dev)
+    bs = 64  # batch size
 
-lr = 0.1
-opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    dev = torch.device('mps')
 
-epochs = 10
-fit(epochs, model, loss_func, opt, train_dl, valid_dl)
+    def preprocess(x, y):
+        return x.view(-1, 1, 28, 28).to(dev), y.to(dev)
+
+    train_dl, valid_dl = get_data(train_ds, valid_ds, bs)
+    train_dl = WrappedDataLoader(train_dl, preprocess)
+    valid_dl = WrappedDataLoader(valid_dl, preprocess)
+
+    model = Mnist_CNN()
+    model.to(dev)
+
+    lr = 0.1
+    opt = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+    epochs = 10
+    loss_func = F.cross_entropy
+    fit(epochs, model, loss_func, opt, train_dl, valid_dl)
+
+
+if __name__ == '__main__':
+    _main()
