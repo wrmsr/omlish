78d981811201d099db7ef465c6f2a435a8dd6e14
wrmsr <timwilloney@gmail.com>
2024-04-02 16:55:22 -0700

    --
    Former-commit-id: 6a22443623dbcadec06c39a442c6a45b68afad55

--- dataplays/dp20240315_torch_tuts/i_s2s_xlat2.py
+++ dataplays/dp20240315_torch_tuts/i_s2s_xlat2.py
@@ -221,8 +221,8 @@ class DumbDecoderRNN(nn.Module, DecoderRNN):
 class BahdanauAttention(nn.Module):
     def __init__(self, hidden_size: int) -> None:
         super().__init__()
-        self.Wa = nn.Linear(hidden_size, hidden_size)
-        self.Ua = nn.Linear(hidden_size, hidden_size)
+        self.Wa = nn.Linear(hidden_size, hidden_size)  # query
+        self.Ua = nn.Linear(hidden_size, hidden_size)  # keys
         self.Va = nn.Linear(hidden_size, 1)
 
     def forward(self, query: Tensor, keys: Tensor) -> tuple[Tensor, Tensor]:
