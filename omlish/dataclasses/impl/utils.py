8bfcf0c0cd85e4eba62f328aa1129d371b64f3c3
wrmsr <timwilloney@gmail.com>
2024-04-10 11:45:32 -0700

    --
    Former-commit-id: 1a7e37f52814968b665998e336ea8c1e725036a5

--- omlish/dataclasses/impl/utils.py
+++ omlish/dataclasses/impl/utils.py
@@ -25,7 +25,7 @@ def create_fn(
         locals = {}
     return_annotation = ''
     if return_type.present:
-        locals['__dataclass_return_type__'] = return_type.must()
+        locals['__dataclass_return_type__'] = return_type()
         return_annotation = '->__dataclass_return_type__'
     args = ','.join(args)
     body = '\n'.join(f'  {b}' for b in body)
@@ -33,15 +33,114 @@ def create_fn(
     txt = f' def {name}({args}){return_annotation}:\n{body}'
 
     local_vars = ', '.join(locals.keys())
-    txt = f"def __create_fn__({local_vars}):\n{txt}\n return {name}"
+    txt = f'def __create_fn__({local_vars}):\n{txt}\n return {name}'
     ns: dict[str, ta.Any] = {}
     exec(txt, globals, ns)  # type: ignore
     return ns['__create_fn__'](**locals)
 
 
+class FuncBuilder:
+    def __init__(self, globals: Namespace) -> None:
+        super().__init__()
+
+        self.names: list[str] = []
+        self.src: list[str] = []
+        self.globals = globals
+        self.locals: Namespace = {}
+        self.overwrite_errors: dict[str, bool] = {}
+        self.unconditional_adds: dict[str, bool] = {}
+
+    def add_fn(
+            self,
+            name: str,
+            args: ta.Sequence[str],
+            body: ta.Sequence[str],
+            *,
+            locals: ta.Optional[Namespace] = None,
+            return_type: lang.Maybe[ta.Any] = lang.empty(),
+            overwrite_error: bool = False,
+            unconditional_add: bool = False,
+            decorator: ta.Optional[str] = None,
+    ) -> None:
+        if locals is not None:
+            self.locals.update(locals)
+
+        # Keep track if this method is allowed to be overwritten if it already exists in the class.  The error is
+        # method-specific, so keep it with the name.  We'll use this when we generate all of the functions in the
+        # add_fns_to_class call.  overwrite_error is either True, in which case we'll raise an error, or it's a string,
+        # in which case we'll raise an error and append this string.
+        if overwrite_error:
+            self.overwrite_errors[name] = overwrite_error
+
+        # Should this function always overwrite anything that's already in the class?  The default is to not overwrite a
+        # function that already exists.
+        if unconditional_add:
+            self.unconditional_adds[name] = True
+
+        self.names.append(name)
+
+        if return_type.present:
+            self.locals[f'__dataclass_{name}_return_type__'] = return_type()
+            return_annotation = f'->__dataclass_{name}_return_type__'
+        else:
+            return_annotation = ''
+        args = ','.join(args)
+        body = '\n'.join(body)
+
+        # Compute the text of the entire function, add it to the text we're generating.
+        self.src.append(f'{" {decorator}\n" if decorator else ""} def {name}({args}){return_annotation}:\n{body}')
+
+    def add_fns_to_class(self, cls: type) -> None:
+        # The source to all of the functions we're generating.
+        fns_src = '\n'.join(self.src)
+
+        # The locals they use.
+        local_vars = ','.join(self.locals)
+
+        # The names of all of the functions, used for the return value of the outer function.  Need to handle the
+        # 0-tuple specially.
+        if not self.names:
+            return_names = '()'
+        else:
+            return_names = f'({",".join(self.names)},)'
+
+        # txt is the entire function we're going to execute, including the
+        # bodies of the functions we're defining.  Here's a greatly simplified
+        # version:
+        # def __create_fn__():
+        #  def __init__(self, x, y):
+        #   self.x = x
+        #   self.y = y
+        #  @recursive_repr
+        #  def __repr__(self):
+        #   return f'cls(x={self.x!r},y={self.y!r})'
+        # return __init__,__repr__
+
+        txt = f'def __create_fn__({local_vars}):\n{fns_src}\n return {return_names}'
+        ns = {}
+        exec(txt, self.globals, ns)  # type: ignore
+        fns = ns['__create_fn__'](**self.locals)
+
+        # Now that we've generated the functions, assign them into cls.
+        for name, fn in zip(self.names, fns):
+            fn.__qualname__ = f'{cls.__qualname__}.{fn.__name__}'
+            if self.unconditional_adds.get(name, False):
+                setattr(cls, name, fn)
+            else:
+                already_exists = set_new_attribute(cls, name, fn)
+
+                # See if it's an error to overwrite this particular function.
+                if already_exists and (msg_extra := self.overwrite_errors.get(name)):
+                    error_msg = (f'Cannot overwrite attribute {fn.__name__} in class {cls.__name__}')
+                    if not msg_extra is True:
+                        error_msg = f'{error_msg} {msg_extra}'
+
+                    raise TypeError(error_msg)
+
+
 def set_qualname(cls: type, value: T) -> T:
     if isinstance(value, types.FunctionType):
-        value.__qualname__ = f"{cls.__qualname__}.{value.__name__}"
+        value.__qualname__ = f'{cls.__qualname__}.{value.__name__}'
     return value
 
 
