9c4a76d90088eb89ca71c0f5e2e392b4d4a5601a
wrmsr <timwilloney@gmail.com>
2024-04-22 16:06:57 -0700

    --

--- dataplays/dp20240419_tunz_t2t/model/transformer.py
+++ dataplays/dp20240419_tunz_t2t/model/transformer.py
@@ -1,5 +1,6 @@
 import math
 
+from torch import Tensor
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,8 +13,38 @@ from .common import initialize_weight
 # pylint: disable=arguments-differ
 
 
+def simple_sdpa(
+        q: Tensor,
+        k: Tensor,
+        mask: Tensor,
+        scale: float,
+) -> Tensor:
+    # Scaled Dot-Product Attention.
+    # Attention(Q, K, V) = softmax((QK^T)/sqrt(d_k))V
+    q.mul_(scale)
+    x = torch.matmul(q, k)  # [b, h, q_len, k_len]
+    x.masked_fill_(mask.unsqueeze(1).type(torch.bool), -1e9)
+    return x
+
+
+def fast_sdpa(q, k, mask, scale):
+    # from tcop.masked_softmax import MaskedSoftmax
+    # https://github.com/tunz/tcop-pytorch/blob/fe5dada36964085850d5a50405498c193fb5c426/tcop/masked_softmax.py
+    # -> git+https://github.com/wrmsr/tcop-pytorch/tree/deb0a2deb4ef998d89c33c2f1d618df64bb206a4
+    x = torch.matmul(q, k)  # [b, h, q_len, k_len]
+    x = MaskedSoftmax.apply(x, mask, scale)  # noqa
+    return x
+
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self, hidden_size, dropout_rate, head_size=8):
+    sdpa = classmethod(simple_sdpa)  # noqa
+
+    def __init__(
+            self,
+            hidden_size: int,
+            dropout_rate: float,
+            head_size: int = 8,
+    ) -> None:
         super().__init__()
 
         self.head_size = head_size
@@ -33,7 +64,14 @@ class MultiHeadAttention(nn.Module):
         self.output_layer = nn.Linear(head_size * att_size, hidden_size, bias=False)
         initialize_weight(self.output_layer)
 
-    def forward(self, q, k, v, mask, cache=None):
+    def forward(
+            self,
+            q: Tensor,
+            k: Tensor,
+            v: Tensor,
+            mask: Tensor,
+            cache: Tensor | None = None,
+    ) -> Tensor:
         orig_q_size = q.size()
 
         d_k = self.att_size
@@ -76,7 +114,12 @@ class MultiHeadAttention(nn.Module):
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, hidden_size, filter_size, dropout_rate):
+    def __init__(
+            self,
+            hidden_size: int,
+            filter_size: int,
+            dropout_rate: float,
+    ) -> None:
         super().__init__()
 
         self.self_attention_norm = nn.LayerNorm(hidden_size, eps=1e-6)
@@ -87,7 +130,11 @@ class EncoderLayer(nn.Module):
         self.ffn = FeedForwardNetwork(hidden_size, filter_size, dropout_rate)
         self.ffn_dropout = nn.Dropout(dropout_rate)
 
-    def forward(self, x, mask):  # pylint: disable=arguments-differ
+    def forward(
+            self,
+            x: Tensor,
+            mask: Tensor,
+    ) -> Tensor:
         y = self.self_attention_norm(x)
         y = self.self_attention(y, y, y, mask)
         y = self.self_attention_dropout(y)
@@ -101,7 +148,12 @@ class EncoderLayer(nn.Module):
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, hidden_size, filter_size, dropout_rate):
+    def __init__(
+            self,
+            hidden_size: int,
+            filter_size: int,
+            dropout_rate: float,
+    ) -> None:
         super().__init__()
 
         self.self_attention_norm = nn.LayerNorm(hidden_size, eps=1e-6)
@@ -116,7 +168,14 @@ class DecoderLayer(nn.Module):
         self.ffn = FeedForwardNetwork(hidden_size, filter_size, dropout_rate)
         self.ffn_dropout = nn.Dropout(dropout_rate)
 
-    def forward(self, x, enc_output, self_mask, i_mask, cache):
+    def forward(
+            self,
+            x: Tensor,
+            enc_output: Tensor,
+            self_mask: Tensor,
+            i_mask: Tensor,
+            cache: Tensor,
+    ) -> Tensor:
         y = self.self_attention_norm(x)
         y = self.self_attention(y, y, y, self_mask)
         y = self.self_attention_dropout(y)
@@ -136,7 +195,13 @@ class DecoderLayer(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, hidden_size, filter_size, dropout_rate, n_layers):
+    def __init__(
+            self,
+            hidden_size: int,
+            filter_size: int,
+            dropout_rate: float,
+            n_layers: int,
+    ) -> None:
         super().__init__()
 
         encoders = [
@@ -147,7 +212,11 @@ class Encoder(nn.Module):
 
         self.last_norm = nn.LayerNorm(hidden_size, eps=1e-6)
 
-    def forward(self, inputs, mask):
+    def forward(
+            self,
+            inputs: Tensor,
+            mask: Tensor,
+    ) -> Tensor:
         encoder_output = inputs
         for enc_layer in self.layers:
             encoder_output = enc_layer(encoder_output, mask)
@@ -155,7 +224,13 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, hidden_size, filter_size, dropout_rate, n_layers):
+    def __init__(
+            self,
+            hidden_size: int,
+            filter_size: int,
+            dropout_rate: float,
+            n_layers: int,
+    ) -> None:
         super().__init__()
 
         decoders = [
@@ -166,7 +241,14 @@ class Decoder(nn.Module):
 
         self.last_norm = nn.LayerNorm(hidden_size, eps=1e-6)
 
-    def forward(self, targets, enc_output, i_mask, t_self_mask, cache):
+    def forward(
+            self,
+            targets: Tensor,
+            enc_output: Tensor,
+            i_mask: Tensor,
+            t_self_mask: Tensor,
+            cache: Tensor | None,
+    ) -> Tensor:
         decoder_output = targets
         for i, dec_layer in enumerate(self.layers):
             layer_cache = None
@@ -185,16 +267,18 @@ class Decoder(nn.Module):
 
 
 class Transformer(nn.Module):
+    fast_style_mask = False
+
     def __init__(
             self,
-            i_vocab_size,
-            t_vocab_size,
-            n_layers=6,
-            hidden_size=512,
-            filter_size=2048,
-            dropout_rate=0.1,
-            share_target_embedding=True,
-            has_inputs=True,
+            i_vocab_size: int,
+            t_vocab_size: int,
+            n_layers: int = 6,
+            hidden_size: int = 512,
+            filter_size: int = 2048,
+            dropout_rate: float = 0.1,
+            share_target_embedding: bool = True,
+            has_inputs: bool = True,
             src_pad_idx=None,
             trg_pad_idx=None,
     ):
@@ -250,7 +334,11 @@ class Transformer(nn.Module):
         )
         self.register_buffer('inv_timescales', inv_timescales)
 
-    def forward(self, inputs, targets):
+    def forward(
+            self,
+            inputs: Tensor,
+            targets: Tensor,
+    ) -> Tensor:
         enc_output, i_mask = None, None
         if self.has_inputs:
             i_mask = utils.create_pad_mask(inputs, self.src_pad_idx)
@@ -261,7 +349,11 @@ class Transformer(nn.Module):
         t_self_mask = utils.create_trg_self_mask(target_size, device=targets.device)
         return self.decode(targets, enc_output, i_mask, t_self_mask, t_mask)
 
-    def encode(self, inputs, i_mask):
+    def encode(
+            self,
+            inputs: Tensor,
+            i_mask: Tensor,
+    ) -> Tensor:
         # Input embedding
         input_embedded = self.i_vocab_embedding(inputs)
         input_embedded.masked_fill_(i_mask.squeeze(1).unsqueeze(-1), 0)
@@ -269,17 +361,19 @@ class Transformer(nn.Module):
         input_embedded += self.get_position_encoding(inputs)
         input_embedded = self.i_emb_dropout(input_embedded)
 
+        if self.fast_style_mask:
+            i_mask = i_mask.size(2) - i_mask.sum(dim=2, dtype=torch.int32)
         return self.encoder(input_embedded, i_mask)
 
     def decode(
             self,
-            targets,
-            enc_output,
-            i_mask,
-            t_self_mask,
-            t_mask,
-            cache=None,
-    ):
+            targets: Tensor,
+            enc_output: Tensor,
+            i_mask: Tensor,
+            t_self_mask: Tensor,
+            t_mask: Tensor,
+            cache: Tensor | None = None,
+    ) -> Tensor:
         # target embedding
         target_embedded = self.t_vocab_embedding(targets)
         target_embedded.masked_fill_(t_mask.squeeze(1).unsqueeze(-1), 0)
@@ -293,6 +387,11 @@ class Transformer(nn.Module):
         target_embedded = self.t_emb_dropout(target_embedded)
 
         # decoder
+        if self.fast_style_mask:
+            if i_mask is not None:
+                i_mask = i_mask.size(2) - i_mask.sum(dim=2, dtype=torch.int32)
+            t_self_mask = t_self_mask.size(2) - t_self_mask.sum(dim=2, dtype=torch.int32)
+
         decoder_output = self.decoder(
             target_embedded,
             enc_output,
@@ -308,7 +407,10 @@ class Transformer(nn.Module):
 
         return output
 
-    def get_position_encoding(self, x):
+    def get_position_encoding(
+            self,
+            x: Tensor,
+    ) -> Tensor:
         max_length = x.size()[1]
         position = torch.arange(max_length, dtype=torch.float32, device=x.device)
         scaled_time = position.unsqueeze(1) * self.inv_timescales.unsqueeze(0)
