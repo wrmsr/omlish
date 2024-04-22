00eabe44afd9bad622549ad47ea000945499d004
wrmsr <timwilloney@gmail.com>
2024-04-22 12:28:31 -0700

    --
    Former-commit-id: a355b8b38b38d31edec9befccf9e62b9c2772b5d

--- dataplays/dp20240419_tunz_t2t/dataset/common.py
+++ dataplays/dp20240419_tunz_t2t/dataset/common.py
@@ -1,11 +1,11 @@
 import pickle
 
 import torch
-# from torchtext.legacy.data import Iterator
+from torchtext_0_3_1.data import Iterator
 from tqdm import tqdm
 
 
-class BucketByLengthIterator:  # (Iterator):
+class BucketByLengthIterator(Iterator):
     def __init__(
             self,
             *args,
