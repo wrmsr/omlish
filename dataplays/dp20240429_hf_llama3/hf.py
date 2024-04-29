ad9734bef3c089e5cf4ace34e4acf45e245def3a
wrmsr <timwilloney@gmail.com>
2024-04-29 15:52:19 -0700

    --
    Former-commit-id: b10308c2863d70acb30b377b2e3c6df43491526b

--- dataplays/dp20240429_hf_llama3/hf.py
+++ dataplays/dp20240429_hf_llama3/hf.py
@@ -3,7 +3,11 @@ import transformers
 
 def _main():
     model_name = "meta-llama/Meta-Llama-3-8B"
-    pipeline = transformers.pipeline("text-generation", model=model_name)
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model_name,
+        device='mps',
+    )
     print(pipeline('hi there'))
 
 
