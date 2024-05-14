3692a1e36a080c5732978db270984d21e8b54174
wrmsr <timwilloney@gmail.com>
2024-05-14 12:38:34 -0700

    --
    Former-commit-id: 9ac1721e1ee2e1a63d4dc124b7b119ec4ca327ee

--- omserv/server/server.py
+++ omserv/server/server.py
@@ -1,13 +1,31 @@
+"""
+aiojobs
+
+aiopg
+aiomysql
+
+aiobotocore
+
+aiorwlock
+"""
+import abc
 import aiohttp.web
 
 
-async def handle(request: aiohttp.web.Request) -> aiohttp.web.Response:
-    return aiohttp.web.Response(text="Hello, World!")
+class Handler(abc.ABC):
+    @abc.abstractmethod
+    async def __call__(self, request: aiohttp.web.Request) -> aiohttp.web.StreamResponse:
+        raise NotImplementedError
+
+
+class HelloHandler(Handler):
+    async def __call__(self, request: aiohttp.web.Request) -> aiohttp.web.StreamResponse:
+        return aiohttp.web.Response(text="Hello, World!")
 
 
 def _main() -> None:
     app = aiohttp.web.Application()
-    app.add_routes([aiohttp.web.get('/', handle)])
+    app.add_routes([aiohttp.web.get('/', HelloHandler())])
 
     aiohttp.web.run_app(app)
 
