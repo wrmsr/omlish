671be33cadd8f8b32e3bbfb3ef59abaa85981038
wrmsr <timwilloney@gmail.com>
2024-05-14 13:43:42 -0700

    --
    Former-commit-id: e8611d54f9c6aa19843ad4968a8e81d87d607834

--- omserv/infra/ssh.py
+++ omserv/infra/ssh.py
@@ -1,11 +1,34 @@
 """
 https://github.com/ronf/asyncssh
+
+bcrypt
+fido2
+gssapi
+libnacl
+pkcs11
+pyOpenSSL
+
+asyncssh[bcrypt,fido2,gssapi,libnacl,pkcs11,pyOpenSSL]
 """
 import asyncio
 
+import asyncssh
+import yaml
+
 
 async def _a_main() -> None:
-    pass
+    with open('secrets.yml', 'r') as f:
+        cfg = yaml.safe_load(f)
+
+    print(cfg)
+
+    async with asyncssh.connect(
+            cfg['ec2_ssh_host'],
+            username=cfg['ec2_ssh_user'],
+            client_keys=[cfg['ec2_ssh_key_file']],
+    ) as conn:
+        result = await conn.run('echo "Hello!"', check=True)
+        print(result.stdout, end='')
 
 
 if __name__ == '__main__':
