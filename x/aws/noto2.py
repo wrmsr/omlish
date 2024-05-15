835c43a2fcfa5b3504e0bcb51d09db3fba787ff5
wrmsr <timwilloney@gmail.com>
2024-05-15 13:34:24 -0700

    --
    Former-commit-id: 1e71e6295aff072021d93cdbbce790357b2ac918

--- x/aws/noto2.py
+++ x/aws/noto2.py
@@ -2,6 +2,7 @@ import configparser
 import io
 import os.path
 
+import boto3
 import botocore.model
 import botocore.session
 
@@ -9,6 +10,7 @@ import botocore.session
 def _main() -> None:
     with open(os.path.expanduser('~/.aws/credentials'), 'r') as f:
         txt = f.read()
+
     config = configparser.ConfigParser()
     config.read_file(io.StringIO(txt))
     cred_cfg = {k.lower(): v for k, v in config.items('default')}
@@ -20,6 +22,47 @@ def _main() -> None:
     region_name = 'us-west-1'
     access_key_id = cred_cfg['aws_access_key_id']
     secret_access_key = cred_cfg['aws_secret_access_key']
+
+    ##
+
+    ec2 = boto3.resource(
+        'ec2',
+        region_name=region_name,
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+    )
+
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}],
+    )
+
+    for instance in instances:
+        print(instance.id, instance.instance_type)
+
+    ##
+
+    session = botocore.session.Session()
+
+    ec2_client = session.create_client(
+        'ec2',
+        region_name=region_name,
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+    )
+
+    response = ec2_client.describe_instances(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}],
+    )
+
+    running_instances = [
+        instance
+        for reservation in response['Reservations']
+        for instance in reservation['Instances']
+        if instance['State']['Name'] == 'running'
+    ]
+
+    for instance in running_instances:
+        print(instance['InstanceId'], instance['InstanceType'])
 
 
 if __name__ == '__main__':
