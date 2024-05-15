adbe89ce82db4133f9897710cefa3ce6d96a210a
wrmsr <timwilloney@gmail.com>
2024-05-15 15:12:54 -0700

    --
    Former-commit-id: 38e66d8a697efdba84d60d8a5ba7cd5620e9e960

--- x/aws/noto2.py
+++ x/aws/noto2.py
@@ -64,6 +64,78 @@ def _main() -> None:
     for instance in running_instances:
         print(instance['InstanceId'], instance['InstanceType'])
 
+    ##
+
+    """
+    result = {Config} <botocore.config.Config object at 0x144977610>
+ NON_LEGACY_OPTION_DEFAULTS = {dict: 1} {'connect_timeout': None}
+ OPTION_DEFAULTS = {OrderedDict: 24} OrderedDict([('region_name', None), ('signature_version', None), ('user_agent', None), ('user_agent_extra', None), ('user_agen..., None), ('request_min_compression_size_bytes', None), ('disable_request_compression', None), ('client_context_params', None)])
+ client_cert = {NoneType} None
+ client_context_params = {NoneType} None
+ connect_timeout = {int} 60
+ defaults_mode = {NoneType} None
+ disable_request_compression = {bool} False
+ endpoint_discovery_enabled = {NoneType} None
+ ignore_configured_endpoint_urls = {NoneType} None
+ inject_host_prefix = {bool} True
+ max_pool_connections = {int} 10
+ parameter_validation = {bool} True
+ proxies = {NoneType} None
+ proxies_config = {NoneType} None
+ read_timeout = {int} 60
+ region_name = {str} 'us-west-1'
+ request_min_compression_size_bytes = {int} 10240
+ retries = {dict: 1} {'mode': 'legacy'}
+ s3 = {NoneType} None
+ signature_version = {str} 'v4'
+ tcp_keepalive = {NoneType} None
+ use_dualstack_endpoint = {NoneType} None
+ use_fips_endpoint = {NoneType} None
+ user_agent = {str} 'Botocore/1.34.69 ua/2.0 os/macos#21.6.0 md/arch#arm64 lang/python#3.11.9 md/pyimpl#CPython'
+ user_agent_appid = {NoneType} None
+ user_agent_extra = {NoneType} None
+    """
+
+    """
+    {
+        'auth_type': None,
+        'client_config': <botocore.config.Config object at 0x144977610 >,
+        'client_region': 'us-west-1',
+        'has_streaming_input': False,
+    }
+    
+    {'Filters': [{'Name': 'instance-state-name', 'Values': ['running']}]}
+    
+    endpoint_url = 'https://ec2.us-west-1.amazonaws.com'
+    additional_headers = {}
+    properties = {}
+    
+    request_dict = {
+        'url_path': '/',
+        'query_string': '',
+        'method': 'POST',
+        'headers': {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+            'User-Agent': 'Botocore/1.34.69 ua/2.0 os/macos#21.6.0 md/arch#arm64 lang/python#3.11.9 md/pyimpl#CPython cfg/retry-mode#legacy',
+        },
+        'body': {
+            'Action': 'DescribeInstances',
+            'Version': '2016-11-15',
+            'Filter.1.Name': 'instance-state-name',
+            'Filter.1.Value.1': 'running',
+        },
+        'url': 'https://ec2.us-west-1.amazonaws.com/',
+        'context': {
+            'client_region': 'us-west-1',
+            'client_config': <botocore.config.Config object at 0x144977610>,
+            'has_streaming_input': False,
+            'auth_type': None,
+        },
+    }
+    
+    service_id = 'ec2'
+    """
+
 
 if __name__ == '__main__':
     _main()
