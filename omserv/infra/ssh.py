2fb4cee39ea1041fab0e9164e6d234aa2356ad63
wrmsr <timwilloney@gmail.com>
2024-05-15 17:55:03 -0700

    --
    Former-commit-id: c173247fb588f150f863407e5d2bed8ac298c90b

--- omserv/infra/ssh.py
+++ omserv/infra/ssh.py
@@ -10,13 +10,43 @@ pyOpenSSL
 
 asyncssh[bcrypt,fido2,gssapi,libnacl,pkcs11,pyOpenSSL]
 """
+import abc
 import asyncio
+import typing as ta
 
 import asyncssh
 
+from omlish import dataclasses as dc
+from omlish import lang
 from omlish import logs
 
 from ..secrets import load_secrets
+
+
+class SshCommandRunner(lang.Abstract):
+    @dc.dataclass(frozen=True)
+    class Config:
+        host: ta.Optional[str] = None
+        port: ta.Optional[int] = None
+
+        username: ta.Optional[str] = None
+        password: ta.Optional[str] = None
+
+        key_file_path: ta.Optional[str] = None
+
+
+class CommandRunner(lang.Abstract):
+    @dc.dataclass(frozen=True)
+    class Command:
+        pass
+
+    @dc.dataclass(frozen=True)
+    class CommandResult:
+        rc: int
+
+    @abc.abstractmethod
+    async def run_command(self, cmd: Command) -> CommandResult:
+        raise NotImplementedError
 
 
 async def _a_main() -> None:
