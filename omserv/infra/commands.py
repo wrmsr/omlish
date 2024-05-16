5c13c096b45834183842a4f52e53c60f5c4416a4
wrmsr <timwilloney@gmail.com>
2024-05-15 18:14:56 -0700

    --
    Former-commit-id: 27a7618b83247304d59da224183c58b2b056f0e7

--- omserv/infra/commands.py
+++ omserv/infra/commands.py
@@ -7,11 +7,15 @@ import abc
 import asyncio
 import typing as ta
 
-import asyncssh
-
+from omlish import check
 from omlish import dataclasses as dc
 from omlish import lang
 from omlish import logs
+
+if ta.TYPE_CHECKING:
+    import asyncssh
+else:
+    asyncssh = lang.proxy_import('asyncssh')
 
 from ..secrets import load_secrets
 
@@ -62,11 +66,59 @@ class SshConfig:
     key_file_path: ta.Optional[str] = None
 
 
+class SshSubprocessCommandRunner(CommandRunner):
+    def __init__(
+            self,
+            cfg: SshConfig,
+            lcr: ta.Optional[LocalCommandRunner] = None,
+    ) -> None:
+        super().__init__()
+        self._cfg = check.isinstance(cfg, SshConfig)
+        self._lcr = check.isinstance(lcr, LocalCommandRunner) if lcr is not None else LocalCommandRunner()
+
+    async def run_command(self, cmd: CommandRunner.Command) -> CommandRunner.Result:
+        args = ['ssh']
+
+        if self._cfg.key_file_path is not None:
+            args.extend(['-i', self._cfg.key_file_path])
+
+        if self._cfg.port is not None:
+            args.extend(['-p', str(self._cfg.port)])
+
+        dst = check.non_empty_str(self._cfg.host)
+        if self._cfg.username is not None:
+            dst = f'{self._cfg.username}@{dst}'
+        if self._cfg.password is not None:
+            raise NotImplementedError
+
+        args.append(dst)
+        args.extend(cmd.args)
+        lcmd = CommandRunner.Command(
+            args=args,
+            in_=cmd.in_,
+        )
+        return await self._lcr.run_command(lcmd)
+
+
 async def _a_main() -> None:
-    cr = LocalCommandRunner()
-    rc = await cr.run_command(cr.Command(
-        ['ls', '-al',]
-    ))
+    cmd = CommandRunner.Command(
+        ['ls', '-al'],
+    )
+
+    rc = await LocalCommandRunner().run_command(cmd)
+    check.equal(rc.rc, 0)
+    print(rc.out.decode())
+
+    cfg = load_secrets()
+
+    sc = SshConfig(
+        host=cfg['ec2_ssh_host'],
+        username=cfg['ec2_ssh_user'],
+        key_file_path=cfg['ec2_ssh_key_file'],
+    )
+
+    rc = await SshSubprocessCommandRunner(sc).run_command(cmd)
+    check.equal(rc.rc, 0)
     print(rc.out.decode())
 
 
