9694d8d6b785da192ca5e7ebdf1963937b006ac9
wrmsr <timwilloney@gmail.com>
2024-05-17 12:53:27 -0700

    --
    Former-commit-id: b0d764f46dfcb31507914bb8fe02658c3fa48b79

--- omlish/lang/contextmanagers.py
+++ omlish/lang/contextmanagers.py
@@ -1,5 +1,20 @@
 import contextlib
+import types
 import typing as ta
+
+
+class ContextManaged:
+
+    def __enter__(self: ta.Self) -> ta.Self:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: ta.Optional[ta.Type[Exception]],
+            exc_val: ta.Optional[Exception],
+            exc_tb: ta.Optional[types.TracebackType]
+    ) -> ta.Optional[bool]:
+        return None
 
 
 @contextlib.contextmanager
