824ec97f4d8d826992458a9727a2a0db4f3ddba0
wrmsr <timwilloney@gmail.com>
2024-05-21 16:57:29 -0700

    --
    Former-commit-id: 7c0e2850ab2a55621f6e1019022cfb05e4be9ac1

--- x/deploy/deploy.py
+++ x/deploy/deploy.py
@@ -6,12 +6,18 @@ from omserv.infra import cmds
 
 async def _a_main():
     cwd = tempfile.mkdtemp()
+    print(cwd)
 
     cr: cmds.CommandRunner = cmds.LocalCommandRunner(cmds.LocalCommandRunner.Config(
         cwd=cwd,
     ))
 
-    res = await cr.run_command(cr.Command(['ls', '-al']))
+    res = await cr.run_command(cr.Command([
+        'git',
+        'clone',
+        '--depth', '1',
+        'https://github.com/wrmsr/omlish',
+    ]))
     res.check()
 
     print(res.out.decode())
