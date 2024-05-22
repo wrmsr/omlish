c795ca3601ca2d1c445f0466d55ad4ddac460fc4
wrmsr <timwilloney@gmail.com>
2024-05-22 16:59:22 -0700

    --

--- x/ts/ts.py
+++ x/ts/ts.py
@@ -1,5 +1,32 @@
 """
 https://github.com/tailscale/tailscale/blob/main/api.md
 
-curl -su "$TAILSCALE_API_KEY:" https://api.tailscale.com/api/v2/device/{barf}
+curl -su "$TAILSCALE_API_KEY:" https://api.tailscale.com/api/v2/tailnet/-/devices
 """
+import datetime
+
+from omlish import dataclasses as dc
+
+
+@dc.dataclass(frozen=True)
+class Device:
+    addresses: list[str]
+    authorized: bool
+    blocksIncomingConnections: bool
+    clientVersion: str
+    created: datetime.datetime
+    expires: datetime.datetime
+    hostname: str
+    id: str
+    isExternal: bool
+    keyExpiryDisabled: bool
+    lastSeen: datetime.datetime
+    machineKey: str
+    name: str
+    nodeId: str
+    nodeKey: str
+    os: str
+    tailnetLockError: str
+    tailnetLockKey: str
+    updateAvailable: bool
+    user: bool
