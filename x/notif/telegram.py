e77642f54c7f896360d1afb227a998eaf515b490
wrmsr <timwilloney@gmail.com>
2024-05-23 15:38:53 -0700

    --
    Former-commit-id: 8407363558dae1a76fffddc02d83edfb8e8255df

---
+++ x/notif/telegram.py
@@ -0,0 +1,26 @@
+"""
+curl \
+    -H 'Content-Type: application/json' \
+    "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
+    -d "{\"chat_id\":\"$TELEGRAM_CHAT_ID\",\"text\":\"hi\"}"
+"""
+import asyncio
+
+from omserv.secrets import load_secrets
+
+import telegram
+
+
+async def _a_main():
+    token = load_secrets()['telegram_token']
+
+    bot = telegram.Bot(token)
+    async with bot:
+        print(await bot.get_me())
+
+        updates = list(await bot.get_updates())
+        print(updates)
+
+
+if __name__ == '__main__':
+    asyncio.run(_a_main())
