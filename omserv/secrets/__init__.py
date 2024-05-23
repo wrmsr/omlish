00f091db0ac534c9fa381e00c69645151212e13c
wrmsr <timwilloney@gmail.com>
2024-05-22 17:29:51 -0700

    --
    Former-commit-id: 92b1a8834cec0647ca0c50c64c8024d38c1d19c2

--- omserv/secrets/__init__.py
+++ omserv/secrets/__init__.py
@@ -1,6 +1,8 @@
+import typing as ta
+
 import yaml
 
 
-def load_secrets() -> dict:
+def load_secrets() -> dict[str, ta.Any]:
     with open('secrets.yml', 'r') as f:
         return yaml.safe_load(f)
