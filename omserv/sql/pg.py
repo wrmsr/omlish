7afe5e5c67d9d7807889ae3a2f8f349ee0cde4e6
wrmsr <timwilloney@gmail.com>
2024-05-23 18:19:31 -0700

    --
    Former-commit-id: b30a4cf1f3209b219763785bf78e1dd2d38c7fb5

---
+++ omserv/sql/pg.py
@@ -0,0 +1,46 @@
+import asyncio
+
+import sqlalchemy as sa
+import sqlalchemy.ext.asyncio
+
+from ..secrets import load_secrets  # noqa
+
+
+meta = sa.MetaData()
+t1 = sa.Table(
+    't1',
+    meta,
+    sa.Column('name', sa.String(50), primary_key=True),
+)
+
+
+async def _a_main() -> None:
+    # cfg = load_secrets()
+
+    port = 35225
+
+    engine = sa.ext.asyncio.create_async_engine(f'postgresql+asyncpg://omlish:omlish@localhost:{port}', echo=True)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(meta.drop_all)
+        await conn.run_sync(meta.create_all)
+
+        await conn.execute(
+            t1.insert(), [
+                {'name': 'some name 1'},
+                {'name': 'some name 2'},
+            ]
+        )
+
+    async with engine.connect() as conn:
+        # sa.select a Result, which will be delivered with buffered results
+        result = await conn.execute(sa.select(t1).where(t1.c.name == 'some name 1'))
+
+        print(result.fetchall())
+
+    # for AsyncEngine created in function scope, close and clean-up pooled connections
+    await engine.dispose()
+
+
+if __name__ == '__main__':
+    asyncio.run(_a_main())
