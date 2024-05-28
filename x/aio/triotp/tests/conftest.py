f2aae06d484398a4fe93a49e0980dedd17219f30
wrmsr <timwilloney@gmail.com>
2024-05-28 14:24:04 -0700

    --

---
+++ x/aio/triotp/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from .. import mailbox, application
+import trio
+
+import logbook
+
+from pytest_trio.enable_trio_mode import *
+
+
+@pytest.fixture
+def log_handler():
+    handler = logbook.TestHandler(level=logbook.DEBUG)
+
+    with handler.applicationbound():
+        yield handler
+
+
+@pytest.fixture
+def mailbox_env():
+    mailbox._init()
