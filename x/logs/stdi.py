ce9bba96ac04f6f409419383ceaded3dc038f36a
wrmsr <timwilloney@gmail.com>
2024-05-28 18:29:15 -0700

    --

--- x/logs/stdi.py
+++ x/logs/stdi.py
@@ -5,6 +5,11 @@ import typing as ta  # noqa
 from omlish import logs
 
 
+##
+
+Level: ta.TypeAlias = int
+
+
 ExceptionInfo: ta.TypeAlias = tuple[type[BaseException], BaseException, types.TracebackType]
 
 
@@ -13,7 +18,7 @@ class LogRecord:
     msg: str
     args: tuple
     levelname: str
-    levelno: int
+    levelno: Level
     pathname: str
     filename: str
     module: str
@@ -29,6 +34,108 @@ class LogRecord:
     threadName: str
     processName: str
     process: int
+
+
+##
+
+
+class Formatter:
+    default_time_format: ta.ClassVar[str]
+    default_msec_format: ta.ClassVar[str]
+
+    def formatTime(self, record: LogRecord, datefmt: str | None = None) -> str: ...
+
+    def formatException(self, ei: ExceptionInfo) -> str: ...
+
+    def usesTime(self) -> bool: ...
+
+    def formatMessage(self, record: LogRecord) -> str: ...
+
+    def formatStack(self, stack_info: str) -> str: ...
+
+    def format(self, record: LogRecord) -> str: ...
+
+
+class BufferingFormatter:
+
+    def formatHeader(self, records: ta.Sequence[LogRecord]) -> str: ...
+
+    def formatFooter(self, records: ta.Sequence[LogRecord]) -> str: ...
+
+    def format(self, records: ta.Sequence[LogRecord]) -> str: ...
+
+
+##
+
+
+class Filter:
+    def filter(self, record: LogRecord) -> bool: ...
+
+
+class Filterer:
+    def addFilter(self, filter: Filter) -> None: ...
+
+    def removeFilter(self, filter: Filter) -> None: ...
+
+    def filter(self, record: LogRecord) -> bool: ...
+
+
+##
+
+
+class Handler:
+    level: Level
+
+    def get_name(self) -> str: ...
+
+    def set_name(self, name: str) -> None: ...
+
+    name: str
+
+    def createLock(self) -> None: ...
+
+    def acquire(self) -> None: ...
+
+    def release(self) -> None: ...
+
+    def setLevel(self, level: Level) -> None: ...
+
+    def format(self, record: LogRecord) -> str: ...
+
+    def emit(self, record: LogRecord) -> None: ...
+
+    def handle(self, record: LogRecord) -> bool: ...
+
+    def setFormatter(self, fmt: Formatter) -> None: ...
+
+    def flush(self) -> None: ...
+
+    def close(self) -> None: ...
+
+    def handleError(self, record: LogRecord) -> None: ...
+
+
+class Stream(ta.Protocol):
+    def write(self, s: str) -> None: ...
+
+    def flush(self) -> None: ...  # OPTIONAL METHOD
+
+    def close(self) -> None: ...  # OPTIONAL METHOD
+
+
+class StreamHandler(Handler):
+    terminator: ta.ClassVar[str]
+
+    stream: Stream
+
+    def flush(self) -> None: ...
+
+    def emit(self, record: LogRecord) -> None: ...
+
+    def setStream(self, stream: Stream) -> None: ...
+
+
+##
 
 
 def _main():
