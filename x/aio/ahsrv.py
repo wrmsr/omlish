906b161e94f63f510611f2871470bc0ab1c19a8a
wrmsr <timwilloney@gmail.com>
2024-05-29 13:48:19 -0700

    --

---
+++ x/aio/ahsrv.py
@@ -0,0 +1,37 @@
+import abc
+import asyncio
+import typing as ta
+
+import aiohttp.web as aw
+
+
+class Handler(abc.ABC):
+    @abc.abstractmethod
+    async def __call__(self, request: aw.Request) -> aw.StreamResponse:
+        raise NotImplementedError
+
+
+class HelloHandler(Handler):
+    async def __call__(self, request: aw.Request) -> aw.StreamResponse:
+        return aw.Response(text='Hello, World!')
+
+
+async def a_run_app(app: ta.Any, **kwargs: ta.Any) -> None:
+    main_task = asyncio.get_event_loop().create_task(aw._run_app(app, **kwargs))  # noqa
+    try:
+        await main_task
+    except (aw.GracefulExit, KeyboardInterrupt):  # pragma: no cover
+        pass
+    finally:
+        main_task.cancel()
+
+
+async def _a_main() -> None:
+    app = aw.Application()
+    app.add_routes([aw.get('/', HelloHandler())])
+
+    await a_run_app(app)
+
+
+if __name__ == '__main__':
+    asyncio.run(_a_main())
