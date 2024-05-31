39afbbf9b30eda87de2084dd69768c8904e43bcb
wrmsr <timwilloney@gmail.com>
2024-05-31 13:45:20 -0700

    --

--- x/cy/_math.pyx
+++ x/cy/_math.pyx
@@ -1,17 +1,17 @@
-cdef get_bit(bit: int, value: int) -> int:
+cpdef long get_bit(int bit, long value):
     return (value >> bit) & 1
 
 
-cdef get_bits(bits_from: int, num_bits: int, value: int) -> int:
+cpdef long get_bits(int bits_from, int num_bits, long value):
     return (value & ((1 << (bits_from + num_bits)) - 1)) >> bits_from
 
 
-cdef set_bit(bit: int, bit_value: int, value: int) -> int:
+cpdef long set_bit(int bit, int bit_value, long value):
     if bit_value:
         return value | (1 << bit)
     else:
         return value & ~(1 << bit)
 
 
-cdef set_bits(bits_from: int, num_bits: int, bits_value: int, value: int) -> int:
+cpdef long set_bits(int bits_from, int num_bits, long bits_value, long value):
     return value & ~(((1 << num_bits) - 1) << bits_from) | (bits_value << bits_from)
