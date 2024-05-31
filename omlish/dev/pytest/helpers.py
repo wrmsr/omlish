30d15d3ca39894033a964c19a75128ed5c6ac4cf
wrmsr <timwilloney@gmail.com>
2024-05-31 14:01:09 -0700

    --

--- omlish/dev/pytest/helpers.py
+++ omlish/dev/pytest/helpers.py
@@ -25,8 +25,6 @@ def assert_raises_star(et):
     num_caught = 0
     try:
         yield
-    except* et as eg:
+    except* et as eg:  # noqa
         num_caught += 1
     assert num_caught > 0
-
-
