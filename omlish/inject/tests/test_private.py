ebb429fdc5c011f0b7ddd22664252d7e9f8018e8
wrmsr <timwilloney@gmail.com>
2024-06-05 14:33:07 -0700

    --

--- omlish/inject/tests/test_private.py
+++ omlish/inject/tests/test_private.py
@@ -3,16 +3,24 @@ import typing as ta
 from ... import inject as inj  # noqa
 
 
-def private(*args: ta.Any) -> inj.Bindings:
-    raise NotImplementedError
+class _PrivateBindings(ta.NamedTuple):
+    bs: inj.Bindings
+
+
+def private(*args: ta.Any) -> inj.Binding:
+    return inj.as_(inj.array(_PrivateBindings), _PrivateBindings(inj.bind(*args)))
 
 
 class _ExposedKey(ta.NamedTuple):
     key: inj.Key
 
 
-def expose(*args: ta.Any) -> inj.Bindings:
-    return inj.bind(*[inj.as_(inj.array(_ExposedKey), _ExposedKey(inj.as_key(a))) for a in args])
+def expose(arg: ta.Any) -> inj.Binding:
+    return inj.as_(inj.array(_ExposedKey), _ExposedKey(inj.as_key(arg)))
+
+
+def process_private_bindings(bs: inj.Bindings) -> inj.Bindings:
+    raise NotImplementedError
 
 
 def test_private():
@@ -28,6 +36,7 @@ def test_private():
             expose(str),
         ),
     )
+    bs = process_private_bindings(bs)
     i = inj.create_injector(bs)
     assert i.provide(int) == 420
     assert i.provide(str) == 'hi'
