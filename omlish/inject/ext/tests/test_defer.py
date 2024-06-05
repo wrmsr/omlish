de1e0ed06066afe03feb25b6c2a4bde73ad8b583
wrmsr <timwilloney@gmail.com>
2024-06-05 16:06:58 -0700

    --
    Former-commit-id: 06e06652fa7fd25fe8f24735c95c072832cf49d5

---
+++ omlish/inject/ext/tests/test_defer.py
@@ -0,0 +1,25 @@
+import contextlib
+import typing as ta
+
+from .... import inject as inj
+from ...bindings import as_binding
+
+
+def closing(obj: ta.Any) -> inj.Binding:
+    b = as_binding(obj)
+    return b
+
+
+class ClosingThing:
+    def close(self):
+        print(f'{self} closed')
+
+
+def test_defer():
+    i = inj.create_injector(inj.bind(
+        inj.singleton(contextlib.ExitStack),
+        closing()
+    ))
+
+    with i.provide(contextlib.ExitStack) as es:
+        pass
