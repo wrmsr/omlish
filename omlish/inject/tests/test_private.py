4642cf51f929179bfebbdad1175781ebecc5fc07
wrmsr <timwilloney@gmail.com>
2024-06-05 14:39:39 -0700

    --
    Former-commit-id: c2904b7858b621299d47ef12cdb4188bc04e4209

--- omlish/inject/tests/test_private.py
+++ omlish/inject/tests/test_private.py
@@ -1,5 +1,6 @@
 import typing as ta
 
+from ... import check
 from ... import inject as inj  # noqa
 
 
@@ -7,19 +8,32 @@ class _PrivateBindings(ta.NamedTuple):
     bs: inj.Bindings
 
 
+_PRIVATE_ARRAY_KEY = inj.array(_PrivateBindings)
+
+
 def private(*args: ta.Any) -> inj.Binding:
-    return inj.as_(inj.array(_PrivateBindings), _PrivateBindings(inj.bind(*args)))
+    return inj.as_(_PRIVATE_ARRAY_KEY, _PrivateBindings(inj.bind(*args)))
 
 
 class _ExposedKey(ta.NamedTuple):
     key: inj.Key
 
 
+_EXPOSED_ARRAY_KEY = inj.array(_ExposedKey)
+
+
 def expose(arg: ta.Any) -> inj.Binding:
-    return inj.as_(inj.array(_ExposedKey), _ExposedKey(inj.as_key(arg)))
+    return inj.as_(_EXPOSED_ARRAY_KEY, _ExposedKey(inj.as_key(arg)))
 
 
 def process_private_bindings(bs: inj.Bindings) -> inj.Bindings:
+    def process_private(p: _PrivateBindings):
+        raise NotImplementedError
+
+    for b in bs.bindings():
+        if b.key == _PRIVATE_ARRAY_KEY:
+            process_private(b.provider.provider_fn()(None))
+
     raise NotImplementedError
 
 
