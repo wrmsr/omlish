55f9665ab487b58bae595337415e4b3e593d3c31
wrmsr <timwilloney@gmail.com>
2024-06-05 15:36:02 -0700

    --
    Former-commit-id: e9789558d2b2df625077b816392bda5acd9d00ce

---
+++ omlish/inject/ext/tests/test_private.py
@@ -0,0 +1,67 @@
+import typing as ta
+
+import pytest  # noqa
+
+from ...bindings import as_
+from ...bindings import as_key
+from ...bindings import bind
+from ...injector import create_injector
+from ...keys import array
+from ...types import Binding
+from ...types import Bindings
+from ...types import Key
+
+
+class _Private(ta.NamedTuple):
+    bs: Bindings
+
+
+_PRIVATE_ARRAY_KEY = array(_Private)
+
+
+def private(*args: ta.Any) -> Binding:
+    return as_(_PRIVATE_ARRAY_KEY, _Private(bind(*args)))
+
+
+class _Exposed(ta.NamedTuple):
+    key: Key
+
+
+_EXPOSED_ARRAY_KEY = array(_Exposed)
+
+
+def expose(arg: ta.Any) -> Binding:
+    return as_(_EXPOSED_ARRAY_KEY, _Exposed(as_key(arg)))
+
+
+def process_private_bindings(bs: Bindings) -> Bindings:
+    def process_private(p: _Private):
+        for b in p.bs.bindings():
+            if b.key == _EXPOSED_ARRAY_KEY:
+                raise NotImplementedError
+
+    for b in bs.bindings():
+        if b.key == _PRIVATE_ARRAY_KEY:
+            process_private(b.provider.provider_fn()(None))  # type: ignore
+
+    raise NotImplementedError
+
+
+@pytest.mark.skip('fixme')
+def test_private():
+    bs = bind(
+        private(
+            420,
+            12.3,
+            expose(int),
+        ),
+        private(
+            'hi',
+            12.3,
+            expose(str),
+        ),
+    )
+    bs = process_private_bindings(bs)
+    i = create_injector(bs)
+    assert i.provide(int) == 420
+    assert i.provide(str) == 'hi'
