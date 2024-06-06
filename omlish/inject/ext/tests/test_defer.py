43156bbed04c82b062b9edc2d0beace90bbe9717
wrmsr <timwilloney@gmail.com>
2024-06-06 09:40:54 -0700

    --
    Former-commit-id: b3fa1ecda8a52ea59198b6e2f7a6b7c951b08001

--- omlish/inject/ext/tests/test_defer.py
+++ omlish/inject/ext/tests/test_defer.py
@@ -1,13 +1,35 @@
 import contextlib
 import typing as ta
 
+from .... import dataclasses as dc
 from .... import inject as inj
-from ...bindings import as_binding
+from .... import lang
+from ...providers import as_provider
+from ...types import Injector
+from ...types import Key
+from ...types import Provider
+from ...types import ProviderFn
 
 
-def closing(obj: ta.Any) -> inj.Binding:
-    b = as_binding(obj)
-    return b
+@dc.dataclass(frozen=True)
+class ClosingProvider(Provider):
+    p: Provider
+
+    def provided_cls(self, rec: ta.Callable[[Key], type]) -> type:
+        return self.p.provided_cls(rec)
+
+    def provider_fn(self) -> ProviderFn:
+        def pfn(i: Injector) -> ta.Any:
+            v = ufn(i)
+            i[contextlib.ExitStack].enter_context(lang.defer(v.close))  # noqa
+            return v
+
+        ufn = self.p.provider_fn()
+        return pfn
+
+
+def closing(obj: ta.Any) -> inj.Provider:
+    return ClosingProvider(as_provider(obj))
 
 
 class ClosingThing:
@@ -18,8 +40,10 @@ class ClosingThing:
 def test_defer():
     i = inj.create_injector(inj.bind(
         inj.singleton(contextlib.ExitStack),
-        # closing()
+        closing(ClosingThing),
     ))
 
+    print()
     with i[contextlib.ExitStack] as es:  # noqa
-        pass
+        print(i[ClosingThing])
+        print(i[ClosingThing])
