9f463d6827a103f1bc569886a3dc9120f047f4bd
wrmsr <timwilloney@gmail.com>
2024-06-07 15:09:55 -0700

    --
    Former-commit-id: 2736a4786fb28a9e02e8feb420f7619d9699558d

---
+++ omlish/inject/ext/defer.py
@@ -0,0 +1,38 @@
+import contextlib
+import typing as ta
+
+from ... import dataclasses as dc
+from ... import inject as inj
+from ... import lang
+from ..providers import as_provider
+from ..types import Injector
+from ..types import Key
+from ..types import Provider
+from ..types import ProviderFn
+
+
+@dc.dataclass(frozen=True, eq=False)
+class ClosingProvider(Provider):
+    p: Provider
+
+    def provided_cls(self, rec: ta.Callable[[Key], type]) -> type:
+        return self.p.provided_cls(rec)
+
+    def required_keys(self) -> frozenset[Key | None]:
+        return self.p.required_keys()
+
+    def children(self) -> ta.Iterable[Provider]:
+        return (self.p,)
+
+    def provider_fn(self) -> ProviderFn:
+        def pfn(i: Injector) -> ta.Any:
+            v = ufn(i)
+            i[contextlib.ExitStack].enter_context(lang.defer(v.close))  # noqa
+            return v
+
+        ufn = self.p.provider_fn()
+        return pfn
+
+
+def closing(obj: ta.Any) -> inj.Provider:
+    return ClosingProvider(as_provider(obj))
---
+++ omlish/inject/ext/private.py
@@ -0,0 +1,96 @@
+import itertools
+import typing as ta
+
+from ... import cached
+from ... import check
+from ... import dataclasses as dc
+from ... import lang
+from ..bindings import as_
+from ..bindings import as_key
+from ..bindings import bind
+from ..injector import create_injector
+from ..keys import array
+from ..providers import ConstProvider
+from ..providers import SingletonProvider
+from ..types import Binding
+from ..types import Bindings
+from ..types import Injector
+from ..types import Key
+from ..types import Provider
+from ..types import ProviderFn
+
+
+_ANONYMOUS_PRIVATE_SCOPE_COUNT = itertools.count()
+
+PrivateScopeName = ta.NewType('PrivateScopeName', str)
+
+
+@dc.dataclass(frozen=True, eq=False)
+class PrivateScopeProvider(Provider):
+    psn: PrivateScopeName
+    bs: Bindings
+
+    def provided_cls(self, rec: ta.Callable[[Key], type]) -> type:
+        return Injector
+
+    @lang.cached_nullary
+    def required_keys(self) -> frozenset[Key | None]:
+        return frozenset(k for b in self.bs.bindings() for k in b.provider.required_keys())
+
+    def children(self) -> ta.Iterable[Provider]:
+        for b in self.bs.bindings():
+            yield b.provider
+
+    def provider_fn(self) -> ProviderFn:
+        def pfn(i: Injector) -> ta.Any:
+            return create_injector(self.bs, i)
+        return pfn
+
+
+class _Exposed(ta.NamedTuple):
+    key: Key
+
+
+_EXPOSED_ARRAY_KEY = array(_Exposed)
+
+
+def expose(arg: ta.Any) -> Binding:
+    return as_(_EXPOSED_ARRAY_KEY, _Exposed(as_key(arg)))
+
+
+@dc.dataclass(frozen=True, eq=False)
+class ExposedPrivateProvider(Provider):
+    psn: PrivateScopeName
+    k: Key
+
+    @cached.property
+    def pik(self) -> Key:
+        return Key(Injector, tag=self.psn)
+
+    def provided_cls(self, rec: ta.Callable[[Key], type]) -> type:
+        return self.k.cls
+
+    def required_keys(self) -> frozenset[Key | None]:
+        return frozenset([self.pik])
+
+    def children(self) -> ta.Iterable[Provider]:
+        return ()
+
+    def provider_fn(self) -> ProviderFn:
+        def pfn(i: Injector) -> ta.Any:
+            pi = i.provide(self.pik)
+            return pi.provide(self.k)
+        return pfn
+
+
+def private(*args: ta.Any, name: str | None = None) -> Bindings:
+    if name is None:
+        name = f'anon-{next(_ANONYMOUS_PRIVATE_SCOPE_COUNT)}'
+    psn = PrivateScopeName(name)
+    pbs = bind(*args, Binding(Key(PrivateScopeName), ConstProvider(PrivateScopeName, psn)))
+    ebs: list[Binding] = [Binding(Key(Injector, tag=psn), SingletonProvider(PrivateScopeProvider(psn, pbs)))]  # noqa
+    for b in pbs.bindings():
+        if b.key == _EXPOSED_ARRAY_KEY:
+            ek = check.isinstance(check.isinstance(b.provider, ConstProvider).v, _Exposed).key
+            ebs.append(Binding(ek, ExposedPrivateProvider(psn, ek)))
+    return bind(*ebs)
