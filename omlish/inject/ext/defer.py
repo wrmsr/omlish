d8f05f47761e65474cf31f71d9761c56fdaec1e7
wrmsr <timwilloney@gmail.com>
2024-06-07 15:29:45 -0700

    --
    Former-commit-id: 3f02d1d08700e145f9fb24ab671c420301487a1e

--- omlish/inject/ext/defer.py
+++ omlish/inject/ext/defer.py
@@ -5,6 +5,7 @@ from ... import dataclasses as dc
 from ... import inject as inj
 from ... import lang
 from ..providers import as_provider
+from ..types import Bindings
 from ..types import Injector
 from ..types import Key
 from ..types import Provider
@@ -36,3 +37,13 @@ class ClosingProvider(Provider):
 
 def closing(obj: ta.Any) -> inj.Provider:
     return ClosingProvider(as_provider(obj))
+
+
+@contextlib.contextmanager
+def create_defer_injector(bs: Bindings, p: ta.Optional[Injector] = None) -> ta.Generator[Injector, None, None]:
+    i = inj.create_injector(inj.bind(
+        bs,
+        inj.singleton(contextlib.ExitStack),
+    ), p)
+    with i[contextlib.ExitStack]:
+        yield i
