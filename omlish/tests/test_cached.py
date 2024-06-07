56e6399b5439cbf14aeeb72583255f10b97d8a1c
wrmsr <timwilloney@gmail.com>
2024-06-07 15:24:11 -0700

    --
    Former-commit-id: 1a753353c39c784b7dba74ba7c8d39666218e85c

--- omlish/tests/test_cached.py
+++ omlish/tests/test_cached.py
@@ -1,19 +1,6 @@
 from .. import cached
 
 
-def test_nullary():
-    n = 0
-
-    @cached.nullary
-    def fn():
-        nonlocal n
-        n += 1
-        return n
-
-    assert fn() == 1
-    assert fn() == 1
-
-
 def test_property():
     n = 0
 
