ec3a39a2cb77259ccfbe066fb8dc4e226a0c72d6
wrmsr <timwilloney@gmail.com>
2024-06-08 15:41:27 -0700

    --
    Former-commit-id: 0c4aa285526ca29a652e597e01a19a53a2f3e09c

---
+++ omlish/dispatch/tests/__init__.py
@@ -0,0 +1,25 @@
+"""
+.venv/bin/python    -m omlish.dispatch.tests.bench_dispatch 94.141 ns / it
+.venv/bin/python    -m omlish.dispatch.tests.bench_function 205.286 ns / it
+.venv/bin/python    -m omlish.dispatch.tests.bench_method   431.506 ns / it
+
+.venv-12/bin/python -m omlish.dispatch.tests.bench_dispatch 118.369 ns / it
+.venv-12/bin/python -m omlish.dispatch.tests.bench_function 250.851 ns / it
+.venv-12/bin/python -m omlish.dispatch.tests.bench_method   563.102 ns / it
+
+.venv-13/bin/python -m omlish.dispatch.tests.bench_dispatch 108.972 ns / it
+.venv-13/bin/python -m omlish.dispatch.tests.bench_function 251.986 ns / it
+.venv-13/bin/python -m omlish.dispatch.tests.bench_method   542.172 ns / it
+
+.venv/bin/python    -m omlish.dispatch.tests.bench_dispatch 106.281 ns / it
+.venv/bin/python    -m omlish.dispatch.tests.bench_function 195.354 ns / it
+.venv/bin/python    -m omlish.dispatch.tests.bench_method   422.241 ns / it
+
+.venv-12/bin/python -m omlish.dispatch.tests.bench_dispatch 123.884 ns / it
+.venv-12/bin/python -m omlish.dispatch.tests.bench_function 247.556 ns / it
+.venv-12/bin/python -m omlish.dispatch.tests.bench_method   553.816 ns / it
+
+.venv-13/bin/python -m omlish.dispatch.tests.bench_dispatch 122.654 ns / it
+.venv-13/bin/python -m omlish.dispatch.tests.bench_function 243.788 ns / it
+.venv-13/bin/python -m omlish.dispatch.tests.bench_method   544.519 ns / it
+"""
