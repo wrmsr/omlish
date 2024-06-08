555d04b46f914d8e98aad128c1fe87bc3a8c79c9
wrmsr <timwilloney@gmail.com>
2024-06-08 16:26:51 -0700

    --

--- omlish/marshal/tests/test_objects.py
+++ omlish/marshal/tests/test_objects.py
@@ -1,6 +1,12 @@
 import typing as ta
 
 from ... import dataclasses as dc
+from ..base import UnmarshalContext
+from ..objects import FieldInfo
+from ..objects import FieldMetadata
+from ..objects import ObjectUnmarshaler
+from ..primitives import PRIMITIVE_MARSHALER_UNMARSHALER
+from ..registries import Registry
 
 
 @dc.dataclass()
@@ -11,4 +17,21 @@ class C:
 
 
 def test_unknown_fields():
-    pass
+    ou = ObjectUnmarshaler(
+        C,
+        {
+            'i': (
+                FieldInfo(
+
+                ),
+                PRIMITIVE_MARSHALER_UNMARSHALER,
+            ),
+            's': (
+                PRIMITIVE_MARSHALER_UNMARSHALER,
+            ),
+        },
+        unknown_field='x',
+    )
+
+    c = ou.unmarshal(UnmarshalContext(Registry()), {'i': 420, 's': 'foo', 'qqq': 'huh'})
+    print(c)
