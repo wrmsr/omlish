57db1b9fa5c8cd49659acd4e5a4e9cdbf0f310a7
wrmsr <timwilloney@gmail.com>
2024-06-08 00:42:08 -0700

    --
    Former-commit-id: 746b39bde90873c0131fa5bedd182b35fae7fef0

--- omlish/inject/ext/tests/test_threads.py
+++ omlish/inject/ext/tests/test_threads.py
@@ -1,50 +1,12 @@
-import threading
-import typing as ta
-
-from ...providers import CtorProvider
-from ...providers import SingletonProvider
-from ...types import Binding
-from ...types import Injector
-from ...types import Key
-from ...types import Provider
-from ...types import ProviderFn
-
-
-class _ThreadLocals:
-    def __init__(self) -> None:
-        super().__init__()
-        self.tls = threading.local()
-
-
-_THREAD_LOCALS_KEY = Key(_ThreadLocals)
-
-
-class ThreadLocalProvider(Provider):
-    p: Provider
-
-    def provided_cls(self, rec: ta.Callable[[Key], type]) -> type:
-        return self.p.provided_cls()
-
-    def required_keys(self) -> frozenset[Key | None]:
-        return frozenset([*self.p.required_keys(), _THREAD_LOCALS_KEY])
-
-    def children(self) -> ta.Iterable[Provider]:
-        return (self.p,)
-
-    def provider_fn(self) -> ProviderFn:
-        def pfn(i: Injector) -> ta.Any:
-            tls = i.provide(_THREAD_LOCALS_KEY).tls
-            raise NotImplementedError
-        return pfn
-
-
-def thread_local(p: ta.Any) -> Provider:
-    return ThreadLocalProvider(p)
-
-
-def bind_thread_locals() -> Binding:
-    return Binding(_THREAD_LOCALS_KEY, SingletonProvider(CtorProvider(_ThreadLocals)))
+from ...bindings import bind
+from ...injector import create_injector
+from ..threads import bind_thread_locals
+from ..threads import thread_local
 
 
 def test_threads():
-    pass
+    i = create_injector(bind(
+        thread_local(420),
+        bind_thread_locals(),
+    ))
+    assert i[int] == 420
