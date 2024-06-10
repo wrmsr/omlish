280f6345becf62f01fd305ab3992afcd9b066725
wrmsr <timwilloney@gmail.com>
2024-06-10 12:56:52 -0700

    --
    Former-commit-id: aee8196fa1c08d35f46db4f2e1ed9329b5498eda

--- omlish/marshal/tests/test_objects.py
+++ omlish/marshal/tests/test_objects.py
@@ -1,9 +1,11 @@
 import typing as ta
 
 from ... import dataclasses as dc
+from ..base import MarshalContext
 from ..base import UnmarshalContext
 from ..objects import FieldInfo
 from ..objects import FieldMetadata
+from ..objects import ObjectMarshaler
 from ..objects import ObjectUnmarshaler
 from ..primitives import PRIMITIVE_MARSHALER_UNMARSHALER
 from ..registries import Registry
@@ -17,21 +19,34 @@ class C:
 
 
 def test_unknown_fields():
+    fis = [
+        FieldInfo(
+            'i',
+            int,
+            FieldMetadata(),
+            'i',
+            ['i'],
+        ),
+        FieldInfo(
+            's',
+            str,
+            FieldMetadata(),
+            's',
+            ['s'],
+        ),
+    ]
+
     ou = ObjectUnmarshaler(
         C,
-        {
-            'i': (
-                FieldInfo(
-
-                ),
-                PRIMITIVE_MARSHALER_UNMARSHALER,
-            ),
-            's': (
-                PRIMITIVE_MARSHALER_UNMARSHALER,
-            ),
-        },
+        {fi.name: (fi, PRIMITIVE_MARSHALER_UNMARSHALER) for fi in fis},
         unknown_field='x',
     )
-
     c = ou.unmarshal(UnmarshalContext(Registry()), {'i': 420, 's': 'foo', 'qqq': 'huh'})
     print(c)
+
+    om = ObjectMarshaler(
+        [(fi, PRIMITIVE_MARSHALER_UNMARSHALER) for fi in fis],
+        unknown_field='x',
+    )
+    o = om.marshal(MarshalContext(Registry()), c)
+    print(o)
