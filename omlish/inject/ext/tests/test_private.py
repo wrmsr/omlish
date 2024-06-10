8f1fe8f45c220b802a6af34b8c365a7677b85586
wrmsr <timwilloney@gmail.com>
2024-06-10 13:59:23 -0700

    --

--- omlish/inject/ext/tests/test_private.py
+++ omlish/inject/ext/tests/test_private.py
@@ -1,9 +1,11 @@
-from ..private import expose
-from ..private import private
-from ...bindings import bind
-from ...injector import create_injector
 from .... import dataclasses as dc
 from .... import lang
+from ...bindings import as_
+from ...bindings import bind
+from ...injector import create_injector
+from ...keys import array
+from ..private import expose
+from ..private import private
 
 
 @dc.dataclass(frozen=True)
@@ -20,11 +22,29 @@ def test_private():
         ),
         private(
             lang.typed_lambda(str, f=float, foo=Foo)(lambda f, foo: f'{f}! {foo.s}'),
-            12.3,
+            32.1,
             expose(str),
         ),
         Foo('foo'),
     )
     i = create_injector(bs)
     assert i.provide(int) == 420
-    assert i.provide(str) == '12.3! foo'
+    assert i.provide(str) == '32.1! foo'
+
+
+def test_private_array():
+    bs = bind(
+        private(
+            as_(array(str), 'hi'),
+            12.3,
+            expose(array(str)),
+        ),
+        private(
+            as_(array(str), lang.typed_lambda(str, f=float, foo=Foo)(lambda f, foo: f'{f}! {foo.s}')),
+            32.1,
+            expose(array(str)),
+        ),
+        Foo('foo'),
+    )
+    i = create_injector(bs)
+    assert sorted(i.provide(array(str))) == ['32.1! foo', 'hi']
