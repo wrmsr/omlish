80b69f0c8fe6e03c5b4a7307adcaf4d588a6aee4
wrmsr <timwilloney@gmail.com>
2024-06-10 13:39:22 -0700

    --
    Former-commit-id: e5fb530f8941bf70be5cf0f29491ae2574cfb36a

---
+++ omlish/dataclasses/impl/processing.py
@@ -0,0 +1,16 @@
+from ... import lang
+from .reflect import ClassInfo
+
+
+class Processor(lang.Abstract):
+    def __init__(self, info: ClassInfo) -> None:
+        super().__init__()
+        self._cls = info.cls
+        self._info = info
+
+    @lang.cached_nullary
+    def process(self) -> None:
+        self._process()
+
+    def _process(self) -> None:
+        raise NotImplementedError
---
+++ omlish/dataclasses/impl/simple.py
@@ -0,0 +1,142 @@
+import inspect
+
+from ... import lang
+from .classes import cmp_fn
+from .classes import frozen_get_del_attr
+from .classes import repr_fn
+from .fields import field_assign
+from .init import get_init_fields
+from .params import get_field_extras
+from .processing import Processor
+from .replace import _replace
+from .utils import create_fn
+from .utils import set_new_attribute
+from .utils import tuple_str
+
+
+class OverridesProcessor(Processor):
+    def _process(self) -> None:
+        for f in self._info.instance_fields:
+            fx = get_field_extras(f)
+            if not fx.override:
+                continue
+
+            if self._info.params12.slots:
+                raise TypeError
+
+            self_name = '__dataclass_self__' if 'self' in self._info.fields else 'self'
+
+            getter = create_fn(
+                f.name,
+                (self_name,),
+                [f'return {self_name}.__dict__[{f.name!r}]'],
+                globals=self._info.globals,
+                return_type=lang.just(f.type),
+            )
+            prop = property(getter)
+
+            if not self._info.params.frozen:
+                setter = create_fn(
+                    f.name,
+                    (self_name, f'{f.name}: __dataclass_type_{f.name}__'),
+                    [field_assign(self._info.params.frozen, f.name, f.name, self_name, fx.override)],
+                    globals=self._info.globals,
+                    locals={f'__dataclass_type_{f.name}__': f.type},
+                    return_type=lang.just(None),
+                )
+                prop = prop.setter(setter)
+
+            set_new_attribute(
+                self._cls,
+                f.name,
+                prop,
+            )
+
+
+class ReprProcessor(Processor):
+    def _process(self) -> None:
+        if not self._info.params.repr:
+            return
+
+        flds = [f for f in self._info.instance_fields if f.repr]
+        set_new_attribute(self._cls, '__repr__', repr_fn(flds, self._info.globals))
+
+
+class EqProcessor(Processor):
+    def _process(self) -> None:
+        if not self._info.params.eq:
+            return
+
+        # flds = [f for f in self._info.instance_fields if f.compare]
+        # self_tuple = tuple_str('self', flds)
+        # other_tuple = tuple_str('other', flds)
+        # set_new_attribute(cls, '__eq__', _cmp_fn('__eq__', '==', self_tuple, other_tuple, globals=globals))
+        cmp_fields = (field for field in self._info.instance_fields if field.compare)
+        terms = [f'self.{field.name} == other.{field.name}' for field in cmp_fields]
+        field_comparisons = ' and '.join(terms) or 'True'
+        body = [
+            f'if self is other:',
+            f' return True',
+            f'if other.__class__ is self.__class__:',
+            f' return {field_comparisons}',
+            f'return NotImplemented',
+        ]
+        func = create_fn('__eq__', ('self', 'other'), body, globals=self._info.globals)
+        set_new_attribute(self._cls, '__eq__', func)
+
+
+class OrderProcessor(Processor):
+    def _process(self) -> None:
+        if not self._info.params.order:
+            return
+
+        flds = [f for f in self._info.instance_fields if f.compare]
+        self_tuple = tuple_str('self', flds)
+        other_tuple = tuple_str('other', flds)
+        for name, op in [
+            ('__lt__', '<'),
+            ('__le__', '<='),
+            ('__gt__', '>'),
+            ('__ge__', '>='),
+        ]:
+            if set_new_attribute(self._cls, name, cmp_fn(name, op, self_tuple, other_tuple, globals=self._info.globals)):  # noqa
+                raise TypeError(
+                    f'Cannot overwrite attribute {name} in class {self._cls.__name__}. '
+                    f'Consider using functools.total_ordering'
+                )
+
+
+class FrozenProcessor(Processor):
+    def _process(self) -> None:
+        if not self._info.params.frozen:
+            return
+
+        for fn in frozen_get_del_attr(self._cls, self._info.instance_fields, self._info.globals):
+            if set_new_attribute(self._cls, fn.__name__, fn):
+                raise TypeError(f'Cannot overwrite attribute {fn.__name__} in class {self._cls.__name__}')
+
+
+class DocProcessor(Processor):
+    def _process(self) -> None:
+        if getattr(self._cls, '__doc__'):
+            return
+
+        try:
+            text_sig = str(inspect.signature(self._cls)).replace(' -> None', '')
+        except (TypeError, ValueError):
+            text_sig = ''
+        self._cls.__doc__ = (self._cls.__name__ + text_sig)
+
+
+class MatchArgsProcessor(Processor):
+    def _process(self) -> None:
+        if not self._info.params12.match_args:
+            return
+
+        ifs = get_init_fields(self._info.fields.values())
+        set_new_attribute(self._cls, '__match_args__', tuple(f.name for f in ifs.std))
+
+
+class ReplaceProcessor(Processor):
+    def _process(self) -> None:
+        set_new_attribute(self._cls, '__replace__', _replace)
