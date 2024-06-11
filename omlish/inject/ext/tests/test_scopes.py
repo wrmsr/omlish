bc6996481a5dee4f68c2ea9f5b0e4539b9f2e376
wrmsr <timwilloney@gmail.com>
2024-06-10 23:53:50 -0700

    --
    Former-commit-id: 53745c5d4b7cb78d7b22d7694577fa4fbcd17909

--- omlish/inject/ext/tests/test_scopes.py
+++ omlish/inject/ext/tests/test_scopes.py
@@ -5,15 +5,57 @@ TODO:
  - scope-eagers
  - in_(request_scope, ...)
 """
+import typing as ta
+
+from .... import dataclasses as dc
+from ...bindings import bind
+from ...injector import create_injector
+from ...providers import SingletonProvider
+from ...providers import as_provider
+from ...providers import ctor
+from ...types import Binding
+from ...types import Cls
+from ...types import Injector
+from ...types import Key
+from ...types import Provider
+from ...types import ProviderFn
 
 
-class RequestScope:
-    pass
+class _SimpleScope:
+    provisions: dict[Key, ta.Any]
 
 
-class QueryScope:
-    pass
+_SIMPLE_SCOPE_KEY = Key(_SimpleScope)
+
+
+@dc.dataclass(frozen=True, eq=False)
+class SimpleScopedProvider(Provider):
+    p: Provider
+
+    def provided_cls(self, rec: ta.Callable[[Key], Cls]) -> Cls:
+        return self.p.provided_cls(rec)
+
+    def required_keys(self) -> frozenset[Key | None]:
+        return frozenset([*self.p.required_keys(), _SIMPLE_SCOPE_KEY])
+
+    def children(self) -> ta.Iterable[Provider]:
+        return (self.p,)
+
+    def provider_fn(self) -> ProviderFn:
+        raise NotImplementedError
+
+
+def simple_scoped(p: ta.Any) -> Provider:
+    return SimpleScopedProvider(as_provider(p))
+
+
+def bind_simple_scope() -> Binding:
+    return Binding(_SIMPLE_SCOPE_KEY, SingletonProvider(ctor(_SimpleScope)))
 
 
 def test_scopes():
-    pass
+    i = create_injector(bind(
+        simple_scoped(420),
+        bind_simple_scope(),
+    ))
+    assert i[int] == 420
