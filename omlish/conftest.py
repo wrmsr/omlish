f6f43b302ef73d78d9e691b58901865d71ea7bcb
wrmsr <timwilloney@gmail.com>
2024-06-11 16:22:10 -0700

    --
    Former-commit-id: 2334b2f1cc95bc6e3b32dccac2f92b0f8011602a

--- omlish/conftest.py
+++ omlish/conftest.py
@@ -1,5 +1,6 @@
 from .dev import pytest as ptu  # noqa
 from .inject.dev import pytest as pti  # noqa
+from .inject.dev.pytest.harness import harness  # noqa
 
 
 def pytest_addhooks(pluginmanager):
