b213a0268a80b168c3075e0165f3fcdf7b1a25cc
wrmsr <timwilloney@gmail.com>
2024-06-15 18:53:06 -0700

    --
    Former-commit-id: 171093f2913b99ff67c9f5ff815db22e0096927c

--- x/infra/systemd/systemd.py
+++ x/infra/systemd/systemd.py
@@ -1,6 +1,16 @@
 import ctypes as ct
 
 
+class iovec(ct.Structure):
+    pass
+
+
+iovec._fields_ = [
+    ('iov_base', ct.c_void_p),  # Pointer to data.
+    ('iov_len', ct.c_size_t),  # Length of data.
+]
+
+
 def _main():
     pass
 
