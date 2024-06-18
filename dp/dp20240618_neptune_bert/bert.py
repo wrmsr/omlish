d523b0ba65b730388037f418dcedd1e3f614f99e
wrmsr <timwilloney@gmail.com>
2024-06-18 15:14:01 -0700

    --
    Former-commit-id: 01b455d4b1495b3d608d49495f7d69132ba16d45

--- dp/dp20240618_neptune_bert/bert.py
+++ dp/dp20240618_neptune_bert/bert.py
@@ -126,8 +126,8 @@ def get_attn_pad_mask(
     batch_size, len_q = seq_q.size()
     batch_size, len_k = seq_k.size()
     # eq(zero) is PAD token
-    pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # batch_size x 1 x len_k(=len_q), one is masking
-    return pad_attn_mask.expand(batch_size, len_q, len_k)  # batch_size x len_q x len_k
+    pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # (batch_size x 1 x len_k(=len_q)), one is masking
+    return pad_attn_mask.expand(batch_size, len_q, len_k)  # (batch_size x len_q x len_k)
 
 
 def gelu(x: torch.tensor) -> torch.Tensor:
