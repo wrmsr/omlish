fc82c5977d91052cccf16e1d2cf21ace0a054e3d
wrmsr <timwilloney@gmail.com>
2024-06-18 17:58:02 -0700

    --

--- omlish/inject2/impl/elements.py
+++ omlish/inject2/impl/elements.py
@@ -2,10 +2,16 @@ import typing as ta
 
 from ... import lang
 from ..elements import Element
+from ..keys import Key
+from .providers import ProviderImpl
 
 
 class ElementCollection(lang.Final):
     def __init__(self, es: ta.Iterable[Element]) -> None:
         super().__init__()
 
-        lst = list(es)
+        self._lst = list(es)
+
+    @lang.cached_nullary
+    def provider_map(self) -> ta.Mapping[Key, ProviderImpl]:
+        raise NotImplementedError
