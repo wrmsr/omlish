6db25ea215bbac434e1973bd94000ef31bdf3c37
wrmsr <timwilloney@gmail.com>
2024-06-18 18:11:10 -0700

    --
    Former-commit-id: 0943ec18c210e4209c6a230e0fd021a74be9be4b

--- omlish/inject2/impl/elements.py
+++ omlish/inject2/impl/elements.py
@@ -2,19 +2,29 @@ import typing as ta
 
 from ... import check
 from ... import lang
+from ..bindings import Binding
 from ..elements import Element
 from ..elements import Elements
+from ..exceptions import DuplicateKeyException
 from ..keys import Key
 from .providers import ProviderImpl
+from .providers import make_provider_impl
 
 
 class ElementCollection(lang.Final):
-    def __init__(self, es: Elements) -> None:
+    def __init__(self, src: Elements) -> None:
         super().__init__()
 
-        self._es = check.isinstance(es, Elements)
-        self._lst: ta.Sequence[Element] = list(es)
+        self._src = check.isinstance(src, Elements)
+        self._es: ta.Sequence[Element] = list(src)
 
     @lang.cached_nullary
     def provider_map(self) -> ta.Mapping[Key, ProviderImpl]:
-        raise NotImplementedError
+        pm: dict[Key, ProviderImpl] = {}
+        for e in self._es:
+            if isinstance(e, Binding):
+                if e.key in pm:
+                    raise DuplicateKeyException(e.key)
+                p = make_provider_impl(e.provider)
+                pm[e.key] = p
+        return pm
