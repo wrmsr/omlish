5651d26eb4462ef3e4309fdfac0207c51d46e839
wrmsr <timwilloney@gmail.com>
2024-06-18 18:01:43 -0700

    --
    Former-commit-id: 3069723a1848238a5ddf1defb4ea690056ddc1c1

--- omlish/inject2/impl/elements.py
+++ omlish/inject2/impl/elements.py
@@ -1,16 +1,19 @@
 import typing as ta
 
+from ... import check
 from ... import lang
 from ..elements import Element
+from ..elements import Elements
 from ..keys import Key
 from .providers import ProviderImpl
 
 
 class ElementCollection(lang.Final):
-    def __init__(self, es: ta.Iterable[Element]) -> None:
+    def __init__(self, es: Elements) -> None:
         super().__init__()
 
-        self._lst = list(es)
+        self._es = check.isinstance(es, Elements)
+        self._lst: ta.Sequence[Element] = list(es)
 
     @lang.cached_nullary
     def provider_map(self) -> ta.Mapping[Key, ProviderImpl]:
