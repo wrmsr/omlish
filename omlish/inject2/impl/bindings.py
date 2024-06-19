9bf8951269f928e30fa6466469b4531c5b1958f2
wrmsr <timwilloney@gmail.com>
2024-06-19 12:06:27 -0700

    --
    Former-commit-id: ed6614b0f78055c5f35f39ae7ebae20d4ea5edec

---
+++ omlish/inject2/impl/bindings.py
@@ -0,0 +1,18 @@
+from ... import dataclasses as dc
+from ... import lang
+from ..bindings import Binding
+from ..keys import Key
+from .providers import ProviderImpl
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+@dc.extra_params(cache_hash=True)
+class BindingImpl(lang.Final):
+    key: Key
+    provider: ProviderImpl
+    # scope: ScopeTag
+    binding: Binding | None = None
+
