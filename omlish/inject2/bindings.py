e74ce380b03b1ec5b3ee02766a343fc5b8d743ac
wrmsr <timwilloney@gmail.com>
2024-06-18 17:10:28 -0700

    --
    Former-commit-id: db071c746bb11ad9f3ea6e31de05b024bfd6f701

--- omlish/inject2/bindings.py
+++ omlish/inject2/bindings.py
@@ -23,15 +23,11 @@ from .types import _ProviderGen
 
 def as_binding(o: ta.Any) -> Binding:
     check.not_none(o)
-    check.not_isinstance(o, Bindings)
+    check.not_isinstance(o, (Element, Elements))
     if isinstance(o, Binding):
         return o
-    if isinstance(o, _BindingGen):
-        return o._gen_binding()  # noqa
     if isinstance(o, Provider):
         return Binding(Key(o.provided_cls(lambda _: lang.raise_(TypeError(o)))), o)  # noqa
-    if isinstance(o, _ProviderGen):
-        return as_binding(o._gen_provider())  # noqa
     if isinstance(o, type):
         return as_binding(ctor_provider(o))
     if callable(o):
@@ -65,23 +61,3 @@ def bind(*args: ta.Any) -> Bindings:
             bs.append(as_binding(a))
     # This will contain a stacktrace, so no unnesting.
     return _Bindings(bs=bs, ps=ps)
-
-
-##
-
-
-def build_provider_map(bs: Bindings) -> ta.Mapping[Key, Provider]:
-    pm: dict[Key, Provider] = {}
-    am: dict[Key, list[Provider]] = {}
-    for b in bs.bindings():
-        if b.key.multi:
-            am.setdefault(b.key, []).append(b.provider)
-        else:
-            if b.key in pm:
-                raise KeyError(b.key)
-            pm[b.key] = b.provider
-    if am:
-        for k, aps in am.items():
-            pm[k] = multi_provider(k.cls, *aps)
-
-    return pm
