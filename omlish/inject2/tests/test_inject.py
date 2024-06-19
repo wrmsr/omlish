a4af43af179149a1edebf02637db1e47e9832a11
wrmsr <timwilloney@gmail.com>
2024-06-18 18:22:34 -0700

    --
    Former-commit-id: 522526edda60d85d8e505eb738ac73b9d5cc6c68

--- omlish/inject2/tests/test_inject.py
+++ omlish/inject2/tests/test_inject.py
@@ -1,6 +1,7 @@
 import pprint
 
 from ... import inject2 as inj
+from ... import lang
 from ..impl.elements import ElementCollection
 from ..impl.injector import InjectorImpl
 
@@ -8,6 +9,7 @@ from ..impl.injector import InjectorImpl
 def test_inject():
     es = inj.Elements([
         inj.as_binding(420),
+        inj.as_binding(lang.typed_lambda(str, i=int)(lambda i: str(i))),
     ])
 
     pprint.pprint(es)
@@ -15,3 +17,4 @@ def test_inject():
     ec = ElementCollection(es)
     i = InjectorImpl(ec)
     assert i.provide(int) == 420
+    assert i.provide(str) == '420'
