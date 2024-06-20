5ec58f8d84ee7b1a3742c62b2be163aa965c2dbd
wrmsr <timwilloney@gmail.com>
2024-06-19 20:29:58 -0700

    --

--- omlish/inject2/eager.py
+++ omlish/inject2/eager.py
@@ -1,13 +1,10 @@
 import typing as ta
 
-from .. import check
 from .. import dataclasses as dc
 from .. import lang
-from .bindings import Binding
-from .bindings import as_binding
 from .elements import Element
-from .elements import Elements
 from .keys import Key
+from .keys import as_key
 
 
 @dc.dataclass(frozen=True)
@@ -15,6 +12,5 @@ class Eager(Element, lang.Final):
     key: Key
 
 
-def eager(*args: ta.Any) -> Elements:
-    bs = [check.isinstance(as_binding(a), Binding) for a in args]
-    return Elements([*bs, *(Eager(b.key) for b in bs)])
+def eager(k: ta.Any) -> Element:
+    return Eager(as_key(k))
