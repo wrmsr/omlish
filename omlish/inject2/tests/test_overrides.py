4ee3c02890c4fb078c037c2a6cf73b9628ef670f
wrmsr <timwilloney@gmail.com>
2024-06-26 16:08:57 -0700

    --
    Former-commit-id: 9be7dae031951f43bec9060476fd67c15a459cb1

---
+++ omlish/inject2/tests/test_overrides.py
@@ -0,0 +1,17 @@
+from ... import inject2 as inj
+from ... import lang
+
+
+def test_override():
+    es = inj.as_elements(
+        inj.override(
+            inj.as_binding(421),
+            inj.as_binding(420),
+        ),
+        inj.as_binding(5.2),
+        inj.as_binding(lang.typed_lambda(str, i=int, f=float)(lambda i, f: f'{i}, {f}')),
+    )
+
+    i = inj.create_injector(es)
+    assert i.provide(int) == 421
+    assert i.provide(str) == '421, 5.2'
