05445cb997c42943925f45a22271597f9abcd02f
wrmsr <timwilloney@gmail.com>
2024-06-26 13:03:05 -0700

    --
    Former-commit-id: abaf84ac14a9dbcba2ea023ea7e4ef307e9a600e

--- omlish/inject2/elements.py
+++ omlish/inject2/elements.py
@@ -1,7 +1,9 @@
+"""
+TODO:
+ - as_element[s] - universal
+"""
 import typing as ta
 
-from .. import check
-from .. import collections as col
 from .. import dataclasses as dc
 from .. import lang
 
@@ -10,10 +12,10 @@ class Element(lang.Abstract):
     pass
 
 
-@dc.dataclass(frozen=True, eq=False)
+@dc.dataclass(frozen=True)
 class Elements(lang.Final):
-    es: ta.Sequence[Element] | None = dc.xfield(default=None, coerce=col.optional_seq_of(check.of_isinstance(Element)))
-    cs: ta.Sequence['Elements'] | None = dc.xfield(default=None, coerce=col.optional_seq_of(lambda e: check.isinstance(e, Elements)))  # noqa
+    es: frozenset[Element] | None = None
+    cs: frozenset['Elements'] | None = None
 
     def __iter__(self) -> ta.Generator[Element, None, None]:
         if self.es:
@@ -21,3 +23,21 @@ class Elements(lang.Final):
         if self.cs:
             for c in self.cs:
                 yield from c
+
+
+def as_elements(*args: ta.Union[Element, Elements]) -> Elements:
+    es: set[Element] = set()
+    cs: set['Elements'] = set()
+    for a in args:
+        if isinstance(a, Element):
+            es.add(a)
+        elif isinstance(a, Elements):
+            cs.add(a)
+        else:
+            raise TypeError(a)
+    if not es and len(cs) == 1:
+        return next(iter(cs))
+    return Elements(
+        frozenset(es) if es else None,
+        frozenset(cs) if cs else None,
+    )
