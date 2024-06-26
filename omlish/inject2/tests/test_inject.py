04516241de6932ec174ef4ecd61621e187e54b3e
wrmsr <timwilloney@gmail.com>
2024-06-26 16:07:17 -0700

    --
    Former-commit-id: b1ee5da8b70cfc3c70ed2ab1353695e16a78a466

--- omlish/inject2/tests/test_inject.py
+++ omlish/inject2/tests/test_inject.py
@@ -1,3 +1,4 @@
+import contextlib
 import typing as ta
 
 from ... import dataclasses as dc
@@ -103,22 +104,52 @@ def test_private():
     assert i.provide(str) == '32.1! foo'
 
 
+class SomeManager:
+    ec = 0
+    xc = 0
+
+    def __enter__(self):
+        self.ec += 1
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.xc += 1
+
+
 def test_managed():
-    class SomeManager:
-        ec = 0
-        xc = 0
+    # with inj.create_managed_injector(inj.as_elements(
+    #     inj.as_binding(420),
+    #     jank_managed(SomeManager),
+    # )) as i:
+    #     assert i[int] == 420
+    #     sm = i[SomeManager]
+    #     assert (sm.ec, sm.xc) == (1, 0)
+    # assert (sm.ec, sm.xc) == (1, 1)
+    pass
 
-        def __enter__(self):
-            self.ec += 1
 
-        def __exit__(self, exc_type, exc_val, exc_tb):
-            self.xc += 1
+def test_custom_inject():
+    @dc.dataclass(frozen=True)
+    @dc.extra_params(cache_hash=True)
+    class _JankManagedTag(lang.Final):
+        tag: ta.Any
 
-    from ..managed import jank_managed
+    def jank_managed(a):
+        b = inj.as_binding(a)
+        mg_key = dc.replace(b.key, tag=_JankManagedTag(b.key.tag))
+
+        def prov(i: inj.Injector):
+            o = i[mg_key]
+            i[contextlib.ExitStack].enter_context(o)
+            return o
+
+        return inj.as_elements(
+            dc.replace(b, key=mg_key),
+            dc.replace(b, provider=inj.fn(prov, b.key.cls)),
+        )
 
     with inj.create_managed_injector(inj.as_elements(
-        inj.as_binding(420),
-        jank_managed(SomeManager),
+            inj.as_binding(420),
+            jank_managed(SomeManager),
     )) as i:
         assert i[int] == 420
         sm = i[SomeManager]
