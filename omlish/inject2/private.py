1f51b6d0a8fac5f994be0ec5d59114843ec57cf8
wrmsr <timwilloney@gmail.com>
2024-06-25 23:28:56 -0700

    --
    Former-commit-id: 961497c01e355c51686ccb89b2ad862b50c49dfa

---
+++ omlish/inject2/private.py
@@ -0,0 +1,27 @@
+import typing as ta
+
+from .. import check
+from .. import dataclasses as dc
+from .. import lang
+from .elements import Element
+from .elements import Elements
+from .keys import Key
+from .keys import as_key
+
+
+@dc.dataclass(frozen=True)
+class Expose(Element, lang.Final):
+    key: Key
+
+
+def expose(k: ta.Any) -> Element:
+    return Expose(as_key(k))
+
+
+@dc.dataclass(frozen=True)
+class Private(Element, lang.Final):
+    elements: Elements
+
+
+def private(es: Elements) -> Element:
+    return Private(check.isinstance(es, Elements))
