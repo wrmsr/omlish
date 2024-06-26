6a578bf8602314fd5eb86c5ca7247e555846c14c
wrmsr <timwilloney@gmail.com>
2024-06-26 13:57:37 -0700

    --
    Former-commit-id: b10834c3eb74d8c9d17213b8e84bf6f5daff3468

--- omlish/inject2/impl/private.py
+++ omlish/inject2/impl/private.py
@@ -5,16 +5,21 @@ TODO:
 import itertools
 import typing as ta
 
+from .. import Cls
 from ... import check
 from ... import dataclasses as dc
 from ... import lang
 from ..injector import Injector
 from ..keys import Key
+from ..private import Expose
 from ..private import Private
 from ..providers import Provider
 from .elements import ElementCollection
 from .injector import InjectorImpl
 from .providers import ProviderImpl
+
+
+##
 
 
 _PRIVATE_COUNT = itertools.count()
@@ -23,6 +28,9 @@ _PRIVATE_COUNT = itertools.count()
 @dc.dataclass(frozen=True)
 class PrivateInjectorId(lang.Final):
     id: int
+
+
+##
 
 
 @dc.dataclass(frozen=True, eq=False)
@@ -36,6 +44,9 @@ class PrivateInjectorProviderImpl(ProviderImpl):
 
     def provide(self, injector: Injector) -> ta.Any:
         return InjectorImpl(self.ec, check.isinstance(injector, InjectorImpl))
+
+
+##
 
 
 @dc.dataclass(frozen=True, eq=False)
@@ -52,10 +63,27 @@ class ExposedPrivateProviderImpl(ProviderImpl):
         return pi.provide(self.k)
 
 
+##
+
+
+@dc.dataclass(frozen=True)
+@dc.extra_params(cache_hash=True)
+class ExposedPrivateProvider(Provider):
+    pid: PrivateInjectorId
+    k: Key
+
+    def provided_cls(self) -> Cls | None:
+        raise NotImplementedError
+
+
 @dc.dataclass(frozen=True)
 class PrivateInfo(lang.Final):
     owner: ElementCollection
     p: Private
+
+    @lang.cached_function
+    def id(self) -> PrivateInjectorId:
+        return PrivateInjectorId(next(_PRIVATE_COUNT))
 
     @lang.cached_function
     def element_collection(self) -> ElementCollection:
@@ -63,4 +91,5 @@ class PrivateInfo(lang.Final):
 
     @lang.cached_function
     def exposed_providers(self) -> ta.Mapping[Key, ExposedPrivateProviderImpl]:
+        exs = self.element_collection().elements_of_type(Expose)
         raise NotImplementedError
