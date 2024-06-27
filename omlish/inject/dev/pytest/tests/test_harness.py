03d6b12505a80673aff114311ad1c2f3c5983e64
wrmsr <timwilloney@gmail.com>
2024-06-27 13:45:46 -0700

    --

--- omlish/inject/dev/pytest/tests/test_harness.py
+++ omlish/inject/dev/pytest/tests/test_harness.py
@@ -1,5 +1,12 @@
+import pytest
+
+from ..... import inject as inj
 from ..harness import Harness
+from ..harness import PytestScope
 
 
 def test_pytest(harness):
     assert isinstance(harness, Harness)
+    fr: pytest.FixtureRequest = harness[inj.tag(pytest.FixtureRequest, PytestScope.FUNCTION)]
+    assert isinstance(fr, pytest.FixtureRequest)
+    assert fr.function is test_pytest
