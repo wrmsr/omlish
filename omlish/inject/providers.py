920d1080a5e64e08fdba5e6b9e72c35b6a0b606a
wrmsr <timwilloney@gmail.com>
2024-06-26 20:22:30 -0700

    --

--- omlish/inject/providers.py
+++ omlish/inject/providers.py
@@ -1,39 +1,44 @@
+import abc
 import typing as ta
 
 from .. import check
 from .. import dataclasses as dc
 from .. import lang
-from .inspect import KwargsTarget
-from .inspect import build_kwargs_target
-from .inspect import signature
+from .elements import Element
+from .elements import Elements
+from .impl.inspect import signature
+from .keys import Key
 from .keys import as_key
-from .types import Binding
-from .types import Bindings
 from .types import Cls
-from .types import Injector
-from .types import Key
-from .types import Provider
-from .types import ProviderFn
-from .types import _BindingGen
-from .types import _ProviderGen
+
+
+class _Missing(lang.NotInstantiable):
+    pass
+
+
+##
+
+
+class Provider(lang.Abstract):
+    @abc.abstractmethod
+    def provided_cls(self) -> Cls | None:
+        raise NotImplementedError
 
 
 ##
 
 
 def as_provider(o: ta.Any) -> Provider:
-    check.not_isinstance(o, (Binding, _BindingGen, Bindings))
+    check.not_isinstance(o, (Element, Elements))
     if isinstance(o, Provider):
         return o
-    if isinstance(o, _ProviderGen):
-        return o._gen_provider()  # noqa
     if isinstance(o, Key):
-        return LinkProvider(o)
+        return link(o)
     if isinstance(o, type):
         return ctor(o)
     if callable(o):
         return fn(o)
-    return ConstProvider(type(o), o)
+    return const(o)
 
 
 ##
@@ -41,33 +46,20 @@ def as_provider(o: ta.Any) -> Provider:
 
 @dc.dataclass(frozen=True, eq=False)
 class FnProvider(Provider):
-    cls: Cls
     fn: ta.Any
-    kt: KwargsTarget
+    cls: Cls | None = None
 
-    def provided_cls(self, rec: ta.Callable[[Key], Cls]) -> Cls:
+    def provided_cls(self) -> Cls | None:
         return self.cls
 
-    @lang.cached_function
-    def required_keys(self) -> frozenset[Key | None]:
-        return frozenset(kw.key for kw in self.kt.kwargs)
 
-    def children(self) -> ta.Iterable[Provider]:
-        return ()
-
-    def provider_fn(self) -> ProviderFn:
-        def pfn(i: Injector) -> ta.Any:
-            return i.inject(self.fn)
-        return pfn
-
-
-def fn(fn: ta.Any, cls: ta.Optional[type] = None) -> Provider:
+def fn(fn: ta.Any, cls: ta.Optional[Cls] = _Missing) -> Provider:
     check.not_isinstance(fn, type)
-    sig = signature(fn)
-    if cls is None:
+    check.callable(fn)
+    if cls is _Missing:
+        sig = signature(fn)
         cls = check.isinstance(sig.return_annotation, type)
-    kt = build_kwargs_target(fn)
-    return FnProvider(cls, fn, kt)
+    return FnProvider(fn, cls)
 
 
 ##
@@ -76,28 +68,14 @@ def fn(fn: ta.Any, cls: ta.Optional[type] = None) -> Provider:
 @dc.dataclass(frozen=True, eq=False)
 class CtorProvider(Provider):
     cls: type
-    kt: KwargsTarget
 
-    def provided_cls(self, rec: ta.Callable[[Key], Cls]) -> Cls:
+    def provided_cls(self) -> Cls:
         return self.cls
-
-    @lang.cached_function
-    def required_keys(self) -> frozenset[Key | None]:
-        return frozenset(kw.key for kw in self.kt.kwargs)
-
-    def children(self) -> ta.Iterable[Provider]:
-        return ()
-
-    def provider_fn(self) -> ProviderFn:
-        def pfn(i: Injector) -> ta.Any:
-            return i.inject(self.cls)
-        return pfn
 
 
 def ctor(cls: type) -> Provider:
     check.isinstance(cls, type)
-    kt = build_kwargs_target(cls)
-    return CtorProvider(cls, kt)
+    return CtorProvider(cls)
 
 
 ##
@@ -105,59 +83,17 @@ def ctor(cls: type) -> Provider:
 
 @dc.dataclass(frozen=True, eq=False)
 class ConstProvider(Provider):
-    cls: Cls
     v: ta.Any
+    cls: Cls | None = None
 
-    def provided_cls(self, rec: ta.Callable[[Key], Cls]) -> Cls:
+    def provided_cls(self) -> Cls | None:
         return self.cls
 
-    def required_keys(self) -> frozenset[Key | None]:
-        return frozenset()
 
-    def children(self) -> ta.Iterable[Provider]:
-        return ()
-
-    def provider_fn(self) -> ProviderFn:
-        return lambda _: self.v
-
-
-def const(v: ta.Any, cls: ta.Optional[Cls] = None) -> Provider:
-    if cls is None:
+def const(v: ta.Any, cls: ta.Optional[Cls] = _Missing) -> Provider:
+    if cls is _Missing:
         cls = type(v)
-    return ConstProvider(cls, v)
-
-
-##
-
-
-@dc.dataclass(frozen=True, eq=False)
-class SingletonProvider(Provider):
-    p: Provider
-
-    def provided_cls(self, rec: ta.Callable[[Key], Cls]) -> Cls:
-        return self.p.provided_cls(rec)
-
-    def required_keys(self) -> frozenset[Key | None]:
-        return self.p.required_keys()
-
-    def children(self) -> ta.Iterable[Provider]:
-        return (self.p,)
-
-    def provider_fn(self) -> ProviderFn:
-        v = not_set = object()
-        ufn = self.p.provider_fn()
-
-        def pfn(i: Injector) -> ta.Any:
-            nonlocal v
-            if v is not_set:
-                v = ufn(i)
-            return v
-
-        return pfn
-
-
-def singleton(p: ta.Any) -> Provider:
-    return SingletonProvider(as_provider(p))
+    return ConstProvider(v, cls)
 
 
 ##
@@ -167,20 +103,8 @@ def singleton(p: ta.Any) -> Provider:
 class LinkProvider(Provider):
     k: Key
 
-    def provided_cls(self, rec: ta.Callable[[Key], Cls]) -> Cls:
-        return rec(self.k)
-
-    def required_keys(self) -> frozenset[Key | None]:
-        return frozenset([self.k])
-
-    def children(self) -> ta.Iterable[Provider]:
-        return ()
-
-    def provider_fn(self) -> ProviderFn:
-        def pfn(i: Injector) -> ta.Any:
-            return i.provide(self.k)
-
-        return pfn
+    def provided_cls(self) -> Cls | None:
+        return None
 
 
 def link(k: ta.Any) -> Provider:
