729bdf93d72f020af815ab1752101b5eeeb15359
wrmsr <timwilloney@gmail.com>
2024-06-28 15:49:30 -0700

    --
    Former-commit-id: 11b5e5659176b51891d7e7b93a4db1007923e53f

--- omserv/infra/models.py
+++
@@ -1,6 +0,0 @@
-from omlish import dataclasses as dc
-
-
-@dc.dataclass(frozen=True)
-class Server:
-    host: str
---
+++ omserv/server/tests/test_server.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_server_asyncio():
+    pass
+
+
+@pytest.mark.trio
+async def test_server_trio():
+    pass
