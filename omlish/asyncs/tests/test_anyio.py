850fff7ccee6b9b52283765f7c6d513c9beeb811
wrmsr <timwilloney@gmail.com>
2024-06-28 17:36:42 -0700

    --
    Former-commit-id: 8e3bc474589beeaad965343cfd9a562a9f101f0c

--- omlish/asyncs/tests/test_anyio.py
+++ omlish/asyncs/tests/test_anyio.py
@@ -1,7 +1,36 @@
-import anyio  # noqa
+import socket
+import typing as ta
+
+import anyio
 import pytest
 
 
+@pytest.fixture
+def server_sock() -> ta.Generator[socket.socket, None, None]:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    sock.bind(('localhost', 0))
+    sock.listen()
+    yield sock
+    sock.close()
+
+
+@pytest.fixture
+def server_addr(server_sock: socket.socket) -> tuple[str, int]:
+    return server_sock.getsockname()[:2]
+
+
 @pytest.mark.asyncio
-async def test_sockets():
-    pass
+async def test_send_receive(
+        server_sock: socket.socket,
+        server_addr: tuple[str, int],
+) -> None:
+    async with await anyio.connect_tcp(*server_addr) as stream:
+        client, _ = server_sock.accept()
+        await stream.send(b'blah')
+        request = client.recv(100)
+        client.sendall(request[::-1])
+        response = await stream.receive()
+        client.close()
+
+    assert response == b'halb'
