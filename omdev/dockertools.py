72027569a50fe0fffe613ce1f8d7ebffccd2415e
wrmsr <timwilloney@gmail.com>
2024-07-02 13:02:17 -0700

    --
    Former-commit-id: 75317e21c4b700dbd89f3053201f5bfae60f11d4

---
+++ omdev/dockertools.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+
+from omlish import argparse as ap
+from omlish import check
+from omlish import logs
+
+
+class Cli(ap.Cli):
+    @ap.command(
+        ap.arg('args', nargs='*'),
+    )
+    def ns1(self):
+        """
+        - https://gist.github.com/BretFisher/5e1a0c7bcca4c735e716abf62afad389
+        - https://github.com/justincormack/nsenter1/blob/8d3ba504b2c14d73c70cf34f1ec6943c093f1b02/nsenter1.c
+        """
+        exe = check.not_none(shutil.which('docker'))
+        os.execl(
+            exe,
+            exe,
+            'run',
+            '-it',
+            '--privileged',
+            '--pid=host',
+            'debian',
+            'nsenter',
+            '-t', '1',
+            '-m',
+            '-u',
+            '-n',
+            '-i',
+            *self.args.args,
+        )
+
+
+if __name__ == '__main__':
+    logs.configure_standard_logging('INFO')
+    Cli()()
