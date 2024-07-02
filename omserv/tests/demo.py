e7ceb13c6b1795823a9b530333717e6f8c035929
wrmsr <timwilloney@gmail.com>
2024-07-02 16:20:44 -0700

    --
    Former-commit-id: e69ffa1da585910521fc7e069c022ccb2d831f56

---
+++ omserv/tests/demo.py
@@ -0,0 +1,23 @@
+import functools
+
+from omlish import logs
+import anyio
+
+from .. import server
+from ..server.tests.hello import hello_app
+
+
+async def _a_main():
+    logs.configure_standard_logging('INFO')
+
+    async with anyio.create_task_group() as tg:
+        tg.start_soon(functools.partial(
+            server.serve,
+            hello_app,
+            server.Config(),
+            handle_shutdown_signals=True,  # (backend != 'trio'),
+        ))
+
+
+if __name__ == '__main__':
+    anyio.run(_a_main)
