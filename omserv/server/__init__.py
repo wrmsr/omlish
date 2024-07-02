1c6fb50fc1110048cd73a70afe97d475efeee2e4
wrmsr <timwilloney@gmail.com>
2024-07-01 17:17:14 -0700

    --
    Former-commit-id: 212c0b41979ff2007ae9ca993535aaa45a863964

--- omserv/server/__init__.py
+++ omserv/server/__init__.py
@@ -1,5 +1,12 @@
 """
 Based on https://github.com/pgjones/hypercorn
+
+TODO:
+ - add ssl back lol
+ - events as dc's
+ - injectify
+ - lifecycle / otp-ify
+ - configify
 """
 from .config import Config  # noqa
 from .serving import serve  # noqa
