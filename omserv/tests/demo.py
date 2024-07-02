0d5689d5b07efdc3f2f4a4c730bc0eb8525c2558
wrmsr <timwilloney@gmail.com>
2024-07-02 16:25:03 -0700

    --
    Former-commit-id: 64b68652dc07ce726b0aa312d87e4e398f7aa8aa

--- omserv/tests/demo.py
+++ omserv/tests/demo.py
@@ -1,4 +1,6 @@
 import functools
+import logging
+import itertools
 
 from omlish import logs
 import anyio
@@ -8,7 +10,16 @@ from .. import server
 from ..server.tests.hello import hello_app
 
 
-async def _a_main():
+log = logging.getLogger(__name__)
+
+
+async def _ticker(delay_s: int | float = 3.) -> None:
+    for i in itertools.count():
+        await anyio.sleep(delay_s)
+        log.info(f'tick: {i}')
+
+
+async def _a_main() -> None:
     logs.configure_standard_logging('INFO')
 
     async with anyio.create_task_group() as tg:
@@ -18,6 +29,7 @@ async def _a_main():
             server.Config(),
             handle_shutdown_signals=sniffio.current_async_library() != 'trio',
         ))
+        tg.start_soon(_ticker)
 
 
 if __name__ == '__main__':
