c03d23e42808d199def78731d84badd7201a3f21
wrmsr <timwilloney@gmail.com>
2024-07-03 15:55:18 -0700

    --

---
+++ x/deploy/demo/demo.py
@@ -0,0 +1,49 @@
+import os.path
+import subprocess
+
+from omlish.docker import timebomb_payload
+
+
+TIMEBOMB_DELAY_S = 20 * 60
+
+
+def _main():
+    img_name = 'wrmsr/omlish-deploy-demo'
+    cur_dir = os.path.dirname(__file__)
+    subprocess.check_call([
+        'docker', 'build',
+        '--tag', img_name,
+        '-f', os.path.join(cur_dir, 'Dockerfile'),
+        cur_dir,
+    ])
+
+    ctr_id = subprocess.check_output([
+        'docker', 'run',
+        '-d',
+        '-p', '9081:9081',
+        img_name,
+        'sleep', 'infinity',
+    ]).decode().strip()
+    print(f'{ctr_id=}')
+
+    try:
+        if TIMEBOMB_DELAY_S:
+            subprocess.check_call([
+                'docker', 'exec',
+                '-id', ctr_id,
+                'sh', '-c',
+                timebomb_payload(TIMEBOMB_DELAY_S),
+            ])
+
+            print()
+            print(ctr_id)
+            print()
+            print('done - press enter to die')
+            input()
+
+    finally:
+        subprocess.check_call(['docker', 'kill', '-sKILL', ctr_id])
+
+
+if __name__ == '__main__':
+    _main()
