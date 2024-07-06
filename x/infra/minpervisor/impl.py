f798fe788fddde57db87ace24574b6bd42414f21
wrmsr <timwilloney@gmail.com>
2024-07-05 20:47:48 -0700

    --
    Former-commit-id: 13d07731d176724e43e050402d7cbe70751ba404

--- x/infra/minpervisor/impl.py
+++ x/infra/minpervisor/impl.py
@@ -1,3 +1,26 @@
+"""
+- https://github.com/python-trio/trio/issues/927
+- https://github.com/oremanj/trio-monitor
+- https://bugs.python.org/issue39060
+
+    for p in ps:
+        p._proc.send_signal(signal.SIGTERM)
+
+    for p in ps:
+        async with anyio.create_task_group() as tg:
+            with anyio.move_on_after(3) as scope:
+                await p._proc.wait()
+
+            if scope.cancelled_caught:
+                p._proc.send_signal(signal.SIGKILL)
+
+                with anyio.move_on_after(3) as scope:
+                    await p._proc.wait()
+
+                if scope.cancelled_caught:
+                    print('FAILED TO REAP')
+
+"""
 import dataclasses as dc
 import signal
 import typing as ta
@@ -18,10 +41,16 @@ class Process:
 
         self._proc: anyio.abc.Process | None = None
 
-    async def start(self) -> None:
-        self._proc = await anyio.open_process(
+    async def run(self) -> None:
+        proc = await anyio.open_process(
             self._cfg.cmd
         )
+        async with proc:
+            try:
+                await proc.wait()
+            except anyio.get_cancelled_exc_class():
+                print('dying')
+                raise
 
 
 async def _a_main():
@@ -34,27 +63,18 @@ async def _a_main():
     for pc in pcs:
         p = Process(pc)
         ps.append(p)
-        await p.start()
 
-    await anyio.sleep(10)
+    async def inner(*, task_status: anyio.abc.TaskStatus) -> None:
+        task_status.started()
+        await anyio.sleep(10)
+        print('canceling')
+        tg.cancel_scope.cancel()
 
-    for p in ps:
-        p._proc.send_signal(signal.SIGTERM)
-
-    for p in ps:
-        async with anyio.create_task_group() as tg:
-            with anyio.move_on_after(3) as scope:
-                await p._proc.wait()
-
-            if scope.cancelled_caught:
-                p._proc.send_signal(signal.SIGKILL)
-
-                with anyio.move_on_after(3) as scope:
-                    await p._proc.wait()
-
-                if scope.cancelled_caught:
-                    print('FAILED TO REAP')
+    async with anyio.create_task_group() as tg:
+        await tg.start(inner)
+        for p in ps:
+            tg.start_soon(p.run)
 
 
 if __name__ == '__main__':
-    anyio.run(_a_main)
+    anyio.run(_a_main, backend='trio')
