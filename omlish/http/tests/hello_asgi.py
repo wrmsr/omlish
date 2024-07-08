b9b8b101a43510d69bd7aae8371b61cee958ff6d
wrmsr <timwilloney@gmail.com>
2024-07-08 14:46:42 -0700

    --
    Former-commit-id: c25dbf9fe6bd4e6b25fc5f68c11a4b10e8aef4e6

--- omlish/http/tests/hello_asgi.py
+++ omlish/http/tests/hello_asgi.py
@@ -1,3 +1,19 @@
+"""
+https://asgi.readthedocs.io/_/downloads/en/latest/pdf/
+
+REQUEST_METHOD is the method
+SCRIPT_NAME is root_path
+PATH_INFO can be derived by stripping root_path from path
+QUERY_STRING is query_string
+CONTENT_TYPE can be extracted from headers
+CONTENT_LENGTH can be extracted from headers
+SERVER_NAME and SERVER_PORT are in server
+REMOTE_HOST/REMOTE_ADDR and REMOTE_PORT are in client
+SERVER_PROTOCOL is encoded in http_version
+wsgi.url_scheme is scheme
+wsgi.input is a StringIO based around the http.request messages
+wsgi.errors is directed by the wrapper as needed
+"""
 import typing as ta
 
 from ... import check
@@ -25,6 +41,10 @@ def app(scope):
 def _main() -> None:
     uvicorn: ta.Any = __import__('uvicorn')
     uvicorn.run(app, host='', port=8000)
+
+    # import anyio
+    # omserv: ta.Any = __import__('omserv.server')
+    # anyio.run(omserv.server.serve, app, omserv.server.Config())
 
 
 if __name__ == '__main__':
