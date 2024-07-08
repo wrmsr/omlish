7fbd168ae5c6250d45b89d8a441c07d6962ca9e1
wrmsr <timwilloney@gmail.com>
2024-07-08 16:56:31 -0700

    --

---
+++ x/jobs/jobs.py
@@ -0,0 +1,16 @@
+"""
+design:
+ - sched / cron
+ - spawn / subproc
+ - flip / wait
+ - dags?
+
+notes:
+ - probably interacts with caching
+
+lookit:
+ - https://github.com/Yelp/tron
+ - https://github.com/dbader/schedule
+ - https://github.com/spotify/luigi
+ - https://github.com/apache/airflow/ jk god
+"""
--- x/sysd/journald.py
+++ x/sysd/journald.py
@@ -1,6 +1,84 @@
 """
 - https://www.freedesktop.org/software/systemd/man/latest/systemd.journal-fields.html
 - https://github.com/mosquito/cysystemd/tree/master/cysystemd
+
+priorities
+LOG_EMERG   = 0   /* system is unusable */
+LOG_ALERT   = 1   /* action must be taken immediately */
+LOG_CRIT    = 2   /* critical conditions */
+LOG_ERR     = 3   /* error conditions */
+LOG_WARNING = 4   /* warning conditions */
+LOG_NOTICE  = 5   /* normal but significant condition */
+LOG_INFO    = 6   /* informational */
+LOG_DEBUG   = 7   /* debug-level messages */
+
+facilities
+LOG_KERN     = 0  /* kernel messages */
+LOG_USER     = 1  /* random user-level messages */
+LOG_MAIL     = 2  /* mail system */
+LOG_DAEMON   = 3  /* system daemons */
+LOG_AUTH     = 4  /* security/authorization messages */
+LOG_SYSLOG   = 5  /* messages generated internally by syslogd */
+LOG_LPR      = 6  /* line printer subsystem */
+LOG_NEWS     = 7  /* network news subsystem */
+LOG_UUCP     = 8  /* UUCP subsystem */
+LOG_CRON     = 9  /* clock daemon */
+LOG_AUTHPRIV = 10 /* security/authorization messages (private) */
+LOG_FTP      = 11 /* ftp daemon */
+
+LOG_LOCAL0   = 16 /* reserved for local use */
+LOG_LOCAL1   = 17 /* reserved for local use */
+LOG_LOCAL2   = 18 /* reserved for local use */
+LOG_LOCAL3   = 19 /* reserved for local use */
+LOG_LOCAL4   = 20 /* reserved for local use */
+LOG_LOCAL5   = 21 /* reserved for local use */
+LOG_LOCAL6   = 22 /* reserved for local use */
+LOG_LOCAL7   = 23 /* reserved for local use */
+
+LOG_PID     = 0x01    /* log the pid with each message */
+LOG_CONS    = 0x02    /* log on the console if errors in sending */
+LOG_ODELAY  = 0x04    /* delay open until first syslog() (default) */
+LOG_NDELAY  = 0x08    /* don't delay open */
+LOG_NOWAIT  = 0x10    /* don't wait for console forks: DEPRECATED */
+LOG_PERROR  = 0x20    /* log to stderr as well */
+
+https://www.freedesktop.org/software/systemd/man/latest/systemd.journal-fields.html
+
+ts = current_time_micros
+
+hash_fields = (
+    message,
+    record.funcName,
+    record.levelno,
+    record.process,
+    record.processName,
+    record.levelname,
+    record.pathname,
+    record.name,
+    record.thread,
+    record.lineno,
+    ts,
+    tb_message,
+)
+
+message_id = uuid.uuid3(uuid.NAMESPACE_OID, "$".join(str(x) for x in hash_fields)).hex
+
+message
+priority
+syslog_facility
+code_file
+code_line
+code_func
+syslog_identifier
+message_raw
+message_id
+code_module
+logger_name
+pid
+proccess_name
+errno
+relative_ts
+thread_name
 """
 import ctypes as ct
 
