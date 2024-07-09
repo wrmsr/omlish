3fbe42846c4c2b2f7a2a14413f20a5bc6b5928bc
wrmsr <timwilloney@gmail.com>
2024-07-08 20:36:11 -0700

    --
    Former-commit-id: dd38f5af3ef3101d504adb0eb2e993732e835e77

--- omlish/__about__.py
+++ omlish/__about__.py
@@ -1,23 +1,7 @@
-# -*- coding: utf-8 -*-
 __name__ = 'omlish'
-__version__ = '0.0.0.dev0'
-__description__ = 'omlish'
 __author__ = 'wrmsr'
 __url__ = 'https://github.com/wrmsr/omlish'
-__license__ = 'BSD'
+__license__ = 'BSD-3-Clause'
+__requires_python__ = '>=3.11'
 
-__python_requires__ = '>=3.11'
-
-__classifiers__ = [
-    'License :: OSI Approved :: BSD License',
-
-    'Intended Audience :: Developers',
-
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3 :: Only',
-
-    'Programming Language :: Python',
-    'Programming Language :: Python :: Implementation :: CPython',
-
-    'Operating System :: OS Independent',
-]
+__version__ = '0.0.0.dev0'
