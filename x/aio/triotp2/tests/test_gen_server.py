3cb38af7ce413853c8615e34da9ed3fecd6ecef6
wrmsr <timwilloney@gmail.com>
2024-07-09 14:41:07 -0700

    --

--- x/aio/triotp2/tests/test_gen_server.py
+++ x/aio/triotp2/tests/test_gen_server.py
@@ -66,7 +66,7 @@ async def test_kvstore_call_delayed(test_state):
     async with trio.open_nursery() as nursery:
         resp = await sample_kvstore.special_call.delayed(nursery)
 
-    assert resp == "done"
+    assert resp == 'done'
 
 
 @pytest.mark.trio
@@ -97,3 +97,77 @@ async def test_kvstore_call_failure(test_state):
 
     assert isinstance(test_state.terminated_with, RuntimeError)
     assert test_state.did_raise is test_state.terminated_with
+
+
+@pytest.mark.trio
+async def test_kvstore_cast_normal(test_state):
+    await sample_kvstore.special_cast.normal()
+
+    with trio.fail_after(0.1):
+        await test_state.casted.wait()
+
+
+@pytest.mark.trio
+async def tests_kvstore_cast_stop(test_state):
+    await sample_kvstore.special_cast.stop()
+
+    with trio.fail_after(0.1):
+        await test_state.stopped.wait()
+
+    assert test_state.terminated_with is None
+    assert test_state.did_raise is None
+
+
+@pytest.mark.trio
+async def test_kvstore_cast_fail(test_state):
+    await sample_kvstore.special_cast.fail()
+
+    with trio.fail_after(0.1):
+        await test_state.stopped.wait()
+
+    assert isinstance(test_state.terminated_with, NotImplementedError)
+    assert test_state.did_raise is test_state.terminated_with
+
+
+@pytest.mark.trio
+async def test_kvstore_info_stop(test_state):
+    await sample_kvstore.special_info.stop()
+
+    with trio.fail_after(0.1):
+        await test_state.stopped.wait()
+
+    assert test_state.terminated_with is None
+    assert test_state.did_raise is None
+
+
+@pytest.mark.trio
+async def test_kvstore_info_fail(test_state):
+    await sample_kvstore.special_info.fail()
+
+    with trio.fail_after(0.1):
+        await test_state.stopped.wait()
+
+    assert isinstance(test_state.terminated_with, RuntimeError)
+    assert test_state.did_raise is test_state.terminated_with
+
+
+@pytest.mark.trio
+async def test_kvstore_info_matched(test_state):
+    await sample_kvstore.special_info.matched('foo')
+
+    with trio.fail_after(0.1):
+        await test_state.info.wait()
+
+    assert test_state.info_val == 'foo'
+
+
+@pytest.mark.trio
+async def test_kvstore_info_no_match(test_state):
+    await sample_kvstore.special_info.no_match('foo')
+
+    with trio.fail_after(0.1):
+        await test_state.info.wait()
+
+    assert test_state.info_val is None
+    assert len(test_state.unknown_info) == 1
+    assert test_state.unknown_info[0] == ('special_info_no_match', 'foo')
---
+++ x/aio/triotp2/tests/test_node.py
@@ -0,0 +1,34 @@
+import pytest
+
+from .. import triotp2 as t2
+
+
+class SampleData:
+    def __init__(self):
+        self.count = 0
+
+
+@pytest.fixture
+def test_data():
+    return SampleData()
+
+
+class sample_app(t2.Module):
+    __name__ = 'sample_app'
+
+    async def start(self, test_data):
+        test_data.count += 1
+
+
+def test_node_run(test_data):
+    t2.node_run(
+        apps=[
+            t2.app_spec(
+                module=sample_app(),
+                start_arg=test_data,
+                permanent=False,
+            )
+        ]
+    )
+
+    assert test_data.count == 1
