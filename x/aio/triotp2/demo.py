4093842e14cd443c990e0a002a69ff1673b17130
wrmsr <timwilloney@gmail.com>
2024-07-09 12:47:09 -0700

    --

---
+++ x/aio/triotp2/demo.py
@@ -0,0 +1,38 @@
+import trio
+
+from . import triotp2 as t2
+
+
+async def fn0():
+    for i in range(4):
+        await trio.sleep(2)
+        print(f'fn0: {i=}')
+    raise Exception('fn0')
+
+
+async def fn1():
+    for i in range(3):
+        await trio.sleep(3)
+        print(f'fn1: {i=}')
+    raise Exception('fn1')
+
+
+async def start():
+   children = [
+     t2.child_spec(
+       id='fn0',
+       task=fn0,
+       args=[],
+     ),
+     t2.child_spec(
+       id='fn1',
+       task=fn1,
+       args=[],
+     ),
+   ]
+   opts = t2.supervisor_options()
+   await t2.supervisor_start(children, opts)
+
+
+if __name__ == '__main__':
+    trio.run(start)
