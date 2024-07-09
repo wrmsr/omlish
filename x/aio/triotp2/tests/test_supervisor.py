02453f714d759325403ab48f66085fa6ac23b1f5
wrmsr <timwilloney@gmail.com>
2024-07-09 14:49:22 -0700

    --
    Former-commit-id: 215d447ac9595fd89cfd9deb91fe0af2f9c16713

--- x/aio/triotp2/tests/test_supervisor.py
+++ x/aio/triotp2/tests/test_supervisor.py
@@ -1,8 +1,9 @@
 import pytest
-
-from .. import supervisor
 import trio
+
 from omlish.testing.pytest import assert_raises_star
+
+from .. import triotp2 as t2
 
 
 class SampleData:
@@ -16,38 +17,40 @@ async def sample_task(test_data):
 
 async def sample_task_error(test_data):
     test_data.exec_count += 1
-    raise RuntimeError("pytest")
+    raise RuntimeError('pytest')
 
 
-@pytest.mark.parametrize("max_restarts", [1, 3, 5])
+@pytest.mark.trio
+@pytest.mark.parametrize('max_restarts', [1, 3, 5])
 async def test_automatic_restart_permanent(max_restarts, log_handler):
     test_data = SampleData()
 
     async with trio.open_nursery() as nursery:
         children = [
-            supervisor.child_spec(
-                id="sample_task",
+            t2.child_spec(
+                id='sample_task',
                 task=sample_task,
                 args=[test_data],
-                restart=supervisor.restart_strategy.PERMANENT,
+                restart=t2.restart_strategy.PERMANENT,
             ),
         ]
-        opts = supervisor.options(
+        opts = t2.supervisor_options(
             max_restarts=max_restarts,
             max_seconds=5,
         )
-        await nursery.start(supervisor.start, children, opts)
+        await nursery.start(t2.supervisor_start, children, opts)
 
     assert test_data.exec_count == (max_restarts + 1)
     assert log_handler.has_errors
 
 
-@pytest.mark.parametrize("max_restarts", [1, 3, 5])
+@pytest.mark.trio
+@pytest.mark.parametrize('max_restarts', [1, 3, 5])
 @pytest.mark.parametrize(
-    "strategy",
+    'strategy',
     [
-        supervisor.restart_strategy.PERMANENT,
-        supervisor.restart_strategy.TRANSIENT,
+        t2.restart_strategy.PERMANENT,
+        t2.restart_strategy.TRANSIENT,
     ],
 )
 async def test_automatic_restart_crash(max_restarts, strategy, log_handler):
@@ -56,28 +59,29 @@ async def test_automatic_restart_crash(max_restarts, strategy, log_handler):
     with assert_raises_star(RuntimeError):
         async with trio.open_nursery() as nursery:
             children = [
-                supervisor.child_spec(
-                    id="sample_task",
+                t2.child_spec(
+                    id='sample_task',
                     task=sample_task_error,
                     args=[test_data],
                     restart=strategy,
                 ),
             ]
-            opts = supervisor.options(
+            opts = t2.supervisor_options(
                 max_restarts=max_restarts,
                 max_seconds=5,
             )
-            await nursery.start(supervisor.start, children, opts)
+            await nursery.start(t2.supervisor_start, children, opts)
 
     assert test_data.exec_count == (max_restarts + 1)
     assert log_handler.has_errors
 
 
+@pytest.mark.trio
 @pytest.mark.parametrize(
-    "strategy",
+    'strategy',
     [
-        supervisor.restart_strategy.TEMPORARY,
-        supervisor.restart_strategy.TRANSIENT,
+        t2.restart_strategy.TEMPORARY,
+        t2.restart_strategy.TRANSIENT,
     ],
 )
 async def test_no_restart(strategy, log_handler):
@@ -85,18 +89,18 @@ async def test_no_restart(strategy, log_handler):
 
     async with trio.open_nursery() as nursery:
         children = [
-            supervisor.child_spec(
-                id="sample_task",
+            t2.child_spec(
+                id='sample_task',
                 task=sample_task,
                 args=[test_data],
                 restart=strategy,
             ),
         ]
-        opts = supervisor.options(
+        opts = t2.supervisor_options(
             max_restarts=3,
             max_seconds=5,
         )
-        await nursery.start(supervisor.start, children, opts)
+        await nursery.start(t2.supervisor_start, children, opts)
 
     assert test_data.exec_count == 1
     assert not log_handler.has_errors
