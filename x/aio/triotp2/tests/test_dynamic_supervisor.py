580b1c177550bb5c0fc0148e3473a77e243abf53
wrmsr <timwilloney@gmail.com>
2024-07-09 14:50:42 -0700

    --

---
+++ x/aio/triotp2/tests/test_dynamic_supervisor.py
@@ -0,0 +1,126 @@
+import pytest
+import trio
+
+from omlish.testing.pytest import assert_raises_star
+
+from .. import triotp2 as t2
+
+
+class SampleData:
+    def __init__(self):
+        self.exec_count = 0
+
+
+async def sample_task(test_data):
+    test_data.exec_count += 1
+
+
+async def sample_task_error(test_data):
+    test_data.exec_count += 1
+    raise RuntimeError('pytest')
+
+
+@pytest.mark.trio
+@pytest.mark.parametrize('max_restarts', [1, 3, 5])
+async def test_automatic_restart_permanent(max_restarts, log_handler, mailbox_env):
+    test_data = SampleData()
+
+    async with trio.open_nursery() as nursery:
+        children = [
+            t2.child_spec(
+                id='sample_task',
+                task=sample_task,
+                args=[test_data],
+                restart=t2.restart_strategy.PERMANENT,
+            ),
+        ]
+        opts = t2.supervisor_options(
+            max_restarts=max_restarts,
+            max_seconds=5,
+        )
+        mid = await nursery.start(t2.dynamic_supervisor_start, opts)
+
+        for child_spec in children:
+            await t2.dynamic_supervisor_start_child(mid, child_spec)
+
+        await trio.sleep(0.5)
+        nursery.cancel_scope.cancel()
+
+    assert test_data.exec_count == (max_restarts + 1)
+    assert log_handler.has_errors
+
+
+@pytest.mark.trio
+@pytest.mark.parametrize('max_restarts', [1, 3, 5])
+@pytest.mark.parametrize(
+    'strategy',
+    [
+        t2.restart_strategy.PERMANENT,
+        t2.restart_strategy.TRANSIENT,
+    ],
+)
+async def test_automatic_restart_crash(
+    max_restarts, strategy, log_handler, mailbox_env
+):
+    test_data = SampleData()
+
+    with assert_raises_star(RuntimeError):
+        async with trio.open_nursery() as nursery:
+            children = [
+                t2.child_spec(
+                    id='sample_task',
+                    task=sample_task_error,
+                    args=[test_data],
+                    restart=strategy,
+                ),
+            ]
+            opts = t2.supervisor_options(
+                max_restarts=max_restarts,
+                max_seconds=5,
+            )
+            mid = await nursery.start(t2.dynamic_supervisor_start, opts)
+
+            for child_spec in children:
+                await t2.dynamic_supervisor_start_child(mid, child_spec)
+
+            await trio.sleep(0.5)
+            nursery.cancel_scope.cancel()
+
+    assert test_data.exec_count == (max_restarts + 1)
+    assert log_handler.has_errors
+
+
+@pytest.mark.trio
+@pytest.mark.parametrize(
+    'strategy',
+    [
+        t2.restart_strategy.TEMPORARY,
+        t2.restart_strategy.TRANSIENT,
+    ],
+)
+async def test_no_restart(strategy, log_handler, mailbox_env):
+    test_data = SampleData()
+
+    async with trio.open_nursery() as nursery:
+        children = [
+            t2.child_spec(
+                id='sample_task',
+                task=sample_task,
+                args=[test_data],
+                restart=strategy,
+            ),
+        ]
+        opts = t2.supervisor_options(
+            max_restarts=3,
+            max_seconds=5,
+        )
+        mid = await nursery.start(t2.dynamic_supervisor_start, opts)
+
+        for child_spec in children:
+            await t2.dynamic_supervisor_start_child(mid, child_spec)
+
+        await trio.sleep(0.5)
+        nursery.cancel_scope.cancel()
+
+    assert test_data.exec_count == 1
+    assert not log_handler.has_errors
