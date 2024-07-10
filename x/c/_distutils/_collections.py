39d766ed5bf0d986978e2b9282bc131a602c43a1
wrmsr <timwilloney@gmail.com>
2024-07-09 17:16:14 -0700

    --
    Former-commit-id: 3c9a3dae889ce925470cb7a1142a1ec29f139ac5

--- x/c/_distutils/_collections.py
+++ x/c/_distutils/_collections.py
@@ -117,9 +117,8 @@ class RangeMap(dict):
 
     def get(self, key, default=None):
         """
-        Return the value for key if key is in the dictionary, else default.
-        If default is not given, it defaults to None, so that this method
-        never raises a KeyError.
+        Return the value for key if key is in the dictionary, else default. If default is not given, it defaults to
+        None, so that this method never raises a KeyError.
         """
         try:
             return self[key]
