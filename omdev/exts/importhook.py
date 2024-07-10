1f30a0b90f9b9c056e529f45d33de7abaa4697d6
wrmsr <timwilloney@gmail.com>
2024-07-10 14:57:21 -0700

    --
    Former-commit-id: aee7bd0db976f6d45cc8d7042ecaed4876028a55

--- omdev/exts/importhook.py
+++ omdev/exts/importhook.py
@@ -3,11 +3,14 @@ TODO:
  - remove imp
  - whitelist packages
 """
+import dataclasses as dc
 import imp
 import importlib
 import importlib.abc
 import importlib.machinery
+import os.path
 import sys
+import typing as ta
 
 from . import build
 
@@ -36,10 +39,38 @@ class CExtensionLoader(importlib.abc.Loader):
         return imp.load_dynamic(self._fullname, so_path)
 
 
+LoaderDetails: ta.TypeAlias = tuple[type[importlib.abc.Loader], ta.Sequence[str]]
 loader_details = (CExtensionLoader, ['.c', '.cc', '.cpp', '.cxx'])
 
 
+##
+
+
+@dc.dataclass(frozen=True)
+class FileFinderPathHook:
+    lds: ta.Sequence[LoaderDetails]
+
+    def __call__(self, path: str) -> importlib.machinery.FileFinder:
+        if not path:
+            path = os.getcwd()
+        if not os.path.isdir(path):
+            raise ImportError('only directories are supported', path=path)
+        return importlib.machinery.FileFinder(path, *self.lds)
+
+
 def install():
-    sys.path_hooks.insert(0, importlib.machinery.FileFinder.path_hook(loader_details))
+    sys.path_hooks.insert(0, FileFinderPathHook([loader_details]))
+    sys.path_importer_cache.clear()
+    importlib.invalidate_caches()
+
+
+def uninstall():
+    def is_c_loader(h):
+        return (
+                isinstance(h, FileFinderPathHook) and
+                h.lds == [loader_details]
+        )
+
+    sys.path_hooks = [h for h in sys.path_hooks if not is_c_loader(h)]
     sys.path_importer_cache.clear()
     importlib.invalidate_caches()
