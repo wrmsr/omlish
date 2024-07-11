ac66ca692becc8cf5fe05b63496a31a545d30753
wrmsr <timwilloney@gmail.com>
2024-07-10 23:37:04 -0700

    --
    Former-commit-id: 61c859d26250c0e53468248ce61a7d1e5a9f4de5

--- omlish/__about__.py
+++ omlish/__about__.py
@@ -2,6 +2,6 @@ __name__ = 'omlish'
 __author__ = 'wrmsr'
 __url__ = 'https://github.com/wrmsr/omlish'
 __license__ = 'BSD-3-Clause'
-__requires_python__ = '>=3.11'
+__requires_python__ = '>=3.12'
 
 __version__ = '0.0.0.dev1'
