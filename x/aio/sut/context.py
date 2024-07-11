a8167efe3db3930cb4a246082e0ec76225c9c0ee
wrmsr <timwilloney@gmail.com>
2024-07-11 16:13:10 -0700

    --
    Former-commit-id: 37acbc5fcb3e2c5603839550108e09bcf669a4f2

---
+++ x/aio/sut/context.py
@@ -0,0 +1,18 @@
+import anyio.abc
+
+from omlish import lang
+
+from .types import Context
+
+
+class ContextImpl(Context, lang.Final):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._done = anyio.Event()
+
+    async def done(self) -> None:
+        await self._done.wait()
+
+    def err(self) -> Exception | None:
+        raise NotImplementedError
