72006e6807d62be0e3714bb3229f828f60f56c3d
wrmsr <timwilloney@gmail.com>
2024-07-12 12:20:55 -0700

    --
    Former-commit-id: 1c7d09f0ec1d7df1e62e15724a1f050ce5817e85

--- omlish/sql/tests/test_dbs.py
+++ omlish/sql/tests/test_dbs.py
@@ -1,6 +1,8 @@
+import typing as ta
+
 from .dbs import Dbs
 
 
 def test_dbs(harness):
     dbs = harness[Dbs]
-    assert isinstance(dbs.specs(), list)
+    assert isinstance(dbs.specs(), ta.Mapping)
