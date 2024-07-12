d554af802f09c609b741971d8ca943547c85f10e
wrmsr <timwilloney@gmail.com>
2024-07-12 11:45:36 -0700

    --

--- omlish/dataclasses/impl/__init__.py
+++ omlish/dataclasses/impl/__init__.py
@@ -3,4 +3,6 @@ TODO:
  - metaclass:
   - cleanup confer
  - descriptors - check_type/validators don't handle setters lol
+ - deep_frozen?
+ - repr='redact', 'if_not_none', 'if_truthy', dc.if_(lambda v: ...)? just a lambda returning str | None?
 """
