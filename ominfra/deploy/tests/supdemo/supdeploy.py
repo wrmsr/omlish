af3b0eec8f812b66c228cf040cf1058bdf4ae6d6
wrmsr <timwilloney@gmail.com>
2024-07-15 20:24:46 -0700

    --
    Former-commit-id: 6af543d24df53f67905c9533c9b7b6eeb3a424f4

---
+++ ominfra/deploy/tests/supdemo/supdeploy.py
@@ -0,0 +1,17 @@
+"""
+https://docs.docker.com/config/containers/multi-service_container/#use-a-process-manager
+https://serverfault.com/questions/211525/supervisor-not-loading-new-configuration-files
+
+adduser \
+  --system \
+  --disabled-password \
+  --group \
+  --shell /bin/bash \
+  omlish
+"""
+def _main() -> None:
+    print('hi from supdeploy')
+
+
+if __name__ == '__main__':
+    _main()
