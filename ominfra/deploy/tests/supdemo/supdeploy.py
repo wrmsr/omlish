a69c87b54e0f4c1764354ab201cfd3c5cf86cccb
wrmsr <timwilloney@gmail.com>
2024-07-15 20:30:44 -0700

    --
    Former-commit-id: 480550fa961697399b1ae15cb8be614be0f4261b

--- ominfra/deploy/tests/supdemo/supdeploy.py
+++ ominfra/deploy/tests/supdemo/supdeploy.py
@@ -1,4 +1,13 @@
 """
+~omlish
+  /conf
+    /nginx
+      wrmsr--omlish--<rev>.conf
+    /supervisor
+      wrmsr--omlish--<rev>.conf
+  /venv
+    /wrmsr--omlish--<rev>--deploy
+
 https://docs.docker.com/config/containers/multi-service_container/#use-a-process-manager
 https://serverfault.com/questions/211525/supervisor-not-loading-new-configuration-files
 
@@ -9,6 +18,8 @@ adduser \
   --shell /bin/bash \
   omlish
 """
+
+
 def _main() -> None:
     print('hi from supdeploy')
 
