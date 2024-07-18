ad8d7ccb5151ede4ca9cb87162d756839852f19d
wrmsr <timwilloney@gmail.com>
2024-07-18 12:32:05 -0700

    --
    Former-commit-id: a507fe806c993d2b536c67cd02adca1928a5cdd9

--- omlish/tests/test_fnpairs.py
+++ omlish/tests/test_fnpairs.py
@@ -11,3 +11,26 @@ def test_text():
     fp = fnpairs.text('utf-8')
     assert fp.forward('hi') == b'hi'
     assert fp.backward(b'hi') == 'hi'
+
+
+def test_compose():
+    fp = fnpairs.text('utf-8').compose(fnpairs.Gzip())
+    buf = fp.forward('hi')
+    assert isinstance(buf, bytes)
+    assert fp.backward(buf) == 'hi'
+
+    jzfp = fnpairs.Json() \
+        .compose(fnpairs.UTF8) \
+        .compose(fnpairs.Gzip())
+    obj = {'hi': ['there', 420]}
+    buf = jzfp.forward(obj)
+    assert isinstance(buf, bytes)
+    assert jzfp.backward(buf) == obj
+
+    jlzfp = fnpairs.JsonLines() \
+        .compose(fnpairs.UTF8) \
+        .compose(fnpairs.Gzip())
+    obj2 = [{'hi': ['there', 420]}, {'bye': {'yes': None}}]
+    buf = jlzfp.forward(obj2)
+    assert isinstance(buf, bytes)
+    assert jlzfp.backward(buf) == obj2
