cb4011574ba27e852a353d2b546933783495c553
wrmsr <timwilloney@gmail.com>
2024-07-18 13:00:28 -0700

    --
    Former-commit-id: c7f15edb5882f512ea28132d7930966a00df7f3c

--- ominfra/deploy/tests/supdemo/supdeploy.py
+++ ominfra/deploy/tests/supdemo/supdeploy.py
@@ -3,17 +3,19 @@
   deploy.pid (flock)
   /conf
     /env
-      wrmsr--omlish--<rev>.env
+      wrmsr--omlish--<spec>.env
     /nginx
-      wrmsr--omlish--<rev>.conf
+      wrmsr--omlish--<spec>.conf
     /supervisor
-      wrmsr--omlish--<rev>.conf
+      wrmsr--omlish--<spec>.conf
   /venv
-    /wrmsr--omlish--<rev>
+    /wrmsr--omlish--<spec>
 
 ?
   /logs
-    /wrmsr--omlish--<rev>
+    /wrmsr--omlish--<spec>
+
+spec = <rev>--<when>
 
 https://docs.docker.com/config/containers/multi-service_container/#use-a-process-manager
 https://serverfault.com/questions/211525/supervisor-not-loading-new-configuration-files
@@ -24,11 +26,30 @@ adduser \
   --group \
   --shell /bin/bash \
   omlish
+
+==
+
+~piku/.piku
+  /apps/<app>
+  /data/<app>
+  /envs/<app>
+  /logs/<app>
+  /nginx
+  /repos/<app>
+  /uwsgi
+  /uwsgi-available/<app>_<proc>.<n>.ini
+
 """
+import subprocess
+
+
+def sh(*ss):
+    subprocess.check_call(' && '.join(ss), shell=True)
 
 
 def _main() -> None:
     print('hi from supdeploy')
+    sh('hostname')
 
 
 if __name__ == '__main__':
