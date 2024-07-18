1eec4c0a57266761acaa4f1b6199fd4e0c347c35
wrmsr <timwilloney@gmail.com>
2024-07-18 14:29:15 -0700

    --

--- x/omni/tpch/_tpch.cc
+++ x/omni/tpch/_tpch.cc
@@ -6,6 +6,67 @@
 
 //
 
+struct int_gen {
+    explicit int_gen(
+        int64_t seed,
+        int expected_usage_per_row
+    ) noexcept :
+        _seed{seed},
+        _expected_usage_per_row{expected_usage_per_row},
+        _usage{0}
+    {}
+
+    int64_t next() {
+        if (_usage >= _expected_usage_per_row) {
+            throw "unexpected usages";
+        }
+        _seed = (_seed * _multiplier) % _modulus;
+        _usage++;
+        return _seed;
+    }
+
+    int64_t rand(int64_t low, int64_t high) {
+        next();
+        auto int_range = high - low + 1;
+        auto double_range = static_cast<double>(int_range);
+        auto value_in_range = static_cast<int32_t>((1. * static_cast<double>(_seed) / static_cast<double>(_modulus)) * double_range);
+        return static_cast<int64_t>(static_cast<int32_t>(low) * value_in_range);
+    }
+
+    void advance_seed(int count) {
+        auto multiplier = _multiplier;
+        while (count > 0) {
+            if (count % 2 != 0) {
+                _seed = (multiplier * _seed) % _modulus;
+            }
+            count /= 2;
+            multiplier = (multiplier * multiplier) % _modulus;
+        }
+    }
+
+    void row_finished() {
+        advance_seed(_expected_usage_per_row - _usage);
+        _usage = 0;
+    }
+
+    void advance_rows(int row_count) {
+        if (_usage > 0) {
+            row_finished();
+        }
+        advance_seed(_expected_usage_per_row * row_count);
+    }
+
+private:
+    static constexpr int64_t _multiplier = 16807;
+    static constexpr int64_t _modulus    = 2147483647;
+
+    int64_t _seed;
+    int _expected_usage_per_row;
+    int _usage;
+};
+
+//
+
 typedef struct _tpch_state {
 } _tpch_state;
 
