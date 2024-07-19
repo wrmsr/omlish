63663d87c9e0564bf3b6585b23a22cf3d1fdd757
wrmsr <timwilloney@gmail.com>
2024-07-19 16:22:46 -0700

    --
    Former-commit-id: 6b8ce0b97fecd908dbf7460a15c05381a49ae56a

--- omlish/lang/tests/test_imports.py
+++ omlish/lang/tests/test_imports.py
@@ -1,5 +1,6 @@
 from ..imports import lazy_import
 from ..imports import proxy_import
+from ..imports import yield_importable
 
 
 def test_lazy():
@@ -10,3 +11,15 @@ def test_lazy():
 def test_proxy():
     sys = proxy_import('sys')
     assert sys.version_info[0] == 3
+
+
+def test_yield_importable():
+    base_pkg = __package__.rpartition('.')[0]
+
+    lst = list(yield_importable(base_pkg))
+    assert base_pkg + '.imports' in lst
+    assert __name__ not in lst
+
+    lst = list(yield_importable(base_pkg, recursive=True))
+    assert base_pkg + '.imports' in lst
+    assert __name__ in lst
