aefffd6229650912621174543d351211be981a38
wrmsr <timwilloney@gmail.com>
2024-07-19 13:00:38 -0700

    --
    Former-commit-id: 7020f3a15def462c254e39284a95b5569c1575e8

--- ominfra/deploy/tests/supdemo/supdeploy.py
+++ ominfra/deploy/tests/supdemo/supdeploy.py
@@ -1,4 +1,10 @@
 """
+TODO:
+ - flock
+ - interp.py
+
+==
+
 ~deploy
   deploy.pid (flock)
   /app
@@ -30,6 +36,7 @@ adduser \
   omlish
  ==
 """  # noqa
+import dataclasses as dc
 import functools
 import logging
 import os.path
@@ -95,27 +102,184 @@ def _subprocess_check_output(*args, **kwargs):
 ##
 
 
-PYTHON_BIN = 'python3.12'
-USERNAME = 'deploy'
-APP_NAME = 'omlish'
-REPO_URL = 'https://github.com/wrmsr/omlish'
-REVISION = 'cb60a99124c4d6973ac6e88d1a4313bcc9d8a197'
-REQUIREMENTS_TXT = 'requirements.txt'
-ENTRYPOINT = 'omserv.server.tests.hello'
-
-GLOBAL_SUPERVISOR_CONF_PATH = '/etc/supervisor/conf.d/supervisord.conf'
-GLOBAL_NGINX_CONF_PATH = '/etc/nginx/sites-enabled/deploy.conf'
+@dc.dataclass(frozen=True)
+class DeployConfig:
+    python_bin: str
+    app_name: str
+    repo_url: str
+    revision: str
+    requirements_txt: str
+    entrypoint: str
 
 
-def sh(*ss):
-    s = ' && '.join(ss)
-    log.info('Executing: %s', s)
-    _subprocess_check_call(s, shell=True)
+@dc.dataclass(frozen=True)
+class HostConfig:
+    username: str = 'deploy'
+
+    global_supervisor_conf_file_path: str = '/etc/supervisor/conf.d/supervisord.conf'
+    global_nginx_conf_file_path: str = '/etc/nginx/sites-enabled/deploy.conf'
 
 
-def ush(*ss):
-    s = ' && '.join(ss)
-    sh(f"su - {USERNAME} -c {shlex.quote(s)}")
+class Deployment:
+
+    def __init__(
+            self,
+            cfg: DeployConfig,
+            host_cfg: HostConfig = HostConfig(),
+    ) -> None:
+        super().__init__()
+        self._cfg = cfg
+        self._host_cfg = host_cfg
+
+    def sh(self, *ss: str) -> None:
+        s = ' && '.join(ss)
+        log.info('Executing: %s', s)
+        _subprocess_check_call(s, shell=True)
+
+    def ush(self, *ss: str) -> None:
+        s = ' && '.join(ss)
+        self.sh(f"su - {self._host_cfg.username} -c {shlex.quote(s)}")
+
+    @cached_nullary
+    def pwn(self) -> pwd.struct_passwd:
+        try:
+            return pwd.getpwnam(self._host_cfg.username)
+        except KeyError:
+            log.info('Creating user %s', self._host_cfg.username)
+            self.sh(' '.join([
+                'adduser',
+                '--system',
+                '--disabled-password',
+                '--group',
+                '--shell /bin/bash',
+                self._host_cfg.username,
+            ]))
+            return pwd.getpwnam(self._host_cfg.username)
+
+    @cached_nullary
+    def home_dir(self) -> str:
+        return os.path.expanduser(f'~{self._host_cfg.username}')
+
+    @cached_nullary
+    def create_dirs(self) -> None:
+        pwn = self.pwn()
+
+        for dn in [
+            'app',
+            'conf',
+            'conf/env',
+            'conf/nginx',
+            'conf/supervisor',
+            'venv',
+        ]:
+            fp = os.path.join(self.home_dir(), dn)
+            if not os.path.exists(fp):
+                log.info('Creating directory: %s', fp)
+                os.mkdir(fp)
+                os.chown(fp, pwn.pw_uid, pwn.pw_gid)
+
+    @cached_nullary
+    def create_global_nginx_conf(self) -> None:
+        nginx_conf_dir = os.path.join(self.home_dir(), 'conf/nginx')
+        if not os.path.isfile(self._host_cfg.global_nginx_conf_file_path):
+            log.info('Writing global nginx conf at %s', self._host_cfg.global_nginx_conf_file_path)
+            with open(self._host_cfg.global_nginx_conf_file_path, 'w') as f:
+                f.write(f'include {nginx_conf_dir}/*.conf;\n')
+
+    @cached_nullary
+    def create_global_supervisor_conf(self) -> None:
+        sup_conf_dir = os.path.join(self.home_dir(), 'conf/supervisor')
+        with open(self._host_cfg.global_supervisor_conf_file_path, 'r') as f:
+            glo_sup_conf = f.read()
+        if sup_conf_dir not in glo_sup_conf:
+            log.info('Updating global supervisor conf at %s', self._host_cfg.global_supervisor_conf_file_path)  # noqa
+            glo_sup_conf += f"""
+[include]
+files = {self.home_dir()}/conf/supervisor/*.conf
+"""
+            with open(self._host_cfg.global_supervisor_conf_file_path, 'w') as f:
+                f.write(glo_sup_conf)
+
+    @cached_nullary
+    def clone_repo(self) -> None:
+        clone_submodules = False
+        self.ush(
+            'cd ~/app',
+            f'git clone --depth 1 {self._cfg.repo_url} {self._cfg.app_name}',
+            *([
+                f'cd {self._cfg.app_name}',
+                'git submodule update --init',
+            ] if clone_submodules else []),
+        )
+
+    @cached_nullary
+    def setup_venv(self) -> None:
+        self.ush(
+            'cd ~/venv',
+            f'{self._cfg.python_bin} -mvenv {self._cfg.app_name}',
+            f'{self._cfg.app_name}/bin/python -mpip install -r ~deploy/app/{self._cfg.app_name}/{self._cfg.requirements_txt}',  # noqa
+        )
+
+    @cached_nullary
+    def create_nginx_conf(self) -> None:
+        nginx_conf = f"""
+server {{
+    listen 80;
+    location / {{
+        proxy_pass http://127.0.0.1:8000/;
+    }}
+}}
+"""  # noqa
+        nginx_conf_file = os.path.join(self.home_dir(), f'conf/nginx/{self._cfg.app_name}.conf')
+        log.info('Writing nginx conf to %s', nginx_conf_file)
+        with open(nginx_conf_file, 'w') as f:
+            f.write(nginx_conf)
+
+    @cached_nullary
+    def create_supervisor_conf(self) -> None:
+        sup_conf = f"""
+[program:{self._cfg.app_name}]
+command={self.home_dir()}/venv/{self._cfg.app_name}/bin/python -m {self._cfg.entrypoint}
+directory={self.home_dir()}/app/{self._cfg.app_name}
+user={self._host_cfg.username}
+autostart=true
+autorestart=true
+"""
+        sup_conf_file = os.path.join(self.home_dir(), f'conf/supervisor/{self._cfg.app_name}.conf')
+        log.info('Writing supervisor conf to %s', sup_conf_file)
+        with open(sup_conf_file, 'w') as f:
+            f.write(sup_conf)
+
+    @cached_nullary
+    def deploy(self) -> None:
+        self.pwn()
+
+        self.create_dirs()
+
+        self.create_global_nginx_conf()
+
+        log.info('Starting nginx')
+        self.sh('service nginx start')
+
+        self.create_global_supervisor_conf()
+
+        self.clone_repo()
+
+        self.setup_venv()
+
+        self.create_nginx_conf()
+
+        self.create_supervisor_conf()
+
+        log.info('Poking supervisor')
+        self.sh('kill -HUP 1')
+
+        log.info('Poking nginx')
+        self.sh('nginx -s reload')
+
+        log.info('Shitty deploy complete!')
+
+##
 
 
 def _main() -> None:
@@ -129,125 +293,14 @@ def _main() -> None:
     logging.root.addHandler(logging.StreamHandler())
     logging.root.setLevel('INFO')
 
-    ###
-
-    try:
-        pwn = pwd.getpwnam(USERNAME)
-    except KeyError:
-        log.info('Creating user %s', USERNAME)
-        sh(' '.join([
-            'adduser',
-            '--system',
-            '--disabled-password',
-            '--group',
-            '--shell /bin/bash',
-            USERNAME,
-        ]))
-        pwn = pwd.getpwnam(USERNAME)
-
-    ##
-
-    home_dir = os.path.expanduser(f'~{USERNAME}')
-    for dn in [
-        'app',
-        'conf',
-        'conf/env',
-        'conf/nginx',
-        'conf/supervisor',
-        'venv',
-    ]:
-        fp = os.path.join(home_dir, dn)
-        if not os.path.exists(fp):
-            log.info('Creating directory: %s', fp)
-            os.mkdir(fp)
-            os.chown(fp, pwn.pw_uid, pwn.pw_gid)
-
-    ##
-
-    nginx_conf_dir = os.path.join(home_dir, 'conf/nginx')
-    if not os.path.isfile(GLOBAL_NGINX_CONF_PATH):
-        log.info('Writing global nginx conf at %s', GLOBAL_NGINX_CONF_PATH)
-        with open(GLOBAL_NGINX_CONF_PATH, 'w') as f:
-            f.write(f'include {nginx_conf_dir}/*.conf;\n')
-
-    log.info('Starting nginx')
-    sh('service nginx start')
-
-    ##
-
-    sup_conf_dir = os.path.join(home_dir, 'conf/supervisor')
-    with open(GLOBAL_SUPERVISOR_CONF_PATH, 'r') as f:
-        glo_sup_conf = f.read()
-    if sup_conf_dir not in glo_sup_conf:
-        log.info('Updating global supervisor conf at %s', GLOBAL_SUPERVISOR_CONF_PATH)
-        glo_sup_conf += f"""
-[include]
-files = {home_dir}/conf/supervisor/*.conf
-"""
-        with open(GLOBAL_SUPERVISOR_CONF_PATH, 'w') as f:
-            f.write(glo_sup_conf)
-
-    ###
-
-    clone_submodules = False
-    ush(
-        'cd ~/app',
-        f'git clone --depth 1 {REPO_URL} {APP_NAME}',
-        *([
-            f'cd {APP_NAME}',
-            'git submodule update --init',
-        ] if clone_submodules else []),
-    )
-
-    ##
-
-    ush(
-        'cd ~/venv',
-        f'{PYTHON_BIN} -mvenv {APP_NAME}',
-        f'{APP_NAME}/bin/python -mpip install -r ~deploy/app/{APP_NAME}/{REQUIREMENTS_TXT}',
-    )
-
-    ##
-
-    nginx_conf = f"""
-server {{
-    listen 80;
-    location / {{
-        proxy_pass http://127.0.0.1:8000/;
-    }}
-}}
-"""  # noqa
-    nginx_conf_file = os.path.join(home_dir, f'conf/nginx/{APP_NAME}.conf')
-    log.info('Writing nginx conf to %s', nginx_conf_file)
-    with open(nginx_conf_file, 'w') as f:
-        f.write(nginx_conf)
-
-    ##
-
-    sup_conf = f"""
-[program:{APP_NAME}]
-command={home_dir}/venv/{APP_NAME}/bin/python -m {ENTRYPOINT}
-directory={home_dir}/app/{APP_NAME}
-user={USERNAME}
-autostart=true
-autorestart=true
-"""
-    sup_conf_file = os.path.join(home_dir, f'conf/supervisor/{APP_NAME}.conf')
-    log.info('Writing supervisor conf to %s', sup_conf_file)
-    with open(sup_conf_file, 'w') as f:
-        f.write(sup_conf)
-
-    log.info('Poking supervisor')
-    sh('kill -HUP 1')
-
-    ##
-
-    log.info('Poking nginx')
-    sh('nginx -s reload')
-
-    ##
-
-    log.info('Shitty deploy complete!')
+    Deployment(DeployConfig(
+        python_bin='python3.12',
+        app_name='omlish',
+        repo_url='https://github.com/wrmsr/omlish',
+        revision='cb60a99124c4d6973ac6e88d1a4313bcc9d8a197',
+        requirements_txt='requirements.txt',
+        entrypoint='omserv.server.tests.hello',
+    )).deploy()
 
 
 if __name__ == '__main__':
