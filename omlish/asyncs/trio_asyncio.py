429ef0bb28f5a31456d7d895652ec2d126bb75ff
wrmsr <timwilloney@gmail.com>
2024-07-23 14:33:05 -0700

    --
    Former-commit-id: 97691bd4cefe90632c75d20be03d7df9f4765b8b

--- omlish/asyncs/trio_asyncio.py
+++ omlish/asyncs/trio_asyncio.py
@@ -4,9 +4,11 @@ import typing as ta
 from .. import lang
 
 if ta.TYPE_CHECKING:
+    import asyncio
     import sniffio
     import trio_asyncio
 else:
+    asyncio = lang.proxy_import('asyncio')
     sniffio = lang.proxy_import('sniffio')
     trio_asyncio = lang.proxy_import('trio_asyncio')
 
@@ -16,14 +18,24 @@ def check_trio_asyncio() -> None:
         raise RuntimeError('trio_asyncio loop not running')
 
 
-def with_trio_asyncio_loop(fn):
+def with_trio_asyncio_loop(fn, *, wait=False):
     @functools.wraps(fn)
     async def inner(*args, **kwargs):
-        if trio_asyncio.current_loop.get() is None:
-            if sniffio.current_async_library() != 'trio':
-                raise RuntimeError('trio loop not running')
-            async with trio_asyncio.open_loop():
-                await fn(*args, **kwargs)
-        else:
+        if trio_asyncio.current_loop.get() is not None:
             await fn(*args, **kwargs)
+            return
+
+        if sniffio.current_async_library() != 'trio':
+            raise RuntimeError('trio loop not running')
+
+        loop: asyncio.BaseEventLoop
+        async with trio_asyncio.open_loop() as loop:
+            try:
+                await fn(*args, **kwargs)
+            finally:
+                if wait:
+                    # FIXME: lol
+                    while asyncio.all_tasks(loop):
+                        await asyncio.sleep(.2)
+
     return inner
