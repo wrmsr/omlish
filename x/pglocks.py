f8c8e997d6bbd8cd809c76ab2015dd16562fa059
wrmsr <timwilloney@gmail.com>
2024-07-23 16:05:24 -0700

    --

--- x/pglocks.py
+++ x/pglocks.py
@@ -3,6 +3,9 @@ https://www.postgresql.org/docs/16/functions-admin.html#FUNCTIONS-ADVISORY-LOCKS
 
 ==
 
+postgres@omlish-1:postgres> create extension pgcrypto;
+CREATE EXTENSION
+
 postgres@omlish-1:postgres> select trim(leading '\' from substr(digest('foo', 'sha1'), 1, 8)::text)::bit(64)::bigint
 +--------------------+
 | ltrim              |
@@ -13,6 +16,8 @@ SELECT 1
 Time: 0.030s
 
 ==
+
+In [9]: import struct, hashlib
 
 In [14]: struct.unpack('>q', hashlib.sha1(b'foo').digest()[:8])
 Out[14]: (859844163007352795,)
