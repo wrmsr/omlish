ceed2ea639fcf603a04019a3af0c6d37501e6cf1
wrmsr <timwilloney@gmail.com>
2024-07-23 16:41:24 -0700

    --
    Former-commit-id: f45300d899b1df483e4310cf1e61f1224db34665

---
+++ omlish/diag/procstats.py
@@ -0,0 +1,31 @@
+import dataclasses as dc
+import os
+import typing as ta
+
+from .. import lang
+
+if ta.TYPE_CHECKING:
+    import psutil as _psutil
+else:
+    _psutil = lang.proxy_import('psutil')
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class ProcStats:
+    pid: int
+
+    rss: int
+
+
+def get_psutil_procstats(pid: int | None = None) -> ProcStats:
+    if pid is None:
+        pid = os.getpid()
+
+    proc = _psutil.Process(pid)
+    mi = proc.memory_info()
+
+    return ProcStats(
+        pid=pid,
+
+        rss=mi.rss,
+    )
