b4207b9ad5950aaca70ecdf0bb88ea4172569afa
wrmsr <timwilloney@gmail.com>
2024-07-23 15:09:51 -0700

    --
    Former-commit-id: 767d421c5a2683087bf1737ff1fa977cd1ea5030

---
+++ omlish/__init__.py
@@ -0,0 +1,8 @@
+import sys as _sys
+
+
+REQUIRED_PYTHON_VERSION = (3, 12)
+
+
+if _sys.version_info < REQUIRED_PYTHON_VERSION:
+    raise EnvironmentError(f'Python version {_sys.version_info=} < {REQUIRED_PYTHON_VERSION}')
