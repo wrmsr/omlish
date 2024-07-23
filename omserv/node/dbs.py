f45300d899b1df483e4310cf1e61f1224db34665
wrmsr <timwilloney@gmail.com>
2024-07-23 16:41:24 -0700

    --

--- omserv/node/dbs.py
+++ omserv/node/dbs.py
@@ -1,6 +1,15 @@
-from ..secrets import load_secrets
+from ..secrets import load_secrets  # noqa
+
+
+# def get_db_url() -> str:
+#     cfg = load_secrets()
+#     return f'postgresql+asyncpg://{cfg["postgres_user"]}:{cfg["postgres_pass"]}@{cfg["postgres_host"]}:5432'
 
 
 def get_db_url() -> str:
-    cfg = load_secrets()
-    return f'postgresql+asyncpg://{cfg["postgres_user"]}:{cfg["postgres_pass"]}@{cfg["postgres_host"]}:5432'
+    from omlish import docker
+    cc = docker.ComposeConfig('omlish-', file_path='docker/docker-compose.yml')
+    pg = cc.get_services()['postgres']
+    port = docker.get_compose_port(pg, 5432)
+    env = pg['environment']
+    return f'postgresql+asyncpg://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@127.0.0.1:{port}'
