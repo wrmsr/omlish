44897993573ee156a9314828d27e11bb523056a9
wrmsr <timwilloney@gmail.com>
2024-07-24 15:27:00 -0700

    --
    Former-commit-id: fbbb5d5c9409e67c2eefed9090a70438a3c7ab5a

--- omserv/node/dbs.py
+++ omserv/node/dbs.py
@@ -1,15 +1,24 @@
+import os
+
 from ..secrets import load_secrets  # noqa
 
 
-# def get_db_url() -> str:
-#     cfg = load_secrets()
-#     return f'postgresql+asyncpg://{cfg["postgres_user"]}:{cfg["postgres_pass"]}@{cfg["postgres_host"]}:5432'
+def get_docker_db_url() -> str:
+    cfg = load_secrets()
+    return f'postgresql+asyncpg://{cfg["postgres_user"]}:{cfg["postgres_pass"]}@{cfg["postgres_host"]}:5432'
 
 
-def get_db_url() -> str:
+def get_secret_db_url() -> str:
     from omlish import docker
     cc = docker.ComposeConfig('omlish-', file_path='docker/docker-compose.yml')
     pg = cc.get_services()['postgres']
     port = docker.get_compose_port(pg, 5432)
     env = pg['environment']
     return f'postgresql+asyncpg://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@127.0.0.1:{port}'
+
+
+def get_db_url() -> str:
+    if os.environ.get('PROD', '').lower() not in ('0', 'false'):  # FIXME: lol
+        return get_secret_db_url()
+    else:
+        return get_docker_db_url()
