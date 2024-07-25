730d7be3adce82b5a133905368f91a35afeac7f7
wrmsr <timwilloney@gmail.com>
2024-07-25 16:56:08 -0700

    --
    Former-commit-id: 064a7792393b6e488041c74ffb214b33dc6e8d29

--- omserv/server/tests/demo_auth/sessions.py
+++ omserv/server/tests/demo_auth/sessions.py
@@ -9,43 +9,54 @@ import hmac
 import json
 import struct
 import time
+import typing as ta
 
 
-def _main() -> None:
-    secret_key = 'secret-key-goes-here'
-    salt = 'cookie-session'
+secret_key = 'secret-key-goes-here'
+salt = 'cookie-session'
 
-    signed_value = b'eyJfZnJlc2giOmZhbHNlfQ.ZqLLYg.4hMQ-ZLN_40k-q7efM87KEEx93g'
+
+digestmod = hashlib.sha1
+
+
+def base64_encode(b: bytes) -> bytes:
+    return base64.urlsafe_b64encode(b).rstrip(b"=")
+
+
+def base64_decode(b: bytes) -> bytes:
+    b += b"=" * (-len(b) % 4)
+    return base64.urlsafe_b64decode(b)
+
+
+def get_signature(key: bytes, value: bytes) -> bytes:
+    mac = hmac.new(key, msg=value, digestmod=digestmod)
+    return mac.digest()
+
+
+def verify_signature(key: bytes, value: bytes, sig: bytes) -> bool:
+    return hmac.compare_digest(sig, get_signature(key, value))
+
+
+def int_to_bytes(num: int) -> bytes:
+    return struct.pack('>Q', num).lstrip(b'\0')
+
+
+def bytes_to_int(bytestr: bytes) -> int:
+    return struct.unpack('>Q', bytestr.rjust(8, b'\0'))[0]
+
+
+def load_session_cookie(signed_value: bytes) -> ta.Any:
     sep = b'.'
     value, sig = signed_value.rsplit(sep, 1)
 
-    def base64_decode(string: str | bytes) -> bytes:
-        string += b"=" * (-len(string) % 4)
-        return base64.urlsafe_b64decode(string)
-
     sig_b = base64_decode(sig)
-
-    digestmod = hashlib.sha1
 
     mac = hmac.new(secret_key.encode(), digestmod=digestmod)
     mac.update(salt.encode())
     d_key = mac.digest()
 
-    def get_signature(key: bytes, value: bytes) -> bytes:
-        mac = hmac.new(key, msg=value, digestmod=digestmod)
-        return mac.digest()
-
-    def verify_signature(key: bytes, value: bytes, sig: bytes) -> bool:
-        return hmac.compare_digest(sig, get_signature(key, value))
-
     if not verify_signature(d_key, value, sig_b):
         raise Exception
-
-    def int_to_bytes(num: int) -> bytes:
-        return struct.pack('>Q', num).lstrip(b'\0')
-
-    def bytes_to_int(bytestr: bytes) -> int:
-        return struct.unpack('>Q', bytestr.rjust(8, b'\0'))[0]
 
     value, ts_bytes = value.rsplit(sep, 1)
     ts_int = bytes_to_int(base64_decode(ts_bytes))
@@ -60,14 +71,21 @@ def _main() -> None:
 
     payload = value
 
-    # if payload.startswith(b'.'):
-    #     payload = payload[1:]
-    #     decompress = True
+    if payload.startswith(b'.'):
+        # payload = payload[1:]
+        # decompress = True
+        raise NotImplementedError('compression')
 
     jsrc = base64_decode(payload)
 
     obj = json.loads(jsrc.decode())
 
+    return obj
+
+
+def _main() -> None:
+    signed_value = b'eyJfZnJlc2giOmZhbHNlfQ.ZqLLYg.4hMQ-ZLN_40k-q7efM87KEEx93g'
+    obj = load_session_cookie(signed_value)
     print(obj)
 
 
