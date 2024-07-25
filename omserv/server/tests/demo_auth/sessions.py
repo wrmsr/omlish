f0b3c6fd19305ada69e45da9f4edf1e8b099d9a4
wrmsr <timwilloney@gmail.com>
2024-07-25 16:50:55 -0700

    --
    Former-commit-id: bc3c7d47a73dee834c1d33447c30e47b3ed5c0c5

--- omserv/server/tests/demo_auth/sessions.py
+++ omserv/server/tests/demo_auth/sessions.py
@@ -1,6 +1,14 @@
+"""
+TODO:
+ - compress
+ - 'tagged' json marshal
+"""
 import base64
 import hashlib
 import hmac
+import json
+import struct
+import time
 
 
 def _main() -> None:
@@ -32,6 +40,35 @@ def _main() -> None:
 
     if not verify_signature(d_key, value, sig_b):
         raise Exception
+
+    def int_to_bytes(num: int) -> bytes:
+        return struct.pack('>Q', num).lstrip(b'\0')
+
+    def bytes_to_int(bytestr: bytes) -> int:
+        return struct.unpack('>Q', bytestr.rjust(8, b'\0'))[0]
+
+    value, ts_bytes = value.rsplit(sep, 1)
+    ts_int = bytes_to_int(base64_decode(ts_bytes))
+
+    max_age = 31 * 24 * 60 * 60
+    age = int(time.time()) - ts_int
+
+    if age > max_age:
+        raise Exception
+    if age < 0:
+        raise Exception
+
+    payload = value
+
+    # if payload.startswith(b'.'):
+    #     payload = payload[1:]
+    #     decompress = True
+
+    jsrc = base64_decode(payload)
+
+    obj = json.loads(jsrc.decode())
+
+    print(obj)
 
 
 if __name__ == '__main__':
