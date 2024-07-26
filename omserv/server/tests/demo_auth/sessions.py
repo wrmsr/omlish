2ea97e4c125b191c9b79f29622790ef05746839e
wrmsr <timwilloney@gmail.com>
2024-07-25 17:20:26 -0700

    --
    Former-commit-id: e25200a782ac50c3d529082730164d72cc1c27e8

--- omserv/server/tests/demo_auth/sessions.py
+++ omserv/server/tests/demo_auth/sessions.py
@@ -1,7 +1,7 @@
 """
 TODO:
- - compress
  - 'tagged' json marshal
+ - expires
 """
 import base64
 import hashlib
@@ -10,13 +10,20 @@ import json
 import struct
 import time
 import typing as ta
+import zlib
 
 
 SECRET_KEY = 'secret-key-goes-here'  # noqa
 SALT = 'cookie-session'
-
+SEP = b'.'
 
 DIGESTMOD = hashlib.sha1
+
+
+def derive_key() -> bytes:
+    mac = hmac.new(SECRET_KEY.encode(), digestmod=DIGESTMOD)
+    mac.update(SALT.encode())
+    return mac.digest()
 
 
 def base64_encode(b: bytes) -> bytes:
@@ -46,19 +53,14 @@ def bytes_to_int(bytestr: bytes) -> int:
 
 
 def load_session_cookie(signed_value: bytes) -> ta.Any:
-    sep = b'.'
-    value, sig = signed_value.rsplit(sep, 1)
+    value, sig = signed_value.rsplit(SEP, 1)
 
     sig_b = base64_decode(sig)
 
-    mac = hmac.new(SECRET_KEY.encode(), digestmod=DIGESTMOD)
-    mac.update(SALT.encode())
-    d_key = mac.digest()
-
-    if not verify_signature(d_key, value, sig_b):
+    if not verify_signature(derive_key(), value, sig_b):
         raise Exception
 
-    value, ts_bytes = value.rsplit(sep, 1)
+    value, ts_bytes = value.rsplit(SEP, 1)
     ts_int = bytes_to_int(base64_decode(ts_bytes))
 
     max_age = 31 * 24 * 60 * 60
@@ -71,28 +73,53 @@ def load_session_cookie(signed_value: bytes) -> ta.Any:
 
     payload = value
 
+    decompress = False
     if payload.startswith(b'.'):
-        # payload = payload[1:]
-        # decompress = True
-        raise NotImplementedError('compression')
+        payload = payload[1:]
+        decompress = True
 
-    jsrc = base64_decode(payload)
+    jb = base64_decode(payload)
 
-    obj = json.loads(jsrc.decode())
+    if decompress:
+        jb = zlib.decompress(jb)
+
+    obj = json.loads(jb.decode())
 
     return obj
 
 
 def save_session_cookie(obj: ta.Any) -> bytes:
-    raise NotImplementedError
+    jb = json.dumps(obj, separators=(',', ':')).encode()
+
+    is_compressed = False
+    compressed = zlib.compress(jb)
+
+    if len(compressed) < (len(jb) - 1):
+        jb = compressed
+        is_compressed = True
+
+    base64d = base64_encode(jb)
+
+    if is_compressed:
+        base64d = b'.' + base64d
+
+    payload = base64d
+
+    timestamp = base64_encode(int_to_bytes(int(time.time())))
+
+    value = payload + SEP + timestamp
+    return value + SEP + base64_encode(get_signature(derive_key(), value))
 
 
 def _main() -> None:
-    signed_value = b'eyJfZnJlc2giOmZhbHNlfQ.ZqLLYg.4hMQ-ZLN_40k-q7efM87KEEx93g'
-    obj = load_session_cookie(signed_value)
-    print(obj)
-    out = save_session_cookie(obj)
-    print(out)
+    sv = b'eyJfZnJlc2giOmZhbHNlfQ.ZqLLYg.4hMQ-ZLN_40k-q7efM87KEEx93g'
+
+    print(sv)
+    for _ in range(3):
+        obj = load_session_cookie(sv)
+        print(obj)
+        sv = save_session_cookie(obj)
+        print(sv)
 
 
 if __name__ == '__main__':
