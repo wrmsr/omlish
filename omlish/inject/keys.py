b9d41cf1d807e2381c20f7654cce38cd6f0170d3
wrmsr <timwilloney@gmail.com>
2024-07-29 14:36:27 -0700

    --

--- omlish/inject/keys.py
+++ omlish/inject/keys.py
@@ -3,6 +3,7 @@ import typing as ta
 
 from .. import dataclasses as dc
 from .. import lang
+from .. import reflect as rfl
 
 
 T = ta.TypeVar('T')
@@ -14,7 +15,7 @@ T = ta.TypeVar('T')
 @dc.dataclass(frozen=True)
 @dc.extra_params(cache_hash=True)
 class Key(lang.Final, ta.Generic[T]):
-    cls: type[T] | ta.NewType
+    ty: rfl.Type
     tag: ta.Any = dc.field(default=None, kw_only=True)
 
 
