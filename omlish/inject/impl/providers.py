f4cc48579536a1b15474e55cc0358509260cd7ca
wrmsr <timwilloney@gmail.com>
2024-07-29 12:55:05 -0700

    --
    Former-commit-id: 291eeb24232569d2e8753cf259d18d99f2d7ecbf

--- omlish/inject/impl/providers.py
+++ omlish/inject/impl/providers.py
@@ -5,7 +5,6 @@ TODO:
 import abc
 import typing as ta
 
-from ... import check
 from ... import dataclasses as dc
 from ... import lang
 from .. import Cls
@@ -73,43 +72,6 @@ class LinkProviderImpl(ProviderImpl, lang.Final):
 
     def provide(self, injector: Injector) -> ta.Any:
         return injector.provide(self.p.k)
-
-
-_ILLEGAL_MULTI_TYPES = (str, bytes, bytearray)
-
-
-def _unnest_multi_providers(ps: ta.Iterable[ProviderImpl]) -> ta.Sequence[ProviderImpl]:
-    lst = []
-
-    def rec(o):
-        if isinstance(o, MultiProviderImpl):
-            for c in o.ps:
-                rec(c)
-        else:
-            lst.append(check.isinstance(o, ProviderImpl))
-
-    for o in ps:
-        rec(o)
-    return tuple(lst)
-
-
-@dc.dataclass(frozen=True, eq=False)
-class MultiProviderImpl(ProviderImpl, lang.Final):
-    ps: ta.Sequence[ProviderImpl] = dc.xfield(coerce=_unnest_multi_providers)
-
-    @property
-    def providers(self) -> ta.Iterable[Provider]:
-        for p in self.ps:
-            yield from p.providers
-
-    def provide(self, injector: Injector) -> ta.Any:
-        rv = []
-        for ep in self.ps:
-            o = ep.provide(injector)
-            if isinstance(o, _ILLEGAL_MULTI_TYPES):
-                raise TypeError(o)
-            rv.extend(o)
-        return rv
 
 
 PROVIDER_IMPLS_BY_PROVIDER: dict[type[Provider], ta.Callable[..., ProviderImpl]] = {
