5c610eadbf956758b5f4e62a5397811d0804aeab
wrmsr <timwilloney@gmail.com>
2024-07-29 13:57:37 -0700

    --
    Former-commit-id: 6660d1d4c814b16991c6172aadf0872f9d372a40

--- omlish/inject/impl/multis.py
+++ omlish/inject/impl/multis.py
@@ -1,34 +1,21 @@
 import typing as ta
 
-from ... import check
 from ... import dataclasses as dc
 from ... import lang
 from ..injector import Injector
+from ..multis import MapProvider
+from ..multis import SetProvider
 from ..providers import Provider
+from .providers import PROVIDER_IMPLS_BY_PROVIDER
 from .providers import ProviderImpl
 
 
 _ILLEGAL_MULTI_TYPES = (str, bytes, bytearray)
 
 
-def _unnest_multi_providers(ps: ta.Iterable[ProviderImpl]) -> ta.Sequence[ProviderImpl]:
-    lst = []
-
-    def rec(o):
-        if isinstance(o, MultiProviderImpl):
-            for c in o.ps:
-                rec(c)
-        else:
-            lst.append(check.isinstance(o, ProviderImpl))
-
-    for o in ps:
-        rec(o)
-    return tuple(lst)
-
-
 @dc.dataclass(frozen=True, eq=False)
-class MultiProviderImpl(ProviderImpl, lang.Final):
-    ps: ta.Sequence[ProviderImpl] = dc.xfield(coerce=_unnest_multi_providers)
+class SetProviderImpl(ProviderImpl, lang.Final):
+    ps: ta.Sequence[ProviderImpl]
 
     @property
     def providers(self) -> ta.Iterable[Provider]:
@@ -43,3 +30,32 @@ class MultiProviderImpl(ProviderImpl, lang.Final):
                 raise TypeError(o)
             rv.add(o)
         return rv
+
+
+PROVIDER_IMPLS_BY_PROVIDER[SetProvider] = SetProviderImpl
+
+
+@dc.dataclass(frozen=True, eq=False)
+class MapProviderImpl(ProviderImpl, lang.Final):
+    class Entry(ta.NamedTuple):
+        k: ta.Any
+        v: ProviderImpl
+
+    es: ta.Sequence[Entry]
+
+    @property
+    def providers(self) -> ta.Iterable[Provider]:
+        for e in self.es:
+            yield from e.v.providers
+
+    def provide(self, injector: Injector) -> ta.Any:
+        rv = set()
+        for ep in self.ps:
+            o = ep.provide(injector)
+            if isinstance(o, _ILLEGAL_MULTI_TYPES):
+                raise TypeError(o)
+            rv.add(o)
+        return rv
+
+
+PROVIDER_IMPLS_BY_PROVIDER[MapProvider] = MapProviderImpl
