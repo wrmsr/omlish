faa0e6a9782ba36ce89d963a5d750a6cf4904c2b
wrmsr <timwilloney@gmail.com>
2024-07-30 12:31:40 -0700

    --
    Former-commit-id: 1e1554189ac7d9e77b8e04b571dabed8bf8c3792

--- omlish/testing/pytest/helpers.py
+++ omlish/testing/pytest/helpers.py
@@ -33,3 +33,12 @@ def assert_raises_star(et):
     except* et as eg:  # noqa
         num_caught += 1
     assert num_caught > 0
+
+
+@pytest.fixture
+def asyncio_drainer(event_loop):
+    try:
+        yield
+    finally:
+        while event_loop._ready or event_loop._scheduled:  # noqa
+            event_loop._run_once()  # noqa
