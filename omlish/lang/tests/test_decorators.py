dd1d09e3563c5b21b20796fd212133a645c30bf7
wrmsr <timwilloney@gmail.com>
2024-07-31 11:36:34 -0700

    --
    Former-commit-id: fe1611845b0e31657a196858fc450976d61bf109

--- omlish/lang/tests/test_decorators.py
+++ omlish/lang/tests/test_decorators.py
@@ -5,12 +5,24 @@ import functools
 
 
 class decorator:  # noqa
-    def __init__(self, fn):
-        super().__init__()
-        self._fn = fn
+    def __init__(self, wrapper):
+        self._wrapper = wrapper
+        functools.update_wrapper(self, wrapper)
 
-    def __call__(self, *args, **kwargs):
-        return self._fn(*args, **kwargs)
+    def __call__(self, fn, *args, **kwargs):
+        return self._Descriptor(self._wrapper, fn, args, kwargs)
+
+    class _Descriptor:
+        def __init__(self, wrapper, fn, args, kwargs):
+            self._wrapper, self._fn, self._args, self._kwargs = wrapper, fn, args, kwargs
+            self._wrapped = wrapper(fn, *args, **kwargs)
+            functools.update_wrapper(self, self._wrapped)
+
+        def __call__(self, *args, **kwargs):
+            return self._wrapped(*args, **kwargs)
+
+        def __get__(self, instance, owner):
+            return self.__class__(self._wrapper, self._fn.__get__(instance, owner), self._args, self._kwargs)
 
 
 def test_single_decorator():
@@ -40,17 +52,30 @@ def test_single_decorator():
 
         @my_decorator
         @classmethod
-        def c(cls, x):
+        def c1(cls, x):
             return cls.z + x + 1
 
         @my_decorator
         @staticmethod
-        def s(x):
+        def s1(x):
             return x + 1
 
-    assert Foo(4).m(2) == 7
-    assert Foo.c(2) == 8
-    assert Foo.c(1) == 2
+        @classmethod
+        @my_decorator
+        def c2(cls, x):
+            return cls.z + x + 1
+
+        @staticmethod
+        @my_decorator
+        def s2(x):
+            return x + 1
+
+    assert Foo(4).m(2) == 8
+    assert Foo.m(Foo(4), 2) == 8
+    assert Foo.c1(2) == 9
+    assert Foo.s1(1) == 3
+    assert Foo.c2(2) == 9
+    assert Foo.s2(1) == 3
 
 
 # def test_double_decorator():
