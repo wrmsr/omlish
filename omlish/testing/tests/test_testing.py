1e44bec6be72ff0f38af6784e1572458df216574
wrmsr <timwilloney@gmail.com>
2024-07-30 18:14:34 -0700

    --
    Former-commit-id: d26546fa7c6f37c20ae869e05fb62f934514778e

--- omlish/testing/tests/test_testing.py
+++ omlish/testing/tests/test_testing.py
@@ -10,15 +10,22 @@ from ..testing import raise_in_thread
 @pytest.mark.skipif(sys.implementation.name != 'cpython', reason='cpython only')
 def test_raise_in_thread():
     c = 0
+    e = None
     f = False
+
+    class FooError(Exception):
+        pass
 
     def proc():
         nonlocal c
+        nonlocal e
         nonlocal f
         try:
             while True:
                 time.sleep(.05)
                 c += 1
+        except FooError as e_:
+            e = e_
         finally:
             f = True
 
@@ -30,10 +37,11 @@ def test_raise_in_thread():
     time.sleep(.4)
     assert not f
 
-    raise_in_thread(t, SystemExit)
+    raise_in_thread(t, FooError)
 
     t.join()
     assert not t.is_alive()
 
     assert c > 1
+    assert isinstance(e, FooError)
     assert f
