16b4c91def34974fdcfe64f8f61bfaea4e65278b
wrmsr <timwilloney@gmail.com>
2024-07-30 17:14:51 -0700

    --

--- dp/dp20240729_pt_vae/vae.py
+++ dp/dp20240729_pt_vae/vae.py
@@ -40,20 +40,53 @@ def decode_img(a):
     return PIL.Image.fromarray(a)
 
 
+class Vae(nn.Module):
+    def __init__(
+            self,
+            x_dim,
+            h_dim1,
+            h_dim2,
+            z_dim,
+    ):
+        super().__init__()
+
+        self.x_dim = x_dim
+        self.h_dim1 = h_dim1
+        self.h_dim2 = h_dim2
+        self.z_dim = z_dim
+
+        # encoder part
+        self.fc1 = nn.Linear(x_dim, h_dim1)
+        self.fc2 = nn.Linear(h_dim1, h_dim2)
+        self.fc31 = nn.Linear(h_dim2, z_dim)
+        self.fc32 = nn.Linear(h_dim2, z_dim)
+        # decoder part
+        self.fc4 = nn.Linear(z_dim, h_dim2)
+        self.fc5 = nn.Linear(h_dim2, h_dim1)
+        self.fc6 = nn.Linear(h_dim1, x_dim)
+
+    def encoder(self, x):
+        h = F.relu(self.fc1(x))
+        h = F.relu(self.fc2(h))
+        return self.fc31(h), self.fc32(h)  # mu, log_var
+
+    def sampling(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)  # return z sample
+
+    def decoder(self, z):
+        h = F.relu(self.fc4(z))
+        h = F.relu(self.fc5(h))
+        return F.sigmoid(self.fc6(h))
+
+    def forward(self, x):
+        mu, log_var = self.encoder(x.view(-1, 784))
+        z = self.sampling(mu, log_var)
+        return self.decoder(z), mu, log_var
+
+
 def _main():
-    bs = 100
-
-    # MNIST Dataset
-    # train_dataset = datasets.MNIST(root='./mnist_data/', train=True, transform=transforms.ToTensor(), download=True)
-    # test_dataset = datasets.MNIST(root='./mnist_data/', train=False, transform=transforms.ToTensor(), download=False)
-    #
-    # train_ds = torch.utils.data.TensorDataset(torch.tensor(x_train))
-    # train_dl = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-    #
-    # # Data Loader (Input Pipeline)
-    # train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=bs, shuffle=True)
-    # test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=bs, shuffle=False)
-
     from keras.datasets import mnist  # noqa
     train, test = mnist.load_data()
     x_train, y_train = prepare(*train)
@@ -68,51 +101,6 @@ def _main():
     test_ds = torch.utils.data.TensorDataset(torch.tensor(x_test))
     test_loader = torch.utils.data.DataLoader(test_ds, batch_size=batch_size, shuffle=True)
 
-    class Vae(nn.Module):
-        def __init__(
-                self,
-                x_dim,
-                h_dim1,
-                h_dim2,
-                z_dim,
-        ):
-            super().__init__()
-
-            self.x_dim = x_dim
-            self.h_dim1 = h_dim1
-            self.h_dim2 = h_dim2
-            self.z_dim = z_dim
-
-            # encoder part
-            self.fc1 = nn.Linear(x_dim, h_dim1)
-            self.fc2 = nn.Linear(h_dim1, h_dim2)
-            self.fc31 = nn.Linear(h_dim2, z_dim)
-            self.fc32 = nn.Linear(h_dim2, z_dim)
-            # decoder part
-            self.fc4 = nn.Linear(z_dim, h_dim2)
-            self.fc5 = nn.Linear(h_dim2, h_dim1)
-            self.fc6 = nn.Linear(h_dim1, x_dim)
-
-        def encoder(self, x):
-            h = F.relu(self.fc1(x))
-            h = F.relu(self.fc2(h))
-            return self.fc31(h), self.fc32(h)  # mu, log_var
-
-        def sampling(self, mu, log_var):
-            std = torch.exp(0.5 * log_var)
-            eps = torch.randn_like(std)
-            return eps.mul(std).add_(mu)  # return z sample
-
-        def decoder(self, z):
-            h = F.relu(self.fc4(z))
-            h = F.relu(self.fc5(h))
-            return F.sigmoid(self.fc6(h))
-
-        def forward(self, x):
-            mu, log_var = self.encoder(x.view(-1, 784))
-            z = self.sampling(mu, log_var)
-            return self.decoder(z), mu, log_var
-
     # build model
     vae = Vae(x_dim=784, h_dim1=512, h_dim2=256, z_dim=2)
     if torch.cuda.is_available():
@@ -122,9 +110,9 @@ def _main():
 
     # return reconstruction error + KL divergence losses
     def loss_function(recon_x, x, mu, log_var):
-        BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-        KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-        return BCE + KLD
+        reconstruction_loss = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+        kl_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        return reconstruction_loss + kl_loss
 
     def train(epoch):
         vae.train()
@@ -145,10 +133,22 @@ def _main():
             optimizer.step()
 
             if batch_idx % 100 == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                           100. * batch_idx / len(train_loader), loss.item() / len(data)))
-        print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(train_loader.dataset)))
+                print(
+                    'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch,
+                        batch_idx * len(data),
+                        -len(train_loader.dataset),
+                        100. * batch_idx / len(train_loader),
+                        loss.item() / len(data),
+                    ),
+                )
+
+        print(
+            '====> Epoch: {} Average loss: {:.4f}'.format(
+                epoch,
+                train_loss / len(train_loader.dataset),
+            ),
+        )
 
     def test():
         vae.eval()
@@ -168,7 +168,13 @@ def _main():
         print('====> Test set loss: {:.4f}'.format(test_loss))
 
         random_number = np.asarray([[np.random.normal() for _ in range(vae.z_dim)]])
-        decode_img(vae.decoder(torch.Tensor(random_number)).reshape(img_width, img_height).detach().numpy()).resize((56, 56)).show()
+        decode_img(
+            vae.decoder(torch.Tensor(random_number))
+                .reshape(img_width, img_height)
+                .detach()
+                .numpy()
+            ).resize((56, 56)) \
+            .show()
 
     for epoch in range(1, 51):
         train(epoch)
