81a2ff9783a0cd2e1747274abb5310dd07bc49cf
wrmsr <timwilloney@gmail.com>
2024-08-01 12:05:28 -0700

    --
    Former-commit-id: 59f8003fff8096f09a49b9af7225226f8944d66b

--- omlish/marshal/naming.py
+++ omlish/marshal/naming.py
@@ -7,6 +7,7 @@ from .base import Option
 class Naming(Option, enum.Enum):
     SNAKE = 'snake'
     CAMEL = 'camel'
+    LOW_CAMEL = 'low_camel'
 
 
 def translate_name(n: str, e: Naming) -> str:
@@ -14,4 +15,7 @@ def translate_name(n: str, e: Naming) -> str:
         return lang.snake_case(n)
     if e is Naming.CAMEL:
         return lang.camel_case(n)
+    if e is Naming.LOW_CAMEL:
+        r = lang.camel_case(n)
+        return (r[0].lower() + r[1:]) if r else r
     raise ValueError(e)
