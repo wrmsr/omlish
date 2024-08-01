c24cf4d85dd6d439e0ac73f461c6432913e9da3c
wrmsr <timwilloney@gmail.com>
2024-08-01 14:55:18 -0700

    --
    Former-commit-id: 0861ad5c423b614b3606cdbeaaf3a375dee929d4

--- ominfra/deploy/tests/sddemo/sd_demo.py
+++ ominfra/deploy/tests/sddemo/sd_demo.py
@@ -44,11 +44,10 @@ qemu-system-aarch64 \
 https://gist.github.com/si458/98aa940837784e9ef9bff9e24a7a8bfd
 """
 import os.path
-import subprocess
-import traceback
 
-from omlish.docker import timebomb_payload
 from omlish.testing.pydevd import silence_subprocess_check
+
+from .. import utils as u
 
 
 TIMEBOMB_DELAY_S = 20 * 60
@@ -59,44 +58,10 @@ def _main():
 
     img_name = 'wrmsr/omlish-sd-demo'
     cur_dir = os.path.dirname(__file__)
-    subprocess.check_call([
-        'docker', 'build',
-        '--tag', img_name,
-        '-f', os.path.join(cur_dir, 'Dockerfile'),
-        cur_dir,
-    ])
+    u.build_docker_image(img_name, cur_dir)
 
-    ctr_id = subprocess.check_output([
-        'docker', 'run',
-        '-d',
-        img_name,
-    ]).decode().strip()
-    print(f'{ctr_id=}')
-
-    try:
-        if TIMEBOMB_DELAY_S:
-            subprocess.check_call([
-                'docker', 'exec', '-id', ctr_id,
-                'sh', '-c', timebomb_payload(TIMEBOMB_DELAY_S),
-            ])
-
-        print()
-        print(ctr_id)
-        print()
-        print('done - press enter to die')
-        input()
-
-    except Exception:  # noqa
-        traceback.print_exc()
-
-        print()
-        print(ctr_id)
-        print()
-        print('failed - press enter to die')
-        input()
-
-    finally:
-        subprocess.check_call(['docker', 'kill', '-sKILL', ctr_id])
+    with u.launch_docker_container(img_name, timebomb_delay_s=TIMEBOMB_DELAY_S) as ctr_id:
+        pass
 
 
 if __name__ == '__main__':
--- ominfra/deploy/tests/supdemo/docker_demo.py
+++ ominfra/deploy/tests/supdemo/docker_demo.py
@@ -1,11 +1,7 @@
 import json
 import os.path
-import subprocess
-import textwrap
-import traceback
 
-from omlish.docker import timebomb_payload
-from omlish.testing.pydevd import silence_subprocess_check
+from .. import utils as u
 
 
 TIMEBOMB_DELAY_S = 20 * 60
@@ -14,55 +10,18 @@ SCRIPT_TEMP_FILE = False
 PYCHARM_DEBUG = False
 
 
-def _main():
-    silence_subprocess_check()
-
+def _main() -> None:
     img_name = 'wrmsr/omlish-sup-demo'
     cur_dir = os.path.dirname(__file__)
-    subprocess.check_call([
-        'docker', 'build',
-        '--tag', img_name,
-        '-f', os.path.join(cur_dir, 'Dockerfile'),
-        cur_dir,
-    ])
 
-    ctr_id = subprocess.check_output([
-        'docker', 'run',
-        '-d',
-        '-p', '9322:22',
-        '-p', '9380:80',
-        '-p', '9343:443',
-        img_name,
-    ]).decode().strip()
-    print(f'{ctr_id=}')
-
-    try:
-        if TIMEBOMB_DELAY_S:
-            subprocess.check_call([
-                'docker', 'exec', '-id', ctr_id,
-                'sh', '-c', timebomb_payload(TIMEBOMB_DELAY_S),
-            ])
-
-        with open(os.path.join(os.path.dirname(__file__), 'scripts/supdeploy.py')) as f:
-            buf = f.read()
-
-        if PYCHARM_DEBUG:
-            pycharm_port = 43251
-            pycharm_version = '241.18034.82'
-            buf = textwrap.dedent(f"""
-                import subprocess
-                import sys
-                subprocess.check_call([sys.executable, '-mpip', 'install', f'pydevd-pycharm~={pycharm_version}'])
-
-                import pydevd_pycharm  # noqa
-                pydevd_pycharm.settrace(
-                    'docker.for.mac.localhost',
-                    port={pycharm_port},
-                    stdoutToServer=True,
-                    stderrToServer=True,
-                )
-            """) + '\n' * 2 + buf
-
+    u.build_docker_image(img_name, cur_dir)
+    with u.launch_docker_container(
+            img_name,
+            '-p', '9322:22',
+            '-p', '9380:80',
+            '-p', '9343:443',
+            timebomb_delay_s=TIMEBOMB_DELAY_S,
+    ) as ctr_id:
         cfg = dict(
             python_bin='python3.12',
             app_name='omlish',
@@ -72,45 +31,26 @@ def _main():
             entrypoint='omserv.server.tests.hello',
         )
 
+        ##
+
+        with open(os.path.join(os.path.dirname(__file__), 'scripts/supdeploy.py')) as f:
+            buf = f.read()
+
+        if PYCHARM_DEBUG:
+            buf = u.pycharm_debug_preamble(43251) + '\n' * 2 + buf
+
         if SCRIPT_TEMP_FILE:
-            fname = subprocess.check_output([
-                'docker', 'exec', ctr_id,
-                'mktemp', '--suffix=-supdeploy',
-            ]).decode().strip()
-
-            subprocess.run([
-                'docker', 'exec', '-i', ctr_id,
-                'sh', '-c', f'cp /dev/stdin {fname}',
-            ], input=buf.encode(), check=True)
-
-            subprocess.check_call([
+            fname = u.write_docker_temp_file(ctr_id, buf.encode())
+            u.check_call([
                 'docker', 'exec', '-i', ctr_id,
                 'python3', fname, 'deploy', json.dumps(cfg),
             ])
 
         else:
-            subprocess.run([
+            u.run([
                 'docker', 'exec', '-i', ctr_id,
                 'python3', '-', 'deploy', json.dumps(cfg),
             ], input=buf.encode(), check=True)
-
-        print()
-        print(ctr_id)
-        print()
-        print('done - press enter to die')
-        input()
-
-    except Exception:  # noqa
-        traceback.print_exc()
-
-        print()
-        print(ctr_id)
-        print()
-        print('failed - press enter to die')
-        input()
-
-    finally:
-        subprocess.check_call(['docker', 'kill', '-sKILL', ctr_id])
 
 
 if __name__ == '__main__':
