2c0f5d64622174bc8be0d9719d3bd43697f9074a
wrmsr <timwilloney@gmail.com>
2024-08-01 15:05:46 -0700

    --
    Former-commit-id: f50120566cf3c358e1dc1b7c6eea6cfb8d9730bf

--- ominfra/deploy/tests/demo/demo.py
+++ ominfra/deploy/tests/demo/demo.py
@@ -7,62 +7,45 @@ import os.path
 import typing as ta
 
 from omlish import check
-from omlish.docker import timebomb_payload
-from omlish.testing.pydevd import silence_subprocess_check
 
 from .... import cmds
 from .... import ssh
 from ...deploy import do_deploy
+from .. import utils as u
 
 
 TIMEBOMB_DELAY_S = 20 * 60
 
 
-async def _a_main():
-    silence_subprocess_check()
+async def check_call(cmd: ta.Sequence[str], **kwargs: ta.Any) -> None:
+    proc = await asyncio.create_subprocess_exec(*cmd, **kwargs)
+    await proc.wait()
+    if proc.returncode:
+        raise Exception(f'process failed: {proc.returncode}')
 
+
+async def check_output(cmd: ta.Sequence[str], **kwargs: ta.Any) -> bytes:
+    proc = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, **kwargs)
+    await proc.wait()
+    if proc.returncode:
+        raise Exception(f'process failed: {proc.returncode}')
+    return await check.not_none(proc.stdout).read()
+
+
+async def _a_main():
     img_name = 'wrmsr/omlish-deploy-demo'
     cur_dir = os.path.dirname(__file__)
-
-    async def check_call(cmd: ta.Sequence[str], **kwargs: ta.Any) -> None:
-        proc = await asyncio.create_subprocess_exec(*cmd, **kwargs)
-        await proc.wait()
-        if proc.returncode:
-            raise Exception(f'process failed: {proc.returncode}')
-
-    async def check_output(cmd: ta.Sequence[str], **kwargs: ta.Any) -> bytes:
-        proc = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, **kwargs)
-        await proc.wait()
-        if proc.returncode:
-            raise Exception(f'process failed: {proc.returncode}')
-        return await check.not_none(proc.stdout).read()
-
-    await check_call([
-        'docker', 'build',
-        '--tag', img_name,
-        '-f', os.path.join(cur_dir, 'Dockerfile'),
-        cur_dir,
-    ])
+    u.build_docker_image(img_name, cur_dir)
 
     ssh_password = 'foobar'  # noqa
 
-    ctr_id = (await check_output([
-        'docker', 'run',
-        '-d',
-        '-e', f'SSH_PASSWORD={ssh_password}',
-        '-p', '9082:22',
-        '-p', '9081:9081',
-        img_name,
-    ])).decode().strip()
-    print(f'{ctr_id=}')
-
-    try:
-        if TIMEBOMB_DELAY_S:
-            await check_call([
-                'docker', 'exec', '-id', ctr_id,
-                'sh', '-c', timebomb_payload(TIMEBOMB_DELAY_S),
-            ])
-
+    with u.launch_docker_container(
+            '-e', f'SSH_PASSWORD={ssh_password}',
+            '-p', '9082:22',
+            '-p', '9081:9081',
+            img_name,
+            timebomb_delay_s=TIMEBOMB_DELAY_S,
+    ) as ctr_id:  # noqa
         cr: cmds.CommandRunner = ssh.AsyncsshSshCommandRunner(ssh.SshConfig(
             host='localhost',
             port=9082,
@@ -75,17 +58,9 @@ async def _a_main():
                 cr,
                 skip_submodules=True,
             )
+
         except Exception as e:  # noqa
             print(e)
-
-        print()
-        print(ctr_id)
-        print()
-        print('done - press enter to die')
-        input()
-
-    finally:
-        await check_call(['docker', 'kill', '-sKILL', ctr_id])
 
 
 if __name__ == '__main__':
