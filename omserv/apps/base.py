93100aa7ad8cd44afd8fbd9eedb95b3607ac34e6
wrmsr <timwilloney@gmail.com>
2024-08-02 15:51:02 -0700

    --
    Former-commit-id: 1f6ff2950ccd024d5f76601d3e5c6d74877605d8

--- omserv/apps/base.py
+++ omserv/apps/base.py
@@ -1,24 +1,6 @@
 import contextvars
-import os
 
 from omlish.http.asgi import AsgiScope
 
-from .j2 import j2_helper
-
-
-##
-
 
 SCOPE: contextvars.ContextVar[AsgiScope] = contextvars.ContextVar('scope')
-
-
-##
-
-
-def base_server_url() -> str:
-    return os.environ.get('BASE_SERVER_URL', 'http://localhost:8000/')
-
-
-@j2_helper
-def url_for(s: str) -> str:
-    return base_server_url() + s
