c8e6464ab32291d3f7d3f7e169085e9ae711fd91
wrmsr <timwilloney@gmail.com>
2024-08-01 20:36:18 -0700

    --

--- omlish/inject/overrides.py
+++ omlish/inject/overrides.py
@@ -1,18 +1,19 @@
 import typing as ta
 
+from .. import check
 from .. import dataclasses as dc
 from .. import lang
-from .binder import bind
 from .elements import Element
 from .elements import Elements
+from .elements import as_elements
 
 
 @dc.dataclass(frozen=True)
 @dc.extra_params(cache_hash=True)
 class Overrides(Element, lang.Final):
-    ovr: Elements
-    src: Elements
+    ovr: Elements = dc.xfield(coerce=check.of_isinstance(Elements))
+    src: Elements = dc.xfield(coerce=check.of_isinstance(Elements))
 
 
 def override(ovr: ta.Any, *a: ta.Any) -> Element:
-    return Overrides(bind(ovr), bind(*a))
+    return Overrides(as_elements(ovr), as_elements(*a))
