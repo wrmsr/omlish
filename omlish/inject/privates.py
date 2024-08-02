0c8af40fb2a4215352c6663f3656837197d7d596
wrmsr <timwilloney@gmail.com>
2024-08-01 22:53:58 -0700

    --
    Former-commit-id: c33537f4f4ff14c99c3d8a0c11d4adec45041261

--- omlish/inject/privates.py
+++ omlish/inject/privates.py
@@ -2,17 +2,24 @@ from .. import check
 from .. import dataclasses as dc
 from .. import lang
 from .elements import Element
+from .elements import Elemental
 from .elements import Elements
+from .elements import as_elements
 from .keys import Key
+from .keys import as_key
 
 
 @dc.dataclass(frozen=True)
 @dc.extra_params(cache_hash=True)
 class Expose(Element, lang.Final):
-    key: Key = dc.xfield(coerce=check.of_isinstance(Key))
+    key: Key = dc.xfield(coerce=as_key)
 
 
 @dc.dataclass(frozen=True)
 @dc.extra_params(cache_hash=True)
 class Private(Element, lang.Final):
     elements: Elements = dc.xfield(coerce=check.of_isinstance(Elements))
+
+
+def private(*args: Elemental) -> Private:
+    return Private(as_elements(*args))
