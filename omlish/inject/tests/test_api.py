151c103fb498cb23451f35f2719e2e5b31ad35ba
wrmsr <timwilloney@gmail.com>
2024-08-01 19:56:16 -0700

    --
    Former-commit-id: 18301842ddb07fd1b1d19a9827dc7c95fa9210be

--- omlish/inject/tests/test_api.py
+++ omlish/inject/tests/test_api.py
@@ -29,15 +29,49 @@ class Thread(Scope, lang.Singleton, lang.Final):
 class SeededScope(Scope, lang.Final):
 class ScopeSeededProvider(Provider):
 """
+import inspect
 import typing as ta
 
+from ... import reflect as rfl
+from ..bindings import Binding
 from ..elements import Element
 from ..elements import Elements
+from ..keys import Key
+from ..keys import as_key
+from ..providers import ConstProvider
+from ..types import Scope
 
 
-def bind() -> Element | Elements:
+def bind(
+        obj: ta.Any,
+        *,
+        tag: ta.Any = None,
+
+        eager: bool = False,
+
+        in_: Scope | None = None,
+        singleton: bool = False,
+
+        to_fn: ta.Any = None,
+        to_ctor: ta.Any = None,
+        to_const: ta.Any = None,
+        to_key: ta.Any = None,
+) -> Element | Elements:
+    if obj is inspect.Parameter.empty or obj is None:
+        raise TypeError(obj)
+
+    if isinstance(obj, Key):
+        key = obj
+    elif isinstance(obj, rfl.TYPES) or rfl.is_type(obj):
+        key = Key(rfl.type_(obj))
+    else:
+        if to_const is not None:
+            raise TypeError('Cannot bind instance with to_const')
+        to_const = obj
+        key = Key(rfl.type_(type(obj)))
+
     raise NotImplementedError
 
 
 def test_api():
-    pass
+    assert bind(5) == Binding(Key(rfl.type_(int)), ConstProvider(5))
