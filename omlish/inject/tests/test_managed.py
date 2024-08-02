f0ffa02473156b1a90142fa6f2f88c51c636539f
wrmsr <timwilloney@gmail.com>
2024-08-01 21:09:17 -0700

    --

--- omlish/inject/tests/test_managed.py
+++ omlish/inject/tests/test_managed.py
@@ -1,9 +1,9 @@
-import contextlib
-import typing as ta
-
-from ... import dataclasses as dc
-from ... import inject as inj
-from ... import lang
+# import contextlib
+# import typing as ta
+#
+# from ... import dataclasses as dc
+# from ... import inject as inj
+# from ... import lang
 
 
 class SomeManager:
@@ -17,43 +17,42 @@ class SomeManager:
         self.xc += 1
 
 
-def test_managed():
-    # with inj.create_managed_injector(inj.as_elements(
-    #     inj.as_binding(420),
-    #     inj.managed(SomeManager),
-    # )) as i:
-    #     assert i[int] == 420
-    #     sm = i[SomeManager]
-    #     assert (sm.ec, sm.xc) == (1, 0)
-    # assert (sm.ec, sm.xc) == (1, 1)
-    pass
+# def test_managed():
+#     with inj.create_managed_injector(inj.as_elements(
+#         inj.as_binding(420),
+#         inj.managed(SomeManager),
+#     )) as i:
+#         assert i[int] == 420
+#         sm = i[SomeManager]
+#         assert (sm.ec, sm.xc) == (1, 0)
+#     assert (sm.ec, sm.xc) == (1, 1)
 
 
-def test_custom_inject():
-    @dc.dataclass(frozen=True)
-    @dc.extra_params(cache_hash=True)
-    class _JankManagedTag(lang.Final):
-        tag: ta.Any
-
-    def jank_managed(a):
-        b = inj.as_binding(a)
-        mg_key = dc.replace(b.key, tag=_JankManagedTag(b.key.tag))
-
-        def prov(i: inj.Injector):
-            o = i[mg_key]
-            i[contextlib.ExitStack].enter_context(o)
-            return o
-
-        return inj.as_elements(
-            dc.replace(b, key=mg_key),
-            dc.replace(b, provider=inj.fn(prov, b.key.ty)),
-        )
-
-    with inj.create_managed_injector(inj.as_elements(
-            inj.bind(420),
-            jank_managed(SomeManager),
-    )) as i:
-        assert i[int] == 420
-        sm = i[SomeManager]
-        assert (sm.ec, sm.xc) == (1, 0)
-    assert (sm.ec, sm.xc) == (1, 1)
+# def test_custom_inject():
+#     @dc.dataclass(frozen=True)
+#     @dc.extra_params(cache_hash=True)
+#     class _JankManagedTag(lang.Final):
+#         tag: ta.Any
+#
+#     def jank_managed(a):
+#         b = inj.as_binding(a)
+#         mg_key = dc.replace(b.key, tag=_JankManagedTag(b.key.tag))
+#
+#         def prov(i: inj.Injector):
+#             o = i[mg_key]
+#             i[contextlib.ExitStack].enter_context(o)
+#             return o
+#
+#         return inj.as_elements(
+#             dc.replace(b, key=mg_key),
+#             dc.replace(b, provider=inj.fn(prov, b.key.ty)),
+#         )
+#
+#     with inj.create_managed_injector(inj.as_elements(
+#             inj.bind(420),
+#             jank_managed(SomeManager),
+#     )) as i:
+#         assert i[int] == 420
+#         sm = i[SomeManager]
+#         assert (sm.ec, sm.xc) == (1, 0)
+#     assert (sm.ec, sm.xc) == (1, 1)
