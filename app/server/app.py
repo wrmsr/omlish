a46b30747cc17309fb3cd13bd5b560bbb11977f0
wrmsr <timwilloney@gmail.com>
2024-08-02 15:40:52 -0700

    --
    Former-commit-id: a9fed7b8b3918826415393fc2c21f6b7d895ffbd

--- app/server/app.py
+++ app/server/app.py
@@ -10,7 +10,6 @@ https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-yo
 """
 import datetime
 import logging
-import typing as ta
 
 from omlish import inject as inj
 from omlish import lang
@@ -20,26 +19,11 @@ from omlish.http.asgi import AsgiRecv
 from omlish.http.asgi import AsgiScope
 from omlish.http.asgi import AsgiSend
 
-from ..users import User
 from ..users import UserStore
-from .apps import inject as apps_inject
-from .apps.base import SCOPE
+from .apps import inject as apps_inj
 from .apps.j2 import J2Templates
-from .apps.markers import AppMarker
-from .apps.markers import AppMarkerProcessor
-from .apps.markers import get_app_markers
-from .apps.routes import Handler_
-from .apps.routes import Route
 from .apps.routes import RouteHandlerApp
-from .apps.sessions import SESSION
-from .apps.users import USER
-from .handlers.favicon import FaviconHandler
-from .handlers.index import IndexHandler
-from .handlers.login import LoginHandler
-from .handlers.logout import LogoutHandler
-from .handlers.profile import ProfileHandler
-from .handlers.signup import SignupHandler
-from .handlers.tik import TikHandler
+from .handlers import inject as handlers_inj
 
 
 log = logging.getLogger(__name__)
@@ -64,57 +48,22 @@ def bind_cookie_session_store() -> inj.Elemental:
     )
 
 
-def bind_handler(hc: type[Handler_]) -> inj.Elemental:
-    return inj.as_elements(
-        inj.bind(hc, singleton=True),
-        inj.set_binder[Handler_]().bind(hc),
-    )
-
-
-def _build_route_handler_map(
-        handlers: ta.AbstractSet[Handler_],
-        processors: ta.Mapping[type[AppMarker], AppMarkerProcessor],
-) -> ta.Mapping[Route, AsgiApp]:
-    route_handlers: dict[Route, AsgiApp] = {}
-    for h in handlers:
-        for rh in h.get_route_handlers():
-            app = rh.handler
-            markers = get_app_markers(rh.handler)
-            for m in markers:
-                mp = processors[type(m)]
-                if mp is not None:
-                    app = mp(app)
-            route_handlers[rh.route] = app
-    return route_handlers
-
-
 def bind_server_app() -> inj.Elemental:
     return inj.private(
-        inj.bind(ta.Callable[[], AsgiScope], to_const=SCOPE.get),
-        inj.bind(ta.Callable[[], sessions.Session], to_const=SESSION.get),
-        inj.bind(ta.Callable[[], User | None], to_const=USER.get),
-
-        inj.bind(J2Templates.Config(reload=True)),
+        inj.bind(J2Templates.Config(
+            resource_root=__package__ + '.templates',
+            reload=True,
+        )),
         inj.bind(J2Templates, singleton=True),
 
-        apps_inject.bind(),
+        apps_inj.bind(),
+        apps_inj.bind_route_handler_map(),
+
+        handlers_inj.bind(),
 
         bind_cookie_session_store(),
 
         inj.bind(UserStore, singleton=True),
-
-        inj.set_binder[Handler_](),
-
-        bind_handler(IndexHandler),
-        bind_handler(ProfileHandler),
-        bind_handler(LoginHandler),
-        bind_handler(SignupHandler),
-        bind_handler(LogoutHandler),
-        bind_handler(FaviconHandler),
-        bind_handler(TikHandler),
-
-        inj.bind(_build_route_handler_map, singleton=True),
-        inj.map_binder[type[AppMarker], AppMarkerProcessor](),
 
         inj.bind(RouteHandlerApp, singleton=True),
         inj.bind(AsgiApp, to_key=RouteHandlerApp, expose=True),
