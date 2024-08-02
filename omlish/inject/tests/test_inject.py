476bbfa195ee7451d5e2b0d4be6d00f2caad5eb7
wrmsr <timwilloney@gmail.com>
2024-08-01 21:43:03 -0700

    --
    Former-commit-id: ebc729914ccac120544f6f623900b8b806d99f67

--- omlish/inject/tests/test_inject.py
+++ omlish/inject/tests/test_inject.py
@@ -1,5 +1,3 @@
-import typing as ta
-
 from ... import inject as inj
 from ... import lang
 
@@ -13,36 +11,6 @@ def test_inject():
     i = inj.create_injector(es)
     assert i.provide(int) == 420
     assert i.provide(str) == '420'
-
-
-def test_set_multi():
-    es = inj.as_elements(
-        inj.bind_set_provider(ta.AbstractSet[int]),
-
-        inj.bind(420, tag='four twenty'),
-        inj.SetBinding(inj.as_key(ta.AbstractSet[int]), inj.Key(int, tag='four twenty')),
-
-        inj.bind(421, tag='four twenty one'),
-        inj.SetBinding(inj.as_key(ta.AbstractSet[int]), inj.Key(int, tag='four twenty one')),
-    )
-
-    i = inj.create_injector(es)
-    assert i.provide(inj.as_key(ta.AbstractSet[int])) == {420, 421}
-
-
-def test_map_multi():
-    es = inj.as_elements(
-        inj.bind_map_provider(ta.Mapping[str, int]),
-
-        inj.bind(420, tag='four twenty'),
-        inj.MapBinding(inj.as_key(ta.Mapping[str, int]), 'a', inj.Key(int, tag='four twenty')),
-
-        inj.bind(421, tag='four twenty one'),
-        inj.MapBinding(inj.as_key(ta.Mapping[str, int]), 'b', inj.Key(int, tag='four twenty one')),
-    )
-
-    i = inj.create_injector(es)
-    assert i.provide(inj.as_key(ta.Mapping[str, int])) == {'a': 420, 'b': 421}
 
 
 def test_optional():
