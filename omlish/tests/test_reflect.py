c37f2db2de282b7f6a32aecb0726cd4f7e336bee
wrmsr <timwilloney@gmail.com>
2024-08-02 14:50:25 -0700

    --
    Former-commit-id: 45a6ecad6d2a140c92abe00b26c51634753bddc3

--- omlish/tests/test_reflect.py
+++ omlish/tests/test_reflect.py
@@ -4,6 +4,7 @@ import typing as ta
 
 import pytest
 
+from .. import check
 from .. import reflect as rfl
 
 
@@ -17,24 +18,28 @@ P = ta.ParamSpec('P')
 _0, _1, _2, _3 = rfl._KNOWN_SPECIAL_TYPE_VARS[:4]  # noqa
 
 
+def assert_generic_full_eq(l: ta.Any, r: rfl.Generic) -> None:
+    assert check.isinstance(rfl.type_(l), rfl.Generic).full_eq(r)
+
+
 def test_simple_reflect_type():
     assert rfl.type_(int) is int
     assert rfl.type_(ta.Union[int, float]) == rfl.Union(frozenset([int, float]))  # noqa
     assert rfl.type_(ta.Optional[int]) == rfl.Union(frozenset([int, type(None)]))  # noqa
 
-    assert rfl.type_(ta.Sequence[int]) == rfl.Generic(collections.abc.Sequence, (int,), (_0,), ta.Sequence[int])
-    assert rfl.type_(ta.Mapping[int, str]) == rfl.Generic(collections.abc.Mapping, (int, str), (_0, _1), ta.Mapping[int, str])  # noqa
-    assert rfl.type_(ta.Mapping[int, ta.Optional[str]]) == rfl.Generic(collections.abc.Mapping, (int, rfl.Union(frozenset([str, type(None)]))), (_0, _1), ta.Mapping[int, ta.Optional[str]])  # noqa
-    assert rfl.type_(ta.Mapping[int, ta.Sequence[ta.Optional[str]]]) == rfl.Generic(collections.abc.Mapping, (int, rfl.Generic(collections.abc.Sequence, (rfl.Union(frozenset([str, type(None)])),), (_0,), ta.Sequence[ta.Optional[str]])), (_0, _1), ta.Mapping[int, ta.Sequence[ta.Optional[str]]])  # noqa
+    assert_generic_full_eq(ta.Sequence[int], rfl.Generic(collections.abc.Sequence, (int,), (_0,), ta.Sequence[int]))
+    assert_generic_full_eq(ta.Mapping[int, str], rfl.Generic(collections.abc.Mapping, (int, str), (_0, _1), ta.Mapping[int, str]))  # noqa
+    assert_generic_full_eq(ta.Mapping[int, ta.Optional[str]], rfl.Generic(collections.abc.Mapping, (int, rfl.Union(frozenset([str, type(None)]))), (_0, _1), ta.Mapping[int, ta.Optional[str]]))  # noqa
+    assert_generic_full_eq(ta.Mapping[int, ta.Sequence[ta.Optional[str]]], rfl.Generic(collections.abc.Mapping, (int, rfl.Generic(collections.abc.Sequence, (rfl.Union(frozenset([str, type(None)])),), (_0,), ta.Sequence[ta.Optional[str]])), (_0, _1), ta.Mapping[int, ta.Sequence[ta.Optional[str]]]))  # noqa
 
-    assert rfl.type_(list[int]) == rfl.Generic(list, (int,), (_0,), list[int])
-    assert rfl.type_(set[int]) == rfl.Generic(set, (int,), (_0,), set[int])
-    assert rfl.type_(dict[int, str]) == rfl.Generic(dict, (int, str), (_0, _1), dict[int, str])
+    assert_generic_full_eq(list[int], rfl.Generic(list, (int,), (_0,), list[int]))
+    assert_generic_full_eq(set[int], rfl.Generic(set, (int,), (_0,), set[int]))
+    assert_generic_full_eq(dict[int, str], rfl.Generic(dict, (int, str), (_0, _1), dict[int, str]))
 
     assert rfl.type_(list) is list
-    assert rfl.type_(ta.List) == rfl.Generic(list, (_0,), (_0,), ta.List)  # noqa
-    assert rfl.type_(list[int]) == rfl.Generic(list, (int,), (_0,), list[int])
-    assert rfl.type_(ta.List[int]) == rfl.Generic(list, (int,), (_0,), ta.List[int])  # noqa
+    assert_generic_full_eq(ta.List, rfl.Generic(list, (_0,), (_0,), ta.List))  # noqa
+    assert_generic_full_eq(list[int], rfl.Generic(list, (int,), (_0,), list[int]))
+    assert_generic_full_eq(ta.List[int], rfl.Generic(list, (int,), (_0,), ta.List[int]))  # noqa
 
 
 def test_new_unions():
@@ -141,9 +146,9 @@ def test_newtype():
 
 
 def test_callable():
-    assert rfl.type_(ta.Callable[[], int]) == rfl.Generic(collections.abc.Callable, (int,), (_0,), ta.Callable[[], int])  # type: ignore  # noqa
-    assert rfl.type_(ta.Callable[[int], None]) == rfl.Generic(collections.abc.Callable, (int, type(None)), (_0, _1), ta.Callable[[int], None])  # type: ignore  # noqa
-    assert rfl.type_(ta.Callable[[int, float], str]) == rfl.Generic(collections.abc.Callable, (int, float, str), (_0, _1, _2), ta.Callable[[int, float], str])  # type: ignore  # noqa
+    assert_generic_full_eq(ta.Callable[[], int], rfl.Generic(collections.abc.Callable, (int,), (_0,), ta.Callable[[], int]))  # type: ignore  # noqa
+    assert_generic_full_eq(ta.Callable[[int], None], rfl.Generic(collections.abc.Callable, (int, type(None)), (_0, _1), ta.Callable[[int], None]))  # type: ignore  # noqa
+    assert_generic_full_eq(ta.Callable[[int, float], str], rfl.Generic(collections.abc.Callable, (int, float, str), (_0, _1, _2), ta.Callable[[int, float], str]))  # type: ignore  # noqa
 
     with pytest.raises(TypeError):
         rfl.type_(ta.Callable[..., int])
@@ -154,5 +159,5 @@ def test_callable():
 
 
 def test_generic_type():
-    assert rfl.type_(type[int]) == rfl.Generic(type, (int,), (_0,), type[int])
+    assert_generic_full_eq(type[int], rfl.Generic(type, (int,), (_0,), type[int]))
     assert rfl.type_(type) is type
