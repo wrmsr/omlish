83e8d55955e2caf38431d00ea74c743982cbbcab
wrmsr <timwilloney@gmail.com>
2024-08-02 10:39:22 -0700

    --
    Former-commit-id: 4de0e62a87d7073f9988ddc5d0ba0c5c291f71cc

--- omlish/inject/origins.py
+++ omlish/inject/origins.py
@@ -1,11 +1,6 @@
-import sys
-import types
 import typing as ta
 
 from .. import dataclasses as dc
-
-
-_ORIGIN_ATTR = '__inject_origin__'
 
 
 @dc.dataclass(frozen=True)
@@ -13,43 +8,6 @@ class Origin:
     lst: ta.Sequence[str]
 
 
-_ORIGIN_FRAMES = 5
-_ORIGIN_BASE_OFS = 2
-_ORIGIN_IGNORED_PACKAGES = frozenset([
-    __package__,
-    __package__ + '.impl',
-])
-
-
-def _is_origin_frame(f: types.FrameType) -> bool:
-    gl = f.f_globals
-    try:
-        pkg = gl['__package__']
-    except KeyError:
-        pass
-    else:
-        if pkg in _ORIGIN_IGNORED_PACKAGES:
-            return False
-    return True
-
-
-def _build_origin(ofs: int = 0) -> Origin:
-    lst = []  # type: ignore
-    cur = sys._getframe(_ORIGIN_BASE_OFS + ofs)  # noqa
-    while len(lst) < _ORIGIN_FRAMES and cur is not None:
-        if _is_origin_frame(cur):
-            lst.append(cur)
-        cur = cur.f_back  # type: ignore
-    return Origin([str(f) for f in lst])
-
-
-class HasOrigin:
-    @property
-    def origin(self) -> Origin:
-        raise NotImplementedError
-
-    def __post_init__(self) -> None:
-        dc.maybe_post_init(super())
-        if _ORIGIN_ATTR in self.__dict__:
-            raise KeyError('Origin already set')
-        self.__dict__[_ORIGIN_ATTR] = _build_origin()
+@dc.dataclass(frozen=True)
+class Origins:
+    lst: ta.Sequence[Origin]
