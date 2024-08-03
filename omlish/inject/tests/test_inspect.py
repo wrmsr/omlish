d51ef957eba229547437f32ca65b9bc7a042fcfc
wrmsr <timwilloney@gmail.com>
2024-08-03 16:38:45 -0700

    --

---
+++ omlish/inject/tests/test_inspect.py
@@ -0,0 +1,18 @@
+import typing as ta
+
+from ... import check
+from ... import dataclasses as dc
+from ..impl.inspect import signature
+
+
+@dc.dataclass(frozen=True)
+class Tag:
+    tag: ta.Any = dc.xfield(check=lambda o: not isinstance(o, Tag))
+
+
+def test_tags():
+    def f(a: ta.Annotated[int, Tag(420)]):
+        pass
+
+    s = signature(f)
+    print(s)
