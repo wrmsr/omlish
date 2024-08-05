f6b4632404f09aa6719f9af4f38930d38385e850
wrmsr <timwilloney@gmail.com>
2024-08-05 13:16:01 -0700

    --
    Former-commit-id: 924e1fa5d3806d55cf0d5391eb663090bdf63f62

--- app/server/handlers/inject.py
+++ app/server/handlers/inject.py
@@ -1,6 +1,7 @@
 from omlish import inject as inj
 from omserv.apps.inject import bind_handler
 
+from .diag import DiagHandler
 from .favicon import FaviconHandler
 from .index import IndexHandler
 from .login import LoginHandler
@@ -12,11 +13,12 @@ from .tik import TikHandler
 
 def bind() -> inj.Elemental:
     return inj.as_elements(
-        bind_handler(IndexHandler),
-        bind_handler(ProfileHandler),
-        bind_handler(LoginHandler),
-        bind_handler(SignupHandler),
-        bind_handler(LogoutHandler),
+        bind_handler(DiagHandler),
         bind_handler(FaviconHandler),
+        bind_handler(IndexHandler),
+        bind_handler(LoginHandler),
+        bind_handler(LogoutHandler),
+        bind_handler(ProfileHandler),
+        bind_handler(SignupHandler),
         bind_handler(TikHandler),
     )
