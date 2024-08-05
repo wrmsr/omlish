342043c21dc9a69fbaee6b47af536167a983851e
wrmsr <timwilloney@gmail.com>
2024-08-05 16:12:45 -0700

    --
    Former-commit-id: 4dfc04fc3e0644c8e2ab0122817cf10e53447e83

--- omlish/configs/tests/test_strings.py
+++ omlish/configs/tests/test_strings.py
@@ -1,5 +1,10 @@
+import typing as ta
+
 from ... import dataclasses as dc
 from ... import lang
+
+
+T = ta.TypeVar('T')
 
 
 class InterpolateStringsMetadata(lang.Marker):
@@ -18,10 +23,26 @@ def interpolate_field(f: dc.Field) -> dc.Field:
     return dc.update_field_metadata(f, {InterpolateStringsMetadata: True})
 
 
+@dc.dataclass(frozen=True)
 class Foo:
     i: int
     s: str = dc.field() | interpolate_field
 
 
+def interpolate_fields(obj: T, replacements: ta.Mapping[str, str]) -> T:
+    kw = {}
+    for f in dc.fields(obj):
+        if not f.metadata.get(InterpolateStringsMetadata):
+            continue
+        raise NotImplementedError
+    if not kw:
+        return obj
+    return dc.replace(obj, **kw)
+
+
 def test_strings():
-    pass
+    c = Foo(4, 'hi $THERE bye')
+    assert c.s == 'hi $THERE bye'
+    rpl = {'$THERE': 'yes'}
+    c2 = interpolate_fields(c, rpl)
+    assert c2.s == 'hi yes bye'
