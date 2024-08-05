f6db9f62143d5fdcab66f46e3627b513c17432c9
wrmsr <timwilloney@gmail.com>
2024-08-05 16:19:23 -0700

    --
    Former-commit-id: 3375ac501200b96d10555630715b0f59bf8ffbed

--- omlish/configs/tests/test_strings.py
+++ omlish/configs/tests/test_strings.py
@@ -24,17 +24,6 @@ def interpolate_field(f: dc.Field) -> dc.Field:
     return dc.update_field_metadata(f, {InterpolateStringsMetadata: True})
 
 
-@dc.dataclass(frozen=True)
-class Foo:
-    i: int
-    s: str = dc.field() | interpolate_field
-
-
-@dc.dataclass(frozen=True)
-class Foo2:
-    l: list[Foo]
-
-
 def interpolate_fields(obj: T, replacements: ta.Mapping[str, str]) -> T:
     kw = {}
 
@@ -55,6 +44,25 @@ def interpolate_fields(obj: T, replacements: ta.Mapping[str, str]) -> T:
     return dc.replace(obj, **kw)
 
 
+##
+
+
+@dc.dataclass(frozen=True)
+class Foo:
+    i: int
+    s: str = dc.field() | interpolate_field
+
+
+@dc.dataclass(frozen=True)
+class ParentFoo:
+    f: Foo
+
+
+@dc.dataclass(frozen=True)
+class ListFoo:
+    l: list[Foo]
+
+
 def test_strings():
     c = Foo(4, 'hi $THERE bye')
     assert c.s == 'hi $THERE bye'
@@ -62,10 +70,16 @@ def test_strings():
     c2 = interpolate_fields(c, rpl)
     assert c2.s == 'hi yes bye'
 
-    lc = Foo2([Foo(4, 'hi $THERE bye'), Foo(5, 'bye $HERE hi')])
+    pc = ParentFoo(Foo(4, 'hi $THERE bye'))
+    assert pc.f.s == 'hi $THERE bye'
+    rpl = {'$THERE': 'yes'}
+    pc2 = interpolate_fields(pc, rpl)
+    assert pc2.f.s == 'hi yes bye'
+
+    lc = ListFoo([Foo(4, 'hi $THERE bye'), Foo(5, 'bye $HERE hi')])
     assert lc.l[0].s == 'hi $THERE bye'
     assert lc.l[1].s == 'bye $HERE hi'
     rpl = {'$THERE': 'yes', '$HERE': 'no'}
-    lc2 = interpolate_fields(c, rpl)
+    lc2 = interpolate_fields(lc, rpl)
     assert lc2.l[0].s == 'hi yes bye'
     assert lc2.l[1].s == 'bye no hi'
