ab50e651049ac4f69a69a327602ca80ef8370257
wrmsr <timwilloney@gmail.com>
2024-08-06 10:11:45 -0700

    --
    Former-commit-id: c9f9742519a285193a31d9c4f42565656851773c

---
+++ omlish/sql/tests/test_duckdb.py
@@ -0,0 +1,27 @@
+import pytest
+import sqlalchemy as sa
+
+from ... import lang
+from ...diag import pydevd as pdu  # noqa
+from ...testing import pytest as ptu
+from .. import duckdb as _duckdb
+
+
+##
+
+
+@pytest.fixture(autouse=True)
+def _patch_for_trio_asyncio_fixture():
+    pdu.patch_for_trio_asyncio()
+
+
+##
+
+
+@ptu.skip_if_cant_import('duckdb')
+def test_postgres_duckdb():
+    url = _duckdb.DuckdbDialect.name + '://'
+
+    with lang.disposing(sa.create_engine(url, echo=True)) as engine:
+        with engine.connect() as conn:
+            print(conn.execute(sa.text('select 1')).fetchall())
