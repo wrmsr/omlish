3026b20381a26e61c70cd81e3b65918765aa80d3
wrmsr <timwilloney@gmail.com>
2024-08-06 10:49:09 -0700

    --
    Former-commit-id: 049e0ecc76bcd39a0fcc7c664f9a6a90b00b916f

--- x/maysync.py
+++ x/maysync.py
@@ -1,3 +1,35 @@
 """
 https://github.com/sqlalchemy/sqlalchemy/blob/main/lib/sqlalchemy/util/concurrency.py
 """
+import asyncio
+import greenlet
+
+
+def callback():
+    return 'callback'
+
+
+async def a_callback():
+    return 'a_callback'
+
+
+def func(cb):
+    return 'func -> ' + cb()
+
+
+async def a_func(a_cb):
+    return 'a_func -> ' + await a_cb()
+
+
+async def _a_main():
+    print(await a_func(a_callback))
+
+
+def _main():
+    print(func(callback))
+
+    asyncio.run(_a_main())
+
+
+if __name__ == '__main__':
+    _main()
