cdbcf1be35b77a4044d4996702f6e75d2160e1d2
wrmsr <timwilloney@gmail.com>
2024-08-06 10:55:22 -0700

    --

--- x/maysync.py
+++ x/maysync.py
@@ -5,28 +5,43 @@ import asyncio
 import greenlet
 
 
-def callback():
-    return 'callback'
+##
 
 
-async def a_callback():
-    return 'a_callback'
+def s_to_a(fn):
+    async def inner(*args, **kwargs):
+        return fn(*args, **kwargs)
+    return inner
 
 
-def func(cb):
-    return 'func -> ' + cb()
+##
 
 
-async def a_func(a_cb):
-    return 'a_func -> ' + await a_cb()
+def callback(arg):
+    return f'callback({arg})'
+
+
+async def a_callback(arg):
+    return f'a_callback({arg})'
+
+
+def func(cb, arg):
+    return f'func({arg}) -> {cb(arg)}'
+
+
+async def a_func(a_cb, arg):
+    return f'a_func({arg}) -> {await a_cb(arg)}'
 
 
 async def _a_main():
-    print(await a_func(a_callback))
+    print(await a_func(a_callback, 'arg'))
+    print(await a_func(s_to_a(callback), 'arg'))
 
 
 def _main():
-    print(func(callback))
+    print(func(callback, 'arg'))
+
+    #
 
     asyncio.run(_a_main())
 
