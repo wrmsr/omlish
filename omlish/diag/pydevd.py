c87c8e02f50a0d30f6390914e9205f6f02aa00d2
wrmsr <timwilloney@gmail.com>
2024-08-05 18:15:33 -0700

    --
    Former-commit-id: f3fe04831798859c6f80cc3e043e25476c742043

--- omlish/diag/pydevd.py
+++ omlish/diag/pydevd.py
@@ -32,6 +32,7 @@ import os
 import sys
 import tempfile
 import textwrap
+import threading
 import types
 import typing as ta
 
@@ -243,3 +244,37 @@ def maybe_reexec(
         args = [args[0], bootstrap_path]
 
     os.execvp(sys.executable, args)
+
+
+def debug_unhandled_exception(exc_info: ta.Any = None) -> None:
+    if exc_info is None:
+        exc_info = sys.exc_info()
+
+    try:
+        import pydevd
+        from pydevd import pydevd_tracing
+
+    except ImportError:
+        return
+
+    exctype, value, traceback = exc_info
+    frames = []
+    while traceback:
+        frames.append(traceback.tb_frame)
+        traceback = traceback.tb_next
+
+    thread = threading.current_thread()
+    frames_by_id = {id(frame): frame for frame in frames}
+    frame = frames[-1]
+    exception = (exctype, value, traceback)
+
+    if hasattr(thread, 'additional_info'):
+        thread.additional_info.pydev_message = 'server exception'
+    try:
+        debugger = pydevd.debugger  # noqa
+    except AttributeError:
+        debugger = pydevd.get_global_debugger()  # noqa
+
+    pydevd_tracing.SetTrace(None)
+
+    debugger.stop_on_unhandled_exception(thread, frame, frames_by_id, exception)
