e78d1033c7e2ff1bee137469c590ae236192159a
wrmsr <timwilloney@gmail.com>
2024-08-06 11:03:50 -0700

    --

--- x/maysync.py
+++ x/maysync.py
@@ -4,6 +4,8 @@ https://github.com/sqlalchemy/sqlalchemy/blob/main/lib/sqlalchemy/util/concurren
 import asyncio
 import greenlet
 
+from omlish import asyncs
+
 
 ##
 
@@ -11,6 +13,12 @@ import greenlet
 def s_to_a(fn):
     async def inner(*args, **kwargs):
         return fn(*args, **kwargs)
+    return inner
+
+
+def a_to_s(fn):
+    def inner(*args, **kwargs):
+        return asyncs.sync_await(fn, *args, **kwargs)
     return inner
 
 
@@ -34,12 +42,23 @@ async def a_func(a_cb, arg):
 
 
 async def _a_main():
+    print('_a_main')
+
     print(await a_func(a_callback, 'arg'))
     print(await a_func(s_to_a(callback), 'arg'))
 
+    print(await s_to_a(func)(callback, 'arg'))
+    print(await s_to_a(func)(a_to_s(a_callback), 'arg'))
+
 
 def _main():
+    print('_main')
+
     print(func(callback, 'arg'))
+    print(func(a_to_s(a_callback), 'arg'))
+
+    print(a_to_s(a_func)(a_callback, 'arg'))
+    print(a_to_s(a_func)(s_to_a(callback), 'arg'))
 
     #
 
