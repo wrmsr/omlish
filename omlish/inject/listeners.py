cf6f31f70a8ac2c754d587fcecbe39ee7263afe4
wrmsr <timwilloney@gmail.com>
2024-08-06 12:57:16 -0700

    --

--- omlish/inject/listeners.py
+++ omlish/inject/listeners.py
@@ -2,12 +2,18 @@ import typing as ta
 
 from .. import dataclasses as dc
 from .. import lang
+from .bindings import Binding
 from .elements import Element
 from .injector import Injector
 from .keys import Key
 
 
-ProvisionListener: ta.TypeAlias = ta.Callable[[Injector, Key, ta.Callable[[], ta.Any]], ta.Callable[[], ta.Any]]
+ProvisionListener: ta.TypeAlias = ta.Callable[[
+    Injector,
+    Key,
+    Binding,
+    ta.Callable[[], ta.Any],
+], ta.Callable[[], ta.Any]]
 
 
 @dc.dataclass(frozen=True)
