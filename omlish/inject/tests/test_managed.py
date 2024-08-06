b1af473c3e58978870d54fe38c37a0360fcf28de
wrmsr <timwilloney@gmail.com>
2024-08-05 18:08:58 -0700

    --
    Former-commit-id: 5d8dd151c28f99e8ba7d4618a44a4691c6bea70e

--- omlish/inject/tests/test_managed.py
+++ omlish/inject/tests/test_managed.py
@@ -1,20 +1,40 @@
-# import contextlib
-# import typing as ta
-#
-# from ... import dataclasses as dc
-# from ... import inject as inj
-# from ... import lang
+import contextlib
+import typing as ta
+
+from ... import dataclasses as dc  # noqa
+from ... import inject as inj
+from ... import lang  # noqa
 
 
 class SomeManager:
     ec = 0
     xc = 0
 
-    def __enter__(self):
+    def __enter__(self) -> ta.Self:
         self.ec += 1
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.xc += 1
+
+
+def test_managed():
+    def _provide_some_manager(
+            i: inj.Injector,
+            es: contextlib.ExitStack,
+    ) -> SomeManager:
+        return es.enter_context(i.inject(SomeManager))
+
+    with inj.create_managed_injector(
+        inj.bind(_provide_some_manager, singleton=True),
+    ) as i:
+        sm = i[SomeManager]
+        assert sm.ec == 1
+        assert i[SomeManager] is sm
+        assert sm.ec == 1
+        assert sm.xc == 0
+    assert sm.ec == 1
+    assert sm.xc == 1
 
 
 # def test_managed():
