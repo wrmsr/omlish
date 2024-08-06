363aa790d8e6f4e60ab6ba5c04a048d85e32d333
wrmsr <timwilloney@gmail.com>
2024-08-06 14:21:09 -0700

    --
    Former-commit-id: b1d478c5e040be1330aa4cc8820cb1382f13a9a0

--- omdev/exts/build.py
+++ omdev/exts/build.py
@@ -16,6 +16,7 @@ def build_ext(
     ext = du.Extension(
         fullname,
         sources=[src_path],
+        include_dirs=[os.path.dirname(src_path)],
         extra_compile_args=[
             *(['-std=c++14'] if any(src_path.endswith(sf) for sf in ('cc', 'cpp')) else []),
         ],
