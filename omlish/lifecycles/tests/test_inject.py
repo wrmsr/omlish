eaa7a078dc13fdad169daa123baeb5c9ca87a31f
wrmsr <timwilloney@gmail.com>
2024-08-06 17:50:57 -0700

    --
    Former-commit-id: 2e2e4640528ae2d370c86453d13ff01cfbd1f27c

--- omlish/lifecycles/tests/test_inject.py
+++ omlish/lifecycles/tests/test_inject.py
@@ -1,19 +1,28 @@
 """
 TODO:
+ - ** WeakIdentitySet **
  - *external* Lifecycles - objs themselves hide their lc's
 """
 import typing as ta
 import weakref
 
 from ... import check
+from ... import collections as col
 from ... import dataclasses as dc
 from ... import inject as inj
 from ... import lang
 from ..base import Lifecycle
+from ..contextmanagers import LifecycleContextManager
 from ..manager import LifecycleManager
 
 
 ##
+
+
+def _new_weak_identity_set():
+    s = weakref.WeakSet()
+    s.data = col.IdentitySet()
+    return s
 
 
 class _LifecycleRegistrar(lang.Final):
@@ -21,7 +30,7 @@ class _LifecycleRegistrar(lang.Final):
     def __init__(self) -> None:
         super().__init__()
 
-        self._seen: ta.MutableSet[ta.Any] = weakref.WeakSet()
+        self._seen: ta.MutableSet[ta.Any] = _new_weak_identity_set()
         self._stack: list[_LifecycleRegistrar.State] = []
 
     @dc.dataclass(frozen=True)
@@ -68,23 +77,37 @@ class _LifecycleRegistrar(lang.Final):
 ##
 
 
-@dc.dataclass(frozen=True)
+class DumbLifecycle(Lifecycle):
+    def lifecycle_construct(self) -> None:
+        print(f'{self}.lifecycle_construct')
+
+    def lifecycle_start(self) -> None:
+        print(f'{self}.lifecycle_start')
+
+    def lifecycle_stop(self) -> None:
+        print(f'{self}.lifecycle_stop')
+
+    def lifecycle_destroy(self) -> None:
+        print(f'{self}.lifecycle_destroy')
+
+
+@dc.dataclass()
 class Db(Lifecycle):
     pass
 
 
-@dc.dataclass(frozen=True)
+@dc.dataclass()
 class ServiceA(Lifecycle):
     db: Db
     i: int
 
 
-@dc.dataclass(frozen=True)
+@dc.dataclass()
 class ServiceB(Lifecycle):
     db: Db
 
 
-@dc.dataclass(frozen=True)
+@dc.dataclass()
 class ServiceC(Lifecycle):
     a: ServiceA
     b: ServiceB
@@ -102,9 +125,9 @@ def test_inject():
             inj.bind(Db, singleton=True),
             inj.bind(ServiceA, singleton=True),
             inj.bind(ServiceB, singleton=True),
-            inj.bind(ServiceC, singleton=True),
+            inj.bind(ServiceC, singleton=True, eager=True),
 
             inj.bind(420),
     ) as i:  # noqa
-        c = i[ServiceC]
-        print(c)
+        with LifecycleContextManager(i[LifecycleManager]):
+            print(i[ServiceC])
