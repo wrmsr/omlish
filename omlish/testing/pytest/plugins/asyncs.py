df3938d8dd4db2b107edf3fb72ac928a1a9c762a
wrmsr <timwilloney@gmail.com>
2024-08-07 13:13:45 -0700

    --
    Former-commit-id: 81d507f8b18ecb5b90aced8cc8868a224e73d55d

--- omlish/testing/pytest/plugins/asyncs.py
+++ omlish/testing/pytest/plugins/asyncs.py
@@ -1,18 +1,36 @@
+import inspect
+
+import pytest
+
+from .... import lang
 from ._registry import register
 
+
+KNOWN_BACKENDS = (
+    'asyncio',
+    'trio',
+)
 
 PARAM_NAME = '__async_backend'
 
 
 @register
 class AsyncsPlugin:
+    async_backends = [
+        *[s for s in KNOWN_BACKENDS if lang.can_import(s)],
+    ]
 
     def pytest_configure(self, config):
         config.addinivalue_line('markers', 'all_async_backends: marks for all async backends')
 
-    def pytest_generate_tests(self, metafunc):
-        if metafunc.definition.get_closest_marker('all_async_backends') is None:
-            return
-
-        metafunc.fixturenames.append(PARAM_NAME)
-        metafunc.parametrize(PARAM_NAME, ['asyncio', 'trio'])
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_pycollect_makeitem(self, collector, name, obj) -> None:
+        # ~> https://github.com/agronholm/anyio/blob/f8f269699795373057ac7b0153ec1a217d94461a/src/anyio/pytest_plugin.py#L91  # noqa
+        if collector.istestfunction(obj, name) and inspect.iscoroutinefunction(obj):
+            if (
+                    collector.get_closest_marker('all_async_backends') is not None or
+                    any(marker.name == 'all_async_backends' for marker in getattr(obj, 'pytestmark', ()))
+            ):
+                pytest.mark.anyio()(obj)
+                pytest.mark.usefixtures("anyio_backend")(obj)
+                pytest.mark.parametrize('anyio_backend', self.async_backends)(obj)
