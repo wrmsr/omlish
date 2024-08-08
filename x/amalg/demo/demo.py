997e1d56c4520c96906df1af96e304f1cdc5470c
wrmsr <timwilloney@gmail.com>
2024-08-08 13:45:43 -0700

    --

--- x/amalg/demo/demo.py
+++ x/amalg/demo/demo.py
@@ -2,14 +2,20 @@ import os.path  # noqa
 import pprint
 import typing as ta  # noqa
 
-from .stdlib import check_not_none
+from .std.cached import cached_nullary
+from .std.check import check_not_none
+
+
+@cached_nullary
+def _foo():
+    return 5
 
 
 def _main() -> None:
     """Docstring"""
 
     # Comment
-    check_not_none(5)  # Inline comment
+    check_not_none(_foo())  # Inline comment
 
     pprint.pprint('hi')
 
---
+++ x/amalg/demo/std/check.py
@@ -0,0 +1,16 @@
+import typing as ta
+
+
+T = ta.TypeVar('T')
+
+
+def check_not_none(v: ta.Optional[T]) -> T:  # noqa
+    if v is None:
+        raise ValueError
+    return v
+
+
+def check_not(v: ta.Any) -> None:
+    if v:
+        raise ValueError(v)
+    return v
