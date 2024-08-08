a34d81bd87645fa5f542e0c5c8407c1c3763323b
wrmsr <timwilloney@gmail.com>
2024-08-08 16:34:30 -0700

    --
    Former-commit-id: b19c076a4ece14beb9ba6283858e2913b416fff8

--- app/server/tests/test_app.py
+++ app/server/tests/test_app.py
@@ -27,10 +27,11 @@ def randhex(l: int) -> str:
 
 
 @ptu.skip_if_cant_import('greenlet')
-@pytest.mark.all_asyncs('trio', 'asyncio')
-# @pytest.mark.trio
-# @pytest.mark.asyncio
-# @pytest.mark.trio_asyncio
+@pytest.mark.all_asyncs(
+    'asyncio',
+    # 'trio',
+    'trio_asyncio',
+)
 # @pytest.mark.drain_asyncio
 async def test_demo_auth():
     # from omlish import logs  # noqa
