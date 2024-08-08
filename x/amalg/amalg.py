d824d7ce12ba86db35d314290c2c47cac38a22cb
wrmsr <timwilloney@gmail.com>
2024-08-07 17:54:54 -0700

    --
    Former-commit-id: 023e05191ce2a14d7074425af010dd7a7481a851

--- x/amalg/amalg.py
+++ x/amalg/amalg.py
@@ -1,11 +1,21 @@
 import os.path
+import pprint
+
+import tokenize_rt
 
 
 def _main() -> None:
     root_dir = os.path.dirname(__file__)
     with open(os.path.join(root_dir, 'demo/demo.py')) as f:
         src = f.read()
+
     print(src)
+
+    toks = tokenize_rt.src_to_tokens(src)
+    pprint.pprint(toks)
+
+    src2 = tokenize_rt.tokens_to_src(toks)
+    print(src2)
 
 
 if __name__ == '__main__':
