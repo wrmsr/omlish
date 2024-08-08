1f56e587a14d6e98c87adbc13796ac84e3e3c27d
wrmsr <timwilloney@gmail.com>
2024-08-08 10:02:55 -0700

    --
    Former-commit-id: 012297ce0de28bfc87e4959f53c1f69ff96c4b4d

--- omlish/testing/pytest/plugins/asyncs.py
+++ omlish/testing/pytest/plugins/asyncs.py
@@ -36,12 +36,8 @@ class AsyncsPlugin:
         for c in metafunc._calls:  # noqa
             be = c.params[PARAM_NAME]
             if be == 'trio_asyncio':
-                # ~> https://github.com/agronholm/anyio/blob/f8f269699795373057ac7b0153ec1a217d94461a/src/anyio/pytest_plugin.py#L91  # noqa
-                # c.marks.extend([
-                #     pytest.Mark('anyio', (), {}),
-                #     pytest.Mark('usefixtures', ('anyio_backend',), {}),
-                #     pytest.Mark('anyio_backend', ('asyncio',), {}),
-                # ])
+                c.marks.append(pytest.Mark('trio', (), {}))
+                c.fixturenames.append('__trio_asyncio_fixture')
                 raise NotImplementedError
             else:
                 c.marks.append(pytest.Mark(be, (), {}))
