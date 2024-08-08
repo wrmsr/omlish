32786c6e816cf13e2ea16705f264d18335e0253d
wrmsr <timwilloney@gmail.com>
2024-08-07 17:56:25 -0700

    --

--- x/amalg/amalg.py
+++ x/amalg/amalg.py
@@ -6,16 +6,23 @@ import tokenize_rt
 
 def _main() -> None:
     root_dir = os.path.dirname(__file__)
-    with open(os.path.join(root_dir, 'demo/demo.py')) as f:
-        src = f.read()
 
-    print(src)
+    for src_file in [
+        'demo/demo.py',
+        'demo/stdlib.py',
+    ]:
+        print(src_file)
 
-    toks = tokenize_rt.src_to_tokens(src)
-    pprint.pprint(toks)
+        with open(os.path.join(root_dir, src_file)) as f:
+            src = f.read()
 
-    src2 = tokenize_rt.tokens_to_src(toks)
-    print(src2)
+        print(src)
+
+        toks = tokenize_rt.src_to_tokens(src)
+        pprint.pprint(toks)
+
+        src2 = tokenize_rt.tokens_to_src(toks)
+        print(src2)
 
 
 if __name__ == '__main__':
