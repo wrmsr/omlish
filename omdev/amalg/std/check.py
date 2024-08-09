ba147c9b68834583dded90104810d16c8cb889d6
wrmsr <timwilloney@gmail.com>
2024-08-09 12:58:01 -0700

    --
    Former-commit-id: 8101690a20df118426f86ab3fa30d6b1772fd46b

--- omdev/amalg/std/check.py
+++ omdev/amalg/std/check.py
@@ -1,10 +1,11 @@
+# ruff: noqa: UP007
 import typing as ta
 
 
 T = ta.TypeVar('T')
 
 
-def check_not_none(v: ta.Optional[T]) -> T:  # noqa
+def check_not_none(v: ta.Optional[T]) -> T:
     if v is None:
         raise ValueError
     return v
