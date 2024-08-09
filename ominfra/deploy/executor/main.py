14ec80b738ba472b88246a519f9d59efdd387fd6
wrmsr <timwilloney@gmail.com>
2024-08-09 13:56:53 -0700

    --
    Former-commit-id: 54e25c03744b3216389f9dce6aeee1feb2a71701

--- ominfra/deploy/executor/main.py
+++ ominfra/deploy/executor/main.py
@@ -100,27 +100,7 @@ from .base import Concern
 from .base import Deployment
 from .base import Phase
 from .base import run_in_phase
-
-
-##
-
-
-class User(Concern):
-    @run_in_phase(Phase.HOST)
-    def create_user(self) -> None:
-        try:
-            pwd.getpwnam(self._d.host_cfg.username)
-        except KeyError:
-            log.info('Creating user %s', self._d.host_cfg.username)
-            self._d.sh(' '.join([
-                'adduser',
-                '--system',
-                '--disabled-password',
-                '--group',
-                '--shell /bin/bash',
-                self._d.host_cfg.username,
-            ]))
-            pwd.getpwnam(self._d.host_cfg.username)
+from .concerns.user import User
 
 
 ##
