26fcb7d63134a75f4d60ffc5b7fb3f32a7168749
wrmsr <timwilloney@gmail.com>
2024-08-09 13:46:06 -0700

    --
    Former-commit-id: bfd1f6ccb189f9700c3addf71b82c304a7c9df5f

--- omdev/scripts/interp.py
+++ omdev/scripts/interp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# @omdev-amalg-output ../interp/interp.py
 """
 TODO:
  - install (git clone) pyenv
--- omdev/scripts/pyproject.py
+++ omdev/scripts/pyproject.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# @omdev-amalg-output ../pyproject/pyproject.py
 """
 TODO:
  - check / tests, src dir sets
