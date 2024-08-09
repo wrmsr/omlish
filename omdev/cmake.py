2fc02fed627d80de029254f54e5e0fba09251952
wrmsr <timwilloney@gmail.com>
2024-08-08 21:08:59 -0700

    --
    Former-commit-id: 6180c4337508f8fbe4a11077e3aea3f4648c2068

--- omdev/cmake.py
+++ omdev/cmake.py
@@ -75,12 +75,18 @@ class Executable(Target):
 
 class CmakeGen:
 
-    def __init__(self, out: io.TextIOBase) -> None:
+    def __init__(
+            self,
+            out: io.TextIOBase,
+            *,
+            indent: int = 4,
+    ) -> None:
         super().__init__()
 
         self._out = out
+        self._indent = indent
 
-    def _write(
+    def write(
             self,
             obj: str | ta.Sequence[str] = '',
             *,
@@ -101,56 +107,45 @@ class CmakeGen:
             if spacing is not None:
                 self._out.write(spacing)
 
-    def _write_section(self, label: str) -> None:
-        self._write(['', f'### {label}', ''])
+    def write_section(self, label: str) -> None:
+        self.write(['', f'### {label}', ''])
 
-    def _write_cmd(self, cmd: Command) -> None:
+    def write_cmd(self, cmd: Command) -> None:
         args = cmd.args
         if not isinstance(args, str):
             args = ' '.join(args)
         if not cmd.lines:
-            self._write(f'{cmd.name}({args})')
+            self.write(f'{cmd.name}({args})')
         else:
             if isinstance(cmd.lines, str):
                 raise TypeError(cmd.lines)
-            self._write(f'{cmd.name}({args}')
-            self._write(cmd.lines, indent=8)
-            self._write(')', indent=8)
-            self._write()
+            self.write(f'{cmd.name}({args}')
+            self.write(cmd.lines, indent=self._indent)
+            self.write(')')
+            self.write()
 
-    def _write_var(self, var: Var) -> None:
-        return self._write_cmd(Command('set', var.name, [var.value] if isinstance(var.value, str) else var.value))
+    def write_var(self, var: Var) -> None:
+        return self.write_cmd(Command('set', var.name, [var.value] if isinstance(var.value, str) else var.value))
 
-    def _write_target(self, target: Target) -> None:
-        self._write_section(target.name)
-        self._write_cmd(Command(target.command_name, [target.name, *target.command_extra], target.source_files))
+    def write_target(self, target: Target) -> None:
+        self.write_section(target.name)
+        self.write_cmd(Command(target.command_name, [target.name, *target.command_extra], target.source_files))
         if target.include_directories:
-            self._write_cmd(Command('target_include_directories', [target.name, 'PRIVATE'], target.include_directories))
+            self.write_cmd(Command('target_include_directories', [target.name, 'PRIVATE'], target.include_directories))
         if target.compile_options:
-            self._write_cmd(Command('target_compile_options', [target.name, 'PRIVATE'], target.compile_options))
+            self.write_cmd(Command('target_compile_options', [target.name, 'PRIVATE'], target.compile_options))
         if target.link_options:
-            self._write_cmd(Command('target_link_options', [target.name, 'PRIVATE'], target.link_options))
+            self.write_cmd(Command('target_link_options', [target.name, 'PRIVATE'], target.link_options))
         if target.compile_flags_by_source_file:
             for sf, cf in target.compile_flags_by_source_file.items():
                 cf = ['"' + f.replace('"', '\\"') + '"' for f in cf]
-                self._write_cmd(Command('set_source_files_properties', [sf, 'PROPERTIES', 'COMPILE_FLAGS'], cf))
+                self.write_cmd(Command('set_source_files_properties', [sf, 'PROPERTIES', 'COMPILE_FLAGS'], cf))
         if target.link_libraries:
-            self._write_cmd(Command('target_link_libraries', [target.name, 'PRIVATE'], target.link_libraries))
+            self.write_cmd(Command('target_link_libraries', [target.name, 'PRIVATE'], target.link_libraries))
 
     @property
-    def preamble(self) -> list[str]:
+    def preamble(self) -> ta.Sequence[str]:
         return [
             'cmake_minimum_required(VERSION 3.1...3.16)',
             'set(EXECUTABLE_OUTPUT_PATH ${CMAKE_SOURCE_DIR})',
         ]
-
-    @property
-    def common_vars(self) -> list[Var]:
-        return []
-
-    def write(self) -> None:
-        self._write(self.preamble, spacing=1)
-
-        self._write_section('Common')
-        for var in self.common_vars:
-            self._write_var(var)
