9363715364f364534c6688da21b752d32c3d27e6
wrmsr <timwilloney@gmail.com>
2024-08-09 12:07:44 -0700

    --
    Former-commit-id: 25f817d3ca66bdc45eaba42581d03cbba656eacf

--- x/amalg/amalg.py
+++ x/amalg/amalg.py
@@ -366,9 +366,9 @@ def _main() -> None:
     src_base_dir = os.path.dirname(__file__)
     for main_file in [
         'demo/demo.py',
-        # 'demo/deploy/deploy.py',
-        # 'demo/interp/interp.py',
-        # 'demo/pyproject/pyproject.py',
+        'demo/deploy/deploy.py',
+        'demo/interp/interp.py',
+        'demo/pyproject/pyproject.py',
     ]:
         main_path = os.path.abspath(os.path.join(src_base_dir, main_file))
 
