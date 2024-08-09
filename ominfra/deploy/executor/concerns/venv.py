b354f1e862def2e908754bd5ab535e685289a015
wrmsr <timwilloney@gmail.com>
2024-08-09 14:00:07 -0700

    --
    Former-commit-id: c1f4ba780d17163de80b24ab3947d50b8c4fd1ee

---
+++ ominfra/deploy/executor/concerns/venv.py
@@ -0,0 +1,18 @@
+from ..base import Concern
+from ..base import Phase
+from ..base import run_in_phase
+
+
+class Venv(Concern):
+    @run_in_phase(Phase.ENV)
+    def setup_venv(self) -> None:
+        self._d.ush(
+            'cd ~/venv',
+            f'{self._d.cfg.python_bin} -mvenv {self._d.cfg.app_name}',
+
+            # https://stackoverflow.com/questions/77364550/attributeerror-module-pkgutil-has-no-attribute-impimporter-did-you-mean
+            f'{self._d.cfg.app_name}/bin/python -m ensurepip',
+            f'{self._d.cfg.app_name}/bin/python -mpip install --upgrade setuptools pip',
+
+            f'{self._d.cfg.app_name}/bin/python -mpip install -r ~deploy/app/{self._d.cfg.app_name}/{self._d.cfg.requirements_txt}',  # noqa
+        )
