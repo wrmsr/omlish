47ee7206af6fcacbb13b49c7acab8dfee75f0870
wrmsr <timwilloney@gmail.com>
2024-08-09 09:30:02 -0700

    --
    Former-commit-id: 3c4672fe25e909f57499044b8ccfdfdf094cc2bc

--- app/worker/__main__.py
+++ app/worker/__main__.py
@@ -1,6 +1,10 @@
-from ..shell import run_shell
-from .main import worker_main
-
-
 if __name__ == '__main__':
-    run_shell(worker_main)
+    from .. import dbs
+    from .. import shell
+    from .inject import bind
+
+    shell.run_shell(
+        shell.bind_asgi_server(bind()),
+        shell.bind_node_registrant(),
+        dbs.bind_dbs(),
+    )
