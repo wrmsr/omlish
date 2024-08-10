a992da37ed9166175ddb240f7c25494e75cffbac
wrmsr <timwilloney@gmail.com>
2024-08-10 16:27:58 -0700

    --
    Former-commit-id: 6181b3bb6636ee3a7085203862efeb945422d746

--- omdev/amalg/std/tests/test_marshal.py
+++ omdev/amalg/std/tests/test_marshal.py
@@ -1,4 +1,6 @@
 # ruff: noqa: PT009
+import datetime
+import json
 import unittest
 
 from .. import marshal as msh
@@ -6,7 +8,22 @@ from .. import marshal as msh
 
 class TestMarshal(unittest.TestCase):
     def test_marshal(self):
-        self.assertEqual(msh.marshal_obj(5), 5)
+        for st in [
+            5,
+            5.,
+            'abc',
+            b'abc',
+            bytearray(b'abc'),
+            datetime.datetime.now(),
+        ]:
+            if isinstance(st, tuple):
+                o, ty = st
+            else:
+                o, ty = st, type(st)
 
-    def test_unmarshal(self):
-        self.assertEqual(msh.unmarshal_obj(5, int), 5)
+            m = msh.marshal_obj(o)
+            s = json.dumps(m)
+            x = json.loads(s)
+            u = msh.unmarshal_obj(x, ty)
+
+            self.assertEqual(u, o)
