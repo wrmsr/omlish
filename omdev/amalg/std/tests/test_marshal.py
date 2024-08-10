5b27789447edad32b649dd74ec4be0d562dbf03e
wrmsr <timwilloney@gmail.com>
2024-08-10 15:23:05 -0700

    --
    Former-commit-id: 477e6c8195b0ce2b036e22492bed6a9db2f62f03

---
+++ omdev/amalg/std/tests/test_marshal.py
@@ -0,0 +1,12 @@
+# ruff: noqa: PT009
+import unittest
+
+from .. import marshal as msh
+
+
+class TestMarshal(unittest.TestCase):
+    def test_marshal(self):
+        self.assertEqual(msh.marshal_obj(5), 5)
+
+    def test_unmarshal(self):
+        self.assertEqual(msh.unmarshal_obj(5), 5)
