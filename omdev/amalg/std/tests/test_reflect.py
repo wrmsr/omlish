ecdc26e5b53c534b2ed1131f8d4d071061e242e7
wrmsr <timwilloney@gmail.com>
2024-08-10 17:08:24 -0700

    --
    Former-commit-id: 8e404a73732eee79545b0f5f1387a54fdcfdd5e4

--- omdev/amalg/std/tests/test_reflect.py
+++ omdev/amalg/std/tests/test_reflect.py
@@ -6,7 +6,7 @@ from .. import reflect as rfl
 
 
 class TestMarshal(unittest.TestCase):
-    def is_optional_alias(self):
+    def test_is_optional_alias(self):
         assert rfl.is_union_alias(ta.Union[int, str])
         assert rfl.is_union_alias(ta.Optional[int])
         assert not rfl.is_union_alias(int)
