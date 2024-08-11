f3c08a0fc707182869aaf4795bf73a7fd6626f36
wrmsr <timwilloney@gmail.com>
2024-08-10 20:30:20 -0700

    --
    Former-commit-id: aa3a563cf6dc2b3d410139168513c4cb443f1bf9

--- omlish/marshal/base64.py
+++ omlish/marshal/base64.py
@@ -1,6 +1,8 @@
 import base64
+import typing as ta
 
 from .. import check
+from .. import dataclasses as dc
 from .base import MarshalContext
 from .base import Marshaler
 from .base import MarshalerFactory
@@ -11,15 +13,28 @@ from .factories import TypeMapFactory
 from .values import Value
 
 
-class Base64MarshalerUnmarshaler(Marshaler, Unmarshaler):
+T = ta.TypeVar('T')
+
+
+@dc.dataclass(frozen=True)
+class Base64MarshalerUnmarshaler(Marshaler, Unmarshaler, ta.Generic[T]):
+    ty: type[T]
+
     def marshal(self, ctx: MarshalContext, o: bytes) -> str:
         return base64.b64encode(o).decode()
 
-    def unmarshal(self, ctx: UnmarshalContext, v: Value) -> bytes:
-        return base64.b64decode(check.isinstance(v, str).encode())
+    def unmarshal(self, ctx: UnmarshalContext, v: Value) -> T:
+        return self.ty(base64.b64decode(check.isinstance(v, str).encode()))
 
 
-BASE64_MARSHALER_UNMARSHALER = Base64MarshalerUnmarshaler()
+BASE64_TYPES = (
+    bytes,
+    bytearray,
+)
 
-BASE64_MARSHALER_FACTORY: MarshalerFactory = TypeMapFactory({bytes: BASE64_MARSHALER_UNMARSHALER})
-BASE64_UNMARSHALER_FACTORY: UnmarshalerFactory = TypeMapFactory({bytes: BASE64_MARSHALER_UNMARSHALER})
+BASE64_MARSHALER_FACTORY: MarshalerFactory = TypeMapFactory({
+    ty: Base64MarshalerUnmarshaler(ty) for ty in BASE64_TYPES
+})
+BASE64_UNMARSHALER_FACTORY: UnmarshalerFactory = TypeMapFactory({
+    ty: Base64MarshalerUnmarshaler(ty) for ty in BASE64_TYPES
+})
