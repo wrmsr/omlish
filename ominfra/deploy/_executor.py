06dd3344e2bcd1dba9aa1cb73c6d68cf10440f42
wrmsr <timwilloney@gmail.com>
2024-08-12 11:38:13 -0700

    --
    Former-commit-id: ed28407645af0220f8b17dee233648f2fe4e95f7

--- ominfra/deploy/_executor.py
+++ ominfra/deploy/_executor.py
@@ -52,6 +52,7 @@ import enum
 import functools
 import json
 import logging
+import os
 import os.path
 import pwd
 import shlex
@@ -93,6 +94,12 @@ class cached_nullary:  # noqa
 # ruff: noqa: UP006 UP007
 
 
+def check_isinstance(v: T, spec: ta.Union[ta.Type[T], tuple]) -> T:
+    if not isinstance(v, spec):
+        raise TypeError(v)
+    return v
+
+
 def check_not_none(v: ta.Optional[T]) -> T:
     if v is None:
         raise ValueError
@@ -102,12 +109,6 @@ def check_not_none(v: ta.Optional[T]) -> T:
 def check_not(v: ta.Any) -> None:
     if v:
         raise ValueError(v)
-    return v
-
-
-def check_isinstance(v: T, spec: ta.Union[ta.Type[T], tuple]) -> T:
-    if not isinstance(v, spec):
-        raise TypeError(v)
     return v
 
 
@@ -474,20 +475,76 @@ def unmarshal_obj(o: ta.Any, ty: ta.Any) -> ta.Any:
 
 ########################################
 # ../../../../omdev/amalg/std/subprocesses.py
+# ruff: noqa: UP006 UP007
 
 
-def _mask_env_kwarg(kwargs):
-    return {**kwargs, **({'env': '...'} if 'env' in kwargs else {})}
+##
 
 
-def subprocess_check_call(*args, stdout=sys.stderr, **kwargs):
-    log.debug((args, _mask_env_kwarg(kwargs)))
-    return subprocess.check_call(*args, stdout=stdout, **kwargs)  # type: ignore
+def _prepare_subprocess_invocation(
+        *args: ta.Any,
+        env: ta.Optional[ta.Mapping[str, ta.Any]] = None,
+        extra_env: ta.Optional[ta.Mapping[str, ta.Any]] = None,
+        **kwargs: ta.Any,
+) -> ta.Tuple[ta.Tuple[ta.Any, ...], ta.Dict[str, ta.Any]]:
+    log.debug(args)
+    if extra_env:
+        log.debug(extra_env)
+
+    if extra_env:
+        env = {**(env if env is not None else os.environ), **extra_env}
+
+    return args, dict(
+        env=env,
+        **kwargs,
+    )
 
 
-def subprocess_check_output(*args, **kwargs):
-    log.debug((args, _mask_env_kwarg(kwargs)))
-    return subprocess.check_output(*args, **kwargs)
+def subprocess_check_call(*args: ta.Any, stdout=sys.stderr, **kwargs) -> None:
+    args, kwargs = _prepare_subprocess_invocation(*args, stdout=stdout, **kwargs)
+    return subprocess.check_call(args, **kwargs)  # type: ignore
+
+
+def subprocess_check_output(*args: ta.Any, **kwargs) -> bytes:
+    args, kwargs = _prepare_subprocess_invocation(*args, **kwargs)
+    return subprocess.check_output(args, **kwargs)
+
+
+##
+
+
+DEFAULT_SUBPROCESS_TRY_EXCEPTIONS: ta.Tuple[ta.Type[Exception], ...] = (
+    FileNotFoundError,
+    subprocess.CalledProcessError,
+)
+
+
+def subprocess_try_call(
+        *args: ta.Any,
+        try_exceptions: ta.Tuple[ta.Type[Exception], ...] = DEFAULT_SUBPROCESS_TRY_EXCEPTIONS,
+        **kwargs,
+) -> bool:
+    try:
+        subprocess_check_call(*args, **kwargs)
+    except try_exceptions as e:  # noqa
+        if log.isEnabledFor(logging.DEBUG):
+            log.exception('command failed')
+        return False
+    else:
+        return True
+
+
+def subprocess_try_output(
+        *args: ta.Any,
+        try_exceptions: ta.Tuple[ta.Type[Exception], ...] = DEFAULT_SUBPROCESS_TRY_EXCEPTIONS,
+        **kwargs,
+) -> ta.Optional[bytes]:
+    try:
+        return subprocess_check_output(*args, **kwargs)
+    except try_exceptions as e:  # noqa
+        if log.isEnabledFor(logging.DEBUG):
+            log.exception('command failed')
+        return None
 
 
 ########################################
@@ -522,7 +579,7 @@ def cmd(
         try_ = True
 
     try:
-        buf = subprocess_check_output(cmd, env=env, **kwargs)
+        buf = subprocess_check_output(*cmd, env=env, **kwargs)
     except es:
         if try_:
             log.exception('cmd failed: %r', cmd)
