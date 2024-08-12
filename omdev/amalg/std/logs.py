efbafb64c9e58cd9b653d07540706dbc6ec2296a
wrmsr <timwilloney@gmail.com>
2024-08-12 15:01:24 -0700

    --
    Former-commit-id: 040beb423bba4cc72dcafe17b2d597c9dcb05847

--- omdev/amalg/std/logs.py
+++ omdev/amalg/std/logs.py
@@ -3,11 +3,12 @@ TODO:
  - debug
 """
 import logging
+import typing as ta
 
 
 log = logging.getLogger(__name__)
 
 
-def setup_standard_logging() -> None:
+def setup_standard_logging(level: ta.Union[int, str] = logging.INFO) -> None:
     logging.root.addHandler(logging.StreamHandler())
-    logging.root.setLevel('INFO')
+    logging.root.setLevel(level)
