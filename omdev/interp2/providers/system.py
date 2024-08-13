112a3933a5620af9e57f5caa74ef6b936658c66a
wrmsr <timwilloney@gmail.com>
2024-08-12 17:52:18 -0700

    --
    Former-commit-id: 00b4a029d470390068f19b0b5e02067f64cb84c8

--- omdev/interp2/providers/system.py
+++ omdev/interp2/providers/system.py
@@ -2,9 +2,9 @@
 TODO:
  - python, python3, python3.12, ...
 """
-# ruff: noqa: UP007
+# ruff: noqa: UP006 UP007
 import dataclasses as dc
-import shutil
+import os
 import typing as ta
 
 from ...amalg.std.cached import cached_nullary
@@ -20,14 +20,52 @@ from .base import query_interp_exe_version
 @dc.dataclass(frozen=True)
 class SystemInterpProvider(InterpProvider):
     cmd: str = 'python3'
+    path: ta.Optional[str] = None
 
     @property
     def name(self) -> str:
         return 'system'
 
+    @staticmethod
+    def _which(
+            cmd: str,
+            *,
+            mode: int = os.F_OK | os.X_OK,
+            path: ta.Optional[str] = None,
+    ) -> ta.Optional[str]:
+        if path is None:
+            path = os.environ.get('PATH', None)
+            if path is None:
+                try:
+                    path = os.confstr('CS_PATH')
+                except (AttributeError, ValueError):
+                    path = os.defpath
+
+        if not path:
+            return None
+
+        path = os.fsdecode(path)
+        pathlst = path.split(os.pathsep)
+
+        def _access_check(fn: str, mode: int) -> bool:
+            return (os.path.exists(fn) and os.access(fn, mode) and not os.path.isdir(fn))
+
+        files = [cmd]
+        seen = set()
+        for d in pathlst:
+            normdir = os.path.normcase(d)
+            if normdir not in seen:
+                seen.add(normdir)
+                for thefile in files:
+                    name = os.path.join(d, thefile)
+                    if _access_check(name, mode):
+                        return name
+
+        return None
+
     @cached_nullary
     def exe(self) -> ta.Optional[str]:
-        return shutil.which(self.cmd)
+        return self._which(self.cmd, path=self.path)
 
     @cached_nullary
     def version(self) -> ta.Optional[InterpVersion]:
--- omdev/interp2/providers/tests/test_system.py
+++ omdev/interp2/providers/tests/test_system.py
@@ -4,6 +4,6 @@ from .. import system as sp
 
 
 class TestBaseProviders(unittest.TestCase):
-    def test_running(self):
+    def test_system(self):
         v = sp.SystemInterpProvider().installed_versions()
         print(sp.SystemInterpProvider().get_version(v[0]))
