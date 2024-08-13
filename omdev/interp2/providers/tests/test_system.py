057dd2058c0c2c2b2eb8014f4f79c17c6a47a7b1
wrmsr <timwilloney@gmail.com>
2024-08-12 18:39:51 -0700

    --
    Former-commit-id: a0d6a94b9c18a5df1d3bc89a9085d7dbc91cdfe0

--- omdev/interp2/providers/tests/test_system.py
+++ omdev/interp2/providers/tests/test_system.py
@@ -5,5 +5,7 @@ from .. import system as sp
 
 class TestBaseProviders(unittest.TestCase):
     def test_system(self):
+        from omlish import logs
+        logs.configure_standard_logging('DEBUG')
         v = sp.SystemInterpProvider().installed_versions()
         print(sp.SystemInterpProvider().get_version(v[0]))
