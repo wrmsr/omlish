bc7c90014ead0d9f0a3f75f9952b3261d7e86b57
wrmsr <timwilloney@gmail.com>
2024-08-13 15:44:14 -0700

    --
    Former-commit-id: f3797b4be51261a4fb19e3d91d4669f633e69ccf

--- x/mkrelimp.py
+++ x/mkrelimp.py
@@ -13,7 +13,6 @@ T = ta.TypeVar('T')
 def indexfn(
         fn: ta.Callable[[T], bool],
         it: ta.Iterable[T],
-        *,
         start: int = 0,
         stop: int | None = None,
         step: int = 1,
@@ -22,6 +21,13 @@ def indexfn(
         if fn(e):
             return start + i * step
     return -1
+
+
+def interleave(sep: T, it: ta.Iterable[T]) -> ta.Iterable[T]:
+    for i, e in enumerate(it):
+        if i > 0:
+            yield sep
+        yield e
 
 
 def _main() -> None:
@@ -41,12 +47,25 @@ def _main() -> None:
 
             ts = trt.src_to_tokens(src)
             ls = tks.split_lines(ts)
-            for i, l in enumerate(ls):
+            for l in ls:
                 ft: trt.Token = l[0]
                 if ft.name != 'NAME' or ft.src not in ('import', 'from'):
                     continue
-                if l[4].name == 'NAME' and l[4].src == mod_name:
-                    raise NotImplementedError
+                if len(l) >= 3 and l[2].name == 'NAME' and l[2].src == mod_name:
+                    print(tks.join_toks(l))
+                    e = indexfn(tks.is_ws, l, 3)
+                    ip = list(l[2:e])
+                    del l[2:e]
+                    ps = [t.src for t in ip if t.name == 'NAME']
+                    ip.extend([
+                        *interleave(
+                            trt.Token(name='OP', src='.'),
+                            [trt.Token(name='NAME', src=p) for p in ps],
+                        ),
+                        trt.Token(name='UNIMPORTANT_WS', src=' '),
+                    ])
+                    l[2:3] = ip
+                    print(tks.join_toks(l))
 
 
 if __name__ == '__main__':
