3cf7925ebb9d4773ed33b222657527366a8957f4
wrmsr <timwilloney@gmail.com>
2024-08-13 15:35:11 -0700

    --
    Former-commit-id: eb7602c67d1e75ec101ca1de0aa68a75c1c8da6d

--- x/mkrelimp.py
+++ x/mkrelimp.py
@@ -1,8 +1,27 @@
+import itertools
 import os.path
+import typing as ta
 
 from omdev import tokens as tks
 from omlish import check
-import tokenize_rt as tr
+import tokenize_rt as trt
+
+
+T = ta.TypeVar('T')
+
+
+def indexfn(
+        fn: ta.Callable[[T], bool],
+        it: ta.Iterable[T],
+        *,
+        start: int = 0,
+        stop: int | None = None,
+        step: int = 1,
+) -> int:
+    for i, e in enumerate(itertools.islice(it, start, stop, step)):
+        if fn(e):
+            return start + i * step
+    return -1
 
 
 def _main() -> None:
@@ -14,8 +33,20 @@ def _main() -> None:
         for fn in fns:
             if not fn.endswith('.py'):
                 continue
+
             print((dp, fn))
-            for
+
+            with open(os.path.join(dp, fn)) as f:
+                src = f.read()
+
+            ts = trt.src_to_tokens(src)
+            ls = tks.split_lines(ts)
+            for i, l in enumerate(ls):
+                ft: trt.Token = l[0]
+                if ft.name != 'NAME' or ft.src not in ('import', 'from'):
+                    continue
+                if l[4].name == 'NAME' and l[4].src == mod_name:
+                    raise NotImplementedError
 
 
 if __name__ == '__main__':
