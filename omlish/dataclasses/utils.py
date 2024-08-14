25b91a31c22c0b3c670dbaa5d42c61f08e3f7f4b
wrmsr <timwilloney@gmail.com>
2024-08-14 15:31:46 -0700

    --
    Former-commit-id: 6fdb88920c45590b56dc0d0a01f9e36aa75363ac

--- omlish/dataclasses/utils.py
+++ omlish/dataclasses/utils.py
@@ -48,6 +48,6 @@ def deep_replace(o: T, *args: str | ta.Callable[[ta.Any], ta.Mapping[str, ta.Any
     if not args:
         return o
     elif len(args) == 1:
-        return dc.replace(o, **args[0](o))
+        return dc.replace(o, **args[0](o))  # type: ignore
     else:
-        return dc.replace(o, **{args[0]: deep_replace(getattr(o, args[0]), *args[1:])})
+        return dc.replace(o, **{args[0]: deep_replace(getattr(o, args[0]), *args[1:])})  # type: ignore
