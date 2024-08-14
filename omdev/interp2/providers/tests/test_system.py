a4e3d2bc624d35c0bd06f2cbac1068e99ff5584d
wrmsr <timwilloney@gmail.com>
2024-08-14 12:42:37 -0700

    --
    Former-commit-id: 06274e1195c06a00b0b5045260d20337cc12c309

--- omdev/interp2/providers/tests/test_system.py
+++ omdev/interp2/providers/tests/test_system.py
@@ -1,8 +1,8 @@
 import unittest
 
-from .. import system as sp
 from ....amalg.std.tests.helpers import SubprocessPatchingTest
 from ....amalg.std.versions.specifiers import SpecifierSet
+from .. import system as sp
 
 
 class TestBaseProviders(SubprocessPatchingTest, unittest.TestCase):
