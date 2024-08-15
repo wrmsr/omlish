b5d0b87f026f741162a6af91f358baf4f42aeb3e
wrmsr <timwilloney@gmail.com>
2024-08-15 10:55:38 -0700

    --
    Former-commit-id: 24e587a8d35420167d41e8982d1c07e1cf01c56c

--- x/asyncs/bluelet/demo/delegation.py
+++ x/asyncs/bluelet/demo/delegation.py
@@ -1,32 +1,23 @@
-"""A demonstration of Bluelet's approach to invoking (delegating to)
-sub-coroutines and spawning child coroutines.
-"""
+"""A demonstration of Bluelet's approach to invoking (delegating to) sub-coroutines and spawning child coroutines."""
 from .. import bluelet
 
 
 def child():
-    print
-    'Child started.'
+    print('Child started.')
     yield bluelet.null()
-    print
-    'Child resumed.'
+    print('Child resumed.')
     yield bluelet.null()
-    print
-    'Child ending.'
+    print('Child ending.')
     yield bluelet.end(42)
 
 
 def parent():
-    print
-    'Parent started.'
+    print('Parent started.')
     yield bluelet.null()
-    print
-    'Parent resumed.'
+    print('Parent resumed.')
     result = yield child()
-    print
-    'Child returned:', repr(result)
-    print
-    'Parent ending.'
+    print('Child returned:', repr(result))
+    print('Parent ending.')
 
 
 def exc_child():
@@ -37,9 +28,8 @@ def exc_child():
 def exc_parent():
     try:
         yield exc_child()
-    except Exception, exc:
-        print
-        'Parent caught:', repr(exc)
+    except Exception as exc:
+        print('Parent caught:', repr(exc))
 
 
 def exc_grandparent():
