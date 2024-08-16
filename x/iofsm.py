326b4cc162e350aede72630ea5a0b0cd36787935
wrmsr <timwilloney@gmail.com>
2024-08-16 11:05:43 -0700

    --
    Former-commit-id: d81105d75f2ce224f1eb908144c0305f56305db4

--- x/iofsm.py
+++ x/iofsm.py
@@ -6,56 +6,76 @@ import typing as ta
 ##
 
 
-class InputEvent(abc.ABC):
+class Event(abc.ABC):
     pass
-
-
-@dc.dataclass(frozen=True)
-class RecvdLine(InputEvent):
-    line: str
-
-
-##
-
-
-class OutputEvent(abc.ABC):
-    pass
-
-
-@dc.dataclass(frozen=True)
-class SendLine(OutputEvent):
-    line: str
-
-
-##
 
 
 class IllegalStateException(Exception):
     pass
 
 
+##
+
+
+@dc.dataclass(frozen=True)
+class RecvdData(Event):
+    data: bytes
+
+
+class LineReader:
+    def __init__(self) -> None:
+        super().__init__()
+        self._buf = bytearray()
+
+    def accept(self, e: Event) -> ta.Iterable[Event]:
+        if isinstance(e, RecvdData):
+            self._buf += e.data
+            out = []
+            while (i := self._buf.find(b'\n')) >= 0:
+                out.append(RecvdLine(self._buf[:i+1].decode()))
+                self._buf = self._buf[i+1:]
+            return out
+        raise IllegalStateException
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class RecvdLine(Event):
+    line: str
+
+
+@dc.dataclass(frozen=True)
+class SendLine(Event):
+    line: str
+
+
+##
+
+
 class AckedEchoProtocol0:
-    ACK0 = 'hi0'
-    ACK1 = 'hi1'
+    ACK0 = 'hi0\n'
+    ACK1 = 'hi1\n'
 
     def __init__(self) -> None:
         super().__init__()
         self._state = 0
 
-    def accept(self, ie: InputEvent) -> ta.Iterable[OutputEvent]:
+    def accept(self, e: Event) -> ta.Iterable[Event]:
         if self._state == 0:
-            if isinstance(ie, RecvdLine):
-                if ie.line == self.ACK0:
+            if isinstance(e, RecvdLine):
+                if e.line == self.ACK0:
                     self._state = 1
                     return []
         if self._state == 1:
-            if isinstance(ie, RecvdLine):
-                if ie.line == self.ACK1:
+            if isinstance(e, RecvdLine):
+                if e.line == self.ACK1:
                     self._state = 2
                     return []
         if self._state == 2:
-            if isinstance(ie, RecvdLine):
-                return [SendLine(ie.line)]
+            if isinstance(e, RecvdLine):
+                return [SendLine(e.line)]
         raise IllegalStateException
 
 
@@ -63,19 +83,22 @@ class AckedEchoProtocol0:
 
 
 def _main() -> None:
-    def handle_output(oe: OutputEvent) -> None:
-        if isinstance(oe, SendLine):
-            print(oe.line)
+    for oe in LineReader().accept(RecvdData(b'hi\nthere\n')):
+        print(repr(oe))
+
+    def handle_output(e: Event) -> None:
+        if isinstance(e, SendLine):
+            print(repr(e))
             return
         raise IllegalStateException
 
     p = AckedEchoProtocol0()
 
     for ie in [
-        RecvdLine('hi0'),
-        RecvdLine('hi1'),
-        RecvdLine('foo'),
-        RecvdLine('bar'),
+        RecvdLine('hi0\n'),
+        RecvdLine('hi1\n'),
+        RecvdLine('foo\n'),
+        RecvdLine('bar\n'),
     ]:
         for oe in p.accept(ie):
             handle_output(oe)
