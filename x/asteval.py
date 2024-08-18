b74f8fd9115fd489f52ed00bdf903a05ccd681be
wrmsr <timwilloney@gmail.com>
2024-08-18 13:15:18 -0700

    --
    Former-commit-id: 47e57f3a65a504380129ea1a4230ed78217e8006

--- x/asteval.py
+++ x/asteval.py
@@ -24,20 +24,48 @@ class EvalAstVisitor(AstVisitor[None, ta.Any]):
     def _visit_Expr(self, n: ast.Expr, c: None) -> ta.Any:  # noqa
         return self.visit(n.value, c)
 
-    _OP_CLS_TO_OPERATOR: ta.Mapping = {
+    _BIN_OP_CLS_TO_OPERATOR: ta.Mapping[type[ast.operator], ta.Callable] = {
         ast.Add: operator.add,
         ast.Sub: operator.sub,
         ast.Mult: operator.mul,
         ast.Div: operator.truediv,
+        ast.Mod: operator.mod,
         ast.FloorDiv: operator.floordiv,
+        ast.Pow: operator.pow,
+        ast.MatMult: operator.matmul,
+
+        ast.BitAnd: operator.and_,
+        ast.BitOr: operator.or_,
+        ast.BitXor: operator.xor,
+        ast.LShift: operator.lshift,
+        ast.RShift: operator.rshift,
     }
 
     @AstVisitor._visit.register  # noqa
     def _visit_BinOp(self, n: ast.BinOp, c: None) -> ta.Any:  # noqa
         left = self.visit(n.left, c)
         right = self.visit(n.right, c)
-        op = self._OP_CLS_TO_OPERATOR[type(n.op)]
+        op = self._BIN_OP_CLS_TO_OPERATOR[type(n.op)]
         return op(left, right)
+
+    _CMP_OP_CLS_TO_OPERATOR: ta.Mapping[type[ast.cmpop], ta.Callable] = {
+        ast.Eq: operator.eq,
+        ast.NotEq: operator.ne,
+        ast.Lt: operator.lt,
+        ast.LtE: operator.le,
+        ast.Gt: operator.gt,
+        ast.GtE: operator.ge,
+
+        ast.Is: operator.is_,
+        ast.IsNot: operator.is_not,
+
+        ast.In: operator.contains,
+        ast.NotIn: lambda *a: not operator.contains(*a),
+    }
+
+    @AstVisitor._visit.register  # noqa
+    def _visit_Compare(self, n: ast.Compare, c: None) -> ta.Any:  # noqa
+        raise NotImplementedError
 
     @AstVisitor._visit.register  # noqa
     def _visit_Constant(self, n: ast.Constant, c: None) -> ta.Any:  # noqa
@@ -47,6 +75,7 @@ class EvalAstVisitor(AstVisitor[None, ta.Any]):
 def _main():
     for s, e in [
         ('2 + 3 + (4 * 2)', 13),
+        ('2 > 1', True),
     ]:
         a = ast.parse(s)
         n = check.single(check.isinstance(a, ast.Module).body)
