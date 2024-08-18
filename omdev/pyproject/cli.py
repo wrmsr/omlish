bebc68fde99e90827eca26a16ba07346a556ea8e
wrmsr <timwilloney@gmail.com>
2024-08-18 16:22:51 -0700

    --
    Former-commit-id: 1514533e26f3d9e93e1cbfd31d0a684bde4cc1a5

--- omdev/pyproject/cli.py
+++ omdev/pyproject/cli.py
@@ -52,24 +52,31 @@ from .configs import VenvConfig
 class VersionsFile:
     name: ta.Optional[str] = '.versions'
 
-    @cached_nullary
-    def contents(self) -> ta.Mapping[str, str]:
-        if not self.name or not os.path.exists(self.name):
-            return {}
-        with open(self.name) as f:
-            lines = f.readlines()
+    @staticmethod
+    def parse(s: str) -> ta.Mapping[str, str]:
         return {
             k: v
-            for l in lines
+            for l in s.splitlines()
             if (sl := l.split('#')[0].strip())
             for k, _, v in (sl.partition('='),)
         }
 
     @cached_nullary
-    def pythons(self) -> ta.Mapping[str, str]:
-        raw_vers = self.contents()
+    def contents(self) -> ta.Mapping[str, str]:
+        if not self.name or not os.path.exists(self.name):
+            return {}
+        with open(self.name) as f:
+            s = f.read()
+        return self.parse(s)
+
+    @staticmethod
+    def get_pythons(d: ta.Mapping[str, str]) -> ta.Mapping[str, str]:
         pfx = 'PYTHON_'
-        return {k[len(pfx):].lower(): v for k, v in raw_vers.items() if k.startswith(pfx)}
+        return {k[len(pfx):].lower(): v for k, v in d.items() if k.startswith(pfx)}
+
+    @cached_nullary
+    def pythons(self) -> ta.Mapping[str, str]:
+        return self.get_pythons(self.contents())
 
 
 ##
@@ -144,7 +151,7 @@ class Venv:
                 ve,
                 '-m', 'pip',
                 'install', '-v',
-                *itertools.chain.from_iterable(['-r', r] for r in ([sr] if isinstance(sr, str) else sr)),
+                *itertools.chain.from_iterable(['-r', r] for r in sr),
             )
 
         return True
