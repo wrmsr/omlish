383dd2d2be781e5828be00949f8b4174723c5f78
wrmsr <timwilloney@gmail.com>
2024-08-19 15:17:10 -0700

    --
    Former-commit-id: 0fc9e7d851c33d61760389d30a35186ee03fb119

--- omdev/scripts/findmagic.py
+++ omdev/scripts/findmagic.py
@@ -1,24 +1,42 @@
 import argparse
 import os.path
+import re
 
 
 def _main(argv=None) -> None:
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--ext', '-x', dest='exts', action='append')
-    arg_parser.add_argument('magic')
+    arg_parser.add_argument('--magic', '-m', dest='magics', action='append')
     arg_parser.add_argument('roots', nargs='*')
     args = arg_parser.parse_args(argv)
-    print(args)
 
+    if not args.magics:
+        raise Exception('Must specify magics')
     if not args.exts:
         raise Exception('Must specify extensions')
 
+    pats = [
+        re.compile('^' + re.escape(m) + r'($|(\s.*))')
+        for m in args.magics
+    ]
+
     for root in args.roots:
-        for dp, dns, fns in os.walk(root):
+        for dp, dns, fns in os.walk(root):  # noqa
             for fn in fns:
                 if not any(fn.endswith(f'.{x}') for x in args.exts):
                     continue
-                print(os.path.join(dp, fn))
+                fp = os.path.join(dp, fn)
+                with open(fp) as f:
+                    src = f.read()
+                if not any(
+                    any(pat.fullmatch(l) for pat in pats)
+                    for l in src.splitlines()
+                ):
+                    continue
+                if fn == '__init__.py':
+                    print(dp)
+                else:
+                    print(fp)
 
 
 if __name__ == '__main__':
