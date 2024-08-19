e8e7491b81b683c917fa95dde04a3d325a582d58
wrmsr <timwilloney@gmail.com>
2024-08-18 17:09:26 -0700

    --
    Former-commit-id: c510e51de7092b0cc9ec429076f443743b666a95

--- omdev/scripts/pyproject.py
+++ omdev/scripts/pyproject.py
@@ -3141,24 +3141,31 @@ DEFAULT_INTERP_RESOLVER = InterpResolver([
 class VersionsFile:
     name: ta.Optional[str] = '.versions'
 
-    @cached_nullary
-    def contents(self) -> ta.Mapping[str, str]:
-        if not self.name or not os.path.exists(self.name):
-            return {}
-        with open(self.name) as f:
-            lines = f.readlines()
+    @staticmethod
+    def parse(s: str) -> ta.Mapping[str, str]:
         return {
             k: v
-            for l in lines
+            for l in s.splitlines()
             if (sl := l.split('#')[0].strip())
             for k, _, v in (sl.partition('='),)
         }
 
     @cached_nullary
-    def pythons(self) -> ta.Mapping[str, str]:
-        raw_vers = self.contents()
+    def contents(self) -> ta.Mapping[str, str]:
+        if not self.name or not os.path.exists(self.name):
+            return {}
+        with open(self.name) as f:
+            s = f.read()
+        return self.parse(s)
+
+    @staticmethod
+    def get_pythons(d: ta.Mapping[str, str]) -> ta.Mapping[str, str]:
         pfx = 'PYTHON_'
-        return {k[len(pfx):].lower(): v for k, v in raw_vers.items() if k.startswith(pfx)}
+        return {k[len(pfx):].lower(): v for k, v in d.items() if k.startswith(pfx)}
+
+    @cached_nullary
+    def pythons(self) -> ta.Mapping[str, str]:
+        return self.get_pythons(self.contents())
 
 
 ##
