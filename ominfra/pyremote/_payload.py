977b9e1bad2aa642965b9ef3b79d55ed74c37f9d
wrmsr <timwilloney@gmail.com>
2024-08-19 12:41:01 -0700

    --
    Former-commit-id: cc94c97d2dd1cbaa321b835cca76ab5d7e1da2c3

---
+++ ominfra/pyremote/_payload.py
@@ -0,0 +1,748 @@
+#!/usr/bin/env python3
+# noinspection DuplicatedCode
+# @omdev-amalg-output payload.py
+# ruff: noqa: UP007
+import abc
+import base64
+import collections.abc
+import dataclasses as dc
+import datetime
+import enum
+import functools
+import inspect
+import io
+import json
+import logging
+import os
+import shlex
+import subprocess
+import sys
+import textwrap
+import typing as ta
+import uuid
+import weakref  # noqa
+import zlib
+
+
+T = ta.TypeVar('T')
+
+
+########################################
+# ../bootstrap.py
+
+
+##
+
+
+_BOOTSTRAP_COMM_FD = 100
+_BOOTSTRAP_SRC_FD = 101
+
+_BOOTSTRAP_CHILD_PID_VAR = '_PYR_CPID'
+_BOOTSTRAP_ARGV0_VAR = '_PYR_ARGV0'
+
+BOOTSTRAP_ACK0 = b'OPYR000\n'
+BOOTSTRAP_ACK1 = b'OPYR001\n'
+
+_BOOTSTRAP_PROC_TITLE_FMT = '(pyremote:%s)'
+
+_BOOTSTRAP_IMPORTS = [
+    'base64',
+    'os',
+    'sys',
+    'zlib',
+]
+
+
+def _bootstrap_main(context_name: str, main_z_len: int) -> None:
+    # Two copies of main src to be sent to parent
+    r0, w0 = os.pipe()
+    r1, w1 = os.pipe()
+
+    if (cp := os.fork()):
+        # Parent process
+
+        # Dup original stdin to comm_fd for use as comm channel
+        os.dup2(0, _BOOTSTRAP_COMM_FD)
+
+        # Overwrite stdin (fed to python repl) with first copy of src
+        os.dup2(r0, 0)
+
+        # Dup second copy of src to src_fd to recover after launch
+        os.dup2(r1, _BOOTSTRAP_SRC_FD)
+
+        # Close remaining fd's
+        for f in [r0, w0, r1, w1]:
+            os.close(f)
+
+        # Save child pid to close after relaunch
+        os.environ[_BOOTSTRAP_CHILD_PID_VAR] = str(cp)
+
+        # Save original argv0
+        os.environ[_BOOTSTRAP_ARGV0_VAR] = sys.executable
+
+        # Start repl reading stdin from r0
+        os.execl(sys.executable, sys.executable + (_BOOTSTRAP_PROC_TITLE_FMT % (context_name,)))
+
+    else:
+        # Child process
+
+        # Write first ack
+        os.write(1, BOOTSTRAP_ACK0)
+
+        # Read main src from stdin
+        main_src = zlib.decompress(os.fdopen(0, 'rb').read(main_z_len))
+
+        # Write both copies of main src
+        for w in [w0, w1]:
+            fp = os.fdopen(w, 'wb', 0)
+            fp.write(main_src)
+            fp.close()
+
+        # Write second ack
+        os.write(1, BOOTSTRAP_ACK1)
+
+        sys.exit(0)
+
+
+#
+
+
+def bootstrap_payload(context_name: str, main_z_len: int) -> str:
+    bs_src = textwrap.dedent(inspect.getsource(_bootstrap_main))
+
+    for gl in [
+        '_BOOTSTRAP_COMM_FD',
+        '_BOOTSTRAP_SRC_FD',
+
+        '_BOOTSTRAP_CHILD_PID_VAR',
+        '_BOOTSTRAP_ARGV0_VAR',
+
+        'BOOTSTRAP_ACK0',
+        'BOOTSTRAP_ACK1',
+
+        '_BOOTSTRAP_PROC_TITLE_FMT',
+    ]:
+        bs_src = bs_src.replace(gl, repr(globals()[gl]))
+
+    bs_src = '\n'.join(
+        cl
+        for l in bs_src.splitlines()
+        if (cl := (l.split('#')[0]).rstrip())
+        if cl.strip()
+    )
+
+    bs_z = zlib.compress(bs_src.encode('utf-8'))
+    bs_z64 = base64.encodebytes(bs_z).replace(b'\n', b'')
+
+    stmts = [
+        f'import {", ".join(_BOOTSTRAP_IMPORTS)}',
+        f'exec(zlib.decompress(base64.decodebytes({bs_z64!r})))',
+        f'_bootstrap_main({context_name!r}, {main_z_len})',
+    ]
+
+    cmd = '; '.join(stmts)
+    return cmd
+
+
+#
+
+
+class PostBoostrap(ta.NamedTuple):
+    input: ta.BinaryIO
+    main_src: str
+
+
+def post_boostrap() -> PostBoostrap:
+    # Restore original argv0
+    sys.executable = os.environ.pop(_BOOTSTRAP_ARGV0_VAR)
+
+    # Reap boostrap child
+    os.waitpid(int(os.environ.pop(_BOOTSTRAP_CHILD_PID_VAR)), 0)
+
+    # Read second copy of main src
+    r1 = os.fdopen(_BOOTSTRAP_SRC_FD, 'rb', 0)
+    main_src = r1.read().decode('utf-8')
+    r1.close()
+
+    return PostBoostrap(
+        input=os.fdopen(_BOOTSTRAP_COMM_FD, 'rb', 0),
+        main_src=main_src,
+    )
+
+
+########################################
+# ../../../omlish/lite/cached.py
+
+
+class cached_nullary:  # noqa
+    def __init__(self, fn):
+        super().__init__()
+        self._fn = fn
+        self._value = self._missing = object()
+        functools.update_wrapper(self, fn)
+
+    def __call__(self, *args, **kwargs):  # noqa
+        if self._value is self._missing:
+            self._value = self._fn()
+        return self._value
+
+    def __get__(self, instance, owner):  # noqa
+        bound = instance.__dict__[self._fn.__name__] = self.__class__(self._fn.__get__(instance, owner))
+        return bound
+
+
+########################################
+# ../../../omlish/lite/check.py
+# ruff: noqa: UP006 UP007
+
+
+def check_isinstance(v: T, spec: ta.Union[ta.Type[T], tuple]) -> T:
+    if not isinstance(v, spec):
+        raise TypeError(v)
+    return v
+
+
+def check_not_isinstance(v: T, spec: ta.Union[type, tuple]) -> T:
+    if isinstance(v, spec):
+        raise TypeError(v)
+    return v
+
+
+def check_not_none(v: ta.Optional[T]) -> T:
+    if v is None:
+        raise ValueError
+    return v
+
+
+def check_not(v: ta.Any) -> None:
+    if v:
+        raise ValueError(v)
+    return v
+
+
+########################################
+# ../../../omlish/lite/json.py
+
+
+##
+
+
+JSON_PRETTY_INDENT = 2
+
+JSON_PRETTY_KWARGS: ta.Mapping[str, ta.Any] = dict(
+    indent=JSON_PRETTY_INDENT,
+)
+
+json_dump_pretty: ta.Callable[..., bytes] = functools.partial(json.dump, **JSON_PRETTY_KWARGS)  # type: ignore
+json_dumps_pretty: ta.Callable[..., str] = functools.partial(json.dumps, **JSON_PRETTY_KWARGS)
+
+
+##
+
+
+JSON_COMPACT_SEPARATORS = (',', ':')
+
+JSON_COMPACT_KWARGS: ta.Mapping[str, ta.Any] = dict(
+    indent=None,
+    separators=JSON_COMPACT_SEPARATORS,
+)
+
+json_dump_compact: ta.Callable[..., bytes] = functools.partial(json.dump, **JSON_COMPACT_KWARGS)  # type: ignore
+json_dumps_compact: ta.Callable[..., str] = functools.partial(json.dumps, **JSON_COMPACT_KWARGS)
+
+
+########################################
+# ../../../omlish/lite/logs.py
+"""
+TODO:
+ - debug
+"""
+# ruff: noqa: UP007
+
+
+log = logging.getLogger(__name__)
+
+
+def configure_standard_logging(level: ta.Union[int, str] = logging.INFO) -> None:
+    logging.root.addHandler(logging.StreamHandler())
+    logging.root.setLevel(level)
+
+
+########################################
+# ../../../omlish/lite/reflect.py
+
+
+_GENERIC_ALIAS_TYPES = (
+    ta._GenericAlias,  # type: ignore  # noqa
+    *([ta._SpecialGenericAlias] if hasattr(ta, '_SpecialGenericAlias') else []),  # noqa
+)
+
+
+def is_generic_alias(obj, *, origin: ta.Any = None) -> bool:
+    return (
+        isinstance(obj, _GENERIC_ALIAS_TYPES) and
+        (origin is None or ta.get_origin(obj) is origin)
+    )
+
+
+is_union_alias = functools.partial(is_generic_alias, origin=ta.Union)
+is_callable_alias = functools.partial(is_generic_alias, origin=ta.Callable)
+
+
+def is_optional_alias(spec: ta.Any) -> bool:
+    return (
+        isinstance(spec, _GENERIC_ALIAS_TYPES) and  # noqa
+        ta.get_origin(spec) is ta.Union and
+        len(ta.get_args(spec)) == 2 and
+        any(a in (None, type(None)) for a in ta.get_args(spec))
+    )
+
+
+def get_optional_alias_arg(spec: ta.Any) -> ta.Any:
+    [it] = [it for it in ta.get_args(spec) if it not in (None, type(None))]
+    return it
+
+
+########################################
+# ../../../omlish/lite/marshal.py
+"""
+TODO:
+ - pickle stdlib objs? have to pin to 3.8 pickle protocol, will be cross-version
+"""
+# ruff: noqa: UP006 UP007
+
+
+##
+
+
+class ObjMarshaler(abc.ABC):
+    @abc.abstractmethod
+    def marshal(self, o: ta.Any) -> ta.Any:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        raise NotImplementedError
+
+
+class NopObjMarshaler(ObjMarshaler):
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return o
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return o
+
+
+@dc.dataclass()
+class ProxyObjMarshaler(ObjMarshaler):
+    m: ta.Optional[ObjMarshaler] = None
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return check_not_none(self.m).marshal(o)
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return check_not_none(self.m).unmarshal(o)
+
+
+@dc.dataclass(frozen=True)
+class CastObjMarshaler(ObjMarshaler):
+    ty: type
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return o
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return self.ty(o)
+
+
+class DynamicObjMarshaler(ObjMarshaler):
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return marshal_obj(o)
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return o
+
+
+@dc.dataclass(frozen=True)
+class Base64ObjMarshaler(ObjMarshaler):
+    ty: type
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return base64.b64encode(o).decode('ascii')
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return self.ty(base64.b64decode(o))
+
+
+@dc.dataclass(frozen=True)
+class EnumObjMarshaler(ObjMarshaler):
+    ty: type
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return o.name
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return self.ty.__members__[o]  # type: ignore
+
+
+@dc.dataclass(frozen=True)
+class OptionalObjMarshaler(ObjMarshaler):
+    item: ObjMarshaler
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        if o is None:
+            return None
+        return self.item.marshal(o)
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        if o is None:
+            return None
+        return self.item.unmarshal(o)
+
+
+@dc.dataclass(frozen=True)
+class SequenceObjMarshaler(ObjMarshaler):
+    ty: type
+    item: ObjMarshaler
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return [self.item.marshal(e) for e in o]
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return self.ty(self.item.unmarshal(e) for e in o)
+
+
+@dc.dataclass(frozen=True)
+class MappingObjMarshaler(ObjMarshaler):
+    ty: type
+    km: ObjMarshaler
+    vm: ObjMarshaler
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return {self.km.marshal(k): self.vm.marshal(v) for k, v in o.items()}
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return self.ty((self.km.unmarshal(k), self.vm.unmarshal(v)) for k, v in o.items())
+
+
+@dc.dataclass(frozen=True)
+class DataclassObjMarshaler(ObjMarshaler):
+    ty: type
+    fs: ta.Mapping[str, ObjMarshaler]
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return {k: m.marshal(getattr(o, k)) for k, m in self.fs.items()}
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return self.ty(**{k: self.fs[k].unmarshal(v) for k, v in o.items()})
+
+
+@dc.dataclass(frozen=True)
+class PolymorphicObjMarshaler(ObjMarshaler):
+    class Impl(ta.NamedTuple):
+        ty: type
+        tag: str
+        m: ObjMarshaler
+
+    impls_by_ty: ta.Mapping[type, Impl]
+    impls_by_tag: ta.Mapping[str, Impl]
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        impl = self.impls_by_ty[type(o)]
+        return {impl.tag: impl.m.marshal(o)}
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        [(t, v)] = o.items()
+        impl = self.impls_by_tag[t]
+        return impl.m.unmarshal(v)
+
+
+@dc.dataclass(frozen=True)
+class DatetimeObjMarshaler(ObjMarshaler):
+    ty: type
+
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return o.isoformat()
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return self.ty.fromisoformat(o)  # type: ignore
+
+
+class UuidObjMarshaler(ObjMarshaler):
+    def marshal(self, o: ta.Any) -> ta.Any:
+        return str(o)
+
+    def unmarshal(self, o: ta.Any) -> ta.Any:
+        return uuid.UUID(o)
+
+
+_OBJ_MARSHALERS: ta.Dict[ta.Any, ObjMarshaler] = {
+    **{t: NopObjMarshaler() for t in (type(None),)},
+    **{t: CastObjMarshaler(t) for t in (int, float, str)},
+    **{t: Base64ObjMarshaler(t) for t in (bytes, bytearray)},
+    **{t: SequenceObjMarshaler(t, DynamicObjMarshaler()) for t in (list, tuple, set, frozenset)},
+    **{t: MappingObjMarshaler(t, DynamicObjMarshaler(), DynamicObjMarshaler()) for t in (dict,)},
+
+    ta.Any: DynamicObjMarshaler(),
+
+    **{t: DatetimeObjMarshaler(t) for t in (datetime.date, datetime.time, datetime.datetime)},
+    uuid.UUID: UuidObjMarshaler(),
+}
+
+_OBJ_MARSHALER_GENERIC_SEQUENCE_TYPES: ta.Dict[ta.Any, type] = {
+    **{t: t for t in (list, tuple, set, frozenset)},
+    **{t: frozenset for t in (collections.abc.Set, collections.abc.MutableSet)},
+    **{t: tuple for t in (collections.abc.Sequence, collections.abc.MutableSequence)},
+}
+
+_OBJ_MARSHALER_GENERIC_MAPPING_TYPES: ta.Dict[ta.Any, type] = {
+    **{t: t for t in (dict,)},
+    **{t: dict for t in (collections.abc.Mapping, collections.abc.MutableMapping)},
+}
+
+
+def register_opj_marshaler(ty: ta.Any, m: ObjMarshaler) -> None:
+    if ty in _OBJ_MARSHALERS:
+        raise KeyError(ty)
+    _OBJ_MARSHALERS[ty] = m
+
+
+def _make_obj_marshaler(ty: ta.Any) -> ObjMarshaler:
+    if isinstance(ty, type) and abc.ABC in ty.__bases__:
+        impls = [
+            PolymorphicObjMarshaler.Impl(
+                ity,
+                ity.__name__,
+                get_obj_marshaler(ity),
+            )
+            for ity in ty.__subclasses__()
+            if abc.ABC not in ity.__bases__
+        ]
+        return PolymorphicObjMarshaler(
+            {i.ty: i for i in impls},
+            {i.tag: i for i in impls},
+        )
+
+    if isinstance(ty, type) and issubclass(ty, enum.Enum):
+        return EnumObjMarshaler(ty)
+
+    if dc.is_dataclass(ty):
+        return DataclassObjMarshaler(
+            ty,
+            {f.name: get_obj_marshaler(f.type) for f in dc.fields(ty)},
+        )
+
+    if is_generic_alias(ty):
+        try:
+            st = _OBJ_MARSHALER_GENERIC_SEQUENCE_TYPES[ta.get_origin(ty)]
+        except KeyError:
+            pass
+        else:
+            [e] = ta.get_args(ty)
+            return SequenceObjMarshaler(st, get_obj_marshaler(e))
+
+        try:
+            mt = _OBJ_MARSHALER_GENERIC_MAPPING_TYPES[ta.get_origin(ty)]
+        except KeyError:
+            pass
+        else:
+            k, v = ta.get_args(ty)
+            return MappingObjMarshaler(mt, get_obj_marshaler(k), get_obj_marshaler(v))
+
+        if is_union_alias(ty):
+            return OptionalObjMarshaler(get_obj_marshaler(get_optional_alias_arg(ty)))
+
+    raise TypeError(ty)
+
+
+def get_obj_marshaler(ty: ta.Any) -> ObjMarshaler:
+    try:
+        return _OBJ_MARSHALERS[ty]
+    except KeyError:
+        pass
+
+    p = ProxyObjMarshaler()
+    _OBJ_MARSHALERS[ty] = p
+    try:
+        m = _make_obj_marshaler(ty)
+    except Exception:
+        del _OBJ_MARSHALERS[ty]
+        raise
+    else:
+        p.m = m
+        _OBJ_MARSHALERS[ty] = m
+        return m
+
+
+def marshal_obj(o: ta.Any, ty: ta.Any = None) -> ta.Any:
+    return get_obj_marshaler(ty if ty is not None else type(o)).marshal(o)
+
+
+def unmarshal_obj(o: ta.Any, ty: ta.Union[ta.Type[T], ta.Any]) -> T:
+    return get_obj_marshaler(ty).unmarshal(o)
+
+
+########################################
+# ../../../omlish/lite/runtime.py
+
+
+@cached_nullary
+def is_debugger_attached() -> bool:
+    return any(frame[1].endswith('pydevd.py') for frame in inspect.stack())
+
+
+REQUIRED_PYTHON_VERSION = (3, 8)
+
+
+def check_runtime_version() -> None:
+    if sys.version_info < REQUIRED_PYTHON_VERSION:
+        raise OSError(
+            f'Requires python {REQUIRED_PYTHON_VERSION}, got {sys.version_info} from {sys.executable}')  # noqa
+
+
+########################################
+# ../../../omlish/lite/subprocesses.py
+# ruff: noqa: UP006 UP007
+
+
+##
+
+
+_SUBPROCESS_SHELL_WRAP_EXECS = False
+
+
+def subprocess_shell_wrap_exec(args: ta.Sequence[str]) -> ta.Tuple[str, ...]:
+    return ('sh', '-c', ' '.join(map(shlex.quote, args)))
+
+
+def subprocess_maybe_shell_wrap_exec(args: ta.Sequence[str]) -> ta.Tuple[str, ...]:
+    if _SUBPROCESS_SHELL_WRAP_EXECS or is_debugger_attached():
+        return subprocess_shell_wrap_exec(args)
+    else:
+        return tuple(args)
+
+
+def _prepare_subprocess_invocation(
+        *args: ta.Any,
+        env: ta.Optional[ta.Mapping[str, ta.Any]] = None,
+        extra_env: ta.Optional[ta.Mapping[str, ta.Any]] = None,
+        quiet: bool = False,
+        **kwargs: ta.Any,
+) -> ta.Tuple[ta.Tuple[ta.Any, ...], ta.Dict[str, ta.Any]]:
+    log.debug(args)
+    if extra_env:
+        log.debug(extra_env)
+
+    if extra_env:
+        env = {**(env if env is not None else os.environ), **extra_env}
+
+    if quiet and 'stderr' not in kwargs:
+        if not log.isEnabledFor(logging.DEBUG):
+            kwargs['stderr'] = subprocess.DEVNULL
+
+    args = subprocess_maybe_shell_wrap_exec(args)
+
+    return args, dict(
+        env=env,
+        **kwargs,
+    )
+
+
+def subprocess_check_call(*args: ta.Any, stdout=sys.stderr, **kwargs: ta.Any) -> None:
+    args, kwargs = _prepare_subprocess_invocation(*args, stdout=stdout, **kwargs)
+    return subprocess.check_call(args, **kwargs)  # type: ignore
+
+
+def subprocess_check_output(*args: ta.Any, **kwargs: ta.Any) -> bytes:
+    args, kwargs = _prepare_subprocess_invocation(*args, **kwargs)
+    return subprocess.check_output(args, **kwargs)
+
+
+def subprocess_check_output_str(*args: ta.Any, **kwargs: ta.Any) -> str:
+    return subprocess_check_output(*args, **kwargs).decode().strip()
+
+
+##
+
+
+DEFAULT_SUBPROCESS_TRY_EXCEPTIONS: ta.Tuple[ta.Type[Exception], ...] = (
+    FileNotFoundError,
+    subprocess.CalledProcessError,
+)
+
+
+def subprocess_try_call(
+        *args: ta.Any,
+        try_exceptions: ta.Tuple[ta.Type[Exception], ...] = DEFAULT_SUBPROCESS_TRY_EXCEPTIONS,
+        **kwargs: ta.Any,
+) -> bool:
+    try:
+        subprocess_check_call(*args, **kwargs)
+    except try_exceptions as e:  # noqa
+        if log.isEnabledFor(logging.DEBUG):
+            log.exception('command failed')
+        return False
+    else:
+        return True
+
+
+def subprocess_try_output(
+        *args: ta.Any,
+        try_exceptions: ta.Tuple[ta.Type[Exception], ...] = DEFAULT_SUBPROCESS_TRY_EXCEPTIONS,
+        **kwargs: ta.Any,
+) -> ta.Optional[bytes]:
+    try:
+        return subprocess_check_output(*args, **kwargs)
+    except try_exceptions as e:  # noqa
+        if log.isEnabledFor(logging.DEBUG):
+            log.exception('command failed')
+        return None
+
+
+def subprocess_try_output_str(*args: ta.Any, **kwargs: ta.Any) -> ta.Optional[str]:
+    out = subprocess_try_output(*args, **kwargs)
+    return out.decode().strip() if out is not None else None
+
+
+########################################
+# payload.py
+
+
+@dc.dataclass(frozen=True)
+class CommandRequest:
+    cmd: ta.Sequence[str]
+    in_: ta.Optional[bytes] = None
+
+
+@dc.dataclass(frozen=True)
+class CommandResponse:
+    req: CommandRequest
+    rc: int
+    out: bytes
+    err: bytes
+
+
+def _payload_loop(input: ta.BinaryIO, output: ta.BinaryIO = sys.stderr.buffer) -> None:  # noqa
+    while (l := input.readline().decode('utf-8').strip()):
+        req: CommandRequest = unmarshal_obj(json.loads(l), CommandRequest)
+        proc = subprocess.Popen(  # type: ignore
+            subprocess_maybe_shell_wrap_exec(req.cmd),
+            **(dict(stdin=io.BytesIO(req.in_)) if req.in_ is not None else {}),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out, err = proc.communicate()
+        resp = CommandResponse(
+            req=req,
+            rc=proc.returncode,
+            out=out,  # noqa
+            err=err,  # noqa
+        )
+        output.write(json_dumps_compact(marshal_obj(resp)).encode('utf-8'))
+        output.write(b'\n')
+        output.flush()
+
+
+def payload_main() -> None:
+    bs = post_boostrap()
+    _payload_loop(bs.input)
---
+++ ominfra/pyremote/bootstrap.py
@@ -0,0 +1,146 @@
+import base64
+import inspect
+import os
+import sys
+import textwrap
+import typing as ta
+import zlib
+
+
+##
+
+
+_BOOTSTRAP_COMM_FD = 100
+_BOOTSTRAP_SRC_FD = 101
+
+_BOOTSTRAP_CHILD_PID_VAR = '_PYR_CPID'
+_BOOTSTRAP_ARGV0_VAR = '_PYR_ARGV0'
+
+BOOTSTRAP_ACK0 = b'OPYR000\n'
+BOOTSTRAP_ACK1 = b'OPYR001\n'
+
+_BOOTSTRAP_PROC_TITLE_FMT = '(pyremote:%s)'
+
+_BOOTSTRAP_IMPORTS = [
+    'base64',
+    'os',
+    'sys',
+    'zlib',
+]
+
+
+def _bootstrap_main(context_name: str, main_z_len: int) -> None:
+    # Two copies of main src to be sent to parent
+    r0, w0 = os.pipe()
+    r1, w1 = os.pipe()
+
+    if (cp := os.fork()):
+        # Parent process
+
+        # Dup original stdin to comm_fd for use as comm channel
+        os.dup2(0, _BOOTSTRAP_COMM_FD)
+
+        # Overwrite stdin (fed to python repl) with first copy of src
+        os.dup2(r0, 0)
+
+        # Dup second copy of src to src_fd to recover after launch
+        os.dup2(r1, _BOOTSTRAP_SRC_FD)
+
+        # Close remaining fd's
+        for f in [r0, w0, r1, w1]:
+            os.close(f)
+
+        # Save child pid to close after relaunch
+        os.environ[_BOOTSTRAP_CHILD_PID_VAR] = str(cp)
+
+        # Save original argv0
+        os.environ[_BOOTSTRAP_ARGV0_VAR] = sys.executable
+
+        # Start repl reading stdin from r0
+        os.execl(sys.executable, sys.executable + (_BOOTSTRAP_PROC_TITLE_FMT % (context_name,)))
+
+    else:
+        # Child process
+
+        # Write first ack
+        os.write(1, BOOTSTRAP_ACK0)
+
+        # Read main src from stdin
+        main_src = zlib.decompress(os.fdopen(0, 'rb').read(main_z_len))
+
+        # Write both copies of main src
+        for w in [w0, w1]:
+            fp = os.fdopen(w, 'wb', 0)
+            fp.write(main_src)
+            fp.close()
+
+        # Write second ack
+        os.write(1, BOOTSTRAP_ACK1)
+
+        sys.exit(0)
+
+
+#
+
+
+def bootstrap_payload(context_name: str, main_z_len: int) -> str:
+    bs_src = textwrap.dedent(inspect.getsource(_bootstrap_main))
+
+    for gl in [
+        '_BOOTSTRAP_COMM_FD',
+        '_BOOTSTRAP_SRC_FD',
+
+        '_BOOTSTRAP_CHILD_PID_VAR',
+        '_BOOTSTRAP_ARGV0_VAR',
+
+        'BOOTSTRAP_ACK0',
+        'BOOTSTRAP_ACK1',
+
+        '_BOOTSTRAP_PROC_TITLE_FMT',
+    ]:
+        bs_src = bs_src.replace(gl, repr(globals()[gl]))
+
+    bs_src = '\n'.join(
+        cl
+        for l in bs_src.splitlines()
+        if (cl := (l.split('#')[0]).rstrip())
+        if cl.strip()
+    )
+
+    bs_z = zlib.compress(bs_src.encode('utf-8'))
+    bs_z64 = base64.encodebytes(bs_z).replace(b'\n', b'')
+
+    stmts = [
+        f'import {", ".join(_BOOTSTRAP_IMPORTS)}',
+        f'exec(zlib.decompress(base64.decodebytes({bs_z64!r})))',
+        f'_bootstrap_main({context_name!r}, {main_z_len})',
+    ]
+
+    cmd = '; '.join(stmts)
+    return cmd
+
+
+#
+
+
+class PostBoostrap(ta.NamedTuple):
+    input: ta.BinaryIO
+    main_src: str
+
+
+def post_boostrap() -> PostBoostrap:
+    # Restore original argv0
+    sys.executable = os.environ.pop(_BOOTSTRAP_ARGV0_VAR)
+
+    # Reap boostrap child
+    os.waitpid(int(os.environ.pop(_BOOTSTRAP_CHILD_PID_VAR)), 0)
+
+    # Read second copy of main src
+    r1 = os.fdopen(_BOOTSTRAP_SRC_FD, 'rb', 0)
+    main_src = r1.read().decode('utf-8')
+    r1.close()
+
+    return PostBoostrap(
+        input=os.fdopen(_BOOTSTRAP_COMM_FD, 'rb', 0),
+        main_src=main_src,
+    )
