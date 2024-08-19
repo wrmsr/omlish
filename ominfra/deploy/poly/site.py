7768becb2e1dd530cb296fb8c2fe70772f01dba6
wrmsr <timwilloney@gmail.com>
2024-08-19 16:39:30 -0700

    --
    Former-commit-id: 561742d0ad484839693e5bd3edade6ae2647afe6

--- ominfra/deploy/poly/site.py
+++ ominfra/deploy/poly/site.py
@@ -1,35 +1,29 @@
 # ruff: noqa: UP006 UP007
 import typing as ta
 
-from .base import Deploy
-from .base import DeployConcern
-from .base import DeployConcernT
+from .base import Site
+from .base import SiteConcern
+from .base import SiteConcernT
 from .base import Runtime
-from .nginx import NginxDeployConcern  # noqa
-from .repo import RepoDeployConcern  # noqa
-from .venv import VenvDeployConcern  # noqa
 
 
-DEPLOY_CONCERN_CLS_BY_CONFIG_CLS: ta.Mapping[ta.Type[DeployConcern.Config], ta.Type[DeployConcern]] = {
-    cls.Config: cls  # type: ignore
-    for cls in DeployConcern.__subclasses__()
-}
+SITE_CONCERN_CLS_BY_CONFIG_CLS: ta.Mapping[ta.Type[SiteConcern.Config], ta.Type[SiteConcern]] = {}
 
 
-class DeployImpl(Deploy):
+class SiteImpl(Site):
     def __init__(
             self,
-            config: Deploy.Config,
+            config: Site.Config,
             runtime: ta.Optional[Runtime] = None,
     ) -> None:
         super().__init__()
         self._config = config
 
         self._concerns = [
-            DEPLOY_CONCERN_CLS_BY_CONFIG_CLS[type(c)](c, self)
+            SITE_CONCERN_CLS_BY_CONFIG_CLS[type(c)](c, self)
             for c in config.concerns
         ]
-        self._concerns_by_cls: ta.Dict[ta.Type[DeployConcern], DeployConcern] = {}
+        self._concerns_by_cls: ta.Dict[ta.Type[SiteConcern], SiteConcern] = {}
         for c in self._concerns:
             if type(c) in self._concerns_by_cls:
                 raise TypeError(f'Duplicate concern type: {c}')
@@ -38,14 +32,14 @@ class DeployImpl(Deploy):
         self._runtime = runtime
 
     @property
-    def config(self) -> 'Deploy.Config':
+    def config(self) -> 'Site.Config':
         return self._config
 
     @property
-    def concerns(self) -> ta.List[DeployConcern]:
+    def concerns(self) -> ta.List[SiteConcern]:
         return self._concerns
 
-    def concern(self, cls: ta.Type[DeployConcernT]) -> DeployConcernT:
+    def concern(self, cls: ta.Type[SiteConcernT]) -> SiteConcernT:
         return self._concerns_by_cls[cls]  # type: ignore
 
     def runtime(self) -> Runtime:
