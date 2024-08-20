122e3900e43ac4e4815677e1b38bd676bb1b7a52
wrmsr <timwilloney@gmail.com>
2024-08-19 17:28:03 -0700

    --
    Former-commit-id: 1b818709145dccfe0706205656df7ee8a1b34fd9

--- ominfra/deploy/poly/nginx.py
+++ ominfra/deploy/poly/nginx.py
@@ -8,6 +8,24 @@ from omlish.lite.cached import cached_nullary
 from .base import DeployConcern
 from .base import FsFile
 from .base import FsItem
+from .base import SiteConcern
+
+
+class NginxSiteConcern(SiteConcern['NginxSiteConcern.Config']):
+    @dc.dataclass(frozen=True)
+    class Config(SiteConcern.Config):
+        global_conf_file: str = '/etc/nginx/sites-enabled/omlish.conf'
+
+    @cached_nullary
+    def confs_dir(self) -> str:
+        return os.path.join(self._site.config.root_dir, 'conf', 'nginx')
+
+    def run(self) -> None:
+        if self._site.runtime().stat(self._config.global_conf_file) is None:
+            self._site.runtime().write_file(
+                self._config.global_conf_file,
+                f'include {self.confs_dir()}/*.conf;\n',
+            )
 
 
 class NginxDeployConcern(DeployConcern['NginxDeployConcern.Config']):
@@ -18,7 +36,7 @@ class NginxDeployConcern(DeployConcern['NginxDeployConcern.Config']):
 
     @cached_nullary
     def conf_file(self) -> str:
-        return os.path.join(self._deploy.config.root_dir, 'conf', 'nginx', self._deploy.config.name + '.conf')
+        return os.path.join(self._deploy.site.concern(NginxSiteConcern).confs_dir(), self._deploy.config.name + '.conf')
 
     @cached_nullary
     def fs_items(self) -> ta.Sequence[FsItem]:
