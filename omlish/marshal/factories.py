a77073f3cbe51d1951c5e7e39bcdb67ccf319890
wrmsr <timwilloney@gmail.com>
2024-08-21 13:50:08 -0700

    --
    Former-commit-id: 9fa8df62025d2f8a786e44f4ada979a387a4c063

--- omlish/marshal/factories.py
+++ omlish/marshal/factories.py
@@ -1,78 +1,82 @@
-import abc
 import dataclasses as dc
-import enum
 import threading
 import typing as ta
 
+from .. import matchfns as mfs
 from .. import reflect as rfl
 
 
 R = ta.TypeVar('R')
 C = ta.TypeVar('C')
-A = ta.TypeVar('A')
-
-
-##
-
-
-class Factory(abc.ABC, ta.Generic[R, C, A]):
-    @abc.abstractmethod
-    def __call__(self, ctx: C, arg: A) -> R | None:
-        raise NotImplementedError
 
 
 ##
 
 
 @dc.dataclass(frozen=True)
-class FuncFactory(ta.Generic[R, C, A]):
-    fn: ta.Callable[[C, A], R | None]
-
-    def __call__(self, ctx: C, arg: A) -> R | None:
-        return self.fn(ctx, arg)
-
-
-##
-
-
-@dc.dataclass(frozen=True)
-class TypeMapFactory(Factory[R, C, rfl.Type]):
+class TypeMapFactory(mfs.MatchFn[[C, rfl.Type], R]):
     m: ta.Mapping[rfl.Type, R] = dc.field(default_factory=dict)
 
-    def __call__(self, ctx: C, rty: rfl.Type) -> R | None:
-        return self.m.get(rty)
+    def guard(self, ctx: C, ty: rfl.Type) -> bool:
+        return ty in self.m
+
+    def fn(self, ctx: C, ty: rfl.Type) -> R:
+        try:
+            return self.m[ty]
+        except KeyError:
+            raise mfs.MatchGuardError(ctx, ty)
 
 
 ##
 
 
-class TypeCacheFactory(Factory[R, C, rfl.Type]):
-    def __init__(self, f: Factory[R, C, rfl.Type]) -> None:
+class TypeCacheFactory(mfs.MatchFn[[C, rfl.Type], R]):
+    def __init__(self, f: mfs.MatchFn[[C, rfl.Type], R]) -> None:
         super().__init__()
         self._f = f
         self._dct: dict[rfl.Type, R | None] = {}
         self._mtx = threading.RLock()
 
-    def __call__(self, ctx: C, rty: rfl.Type) -> R | None:
-        try:
-            return self._dct[rty]
-        except KeyError:
-            pass
+    def guard(self, ctx: C, ty: rfl.Type) -> bool:
         with self._mtx:
             try:
-                return self._dct[rty]
+                e = self._dct[ty]
             except KeyError:
-                ret = self._dct[rty] = self._f(ctx, rty)
-                return ret
+                if self._f.guard(ctx, ty):
+                    return True
+                else:
+                    self._dct[ty] = None
+                    return False
+            else:
+                return e is not None
+
+    def fn(self, ctx: C, ty: rfl.Type) -> R:
+        with self._mtx:
+            try:
+                e = self._dct[ty]
+            except KeyError:
+                try:
+                    ret = self._f.fn(ctx, ty)
+                except mfs.MatchGuardError:
+                    self._dct[ty] = None
+                    raise
+                else:
+                    self._dct[ty] = ret
+                    return ret
+            else:
+                if e is None:
+                    raise mfs.MatchGuardError(ctx, ty)
+                else:
+                    return e
 
 
 ##
 
 
-class RecursiveTypeFactory(Factory[R, C, rfl.Type]):
+class RecursiveTypeFactory(mfs.MatchFn[[C, rfl.Type], R]):
     def __init__(
             self,
-            f: Factory[R, C, rfl.Type],
+            f: mfs.MatchFn[[C, rfl.Type], R],
             prx: ta.Callable[[], tuple[R | None, ta.Callable[[R | None], None]]],
     ) -> None:
         super().__init__()
@@ -80,7 +84,10 @@ class RecursiveTypeFactory(Factory[R, C, rfl.Type]):
         self._prx = prx
         self._dct: dict[rfl.Type, R | None] = {}
 
-    def __call__(self, ctx: C, rty: rfl.Type) -> R | None:
+    def guard(self, ctx: C, ty: rfl.Type) -> bool:
+        return self._f.guard(ctx, ty)
+
+    def fn(self, ctx: C, rty: rfl.Type) -> R | None:
         try:
             return self._dct[rty]
         except KeyError:
@@ -93,37 +100,3 @@ class RecursiveTypeFactory(Factory[R, C, rfl.Type]):
             return r
         finally:
             del self._dct[rty]
-
-
-##
-
-
-class CompositeFactory(Factory[R, C, A]):
-    class Strategy(enum.Enum):
-        FIRST = enum.auto()
-        ONE = enum.auto()
-
-    def __init__(self, *fs: Factory[R, C, A], strategy: Strategy = Strategy.FIRST) -> None:
-        super().__init__()
-        self._fs = fs
-        self._st = strategy
-
-    def __call__(self, ctx: C, arg: A) -> R | None:
-        w: list[R] = []
-        for c in self._fs:
-            if (r := c(ctx, arg)) is None:
-                continue
-            if self._st is CompositeFactory.Strategy.FIRST:
-                return r
-            w.append(r)
-
-        if not w:
-            return None
-
-        if self._st is CompositeFactory.Strategy.ONE:
-            if len(w) == 1:
-                return w[0]
-
-            raise TypeError(f'multiple implementations: {arg} {w}')
-
-        raise TypeError(f'unknown composite strategy: {self._st}')
