75c1c16a6089db717a8a7e374b4063c868f21a8a
wrmsr <timwilloney@gmail.com>
2024-08-21 13:29:41 -0700

    --
    Former-commit-id: e796246617c7582929d48021d7cae8bc2f2af754

--- omlish/lite/logs.py
+++ omlish/lite/logs.py
@@ -6,8 +6,45 @@ TODO:
 import logging
 import typing as ta
 
+from .json import json_dumps_compact
+
 
 log = logging.getLogger(__name__)
+
+
+class JsonLogFormatter(logging.Formatter):
+
+    KEYS: ta.Mapping[str, bool] = {
+        'name': False,
+        'msg': False,
+        'args': False,
+        'levelname': False,
+        'levelno': False,
+        'pathname': False,
+        'filename': False,
+        'module': False,
+        'exc_info': True,
+        'exc_text': True,
+        'stack_info': True,
+        'lineno': False,
+        'funcName': False,
+        'created': False,
+        'msecs': False,
+        'relativeCreated': False,
+        'thread': False,
+        'threadName': False,
+        'processName': False,
+        'process': False,
+    }
+
+    def format(self, record: logging.LogRecord) -> str:
+        dct = {
+            k: v
+            for k, o in self.KEYS.items()
+            for v in [getattr(record, k)]
+            if not (o and v is None)
+        }
+        return json_dumps_compact(dct)
 
 
 def configure_standard_logging(level: ta.Union[int, str] = logging.INFO) -> None:
