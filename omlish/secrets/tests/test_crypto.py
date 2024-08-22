0b055aa3840841d0272a7ae942cff16a3ef8a8c8
wrmsr <timwilloney@gmail.com>
2024-08-22 13:39:18 -0700

    --
    Former-commit-id: 2f713d5fac15278970859f41582cf04c25d796fd

--- omlish/secrets/tests/test_crypto.py
+++ omlish/secrets/tests/test_crypto.py
@@ -15,6 +15,42 @@ def test_openssl_subproc_crypto(sec: crypto.Crypto) -> None:
     raw = b'hi there'
     enc = sec.encrypt(raw, key)
 
+    ##
+
+    # import hashlib
+    # block_size = 16
+    # key_length = 32
+    # iv_length = 16
+    # salt = enc[len('Salted__'):block_size]
+    # dk = hashlib.pbkdf2_hmac('sha512', key, salt, 10_000, key_length + iv_length)
+
+    # # def derive_key_and_iv(password, salt, key_length, iv_length):
+    # #     d = d_i = ''
+    # #     while len(d) < key_length + iv_length:
+    # #         d_i = md5(d_i + password + salt).digest()
+    # #         d += d_i
+    # #     return , dk[key_length:key_length + iv_length]
+
+    # # key, iv = derive_key_and_iv(password, salt, key_length, bs)
+    # # cipher = AES.new(key, AES.MODE_CBC, iv)
+    # # next_chunk = ''
+    # # finished = False
+    # # while not finished:
+    # #     chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
+    # #     if len(next_chunk) == 0:
+    # #         padding_length = ord(chunk[-1])
+    # #         chunk = chunk[:-padding_length]
+    # #         finished = True
+    # #     out_file.write(chunk)
+
+    # from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+    # cipher = Cipher(algorithms.AES256(dk[:key_length]), modes.CBC(dk[key_length:key_length + iv_length]))
+    # decryptor = cipher.decryptor()
+    # dec2 = decryptor.update(enc[16:]) + decryptor.finalize()
+    # print(dec2)
+
+    ##
+
     dec = sec.decrypt(enc, key)
     assert dec == raw
 
