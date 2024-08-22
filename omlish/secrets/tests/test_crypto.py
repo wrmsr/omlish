92f67b9bf7ed2a6268da9fc2d7de64824632a789
wrmsr <timwilloney@gmail.com>
2024-08-22 12:53:33 -0700

    --
    Former-commit-id: de05f669d08bfcdc5f46dbda2addccb0728dd1d6

--- omlish/secrets/tests/test_crypto.py
+++ omlish/secrets/tests/test_crypto.py
@@ -1,23 +1,38 @@
 import pytest
 
-from ..crypto import Crypto
-from ..crypto import OpensslSubprocessCrypto
+from .. import crypto
 from ..subprocesses import pipe_fd_subprocess_file_input
 from ..subprocesses import temp_subprocess_file_input
 
 
 @pytest.mark.parametrize('sec', [
-    OpensslSubprocessCrypto(),
-    OpensslSubprocessCrypto(file_input=temp_subprocess_file_input),  # noqa
-    OpensslSubprocessCrypto(file_input=pipe_fd_subprocess_file_input),  # noqa
+    crypto.OpensslSubprocessCrypto(),
+    crypto.OpensslSubprocessCrypto(file_input=temp_subprocess_file_input),  # noqa
+    # crypto.OpensslSubprocessCrypto(file_input=pipe_fd_subprocess_file_input),  # noqa
 ])
-def test_crypto(sec: Crypto) -> None:
+def test_openssl_subproc_crypto(sec: crypto.Crypto) -> None:
     key = sec.generate_key()
-    print(repr(key))
 
     raw = b'hi there'
     enc = sec.encrypt(raw, key)
-    print(repr(enc))
 
     dec = sec.decrypt(enc, key)
-    print(repr(dec))
+    assert dec == raw
+
+
+@pytest.mark.parametrize('sec', [
+    crypto.FernetCrypto(),
+    crypto.AesgsmCrypto(),
+    crypto.Chacha20Poly1305Crypto(),
+])
+def test_crypto_crypto(sec: crypto.Crypto) -> None:
+    key = sec.generate_key()
+
+    raw = b'hi there'
+    enc = sec.encrypt(raw, key)
+
+    dec = sec.decrypt(enc, key)
+    assert dec == raw
+
+    with pytest.raises(crypto.DecryptionError):
+        sec.decrypt(enc, bytes([key[0] + 1 if key[0] < 255 else 0]) + key[1:])
