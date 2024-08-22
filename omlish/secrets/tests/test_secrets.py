d7d2f90aa8e4460d9a5fbecab3141283b2e1be98
wrmsr <timwilloney@gmail.com>
2024-08-21 22:23:32 -0700

    --
    Former-commit-id: 3624ef66f758844c6cb9afa325420fd5d6b71f44

--- omlish/secrets/tests/test_secrets.py
+++ omlish/secrets/tests/test_secrets.py
@@ -3,6 +3,24 @@ from .. import secrets as sec
 
 def test_logging():
     s = sec.LoggingSecrets(
-        sec.SimpleSecrets({'foo': 'bar'}),
+        sec.MappingSecrets({'foo': 'bar'}),
     )
     assert s.get('foo') == 'bar'
+
+
+def test_cache_ttl():
+    cts = {}
+
+    def f(k):
+        cts[k] = cts.get(k, 0) + 1
+        return k + '!'
+
+    now = 0.
+    s = sec.CachingSecrets(
+        sec.FnSecrets(f),
+        ttl_s=1.,
+        clock=lambda: now,
+    )
+
+    assert not cts
+    
