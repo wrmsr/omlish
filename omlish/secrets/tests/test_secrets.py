31da11efebd09ddd2491d17e6683114bbf0cd13f
wrmsr <timwilloney@gmail.com>
2024-08-21 22:35:27 -0700

    --
    Former-commit-id: b0841697fa492cdb242f97219043fcf443efd2b2

--- omlish/secrets/tests/test_secrets.py
+++ omlish/secrets/tests/test_secrets.py
@@ -15,7 +15,7 @@ def test_cache_ttl():
         cts[k] = cts.get(k, 0) + 1
         return k + '!'
 
-    now = 0.
+    now = .0
     s = sec.CachingSecrets(
         sec.FnSecrets(f),
         ttl_s=1.,
@@ -23,4 +23,21 @@ def test_cache_ttl():
     )
 
     assert not cts
-    
+    assert s.get('a') == 'a!'
+    assert cts['a'] == 1
+    assert s.get('a') == 'a!'
+    assert cts['a'] == 1
+    now = .2
+    assert s.get('b') == 'b!'
+    assert cts['b'] == 1
+    for t in [.5, .9]:
+        now = t
+        assert s.get('a') == 'a!'
+        assert cts['a'] == 1
+        assert s.get('b') == 'b!'
+        assert cts['b'] == 1
+    assert s.get('c') == 'c!'
+    assert cts == {'a': 1, 'b': 1, 'c': 1}
+    now = 1.
+    assert s.get('a') == 'a!'
+    assert cts['a'] == 2
