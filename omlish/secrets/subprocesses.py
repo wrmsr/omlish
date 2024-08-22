536b6a9c1eba4c848bbd157c708a53d635446326
wrmsr <timwilloney@gmail.com>
2024-08-21 17:27:13 -0700

    --
    Former-commit-id: 5276069c8153c67650dcacf95e4d85fde57ace3b

---
+++ omlish/secrets/subprocesses.py
@@ -0,0 +1,38 @@
+import contextlib
+import fcntl
+import os
+import tempfile
+import typing as ta
+
+
+class SubprocessFileInput(ta.NamedTuple):
+    file_path: str
+    pass_fds: ta.Sequence[int]
+
+
+SubprocessFileInputMethod: ta.TypeAlias = ta.Callable[[bytes], ta.ContextManager[SubprocessFileInput]]
+
+
+@contextlib.contextmanager
+def temp_subprocess_file_input(buf: bytes) -> ta.Generator[SubprocessFileInput]:
+    with tempfile.NamedTemporaryFile() as kf:
+        kf.write(buf)
+        kf.flush()
+        yield SubprocessFileInput(kf.name, [])
+
+
+@contextlib.contextmanager
+def pipe_fd_subprocess_file_input(buf: bytes) -> ta.Iterator[SubprocessFileInput]:
+    rfd, wfd = os.pipe()
+    closed_wfd = False
+    try:
+        if hasattr(fcntl, 'F_SETPIPE_SZ'):
+            fcntl.fcntl(wfd, fcntl.F_SETPIPE_SZ, max(len(buf), 0x1000))
+        os.write(wfd, buf)
+        os.close(wfd)
+        closed_wfd = True
+        yield SubprocessFileInput(f'/dev/fd/{rfd}', [rfd])
+    finally:
+        if not closed_wfd:
+            os.close(wfd)
+        os.close(rfd)
