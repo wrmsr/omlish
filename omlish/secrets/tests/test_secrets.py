39dad0d8beaff34776345eab050f74e966d69a17
wrmsr <timwilloney@gmail.com>
2024-08-22 16:09:58 -0700

    --
    Former-commit-id: b6d90513b4047831cfe3a495871446ebf8775c5b

--- omlish/secrets/tests/test_secrets.py
+++ omlish/secrets/tests/test_secrets.py
@@ -1,11 +1,22 @@
+import pickle
+
+import pytest
+
 from .. import secrets as sec
+
+
+def test_opaque():
+    o = sec.Secret('foo')
+    assert o.reveal() == 'foo'
+    with pytest.raises(TypeError):
+        pickle.dumps(o)
 
 
 def test_logging():
     s = sec.LoggingSecrets(
         sec.MappingSecrets({'foo': 'bar'}),
     )
-    assert s.get('foo') == 'bar'
+    assert s.get('foo').reveal() == 'bar'
 
 
 def test_cache_ttl():
@@ -23,40 +34,40 @@ def test_cache_ttl():
     )
 
     assert not cts
-    assert s.get('a') == 'a!'
+    assert s.get('a').reveal() == 'a!'
     assert cts['a'] == 1
-    assert s.get('a') == 'a!'
+    assert s.get('a').reveal() == 'a!'
     assert cts['a'] == 1
     now = .2
-    assert s.get('b') == 'b!'
+    assert s.get('b').reveal() == 'b!'
     assert cts['b'] == 1
     for t in [.5, .9]:
         now = t
-        assert s.get('a') == 'a!'
+        assert s.get('a').reveal() == 'a!'
         assert cts['a'] == 1
-        assert s.get('b') == 'b!'
+        assert s.get('b').reveal() == 'b!'
         assert cts['b'] == 1
-    assert s.get('c') == 'c!'
+    assert s.get('c').reveal() == 'c!'
     assert cts == {'a': 1, 'b': 1, 'c': 1}
     now = 1.
-    assert s.get('a') == 'a!'
+    assert s.get('a').reveal() == 'a!'
     assert cts['a'] == 2
-    assert s.get('a') == 'a!'
+    assert s.get('a').reveal() == 'a!'
     assert cts['a'] == 2
-    assert s.get('b') == 'b!'
+    assert s.get('b').reveal() == 'b!'
     assert cts['b'] == 1
     now = 1.89
-    assert s.get('c') == 'c!'
+    assert s.get('c').reveal() == 'c!'
     assert cts['c'] == 1
-    assert s.get('b') == 'b!'
+    assert s.get('b').reveal() == 'b!'
     assert cts['b'] == 2
     now = 1.9
-    assert s.get('c') == 'c!'
+    assert s.get('c').reveal() == 'c!'
     assert cts['c'] == 2
-    assert s.get('b') == 'b!'
+    assert s.get('b').reveal() == 'b!'
     assert cts['b'] == 2
-    assert s.get('a') == 'a!'
+    assert s.get('a').reveal() == 'a!'
     assert cts['a'] == 2
     now = 2.
-    assert s.get('a') == 'a!'
+    assert s.get('a').reveal() == 'a!'
     assert cts['a'] == 3
