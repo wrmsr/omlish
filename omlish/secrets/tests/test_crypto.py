2b79855307d56868858d615dce9eba3b56ef10c4
wrmsr <timwilloney@gmail.com>
2024-08-21 17:42:40 -0700

    --
    Former-commit-id: e3f292341fcc65a2cb07c25cbe9aa8db63c15713

--- omlish/secrets/tests/test_crypto.py
+++ omlish/secrets/tests/test_crypto.py
@@ -1,16 +1,17 @@
 import pytest
 
-from ..crypto import OpensslShellCrypto
+from ..crypto import Crypto
+from ..crypto import OpensslSubprocessCrypto
 from ..subprocesses import pipe_fd_subprocess_file_input
 from ..subprocesses import temp_subprocess_file_input
 
 
 @pytest.mark.parametrize('sec', [
-    OpensslShellCrypto(),
-    OpensslShellCrypto(file_input=temp_subprocess_file_input),  # noqa
-    OpensslShellCrypto(file_input=pipe_fd_subprocess_file_input),  # noqa
+    OpensslSubprocessCrypto(),
+    OpensslSubprocessCrypto(file_input=temp_subprocess_file_input),  # noqa
+    OpensslSubprocessCrypto(file_input=pipe_fd_subprocess_file_input),  # noqa
 ])
-def test_crypto(sec: OpensslShellCrypto) -> None:
+def test_crypto(sec: Crypto) -> None:
     key = sec.generate_key()
     print(repr(key))
 
