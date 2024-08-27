0b616193e5afbf78f1f5fd62103e365283880c1f
wrmsr <timwilloney@gmail.com>
2024-08-27 10:52:43 -0400

    --
    Former-commit-id: d30f74323221212b5508c068c03f314afbec8cec

---
+++ omdev/__about__.py
@@ -0,0 +1,35 @@
+from omlish.__about__ import ProjectBase
+from omlish.__about__ import SetuptoolsBase
+from omlish.__about__ import __version__
+
+
+class Project(ProjectBase):
+    name = 'omdev'
+    description = 'omdev'
+
+    dependencies = [
+        f'omlish == {__version__}'
+    ]
+
+    optional_dependencies = {
+        'c': [
+            'pycparser >= 2.22',
+            'cffi >= 1.17',
+            'pcpp >= 1.30',
+        ],
+
+        'mypy': [
+            'mypy >= 1.11',
+        ],
+
+        'tokens': [
+            'tokenize_rt >= 6',
+        ]
+    }
+
+
+class Setuptools(SetuptoolsBase):
+    find_packages = {
+        'include': ['omdev', 'omdev.*'],
+        'exclude': [*SetuptoolsBase.find_packages['exclude']],
+    }
