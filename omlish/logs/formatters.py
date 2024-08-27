b7dd766fa77a0e596e552dbcacabfacf85032b30
wrmsr <timwilloney@gmail.com>
2024-08-27 12:33:10 -0400

    --
    Former-commit-id: 03717d69a3be7e02d16bbbc127f16286ceba8d68

--- omlish/logs/formatters.py
+++ omlish/logs/formatters.py
@@ -1,25 +1,10 @@
-# ruff: noqa: ANN201
-# ruff: noqa: N802
-
-import datetime
+# ruff: noqa: ANN201 N802
 import logging
 import typing as ta
 
 from .. import term
-from ..formats import json
-
-
-class StandardLogFormatter(logging.Formatter):
-
-    converter = datetime.datetime.fromtimestamp  # type: ignore
-
-    def formatTime(self, record, datefmt=None):
-        ct = self.converter(record.created)  # type: ignore
-        if datefmt:
-            return ct.strftime(datefmt)  # noqa
-        else:
-            t = ct.strftime("%Y-%m-%d %H:%M:%S")  # noqa
-            return '%s.%03d' % (t, record.msecs)
+from ..lite.logs import JsonLogFormatter  # noqa
+from ..lite.logs import StandardLogFormatter
 
 
 class ColorLogFormatter(StandardLogFormatter):
@@ -39,38 +24,3 @@ class ColorLogFormatter(StandardLogFormatter):
         else:
             buf = term.SGR(c) + buf + term.SGR(term.SGRs.RESET)
         return buf
-
-
-class JsonLogFormatter(logging.Formatter):
-
-    KEYS: ta.Mapping[str, bool] = {
-        'name': False,
-        'msg': False,
-        'args': False,
-        'levelname': False,
-        'levelno': False,
-        'pathname': False,
-        'filename': False,
-        'module': False,
-        'exc_info': True,
-        'exc_text': True,
-        'stack_info': True,
-        'lineno': False,
-        'funcName': False,
-        'created': False,
-        'msecs': False,
-        'relativeCreated': False,
-        'thread': False,
-        'threadName': False,
-        'processName': False,
-        'process': False,
-    }
-
-    def format(self, record: logging.LogRecord) -> str:
-        dct = {
-            k: v
-            for k, o in self.KEYS.items()
-            for v in [getattr(record, k)]
-            if not (o and v is None)
-        }
-        return json.dumps_compact(dct)
