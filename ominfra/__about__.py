2360aa1809b5471bf16fc23b269b10b06ed12639
wrmsr <timwilloney@gmail.com>
2024-08-27 10:52:43 -0400

    --
    Former-commit-id: d30f74323221212b5508c068c03f314afbec8cec

---
+++ ominfra/__about__.py
@@ -0,0 +1,30 @@
+"""
+
+"""
+from omlish.__about__ import ProjectBase
+from omlish.__about__ import SetuptoolsBase
+from omlish.__about__ import __version__
+
+
+class Project(ProjectBase):
+    name = 'ominfra'
+    description = 'ominfra'
+
+    dependencies = [
+        f'omlish == {__version__}'
+    ]
+
+    optional_dependencies = {
+        'ssh': [
+            'paramiko >= 3.4',  # !! LGPL
+
+            "asyncssh >= 2.16; python_version < '3.13'",  # cffi
+        ],
+    }
+
+
+class Setuptools(SetuptoolsBase):
+    find_packages = {
+        'include': ['ominfra', 'ominfra.*'],
+        'exclude': [*SetuptoolsBase.find_packages['exclude']],
+    }
