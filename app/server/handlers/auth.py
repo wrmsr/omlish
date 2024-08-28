7abf7cdb6c7c2c8d0e07b9b32ac010f7fd804473
wrmsr <timwilloney@gmail.com>
2024-08-28 15:26:28 -0400

    --
    Former-commit-id: 3c1a5a0181558bec97466c47ec6cc3cb5492d3ba

--- app/server/handlers/auth.py
+++ app/server/handlers/auth.py
@@ -1,17 +1,10 @@
 import dataclasses as dc
-import functools
-import typing as ta
-
-import anyio.to_thread
 
 from omlish import http as hu
-from omlish import lang
-from omlish.asyncs import anyio as anu
 from omlish.formats import json
 from omlish.http.asgi import AsgiRecv
 from omlish.http.asgi import AsgiScope
 from omlish.http.asgi import AsgiSend
-from omlish.http.asgi import read_body
 from omlish.http.asgi import send_response
 from omserv.apps.routes import Handler_
 from omserv.apps.routes import Route
@@ -21,37 +14,19 @@ from ...users import UserStore
 from ..apps.users import get_auth_user
 
 
-if ta.TYPE_CHECKING:
-    import tiktoken
-else:
-    tiktoken = lang.proxy_import('tiktoken')
-
-
-def _gpt2_enc() -> 'tiktoken.Encoding':
-    return tiktoken.get_encoding('gpt2')
-
-
-gpt2_enc = anu.LazyFn(functools.partial(anyio.to_thread.run_sync, _gpt2_enc))
-
-
 @dc.dataclass(frozen=True)
-class TikHandler(Handler_):
+class AuthHandler(Handler_):
     _users: UserStore
 
-    @handles(Route.post('/tik'))
-    async def handle_post_tik(self, scope: AsgiScope, recv: AsgiRecv, send: AsgiSend) -> None:
+    @handles(Route.post('/check-auth'))
+    async def handle_post_check_auth(self, scope: AsgiScope, recv: AsgiRecv, send: AsgiSend) -> None:
         if (user := await get_auth_user(scope, self._users)) is None:
             await send_response(send, 401)
             return
 
-        enc = await gpt2_enc.get()
-
-        req_body = await read_body(recv)
-        toks = enc.encode(req_body.decode())
         dct = {
             'user_id': user.id,
             'user_name': user.name,
-            'tokens': toks,
         }
         resp_body = json.dumps(dct).encode() + b'\n'
 
--- app/server/handlers/inject.py
+++ app/server/handlers/inject.py
@@ -1,6 +1,7 @@
 from omlish import inject as inj
 from omserv.apps.inject import bind_handler
 
+from .auth import AuthHandler
 from .diag import DiagHandler
 from .favicon import FaviconHandler
 from .index import IndexHandler
@@ -14,6 +15,7 @@ from .tik import TikHandler
 
 def bind() -> inj.Elemental:
     return inj.as_elements(
+        bind_handler(AuthHandler),
         bind_handler(DiagHandler),
         bind_handler(FaviconHandler),
         bind_handler(IndexHandler),
