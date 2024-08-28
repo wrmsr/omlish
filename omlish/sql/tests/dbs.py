a5bb36ac77aea50d57585a460db449e5741c7ba6
wrmsr <timwilloney@gmail.com>
2024-08-28 14:24:46 -0400

    --
    Former-commit-id: 6746ffef48c38dd799f782829d7519affd7bffc5

--- omlish/sql/tests/dbs.py
+++ omlish/sql/tests/dbs.py
@@ -2,6 +2,7 @@ import typing as ta
 
 from ... import check
 from ...docker import get_compose_port
+from ...docker import is_likely_in_docker
 from ...testing.pytest import inject as pti
 from ...tests.docker import ComposeServices
 from ..dbs import DbSpec
@@ -19,22 +20,36 @@ class Dbs:
         svcs = self._compose_services.compose_config().get_services()
         lst: list[DbSpec] = []
 
+        in_docker = is_likely_in_docker()
+
         if (mysql := svcs.get('mysql')):
-            port = get_compose_port(mysql, check.not_none(DbTypes.MYSQL.default_port))
+            if in_docker:
+                host = self._compose_services.prefix + 'mysql'
+                port = DbTypes.MYSQL.default_port
+            else:
+                host = '127.0.0.1'
+                port = get_compose_port(mysql, check.not_none(DbTypes.MYSQL.default_port))
+
             env = mysql['environment']
             lst.append(DbSpec(
                 'mysql',
                 DbTypes.MYSQL,
-                UrlDbLoc(f'mysql://root:{env["MYSQL_ROOT_PASSWORD"]}@127.0.0.1:{port}'),
+                UrlDbLoc(f'mysql://root:{env["MYSQL_ROOT_PASSWORD"]}@{host}:{port}'),
             ))
 
         if (postgres := svcs.get('postgres')):
-            port = get_compose_port(postgres, check.not_none(DbTypes.POSTGRES.default_port))
+            if in_docker:
+                host = self._compose_services.prefix + 'postgres'
+                port = DbTypes.POSTGRES.default_port
+            else:
+                host = '127.0.0.1'
+                port = get_compose_port(postgres, check.not_none(DbTypes.POSTGRES.default_port))
+
             env = postgres['environment']
             lst.append(DbSpec(
                 'postgres',
                 DbTypes.POSTGRES,
-                UrlDbLoc(f'postgresql://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@127.0.0.1:{port}'),
+                UrlDbLoc(f'postgresql://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@{host}:{port}'),
             ))
 
         return {s.name: s for s in lst}
