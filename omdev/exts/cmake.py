0272da4ea6e77ca73ca65aa50834acb566f3f36f
wrmsr <timwilloney@gmail.com>
2024-08-29 10:18:21 -0400

    --
    Former-commit-id: 3f192ccd6bb60bbe635e549e81c041270a16af9c

--- omdev/exts/cmake.py
+++ omdev/exts/cmake.py
@@ -9,6 +9,7 @@ TODO:
    - json? https://github.com/nlohmann/json
   - FindPackages? FetchContent? built_ext won't have that
   - move omml git / data retriever stuff into omdev, get just the one header file from git via sha?
+ - support local built pys 
 
 ==
 
@@ -17,198 +18,307 @@ Done:
   - aight, generate a whole cmake subdir with symlinks to src files lol
 
 """  # noqa
+import argparse
+import dataclasses as dc
 import io
 import logging
 import os.path
 import shutil
 import sys
 import sysconfig
+import typing as ta
 
 from omlish import check
+from omlish import lang
 from omlish import logs
 
 from .. import cmake
+from ..scripts import findmagic
 
 
 log = logging.getLogger(__name__)
 
 
+MAGIC = '@omdev-ext'
+MAGIC_COMMENT = f'// {MAGIC}'
+
+FILE_EXTENSIONS = ('c', 'cc', 'cpp')
+
+
+def _sep_str_grps(*ls: ta.Sequence[str]) -> list[str]:
+    o = []
+    for i, l in enumerate(ls):
+        if not l:
+            continue
+        if i:
+            o.append('')
+        o.extend(check.not_isinstance(l, str))
+    return o
+
+
 class CmakeProjectGen:
+    def __init__(
+            self,
+            exts: ta.Sequence[str],
+            prj_root: str | None = None,
+    ) -> None:
+        super().__init__()
+        self._exts = check.not_isinstance(exts, str)
+        self._prj_root = os.path.abspath(prj_root) if prj_root is not None else os.getcwd()
+
+    #
+
+    @property
+    def prj_root(self) -> str:
+        return self._prj_root
+
+    @lang.cached_function
+    def prj_name(self) -> str:
+        return os.path.basename(os.path.dirname(self.prj_root))
+
+    @lang.cached_function
+    def cmake_dir(self) -> str:
+        cmake_dir = os.path.join(self.prj_root, 'cmake')
+        if os.path.exists(cmake_dir):
+            for e in os.listdir(cmake_dir):
+                if e == '.idea':
+                    continue
+                ep = os.path.join(cmake_dir, e)
+                if os.path.isfile(ep):
+                    os.unlink(ep)
+                else:
+                    shutil.rmtree(ep)
+        else:
+            os.mkdir(cmake_dir)
+        return cmake_dir
+
+    #
+
+    def write_git_ignore(self) -> None:
+        with open(os.path.join(self.cmake_dir(), '.gitignore'), 'w') as f:
+            f.write('\n'.join(sorted(['/cmake-*', '/build'])))
+
+    #
+
+    def write_idea_name(self) -> None:
+        idea_dir = os.path.join(self.cmake_dir(), '.idea')
+        if not os.path.isdir(idea_dir):
+            os.mkdir(idea_dir)
+        idea_name_file = os.path.join(idea_dir, '.name')
+        if not os.path.isfile(idea_name_file):
+            with open(idea_name_file, 'w') as f:
+                f.write(self.prj_name())
+
+    #
+
+    @dc.dataclass(frozen=True, kw_only=True)
+    class PyInfo:
+        venv_exe: str
+        venv_root: str
+        real_exe: str
+        root: str
+        suffix: str
+
+    @lang.cached_function
+    def py_info(self) -> PyInfo:
+        venv_exe = sys.executable
+        real_exe = os.path.realpath(venv_exe)
+        return self.PyInfo(
+            venv_exe=venv_exe,
+            venv_root=os.path.abspath(os.path.join(os.path.dirname(venv_exe), '..')),
+            real_exe=real_exe,
+            root=os.path.abspath(os.path.join(os.path.dirname(real_exe), '..')),
+            suffix='.'.join(map(str, sys.version_info[:2])),
+        )
+
+    #
+
+    @lang.cached_function
+    def ext_files(self) -> ta.Sequence[str]:
+        out = []
+        for e in self._exts:
+            e = os.path.abspath(e)
+            if os.path.isfile(e):
+                out.append(e)
+            elif os.path.isdir(e):
+                out.extend(
+                    findmagic.find_magic(
+                        [e],
+                        [MAGIC_COMMENT],
+                        FILE_EXTENSIONS,
+                    ),
+                )
+            else:
+                raise KeyError(e)
+        return out
+
+    #
+
+    class _CmakeListsGen:
+        def __init__(
+                self,
+                p: 'CmakeProjectGen',
+                out: ta.TextIO,
+        ) -> None:
+            super().__init__()
+            self.p = p
+            self.g = cmake.CmakeGen(out)
+
+        @lang.cached_property
+        def var_prefix(self) -> str:
+            return self.p.prj_name().upper()
+
+        @lang.cached_property
+        def py(self) -> 'CmakeProjectGen.PyInfo':
+            return self.p.py_info()
+
+        def _add_ext(self, ext_src: str) -> None:
+            ext_name = ext_src.rpartition('.')[0].replace('/', '__')
+
+            log.info('Adding cmake c extension: %s -> %s', ext_src, ext_name)
+
+            so_name = ''.join([
+                os.path.basename(ext_src).split('.')[0],
+                '.',
+                sysconfig.get_config_var('SOABI'),
+                sysconfig.get_config_var('SHLIB_SUFFIX'),
+            ])
+
+            sl = os.path.join(self.p.cmake_dir(), ext_src)
+            sal = os.path.abspath(sl)
+            sd = os.path.dirname(sal)
+            os.makedirs(sd, exist_ok=True)
+            rp = os.path.relpath(os.path.abspath(ext_src), sd)
+            os.symlink(rp, sal)
+
+            ml = cmake.ModuleLibrary(
+                ext_name,
+                src_files=[
+                    sl,
+                ],
+                include_dirs=[
+                    f'${{{self.var_prefix}_INCLUDE_DIRECTORIES}}',
+                ],
+                compile_opts=[
+                    f'${{{self.var_prefix}_COMPILE_OPTIONS}}',
+                ],
+                link_dirs=[
+                    f'${{{self.var_prefix}_LINK_DIRECTORIES}}',
+                ],
+                link_libs=[
+                    f'${{{self.var_prefix}_LINK_LIBRARIES}}',
+                ],
+                extra_cmds=[
+                    cmake.Command(
+                        'add_custom_command',
+                        ['TARGET', ext_name, 'POST_BUILD'],
+                        [
+                            ' '.join([
+                                'COMMAND ${CMAKE_COMMAND} -E ',
+                                f'copy $<TARGET_FILE_NAME:{ext_name}> ../../{os.path.dirname(ext_src)}/{so_name}',
+                            ]),
+                            'COMMAND_EXPAND_LISTS',
+                        ],
+                    ),
+                ],
+            )
+            self.g.write_target(ml)
+
+        def run(self) -> None:
+            self.g.write(self.g.preamble)
+            self.g.write('')
+
+            self.g.write(f'project({self.p.prj_name()})')
+            self.g.write('')
+
+            self.g.write_var(cmake.Var(
+                f'{self.var_prefix}_INCLUDE_DIRECTORIES',
+                _sep_str_grps(
+                    [f'{self.py.venv_root}/include'],
+                    [f'{self.py.root}/include/python{self.py.suffix}'],
+                    [
+                        # $ENV{HOME}/src/python/cpython
+                        # $ENV{HOME}/src/python/cpython/include
+                    ],
+                ),
+            ))
+
+            self.g.write_var(cmake.Var(
+                f'{self.var_prefix}_COMPILE_OPTIONS',
+                _sep_str_grps(
+                    [
+                        '-Wsign-compare',
+                        '-Wunreachable-code',
+                        '-DNDEBUG',
+                        '-g',
+                        '-fwrapv',
+                        '-O3',
+                        '-Wall',
+                    ],
+                    [
+                        '-g',
+                        '-c',
+                    ],
+                    ['-std=c++20'],
+                ),
+            ))
+
+            self.g.write_var(cmake.Var(
+                f'{self.var_prefix}_LINK_DIRECTORIES',
+                _sep_str_grps(
+                    [f'{self.py.root}/lib'],
+                    # ['$ENV{HOME}/src/python/cpython'],
+                ),
+            ))
+
+            self.g.write_var(cmake.Var(
+                f'{self.var_prefix}_LINK_LIBRARIES',
+                _sep_str_grps(
+                    *([[
+                        '-bundle',
+                        '"-undefined dynamic_lookup"',
+                    ]] if sys.platform == 'darwin' else []),
+                ),
+            ))
+
+            for ext_src in self.p.ext_files():
+                self._add_ext(os.path.relpath(ext_src, self.p.prj_root))
+
+    #
+
     def run(self) -> None:
-        pass
+        if not os.path.isfile(os.path.join(self._prj_root, 'pyproject.toml')):
+            raise Exception('Must be run in project root')
+
+        self.ext_files()
+
+        log.info('Generating cmake project %s', self.prj_name())
+
+        self.cmake_dir()
+        self.write_git_ignore()
+        self.write_idea_name()
+
+        out = io.StringIO()
+        clg = self._CmakeListsGen(self, out)
+        clg.run()
+
+        with open(os.path.join(self.cmake_dir(), 'CMakeLists.txt'), 'w') as f:
+            f.write(out.getvalue())
 
 
 def _main() -> None:
     logs.configure_standard_logging('INFO')
 
-    prj_root = os.path.abspath(os.getcwd())
-    if not os.path.isfile(os.path.join(prj_root, 'pyproject.toml')):
-        raise Exception('Must be run in project root')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('exts', nargs='*')
 
-    cmake_dir = os.path.join(prj_root, 'cmake')
-    if os.path.exists(cmake_dir):
-        for e in os.listdir(cmake_dir):
-            if e == '.idea':
-                continue
-            ep = os.path.join(cmake_dir, e)
-            if os.path.isfile(ep):
-                os.unlink(ep)
-            else:
-                shutil.rmtree(ep)
-    else:
-        os.mkdir(cmake_dir)
+    args = parser.parse_args()
 
-    with open(os.path.join(cmake_dir, '.gitignore'), 'w') as f:
-        f.write('\n'.join(sorted(['/cmake-*', '/build'])))
+    if not args.exts:
+        raise Exception('must specify exts')
 
-    prj_name = os.path.basename(os.path.dirname(prj_root))
-    log.info('Generating cmake project %s', prj_name)
-
-    idea_dir = os.path.join(cmake_dir, '.idea')
-    if not os.path.isdir(idea_dir):
-        os.mkdir(idea_dir)
-    idea_name_file = os.path.join(idea_dir, '.name')
-    if not os.path.isfile(idea_name_file):
-        with open(idea_name_file, 'w') as f:
-            f.write(prj_name)
-
-    venv_exe = sys.executable
-    venv_root = os.path.abspath(os.path.join(os.path.dirname(venv_exe), '..'))
-    real_exe = os.path.realpath(venv_exe)
-    py_root = os.path.abspath(os.path.join(os.path.dirname(real_exe), '..'))
-    py_sfx = '.'.join(map(str, sys.version_info[:2]))
-
-    out = io.StringIO()
-    gen = cmake.CmakeGen(out)
-
-    var_prefix = prj_name.upper()
-
-    gen.write(gen.preamble)
-    gen.write('')
-
-    gen.write(f'project({prj_name})')
-    gen.write('')
-
-    def sep_grps(*ls):
-        # itertools.interleave? or smth?
-        o = []
-        for i, l in enumerate(ls):
-            if not l:
-                continue
-            if i:
-                o.append('')
-            o.extend(check.not_isinstance(l, str))
-        return o
-
-    gen.write_var(cmake.Var(
-        f'{var_prefix}_INCLUDE_DIRECTORIES',
-        sep_grps(
-            [f'{venv_root}/include'],
-            [f'{py_root}/include/python{py_sfx}'],
-            [
-                # $ENV{HOME}/src/python/cpython
-                # $ENV{HOME}/src/python/cpython/include
-            ],
-        ),
-    ))
-
-    gen.write_var(cmake.Var(
-        f'{var_prefix}_COMPILE_OPTIONS',
-        sep_grps(
-            [
-                '-Wsign-compare',
-                '-Wunreachable-code',
-                '-DNDEBUG',
-                '-g',
-                '-fwrapv',
-                '-O3',
-                '-Wall',
-            ],
-            [
-                '-g',
-                '-c',
-            ],
-            ['-std=c++20'],
-        ),
-    ))
-
-    gen.write_var(cmake.Var(
-        f'{var_prefix}_LINK_DIRECTORIES',
-        sep_grps(
-            [f'{py_root}/lib'],
-            # ['$ENV{HOME}/src/python/cpython'],
-        ),
-    ))
-
-    gen.write_var(cmake.Var(
-        f'{var_prefix}_LINK_LIBRARIES',
-        sep_grps(
-            *([[
-                '-bundle',
-                '"-undefined dynamic_lookup"',
-            ]] if sys.platform == 'darwin' else []),
-        ),
-    ))
-
-    for ext_src in [
-        'omdev/exts/_boilerplate.cc',
-        'x/dev/c/junk.cc',
-        'x/dev/c/_uuid.cc',
-        'x/dev/c/csv/_csvloader.cc',
-    ]:
-        ext_name = ext_src.rpartition('.')[0].replace('/', '__')
-
-        log.info('Adding cmake c extension: %s -> %s', ext_src, ext_name)
-
-        so_name = ''.join([
-            os.path.basename(ext_src).split('.')[0],
-            '.',
-            sysconfig.get_config_var('SOABI'),
-            sysconfig.get_config_var('SHLIB_SUFFIX'),
-        ])
-
-        sl = os.path.join(cmake_dir, ext_src)
-        sal = os.path.abspath(sl)
-        sd = os.path.dirname(sal)
-        os.makedirs(sd, exist_ok=True)
-        rp = os.path.relpath(os.path.abspath(ext_src), sd)
-        os.symlink(rp, sal)
-
-        gen.write_target(cmake.ModuleLibrary(
-            ext_name,
-            src_files=[
-                sl,
-            ],
-            include_dirs=[
-                f'${{{var_prefix}_INCLUDE_DIRECTORIES}}',
-            ],
-            compile_opts=[
-                f'${{{var_prefix}_COMPILE_OPTIONS}}',
-            ],
-            link_dirs=[
-                f'${{{var_prefix}_LINK_DIRECTORIES}}',
-            ],
-            link_libs=[
-                f'${{{var_prefix}_LINK_LIBRARIES}}',
-            ],
-            extra_cmds=[
-                cmake.Command(
-                    'add_custom_command',
-                    ['TARGET', ext_name, 'POST_BUILD'],
-                    [
-                        ' '.join([
-                            'COMMAND ${CMAKE_COMMAND} -E ',
-                            f'copy $<TARGET_FILE_NAME:{ext_name}> ../../{os.path.dirname(ext_src)}/{so_name}',
-                        ]),
-                        'COMMAND_EXPAND_LISTS',
-                    ],
-                ),
-            ],
-        ))
-
-    # print(out.getvalue())
-    with open(os.path.join(cmake_dir, 'CMakeLists.txt'), 'w') as f:
-        f.write(out.getvalue())
+    cpg = CmakeProjectGen(args.exts)
+    cpg.run()
 
 
 if __name__ == '__main__':
