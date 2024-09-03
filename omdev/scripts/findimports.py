0b42e795e9a4a6f646f7fce0b874cd1773d47747
wrmsr <timwilloney@gmail.com>
2024-09-03 15:09:36 -0400

    --
    Former-commit-id: f73c05fabee9b21bcf71d71ae2d95134db9165bd

--- omdev/scripts/findimports.py
+++ omdev/scripts/findimports.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# @omlish-script
 """
 TODO:
  - multiple commands:
@@ -9,52 +10,77 @@ import importlib.machinery
 import importlib.util
 import os.path
 import sys
+import typing as ta
 
 
 _BUILTIN_MODULE_NAMES = frozenset([*sys.builtin_module_names, *sys.stdlib_module_names])
 
 
-def _main() -> None:
-    def handle(fp: str) -> None:
-        def rec(n):
-            nodes.append(n)
-            for c in ast.iter_child_nodes(n):
-                rec(c)
-        # if not os.path.isfile(os.path.join(os.path.dirname(fp), '__init__.py')):
-        #     return
-        with open(fp, 'r') as f:
-            buf = f.read()
-        nodes: list[ast.AST] = []
-        rec(ast.parse(buf))
-        imps.update(na.name for i in nodes if isinstance(i, ast.Import) for na in i.names)
-        imps.update(i.module for i in nodes if isinstance(i, ast.ImportFrom) if i.module and not i.level)
+def whichmod(i: str) -> ta.Literal['bad', 'builtin', 'dep']:
+    try:
+        l = importlib.util.find_spec(i)
+    except (ImportError, ValueError):
+        return 'bad'
 
+    if not isinstance(l, importlib.machinery.ModuleSpec) or not l.origin:
+        return 'bad'
+
+    # if l.origin.startswith(sys.base_prefix) or l.origin == 'frozen':
+    #     return 'builtin'
+
+    if i in _BUILTIN_MODULE_NAMES:
+        return 'builtin'
+
+    return 'dep'
+
+
+def yield_imports(fp: str) -> set[str]:
+    # if not os.path.isfile(os.path.join(os.path.dirname(fp), '__init__.py')):
+    #     return
+
+    with open(fp, 'r') as f:
+        buf = f.read()
+
+    nodes: list[ast.AST] = []
+
+    def rec(n):
+        nodes.append(n)
+        for c in ast.iter_child_nodes(n):
+            rec(c)
+
+    rec(ast.parse(buf))
+
+    return {
+        *(na.name for i in nodes if isinstance(i, ast.Import) for na in i.names),
+        *(i.module for i in nodes if isinstance(i, ast.ImportFrom) if i.module and not i.level),
+    }
+
+
+def find_imports(*rootps: str) -> set[str]:
     imps: set[str] = set()
-    for rootp in sys.argv[1:]:
+
+    for rootp in rootps:
         if os.path.isfile(rootp):
             if rootp.endswith('.py'):
-                handle(os.path.join(os.path.dirname(rootp), os.path.basename(rootp)))
+                imps.update(yield_imports(os.path.join(os.path.dirname(rootp), os.path.basename(rootp))))
+
         else:
             for dp, dns, fns in os.walk(os.path.expanduser(rootp)):  # noqa
                 for fn in fns:
                     if fn.endswith('.py'):
-                        handle(os.path.join(dp, fn))
+                        imps.update(yield_imports(os.path.join(dp, fn)))
 
-    def whichmod(i: str) -> str:
-        try:
-            l = importlib.util.find_spec(i)
-        except (ImportError, ValueError):
-            return 'bad'
-        if not isinstance(l, importlib.machinery.ModuleSpec) or not l.origin:
-            return 'bad'
-        # if l.origin.startswith(sys.base_prefix) or l.origin == 'frozen':
-        #     return 'builtin'
-        if i in _BUILTIN_MODULE_NAMES:
-            return 'builtin'
-        return 'dep'
+    return imps
 
+
+def get_import_deps(imps: set[str]) -> set[str]:
     eimps = {n for n in imps for n in [n.split('.')[0]] if n not in sys.builtin_module_names}
-    deps = {i for i in eimps if whichmod(i) != 'builtin'}
+    return {i for i in eimps if whichmod(i) != 'builtin'}
+
+
+def _main() -> None:
+    imps = find_imports(*sys.argv[1:])
+    deps = get_import_deps(imps)
     print(chr(10).join(sorted(deps)))
 
 
--- omdev/scripts/traceimport.py
+++ omdev/scripts/traceimport.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# @omlish-script
 """
 TODO:
  - hoist first stacktrace file to a full field
