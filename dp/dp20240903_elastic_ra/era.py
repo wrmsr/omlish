0a28adcfd2da981a97269b1c9761d89ebf8f92bf
wrmsr <timwilloney@gmail.com>
2024-09-03 16:21:56 -0400

    --
    Former-commit-id: 6d7ee2004110d9e34b2463374a8ec3961c87e8e2

--- dp/dp20240903_elastic_ra/era.py
+++ dp/dp20240903_elastic_ra/era.py
@@ -1,5 +1,13 @@
+import os.path
+import yaml
+
+
+with open(os.path.expanduser('~/Dropbox/.dotfiles/secrets.yml')) as f:
+    os.environ['TAVILY_API_KEY'] = yaml.safe_load(f)['tavily_api_key']
+
+
 def _main():
-    pass
+    local_llm = 'llama3'
 
 
 if __name__ == '__main__':
