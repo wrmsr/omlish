aef4628dd12f60312f0b9a78e71a8228eb99cdf2
wrmsr <timwilloney@gmail.com>
2024-09-03 10:06:41 -0400

    --
    Former-commit-id: cd1c0aeb51a52ba28ef246a5e38cd76dfb5f7c88

---
+++ x/funcs.py
@@ -0,0 +1,101 @@
+import abc
+import operator
+import typing as ta
+
+
+T = ta.TypeVar('T')
+U = ta.TypeVar('U')
+
+
+class Func(abc.ABC, ta.Generic[T]):
+    @abc.abstractmethod
+    def __call__(self, *args: ta.Any, **kwargs: ta.Any) -> T:
+        raise NotImplementedError
+
+    def pipe(self, fn: ta.Callable[..., U], *args: ta.Any, **kwargs: ta.Any) -> 'Func[U]':
+        return Pipe([self], Bind(fn, *args, **kwargs))
+
+    def __or__(self, fn: ta.Callable[..., U]) -> 'Func[U]':
+        return self.pipe(fn)
+
+    def apply(self, fn: ta.Callable, *args: ta.Any, **kwargs: ta.Any) -> 'Func[T]':
+        return Pipe([self], Apply(Bind(fn, *args, **kwargs)))
+
+    def __and__(self, fn: ta.Callable[..., U]) -> 'Func[U]':
+        return self.apply(fn)
+
+
+class Bind(Func[T]):
+    def __init__(self, fn: ta.Callable[..., T], *args: ta.Any, **kwargs: ta.Any) -> None:
+        super().__init__()
+        if Ellipsis not in args and Ellipsis not in kwargs:
+            args += (Ellipsis,)
+        self._fn = fn
+        self._args = args
+        self._kwargs = kwargs
+
+    def __call__(self, *args: ta.Any, **kwargs: ta.Any) -> T:
+        fa = []
+        for a in self._args:
+            if a is Ellipsis:
+                fa.extend(args)
+            else:
+                fa.append(a)
+
+        fkw = {}
+        for k, v in self._kwargs.items():
+            if v is Ellipsis:
+                if len(args) != 1:
+                    raise ValueError(args)
+                fkw[k] = args[0]
+            else:
+                fkw[k] = v
+        fkw.update(kwargs)
+
+        return self._fn(*fa, **fkw)
+
+
+class Pipe(Func[T]):
+    def __init__(self, lfns: ta.Sequence[ta.Callable], rfn: ta.Callable[..., T]) -> None:
+        super().__init__()
+        self._lfn, *self._rfns = [*lfns, rfn]
+
+    def __call__(self, *args: ta.Any, **kwargs: ta.Any) -> T:
+        o = self._lfn(*args, **kwargs)
+        for fn in self._rfns:
+            o = fn(o)
+        return o
+
+
+class Apply(Func[T]):
+    def __init__(self, *fns: ta.Callable[[T], ...]) -> None:
+        super().__init__()
+        self._fns = fns
+
+    def __call__(self, o: ta.Any) -> T:  # noqa
+        for fn in self._fns:
+            fn(o)
+        return o
+
+
+def _main() -> None:
+    assert Bind(operator.add, 1)(2) == 3
+
+    assert Bind(operator.add, 1).\
+        pipe(operator.mul, 2)\
+        (2) == 6
+
+    assert Bind(operator.add, 1).\
+        apply(print).\
+        pipe(operator.mul, 2).\
+        apply(print)\
+        (2) == 6
+
+    assert (Bind(operator.add, 1) & print | Bind(operator.mul, 2) & print)(2) == 6
+
+    assert Bind(operator.truediv, 2)(4) == .5
+    assert Bind(operator.truediv, ..., 2)(4) == 2.
+
+
+if __name__ == '__main__':
+    _main()
