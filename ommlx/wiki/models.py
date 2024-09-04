6ec85e8fc149bd1c4731190223ea33bbf111bda8
wrmsr <timwilloney@gmail.com>
2024-09-04 13:52:11 -0400

    --
    Former-commit-id: bb40623a14ee833873255044d55fab7a48304b40

--- ommlx/wiki/models.py
+++ ommlx/wiki/models.py
@@ -1,17 +1,8 @@
 import dataclasses as dc
 import typing as ta
 
-from . import xml
-
 
 T = ta.TypeVar('T')
-
-
-##
-
-
-def symm_dct(*ks: T) -> ta.Mapping[T, T]:
-    return {k: k for k in ks}
 
 
 ##
@@ -24,34 +15,12 @@ class Namespace:
     text: str
 
 
-parse_namespace = xml.ElementToObj(
-    Namespace,
-    xml.ElementToKwargs(
-        attrs={
-            'key': ('key', int),
-            'case': 'case',
-        },
-        text='text',
-    ),
-)
-
-
 #
 
 
 @dc.dataclass(frozen=True)
 class Namespaces:
     namespaces: ta.Sequence[Namespace] | None = None
-
-
-parse_namespaces = xml.ElementToObj(
-    Namespaces,
-    xml.ElementToKwargs(
-        list_children={
-            'namespace': ('namespaces', parse_namespace),
-        },
-    ),
-)
 
 
 #
@@ -67,39 +36,12 @@ class SiteInfo:
     namespaces: Namespaces
 
 
-parse_site_info = xml.ElementToObj(
-    SiteInfo,
-    xml.ElementToKwargs(
-        scalars=symm_dct(
-            'sitename',
-            'dbname',
-            'base',
-            'generator',
-            'case',
-        ),
-        single_children={
-            'namespaces': ('namespaces', parse_namespaces),
-        },
-    ),
-)
-
-
 #
 
 
 @dc.dataclass(frozen=True)
 class Redirect:
     title: str
-
-
-parse_redirect = xml.ElementToObj(
-    Redirect,
-    xml.ElementToKwargs(
-        attrs=symm_dct(
-            'title',
-        ),
-    ),
-)
 
 
 #
@@ -113,29 +55,6 @@ class Contributor:
     deleted: bool | None = None
 
 
-def _parse_deleted(s: str) -> bool:
-    if s != 'deleted':
-        raise ValueError(s)
-    return True
-
-
-parse_contributor = xml.ElementToObj(
-    Contributor,
-    xml.ElementToKwargs(
-        attrs={
-            'deleted': ('deleted', _parse_deleted),
-        },
-        scalars={
-            **symm_dct(
-                'username',
-                'ip',
-            ),
-            'id': ('id', int),
-        },
-    ),
-)
-
-
 #
 
 
@@ -144,19 +63,6 @@ class RevisionText:
     bytes: int
     sha1: str
     text: str
-
-
-parse_revision_text = xml.ElementToObj(
-    RevisionText,
-    xml.ElementToKwargs(
-        attrs={
-            'bytes': ('bytes', int),
-            'sha1': 'sha1',
-            'space': None,
-        },
-        text='text',
-    ),
-)
 
 
 #
@@ -175,32 +81,6 @@ class Revision:
     format: str | None = None
     text: RevisionText | None = None
     sha1: str | None = None
-
-
-parse_revision = xml.ElementToObj(
-    Revision,
-    xml.ElementToKwargs(
-        scalars={
-            'id': ('id', int),
-            'parentid': ('parentid', int),
-            'origin': ('origin', int),
-            'minor': ('minor', lambda _: True),
-            **symm_dct(
-                'timestamp',
-                'comment',
-                'model',
-                'format',
-                'sha1',
-            ),
-        },
-        single_children={
-            'text': ('text', parse_revision_text),
-        },
-        list_children={
-            'contributor': ('contributors', parse_contributor),
-        },
-    ),
-)
 
 
 #
@@ -228,27 +108,6 @@ class Page:
     restrictions: str | None = None
     revisions: ta.Sequence[Revision] | None = None
     uploads: ta.Sequence[Upload] | None = None
-
-
-parse_page = xml.ElementToObj(
-    Page,
-    xml.ElementToKwargs(
-        scalars={
-            'ns': ('ns', int),
-            'id': ('id', int),
-            **symm_dct(
-                'title',
-                'restrictions',
-            ),
-        },
-        single_children={
-            'redirect': ('redirect', parse_redirect),
-        },
-        list_children={
-            'revision': ('revisions', parse_revision),
-        },
-    ),
-)
 
 
 #
