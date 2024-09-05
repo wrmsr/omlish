a8c2f47bc4014b5a9dd80aec898bb8bf5100c8fc
wrmsr <timwilloney@gmail.com>
2024-09-05 14:30:32 -0400

    --
    Former-commit-id: 2f6775634491ccbdbe2d883efb60cec8b1241ad4

--- x/_omdev_cext/setup.py
+++ x/_omdev_cext/setup.py
@@ -6,6 +6,7 @@ st.setup(
         st.Extension(
             name='omdev.cexts._boilerplate',
             sources=['omdev/cexts/_boilerplate.cc'],
+            extra_compile_args=['-std=c++20'],
         ),
     ]
 )
