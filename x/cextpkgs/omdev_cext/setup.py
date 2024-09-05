33917d739900fe05085cfb26a71e877b7e671224
wrmsr <timwilloney@gmail.com>
2024-09-05 14:52:55 -0400

    --
    Former-commit-id: d7c6835dfd6fd4994417cd1cdcb85ca5c7f11d72

--- x/cextpkgs/omdev_cext/setup.py
+++ x/cextpkgs/omdev_cext/setup.py
@@ -4,8 +4,8 @@ import setuptools as st
 st.setup(
     ext_modules=[
         st.Extension(
-            name='omdev.cexts._boilerplate',
-            sources=['omdev/cexts/_boilerplate.cc'],
+            name='omdev.cexts._cext._boilerplate',
+            sources=['omdev/cexts/_cext.boilerplate.cc'],
             extra_compile_args=['-std=c++20'],
         ),
     ]
