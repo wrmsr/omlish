7da5d839dae478e47661e0f7a5180d40ca4942ff
wrmsr <timwilloney@gmail.com>
2024-09-05 12:23:08 -0400

    --
    Former-commit-id: 99d8d7a15d2da812ec75be2770cac3ad50ef2439

--- x/formats/json/json.py
+++ x/formats/json/json.py
@@ -391,3 +391,129 @@ class RapidjsonBackend(Backend):
 #
 # dumps_pretty: Dumps = functools.partial(dumps, **PROVIDER.pretty_kwargs)
 # dumpb_pretty: Dumpb = functools.partial(dumpb, **PROVIDER.pretty_kwargs)
+
+import dataclasses as dc
+import enum
+
+import rapidjson as rj
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class WriteMode(enum.Enum):
+    compact: bool = False
+    pretty: bool = False  # use PrettyWriter
+    single_line_array: bool = False  # format arrays on a single line
+
+    def __int__(self) -> int:
+        return (
+            (rj.WM_COMPACT if self.compact else 0) |
+            (rj.WM_PRETTY if self.pretty else 0) |
+            (rj.WM_SINGLE_LINE_ARRAY if self.single_line_array else 0)
+        )
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class NumberMode:
+    none: bool = False
+    decimal: bool = False  # serialize Decimal instances, deserialize floats as Decimal
+    nan: bool = False  # allow "not-a-number" values
+    native: bool = False  # use faster native C library number handling
+
+    def __int__(self) -> int:
+        return (
+            (rj.NM_NONE if self.none else 0) |
+            (rj.NM_DECIMAL if self.decimal else 0) |
+            (rj.NM_NAN if self.nan else 0) |
+            (rj.NM_NATIVE if self.native else 0)
+        )
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class DatetimeMode:
+    none: bool = False
+
+    # formats
+    iso8601: bool = False  # bidirectional ISO8601 for datetimes, dates and times
+    unix_time: bool = False  # serialization only, "Unix epoch"-based number of seconds
+
+    # options
+    only_seconds: bool = False  # truncate values to the whole second, ignoring micro seconds
+    ignore_tz: bool = False  # ignore timezones
+    naive_is_utc: bool = False  # assume naive datetime are in UTC timezone
+    shift_to_utc: bool = False  # shift to/from UTC
+
+    def __int__(self) -> int:
+        return (
+            (rj.DM_NONE if self.none else 0) |
+            (rj.DM_ISO8601 if self.iso8601 else 0) |
+            (rj.DM_UNIX_TIME if self.unix_time else 0) |
+            (rj.DM_ONLY_SECONDS if self.only_seconds else 0) |
+            (rj.DM_IGNORE_TZ if self.ignore_tz else 0) |
+            (rj.DM_NAIVE_IS_UTC if self.naive_is_utc else 0) |
+            (rj.DM_SHIFT_TO_UTC if self.shift_to_utc else 0)
+        )
+
+@dc.dataclass(frozen=True, kw_only=True)
+class UuidMode:
+    none: bool = False
+    canonical: bool = False
+    hex: bool = False
+
+    def __int__(self) -> int:
+        return (
+            (rj.UM_NONE if self.none else 0) |
+            (rj.UM_CANONICAL if self.canonical else 0) |
+            (rj.UM_HEX if self.hex else 0)
+        )
+
+@dc.dataclass(frozen=True, kw_only=True)
+class BytesMode:
+    none: bool = False
+    utf8: bool = False
+
+    def __int__(self) -> int:
+        return (
+            (rj.BM_NONE if self.none else 0) |
+            (rj.BM_UTF8 if self.utf8 else 0)
+        )
+
+@dc.dataclass(frozen=True, kw_only=True)
+class IterableMode:
+    any_iterable: bool = False
+    only_lists: bool = False
+
+    def __int__(self) -> int:
+        return (
+            (rj.IM_ANY_ITERABLE if self.any_iterable else 0) |
+            (rj.IM_ONLY_LISTS if self.only_lists else 0)
+        )
+
+@dc.dataclass(frozen=True, kw_only=True)
+class MappingMode:
+    any_mapping: bool = False
+    only_dicts: bool = False
+    coerce_keys_to_strings: bool = False
+    skip_non_string_keys: bool = False
+    sort_keys: bool = False
+
+    def __int__(self) -> int:
+        return (
+            (rj.MM_ANY_MAPPING if self.any_mapping else 0) |
+            (rj.MM_ONLY_DICTS if self.only_dicts else 0) |
+            (rj.MM_COERCE_KEYS_TO_STRINGS if self.coerce_keys_to_strings else 0) |
+            (rj.MM_SKIP_NON_STRING_KEYS if self.skip_non_string_keys else 0) |
+            (rj.MM_SORT_KEYS if self.sort_keys else 0)
+        )
+
+@dc.dataclass(frozen=True, kw_only=True)
+class ParseMode:
+    none: bool = False
+    comments: bool = False
+    trailing_commas: bool = False
+
+    def __int__(self) -> int:
+        return (
+            (rj.PM_NONE if self.none else 0) |
+            (rj.PM_COMMENTS if self.comments else 0) |
+            (rj.PM_TRAILING_COMMAS if self.trailing_commas else 0)
+        )
