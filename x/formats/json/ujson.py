27005578477d5ef7ae1174020a9a4f11b9bf87c2
wrmsr <timwilloney@gmail.com>
2024-09-05 13:49:53 -0400

    --
    Former-commit-id: 7ce260efd1e690f2d1a16958e9bb39a9780d9e31

--- x/formats/json/ujson.py
+++ x/formats/json/ujson.py
@@ -1,14 +1,24 @@
+"""
+load(fp: File) -> ta.Any
+loads(s: str | bytes | bytearray) -> ta.Any
+dump(obj: ta.Any, fp: File, **DumpOpts) -> None
+dumps(obj: ta.Any, **DumpOpts) -> None
+"""
 import dataclasses as dc
 
 
 @dc.dataclass(frozen=True, kw_only=True)
 class DumpOpts:
-    ensure_ascii
-    encode_html_chars
-    escape_forward_slashes
-    sort_keys
-    indent
-    allow_nan
-    reject_bytes
-    default
-    separators
+    """
+    https://github.com/ultrajson/ultrajson/blob/2e4aba339b0ab08f893590aaa989d12846afc5d6/python/objToJSON.c#L662
+    """
+
+    ensure_ascii: bool = True  # limits output to ASCII and escapes all extended characters above 127.
+    encode_html_chars: bool = False  # enables special encoding of "unsafe" HTML characters into safer Unicode sequences
+    escape_forward_slashes: bool = True  # whether forward slashes (/) are escaped
+    indent: int = 0
+    sort_keys: bool = False
+    allow_nan: bool = True
+    reject_bytes: bool = True
+    default: ta.Callable[[ta.Any], ta.Any] | None = None  # should return a serializable version of obj or raise TypeError  # noqa
+    separators: tuple[str, str] | None = None
