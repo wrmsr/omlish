519ed6ee13a6329bbeef22044cb11fd86defa277
wrmsr <timwilloney@gmail.com>
2024-09-06 16:15:45 -0400

    --
    Former-commit-id: 953598a4e53bc26925ad5b38459401a7f7f007bc

--- dp/dp20240312_dlcb_1_movies/1_movies.py
+++ dp/dp20240312_dlcb_1_movies/1_movies.py
@@ -1,6 +1,8 @@
+import dataclasses as dc
 import os.path
 import pprint as pp
 import random
+import typing as ta
 
 import numpy as np
 import sklearn.svm
@@ -13,14 +15,16 @@ from . import keras as mk  # noqa
 from . import torch as mt  # noqa
 
 
-def _main() -> None:
-    random.seed(5)
+##
 
-    ##
 
-    cache_dir = os.path.join(os.path.dirname(__file__), 'data')
-    data = MoviesData(load_movies(cache_dir))
+@dc.dataclass(frozen=True)
+class Embeddings:
+    movies: np.ndarray  # (len(data.movies), embedding_size)
+    links: np.ndarray  # (len(data.top_links), embedding_size)
 
+
+def build_embeddings(data: MoviesData) -> Embeddings:
     # be = mk
     be = mt
 
@@ -33,7 +37,7 @@ def _main() -> None:
 
     elif be is mt:
         model = mt.make_embedding_model(data)
-        mt.train_embedding_model(model, data, lr=.01)
+        mt.train_embedding_model(model, data, lr=.01, num_epochs=10)
 
         movie_weights = model.movie_embedding.weight.detach().cpu().numpy()
         link_weights = model.link_embedding.weight.detach().cpu().numpy()
@@ -41,28 +45,106 @@ def _main() -> None:
     else:
         raise TypeError(be)
 
-    ##
-
     movie_lengths = np.linalg.norm(movie_weights, axis=1)
     normalized_movies = (movie_weights.T / movie_lengths).T
-
-    def similar_movies(movie, n=10):
-        dists = np.dot(normalized_movies, normalized_movies[data.movie_to_idx[movie]])
-        return np.argsort(dists)[-n:][::-1]
-
-    pp.pprint(similar_movies('Rogue One'))
-    print()
-
-    ##
 
     link_lengths = np.linalg.norm(link_weights, axis=1)
     normalized_links = (link_weights.T / link_lengths).T
 
-    def similar_links(link, n=10):
-        dists = np.dot(normalized_links, normalized_links[data.link_to_idx[link]])
-        return np.argsort(dists)[-n:][::-1]
+    return Embeddings(
+        normalized_movies,
+        normalized_links,
+    )
 
-    pp.pprint(similar_links('George Lucas'))
+
+##
+
+
+def find_closest(emb: np.ndarray, idx: int, n: int = 10) -> list[tuple[int, float]]:
+    dists = np.dot(emb, emb[idx])
+    closest = np.argsort(dists)[-n:]
+    return [
+        (c, dists[c])
+        for c in reversed(closest)
+    ]
+
+
+##
+
+
+def build_movie_recommendations(
+        data: MoviesData,
+        embeddings: Embeddings,
+        best: ta.Sequence[str],
+        worst: ta.Sequence[str],
+) -> np.ndarray:  # (len(data.movies),)
+    y = np.asarray(
+        [1 for _ in best] +
+        [0 for _ in worst]
+    )
+
+    x = np.asarray([
+        embeddings.movies[data.movie_to_idx[movie]]
+        for movie in [*best, *worst]
+    ])
+
+    clf = sklearn.svm.SVC(kernel='linear')  # noqa
+    clf.fit(x, y)
+
+    return clf.decision_function(embeddings.movies)
+
+
+##
+
+
+# def train_linear_model(
+#         x: np.ndarray,
+#         y: np.ndarray,
+#         *,
+#         training_cutoff_ratio: float = .8,
+# ) -> sklearn.linear_model._base.LinearModel:  # noqa
+#     training_cut_off = int(len(x) * training_cutoff_ratio)
+#     regr = sklearn.linear_model.LinearRegression()
+#     regr.fit(x[:training_cut_off], y[:training_cut_off])
+#     return regr
+
+
+#
+
+
+def _main() -> None:
+    random.seed(5)
+
+    ##
+
+    cache_dir = os.path.join(os.path.dirname(__file__), 'data')
+    data = MoviesData(load_movies(cache_dir))
+
+    ##
+
+    embeddings = build_embeddings(data)
+    print()
+
+    ##
+
+    target_movie = 'Rogue One'
+    pp.pprint([
+        (data.movies[c].name, d) for c, d in find_closest(
+            embeddings.movies,
+            data.movie_to_idx[target_movie],
+        )
+    ])
+    print()
+
+    #
+
+    target_link = 'George Lucas'
+    pp.pprint([
+        (data.top_links[c], d) for c, d in find_closest(
+            embeddings.links,
+            data.link_to_idx[target_link],
+        )
+    ])
     print()
 
     ##
@@ -87,17 +169,14 @@ def _main() -> None:
         'Serena (2014 film)',
         'Vacation (2015 film)',
     ]
-    y = np.asarray([1 for _ in best] + [0 for _ in worst])
-    X = np.asarray([normalized_movies[data.movie_to_idx[movie]] for movie in best + worst])
-    pp.pprint(X.shape)
-    print()
 
-    clf = sklearn.svm.SVC(kernel='linear')
-    clf.fit(X, y)
+    estimated_movie_ratings = build_movie_recommendations(
+        data,
+        embeddings,
+        best,
+        worst,
+    )
 
-    ##
-
-    estimated_movie_ratings = clf.decision_function(normalized_movies)
     best = np.argsort(estimated_movie_ratings)
     print('best:')
     for c in reversed(best[-5:]):
@@ -111,14 +190,22 @@ def _main() -> None:
 
     ##
 
-    rotten_y = np.asarray([float(movie.rat_pct[:-1]) / 100 for movie in data.movies if movie.rat_pct])
-    rotten_X = np.asarray([normalized_movies[data.movie_to_idx[movie.name]] for movie in data.movies if movie.rat_pct])
+    rotten_y = np.asarray([
+        float(movie.rat_pct[:-1]) / 100
+        for movie in data.movies
+        if movie.rat_pct
+    ])
+    rotten_x = np.asarray([
+        embeddings.movies[data.movie_to_idx[movie.name]]
+        for movie in data.movies
+        if movie.rat_pct
+    ])
 
-    training_cut_off = int(len(rotten_X) * 0.8)
+    training_cut_off = int(len(rotten_x) * 0.8)
     regr = sklearn.linear_model.LinearRegression()
-    regr.fit(rotten_X[:training_cut_off], rotten_y[:training_cut_off])
+    regr.fit(rotten_x[:training_cut_off], rotten_y[:training_cut_off])
 
-    error = (regr.predict(rotten_X[training_cut_off:]) - rotten_y[training_cut_off:])
+    error = (regr.predict(rotten_x[training_cut_off:]) - rotten_y[training_cut_off:])
     print('mean square error %2.2f' % np.mean(error ** 2))
 
     error = (np.mean(rotten_y[:training_cut_off]) - rotten_y[training_cut_off:])
@@ -156,15 +243,19 @@ def _main() -> None:
     ##
 
     gross_y = np.asarray([gr for gr in movie_gross if gr])
-    gross_X = np.asarray([normalized_movies[data.movie_to_idx[movie.name]] for movie, gr in zip(data.movies, movie_gross) if gr])
+    gross_x = np.asarray([
+        embeddings.movies[data.movie_to_idx[movie.name]]
+        for movie, gr in zip(data.movies, movie_gross)
+        if gr
+    ])
 
-    training_cut_off = int(len(gross_X) * 0.8)
+    training_cut_off = int(len(gross_x) * 0.8)
     regr = sklearn.linear_model.LinearRegression()
-    regr.fit(gross_X[:training_cut_off], gross_y[:training_cut_off])
+    regr.fit(gross_x[:training_cut_off], gross_y[:training_cut_off])
 
     ##
 
-    error = (regr.predict(gross_X[training_cut_off:]) - gross_y[training_cut_off:])
+    error = (regr.predict(gross_x[training_cut_off:]) - gross_y[training_cut_off:])
     print('mean square error %2.2f' % np.mean(error ** 2))
 
     error = (np.mean(gross_y[:training_cut_off]) - gross_y[training_cut_off:])
