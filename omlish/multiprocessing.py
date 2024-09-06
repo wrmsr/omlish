979c799502b2fec70909e25c2087f2a3849e557e
wrmsr <timwilloney@gmail.com>
2024-09-06 08:43:41 -0400

    --
    Former-commit-id: 824d2230a980a7ea4a1c23d083acfd008d0f2194

--- omlish/multiprocessing.py
+++ omlish/multiprocessing.py
@@ -35,6 +35,7 @@ class ExtrasSpawnPosixPopen(mp.popen_spawn_posix.Popen):
             for fd in self.__extra_fds:
                 self.duplicate_for_child(fd)
             self._extra_fds = None
+
         super()._launch(process_obj)  # type: ignore  # noqa
 
 
@@ -44,7 +45,10 @@ class ExtrasSpawnProcess(mp.context.SpawnProcess):
         super().__init__(*args, **kwargs)
 
     def _Popen(self, process_obj: 'ExtrasSpawnProcess') -> ExtrasSpawnPosixPopen:  # type: ignore  # noqa
-        return ExtrasSpawnPosixPopen(process_obj, extras=self.__extras)
+        return ExtrasSpawnPosixPopen(
+            check.isinstance(process_obj, ExtrasSpawnProcess),
+            extras=self.__extras,
+        )
 
     def run(self) -> None:
         if self.__extras.deathsig is not None and sys.platform == 'linux':
@@ -55,8 +59,8 @@ class ExtrasSpawnProcess(mp.context.SpawnProcess):
 
 class ExtrasSpawnContext(mp.context.SpawnContext):
     def __init__(self, extras: SpawnExtras = SpawnExtras()) -> None:
-        super().__init__()
         self.__extras = extras
+        super().__init__()
 
     def Process(self, *args: ta.Any, **kwargs: ta.Any):  # type: ignore  # noqa
         return ExtrasSpawnProcess(*args, extras=self.__extras, **kwargs)
