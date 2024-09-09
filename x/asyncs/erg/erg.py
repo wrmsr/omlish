b8187e7df4e325fdb04d510978b6f113fa308f26
wrmsr <timwilloney@gmail.com>
2024-09-09 11:45:24 -0400

    --
    Former-commit-id: 2c3a6e1f81b67665e78b3ef0a60a0d3b1794f19d

--- x/asyncs/erg/erg.py
+++ x/asyncs/erg/erg.py
@@ -1,9 +1,11 @@
 import abc
 import dataclasses as dc
+import enum
 import itertools
 import time
 import typing as ta
 
+import anyio.abc
 import anyio.streams.stapled
 
 from omlish.asyncs import anyio as aiu
@@ -23,8 +25,17 @@ class Pid:
 
 
 @dc.dataclass(frozen=True)
+class MailboxMessage:
+    src: Pid
+    msg: ta.Any
+
+
+##
+
+
+@dc.dataclass(frozen=True)
 class ProcessMailbox:
-    main: aiu.StapledObjectStream
+    main: aiu.MemoryStapledObjectStream
 
 
 class ProcessBehavior(abc.ABC):
@@ -37,6 +48,15 @@ class ProcessBehavior(abc.ABC):
 
     async def process_terminate(self, e: Exception | None) -> None:
         pass
+
+
+class ProcessState(enum.Enum):
+    INIT = enum.auto()
+    SLEEP = enum.auto()
+    RUN = enum.auto()
+    WAIT_RESPONSE = enum.auto()
+    TERMINATED = enum.auto()
+    ZOMBIE = enum.auto()
 
 
 class Process(abc.ABC):
@@ -55,6 +75,11 @@ class Process(abc.ABC):
     def mailbox(self) -> ProcessMailbox:
         raise NotImplementedError
 
+    @property
+    @abc.abstractmethod
+    def state(self) -> ProcessState:
+        raise NotImplementedError
+
 
 class ProcessImpl(Process):
     def __init__(
@@ -70,6 +95,8 @@ class ProcessImpl(Process):
         self._behavior = behavior
         self._mailbox = mailbox
 
+        self._state = ProcessState.INIT
+
     @property
     def pid(self) -> Pid:
         return self._pid
@@ -81,6 +108,33 @@ class ProcessImpl(Process):
     @property
     def mailbox(self) -> ProcessMailbox:
         return self._mailbox
+
+    @property
+    def state(self) -> ProcessState:
+        return self._state
+
+    async def _run(self) -> None:
+        if self._state != ProcessState.SLEEP:
+            return
+        self._state = ProcessState.RUN
+
+        async def _inner():
+            while True:
+                await self._behavior.process_run()
+
+                if self._state != ProcessState.RUN:
+                    raise Exception('Killed')
+                self._state = ProcessState.SLEEP
+
+                if not self._mailbox.main.receive_stream.statistics().current_buffer_used:
+                    break
+
+                if self._state != ProcessState.SLEEP:
+                    break
+                self._state = ProcessState.RUN
+
+
+        self._node._tg.start_soon(_inner)  # noqa
 
 
 ##
@@ -116,7 +170,14 @@ class Actor(ProcessBehavior):
         await behavior.init()
 
     async def process_run(self) -> None:
-        raise NotImplementedError
+        while True:
+            try:
+                msg = self._proc.mailbox.main.receive_stream.receive_nowait()
+            except anyio.WouldBlock:
+                return
+
+            print(msg)
+            raise NotImplementedError
 
 
 ##
@@ -128,17 +189,22 @@ class ProcessOptions:
 
 
 class Node:
-    def __init__(self) -> None:
+    def __init__(
+            self,
+            tg: anyio.abc.TaskGroup,
+    ) -> None:
         super().__init__()
 
-        self._processes: dict[Pid, Process] = {}
+        self._tg = tg
+
+        self._processes: dict[Pid, ProcessImpl] = {}
         self._id_seq = itertools.count()
 
     async def spawn(
             self,
             behavior_fac: ta.Callable[[], ProcessBehavior],
             opts: ProcessOptions = ProcessOptions(),
-    ) -> Process:
+    ) -> Pid:
         pid = Pid(
             next(self._id_seq),
             int(time.monotonic()),
@@ -159,7 +225,12 @@ class Node:
 
         await behavior.process_init(proc)
 
-        return proc
+        proc._state = ProcessState.SLEEP  # noqa
+        self._processes[pid] = proc
+
+        await proc._run()  # noqa
+
+        return proc.pid
 
 
 #
@@ -171,9 +242,10 @@ class FooActor(Actor, ActorBehavior):
 
 
 async def _main() -> None:
-    node = Node()
-    proc = await node.spawn(FooActor)
-    print(proc)
+    async with anyio.create_task_group() as tg:
+        node = Node(tg)
+        pid = await node.spawn(FooActor)
+        print(pid)
 
 
 if __name__ == '__main__':
