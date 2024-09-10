707e3d4fa320abd291aea6f35fc37ac762d81b01
wrmsr <timwilloney@gmail.com>
2024-09-10 16:51:30 -0400

    --
    Former-commit-id: b7b75aed4db6a0da24408c59382aa7cbe5b2d142

---
+++ omlish/concurrent/tests/test_threadlets.py
@@ -0,0 +1,49 @@
+import logging  # noqa
+
+import pytest  # noqa
+
+from ... import logs  # noqa
+from ..threadlets import GreenletThreadlets
+from ..threadlets import Threadlets
+from .real import RealThreadlets
+
+
+def _test_threadlets(api: Threadlets):
+    done = 0
+
+    def test1():
+        gr2.switch()
+        gr2.switch()
+        nonlocal done
+        done += 1
+
+    def test2():
+        def f():
+            gr1.switch()
+        f()
+        nonlocal done
+        done += 1
+        gr1.switch()
+
+    gr1 = api.spawn(test1)
+    gr2 = api.spawn(test2)
+    gr1.switch()
+    assert done == 2
+
+
+# @pytest.mark.skip
+def test_greenlet():
+    _test_threadlets(GreenletThreadlets())
+
+
+LOG_LIST = logs.ListHandler()
+
+
+@pytest.mark.skip
+def test_real():
+    logs.configure_standard_logging('DEBUG')
+    logging.root.addHandler(LOG_LIST)
+
+    s = RealThreadlets()
+    _test_threadlets(s)
+    s.shutdown()
