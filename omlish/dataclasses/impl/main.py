096d25f4dca94ec87083cc421d293fd727c15971
wrmsr <timwilloney@gmail.com>
2024-09-10 09:18:54 -0400

    --
    Former-commit-id: 98439120fd2bc1048fbb7bfd6262ad799ffe1e7b

--- omlish/dataclasses/impl/main.py
+++ omlish/dataclasses/impl/main.py
@@ -5,14 +5,13 @@ import typing as ta
 from ... import check
 from ... import lang
 from .copy import CopyProcessor
-from .fields import preprocess_field
+from .fields import FieldsProcessor
 from .frozen import FrozenProcessor
 from .hashing import HashProcessor
 from .init import InitProcessor
 from .internals import FIELDS_ATTR
 from .internals import PARAMS_ATTR
 from .internals import Params
-from .internals import is_kw_only
 from .order import OrderProcessor
 from .params import ParamsExtras
 from .processing import Processor
@@ -24,12 +23,6 @@ from .simple import EqProcessor
 from .simple import MatchArgsProcessor
 from .simple import OverridesProcessor
 from .slots import add_slots
-
-
-if ta.TYPE_CHECKING:
-    from . import metaclass
-else:
-    metaclass = lang.proxy_import('.metaclass', __package__)
 
 
 MISSING = dc.MISSING
@@ -50,67 +43,6 @@ class MainProcessor:
         if self._info.params.order and not self._info.params.eq:
             raise ValueError('eq must be true if order is true')
 
-    def _check_frozen_bases(self) -> None:
-        mc_base = getattr(metaclass, 'Data', None)
-        all_frozen_bases = None
-        any_frozen_base = False
-        has_dataclass_bases = False
-        for b in self._cls.__mro__[-1:0:-1]:
-            if b is mc_base:
-                continue
-            base_fields = getattr(b, FIELDS_ATTR, None)
-            if base_fields is not None:
-                has_dataclass_bases = True
-                if all_frozen_bases is None:
-                    all_frozen_bases = True
-                current_frozen = getattr(b, PARAMS_ATTR).frozen
-                all_frozen_bases = all_frozen_bases and current_frozen
-                any_frozen_base = any_frozen_base or current_frozen
-
-        if has_dataclass_bases:
-            if any_frozen_base and not self._info.params.frozen:
-                raise TypeError('cannot inherit non-frozen dataclass from a frozen one')
-
-            if all_frozen_bases is False and self._info.params.frozen:
-                raise TypeError('cannot inherit frozen dataclass from a non-frozen one')
-
-    @lang.cached_function
-    def _process_fields(self) -> None:
-        fields: dict[str, dc.Field] = {}
-
-        for b in self._cls.__mro__[-1:0:-1]:
-            base_fields = getattr(b, FIELDS_ATTR, None)
-            if base_fields is not None:
-                for f in base_fields.values():
-                    fields[f.name] = f
-
-        cls_fields: list[dc.Field] = []
-
-        kw_only = self._info.params.kw_only
-        kw_only_seen = False
-        for name, ann in self._info.cls_annotations.items():
-            if is_kw_only(self._cls, ann):
-                if kw_only_seen:
-                    raise TypeError(f'{name!r} is KW_ONLY, but KW_ONLY has already been specified')
-                kw_only_seen = True
-                kw_only = True
-            else:
-                cls_fields.append(preprocess_field(self._cls, name, ann, kw_only))
-
-        for f in cls_fields:
-            fields[f.name] = f
-            if isinstance(getattr(self._cls, f.name, None), dc.Field):
-                if f.default is MISSING:
-                    delattr(self._cls, f.name)
-                else:
-                    setattr(self._cls, f.name, f.default)
-
-        for name, value in self._cls.__dict__.items():
-            if isinstance(value, dc.Field) and name not in self._info.cls_annotations:
-                raise TypeError(f'{name!r} is a field but has no type annotation')
-
-        setattr(self._cls, FIELDS_ATTR, fields)
-
     @lang.cached_function
     def _transform_slots(self) -> None:
         if self._info.params.weakref_slot and not self._info.params.slots:
@@ -119,28 +51,32 @@ class MainProcessor:
             return
         self._cls = add_slots(self._cls, self._info.params.frozen, self._info.params.weakref_slot)
 
+    PROCESSOR_TYPES: ta.ClassVar[ta.Sequence[type[Processor]]] = [
+        FieldsProcessor,
+        InitProcessor,
+        OverridesProcessor,
+        ReprProcessor,
+        EqProcessor,
+        OrderProcessor,
+        FrozenProcessor,
+        HashProcessor,
+        DocProcessor,
+        MatchArgsProcessor,
+        ReplaceProcessor,
+        CopyProcessor,
+    ]
+
     @lang.cached_function
     def process(self) -> type:
         self._check_params()
-        self._check_frozen_bases()
 
-        self._process_fields()
+        ps = [pcls(self._info) for pcls in self.PROCESSOR_TYPES]
 
-        pcls: type[Processor]
-        for pcls in [
-            InitProcessor,
-            OverridesProcessor,
-            ReprProcessor,
-            EqProcessor,
-            OrderProcessor,
-            FrozenProcessor,
-            HashProcessor,
-            DocProcessor,
-            MatchArgsProcessor,
-            ReplaceProcessor,
-            CopyProcessor,
-        ]:
-            pcls(self._info).process()
+        for p in ps:
+            p.check()
+
+        for p in ps:
+            p.process()
 
         self._transform_slots()
 
