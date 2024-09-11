2e81c045ffb9a85077f97346d4507eaa2001f3b4
wrmsr <timwilloney@gmail.com>
2024-09-11 11:15:39 -0400

    --
    Former-commit-id: 5ffcbcff1d49ebcba648ace2e28467ce5b422e62

--- omlish/diag/tests/test_asts.py
+++ omlish/diag/tests/test_asts.py
@@ -2,24 +2,12 @@ import pytest
 
 from ...testing import pytest as ptu
 from ..asts import ArgsRenderer
-
-
-def check_equal(l, r):
-    if l != r:
-        ra = ArgsRenderer().render_args(l, r)
-        if ra is None:
-            raise TypeError
-        lr, rr = ra
-        msg = f'{lr} != {rr}'
-        raise ValueError(msg)
+from .asts import check_foo
 
 
 @ptu.skip_if_cant_import('executing')
 def test_check_equal():
     assert ArgsRenderer.smoketest()
 
-    def foo(x):
-        return x + 1
-
     with pytest.raises(ValueError):  # noqa
-        check_equal(foo(3) + 2, 8)
+        check_foo()
