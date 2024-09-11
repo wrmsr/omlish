ab1f1fb3656e5cb1122e0f5ef0f7cf0fe91978fc
wrmsr <timwilloney@gmail.com>
2024-09-11 15:04:16 -0400

    --
    Former-commit-id: 99ba0510a3af6008cb417822a439166213dc4d02

--- x/specs/openapi.py
+++ x/specs/openapi.py
@@ -1,12 +1,31 @@
+"""
+https://swagger.io/specification/
+"""
+import dataclasses as dc
 import os.path
 
 import yaml
+
+from omlish import marshal as msh
+
+
+@dc.dataclass(frozen=True)
+class Info:
+    pass
+
+
+@dc.dataclass(frozen=True)
+class Spec:
+    """https://swagger.io/specification/#openapi-object"""
+
+    openapi: str
+    info: Info
 
 
 def _main():
     with open(os.path.join(os.path.dirname(__file__), '..', 'llm', 'openai.yaml')) as f:
         spec = yaml.safe_load(f)
-    print(spec)
+
 
 
 if __name__ == '__main__':
