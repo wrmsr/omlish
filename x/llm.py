6f7947e31611bc02af72b2d8b2e6f7a53a44b70e
wrmsr <timwilloney@gmail.com>
2024-09-11 12:57:42 -0400

    --
    Former-commit-id: c2d684a5f4a6efa22b348025b92bb192a3d2bc56

--- x/llm.py
+++ x/llm.py
@@ -1,5 +1,38 @@
+import argparse
+import os.path
+
+import openai
+import yaml
+
+
+def _load_secrets():
+    with open(os.path.expanduser('~/Dropbox/.dotfiles/secrets.yml')) as f:
+        dct = yaml.safe_load(f)
+    os.environ['OPENAI_API_KEY'] = dct['openai_api_key']
+    os.environ['TAVILY_API_KEY'] = dct['tavily_api_key']
+
+
 def _main() -> None:
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('prompt')
+    args = parser.parse_args()
+
+    prompt = args.prompt
+
+    _load_secrets()
+
+    response = openai.completions.create(
+        model='gpt-3.5-turbo-instruct',
+        prompt=prompt,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+
+    response_text = response.choices[0].text.strip()
+    print(response_text)
 
 
 if __name__ == '__main__':
