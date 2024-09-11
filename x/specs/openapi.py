d4856fde4313659316373dfbf8e583fda87e8c27
wrmsr <timwilloney@gmail.com>
2024-09-11 16:26:12 -0400

    --
    Former-commit-id: 74598ecbaf7fa09e208d15bd9476292fc9f4602f

--- x/specs/openapi.py
+++ x/specs/openapi.py
@@ -405,29 +405,39 @@ def _reference_union_arg(rty: rfl.Type) -> type | None:
 @dc.dataclass(frozen=True)
 class _ReferenceUnionMarshaler(msh.Marshaler):
     m: msh.Marshaler
+    r: msh.Marshaler
 
     def marshal(self, ctx: msh.MarshalContext, o: ta.Any) -> msh.Value:
-        raise NotImplementedError
+        if isinstance(o, Reference):
+            return self.r.marshal(ctx, o)
+        else:
+            return self.m.marshal(ctx, o)
 
 
 class _ReferenceUnionMarshalerFactory(msh.MarshalerFactoryMatchClass):
     @mfs.simple(lambda _, ctx, rty: _reference_union_arg(rty) is not None)
     def _build(self, ctx: msh.MarshalContext, rty: rfl.Type) -> msh.Marshaler:
-        return _ReferenceUnionMarshaler(ctx.make(check.not_none(_reference_union_arg(rty))))
+        return _ReferenceUnionMarshaler(ctx.make(check.not_none(_reference_union_arg(rty))), ctx.make(Reference))
 
 
 @dc.dataclass(frozen=True)
 class _ReferenceUnionUnmarshaler(msh.Unmarshaler):
     u: msh.Unmarshaler
+    r: msh.Unmarshaler
 
     def unmarshal(self, ctx: msh.UnmarshalContext, v: msh.Value) -> ta.Any:
-        raise NotImplementedError
+        if not isinstance(v, ta.Mapping):
+            raise TypeError(v)
+        elif '$ref' in v:
+            return self.r.unmarshal(ctx, v)
+        else:
+            return self.u.unmarshal(ctx, v)
 
 
 class _ReferenceUnionUnmarshalerFactory(msh.UnmarshalerFactoryMatchClass):
     @mfs.simple(lambda _, ctx, rty: _reference_union_arg(rty) is not None)
     def _build(self, ctx: msh.UnmarshalContext, rty: rfl.Type) -> msh.Unmarshaler:
-        return _ReferenceUnionUnmarshaler(ctx.make(check.not_none(_reference_union_arg(rty))))
+        return _ReferenceUnionUnmarshaler(ctx.make(check.not_none(_reference_union_arg(rty))), ctx.make(Reference))
 
 
 @lang.cached_function
