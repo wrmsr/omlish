c8418fad8b688a571f166910a88efb8c1f1ea348
wrmsr <timwilloney@gmail.com>
2024-09-11 16:21:40 -0400

    --
    Former-commit-id: 539612cf51a7f41e9f3c1c72b35e6fbc28ef8411

--- x/specs/openapi.py
+++ x/specs/openapi.py
@@ -8,7 +8,11 @@ import typing as ta
 
 import yaml
 
+from omlish import check
+from omlish import lang
 from omlish import marshal as msh
+from omlish import matchfns as mfs
+from omlish import reflect as rfl
 
 
 ##
@@ -386,6 +390,53 @@ class Openapi:
     security: ta.Sequence[SecurityRequirement] | None = None
     tags: ta.Sequence[Tag] | None = None
     external_docs: ExternalDocumentation | None = None
+
+
+#
+
+
+def _reference_union_arg(rty: rfl.Type) -> type | None:
+    if isinstance(rty, rfl.Union) and len(rty.args) == 2 and Reference in rty.args:
+        return check.single(a for a in rty.args if a is not Reference)
+    else:
+        return None
+
+
+@dc.dataclass(frozen=True)
+class _ReferenceUnionMarshaler(msh.Marshaler):
+    m: msh.Marshaler
+
+    def marshal(self, ctx: msh.MarshalContext, o: ta.Any) -> msh.Value:
+        raise NotImplementedError
+
+
+class _ReferenceUnionMarshalerFactory(msh.MarshalerFactoryMatchClass):
+    @mfs.simple(lambda ctx, rty: _reference_union_arg(rty) is not None)
+    def _build(self, ctx: msh.MarshalContext, rty: rfl.Type) -> msh.Marshaler:
+        return _ReferenceUnionMarshaler(ctx.make(check.not_none(_reference_union_arg(rty))))
+
+
+@dc.dataclass(frozen=True)
+class _ReferenceUnionUnmarshaler(msh.Unmarshaler):
+    u: msh.Unmarshaler
+
+    def unmarshal(self, ctx: msh.UnmarshalContext, v: msh.Value) -> ta.Any:
+        raise NotImplementedError
+
+
+class _ReferenceUnionUnmarshalerFactory(msh.UnmarshalerFactoryMatchClass):
+    @mfs.simple(lambda ctx, rty: _reference_union_arg(rty) is not None)
+    def _build(self, ctx: msh.UnmarshalContext, rty: rfl.Type) -> msh.Unmarshaler:
+        return _ReferenceUnionUnmarshaler(ctx.make(check.not_none(_reference_union_arg(rty))))
+
+
+@lang.cached_function
+def _install_standard_marshalling() -> None:
+    msh.STANDARD_MARSHALER_FACTORIES[0:0] = [_ReferenceUnionMarshalerFactory()]
+    msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [_ReferenceUnionUnmarshalerFactory()]
+
+
+_install_standard_marshalling()
 
 
 ##
