7b67aea3ac750e5a381e76cd596292fc01020527
wrmsr <timwilloney@gmail.com>
2024-09-11 13:57:06 -0400

    --
    Former-commit-id: 94be65de278d72c98e9d00ad6dd8d5a5cd31e21b

--- x/llm.py
+++ x/llm.py
@@ -1,8 +1,11 @@
 import argparse
 import os.path
+import sys
 
 import openai
 import yaml
+
+from omlish.diag import pycharm
 
 
 def _load_secrets():
@@ -19,20 +22,34 @@ def _main() -> None:
 
     prompt = args.prompt
 
+    if not sys.stdin.isatty() and not pycharm.is_pycharm_hosted():
+        stdin_data = sys.stdin.read()
+
+        prompt = '\n'.join([prompt, stdin_data])
+
     _load_secrets()
+
+    stream = True
 
     response = openai.completions.create(
         model='gpt-3.5-turbo-instruct',
         prompt=prompt,
         temperature=0,
-        max_tokens=100,
+        max_tokens=1024,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.0,
+        stream=stream,
     )
 
-    response_text = response.choices[0].text.strip()
-    print(response_text)
+    if stream:
+        for chunk in response:
+            sys.stdout.write(chunk.choices[0].text)
+        print()
+
+    else:
+        response_text = response.choices[0].text.strip()
+        print(response_text)
 
 
 if __name__ == '__main__':
