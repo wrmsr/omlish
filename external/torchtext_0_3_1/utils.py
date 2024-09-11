8aca9804d3908b227812902e6dcf1c4a08765d7d
wrmsr <timwilloney@gmail.com>
2024-09-11 12:53:21 -0400

    --
    Former-commit-id: fdb50292724e7fd58a84786e622e4b1a80492dbe

--- external/torchtext_0_3_1/utils.py
+++ external/torchtext_0_3_1/utils.py
@@ -6,39 +6,48 @@ import tqdm
 
 def reporthook(t):
     """https://github.com/tqdm/tqdm"""
+
     last_b = [0]
 
     def inner(b=1, bsize=1, tsize=None):
         """
-        b: int, optional
-        Number of blocks just transferred [default: 1].
-        bsize: int, optional
-        Size of each block (in tqdm units) [default: 1].
-        tsize: int, optional
-        Total size (in tqdm units). If [default: None] remains unchanged.
+        b: int, optional - Number of blocks just transferred [default: 1].
+        bsize: int, optional - Size of each block (in tqdm units) [default: 1].
+        tsize: int, optional - Total size (in tqdm units). If [default: None] remains unchanged.
         """
+
         if tsize is not None:
             t.total = tsize
         t.update((b - last_b[0]) * bsize)
         last_b[0] = b
+
     return inner
 
 
 def download_from_url(url, path):
     """Download file, with logic (from tensor2tensor) for Google Drive"""
+
     def process_response(r):
         chunk_size = 16 * 1024
         total_size = int(r.headers.get('Content-length', 0))
         with open(path, "wb") as file:
-            with tqdm.tqdm(total=total_size, unit='B',
-                      unit_scale=1, desc=path.split('/')[-1]) as t:
+            with tqdm.tqdm(
+                    total=total_size,
+                    unit='B',
+                    unit_scale=1,
+                    desc=path.split('/')[-1],
+            ) as t:
                 for chunk in r.iter_content(chunk_size):
                     if chunk:
                         file.write(chunk)
                         t.update(len(chunk))
 
     if 'drive.google.com' not in url:
-        response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, stream=True)
+        response = requests.get(
+            url,
+            headers={'User-Agent': 'Mozilla/5.0'},
+            stream=True,
+        )
         process_response(response)
         return
 
@@ -58,9 +67,12 @@ def download_from_url(url, path):
 
 
 def unicode_csv_reader(unicode_csv_data, **kwargs):
-    """Since the standard csv library does not handle unicode in Python 2, we need a wrapper.
-    Borrowed and slightly modified from the Python docs:
-    https://docs.python.org/2/library/csv.html#csv-examples"""
+    """
+    Since the standard csv library does not handle unicode in Python 2, we need a wrapper.
+
+    Borrowed and slightly modified from the Python docs: https://docs.python.org/2/library/csv.html#csv-examples
+    """
+
     for line in csv.reader(unicode_csv_data, **kwargs):
         yield line
 
