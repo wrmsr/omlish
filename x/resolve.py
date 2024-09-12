ebbef61bdd0f5dba81a3536e58730b14f94b2126
wrmsr <timwilloney@gmail.com>
2024-09-12 10:41:03 -0400

    --
    Former-commit-id: 0d9830c73ff3a543bf17ebfad2d8ca78262ad606

--- x/resolve.py
+++ x/resolve.py
@@ -2,6 +2,7 @@ import enum
 import typing as ta
 
 from omlish import cached
+from omlish import check
 from omlish import dataclasses as dc
 
 
@@ -18,12 +19,15 @@ class Attribute:
 @dc.dataclass(frozen=True)
 class Entity:
     name: str
-    src_attrs: ta.Sequence[Attribute]
+    src_attrs: ta.Sequence[Attribute] = dc.field(repr=False)
 
     @cached.property
-    @dc.init
+    @dc.init  # FIXME: doesn't cache lol
     def attrs(self) -> ta.Sequence[Attribute]:
-        raise NotImplementedError
+        return [
+            dc.replace(a, entity=check.replacing_none(a.entity, self))
+            for a in self.src_attrs
+        ]
 
 
 ##
@@ -50,6 +54,8 @@ def _main() -> None:
     user = Entity('user', [Attribute('id'), Attribute('name')])
     business = Entity('business', [Attribute('id'), Attribute('name')])
     review = Entity('review', [Attribute('id'), Attribute('business_id'), Attribute('user_id')])
+
+    print(user.attrs)
 
 
 if __name__ == '__main__':
