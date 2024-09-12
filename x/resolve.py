bb5e1485b4dd073eddf87c069e3918993cfeadb7
wrmsr <timwilloney@gmail.com>
2024-09-12 10:54:41 -0400

    --
    Former-commit-id: 66b9b3b9c7c96eb0e58593b9d3681f4a770449db

--- x/resolve.py
+++ x/resolve.py
@@ -1,5 +1,11 @@
+"""
+FIXME:
+ - @cached.property @dc.init doesn't cache lol, need idiom + dont break pycharm
+"""
+import abc
 import enum
 import operator
+import pprint
 import typing as ta
 
 from omlish import cached
@@ -11,11 +17,26 @@ from omlish import dataclasses as dc
 ##
 
 
+class Resolvable(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def full_name(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def children(self) -> ta.Iterable['Resolvable']:
+        return ()
+
+
 @dc.dataclass(frozen=True)
-class Attribute:
+class Attribute(Resolvable):
     name: str
 
     entity: ta.Optional['Entity'] = None
+
+    @cached.property
+    def full_name(self) -> str:
+        return '.'.join([self.name, check.not_none(self.entity).name])
 
 
 @dc.dataclass(frozen=True)
@@ -23,7 +44,7 @@ class Attributes(ta.Sequence[Attribute]):
     lst: ta.Sequence[Attribute]
 
     @cached.property
-    @dc.init  # FIXME: doesn't cache lol
+    @dc.init
     def by_name(self) -> ta.Mapping[str, Attribute]:
         return col.make_map_by(operator.attrgetter('name'), self.lst, strict=True)
 
@@ -38,17 +59,49 @@ class Attributes(ta.Sequence[Attribute]):
 
 
 @dc.dataclass(frozen=True)
-class Entity:
+class Entity(Resolvable):
     name: str
     src_attrs: ta.Sequence[Attribute] = dc.field(repr=False)
 
+    @property
+    def full_name(self) -> str:
+        return self.name
+
     @cached.property
-    @dc.init  # FIXME: doesn't cache lol
+    @dc.init
     def attrs(self) -> Attributes:
         return Attributes([
             dc.replace(a, entity=check.replacing_none(a.entity, self))
             for a in self.src_attrs
         ])
+
+    @property
+    def children(self) -> ta.Iterable[Resolvable]:
+        return self.attrs
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class Resolvables:
+    roots: ta.Sequence[Resolvable]
+
+    @cached.property
+    @dc.init
+    def all(self) -> ta.Sequence[Resolvable]:
+        out: list[Resolvable] = []
+        stk: list[Resolvable] = list(reversed(self.roots))
+        while stk:
+            cur = stk.pop()
+            out.append(cur)
+            stk.extend(reversed(list(cur.children)))
+        return out
+
+    @cached.property
+    @dc.init
+    def by_full_name(self) -> ta.Mapping[str, Resolvable]:
+        return col.make_map_by(operator.attrgetter('full_name'), self.all)
 
 
 ##
@@ -76,7 +129,9 @@ def _main() -> None:
     business = Entity('business', [Attribute('id'), Attribute('name')])
     review = Entity('review', [Attribute('id'), Attribute('business_id'), Attribute('user_id')])
 
-    print(user.attrs)
+    entities = [user, business, review]
+    resolvables = Resolvables(entities)
+    pprint.pprint(resolvables.by_full_name)
 
 
 if __name__ == '__main__':
