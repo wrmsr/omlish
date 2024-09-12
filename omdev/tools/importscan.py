08d87d1a51b47eced927609225033bb0387ddf2c
wrmsr <timwilloney@gmail.com>
2024-09-12 10:22:25 -0400

    --
    Former-commit-id: 91e8103cb324ff6814d51b53f55e8bafc85df537

--- omdev/tools/importscan.py
+++ omdev/tools/importscan.py
@@ -1,9 +1,3 @@
-"""
-*not* trace - for each importable module (filterable, no tests/conftest) list:
- - post-import rss
- - time taken
- - modules imported
-"""
 import argparse
 import dataclasses as dc
 import inspect
