e86e8f55f42099c59f0b11f3a8575b45b522819d
wrmsr <timwilloney@gmail.com>
2024-09-11 20:35:19 -0400

    --
    Former-commit-id: f62d4818bd8339175f2ab31b9b57f1a46cec8a26

---
+++ x/llm/openai/openai.py
@@ -0,0 +1,11 @@
+"""
+TODO (parts):
+ - https://platform.openai.com/docs/api-reference/audio
+ - https://platform.openai.com/docs/api-reference/fine-tuning
+ - https://platform.openai.com/docs/api-reference/batch
+ - https://platform.openai.com/docs/api-reference/files
+ - https://platform.openai.com/docs/api-reference/uploads
+ - https://platform.openai.com/docs/api-reference/images
+ - https://platform.openai.com/docs/api-reference/models
+ - https://platform.openai.com/docs/api-reference/moderations
+"""
