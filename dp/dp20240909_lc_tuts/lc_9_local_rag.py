7d703d04d74f0808d7be09ff8edb731072eeaf0d
wrmsr <timwilloney@gmail.com>
2024-09-12 18:38:15 -0400

    --
    Former-commit-id: 6c3d7a125f37536f265ddb0ef3e90f1da16cd68d

---
+++ dp/dp20240909_lc_tuts/lc_9_local_rag.py
@@ -0,0 +1,15 @@
+"""
+https://python.langchain.com/v0.2/docs/tutorials/local_rag/
+"""
+
+from ..utils import load_secrets
+
+
+def _main() -> None:
+    load_secrets()
+
+    #
+
+
+if __name__ == '__main__':
+    _main()
