3531ce35910fb32b58f7380ece072ecf803db9fc
wrmsr <timwilloney@gmail.com>
2024-09-12 12:24:52 -0400

    --
    Former-commit-id: 102edb9a2bd73adf03813ad2b017c24aa6b31165

--- x/pathfind.py
+++ x/pathfind.py
@@ -64,37 +64,29 @@ def single_source_shortest_paths(
     """Find path from node ``s`` to all other nodes or just to ``d``.
 
     ``graph``
-        An adjacency list that's structured as a dict of dicts (see
-        :class:`dijkstra.graph.Graph`). Other than the structure, no
-        other assumptions are made about the types of the nodes or edges
-        in the graph. If ``cost_func`` isn't specified, edges will be
-        assumed to be values that can be compared directly (e.g.,
-        numbers, or any other comparable type).
+        An adjacency list that's structured as a dict of dicts (see :class:`dijkstra.graph.Graph`). Other than the
+        structure, no other assumptions are made about the types of the nodes or edges in the graph. If ``cost_func``
+        isn't specified, edges will be assumed to be values that can be compared directly (e.g., numbers, or any other
+        comparable type).
 
     ``s``
         Start node.
 
     ``d``
-        Destination node. If ``d`` is not specified, the algorithm is
-        run normally (i.e., the paths from ``s`` to all reachable nodes
-        are found). If ``d`` is specified, the algorithm is stopped when
-        a path to ``d`` has been found.
+        Destination node. If ``d`` is not specified, the algorithm is run normally (i.e., the paths from ``s`` to all
+        reachable nodes are found). If ``d`` is specified, the algorithm is stopped when a path to ``d`` has been found.
 
     ``annex``
-        Another graph that can be used to augment ``graph`` without
-        altering it.
+        Another graph that can be used to augment ``graph`` without altering it.
 
     ``cost_func``
-        A function to apply to each edge to modify its base cost. The
-        arguments it will be passed are the current node, a neighbor of
-        the current node, the edge that connects the current node to
-        that neighbor, and the edge that was previously traversed to
-        reach the current node.
+        A function to apply to each edge to modify its base cost. The arguments it will be passed are the current node,
+        a neighbor of the current node, the edge that connects the current node to that neighbor, and the edge that was
+        previously traversed to reach the current node.
 
     ``heuristic_func``
-        A function to apply at each iteration to guide the algorithm
-        toward the destination (typically) instead of fanning out. It
-        gets passed the same args as ``cost_func``.
+        A function to apply at each iteration to guide the algorithm toward the destination (typically) instead of
+        fanning out. It gets passed the same args as ``cost_func``.
 
     ``debug``
         If set, return additional info that may be useful for debugging.
@@ -104,49 +96,41 @@ def single_source_shortest_paths(
 
             {v => (u, e, cost from v to u over e), ...}
 
-        If ``debug`` is set, additional debugging info will be returned
-        also. Currently, this info includes costs from ``s`` to reached
-        nodes and the set of visited nodes.
-
+        If ``debug`` is set, additional debugging info will be returned also. Currently, this info includes costs from
+        ``s`` to reached nodes and the set of visited nodes.
     """
+
     counter = itertools.count()
 
-    # Current known costs of paths from s to all nodes that have been
-    # reached so far. Note that "reached" is not the same as "visited".
+    # Current known costs of paths from s to all nodes that have been reached so far. Note that "reached" is not the
+    # same as "visited".
     costs = {s: 0}
 
-    # Predecessor map for each node that has been reached from ``s``.
-    # Keys are nodes that have been reached; values are tuples of
-    # predecessor node, edge traversed to reach predecessor node, and
-    # cost to traverse the edge from the predecessor node to the reached
-    # node.
+    # Predecessor map for each node that has been reached from ``s``. Keys are nodes that have been reached; values are
+    # tuples of predecessor node, edge traversed to reach predecessor node, and cost to traverse the edge from the
+    # predecessor node to the reached node.
     predecessors = {s: (None, None, None)}
 
-    # A priority queue of nodes with known costs from s. The nodes in
-    # this queue are candidates for visitation. Nodes are added to this
-    # queue when they are reached (but only if they have not already
-    # been visited).
+    # A priority queue of nodes with known costs from s. The nodes in this queue are candidates for visitation. Nodes
+    # are added to this queue when they are reached (but only if they have not already been visited).
     visit_queue = [(0, next(counter), s)]
 
-    # Nodes that have been visited. Once a node has been visited, it
-    # won't be visited again. Note that in this context "visited" means
-    # a node has been selected as having the lowest known cost (and it
-    # must have been "reached" to be selected).
+    # Nodes that have been visited. Once a node has been visited, it won't be visited again. Note that in this context
+    # "visited" means a node has been selected as having the lowest known cost (and it must have been "reached" to be
+    # selected).
     visited = set()
 
     while visit_queue:
-        # In the nodes remaining in the graph that have a known cost
-        # from s, find the node, u, that currently has the shortest path
-        # from s.
+        # In the nodes remaining in the graph that have a known cost from s, find the node, u, that currently has the
+        # shortest path from s.
         cost_of_s_to_u, _, u = heapq.heappop(visit_queue)
 
         if u == d:
             break
 
         if u in visited:
-            # This will happen when u has been reached from multiple
-            # nodes before being visited (because multiple entries for
-            # u will have been added to the visit queue).
+            # This will happen when u has been reached from multiple nodes before being visited (because multiple
+            # entries for u will have been added to the visit queue).
             continue
 
         visited.add(u)
@@ -163,8 +147,7 @@ def single_source_shortest_paths(
         # The edge crossed to get to u
         prev_e = predecessors[u][1]
 
-        # Check each of u's neighboring nodes to see if we can update
-        # its cost by reaching it from u.
+        # Check each of u's neighboring nodes to see if we can update its cost by reaching it from u.
         for v in neighbors:
             # Don't backtrack to nodes that have already been visited.
             if v in visited:
@@ -175,28 +158,22 @@ def single_source_shortest_paths(
             # Get the cost of the edge running from u to v.
             cost_of_e = cost_func(u, v, e, prev_e) if cost_func else e
 
-            # Cost of s to u plus the cost of u to v across e--this
-            # is *a* cost from s to v that may or may not be less than
-            # the current known cost to v.
+            # Cost of s to u plus the cost of u to v across e--this is *a* cost from s to v that may or may not be less
+            # than the current known cost to v.
             cost_of_s_to_u_plus_cost_of_e = cost_of_s_to_u + cost_of_e
 
-            # When there is a heuristic function, we use a
-            # "guess-timated" cost, which is the normal cost plus some
-            # other heuristic cost from v to d that is calculated so as
-            # to keep us moving in the right direction (generally more
-            # toward the goal instead of away from it).
+            # When there is a heuristic function, we use a "guess-timated" cost, which is the normal cost plus some
+            # other heuristic cost from v to d that is calculated so as to keep us moving in the right direction
+            # (generally more toward the goal instead of away from it).
             if heuristic_func:
                 additional_cost = heuristic_func(u, v, e, prev_e)
                 cost_of_s_to_u_plus_cost_of_e += additional_cost
 
             if v not in costs or costs[v] > cost_of_s_to_u_plus_cost_of_e:
-                # If the current known cost from s to v is greater than
-                # the cost of the path that was just found (cost of s to
-                # u plus cost of u to v across e), update v's cost in
-                # the cost list and update v's predecessor in the
-                # predecessor list (it's now u). Note that if ``v`` is
-                # not present in the ``costs`` list, its current cost
-                # is considered to be infinity.
+                # If the current known cost from s to v is greater than the cost of the path that was just found (cost
+                # of s to u plus cost of u to v across e), update v's cost in the cost list and update v's predecessor
+                # in the predecessor list (it's now u). Note that if ``v`` is not present in the ``costs`` list, its
+                # current cost is considered to be infinity.
                 costs[v] = cost_of_s_to_u_plus_cost_of_e
                 predecessors[v] = (u, e, cost_of_e)
                 heapq.heappush(visit_queue, (cost_of_s_to_u_plus_cost_of_e, next(counter), v))
@@ -211,18 +188,6 @@ def single_source_shortest_paths(
 
 
 def extract_shortest_path_from_predecessor_list(predecessors, d):
-    """Extract ordered lists of nodes, edges, costs from predecessor list.
-
-    ``predecessors``
-        Predecessor list {u: (v, e), ...} u's predecessor is v via e
-
-    ``d``
-        Destination node
-
-    Returns
-        A :class:`PathInfo` object.
-
-    """
     nodes = [d]  # Nodes on the shortest path from s to d
     edges = []  # Edges on the shortest path from s to d
     costs = []  # Costs of the edges on the shortest path from s to d
@@ -240,6 +205,9 @@ def extract_shortest_path_from_predecessor_list(predecessors, d):
     costs.reverse()
     total_cost = sum(costs)
     return PathInfo(nodes, edges, costs, total_cost)
+
+
+##
 
 
 class TestPathfind:
@@ -296,13 +264,13 @@ class TestPathfind:
             {
                 "a": {"b": 10, "c": 100, "d": 1},
                 "b": {"c": 10},
+                "c": {},
                 "d": {"b": 1, "e": 1},
                 "e": {"f": 1},
+                "f": {"c": 1},
+                "g": {"b": 1},
             }
         )
-
-        graph.setdefault("f", {})["c"] = 1
-        graph.setdefault("g", {})["b"] = 1
 
         nodes = sorted(graph)
         assert nodes == ["a", "b", "c", "d", "e", "f", "g"]
