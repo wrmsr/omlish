5da13eece77f0dfc435aa14708bfca8f881a141c
wrmsr <timwilloney@gmail.com>
2024-09-13 14:16:47 -0400

    --
    Former-commit-id: f747fc4afcfce0089f9de01a7a0e1aa26fb58051

--- x/dockerports.py
+++ x/dockerports.py
@@ -1,9 +1,14 @@
 import os.path
+import re
 import typing as ta
 
 from omlish import check
 from omlish import lang
 from omlish.formats import yaml
+
+
+_PORT_PAT = re.compile(r'(?P<l>\d+):(?P<r>\d+)')
+_PORT_LINE_PAT = re.compile(r"(  )+- '(?P<l>\d+):(?P<r>\d+)'\s*")
 
 
 def _main():
@@ -13,6 +18,8 @@ def _main():
 
     with open(yml_file) as f:
         yml_src = f.read()
+
+    #
 
     class PortEntry(ta.NamedTuple):
         l: int
@@ -35,14 +42,40 @@ def _main():
 
             lst: list[PortEntry] = []
             for port_w in ports:
+                port = check.isinstance(port_w.value, str)
+                if not re.fullmatch(_PORT_PAT, port):
+                    raise Exception(f'Bad port: {port}')
+
                 lst.append(PortEntry(
                     l=port_w.node.start_mark.line,
-                    s=check.isinstance(port_w.value, str),
+                    s=port,
                 ))
 
             dct[name] = lst
 
-    print(dct)
+    #
+
+    base_port = 35220
+
+    #
+
+    src_lines = yml_src.splitlines(keepends=True)
+    cur_port = base_port
+
+    for ps in dct.values():
+        for p in ps:
+            l = src_lines[p.l]
+            if not (m := _PORT_LINE_PAT.fullmatch(l)):
+                raise Exception(f'Bad port line: {p} {l!r}')
+
+            p_l, p_r = map(int, p.s.split(':'))
+            l_l, l_r = map(int, [(gd := m.groupdict())['l'], gd['r']])
+            if p_l != l_l or p_r != l_r:
+                raise Exception(f'Port mismatch: {p}')
+
+            print(p)
+            print(l)
+            print()
 
 
 if __name__ == '__main__':
