c7a989460dd0c769fadfca8734d45e51c32c93c6
wrmsr <timwilloney@gmail.com>
2024-09-13 12:15:17 -0400

    --
    Former-commit-id: b6add3c867cc08093e8f901855661270954289fe

--- x/lc/lc_9_local_rag.py
+++ x/lc/lc_9_local_rag.py
@@ -1,6 +1,9 @@
 """
 https://python.langchain.com/v0.2/docs/tutorials/local_rag/
 """
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
@@ -47,6 +50,72 @@ def _main() -> None:
     )
 
     print(response_message.content)
+
+    #
+
+    prompt = ChatPromptTemplate.from_template(
+        "Summarize the main themes in these retrieved docs: {docs}"
+    )
+
+    # Convert loaded documents into strings by concatenating their content
+    # and ignoring metadata
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    chain = {"docs": format_docs} | prompt | model | StrOutputParser()
+
+    question = "What are the approaches to Task Decomposition?"
+
+    docs = vectorstore.similarity_search(question)
+
+    response = chain.invoke(docs)
+    print(response)
+
+    #
+
+    RAG_TEMPLATE = """
+    You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+
+    <context>
+    {context}
+    </context>
+
+    Answer the following question:
+
+    {question}"""
+
+    rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
+
+    chain = (
+            RunnablePassthrough.assign(context=lambda input: format_docs(input["context"]))
+            | rag_prompt
+            | model
+            | StrOutputParser()
+    )
+
+    question = "What are the approaches to Task Decomposition?"
+
+    docs = vectorstore.similarity_search(question)
+
+    # Run
+    response = chain.invoke({"context": docs, "question": question})
+    print(response)
+
+    #
+
+    retriever = vectorstore.as_retriever()
+
+    qa_chain = (
+            {"context": retriever | format_docs, "question": RunnablePassthrough()}
+            | rag_prompt
+            | model
+            | StrOutputParser()
+    )
+
+    question = "What are the approaches to Task Decomposition?"
+
+    response = qa_chain.invoke(question)
+    print(response)
 
 
 if __name__ == '__main__':
