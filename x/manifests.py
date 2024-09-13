f01bee37294073a0d5e9aee767789e0ae0a6dedf
wrmsr <timwilloney@gmail.com>
2024-09-13 10:05:17 -0400

    --
    Former-commit-id: 7f790278ef5d82ced33b8d8d404f51a876f7301a

--- x/manifests.py
+++ x/manifests.py
@@ -5,7 +5,7 @@
   - IMPORT files w comment
   - comment must immediately precede a global val setter
   - val is grabbed from imported module dict by name
-  - value is repr'd somehow (roundtrip checked)
+  - value is repr'd somehow (roundtrip checked) (naw, json lol)
   - dumped in _manifest.py
  - # @omlish-manifest \n _CACHE_MANIFEST = {'cache': {'name': 'llm', …
  - also can do prechecks!
@@ -13,14 +13,35 @@
 TODO:
  - subprocess interpreter
 """
+import collections
+import dataclasses as dc
+import json
 import os.path
+import inspect
 import re
+import subprocess
+import sys
 import typing as ta
 
+from omlish.lite.cached import cached_nullary
 from omdev import findmagic
 
 
 ##
+
+
+@dc.dataclass(frozen=True)
+class Origin:
+    module: str
+    attr: str
+
+    file: str
+    line: int
+
+
+@dc.dataclass(frozen=True)
+class Manifest(Origin):
+    value: ta.Any
 
 
 MANIFEST_MAGIC = '# @omlish-manifest'
@@ -28,29 +49,72 @@ MANIFEST_MAGIC = '# @omlish-manifest'
 _MANIFEST_GLOBAL_PAT = re.compile(r'^(?P<name>[A-Za-z_][A-Za-z0-9_]*)\s*=.*')
 
 
-def handle_one(file: str, base: str) -> None:
+def _payload(spec: str, *attrs: str) -> None:
+    import importlib
+    import json
+
+    mod = importlib.import_module(spec)
+
+    manifest = getattr(mod, attr)
+
+    mj = json.dumps(manifest)
+    rt_manifest = json.loads(mj)
+
+    if rt_manifest != manifest:
+        raise Exception(f'Manifest failed to roundtrip: {manifest} != {rt_manifest}')
+
+
+def handle_one(file: str, base: str) -> ta.Sequence[Manifest]:
     print((file, base))
 
     if not file.endswith('.py'):
         raise Exception(file)
 
+    mod_name = file.rpartition('.')[0].replace(os.sep, '.')
+
     with open(os.path.join(base, file)) as f:
         src = f.read()
 
-    names = set()
+    origins: list[Origin] = []
     lines = src.splitlines(keepends=True)
     for i, l in enumerate(lines):
         if l.startswith(MANIFEST_MAGIC):
             if (m := _MANIFEST_GLOBAL_PAT.match(nl := lines[i + 1])) is None:
                 raise Exception(nl)
-            names.add(m.groupdict()['name'])
 
-    if not names:
+            origins.append(Origin(
+                module=mod_name,
+                attr=m.groupdict()['name'],
+
+                file=file,
+                line=i + 1,
+            ))
+
+    if not origins:
         raise Exception('no manifests found')
 
-    mod_name = file.rpartition('.')[0].replace(os.sep, '.')
+    if (dups := [k for k, v in collections.Counter(o.attr for o in origins).items() if v > 1]):
+        raise Exception(f'Duplicate attrs: {dups}')
 
-    raise NotImplementedError
+    mod = importlib.import_module(mod_name)
+
+    out: list[Manifest] = []
+
+    for o in origins:
+        manifest = getattr(mod, o.attr)
+
+        mj = json.dumps(manifest)
+        rt_manifest = json.loads(mj)
+
+        if rt_manifest != manifest:
+            raise Exception(f'Manifest failed to roundtrip: {manifest} != {rt_manifest}')
+
+        out.append(Manifest(
+            **dc.asdict(o),
+            value=manifest,
+        ))
+
+    return out
 
 
 ##
