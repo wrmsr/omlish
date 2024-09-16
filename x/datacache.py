060c8f232bf4bf52f258c6ea95b16e0f1455c46b
wrmsr <timwilloney@gmail.com>
2024-09-16 16:45:26 -0400

    --
    Former-commit-id: 23388f87d0caab7fad648450018c607459d5ad9d

--- x/datacache.py
+++ x/datacache.py
@@ -54,6 +54,9 @@ class CacheDataSpec(lang.Abstract, lang.Sealed):
         return hashlib.md5(self.json.encode('utf-8')).hexdigest()  # noqa
 
 
+#
+
+
 def _maybe_sorted_strs(v: ta.Iterable[str] | None) -> ta.Sequence[str] | None:
     if v is None:
         return None
@@ -70,17 +73,30 @@ class GitCacheDataSpec(CacheDataSpec):
     subtrees: ta.Sequence[str] = dc.field(default=None, kw_only=True, coerce=_maybe_sorted_strs)
 
 
+#
+
+
 @dc.dataclass(frozen=True)
 class HttpCacheDataSpec(CacheDataSpec):
-    url: str
+    url: str = dc.field(check=lambda u: bool(urllib.parse.urlparse(u)))
     file_name: str | None = None
+
+
+#
+
+
+def _repo_str(s: str) -> str:
+    u, r = check.non_empty_str(s).split('/')
+    check.non_empty_str(u)
+    check.non_empty_str(r)
+    return s
 
 
 @dc.dataclass(frozen=True)
 class GithubContentCacheDataSpec(CacheDataSpec):
-    repo: str
+    repo: str = dc.field(check=_repo_str)
     rev: str
-    paths: ta.Sequence[str]
+    paths: lang.SequenceNotStr[str]
 
 
 ##
@@ -90,7 +106,8 @@ class GithubContentCacheDataSpec(CacheDataSpec):
 class CacheDataManifest:
     spec: CacheDataSpec
 
-    at: datetime.datetime = dc.field(default_factory=lang.utcnow, kw_only=True)
+    start_at: datetime.datetime = dc.field(kw_only=True)
+    end_at: datetime.datetime = dc.field(kw_only=True)
 
     lib_version: str = dc.field(default_factory=lambda: about.__version__, kw_only=True)
     lib_revision: str = dc.field(default_factory=_lib_revision, kw_only=True)
@@ -115,50 +132,85 @@ _install_standard_marshalling()
 ##
 
 
+class DataCache:
+    def __init__(self, base_dir: str) -> None:
+        super().__init__()
+        self._base_dir = base_dir
+
+        self._items_dir = os.path.join(base_dir, 'items')
+
+    def _fetch_into(self, spec: CacheDataSpec, data_dir: str) -> None:
+        if isinstance(spec, HttpCacheDataSpec):
+            if spec.file_name is not None:
+                file_name = spec.file_name
+            else:
+                file_name = urllib.parse.urlparse(spec.url).path.split('/')[-1]
+
+            urllib.request.urlretrieve(spec.url, os.path.join(data_dir, file_name))
+
+        else:
+            raise TypeError(spec)
+
+    def get(self, spec: CacheDataSpec) -> str:
+        os.makedirs(self._items_dir, exist_ok=True)
+
+        #
+
+        item_dir = os.path.join(self._items_dir, spec.digest)
+        if os.path.isdir(item_dir):
+            data_dir = os.path.join(item_dir, 'data')
+            check.state(os.path.isdir(data_dir))
+            return data_dir
+
+        #
+
+        tmp_dir = tempfile.mkdtemp()
+
+        #
+
+        data_dir = os.path.join(tmp_dir, 'data')
+        os.mkdir(data_dir)
+
+        start_at = lang.utcnow()
+        self._fetch_into(spec, data_dir)
+        end_at = lang.utcnow()
+
+        #
+
+        manifest = CacheDataManifest(
+            spec,
+            start_at=start_at,
+            end_at=end_at,
+        )
+        manifest_json = json.dumps_pretty(msh.marshal(manifest))
+
+        manifest_file = os.path.join(tmp_dir, 'manifest.json')
+        with open(manifest_file, 'w') as f:
+            f.write(manifest_json)
+
+        ##
+
+        os.rename(tmp_dir, item_dir)
+        return os.path.join(item_dir, 'data')
+
+
+##
+
+
 def _main() -> None:
     cache_dir = tempfile.mkdtemp()
     print(f'{cache_dir=}')
 
-    spec = HttpCacheDataSpec('https://github.com/VanushVaswani/keras_mnistm/releases/download/1.0/keras_mnistm.pkl.gz')
+    cache = DataCache(cache_dir)
 
-    print(spec.json)
-    print(spec.digest)
+    #
 
-    ##
-
-    if spec.file_name is not None:
-        file_name = spec.file_name
-    else:
-        file_name = urllib.parse.urlparse(spec.url).path.split('/')[-1]
-
-    retrieved_file, _ = urllib.request.urlretrieve(spec.url)
-
-    ##
-
-    manifest = CacheDataManifest(spec)
-
-    manifest_json = json.dumps_pretty(msh.marshal(manifest))
-
-    ##
-
-    tmp_dir = tempfile.mkdtemp()
-
-    data_dir = os.path.join(tmp_dir, 'data')
-    os.mkdir(data_dir)
-    os.rename(retrieved_file, os.path.join(data_dir, file_name))
-
-    manifest_file = os.path.join(tmp_dir, 'manifest.json')
-    with open(manifest_file, 'w') as f:
-        f.write(manifest_json)
-
-    ##
-
-    item_dir = os.path.join(cache_dir, spec.digest)
-    os.rename(tmp_dir, item_dir)
-
-    ##
-
-    print(f'{item_dir=}')
+    for spec in [
+        HttpCacheDataSpec('https://github.com/VanushVaswani/keras_mnistm/releases/download/1.0/keras_mnistm.pkl.gz'),
+        GithubContentCacheDataSpec('karpathy/char-rnn', 'master', ['data/tinyshakespeare/input.txt'])
+    ]:
+        print(spec)
+        print(cache.get(spec))
 
 
 if __name__ == '__main__':
