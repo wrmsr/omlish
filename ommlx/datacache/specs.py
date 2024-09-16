99dbfdb35ee0d0b7d1effe951a988811777a3946
wrmsr <timwilloney@gmail.com>
2024-09-16 17:20:16 -0400

    --
    Former-commit-id: f6f411a74825ebabdb6d4fdd3c239769647479a1

---
+++ ommlx/datacache/specs.py
@@ -0,0 +1,83 @@
+import hashlib
+import typing as ta
+import urllib.parse
+import urllib.request
+
+from omlish import cached
+from omlish import check
+from omlish import dataclasses as dc
+from omlish import lang
+from omlish import marshal as msh
+from omlish.formats import json
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class CacheDataSpec(lang.Abstract, lang.Sealed):
+    @cached.property
+    def json(self) -> str:
+        return json.dumps_compact(msh.marshal(self, CacheDataSpec))
+
+    @cached.property
+    def digest(self) -> str:
+        return hashlib.md5(self.json.encode('utf-8')).hexdigest()  # noqa
+
+
+##
+
+
+def _maybe_sorted_strs(v: ta.Iterable[str] | None) -> ta.Sequence[str] | None:
+    if v is None:
+        return None
+    return sorted(set(check.not_isinstance(v, str)))
+
+
+@dc.dataclass(frozen=True)
+class GitCacheDataSpec(CacheDataSpec):
+    url: str
+
+    branch: str | None = dc.field(default=None, kw_only=True)
+    rev: str | None = dc.field(default=None, kw_only=True)
+
+    subtrees: ta.Sequence[str] = dc.xfield(default=None, kw_only=True, coerce=_maybe_sorted_strs)
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class HttpCacheDataSpec(CacheDataSpec):
+    url: str = dc.xfield(validate=lambda u: bool(urllib.parse.urlparse(u)))
+    file_name: str | None = None
+
+
+##
+
+
+def _repo_str(s: str) -> str:
+    u, r = check.non_empty_str(s).split('/')
+    check.non_empty_str(u)
+    check.non_empty_str(r)
+    return s
+
+
+@dc.dataclass(frozen=True)
+class GithubContentCacheDataSpec(CacheDataSpec):
+    repo: str = dc.field(validate=_repo_str)  # type: ignore
+    rev: str
+    files: lang.SequenceNotStr[str]
+
+
+##
+
+
+@lang.cached_function
+def _install_standard_marshalling() -> None:
+    specs_poly = msh.polymorphism_from_subclasses(CacheDataSpec)
+    msh.STANDARD_MARSHALER_FACTORIES[0:0] = [msh.PolymorphismMarshalerFactory(specs_poly)]
+    msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [msh.PolymorphismUnmarshalerFactory(specs_poly)]
+
+
+_install_standard_marshalling()
--- ommlx/git.py
+++ ommlx/git.py
@@ -1,8 +1,5 @@
 import os.path
-import shutil
 import subprocess
-import sys
-import tempfile
 import typing as ta
 
 
@@ -11,12 +8,12 @@ def clone_subtree(
         base_dir: str,
         repo_url: str,
         repo_dir: str,
-        repo_subtrees: ta.Sequence[str],
-        branch_name: str | None = None,
+        branch: str | None = None,
         rev: str | None = None,
+        repo_subtrees: ta.Sequence[str],
 ) -> None:
-    if not bool(branch_name) ^ bool(rev):
-        raise ValueError('must set branch_name or rev')
+    if not bool(branch) ^ bool(rev):
+        raise ValueError('must set branch or rev')
 
     if isinstance(repo_subtrees, str):
         raise TypeError(repo_subtrees)
@@ -33,7 +30,7 @@ def clone_subtree(
             '-n',
             '--depth=1',
             '--filter=tree:0',
-            *(['-b', branch_name] if branch_name else []),
+            *(['-b', branch] if branch else []),
             '--single-branch',
             repo_url,
             repo_dir,
@@ -63,47 +60,3 @@ def clone_subtree(
         ],
         cwd=rd,
     )
-
-
-def get_local_git_subtree_path(
-        *,
-        target_dir: str,
-        repo_url: str,
-        rev: str,
-        repo_subtrees: ta.Sequence[str],
-) -> str:
-    tmp_dir = tempfile.mkdtemp()
-    repo_dir = 'repo'
-
-    try:
-        clone_subtree(
-            base_dir=tmp_dir,
-            repo_url=repo_url,
-            repo_dir=repo_dir,
-            repo_subtrees=repo_subtrees,
-            rev=rev,
-        )
-
-        full_repo_dir = os.path.join(tmp_dir, repo_dir)
-        git_dir = os.path.join(full_repo_dir, '.git')
-        if not os.path.isdir(git_dir):
-            raise RuntimeError(git_dir)  # noqa
-        shutil.rmtree(git_dir)
-
-        return full_repo_dir
-
-    except Exception:
-        try:
-            shutil.rmtree(tmp_dir)
-        except Exception as e2:  # noqa
-            print(str(e2), file=sys.stderr)
-        raise
-
-
-if __name__ == '__main__':
-    print(get_local_git_subtree_path(
-        target_dir=os.path.expanduser('~/.cache/dataplay/wp_movies_10k'),
-        repo_url='https://github.com/wrmsr/deep_learning_cookbook',
-        repo_subtrees=['data/wp_movies_10k.ndjson'],
-        rev='138a99b09ffa3a728d261e461440f029e512ac93',
-    ))
