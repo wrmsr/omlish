41e5697f77c63ff61cc92a2a9eccab2f5f6eeee2
wrmsr <timwilloney@gmail.com>
2024-09-16 15:11:43 -0400

    --
    Former-commit-id: f86638b8a58a827e757458e60c71a7b8599c99a3

--- x/embeddings/w2v.py
+++ x/embeddings/w2v.py
@@ -14,8 +14,44 @@ https://huggingface.co/NathaNn1111/word2vec-google-news-negative-300-bin/tree/ma
 hf.hf_hub_download(repo_id='NathaNn1111/word2vec-google-news-negative-300-bin', filename='GoogleNews-vectors-negative300.bin')
 
 ~/.cache/huggingface/hub/models--NathaNn1111--word2vec-google-news-negative-300-bin/snapshots/78856d4586b3a938134c9833d92139f2e056e369/GoogleNews-vectors-negative300.bin
+
+==
+
+# def demo_gensim():
+#     from gensim.models import word2vec
+#
+#     sentences = [['i', 'like', 'apple', 'pie', 'for', 'dessert'],
+#                  ['i', 'dont', 'drive', 'fast', 'cars'],
+#                  ['data', 'science', 'is', 'fun'],
+#                  ['chocolate', 'is', 'my', 'favorite'],
+#                  ['my', 'favorite', 'movie', 'is', 'predator']]
+#
+#     w2v = word2vec.Word2Vec(sentences, min_count=1, vector_size=5)
+#
+#     print(w2v)
+#
+#     print(w2v.wv['chocolate'])
+#
+#     # [-0.04609262 -0.04943436 -0.08968851 -0.08428907  0.01970964]
+#
+#     # list the vocabulary words
+#     words = list(w2v.wv.key_to_index)
+#
+#     print(words)
+#
+#     ##
+#
+#     from gensim.models import KeyedVectors
+#
+#     w2v = KeyedVectors.load_word2vec_format(BIN_FILE, binary=True)
+
 """
+import io
 import os.path
+import struct
+import typing as ta
+
+from omlish import check
 
 
 BIN_FILE = os.path.join(
@@ -27,33 +63,53 @@ BIN_FILE = os.path.join(
 )
 
 
+##
+
+
+def read_bin_file(
+        f: ta.BinaryIO,
+        *,
+        max_words: int | None = None,
+        max_vec_bytes: int | None = None,
+) -> tuple[list[str], list[ta.Sequence[float]]]:
+    buf = []
+    while (c := check.not_empty(f.read(1))) != b'\n':
+        buf.append(c)
+
+    line = b''.join(buf).decode('utf-8')
+    vocab_size, layer_size = map(int, line.strip().split())
+
+    vec_fmt = '<' + 'f' * layer_size
+
+    words: list[str] = []
+    vecs: list[ta.Sequence[float]] = []
+
+    if max_words is not None:
+        vocab_size = max(vocab_size, max_words)
+    if max_vec_bytes is not None:
+        vocab_size = min(vocab_size, max_vec_bytes // (layer_size * 4))
+
+    for i in range(vocab_size):
+        buf = []
+        while (c := check.not_empty(f.read(1))) != b' ':
+            # ignore newlines in front of words (some binary files have newline, some don't)
+            if c != b'\n':
+                buf.append(c)
+        word = b''.join(buf).decode('utf-8')
+        words.append(word)
+
+        vec_buf = f.read(4 * layer_size)
+        vec = struct.unpack(vec_fmt, vec_buf)
+        vecs.append(vec)
+
+    return words, vecs
+
+
 def _main():
-    from gensim.models import word2vec
+    with open(BIN_FILE, 'rb') as f:
+        words, vecs = read_bin_file(f, max_vec_bytes=0x3fffffff)
 
-    sentences = [['i', 'like', 'apple', 'pie', 'for', 'dessert'],
-                 ['i', 'dont', 'drive', 'fast', 'cars'],
-                 ['data', 'science', 'is', 'fun'],
-                 ['chocolate', 'is', 'my', 'favorite'],
-                 ['my', 'favorite', 'movie', 'is', 'predator']]
-
-    w2v = word2vec.Word2Vec(sentences, min_count=1, vector_size=5)
-
-    print(w2v)
-
-    print(w2v.wv['chocolate'])
-
-    # [-0.04609262 -0.04943436 -0.08968851 -0.08428907  0.01970964]
-
-    # list the vocabulary words
-    words = list(w2v.wv.key_to_index)
-
-    print(words)
-
-    ##
-
-    from gensim.models import KeyedVectors
-
-    w2v = KeyedVectors.load_word2vec_format(BIN_FILE, binary=True)
+    print(len(words))
 
 
 if __name__ == '__main__':
