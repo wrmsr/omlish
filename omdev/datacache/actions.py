2086c474ebd381658138d060c42975ec07ee7230
wrmsr <timwilloney@gmail.com>
2024-09-17 10:13:23 -0400

    --
    Former-commit-id: 171ee1c2ab138a38d16f6a5a50d3cbbbc1836e09

---
+++ omdev/datacache/actions.py
@@ -0,0 +1,36 @@
+import typing as ta
+
+from omlish import dataclasses as dc
+from omlish import lang
+from omlish import marshal as msh
+
+from .consts import MARSHAL_VERSION
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class Action(lang.Abstract, lang.Sealed):
+    marshal_version: int = dc.field(default=MARSHAL_VERSION, kw_only=True)
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class ExtractAction(Action, lang.Final):
+    files: ta.Sequence[str]
+
+
+##
+
+
+@lang.cached_function
+def _install_standard_marshalling() -> None:
+    actions_poly = msh.polymorphism_from_subclasses(Action)
+    msh.STANDARD_MARSHALER_FACTORIES[0:0] = [msh.PolymorphismMarshalerFactory(actions_poly)]
+    msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [msh.PolymorphismUnmarshalerFactory(actions_poly)]
+
+
+_install_standard_marshalling()
