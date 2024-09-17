32711710ae59eb841585a240ce9427d107da2871
wrmsr <timwilloney@gmail.com>
2024-09-17 11:11:28 -0400

    --
    Former-commit-id: 6324c2709878c1436f2bcfe75d0baf9a429f9da0

--- x/compcache.py
+++ x/compcache.py
@@ -1,5 +1,9 @@
 """
-compute cache
+TODO:
+ - decorator
+ - thread local cache instance - but shared
+ - filesystem OPTIONAL
+ - locking
  - Keyer scheme
  - per-module-ish CACHE_VERSION convention
  - are pickles stable?
@@ -8,30 +12,73 @@ compute cache
  - ttl
  - nice to have: np mmap
  - compress?
+ - decos, deescriptors, etc
 """
 import tempfile
+import typing as ta
+
+from omlish import collections as col
+from omlish import dataclasses as dc
+from omlish import lang
+
+
+T = ta.TypeVar('T')
 
 
 ##
+
+
+@dc.dataclass(frozen=True)
+class CacheKey(lang.Final):
+    fn: ta.Callable
+    args: tuple
+    kwargs: col.frozendict[str, ta.Any]
+
+    @dc.validate
+    def _check_types(self) -> bool:
+        return (
+                callable(self.fn) and
+                isinstance(self.args, tuple) and
+                isinstance(self.kwargs, col.frozendict)
+        )
 
 
 class Cache:
     def __init__(self, base_dir: str) -> None:
         super().__init__()
 
+        self._base_dir = base_dir
+
+        self._dct: dict[CacheKey, ta.Any] = {}
+
+    def get(self, key: CacheKey) -> ta.Any:
+        raise NotImplementedError
+
+    def __call__(self, fn: ta.Callable[..., T], *args: ta.Any, **kwargs: ta.Any) -> T:
+        key = CacheKey(fn, args, col.frozendict(kwargs))
+        try:
+            return self._dct[key]
+        except KeyError:
+            pass
+        ret = fn(*args, **kwargs)
+        self._dct[key] = ret
+        return ret
+
 
 ##
 
 
 def f(x: int, y: int) -> int:
+    print(f'f({x}, {y})')
     return x + y
 
 
 def g(x: int, y: int) -> int:
+    print(f'g({x}, {y})')
     return f(x, 1) + f(y, 1)
 
 
-def _main():
+def _main() -> None:
     assert g(1, 2) == 5
 
     #
@@ -40,6 +87,11 @@ def _main():
     print(f'{tmp_dir=}')
 
     cache = Cache(tmp_dir)
+
+    #
+
+    for _ in range(2):
+        assert cache(g, 1, 2) == 5
 
 
 if __name__ == '__main__':
