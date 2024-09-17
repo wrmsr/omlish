61e51df402aff8bca0235d8285524fe8048fb589
wrmsr <timwilloney@gmail.com>
2024-09-17 09:35:23 -0400

    --
    Former-commit-id: d8e29daf6562ced8d17b94e6322696d789775749

--- ominfra/__about__.py
+++ ominfra/__about__.py
@@ -25,13 +25,3 @@ class Setuptools(SetuptoolsBase):
         'include': [Project.name, f'{Project.name}.*'],
         'exclude': [*SetuptoolsBase.find_packages['exclude']],
     }
-
-    package_data = {
-        **SetuptoolsBase.package_data,
-
-        '*': [
-            *SetuptoolsBase.package_data['*'],
-
-            'Dockerfile',
-        ],
-    }
