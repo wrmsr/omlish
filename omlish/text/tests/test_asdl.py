36b4f538fc924dd431d1d44955cd121c0cfa53f3
wrmsr <timwilloney@gmail.com>
2024-09-18 14:19:38 -0700

    --

--- omlish/text/tests/test_asdl.py
+++ omlish/text/tests/test_asdl.py
@@ -1,5 +1,5 @@
-from ... import lang
 from .. import asdl
+from ... import lang
 
 
 def test_asdl():
@@ -7,27 +7,42 @@ def test_asdl():
     flat_nodes = asdl.flatten(asdl.AsdlParser().parse(asdl_src))
     node_fields = asdl.build_fields_info(flat_nodes)
     assert node_fields == {
-        'expr': {
+        'BinOp': {
+            'col_offset': ('int', 1),
+            'end_col_offset': ('int', '?'),
+            'end_lineno': ('int', '?'),
+            'left': ('expr', 1),
+            'lineno': ('int', 1),
+            'op': ('operator', 1),
+            'right': ('expr', 1),
+        },
+        'BoolOp': {
             'col_offset': ('int', 1),
             'end_col_offset': ('int', '?'),
             'end_lineno': ('int', '?'),
             'lineno': ('int', 1),
-        },
-        'BoolOp': {
             'op': ('boolop', 1),
             'values': ('expr', '*'),
         },
-        'BinOp': {
-            'left': ('expr', 1),
-            'op': ('operator', 1),
-            'right': ('expr', 1),
+        'Constant': {
+            'col_offset': ('int', 1),
+            'end_col_offset': ('int', '?'),
+            'end_lineno': ('int', '?'),
+            'kind': ('string', '?'),
+            'lineno': ('int', 1),
+            'value': ('constant', 1),
         },
         'UnaryOp': {
+            'col_offset': ('int', 1),
+            'end_col_offset': ('int', '?'),
+            'end_lineno': ('int', '?'),
+            'lineno': ('int', 1),
             'op': ('unaryop', 1),
             'operand': ('expr', 1),
         },
-        'Constant': {
-            'kind': ('string', '?'),
-            'value': ('constant', 1),
-        },
+        'expr': {
+            'col_offset': ('int', 1),
+            'end_col_offset': ('int', '?'),
+            'end_lineno': ('int', '?'),
+            'lineno': ('int', 1)},
     }
