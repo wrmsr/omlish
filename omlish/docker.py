7be2febd286bf66cdbd39161f6e7f98958266c4e
wrmsr <timwilloney@gmail.com>
2024-09-18 17:15:14 -0700

    --

--- omlish/docker.py
+++ omlish/docker.py
@@ -170,9 +170,9 @@ _LIKELY_IN_DOCKER_PATTERN = re.compile(r'^overlay / .*/docker/')
 
 
 def is_likely_in_docker() -> bool:
-    if sys.platform != 'linux':
+    if getattr(sys, 'platform') != 'linux':
         return False
-    with open('/proc/mounts') as f:  # type: ignore
+    with open('/proc/mounts') as f:
         ls = f.readlines()
     return any(_LIKELY_IN_DOCKER_PATTERN.match(l) for l in ls)
 
