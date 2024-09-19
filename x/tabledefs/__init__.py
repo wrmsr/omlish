e049ee0860753ea1ffc556713618a82bc6b005d5
wrmsr <timwilloney@gmail.com>
2024-09-19 16:47:10 -0700

    --

---
+++ x/tabledefs/__init__.py
@@ -0,0 +1,11 @@
+from .tabledefs import (  # noqa
+    TableDef,
+)
+
+
+##
+
+
+from omlish.lang.imports import _register_conditional_import  # noqa
+
+_register_conditional_import('omlish.marshal', '.marshal', __package__)
--- x/tabledefs/marshal.py
+++ x/tabledefs/marshal.py
@@ -1,9 +1,20 @@
+from omlish import lang
+from omlish import marshal as msh
+
+from .datatypes import Datatype
+from .elements import Element
+
+
 def _install_poly(cls: type) -> None:
     p = msh.polymorphism_from_subclasses(cls, naming=msh.Naming.SNAKE)
     msh.STANDARD_MARSHALER_FACTORIES[0:0] = [msh.PolymorphismMarshalerFactory(p)]
     msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [msh.PolymorphismUnmarshalerFactory(p)]
 
 
-_install_poly(Datatype)
-_install_poly(Element)
+@lang.cached_function
+def _install_standard_marshalling() -> None:
+    _install_poly(Datatype)
+    _install_poly(Element)
 
+
+_install_standard_marshalling()
