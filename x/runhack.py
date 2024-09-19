94fa242aae1d0005c59486a275a12fad577ebf49
wrmsr <timwilloney@gmail.com>
2024-09-19 11:30:36 -0700

    --

--- x/runhack.py
+++ x/runhack.py
@@ -1,4 +1,10 @@
 """
+*** THIS GOES IN OMDEV lol ***
+
+pycharm 242.21829.153
+
+==
+
 https://docs.python.org/3/library/site.html
 
 https://github.com/xolox/python-coloredlogs/blob/65bdfe976ac0bf81e8c0bd9a98242b9d666b2859/setup.py#L64
@@ -14,6 +20,164 @@ sys.orig_argv=['/Users/spinlock/.pyenv/versions/3.12.6/Library/Frameworks/Python
 
 sys.argv=['/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pydev/pydevd.py', '--multiprocess', '--qt-support=auto', '--client', '127.0.0.1', '--port', '64687', '--file', '/Users/spinlock/src/wrmsr/omlish/x/dp/dp20240312_llamafs/lfs.py']
 sys.orig_argv=['/Users/spinlock/.pyenv/versions/3.12.6/Library/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/Python', '-X', 'pycache_prefix=/Users/spinlock/Library/Caches/JetBrains/PyCharm2024.2/cpython-cache', '/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pydev/pydevd.py', '--multiprocess', '--qt-support=auto', '--client', '127.0.0.1', '--port', '64687', '--file', '/Users/spinlock/src/wrmsr/omlish/x/dp/dp20240312_llamafs/lfs.py']
+
+==
+BUSTED:
+
+sys.argv=[
+  '/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pydev/pydevd.py',
+  '--multiprocess',
+  '--qt-support=auto',
+  '--client',
+  '127.0.0.1',
+  '--port',
+  '64701',
+  '--file',
+  '/Users/spinlock/src/wrmsr/omlish/x/dp/dp20240312_llamafs/lfs.py',
+]
+sys.orig_argv=[
+  '/Users/spinlock/.pyenv/versions/3.12.6/Library/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/Python',
+  '-X',
+  'pycache_prefix=/Users/spinlock/Library/Caches/JetBrains/PyCharm2024.2/cpython-cache',
+  '/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pydev/pydevd.py',
+  '--multiprocess',
+  '--qt-support=auto',
+  '--client',
+  '127.0.0.1',
+  '--port',
+  '64701',
+  '--file',
+  '/Users/spinlock/src/wrmsr/omlish/x/dp/dp20240312_llamafs/lfs.py',
+]
+os.getcwd()='/Users/spinlock/src/wrmsr/omlish/x/dp/dp20240312_llamafs'
+sorted(os.environ)=[
+  'ASYNCIO_DEBUGGER_ENV',
+  'COMMAND_MODE',
+  'DISPLAY',
+  'DOCKER_CLI_HINTS',
+  'HALT_VARIABLE_RESOLVE_THREADS_ON_STEP_RESUME',
+  'HOME',
+  'HOMEBREW_CELLAR',
+  'HOMEBREW_PREFIX',
+  'HOMEBREW_REPOSITORY',
+  'IDE_PROJECT_ROOTS',
+  'INFOPATH',
+  'IPYTHONENABLE',
+  'LC_CTYPE',
+  'LESS',
+  'LIBRARY_ROOTS',
+  'LOGNAME',
+  'LSCOLORS',
+  'LS_COLORS',
+  'OLDPWD',
+  'PAGER',
+  'PATH',
+  'PS1',
+  'PWD',
+  'PYCHARM_DISPLAY_PORT',
+  'PYCHARM_HOSTED',
+  'PYCHARM_INTERACTIVE_PLOTS',
+  'PYCHARM_PROJECT_ID',
+  'PYDEVD_LOAD_VALUES_ASYNC',
+  'PYTHONIOENCODING',
+  'PYTHONPATH',
+  'PYTHONUNBUFFERED',
+  'SHELL',
+  'TMPDIR',
+  'USER',
+  'USE_LOW_IMPACT_MONITORING',
+  'VIRTUAL_ENV',
+  'WASIENV_DIR',
+  'WASMER_CACHE_DIR',
+  'WASMER_DIR',
+  'XPC_FLAGS',
+  'XPC_SERVICE_NAME',
+  'ZSH',
+  '__CFBundleIdentifier',
+  '__CF_USER_TEXT_ENCODING'
+]
+
+==
+BUSTED:
+
+sys.argv=[
+  '/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pydev/pydevd.py',
+  '--multiprocess',
+  '--qt-support=auto',
+  '--client',
+  '127.0.0.1',
+  '--port',
+  '64722',
+  '--file',
+  '/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pycharm/_jb_pytest_runner.py',
+  '--path',
+  '/Users/spinlock/src/wrmsr/omlish/omlish/lifecycles/tests/test_lifecycles.py',
+]
+sys.orig_argv=[
+  '/Users/spinlock/.pyenv/versions/3.12.6/Library/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/Python',
+  '-X',
+  'pycache_prefix=/Users/spinlock/Library/Caches/JetBrains/PyCharm2024.2/cpython-cache',
+  '/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pydev/pydevd.py',
+  '--multiprocess',
+  '--qt-support=auto',
+  '--client',
+  '127.0.0.1',
+  '--port',
+  '64722',
+  '--file',
+  '/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pycharm/_jb_pytest_runner.py',
+  '--path',
+  '/Users/spinlock/src/wrmsr/omlish/omlish/lifecycles/tests/test_lifecycles.py',
+]
+os.getcwd()='/Users/spinlock/src/wrmsr/omlish/omlish'
+sorted(os.environ)=[
+  'ASYNCIO_DEBUGGER_ENV',
+  'COMMAND_MODE',
+  'DISPLAY',
+  'DOCKER_CLI_HINTS',
+  'HALT_VARIABLE_RESOLVE_THREADS_ON_STEP_RESUME',
+  'HOME',
+  'HOMEBREW_CELLAR',
+  'HOMEBREW_PREFIX',
+  'HOMEBREW_REPOSITORY',
+  'IDE_PROJECT_ROOTS',
+  'INFOPATH',
+  'IPYTHONENABLE',
+  'LC_CTYPE',
+  'LESS',
+  'LIBRARY_ROOTS',
+  'LOGNAME',
+  'LSCOLORS',
+  'LS_COLORS',
+  'OLDPWD',
+  'PAGER',
+  'PATH',
+  'PS1',
+  'PWD',
+  'PYCHARM_DISPLAY_PORT',
+  'PYCHARM_HELPERS_DIR',
+  'PYCHARM_HOSTED',
+  'PYCHARM_INTERACTIVE_PLOTS',
+  'PYCHARM_PROJECT_ID',
+  'PYDEVD_LOAD_VALUES_ASYNC',
+  'PYTEST_RUN_CONFIG',
+  'PYTHONIOENCODING',
+  'PYTHONPATH',
+  'PYTHONUNBUFFERED',
+  'SHELL',
+  'TMPDIR',
+  'USER',
+  'USE_LOW_IMPACT_MONITORING',
+  'VIRTUAL_ENV',
+  'WASIENV_DIR',
+  'WASMER_CACHE_DIR',
+  'WASMER_DIR',
+  'XPC_FLAGS',
+  'XPC_SERVICE_NAME',
+  'ZSH',
+  '__CFBundleIdentifier',
+  '__CF_USER_TEXT_ENCODING',
+]
 
 """
 import os
@@ -31,7 +195,8 @@ def _run() -> None:
 
     print(f'{sys.argv=}')
     print(f'{sys.orig_argv=}')
-    print(f'{os.getcwd()}')
+    print(f'{os.getcwd()=}')
+    print(f'{sorted(os.environ)=}')
 
     # breakpoint()
 
