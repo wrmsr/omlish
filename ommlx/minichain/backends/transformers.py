1fa0f451956c89a2e453b32aa53437e64a4121e3
wrmsr <timwilloney@gmail.com>
2024-09-23 14:42:09 -0700

    --

--- ommlx/minichain/backends/transformers.py
+++ ommlx/minichain/backends/transformers.py
@@ -32,9 +32,13 @@ class TransformersPromptModel(PromptModel_):
         pipeline = transformers.pipeline(
             'text-generation',
             model=self.model,
-            device='mps' if sys.platform == 'darwin' else 'cuda',
-            token=os.environ.get('HUGGINGFACE_HUB_TOKEN'),
-            **(self.kwargs or {}),
+            **{
+                **dict(
+                    device='mps' if sys.platform == 'darwin' else 'cuda',
+                    token=os.environ.get('HUGGINGFACE_HUB_TOKEN'),
+                ),
+                **(self.kwargs or {}),
+            },
         )
         output = pipeline(request.v.s)
         return Response(output)
