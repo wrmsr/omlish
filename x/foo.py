b026a5a3504d8de2d9d9916259f990c985c4185a
wrmsr <timwilloney@gmail.com>
2024-09-23 11:04:00 -0700

    --

--- x/foo.py
+++ x/foo.py
@@ -1,5 +1,16 @@
+import dataclasses as dc
+
+
 SOME_CONST = 420
 
 
+@dc.dataclass(frozen=True)
+class SomeManifestThing:
+    some_const: int
+
+
 # @omlish-manifest
-_FOO_MANIFEST = {'$some_manifest_thing': {'some_const': SOME_CONST}}
+_FOO_MANIFEST = SomeManifestThing(some_const=SOME_CONST)
+
+# @omlish-manifest
+_FOO_MANIFEST2 = {'$x.foo.SomeManifestThing': {'some_const': SOME_CONST}}
