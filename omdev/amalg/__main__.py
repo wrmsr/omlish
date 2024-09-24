d2362ef920cef596fc60adf868d746213a7ef2bb
wrmsr <timwilloney@gmail.com>
2024-09-24 15:23:04 -0700

    --

--- omdev/amalg/__main__.py
+++ omdev/amalg/__main__.py
@@ -1,3 +1,10 @@
+from ..cli import CliModule
+
+
+# @omlish-manifest
+_CLI_MODULE = CliModule('amalg', __name__)
+
+
 if __name__ == '__main__':
     from .amalg import _main
 
--- omdev/interp/__main__.py
+++ omdev/interp/__main__.py
@@ -1,3 +1,10 @@
+from ..cli import CliModule
+
+
+# @omlish-manifest
+_CLI_MODULE = CliModule('interp', __name__)
+
+
 if __name__ == '__main__':
     from .cli import _main
 
