ceca205a83ff9d0659a3cf5136b6d9e5dbe1c2c6
wrmsr <timwilloney@gmail.com>
2024-09-25 14:56:02 -0700

    --

--- omlish/bootstrap/__main__.py
+++ omlish/bootstrap/__main__.py
@@ -1,3 +1,10 @@
+# @omlish-manifest
+_CLI_MODULE = {'$omdev.cli.types.CliModule': {
+    'cmd_name': 'bootstrap',
+    'mod_name': __name__,
+}}
+
+
 if __name__ == '__main__':
     from .main import _main
 
