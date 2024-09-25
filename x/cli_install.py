493453a5e1269459a9196742e5314a224fff3195
wrmsr <timwilloney@gmail.com>
2024-09-25 16:43:57 -0700

    --

--- x/cli_install.py
+++ x/cli_install.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 """
+TODO:
+ - usable as 'curl -LsSf https://raw.githubusercontent.com/wrmsr/omlish/master/x/cli_install.py | python3'
+ - used for self-reinstall, preserving non-root dists
+
+==
+
 if uv --version 2>/dev/null ; then \
     MGR="uv tool" ; \
-    INST="install --prerelease=allow --python=${INSTALL_PYTHON_VERSION}"; \
+    INST="install --refresh --prerelease=allow --python=${INSTALL_PYTHON_VERSION}"; \
     INJ="--with" ; \
 else \
     MGR="pipx" ; \
@@ -16,6 +22,8 @@ for D in ${INSTALL_EXTRAS} ; do \
 done ; \
 ${SHELL} -c "$$CMD"
 """
+import itertools
+import subprocess
 import sys
 
 
@@ -23,7 +31,44 @@ def _main() -> None:
     if sys.version_info < (3, 8):
         raise RuntimeError(f'Unsupported python version: {sys.version_info}')
 
-    raise NotImplementedError
+    subprocess.check_call(['uv', '--version'])
+
+    cli = 'omdev-cli'
+
+    py_ver = '3.12'
+
+    pkgs = [
+        'ominfra',
+        'ommlx',
+        'omserv',
+    ]
+
+    extras = [
+        'openai',
+    ]
+
+    out = subprocess.check_output(['uv', 'tool', 'list']).decode()
+    inst = {
+        s.partition(' ')[0]
+        for l in out.splitlines()
+        if (s := l.strip())
+        and not s.startswith('-')
+    }
+    if cli in inst:
+        subprocess.check_call([
+            'uv', 'tool',
+            'uninstall', cli,
+        ])
+
+    subprocess.run([
+        'uv', 'tool',
+        'install',
+        '--refresh',
+        '--prerelease=allow',
+        f'--python={py_ver}',
+        cli,
+        *itertools.chain.from_iterable(['--with', e] for e in extras),
+    ])
 
 
 if __name__ == '__main__':
