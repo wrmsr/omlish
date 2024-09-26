003081435d7dd607f10bbd9c516f58f53c848142
wrmsr <timwilloney@gmail.com>
2024-09-25 17:47:09 -0700

    --

--- x/lsp.py
+++ x/lsp.py
@@ -1,4 +1,12 @@
 """
 https://stackoverflow.com/a/77482983
 https://github.com/microsoft/monitors4codegen#4-multilspy
+
+!! https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/
+
+https://github.com/python-lsp/python-lsp-server
+
+https://github.com/pappasam/jedi-language-server
+https://github.com/DetachHead/basedpyright
+https://github.com/mtshiba/pylyzer
 """
