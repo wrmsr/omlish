5a00a368992181faad97c9c036822c90c5051ab7
wrmsr <timwilloney@gmail.com>
2024-09-27 10:41:15 -0700

    --

---
+++ omdev/scripts/exectime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# @omlish-script
+import sys
+import time
+
+
+# @omlish-manifest
+_CLI_MODULE = {'$omdev.cli.types.CliModule': {
+    'cmd_name': 'exectime',
+    'mod_name': __name__,
+}}
+
+
+def _main() -> None:
+    [src] = sys.argv[1:]
+    co = compile(src, '<string>', 'exec')
+    start = time.time_ns()
+    exec(co)
+    end = time.time_ns()
+    print(end - start)
+
+
+if __name__ == '__main__':
+    _main()
