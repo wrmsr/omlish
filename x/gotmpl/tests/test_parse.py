388fbbb17987faa2581783d2557b6017b2a6baba
wrmsr <timwilloney@gmail.com>
2024-09-27 11:32:24 -0700

    --

--- x/gotmpl/tests/test_parse.py
+++ x/gotmpl/tests/test_parse.py
@@ -1,11 +1,40 @@
+import operator
 import textwrap
+import typing as ta
+
+from omlish import lang
 from ..parse import parse
+
+
+BUILTINS: ta.Mapping[str, ta.Callable] = {
+    "and": operator.and_,
+    "call": lang.void,  # emptyCall,
+    "html": lang.void,  # HTMLEscaper,
+    "index": lang.void,  # index,
+    "slice": slice,
+    "js": lang.void,  # JSEscaper,
+    "len": len,
+    "not": operator.not_,
+    "or": operator.or_,
+    "print": lang.void,  # fmt.Sprint,
+    "printf": lang.void,  # fmt.Sprintf,
+    "println": lang.void,  # fmt.Sprintln,
+    "urlquery": lang.void,  # URLQueryEscaper,
+
+    # Comparisons
+    "eq": operator.eq,  # ==
+    "ge": operator.ge,  # >=
+    "gt": operator.gt,  # >
+    "le": operator.le,  # <=
+    "lt": operator.lt,  # <
+    "ne": operator.ne,  # !=
+}
 
 
 def test_parse():
     for s in [
         'hi',
-        '{{ hi }} there',
+        # '{{ hi }} there',
         textwrap.dedent("""
             {{- range .Messages }}GPT4 Correct
             {{- if eq .Role "system" }} System:
@@ -15,5 +44,9 @@ def test_parse():
             {{- end }}GPT4 Correct Assistant:
         """),
     ]:
-        t = parse('-', s)['-']
+        t = parse(
+            '-',
+            s,
+            funcs=dict(BUILTINS),
+        )['-']
         print(t)
