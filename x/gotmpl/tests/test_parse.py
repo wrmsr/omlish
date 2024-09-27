0c2e1a06fcaa5c2cc8a960480ec59d001f1d7e25
wrmsr <timwilloney@gmail.com>
2024-09-27 11:49:40 -0700

    --

--- x/gotmpl/tests/test_parse.py
+++ x/gotmpl/tests/test_parse.py
@@ -37,21 +37,21 @@ BUILTINS: ta.Mapping[str, ta.Callable] = {
 
 def test_parse():
     for s in [
-        'hi',
-
-        textwrap.dedent("""
-            {{- range .Messages }}GPT4 Correct
-            {{- if eq .Role "system" }} System:
-            {{- else if eq .Role "user" }} User:
-            {{- else if eq .Role "assistant" }} Assistant:
-            {{- end }} {{ .Content }}<|end_of_turn|>
-            {{- end }}GPT4 Correct Assistant:
-        """),
-
-        "",
-        "{{/*\n\n\n*/}}",
-        " \t\n",
-        "some text",
+        # 'hi',
+        #
+        # textwrap.dedent("""
+        #     {{- range .Messages }}GPT4 Correct
+        #     {{- if eq .Role "system" }} System:
+        #     {{- else if eq .Role "user" }} User:
+        #     {{- else if eq .Role "assistant" }} Assistant:
+        #     {{- end }} {{ .Content }}<|end_of_turn|>
+        #     {{- end }}GPT4 Correct Assistant:
+        # """),
+        #
+        # "",
+        # "{{/*\n\n\n*/}}",
+        # " \t\n",
+        # "some text",
         "{{}}",
         "{{.X}}",
         "{{printf}}",
