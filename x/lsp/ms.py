28caafe2c4fa5e2be34a28c045dc2d9639258562
wrmsr <timwilloney@gmail.com>
2024-09-30 13:31:00 -0700

    --

--- x/lsp/ms.py
+++ x/lsp/ms.py
@@ -13,7 +13,11 @@ https://github.com/DetachHead/basedpyright
 https://github.com/mtshiba/pylyzer
 
 https://github.com/microsoft/pyright/blob/main/docs/mypy-comparison.md
+
+https://github.com/jecki/ts2python
+https://github.com/predragnikolic/OLSP
 """
+import asyncio
 import os
 
 from multilspy.multilspy_logger import MultilspyLogger
@@ -41,13 +45,21 @@ class JediServer(LanguageServer):
         )
 
 
-def _main() -> None:
-    config = MultilspyConfig.from_dict({"code_language": "java"})  # Also supports "python", "rust", "csharp"
+async def _main() -> None:
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    os.chdir(root_dir)
+
+    config = MultilspyConfig.from_dict({
+        "code_language": "python",  # Also supports "python", "rust", "csharp"
+        "trace_lsp_communication": True,
+    })
     logger = MultilspyLogger()
-    lsp = JediServer.create(config, logger, os.getcwd())
-    with lsp.start_server():
-        result = lsp.request_definition
+    lsp = JediServer.create(config, logger, root_dir)
+
+    async with lsp.start_server():
+        result = await lsp.request_definition(os.path.relpath(__file__, root_dir), 25, 17)
+        print(result)
 
 
 if __name__ == '__main__':
-    _main()
+    asyncio.run(_main())
