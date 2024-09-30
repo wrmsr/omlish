f8eefd8c0104369651ba2dda803f707e4006b072
wrmsr <timwilloney@gmail.com>
2024-09-30 13:06:21 -0700

    --

--- omdev/cli/clicmds.py
+++ omdev/cli/clicmds.py
@@ -1,7 +1,12 @@
 import argparse
+import inspect
+import os
+import subprocess
+import sys
 
 from omlish import __about__
 
+from . import install
 from .types import CliModule
 
 
@@ -11,6 +16,41 @@ def _print_version(args) -> None:
 
 def _print_revision(args) -> None:
     print(__about__.__revision__)
+
+
+def _reinstall(args) -> None:
+    mod_name = globals()['__spec__'].name
+    tool_name = '.'.join([mod_name.partition('.')[0], 'tools', 'piptools'])
+
+    out = subprocess.check_output([
+        sys.executable,
+        '-m',
+        tool_name,
+        'list-root-dists',
+    ]).decode()
+
+    deps = sorted(
+        {s for l in out.splitlines() if (s := l.strip())}
+        | set(args.extra_deps or [])
+        - {install.DEFAULT_CLI_PKG}  # noqa
+    )
+
+    if deps:
+        print('Reinstalling with following additional dependencies:')
+        print('\n'.join('  ' + d for d in deps))
+    else:
+        print('No additional dependencies detected.')
+    print()
+    print('Continue with reinstall? (ctrl-c to cancel)')
+    input()
+
+    install_src = inspect.getsource(install)
+
+    os.execl(
+        sys.executable,
+        '-c',
+        install_src,
+    )
 
 
 # @omlish-manifest
@@ -26,6 +66,10 @@ def _main(argv=None) -> None:
 
     parser_revision = subparsers.add_parser('revision')
     parser_revision.set_defaults(func=_print_revision)
+
+    parser_reinstall = subparsers.add_parser('reinstall')
+    parser_reinstall.add_argument('extra_deps', nargs='*')
+    parser_reinstall.set_defaults(func=_reinstall)
 
     args = parser.parse_args(argv)
     if not getattr(args, 'func', None):
