a4efd731d792946ca908c86b0efddcf143bf8b19
wrmsr <timwilloney@gmail.com>
2024-09-30 13:51:00 -0700

    --

---
+++ x/lsp/protocol.py
@@ -0,0 +1,6 @@
+"""
+https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/
+https://github.com/microsoft/vscode-languageserver-node/blob/1f624bd492f23c7c562937d1759e3c86d95e3555/protocol/src/common/protocol.ts#L986
+https://github.com/jecki/ts2python
+https://github.com/predragnikolic/OLSP
+"""
