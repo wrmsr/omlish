ceb2af0d7c412d8209f8767ada11826909b390f9
wrmsr <timwilloney@gmail.com>
2024-10-01 13:49:22 -0700

    --

--- omlish/sql/tests/dbs.py
+++ omlish/sql/tests/dbs.py
@@ -1,6 +1,7 @@
 import typing as ta
 
 from ... import check
+from ... import lang
 from ...docker import get_compose_port
 from ...docker import is_likely_in_docker
 from ...docker.tests.services import ComposeServices
@@ -16,40 +17,50 @@ class Dbs:
         super().__init__()
         self._compose_services = compose_services
 
+    @lang.cached_function
+    def _in_docker(self) -> bool:
+        return is_likely_in_docker()
+
+    def _build_mysql_db(self, name: str, svc: ta.Mapping[str, ta.Any]) -> DbSpec:
+        if self._in_docker():
+            host = self._compose_services.prefix + name
+            port = DbTypes.MYSQL.default_port
+        else:
+            host = '127.0.0.1'
+            port = get_compose_port(svc, check.not_none(DbTypes.MYSQL.default_port))
+
+        env = svc['environment']
+        return DbSpec(
+            name,
+            DbTypes.MYSQL,
+            UrlDbLoc(f'mysql://root:{env["MYSQL_ROOT_PASSWORD"]}@{host}:{port}'),
+        )
+
+    def _build_postgres_db(self, name: str, svc: ta.Mapping[str, ta.Any]) -> DbSpec:
+        if self._in_docker():
+            host = self._compose_services.prefix + name
+            port = DbTypes.POSTGRES.default_port
+        else:
+            host = '127.0.0.1'
+            port = get_compose_port(svc, check.not_none(DbTypes.POSTGRES.default_port))
+
+        env = svc['environment']
+        return DbSpec(
+            name,
+            DbTypes.POSTGRES,
+            UrlDbLoc(f'postgresql://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@{host}:{port}'),
+        )
+
     def specs(self) -> ta.Mapping[str, DbSpec]:
         svcs = self._compose_services.config().get_services()
         lst: list[DbSpec] = []
 
-        in_docker = is_likely_in_docker()
+        for name in ['mysql']:
+            if (svc := svcs.get(name)):
+                lst.append(self._build_mysql_db(name, svc))
 
-        if (mysql := svcs.get('mysql')):
-            if in_docker:
-                host = self._compose_services.prefix + 'mysql'
-                port = DbTypes.MYSQL.default_port
-            else:
-                host = '127.0.0.1'
-                port = get_compose_port(mysql, check.not_none(DbTypes.MYSQL.default_port))
-
-            env = mysql['environment']
-            lst.append(DbSpec(
-                'mysql',
-                DbTypes.MYSQL,
-                UrlDbLoc(f'mysql://root:{env["MYSQL_ROOT_PASSWORD"]}@{host}:{port}'),
-            ))
-
-        if (postgres := svcs.get('postgres')):
-            if in_docker:
-                host = self._compose_services.prefix + 'postgres'
-                port = DbTypes.POSTGRES.default_port
-            else:
-                host = '127.0.0.1'
-                port = get_compose_port(postgres, check.not_none(DbTypes.POSTGRES.default_port))
-
-            env = postgres['environment']
-            lst.append(DbSpec(
-                'postgres',
-                DbTypes.POSTGRES,
-                UrlDbLoc(f'postgresql://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@{host}:{port}'),
-            ))
+        for name in ['postgres', 'pgvector']:
+            if (svc := svcs.get(name)):
+                lst.append(self._build_postgres_db(name, svc))
 
         return {s.name: s for s in lst}
