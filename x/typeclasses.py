42a098288acfe5d7234c083658114b7527b9ccc9
wrmsr <timwilloney@gmail.com>
2024-10-01 10:40:16 -0700

    --

--- x/typeclasses.py
+++ x/typeclasses.py
@@ -4,8 +4,19 @@ TODO:
  - every method is a dispatch.method, call unbound - Doubler.double(5)
   - !! doesn't dispatch on first arg, dispatches on T (if present)
  - cache instances, invalidate on register
+
+==
+
+# TODO: assert Double.double(5) == 10 -- dispatch.method
+# for a, v in cls.__dict__.items():
+#     if getattr(v, '__isabstractmethod__', False):
+#         continue
+#
+#     raise NotImplementedError
+
 """
 import abc
+import dataclasses as dc
 import typing as ta
 
 from omlish import check
@@ -18,47 +29,87 @@ TypeclassT = ta.TypeVar('TypeclassT', bound='Typeclass')
 
 
 class Typeclass(abc.ABC, ta.Generic[T]):
-    _impls: ta.ClassVar[dict[rfl.Type, type['Typeclass']]]
+    @dc.dataclass(frozen=True)
+    class _Impl:
+        cls: type['Typeclass']
+        arg: rfl.Type
 
-    def __init_subclass__(cls, **kwargs):
+        _: dc.KW_ONLY
+
+        singleton: bool = False
+
+    @dc.dataclass(frozen=True)
+    class _Internals:
+        cls: type['Typeclass']
+
+        _: dc.KW_ONLY
+
+        singleton: bool = False
+
+        impls: dict[rfl.Type, 'Typeclass._Impl'] = dc.field(default_factory=dict)
+        insts: dict[rfl.Type, 'Typeclass'] = dc.field(default_factory=dict)
+
+    __typeclass_internals__: ta.ClassVar[_Internals]
+
+    def __init_subclass__(
+            cls,
+            *,
+            singleton: bool | None = None,
+            **kwargs: ta.Any,
+    ) -> None:
         super().__init_subclass__(**kwargs)
 
         if Typeclass in cls.__bases__:
-            # TODO: assert Double.double(5) == 10 -- dispatch.method
-            # for a, v in cls.__dict__.items():
-            #     if getattr(v, '__isabstractmethod__', False):
-            #         continue
-            #
-            #     raise NotImplementedError
-
-            cls._impls = {}
+            cls.__typeclass_internals__ = Typeclass._Internals(
+                cls,
+                singleton=singleton,
+            )
 
         else:
-            cls._impls = lang.access_forbidden('_impls')
+            cls.__typeclass_internals__ = lang.access_forbidden()
 
             for base in rfl.get_orig_bases(cls):
                 rty = rfl.type_(base)
                 if not (isinstance(rty, rfl.Generic) and Typeclass in rty.cls.__bases__):
                     continue
 
-                impls = rty.cls._impls  # noqa
+                intr: Typeclass._Internals = rty.cls.__typeclass_internals__  # noqa
                 arg = check.single(rty.args)
-                if arg in impls:
+
+                if arg in intr.impls:
                     raise KeyError(arg)
-                impls[arg] = cls
+
+                intr.impls[arg] = Typeclass._Impl(
+                    cls,
+                    arg,
+                    singleton=singleton,
+                )
 
     def __new__(cls, *args, **kwargs):
         if Typeclass not in cls.__bases__:
             return super().__new__(cls, *args, **kwargs)
 
-        raise NotImplementedError
+        raise TypeError(cls)
 
     class _DispatchingGenericAlias(ta._GenericAlias, _root=True):  # noqa
         def __call__(self, *args, **kwargs):
-            impl = lookup_typeclass(self.__origin__, check.single(self.__args__))
+            intr: Typeclass._Internals = self.__origin__.__typeclass_internals__  # noqa
+            arg = check.single(self.__args__)
 
-            result = impl(*args, **kwargs)
+            if intr.insts is not None:
+                try:
+                    return intr.insts[arg]
+                except KeyError:
+                    pass
+
+            impl = intr.impls[arg]
+
+            result = impl.cls(*args, **kwargs)
             result.__orig_class__ = self
+
+            if intr.singleton or impl.singleton:
+                intr.insts[arg] = result
+
             return result
 
     rfl.types._SIMPLE_GENERIC_ALIAS_TYPES.add(_DispatchingGenericAlias)  # noqa
@@ -73,10 +124,7 @@ class Typeclass(abc.ABC, ta.Generic[T]):
         return ret
 
 
-def lookup_typeclass(cls: type[TypeclassT], arg: rfl.Type) -> type[TypeclassT]:
-    return cls._impls[arg]
-
-class Doubler(Typeclass[T]):
+class Doubler(Typeclass[T], singleton=True):
     @abc.abstractmethod
     def double(self, x: T) -> T:
         raise NotImplementedError
@@ -88,7 +136,6 @@ class _(Doubler[int]):
 
 
 def _main() -> None:
-    assert lookup_typeclass(Doubler, int)().double(21) == 42
     assert Doubler[int]().double(21) == 42
     # assert Doubler.double(21) == 42
 
