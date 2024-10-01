4b05050303627dec76b922beb1e010516d9bc4d5
wrmsr <timwilloney@gmail.com>
2024-10-01 13:43:35 -0700

    --

--- omlish/docker/tests/services.py
+++ omlish/docker/tests/services.py
@@ -1,15 +1,27 @@
+import dataclasses as dc
+import typing as ta
+
 from ... import lang
 from ...testing.pytest import inject as pti
 from ..compose import ComposeConfig
 
 
+Prefix = ta.NewType('Prefix', str)
+DEFAULT_PREFIX = Prefix('omlish-')
+
+DEFAULT_CONFIG_FILE_PATH = 'docker/compose.yml'
+
+
 @pti.bind()
+@dc.dataclass(frozen=True, kw_only=True)
 class ComposeServices:
-    prefix: str = 'omlish-'
+    prefix: Prefix = DEFAULT_PREFIX
+
+    config_file_path: str = DEFAULT_CONFIG_FILE_PATH
 
     @lang.cached_function
-    def compose_config(self) -> ComposeConfig:
+    def config(self) -> ComposeConfig:
         return ComposeConfig(
             self.prefix,
-            file_path='docker/compose.yml',
+            file_path=self.config_file_path,
         )
