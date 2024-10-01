c5e83ff8e8142f91da8164d4e09fe92b4136ffc9
wrmsr <timwilloney@gmail.com>
2024-10-01 14:30:19 -0700

    --

---
+++ omlish/specs/jmespath/__main__.py
@@ -0,0 +1,11 @@
+# @omlish-manifest
+_CLI_MODULE = {'$omdev.cli.types.CliModule': {
+    'cmd_name': 'jp',
+    'mod_name': __name__,
+}}
+
+
+if __name__ == '__main__':
+    from .cli import _main
+
+    _main()
