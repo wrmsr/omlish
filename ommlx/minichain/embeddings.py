4a4e97f6f3460beef4e62783b023c652231e4acb
wrmsr <timwilloney@gmail.com>
2024-10-02 14:07:12 -0700

    --

--- ommlx/minichain/embeddings.py
+++ ommlx/minichain/embeddings.py
@@ -1,22 +1,37 @@
 import abc
 import typing as ta
 
+from omlish import dataclasses as dc
 from omlish import lang
 
 from .content import Content
 from .models import Model
 from .models import Request
 from .models import Response
+from .models import RequestOption
 from .vectors import Vector
 
 
 ##
 
 
-EmbeddingModel: ta.TypeAlias = Model[Content, Vector]
+EmbeddingInput: ta.TypeAlias = Content
+EmbeddingOutput: ta.TypeAlias = Vector
+
+EmbeddingRequestOptions: ta.TypeAlias = RequestOption
 
 
-class EmbeddingModel_(EmbeddingModel, lang.Abstract):  # noqa
+@dc.dataclass(frozen=True, kw_only=True)
+class EmbeddingRequest(Request[EmbeddingInput, EmbeddingRequestOptions], lang.Final):
+    pass
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class EmbeddingResponse(Response[EmbeddingOutput], lang.Final):
+    pass
+
+
+class EmbeddingModel(Model[EmbeddingRequest, EmbeddingRequestOptions, EmbeddingResponse], lang.Abstract):
     @abc.abstractmethod
-    def generate(self, request: Request[Content]) -> Response[Vector]:
+    def generate(self, request: EmbeddingRequest) -> EmbeddingResponse:
         raise NotImplementedError
