2dbe74f171854f66ff7dc9cf965c3656cb9381c1
wrmsr <timwilloney@gmail.com>
2024-10-02 15:53:50 -0700

    --

--- ommlx/minichain/backends/openai.py
+++ ommlx/minichain/backends/openai.py
@@ -66,6 +66,10 @@ class OpenaiChatModel(ChatModel):
             raise TypeError(m)
 
     def invoke(self, request: ChatRequest) -> ChatResponse:
+        kw: dict = dict(
+            temperature=0,
+        )
+
         response = openai.chat.completions.create(  # noqa
             model=self.model,
             messages=[
@@ -75,12 +79,12 @@ class OpenaiChatModel(ChatModel):
                 )
                 for m in request.v
             ],
-            temperature=0,
             max_tokens=1024,
             top_p=1,
             frequency_penalty=0.0,
             presence_penalty=0.0,
             stream=False,
+            **kw,
         )
 
         return ChatResponse(v=AiMessage(response.choices[0].message.content))  # type: ignore
