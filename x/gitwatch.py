67988cc069e20ae019e724e3f660d60feac78d6b
wrmsr <timwilloney@gmail.com>
2024-10-02 16:28:20 -0700

    --

--- x/gitwatch.py
+++ x/gitwatch.py
@@ -5,13 +5,15 @@ TODO:
   - !!! just gh urls are more convenient lol - blob vs tree already does the right thing
    - !!! @omlish-gitwatch https:// ...
    - dedupe, update all together - handle amalgs naively / correctly
- - TODO: watch only certain branch
+ - TODO: watch only certain branch?
+  - suffix with @ for base branch, default to default branch
 
+Targets:
  - https://github.com/jmespath-community/python-jmespath/tree/aef45e9d665de662eee31b06aeb8261e2bc8b90a
  - https://github.com/pgjones/hypercorn/tree/84d06b8cf47798d2df7722273341e720ec0ea102
  - https://github.com/pypa/packaging/blob/2c885fe91a54559e2382902dce28428ad2887be5/src/packaging/specifiers.py
  - https://github.com/pypa/packaging/blob/2c885fe91a54559e2382902dce28428ad2887be5/src/packaging/version.py
- - https://github.com/pypa/packaging/blob/cf2cbe2aec28f87c6228a6fb136c27931c9af407/src/packaging/_parser.py#L65
+ - https://github.com/pypa/packaging/blob/cf2cbe2aec28f87c6228a6fb136c27931c9af407/src/packaging/_parser.py
  - https://github.com/pypa/packaging/blob/cf2cbe2aec28f87c6228a6fb136c27931c9af407/src/packaging/utils.py
  - https://github.com/pypa/wheel/blob/7bb46d7727e6e89fe56b3c78297b3af2672bbbe2/src/wheel/wheelfile.py
  - https://github.com/python/cpython/blob/21d2a9ab2f4dcbf1be462d3b7f7a231a46bc1cb7/Parser/asdl.py
@@ -31,4 +33,5 @@ import dataclasses as dc
 class GitWatch:
     repo: str
     rev: str
-    path: str | None = None
+    path: str | None = None  # trailing slash means tree
+    base: str | None = None  # defaults to repo default branch
