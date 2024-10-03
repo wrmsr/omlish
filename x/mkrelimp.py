36d5db13f2962b418c5b4567ac8a2fd32cc7bc26
wrmsr <timwilloney@gmail.com>
2024-10-03 11:07:22 -0700

    --

--- x/mkrelimp.py
+++ x/mkrelimp.py
@@ -31,7 +31,7 @@ def interleave(sep: T, it: ta.Iterable[T]) -> ta.Iterable[T]:
 
 
 def _main() -> None:
-    mod_path = 'x/antlr_dev/_runtime'
+    mod_path = os.path.join(os.path.dirname(__file__), 'antlr_dev/_runtime')
     check.state(os.path.isdir(mod_path))
 
     mod_name = os.path.basename(mod_path)
