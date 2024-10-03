1af5f255fe7ce4b5484fae57501af96ada8a3718
wrmsr <timwilloney@gmail.com>
2024-10-03 11:32:26 -0700

    --

--- x/mkrelimp.py
+++ x/mkrelimp.py
@@ -30,59 +30,73 @@ def interleave(sep: T, it: ta.Iterable[T]) -> ta.Iterable[T]:
         yield e
 
 
-def _process_line_tks(
-        in_tks: tks.Tokens,
-        dir_path: str,
-        mod_name: str,
-        mod_path: str,
-) -> tks.Tokens:
-    lst = list(in_tks)
-    pfx = []
-    while lst and (tks.is_ws(lst[0]) or lst[0].name == 'DEDENT'):
-        pfx.append(lst.pop(0))
-    if not lst:
-        return in_tks
+class Processor:
+    def __init__(
+            self,
+            mod_name: str,
+            base_dir: str,
+    ) -> None:
+        super().__init__()
+        self._mod_name = mod_name
+        self._base_dir = base_dir
 
-    first_tk: trt.Token = lst[0]
+    def process_line_tks(
+            self,
+            in_tks: tks.Tokens,
+            src_dir: str,
+    ) -> tks.Tokens:
+        lst = list(in_tks)
+        pfx = []
+        while lst and (tks.is_ws(lst[0]) or lst[0].name == 'DEDENT'):
+            pfx.append(lst.pop(0))
+        if not lst:
+            return in_tks
 
-    if first_tk.name != 'NAME' or first_tk.src not in ('import', 'from'):
-        return in_tks
-    if not (len(lst) >= 3 and lst[2].name == 'NAME' and lst[2].src == mod_name):
-        return in_tks
+        first_tk: trt.Token = lst[0]
 
-    ##
+        if first_tk.name != 'NAME' or first_tk.src not in ('import', 'from'):
+            return in_tks
+        if not (len(lst) >= 3 and lst[2].name == 'NAME' and lst[2].src == self._mod_name):
+            return in_tks
 
-    e = indexfn(tks.is_ws, lst, 3)
-    ip = list(lst[2:e])
-    del lst[2:e]
-    ps = [t.src for t in ip if t.name == 'NAME']
+        ##
 
-    ##
+        e = indexfn(tks.is_ws, lst, 3)
+        ip = list(lst[2:e])
+        del lst[2:e]
+        ps = [t.src for t in ip if t.name == 'NAME']
 
-    rel_path = os.path.relpath(os.path.join(mod_path, *ps[1:]), dir_path)
-    sr = rel_path.split(os.sep)
-    nr = indexfn(lambda s: s != '..', sr)
-    if nr < 0:
-        ps = ['.' * (len(sr) + 1)]
-    else:
-        ps = ['.' * nr, *sr[nr:]]
+        ##
 
-    ##
+        rel_path = os.path.relpath(os.path.join(self._base_dir, *ps[1:]), src_dir)
+        sr = rel_path.split(os.sep)
+        nr = indexfn(lambda s: s != '..', sr)
+        if nr < 0:
+            ps = ['.' * (len(sr) + 1)]
+        else:
+            ps = ['.' * nr, *sr[nr:]]
 
-    lst[2:2] = interleave(
-        trt.Token(name='OP', src='.'),
-        [trt.Token(name='NAME', src=p) for p in ps],
-    )
-    lst[0:0] = pfx
-    return lst
+        ##
+
+        lst[2:2] = interleave(
+            trt.Token(name='OP', src='.'),
+            [trt.Token(name='NAME', src=p) for p in ps],
+        )
+        lst[0:0] = pfx
+        return lst
 
 
 def _main() -> None:
-    mod_path = os.path.join(os.path.dirname(__file__), 'antlr_dev/_runtime')
-    check.state(os.path.isdir(mod_path))
+    base_dir = os.path.join(os.path.dirname(__file__), 'antlr_dev/_runtime')
+    check.state(os.path.isdir(base_dir))
+    mod_name = 'antlr4'  # os.path.basename(base_dir)
 
-    mod_name = 'antlr4'  # os.path.basename(mod_path)
-    for dp, dns, fns in os.walk(mod_path):
+    p = Processor(
+        mod_name,
+        base_dir,
+    )
+
+    for dp, dns, fns in os.walk(base_dir):
         for fn in fns:
             if not fn.endswith('.py'):
                 continue
@@ -94,11 +108,9 @@ def _main() -> None:
             ts = trt.src_to_tokens(src)
             in_ls = tks.split_lines(ts)
             out_ls = [
-                _process_line_tks(
+                p.process_line_tks(
                     l,
                     dp,
-                    mod_name,
-                    mod_path,
                 )
                 for l in in_ls
             ]
