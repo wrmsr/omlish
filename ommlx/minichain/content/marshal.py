1ca7772f4479b40d40d6b2e9887d44385ddded56
wrmsr <timwilloney@gmail.com>
2024-10-08 14:53:05 -0700

    --

--- ommlx/minichain/content/marshal.py
+++ ommlx/minichain/content/marshal.py
@@ -1,11 +1,19 @@
+"""
+FIXME:
+ - str / list special case
+"""
 from omlish import lang
 from omlish import marshal as msh
 
-from .content import Content
+from .content import ExtendedContent
 
 
 @lang.static_init
 def _install_standard_marshalling() -> None:
-    content_poly = msh.polymorphism_from_subclasses(Content, naming=msh.Naming.SNAKE)
-    msh.STANDARD_MARSHALER_FACTORIES[0:0] = [msh.PolymorphismMarshalerFactory(content_poly)]
-    msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [msh.PolymorphismUnmarshalerFactory(content_poly)]
+    content_poly = msh.polymorphism_from_subclasses(ExtendedContent, naming=msh.Naming.SNAKE)
+    msh.STANDARD_MARSHALER_FACTORIES[0:0] = [
+        msh.PolymorphismMarshalerFactory(content_poly),
+    ]
+    msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [
+        msh.PolymorphismUnmarshalerFactory(content_poly),
+    ]
