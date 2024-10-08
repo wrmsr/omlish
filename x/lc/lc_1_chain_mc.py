7d1c65cbbed38d876d59662b69df5ccb34b1b582
wrmsr <timwilloney@gmail.com>
2024-10-08 14:43:06 -0700

    --

--- x/lc/lc_1_chain_mc.py
+++ x/lc/lc_1_chain_mc.py
@@ -3,6 +3,9 @@ from ommlx.minichain.chat import ChatRequest
 from ommlx.minichain.chat import SystemMessage
 from ommlx.minichain.chat import UserMessage
 from ommlx.minichain.content import Text
+from ommlx.minichain.strings import transform_strings
+from ommlx.minichain.templates import DictTemplater
+from ommlx.minichain.templates import TemplatingModel
 
 from x.lc.utils import load_secrets
 
@@ -20,6 +23,27 @@ def _main() -> None:
     ]
 
     result = model(messages)
+    print(result)
+
+    #
+
+    system_template = "Translate the following into {language}:"
+
+    prompt_template = [
+        SystemMessage(system_template),
+        UserMessage.of("{text}"),
+    ]
+
+    templater = DictTemplater({"language": "italian", "text": "hi"})
+
+    result = transform_strings(templater.apply, prompt_template)
+    print(result)
+
+    #
+
+    model = TemplatingModel(model, templater)
+
+    result = model(prompt_template)
     print(result)
 
 
