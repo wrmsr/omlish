2eef0f70da02b372c6e68c9fc8fd797dc2051cbe
wrmsr <timwilloney@gmail.com>
2024-10-09 12:04:15 -0700

    --

--- ominfra/tailscale/cli.py
+++ ominfra/tailscale/cli.py
@@ -1,8 +1,22 @@
+import json
+import subprocess
 import sys
+import typing as ta
 
 from omdev.cli import CliModule
 from omlish import argparse as ap
+from omlish import dataclasses as dc
 from omlish import lang
+from omlish import marshal as msh
+
+
+@dc.dataclass(frozen=True)
+@msh.update_object_metadata(field_naming=msh.Naming.CAMEL, unknown_field='x')
+class Status:
+    version: str
+    backend_state: str
+
+    x: ta.Mapping[str, ta.Any] | None = None
 
 
 class Cli(ap.Cli):
@@ -16,6 +30,18 @@ class Cli(ap.Cli):
     @ap.command(name='bin')
     def bin_cmd(self) -> None:
         print(self.bin())
+
+    @ap.command(
+        ap.arg('name'),
+    )
+    def ip(self) -> None:
+        stdout = subprocess.check_output([
+            self.bin(),
+            'status',
+            '--json',
+        ])
+        status = msh.unmarshal(json.loads(stdout.decode()), Status)
+        print(status)
 
 
 # @omlish-manifest
