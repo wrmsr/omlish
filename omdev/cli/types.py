48af9640036969acb0836ed5eac5bec28d16ca74
wrmsr <timwilloney@gmail.com>
2024-10-09 11:19:23 -0700

    --

--- omdev/cli/types.py
+++ omdev/cli/types.py
@@ -1,10 +1,18 @@
+# ruff: noqa: UP007
 import dataclasses as dc
 import typing as ta
 
 
 @dc.dataclass(frozen=True)
 class CliCmd:
-    cmd_name: str
+    cmd_name: ta.Union[str, ta.Sequence[str]]
+
+    @property
+    def primary_name(self) -> str:
+        if isinstance(self.cmd_name, str):
+            return self.cmd_name
+        else:
+            return self.cmd_name[0]
 
 
 @dc.dataclass(frozen=True)
