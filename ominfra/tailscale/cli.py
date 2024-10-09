63c7bbe7f6d1a39295ba4b788d0aa49387965e38
wrmsr <timwilloney@gmail.com>
2024-10-09 11:53:18 -0700

    --

---
+++ ominfra/tailscale/cli.py
@@ -0,0 +1,13 @@
+from omdev.cli import CliModule
+
+
+# @omlish-manifest
+_CLI_MODULE = CliModule(['tailscale', 'ts'], __name__)
+
+
+def _main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    _main()
