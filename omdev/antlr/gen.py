37c65320445269f8ded5b553750a304de04959fe
wrmsr <timwilloney@gmail.com>
2024-10-10 15:25:37 -0700

    --

--- omdev/antlr/gen.py
+++ omdev/antlr/gen.py
@@ -1,8 +1,10 @@
+import argparse
 import os.path
 import re
 import shutil
 import subprocess
 
+from omlish import check
 from omlish import lang
 
 from ..cache import data as dcache
@@ -15,13 +17,17 @@ ANTLR_JAR_CACHE = dcache.UrlSpec(ANTLR_JAR_URL)
 class GenPy:
     def __init__(
             self,
-            out_dir: str,
+            dir: str,  # noqa
             *,
+            out_subdir: str = '_antlr',
             runtime_import: str = 'omlish.antlr._runtime',
     ) -> None:
         super().__init__()
-        self._out_dir = out_dir
+        check.arg(not os.path.isabs(out_subdir) and '..' not in out_subdir)
+        self._dir = dir
+        self._out_subdir = out_subdir
         self._runtime_import = runtime_import
+        self._out_dir = os.path.join(dir, out_subdir)
 
     @lang.cached_function
     def jar(self) -> str:
@@ -33,12 +39,13 @@ class GenPy:
             '-jar', self.jar(),
             '-Dlanguage=Python3',
             '-visitor',
-            '-o', self._out_dir,
+            '-o', self._out_subdir,
             g4_file,
-        ])
+        ], cwd=self._dir)
 
     def process_py(self, py_file: str) -> None:
-        with open(py_file) as f:
+        ap = os.path.join(self._out_dir, py_file)
+        with open(ap) as f:
             in_lines = list(f)
 
         out_lines = [
@@ -50,34 +57,47 @@ class GenPy:
             l = re.sub(r'^(from antlr4)(.*)', rf'from {self._runtime_import}\2', l)
             out_lines.append(l)
 
-        with open(py_file, 'w') as f:
+        with open(ap, 'w') as f:
             f.write(''.join(out_lines))
+
+    def run(self) -> None:
+        if os.path.exists(self._out_dir):
+            shutil.rmtree(self._out_dir)
+        os.mkdir(self._out_dir)
+        with open(os.path.join(self._out_dir, '__init__.py'), 'w'):
+            pass
+
+        for f in os.listdir(self._dir):
+            fp = os.path.join(self._dir, f)
+            if not os.path.isfile(fp):
+                continue
+            if f.endswith('.g4'):
+                self.process_g4(f)
+
+        for f in list(os.listdir(self._out_dir)):
+            fp = os.path.join(self._out_dir, f)
+            if not os.path.isfile(fp):
+                continue
+            if f.split('.')[-1] in ('interp', 'tokens'):
+                os.unlink(fp)
+            elif f != '__init__.py' and f.endswith('.py'):
+                self.process_py(f)
 
 
 def _main() -> None:
-    base_dir = os.path.dirname(__file__)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('roots', nargs='*')
+    args = parser.parse_args()
 
-    out_dir = os.path.join(base_dir, 'tests', '_antlr')
-    if os.path.exists(out_dir):
-        shutil.rmtree(out_dir)
-    os.mkdir(out_dir)
-    with open(os.path.join(out_dir, '__init__.py'), 'w'):
-        pass
+    base_dir = os.getcwd()
+    if not os.path.isfile(os.path.join(base_dir, 'pyproject.toml')):
+        raise RuntimeError('Must run from project root')
 
-    gp = GenPy(
-        out_dir,
-    )
-
-    for f in os.listdir(g4_dir := os.path.join(base_dir, 'tests')):
-        if f.endswith('.g4'):
-            gp.process_g4(os.path.join(g4_dir, f))
-
-    for f in list(os.listdir(out_dir)):
-        fp = os.path.join(out_dir, f)
-        if f.split('.')[-1] in ('interp', 'tokens'):
-            os.unlink(fp)
-        elif f != '__init__.py' and f.endswith('.py'):
-            gp.process_py(fp)
+    for root_dir in args.roots:
+        print(f'Processing {root_dir}')
+        GenPy(
+            root_dir,
+        ).run()
 
 
 if __name__ == '__main__':
