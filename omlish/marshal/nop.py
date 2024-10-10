bc576325d5682828e3940959a3427c7183d647e8
wrmsr <timwilloney@gmail.com>
2024-10-10 15:52:41 -0700

    --

---
+++ omlish/marshal/nop.py
@@ -0,0 +1,18 @@
+import typing as ta
+
+from .base import MarshalContext
+from .base import Marshaler
+from .base import UnmarshalContext
+from .base import Unmarshaler
+from .values import Value
+
+
+class NopMarshalerUnmarshaler(Marshaler, Unmarshaler):
+    def marshal(self, ctx: MarshalContext, o: ta.Any) -> Value:
+        return o  # noqa
+
+    def unmarshal(self, ctx: UnmarshalContext, v: Value) -> ta.Any:
+        return v
+
+
+NOP_MARSHALER_UNMARSHALER = NopMarshalerUnmarshaler()
