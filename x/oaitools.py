a852381a057abf9ad3b47c869b5a72054ef41a6c
wrmsr <timwilloney@gmail.com>
2024-10-10 16:19:26 -0700

    --

--- x/oaitools.py
+++ x/oaitools.py
@@ -1,35 +1,82 @@
 """
 https://platform.openai.com/docs/guides/function-calling
 """
+import typing as ta
+
 import openai
 
+from omlish import dataclasses as dc
+from omlish import lang
 from omdev.secrets import load_secrets
 
 
-def _main() -> None:
-    tools = [
-        {
-            "type": "function",
-            "function": {
-                "name": "get_delivery_date",
-                "description": (
-                    "Get the delivery date for a customer's order. Call this whenever you need to know the delivery "
-                    "date, for example when a customer asks 'Where is my package'"
-                ),
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "order_id": {
-                            "type": "string",
-                            "description": "The customer's order ID.",
-                        },
-                    },
-                    "required": ["order_id"],
-                    "additionalProperties": False,
+ToolDtype: ta.TypeAlias = str
+
+
+@dc.dataclass(frozen=True)
+class ToolParam(lang.Final):
+    name: str
+    dtype: ToolDtype
+
+    _: dc.KW_ONLY
+
+    desc: str | None = None
+    required: bool = False
+
+
+@dc.dataclass(frozen=True)
+class ToolDef(lang.Final):
+    name: str
+    params: ta.Sequence[ToolParam]
+
+    _: dc.KW_ONLY
+
+    desc: str
+
+
+def _opt_dct_fld(k, v):
+    return {k: v} if v else {}
+
+
+def render_tool_def(td: ToolDef) -> ta.Any:
+    return {
+        'type': 'function',
+        'function': {
+
+            'name': td.name,
+
+            **_opt_dct_fld('description', td.desc),
+
+            'parameters': {
+                'type': 'object',
+                'properties': {
+                    tp.name: {
+                        'type': tp.dtype,
+                        **_opt_dct_fld('description', tp.desc),
+                    }
+                    for tp in td.params
                 },
-            }
+
+                'required': [tp.name for tp in td.params if tp.required],
+                'additionalProperties': False,
+            },
         }
-    ]
+    }
+
+
+def _main() -> None:
+    tool = ToolDef(
+        'get_delivery_date',
+        [
+            ToolParam('order_id', 'string', desc="The customer's order ID.", required=True),
+        ],
+        desc=(
+            "Get the delivery date for a customer's order. Call this whenever you need to know the delivery date, for "
+            "example when a customer asks 'Where is my package'"
+        ),
+    )
+
+    tools = [render_tool_def(tool)]
 
     messages = [
         {
