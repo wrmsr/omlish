c50de68054e9a302ce1565584a5ab69d319db948
wrmsr <timwilloney@gmail.com>
2024-10-11 15:48:38 -0700

    --

--- omdev/secrets.py
+++ omdev/secrets.py
@@ -5,12 +5,12 @@ import yaml
 from omlish import secrets as sec
 
 
-SECRETS_PATH = os.getenv('SECRETS_PATH', os.path.expanduser('~/Dropbox/.dotfiles/secrets.yml'))
+SECRETS_FILE = os.getenv('SECRETS_FILE', '~/Dropbox/.dotfiles/secrets.yml')
 
 
 def load_secrets() -> sec.Secrets:
     dct: dict[str, sec.Secret] = {}
-    with open(SECRETS_PATH) as f:
+    with open(os.path.expanduser(SECRETS_FILE)) as f:
         for k, v in yaml.safe_load(f).items():
             if isinstance(v, str):
                 dct[k] = sec.Secret(key=k, value=v)
