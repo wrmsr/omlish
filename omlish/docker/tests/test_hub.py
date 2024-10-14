dacf00dae2c752904e363ee3608bd69b50752fe3
wrmsr <timwilloney@gmail.com>
2024-10-14 12:10:39 -0700

    --

--- omlish/docker/tests/test_hub.py
+++ omlish/docker/tests/test_hub.py
@@ -9,3 +9,22 @@ def test_hub_image_version():
     info = hub.get_hub_repo_info(repo)
     assert info.tags
     assert info.manifests
+
+
+def test_select_latest_tag():
+    tags = [
+        '1',
+        '1.1',
+        '1.1-foo',
+        '1.2-foo',
+        '1.2-bar',
+        '1.2.1-bar',
+        '1.3-bar',
+        '1.4',
+        '1.10',
+        '2-bar',
+    ]
+
+    assert hub.select_latest_tag(tags) == '1.10'
+    assert hub.select_latest_tag(tags, suffix='foo') == '1.2-foo'
+    assert hub.select_latest_tag(tags, suffix='bar') == '2-bar'
