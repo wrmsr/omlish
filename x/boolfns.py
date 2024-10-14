efa8e6d779bc7e8c1af5550d8be82ddc142655f1
wrmsr <timwilloney@gmail.com>
2024-10-14 14:46:03 -0700

    --

--- x/boolfns.py
+++ x/boolfns.py
@@ -1,4 +1,5 @@
 import abc
+import functools
 import typing as ta
 
 from omlish import check
@@ -14,25 +15,39 @@ P = ta.ParamSpec('P')
 
 @dc.dataclass(frozen=True)
 class BoolFn(lang.Abstract, lang.Sealed, ta.Generic[P]):
+    @property
+    def children(self) -> ta.Sequence['BoolFn[P]']:
+        return ()
+
     @abc.abstractmethod
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> bool:
         raise NotImplementedError
 
     @classmethod
     def of(cls, fn: ta.Callable[P, bool]) -> 'BoolFn[P]':
-        if isinstance(fn, BoolFn):
-            return fn
-        else:
-            return FnBoolFn(fn)
+        return of(fn)
+
+    def __and__(self, other: ta.Callable[P, bool]) -> 'BoolFn[P]':
+        return and_(self, other)
+
+    def __rand__(self, other: ta.Callable[P, bool]) -> 'BoolFn[P]':
+        return and_(other, self)
+
+    def __or__(self, other: ta.Callable[P, bool]) -> 'BoolFn[P]':
+        return or_(self, other)
+
+    def __ror__(self, other: ta.Callable[P, bool]) -> 'BoolFn[P]':
+        return or_(other, self)
+
+    def __invert__(self) -> 'BoolFn[P]':
+        return not_(self)
 
 
 @dc.dataclass(frozen=True)
 class FnBoolFn(BoolFn[P], lang.Final):
-    fn: ta.Callable[P, bool]
+    fn: ta.Callable[P, bool] = dc.xfield(validate=lambda fn: callable(fn) and not isinstance(fn, BoolFn))
 
     def __post_init__(self) -> None:
-        check.callable(self.fn)
-        check.not_isinstance(self.fn, BoolFn)
         lang.update_wrapper(self, self.fn, setattr=object.__setattr__, exclude='__dict__')
 
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> bool:
@@ -41,23 +56,27 @@ class FnBoolFn(BoolFn[P], lang.Final):
 
 @dc.dataclass(frozen=True)
 class NotBoolFn(BoolFn[P], lang.Final):
-    child: BoolFn[P]
+    child: BoolFn[P] = dc.xfield(check_type=BoolFn)
 
-    def __post_init__(self) -> None:
-        check.not_isinstance(self.child, BoolFn)
+    @property
+    def children(self) -> ta.Sequence[BoolFn[P]]:
+        return (self.child,)
 
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> bool:
         return not self.child(*args, **kwargs)
 
 
 @dc.dataclass(frozen=True)
-class AndBoolFn(BoolFn[P], lang.Final):
-    children: ta.Sequence[BoolFn[P]]
+class _CompBoolFn(BoolFn[P], lang.Abstract):  # noqa
+    children: ta.Sequence[BoolFn[P]] = dc.xfield(override=True)
 
     def __post_init__(self) -> None:
         for c in self.children:
             check.isinstance(c, BoolFn)
 
+
+@dc.dataclass(frozen=True)
+class AndBoolFn(_CompBoolFn[P], lang.Final):
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> bool:
         for c in self.children:
             if not c(*args, **kwargs):
@@ -66,13 +85,7 @@ class AndBoolFn(BoolFn[P], lang.Final):
 
 
 @dc.dataclass(frozen=True)
-class OrBoolFn(BoolFn[P], lang.Final):
-    children: ta.Sequence[BoolFn[P]]
-
-    def __post_init__(self) -> None:
-        for c in self.children:
-            check.isinstance(c, BoolFn)
-
+class OrBoolFn(_CompBoolFn[P], lang.Final):
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> bool:
         for c in self.children:
             if c(*args, **kwargs):
@@ -80,13 +93,50 @@ class OrBoolFn(BoolFn[P], lang.Final):
         return False
 
 
+def of(fn: ta.Callable[P, bool]) -> BoolFn[P]:
+    if isinstance(fn, BoolFn):
+        return fn
+    else:
+        return FnBoolFn(fn)  # type: ignore
+
+
+def _comp(cls: type[BoolFn[P]], *fns: ta.Callable[P, bool]) -> BoolFn[P]:
+    if not fns:
+        raise ValueError(fns)
+    elif len(fns) == 1:
+        return of(fns)  # type: ignore
+    else:
+        return cls([of(fn) for fn in fns])
+
+
+and_ = functools.partial(_comp, AndBoolFn)
+or_ = functools.partial(_comp, OrBoolFn)
+
+
+def not_(fn: ta.Callable[P, bool]) -> BoolFn[P]:
+    if isinstance(fn, NotBoolFn):
+        return fn.child
+    else:
+        return NotBoolFn(of(fn))  # type: ignore
+
+
 ##
 
 
 def _main() -> None:
-    bfn = BoolFn.of(lambda x: x < 3)
+    bfn: BoolFn[int] = of(lambda x: x < 3)
     assert bfn(2)
     assert not bfn(3)
+
+    assert not (~bfn)(2)
+    assert (~bfn)(3)
+
+    bfn |= (lambda x: x == 5)
+    assert not bfn(4)
+    assert bfn(5)
+
+    assert (~bfn)(4)
+    assert not (~bfn)(5)
 
 
 if __name__ == '__main__':
