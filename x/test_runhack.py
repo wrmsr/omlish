1148a9a37918f599aba21f32f21f0b9541c1fa1d
wrmsr <timwilloney@gmail.com>
2024-10-15 13:32:41 -0700

    --

--- x/test_runhack.py
+++ x/test_runhack.py
@@ -4,10 +4,21 @@ import typing as ta
 from . import runhack as rh
 
 
+##
+
+
 @dc.dataclass(frozen=True)
 class RunConfig:
     argv: ta.Sequence[str]
     orig_argv: ta.Sequence[str]
+
+
+PYTHON = '/Users/spinlock/.pyenv/versions/3.12.7/Library/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/Python'
+PYDEVD = '/Applications/PyCharm.app/Contents/plugins/python-ce/helpers/pydev/pydevd.py'
+PYCACHE_PREFIX = 'pycache_prefix=/Users/spinlock/Library/Caches/JetBrains/PyCharm2024.2/cpython-cache'
+
+
+#
 
 
 RUN_MODULE_NO_ARG_CFG = RunConfig(**{
@@ -55,6 +66,133 @@ RUN_FILE_FOO_ARG_CFG = RunConfig(**{
         'foo',
     ],
 })
+
+
+#
+
+
+DEBUG_FILE_NO_ARG = RunConfig(**{
+    'argv': [
+        PYDEVD,
+        '--multiprocess',
+        '--qt-support=auto',
+        '--client',
+        '127.0.0.1',
+        '--port',
+        '55770',
+        '--file',
+        'x/js.py',
+    ],
+    'orig_argv': [
+        PYTHON,
+        '-X',
+        PYCACHE_PREFIX,
+        PYDEVD,
+        '--multiprocess',
+        '--qt-support=auto',
+        '--client',
+        '127.0.0.1',
+        '--port',
+        '55770',
+        '--file',
+        'x/js.py'
+    ],
+})
+
+DEBUG_FILE_FOO_ARG = RunConfig(**{
+    'argv': [
+        PYDEVD,
+        '--multiprocess',
+        '--qt-support=auto',
+        '--client',
+        '127.0.0.1',
+        '--port',
+        '55780',
+        '--file',
+        'x/js.py',
+        'foo',
+    ],
+    'orig_argv': [
+        PYTHON,
+        '-X',
+        PYCACHE_PREFIX,
+        PYDEVD,
+        '--multiprocess',
+        '--qt-support=auto',
+        '--client',
+        '127.0.0.1',
+        '--port',
+        '55780',
+        '--file',
+        'x/js.py',
+        'foo',
+    ],
+})
+
+DEBUG_MODULE_NO_ARG = RunConfig(**{
+    'argv': [
+        PYDEVD,
+          '--module',
+          '--multiprocess',
+          '--qt-support=auto',
+          '--client',
+          '127.0.0.1',
+          '--port',
+          '55806',
+          '--file',
+          'x.js',
+    ],
+    'orig_argv': [
+        PYTHON,
+        '-X',
+        PYCACHE_PREFIX,
+        PYDEVD,
+        '--module',
+        '--multiprocess',
+        '--qt-support=auto',
+        '--client',
+        '127.0.0.1',
+        '--port',
+        '55806',
+        '--file',
+        'x.js',
+    ],
+})
+
+DEBUG_MODULE_FOO_ARG = RunConfig(**{
+    'argv': [
+        PYDEVD,
+        '--module',
+        '--multiprocess',
+        '--qt-support=auto',
+        '--client',
+        '127.0.0.1',
+        '--port',
+        '55815',
+        '--file',
+        'x.js',
+        'foo',
+    ],
+    'orig_argv': [
+        PYTHON,
+        '-X',
+        PYCACHE_PREFIX,
+        PYDEVD,
+        '--module',
+        '--multiprocess',
+        '--qt-support=auto',
+        '--client',
+        '127.0.0.1',
+        '--port',
+        '55815',
+        '--file',
+        'x.js',
+        'foo',
+    ],
+})
+
+
+##
 
 
 def test_params():
