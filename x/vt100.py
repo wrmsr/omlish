d697d98adcada1c22fd6901eb734a4e6e57d7f98
wrmsr <timwilloney@gmail.com>
2024-10-16 12:28:08 -0700

    --

--- x/vt100.py
+++ x/vt100.py
@@ -254,85 +254,96 @@ def _check_table():
     print("Tables had all necessary transitions defined.")
 
 
-def _generate_c():
-    class StringPad:
-        @staticmethod
-        def pad(s, length):
-            return s + " " * (length - len(s))
+def _pad(s, length):
+    return s + " " * (length - len(s))
 
-    # Writing vtparse_table.h
-    with open("vtparse_table.h", "w") as f:
-        f.write("typedef enum {\n")
-        for i, state in enumerate(states_in_order):
-            f.write(f"   VTPARSE_STATE_{state.upper()} = {i + 1},\n")
-        f.write("} vtparse_state_t;\n\n")
 
-        f.write("typedef enum {\n")
-        for i, action in enumerate(actions_in_order):
-            f.write(f"   VTPARSE_ACTION_{action.upper()} = {i + 1},\n")
-        f.write("} vtparse_action_t;\n\n")
+def _generate_c() -> dict[str, str]:
+    out = {}
 
-        f.write("typedef unsigned char state_change_t;\n")
-        f.write(f"extern state_change_t STATE_TABLE[{len(states_in_order)}][256];\n")
-        f.write(f"extern vtparse_action_t ENTRY_ACTIONS[{len(states_in_order)}];\n")
-        f.write(f"extern vtparse_action_t EXIT_ACTIONS[{len(states_in_order)}];\n")
-        f.write(f"extern char *ACTION_NAMES[{len(actions_in_order) + 1}];\n")
-        f.write(f"extern char *STATE_NAMES[{len(states_in_order) + 1}];\n\n")
+    #
 
-    print("Wrote vtparse_table.h")
+    import io
+    f = io.StringIO()
 
-    # Writing vtparse_table.c
-    with open("vtparse_table.c", "w") as f:
-        f.write('#include "vtparse_table.h"\n\n')
+    f.write("typedef enum {\n")
+    for i, state in enumerate(states_in_order):
+        f.write(f"   VTPARSE_STATE_{state.upper()} = {i + 1},\n")
+    f.write("} vtparse_state_t;\n\n")
 
-        f.write("char *ACTION_NAMES[] = {\n")
-        f.write('   "<no action>",\n')
-        for action in actions_in_order:
-            f.write(f'   "{action.upper()}",\n')
-        f.write("};\n\n")
+    f.write("typedef enum {\n")
+    for i, action in enumerate(actions_in_order):
+        f.write(f"   VTPARSE_ACTION_{action.upper()} = {i + 1},\n")
+    f.write("} vtparse_action_t;\n\n")
 
-        f.write("char *STATE_NAMES[] = {\n")
-        f.write('   "<no state>",\n')
-        for state in states_in_order:
-            f.write(f'   "{state}",\n')
-        f.write("};\n\n")
+    f.write("typedef unsigned char state_change_t;\n")
+    f.write(f"extern state_change_t STATE_TABLE[{len(states_in_order)}][256];\n")
+    f.write(f"extern vtparse_action_t ENTRY_ACTIONS[{len(states_in_order)}];\n")
+    f.write(f"extern vtparse_action_t EXIT_ACTIONS[{len(states_in_order)}];\n")
+    f.write(f"extern char *ACTION_NAMES[{len(actions_in_order) + 1}];\n")
+    f.write(f"extern char *STATE_NAMES[{len(states_in_order) + 1}];\n\n")
 
-        f.write(f"state_change_t STATE_TABLE[{len(states_in_order)}][256] = {{\n")
-        for i, state in enumerate(states_in_order):
-            f.write(f"  {{  /* VTPARSE_STATE_{state.upper()} = {i} */\n")
-            for j, state_change in enumerate(state_tables[state]):
-                if not state_change:
-                    f.write("    0,\n")
-                else:
-                    action = next((s for s in state_change if isinstance(s, str)), None)
-                    state_trans = next((s for s in state_change if isinstance(s, StateTransition)), None)
-                    action_str = f"VTPARSE_ACTION_{action.upper()}" if action else "0"
-                    state_str = f"VTPARSE_STATE_{state_trans.to_state}" if state_trans else "0"
-                    f.write(
-                        f"/*{str(j).rjust(3)}*/  {StringPad.pad(action_str, 33)} | ({StringPad.pad(state_str, 33)} << 4),\n")
-            f.write("  },\n")
-        f.write("};\n\n")
+    out['vtparse_table.h'] = f.getvalue()
 
-        f.write("vtparse_action_t ENTRY_ACTIONS[] = {\n")
-        for state in states_in_order:
-            actions = states[state]
-            if 'on_entry' in actions:
-                f.write(f"   VTPARSE_ACTION_{actions['on_entry'].upper()}, /* {state} */\n")
+    #
+
+    f = io.StringIO()
+
+    f.write('#include "vtparse_table.h"\n\n')
+
+    f.write("char *ACTION_NAMES[] = {\n")
+    f.write('   "<no action>",\n')
+    for action in actions_in_order:
+        f.write(f'   "{action.upper()}",\n')
+    f.write("};\n\n")
+
+    f.write("char *STATE_NAMES[] = {\n")
+    f.write('   "<no state>",\n')
+    for state in states_in_order:
+        f.write(f'   "{state}",\n')
+    f.write("};\n\n")
+
+    f.write(f"state_change_t STATE_TABLE[{len(states_in_order)}][256] = {{\n")
+    for i, state in enumerate(states_in_order):
+        f.write(f"  {{  /* VTPARSE_STATE_{state.upper()} = {i} */\n")
+        for j, state_change in enumerate(state_tables[state]):
+            if not state_change:
+                f.write("    0,\n")
             else:
-                f.write(f"   0  /* none for {state} */,\n")
-        f.write("};\n\n")
+                action = next((s for s in state_change if isinstance(s, str)), None)
+                state_trans = next((s for s in state_change if isinstance(s, StateTransition)), None)
+                action_str = f"VTPARSE_ACTION_{action.upper()}" if action else "0"
+                state_str = f"VTPARSE_STATE_{state_trans.to_state}" if state_trans else "0"
+                f.write(
+                    f"/*{str(j).rjust(3)}*/  {_pad(action_str, 33)} | ({_pad(state_str, 33)} << 4),\n")
+        f.write("  },\n")
+    f.write("};\n\n")
 
-        f.write("vtparse_action_t EXIT_ACTIONS[] = {\n")
-        for state in states_in_order:
-            actions = states[state]
-            if 'on_exit' in actions:
-                f.write(f"   VTPARSE_ACTION_{actions['on_exit'].upper()}, /* {state} */\n")
-            else:
-                f.write(f"   0  /* none for {state} */,\n")
-        f.write("};\n\n")
+    f.write("vtparse_action_t ENTRY_ACTIONS[] = {\n")
+    for state in states_in_order:
+        actions = states[state]
+        if 'on_entry' in actions:
+            f.write(f"   VTPARSE_ACTION_{actions['on_entry'].upper()}, /* {state} */\n")
+        else:
+            f.write(f"   0  /* none for {state} */,\n")
+    f.write("};\n\n")
 
-    print("Wrote vtparse_table.c")
+    f.write("vtparse_action_t EXIT_ACTIONS[] = {\n")
+    for state in states_in_order:
+        actions = states[state]
+        if 'on_exit' in actions:
+            f.write(f"   VTPARSE_ACTION_{actions['on_exit'].upper()}, /* {state} */\n")
+        else:
+            f.write(f"   0  /* none for {state} */,\n")
+    f.write("};\n\n")
+
+    out['vtparse_table.c'] = f.getvalue()
+
+    #
+
+    return out
 
 
 if __name__ == '__main__':
     _check_table()
+    _generate_c()
