be22354659f6123228a1fe0c027fabcd9a0a4a4a
wrmsr <timwilloney@gmail.com>
2024-10-17 14:39:36 -0700

    --

--- x/oaihttp.py
+++ x/oaihttp.py
@@ -1,5 +1,40 @@
 """
 https://github.com/chr15m/aish/blob/main/aish
+
+{
+  "frequency_penalty": 0.0,
+  "max_tokens": 64,
+  "messages": [
+    {
+      "content": "Is water dry?",
+      "role": "user"
+    }
+  ],
+  "model": "gpt-4o",
+  "presence_penalty": 0.0,
+  "stream": false,
+  "temperature": 0.1,
+  "top_p": 1
+}
+
+{
+  "host": "api.openai.com",
+  "accept-encoding": "gzip, deflate",
+  "connection": "keep-alive",
+  "accept": "application/json",
+  "content-type": "application/json",
+  "user-agent": "OpenAI/Python 1.52.0",
+  "x-stainless-lang": "python",
+  "x-stainless-package-version": "1.52.0",
+  "x-stainless-os": "MacOS",
+  "x-stainless-arch": "arm64",
+  "x-stainless-runtime": "CPython",
+  "x-stainless-runtime-version": "3.12.7",
+  "authorization": "Bearer ...",
+  "x-stainless-async": "false",
+  "x-stainless-retry-count": "0",
+  "content-length": "197"
+}
 """
 from omdev.secrets import load_secrets
 from omlish import http as hu
@@ -11,19 +46,20 @@ def _main() -> None:
 
     print(hu.request(
         'https://api.openai.com/v1/chat/completions',
+        'POST',
         headers={
-            hu.consts.HEADER_AUTH: hu.consts.format_basic_auth_header('', key.reveal()),
+            hu.consts.HEADER_AUTH: hu.consts.format_bearer_auth_header(key.reveal()),
             hu.consts.HEADER_CONTENT_TYPE: hu.consts.CONTENT_TYPE_JSON,
         },
         data=json.dumps(dict(
-            model='gpt-3.5-turbo',
+            model='gpt-4o',
             messages=[
                 dict(
                     role='user',
                     content='Hi!',
                 ),
             ],
-        )).encode('utf-8'),
+        )),
     ).data)
 
 
