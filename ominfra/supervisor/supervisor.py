04471f983729463c71e7d7280e83449d5b3abc77
wrmsr <timwilloney@gmail.com>
2024-10-21 16:11:49 -0700

    --

--- ominfra/supervisor/supervisor.py
+++ ominfra/supervisor/supervisor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+# ruff: noqa: UP006 UP007
 # !@omlish-amalg _amalg.py
 import logging
 import signal
 import time
+import typing as ta
 
 from omlish.lite.check import check_not_none
 from omlish.lite.logs import configure_standard_logging
@@ -23,6 +25,7 @@ from .events import SupervisorStoppingEvent
 from .events import clear_events
 from .events import notify_event
 from .process import ProcessGroup
+from .states import SupervisorState
 from .states import SupervisorStates
 from .states import get_process_state_description
 
@@ -31,54 +34,67 @@ log = logging.getLogger(__name__)
 
 
 class Supervisor:
-    stopping = False  # set after we detect that we are handling a stop request
-    last_shutdown_report = 0.  # throttle for delayed process error reports at stop
-    process_groups: dict  # map of process group name to process group object
-    stop_groups = None  # list used for priority ordered shutdown
 
     def __init__(self, context: ServerContext) -> None:
         super().__init__()
 
-        self.context = context
-        self.process_groups = {}
-        self.ticks: dict = {}
+        self._context = context
+        self._ticks: dict = {}
+        self._process_groups: dict = {}  # map of process group name to process group object
+        self._stop_groups: ta.Optional[ta.List] = None  # list used for priority ordered shutdown
+        self._stopping = False  # set after we detect that we are handling a stop request
+        self._last_shutdown_report = 0.  # throttle for delayed process error reports at stop
 
-    def main(self):
-        if not self.context.first:
+    @property
+    def context(self) -> ServerContext:
+        return self._context
+
+    def get_state(self) -> SupervisorState:
+        return self._context.state
+
+    def main(self) -> None:
+        if not self._context.first:
             # prevent crash on libdispatch-based systems, at least for the first request
-            self.context.cleanup_fds()
+            self._context.cleanup_fds()
 
-        self.context.set_uid_or_exit()
+        self._context.set_uid_or_exit()
 
-        if self.context.first:
-            self.context.set_rlimits_or_exit()
+        if self._context.first:
+            self._context.set_rlimits_or_exit()
 
         # this sets the options.logger object delay logger instantiation until after setuid
-        if not self.context.config.nocleanup:
+        if not self._context.config.nocleanup:
             # clean up old automatic logs
-            self.context.clear_auto_child_logdir()
+            self._context.clear_auto_child_logdir()
 
         self.run()
 
-    def run(self):
-        self.process_groups = {}  # clear
-        self.stop_groups = None  # clear
+    def run(self) -> None:
+        self._process_groups = {}  # clear
+        self._stop_groups = None  # clear
+
         clear_events()
+
         try:
-            for config in self.context.config.groups or []:
+            for config in self._context.config.groups or []:
                 self.add_process_group(config)
-            self.context.set_signals()
-            if not self.context.config.nodaemon and self.context.first:
-                self.context.daemonize()
+
+            self._context.set_signals()
+
+            if not self._context.config.nodaemon and self._context.first:
+                self._context.daemonize()
+
             # writing pid file needs to come *after* daemonizing or pid will be wrong
-            self.context.write_pidfile()
+            self._context.write_pidfile()
+
             self.runforever()
+
         finally:
-            self.context.cleanup()
+            self._context.cleanup()
 
     def diff_to_active(self):
-        new = self.context.config.groups or []
-        cur = [group.config for group in self.process_groups.values()]
+        new = self._context.config.groups or []
+        cur = [group.config for group in self._process_groups.values()]
 
         curdict = dict(zip([cfg.name for cfg in cur], cur))
         newdict = dict(zip([cfg.name for cfg in new], new))
@@ -92,61 +108,61 @@ class Supervisor:
 
     def add_process_group(self, config):
         name = config.name
-        if name not in self.process_groups:
-            group = self.process_groups[name] = ProcessGroup(config, self.context)
+        if name not in self._process_groups:
+            group = self._process_groups[name] = ProcessGroup(config, self._context)
             group.after_setuid()
             notify_event(ProcessGroupAddedEvent(name))
             return True
         return False
 
     def remove_process_group(self, name):
-        if self.process_groups[name].get_unstopped_processes():
+        if self._process_groups[name].get_unstopped_processes():
             return False
-        self.process_groups[name].before_remove()
-        del self.process_groups[name]
+        self._process_groups[name].before_remove()
+        del self._process_groups[name]
         notify_event(ProcessGroupRemovedEvent(name))
         return True
 
     def get_process_map(self):
         process_map = {}
-        for group in self.process_groups.values():
+        for group in self._process_groups.values():
             process_map.update(group.get_dispatchers())
         return process_map
 
     def shutdown_report(self):
         unstopped = []
 
-        for group in self.process_groups.values():
+        for group in self._process_groups.values():
             unstopped.extend(group.get_unstopped_processes())
 
         if unstopped:
             # throttle 'waiting for x to die' reports
             now = time.time()
-            if now > (self.last_shutdown_report + 3):  # every 3 secs
+            if now > (self._last_shutdown_report + 3):  # every 3 secs
                 names = [as_string(p.config.name) for p in unstopped]
                 namestr = ', '.join(names)
                 log.info('waiting for %s to die', namestr)
-                self.last_shutdown_report = now
+                self._last_shutdown_report = now
                 for proc in unstopped:
                     state = get_process_state_description(proc.get_state())
                     log.debug('%s state: %s', proc.config.name, state)
         return unstopped
 
-    def ordered_stop_groups_phase_1(self):
-        if self.stop_groups:
+    def _ordered_stop_groups_phase_1(self):
+        if self._stop_groups:
             # stop the last group (the one with the "highest" priority)
-            self.stop_groups[-1].stop_all()  # type: ignore
+            self._stop_groups[-1].stop_all()
 
-    def ordered_stop_groups_phase_2(self):
+    def _ordered_stop_groups_phase_2(self):
         # after phase 1 we've transitioned and reaped, let's see if we can remove the group we stopped from the
         # stop_groups queue.
-        if self.stop_groups:
+        if self._stop_groups:
             # pop the last group (the one with the "highest" priority)
-            group = self.stop_groups.pop()  # type: ignore
+            group = self._stop_groups.pop()
             if group.get_unstopped_processes():
                 # if any processes in the group aren't yet in a stopped state, we're not yet done shutting this group
                 # down, so push it back on to the end of the stop group queue
-                self.stop_groups.append(group)
+                self._stop_groups.append(group)
 
     def runforever(self):
         notify_event(SupervisorRunningEvent())
@@ -156,17 +172,17 @@ class Supervisor:
             combined_map = {}
             combined_map.update(self.get_process_map())
 
-            pgroups = list(self.process_groups.values())
+            pgroups = list(self._process_groups.values())
             pgroups.sort()
 
-            if self.context.state < SupervisorStates.RUNNING:
-                if not self.stopping:
+            if self._context.state < SupervisorStates.RUNNING:
+                if not self._stopping:
                     # first time, set the stopping flag, do a notification and set stop_groups
-                    self.stopping = True
-                    self.stop_groups = pgroups[:]
+                    self._stopping = True
+                    self._stop_groups = pgroups[:]
                     notify_event(SupervisorStoppingEvent())
 
-                self.ordered_stop_groups_phase_1()
+                self._ordered_stop_groups_phase_1()
 
                 if not self.shutdown_report():
                     # if there are no unstopped processes (we're done killing everything), it's OK to shutdown or reload
@@ -174,11 +190,11 @@ class Supervisor:
 
             for fd, dispatcher in combined_map.items():
                 if dispatcher.readable():
-                    self.context.poller.register_readable(fd)
+                    self._context.poller.register_readable(fd)
                 if dispatcher.writable():
-                    self.context.poller.register_writable(fd)
+                    self._context.poller.register_writable(fd)
 
-            r, w = self.context.poller.poll(timeout)
+            r, w = self._context.poller.poll(timeout)
 
             for fd in r:
                 if fd in combined_map:
@@ -187,7 +203,7 @@ class Supervisor:
                         log.debug('read event caused by %r', dispatcher)
                         dispatcher.handle_read_event()
                         if not dispatcher.readable():
-                            self.context.poller.unregister_readable(fd)
+                            self._context.poller.unregister_readable(fd)
                     except ExitNow:
                         raise
                     except Exception:  # noqa
@@ -197,7 +213,7 @@ class Supervisor:
                     # time, which may cause 100% cpu usage
                     log.debug('unexpected read event from fd %r', fd)
                     try:
-                        self.context.poller.unregister_readable(fd)
+                        self._context.poller.unregister_readable(fd)
                     except Exception:  # noqa
                         pass
 
@@ -208,7 +224,7 @@ class Supervisor:
                         log.debug('write event caused by %r', dispatcher)
                         dispatcher.handle_write_event()
                         if not dispatcher.writable():
-                            self.context.poller.unregister_writable(fd)
+                            self._context.poller.unregister_writable(fd)
                     except ExitNow:
                         raise
                     except Exception:  # noqa
@@ -216,83 +232,90 @@ class Supervisor:
                 else:
                     log.debug('unexpected write event from fd %r', fd)
                     try:
-                        self.context.poller.unregister_writable(fd)
+                        self._context.poller.unregister_writable(fd)
                     except Exception:  # noqa
                         pass
 
             for group in pgroups:
                 group.transition()
 
-            self.reap()
-            self.handle_signal()
-            self.tick()
+            self._reap()
+            self._handle_signal()
+            self._tick()
 
-            if self.context.state < SupervisorStates.RUNNING:
-                self.ordered_stop_groups_phase_2()
+            if self._context.state < SupervisorStates.RUNNING:
+                self._ordered_stop_groups_phase_2()
 
-            if self.context.test:
+            if self._context.test:
                 break
 
-    def tick(self, now=None):
+    def _tick(self, now: ta.Optional[float] = None) -> None:
         """Send one or more 'tick' events when the timeslice related to the period for the event type rolls over"""
+
         if now is None:
             # now won't be None in unit tests
             now = time.time()
+
         for event in TICK_EVENTS:
             period = event.period  # type: ignore
-            last_tick = self.ticks.get(period)
+
+            last_tick = self._ticks.get(period)
             if last_tick is None:
                 # we just started up
-                last_tick = self.ticks[period] = timeslice(period, now)
+                last_tick = self._ticks[period] = timeslice(period, now)
+
             this_tick = timeslice(period, now)
             if this_tick != last_tick:
-                self.ticks[period] = this_tick
+                self._ticks[period] = this_tick
                 notify_event(event(this_tick, self))
 
-    def reap(self, once=False, recursionguard=0):
-        if recursionguard == 100:
+    def _reap(self, *, once: bool = False, depth: int = 0) -> None:
+        if depth >= 100:
             return
-        pid, sts = self.context.waitpid()
-        if pid:
-            process = self.context.pid_history.get(pid, None)
-            if process is None:
-                _, msg = decode_wait_status(check_not_none(sts))
-                log.info('reaped unknown pid %s (%s)', pid, msg)
+
+        pid, sts = self._context.waitpid()
+        if not pid:
+            return
+
+        process = self._context.pid_history.get(pid, None)
+        if process is None:
+            _, msg = decode_wait_status(check_not_none(sts))
+            log.info('reaped unknown pid %s (%s)', pid, msg)
+        else:
+            process.finish(check_not_none(sts))
+            del self._context.pid_history[pid]
+
+        if not once:
+            # keep reaping until no more kids to reap, but don't recurse infinitely
+            self._reap(once=False, depth=depth + 1)
+
+    def _handle_signal(self) -> None:
+        sig = self._context.get_signal()
+        if not sig:
+            return
+
+        if sig in (signal.SIGTERM, signal.SIGINT, signal.SIGQUIT):
+            log.warning('received %s indicating exit request', signame(sig))
+            self._context.state = SupervisorStates.SHUTDOWN
+
+        elif sig == signal.SIGHUP:
+            if self._context.state == SupervisorStates.SHUTDOWN:
+                log.warning('ignored %s indicating restart request (shutdown in progress)', signame(sig))  # noqa
             else:
-                process.finish(check_not_none(sts))
-                del self.context.pid_history[pid]
-            if not once:
-                # keep reaping until no more kids to reap, but don't recurse infinitely
-                self.reap(once=False, recursionguard=recursionguard + 1)
+                log.warning('received %s indicating restart request', signame(sig))  # noqa
+                self._context.state = SupervisorStates.RESTARTING
 
-    def handle_signal(self):
-        sig = self.context.get_signal()
-        if sig:
-            if sig in (signal.SIGTERM, signal.SIGINT, signal.SIGQUIT):
-                log.warning('received %s indicating exit request', signame(sig))
-                self.context.state = SupervisorStates.SHUTDOWN
+        elif sig == signal.SIGCHLD:
+            log.debug('received %s indicating a child quit', signame(sig))
 
-            elif sig == signal.SIGHUP:
-                if self.context.state == SupervisorStates.SHUTDOWN:
-                    log.warning('ignored %s indicating restart request (shutdown in progress)', signame(sig))  # noqa
-                else:
-                    log.warning('received %s indicating restart request', signame(sig))  # noqa
-                    self.context.state = SupervisorStates.RESTARTING
+        elif sig == signal.SIGUSR2:
+            log.info('received %s indicating log reopen request', signame(sig))
+            # self._context.reopen_logs()
+            for group in self._process_groups.values():
+                group.reopen_logs()
 
-            elif sig == signal.SIGCHLD:
-                log.debug('received %s indicating a child quit', signame(sig))
-
-            elif sig == signal.SIGUSR2:
-                log.info('received %s indicating log reopen request', signame(sig))
-                # self.context.reopen_logs()
-                for group in self.process_groups.values():
-                    group.reopen_logs()
-
-            else:
-                log.debug('received %s indicating nothing', signame(sig))
-
-    def get_state(self):
-        return self.context.state
+        else:
+            log.debug('received %s indicating nothing', signame(sig))
 
 
 def timeslice(period, when):
