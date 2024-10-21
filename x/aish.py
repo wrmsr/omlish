2223f050d5e752635b4f8035426a8a28fcb77c97
wrmsr <timwilloney@gmail.com>
2024-10-21 09:58:19 -0700

    --

--- x/aish.py
+++ x/aish.py
@@ -30,11 +30,6 @@ Examples:
  - use imagemagick change the white background in screenshot.png to transparent
 """
 """
-if [ "${SHELLNAME}" != "zsh" ] && [ "$SHELLNAME" != "bash" ]
-then
-  _throw_source_error "${@}"
-fi
-
 if [ "${SHELLNAME}" = "bash" ]
 then
   (return 0 2>/dev/null) && sourced=1 || sourced=0
@@ -54,28 +49,7 @@ then
   fi
 fi
 
-# exit from ctrl-C without exiting user's shell
-trap "return 0" SIGINT
-
-AISH_SHELL="${SHELLNAME}"
-AISH_OS="${OS}"
-
-# start the actual request to OpenAI
-
-request=`echo "${@}" | sed -r 's/"/\\\"/g'`
-
-data='{
-  "model": "'${MODEL}'",
-  "messages": [
-  {"role": "system", "content": "You are an AI shell-scripting expert called Aish."},{"role": "user", "content": "Please write a one-liner for '${AISH_SHELL}' shell for performing the following task. Assume you have access to all of the commonly available unix commands on a modern '${AISH_OS}' system. You can separate multi-line commands with a semicolon or double ampersand but please put them on one line. Respond without using markdown formatting. Please only return the exact bash one-liner command without any superfluous words explaining it, and no formatting. Do NOT use backticks. Do not put backticks around the one-liner. Code should be bare without any markdown formatting. IMPORTANT: If you need to ask for clarification or provide any information other than a one-liner solution, please prefix your response with the exact phrase AISH_FEEDBACK: in all caps, with a colon. If you are responding with anything other than a shell script one-liner you MUST include AISH_FEEDBACK: (including the colon) as the start of the response.\n\n Here is the task I would like you to carry out with the one-liner: '$request'"}
-  ]
-}'
-
-# echo "Sending: ${data}"
-
-echo -ne "...fetching solution..."
-json=$( curl -s ${AISH_URL}/v1/chat/completions -u ":${OPENAI_API_KEY}" -H 'Content-Type: application/json' -d "${data}" )
-echo -ne "\033[2K" ; printf "\r"
+...
 
 if `echo "${json}" | grep -q '"content": '`
 then
@@ -107,6 +81,7 @@ else
 fi
 """
 from omdev.secrets import load_secrets
+from omlish import check
 from ommlx import minichain as mc
 from ommlx.minichain.backends.openai import OpenaiChatModel
 
@@ -115,29 +90,48 @@ def _main() -> None:
     request = 'which process is running on port 8000'
 
     system_prompt = 'You are an AI shell-scripting expert called Aish.'
+
     user_template = (
         "Please write a one-liner for '{shell}' shell for performing the following task. "
+        
         "Assume you have access to all of the commonly available unix commands on a modern '{os}' system. "
-        "You can separate multi-line commands with a semicolon or double ampersand but please put them on one "
-        "line. Respond without using markdown formatting. Please only return the exact bash one-liner command "
-        "without any superfluous words explaining it, and no formatting. Do NOT use backticks. Do not put "
-        "backticks around the one-liner. Code should be bare without any markdown formatting. IMPORTANT: If you "
-        "need to ask for clarification or provide any information other than a one-liner solution, please prefix "
-        "your response with the exact phrase AISH_FEEDBACK: in all caps, with a colon. If you are responding with "
-        "anything other than a shell script one-liner you MUST include AISH_FEEDBACK: (including the colon) as the "
-        "start of the response.\n\nHere is the task I would like you to carry out with the one-liner: '{request}'"
+        
+        "You can separate multi-line commands with a semicolon or double ampersand but please put them on one line. "
+        
+        "Respond without using markdown formatting. "
+
+        "Please only return the exact bash one-liner command without any superfluous words explaining it, and no "
+        "formatting. "
+
+        "Do NOT use backticks. "
+
+        "Do not put backticks around the one-liner. "
+
+        "Code should be bare without any markdown formatting. "
+
+        "IMPORTANT: If you need to ask for clarification or provide any information other than a one-liner solution, "
+        "please prefix your response with the exact phrase AISH_FEEDBACK: in all caps, with a colon. "
+
+        "If you are responding with anything other than a shell script one-liner you MUST include AISH_FEEDBACK: "
+        "(including the colon) as the start of the response."
+
+        "\n\n"
+
+        "Here is the task I would like you to carry out with the one-liner: '{request}'"
     )
 
     llm = OpenaiChatModel(api_key=load_secrets().get('openai_api_key').reveal())
 
-    print(llm([
+    resp = llm([
         mc.SystemMessage(system_prompt),
         mc.UserMessage(user_template.format(
             shell='zsh',
             os='Mac OSX',
             request=request,
         )),
-    ]))
+    ])
+
+    print(check.single(resp.v).m.s)
 
 
 if __name__ == '__main__':
