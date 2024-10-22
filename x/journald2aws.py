4edf15ef3c8ecaf90c3df4b6db86e2453889176a
wrmsr <timwilloney@gmail.com>
2024-10-22 10:37:01 -0700

    --

--- x/journald2aws.py
+++ x/journald2aws.py
@@ -12,10 +12,9 @@ https://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_PutLogE
  - A batch of log events in a single request cannot span more than 24 hours. Otherwise, the operation fails.
  - Each log event can be no larger than 256 KB.
  - The maximum number of log events in a batch is 10,000.
-
-
 """
 import dataclasses as dc
+import operator
 import time
 import typing as ta
 
@@ -31,27 +30,52 @@ from omlish.lite.strings import camel_case
 
 
 class AwsDataclass:
-    _aws_field_maps: ta.ClassVar[ta.Optional[ta.Tuple[
-        ta.Mapping[str, str],  # dc -> aws
-        ta.Mapping[str, str],  # aws -> dc
-    ]]] = None
+    class _AwsConverters(ta.NamedTuple):
+        d2a: ta.Callable
+        a2d: ta.Callable
+
+    _aws_converters: ta.ClassVar[ta.Optional[_AwsConverters]] = None
 
     @classmethod
-    def _get_aws_field_maps(cls) -> ta.Tuple[ta.Mapping[str, str], ta.Mapping[str, str]]:
+    def _get_aws_converters(cls) -> _AwsConverters:
         try:
-            return cls.__dict__['_aws_field_maps']
+            return cls.__dict__['_aws_converters']
         except KeyError:
             pass
 
-        d2a, a2d = {}, {}
+        d2a_lst = []
+        a2d_lst = []
         for f in dc.fields(cls):  # noqa
             d = f.name
             a = camel_case(d, lower=True)
-            d2a[d] = a
-            a2d[a] = d
 
-        ret = cls._aws_field_maps = (d2a, a2d)
+            d2a_lst.append((a, d))
+            a2d_lst.append((d, a))
+
+        def d2a(o):
+            dct = {}
+            for a, d in d2a_lst:
+                x = getattr(o, d)
+                dct[a] = x
+            return dct
+
+        def a2d(v):
+            dct = {}
+            for d, a in a2d_lst:
+                x = v.get(a)
+                if x is not None:
+                    dct[d] = x
+            return cls(**dct)
+
+        ret = cls._aws_converters = AwsDataclass._AwsConverters(d2a, a2d)
         return ret
+
+    def to_aws(self) -> ta.Mapping[str, ta.Any]:
+        return self._get_aws_converters().d2a(self)
+
+    @classmethod
+    def from_aws(cls, v: ta.Mapping[str, ta.Any]) -> 'AwsDataclass':
+        return cls._get_aws_converters().a2d(v)
 
 
 @dc.dataclass(frozen=True)
@@ -86,10 +110,6 @@ class AwsPutLogEventsResponse(AwsDataclass):
 ##
 
 
-print(AwsLogEvent._get_aws_field_maps())
-print(AwsPutLogEventsResponse._get_aws_field_maps())
-
-
 def _main() -> None:
     secrets = load_secrets()
 
@@ -100,45 +120,48 @@ def _main() -> None:
             {"timestamp": int(time.time() * 1000.), "message": "Test log message 3"},
         ],
     }
-    body = json.dumps_compact(payload).encode('utf-8')
 
-    amz_target = 'Logs_20140328.PutLogEvents'
-    url = 'https://logs.us-west-1.amazonaws.com/'
+    print(AwsPutLogEventsRequest.from_aws(payload))
 
-    creds = V4AwsSigner.Credentials(
-        secrets.get('aws_access_key_id').reveal(),
-        secrets.get('aws_secret_access_key').reveal(),
-
-    )
-
-    region_name = 'us-west-1'
-
+    # body = json.dumps_compact(payload).encode('utf-8')
     #
-
-    req = V4AwsSigner.Request(
-        method='POST',
-        url=url,
-        headers={
-            'User-Agent': ['Botocore/1.35.6 ua/2.0 os/macos#21.6.0 md/arch#arm64 lang/python#3.12.5 md/pyimpl#CPython'],
-            'Content-Type': ['application/x-amz-json-1.1'],
-            'X-Amz-Target': [amz_target],
-        },
-        payload=body,
-    )
-
+    # amz_target = 'Logs_20140328.PutLogEvents'
+    # url = 'https://logs.us-west-1.amazonaws.com/'
     #
-
-    sign_hdrs = V4AwsSigner(creds, region_name, 'logs').sign(req, sign_payload=False)
-    req = dc.replace(req, headers={**req.headers, **sign_hdrs})
-
-    resp = httpx.post(
-        req.url,
-        headers=[(k, v) for k, vs in req.headers.items() for v in vs],
-        follow_redirects=True,
-        content=req.payload,
-    )
-
-    print((resp, resp.content))
+    # creds = V4AwsSigner.Credentials(
+    #     secrets.get('aws_access_key_id').reveal(),
+    #     secrets.get('aws_secret_access_key').reveal(),
+    #
+    # )
+    #
+    # region_name = 'us-west-1'
+    #
+    # #
+    #
+    # req = V4AwsSigner.Request(
+    #     method='POST',
+    #     url=url,
+    #     headers={
+    #         'User-Agent': ['Botocore/1.35.6 ua/2.0 os/macos#21.6.0 md/arch#arm64 lang/python#3.12.5 md/pyimpl#CPython'],
+    #         'Content-Type': ['application/x-amz-json-1.1'],
+    #         'X-Amz-Target': [amz_target],
+    #     },
+    #     payload=body,
+    # )
+    #
+    # #
+    #
+    # sign_hdrs = V4AwsSigner(creds, region_name, 'logs').sign(req, sign_payload=False)
+    # req = dc.replace(req, headers={**req.headers, **sign_hdrs})
+    #
+    # resp = httpx.post(
+    #     req.url,
+    #     headers=[(k, v) for k, vs in req.headers.items() for v in vs],
+    #     follow_redirects=True,
+    #     content=req.payload,
+    # )
+    #
+    # print((resp, resp.content))
 
 
 if __name__ == '__main__':
