572c9ec79b79d1f784a014cdb57d256f389c85dc
wrmsr <timwilloney@gmail.com>
2024-10-22 11:18:16 -0700

    --

--- x/journald2aws.py
+++ x/journald2aws.py
@@ -1,207 +1,14 @@
-"""
-https://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_PutLogEvents.html :
- - The maximum batch size is 1,048,576 bytes. This size is calculated as the sum of all event messages in UTF-8, plus 26
-   bytes for each log event.
- - None of the log events in the batch can be more than 2 hours in the future.
- - None of the log events in the batch can be more than 14 days in the past. Also, none of the log events can be from
-   earlier than the retention period of the log group.
- - The log events in the batch must be in chronological order by their timestamp. The timestamp is the time that the
-   event occurred, expressed as the number of milliseconds after Jan 1, 1970 00:00:00 UTC. (In AWS Tools for PowerShell
-   and the AWS SDK for .NET, the timestamp is specified in .NET format: yyyy-mm-ddThh:mm:ss. For example,
-   2017-09-15T13:45:30.)
- - A batch of log events in a single request cannot span more than 24 hours. Otherwise, the operation fails.
- - Each log event can be no larger than 256 KB.
- - The maximum number of log events in a batch is 10,000.
-"""
-import collections.abc
 import dataclasses as dc
-import operator
 import time
-import typing as ta
 
 import httpx
 
 from omdev.secrets import load_secrets
 from ominfra.clouds.aws.auth import V4AwsSigner
+from ominfra.clouds.aws.logs import AwsLogEvent
+from ominfra.clouds.aws.logs import AwsPutLogEventsRequest
+from ominfra.clouds.aws.logs import AwsPutLogEventsResponse
 from omlish.formats import json
-from omlish.lite.cached import cached_nullary
-from omlish.lite.reflect import get_optional_alias_arg
-from omlish.lite.reflect import is_generic_alias
-from omlish.lite.reflect import is_optional_alias
-from omlish.lite.strings import camel_case
-
-
-##
-
-
-class AwsDataclass:
-    class Raw(dict):
-        pass
-
-    #
-
-    _aws_meta: ta.ClassVar[ta.Optional['AwsDataclassMeta']] = None
-
-    @classmethod
-    def _get_aws_meta(cls) -> 'AwsDataclassMeta':
-        try:
-            return cls.__dict__['_aws_meta']
-        except KeyError:
-            pass
-        ret = cls._aws_meta = AwsDataclassMeta(cls)
-        return ret
-
-    #
-
-    def to_aws(self) -> ta.Mapping[str, ta.Any]:
-        return self._get_aws_meta().converters().d2a(self)
-
-    @classmethod
-    def from_aws(cls, v: ta.Mapping[str, ta.Any]) -> 'AwsDataclass':
-        return cls._get_aws_meta().converters().a2d(v)
-
-
-@dc.dataclass(frozen=True)
-class AwsDataclassMeta:
-    cls: ta.Type['AwsDataclass']
-
-    #
-
-    class Field(ta.NamedTuple):
-        d_name: str
-        a_name: str
-        is_opt: bool
-        is_seq: bool
-        dc_cls: ta.Optional[ta.Type['AwsDataclass']]
-
-    @cached_nullary
-    def fields(self) -> ta.Sequence[Field]:
-        fs = []
-        for f in dc.fields(self.cls):  # noqa
-            d_name = f.name
-            a_name = camel_case(d_name, lower=True)
-
-            is_opt = False
-            is_seq = False
-            dc_cls = None
-
-            c = f.type
-            if c is AwsDataclass.Raw:
-                continue
-
-            if is_optional_alias(c):
-                is_opt = True
-                c = get_optional_alias_arg(c)
-
-            if is_generic_alias(c) and ta.get_origin(c) is collections.abc.Sequence:
-                is_seq = True
-                [c] = ta.get_args(c)
-
-            if is_generic_alias(c):
-                raise TypeError(c)
-
-            if isinstance(c, type) and issubclass(c, AwsDataclass):
-                dc_cls = c
-
-            fs.append(AwsDataclassMeta.Field(
-                d_name=d_name,
-                a_name=a_name,
-                is_opt=is_opt,
-                is_seq=is_seq,
-                dc_cls=dc_cls,
-            ))
-
-        return fs
-
-    #
-
-    class Converters(ta.NamedTuple):
-        d2a: ta.Callable
-        a2d: ta.Callable
-
-    @cached_nullary
-    def converters(self) -> Converters:
-        for df in dc.fields(self.cls):  # noqa
-            c = df.type
-
-            if is_optional_alias(c):
-                c = get_optional_alias_arg(c)
-
-            if c is AwsDataclass.Raw:
-                rf = df.name
-                break
-
-        else:
-            rf = None
-
-        fs = [
-            (f, f.dc_cls._get_aws_meta().converters() if f.dc_cls is not None else None)  # noqa
-            for f in self.fields()
-        ]
-
-        def d2a(o):
-            dct = {}
-            for f, cs in fs:
-                x = getattr(o, f.d_name)
-                if x is None:
-                    continue
-                if cs is not None:
-                    if f.is_seq:
-                        x = list(map(cs.d2a, x))
-                    else:
-                        x = cs.d2a(x)
-                dct[f.a_name] = x
-            return dct
-
-        def a2d(v):
-            dct = {}
-            for f, cs in fs:
-                x = v.get(f.a_name)
-                if x is None:
-                    continue
-                if cs is not None:
-                    if f.is_seq:
-                        x = list(map(cs.a2d, x))
-                    else:
-                        x = cs.a2d(x)
-                dct[f.d_name] = x
-            if rf is not None:
-                dct[rf] = self.cls.Raw(v)
-            return self.cls(**dct)
-
-        return AwsDataclassMeta.Converters(d2a, a2d)
-
-
-##
-
-
-@dc.dataclass(frozen=True)
-class AwsLogEvent(AwsDataclass):
-    message: str
-    timestamp: int
-
-
-@dc.dataclass(frozen=True)
-class AwsPutLogEventsRequest(AwsDataclass):
-    log_group_name: str
-    log_stream_name: str
-    log_events: ta.Sequence[AwsLogEvent]
-    sequence_token: ta.Optional[str] = None
-
-
-@dc.dataclass(frozen=True)
-class AwsRejectedLogEventsInfo(AwsDataclass):
-    expired_log_event_end_index: ta.Optional[int] = None
-    too_new_log_event_start_index: ta.Optional[int] = None
-    too_old_log_event_end_index: ta.Optional[int] = None
-
-
-@dc.dataclass(frozen=True)
-class AwsPutLogEventsResponse(AwsDataclass):
-    next_sequence_token: ta.Optional[str] = None
-    rejected_log_events_info: ta.Optional[AwsRejectedLogEventsInfo] = None
-
-    raw: AwsDataclass.Raw = None
 
 
 ##
@@ -210,33 +17,18 @@ class AwsPutLogEventsResponse(AwsDataclass):
 def _main() -> None:
     secrets = load_secrets()
 
-    dpayload = AwsPutLogEventsRequest(
+    payload = AwsPutLogEventsRequest(
         log_group_name='omlish',
         log_stream_name='test',
         log_events=[
             AwsLogEvent(
                 timestamp=int(time.time() * 1000.),
-                message='Test log message 100',
+                message=f'Test log message {time.time()}',
             ),
         ],
     )
 
-    print(dpayload.to_aws())
-
-    payload = {
-        "logGroupName": "omlish",
-        "logStreamName": "test",
-        "logEvents": [
-            {"timestamp": int(time.time() * 1000.), "message": "Test log message 3"},
-        ],
-    }
-
-    print(AwsPutLogEventsRequest.from_aws(payload))
-
-    rpayload = {"nextSequenceToken": "123123123123"}
-    print(AwsPutLogEventsResponse.from_aws(rpayload))
-
-    body = json.dumps_compact(payload).encode('utf-8')
+    body = json.dumps_compact(payload.to_aws()).encode('utf-8')
 
     amz_target = 'Logs_20140328.PutLogEvents'
     url = 'https://logs.us-west-1.amazonaws.com/'
@@ -275,6 +67,9 @@ def _main() -> None:
     )
 
     print((resp, resp.content))
+
+    response = AwsPutLogEventsResponse.from_aws(resp.json())
+    print(response)
 
 
 if __name__ == '__main__':
