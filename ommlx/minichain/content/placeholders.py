f2a94996dc63cf1d6a9c31201788d70b06801bd5
wrmsr <timwilloney@gmail.com>
2024-10-25 16:50:13 -0700

    --

--- ommlx/minichain/content/placeholders.py
+++ ommlx/minichain/content/placeholders.py
@@ -1,8 +1,10 @@
 import typing as ta
 
+from omlish import collections as col
 from omlish import dataclasses as dc
 from omlish import lang
 
+# from ..models import RequestContextItem
 from .content import ExtendedContent
 
 
@@ -12,8 +14,12 @@ from .content import ExtendedContent
 EnvKey = ta.NewType('EnvKey', str)
 
 
-# class Env(col.FrozenDict[str, str], RequestContextItem, lang.Final):
-#     pass
+class Env(
+    col.FrozenDict[str, str],
+    # RequestContextItem,
+    lang.Final,
+):
+    pass
 
 
 ##
