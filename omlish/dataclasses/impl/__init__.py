bf71ba41979e0d68ec32b861c28a0998f468fdda
wrmsr <timwilloney@gmail.com>
2024-10-25 14:35:17 -0700

    --

--- omlish/dataclasses/impl/__init__.py
+++ omlish/dataclasses/impl/__init__.py
@@ -22,4 +22,12 @@ TODO:
  - enums
  - nodal
  - embedding? forward kwargs in general? or only for replace?
+
+TODO refs:
+ - batch up exec calls
+  - https://github.com/python/cpython/commit/8945b7ff55b87d11c747af2dad0e3e4d631e62d6
+ - add doc parameter to dataclasses.field
+  - https://github.com/python/cpython/commit/9c7657f09914254724683d91177aed7947637be5
+ - add decorator argument to make_dataclass
+  - https://github.com/python/cpython/commit/3e3a4d231518f91ff2f3c5a085b3849e32f1d548
 """
