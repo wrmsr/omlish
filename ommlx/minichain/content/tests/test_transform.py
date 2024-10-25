595e11267f08907f970e6812dae33044b8747ed2
wrmsr <timwilloney@gmail.com>
2024-10-24 17:55:29 -0700

    --

--- ommlx/minichain/content/tests/test_transform.py
+++ ommlx/minichain/content/tests/test_transform.py
@@ -1,18 +1,21 @@
 from ..placeholders import Env
 from ..placeholders import EnvKey
 from ..placeholders import Placeholder
-from ..transform import PlaceholderTransform
+from ..transform import PlaceholderFiller
+from ..transform import StringRenderer
 
 
 def test_transform():
     c = [
-        'foo',
+        'foo ',
         [
-            'bar',
+            'bar ',
             Placeholder(EnvKey('huh')),
-            'baz',
+            ' baz',
         ],
-        'fff,'
+        ' fff',
     ]
 
-    print(PlaceholderTransform(Env({'huh': 'what', 'no': 'yes'})).apply(c))
+    c2 = PlaceholderFiller(Env({'huh': 'what', 'no': 'yes'})).apply(c)
+    s = StringRenderer.render_to_str(c2)
+    assert s == 'foo bar what baz fff'
--- ommlx/minichain/content/transform.py
+++ ommlx/minichain/content/transform.py
@@ -1,4 +1,6 @@
 import collections.abc
+import io
+import typing as ta
 
 from omlish import dispatch
 
@@ -11,7 +13,7 @@ from .placeholders import Placeholder
 ##
 
 
-class PlaceholderTransform:
+class PlaceholderFiller:
     def __init__(self, env: Env) -> None:
         super().__init__()
         self._env = env
@@ -33,5 +35,34 @@ class PlaceholderTransform:
         return c
 
     @apply.register
-    def apply_placeholder(self, c: Placeholder) -> Placeholder:
-        raise NotImplementedError
+    def apply_placeholder(self, c: Placeholder) -> str:
+        return self._env[c.k]
+
+
+##
+
+
+class StringRenderer:
+    def __init__(self, out: ta.TextIO) -> None:
+        super().__init__()
+
+        self._out = out
+
+    @dispatch.method
+    def render(self, c: Content) -> None:
+        raise TypeError(c)
+
+    @render.register
+    def render_str(self, c: str) -> None:
+        self._out.write(c)
+
+    @render.register
+    def render_sequence(self, s: collections.abc.Sequence) -> None:
+        for e in s:
+            self.render(e)
+
+    @classmethod
+    def render_to_str(cls, c: Content) -> str:
+        out = io.StringIO()
+        cls(out).render(c)
+        return out.getvalue()
