8b49fe15d3edbdf1163cb78770de627da3cdc29f
wrmsr <timwilloney@gmail.com>
2024-10-28 15:59:53 -0700

    --

--- omlish/sql/tests/test_params.py
+++ omlish/sql/tests/test_params.py
@@ -1,5 +1,24 @@
 from ... import lang
 from ..params import NumericParamsPreparer
+from ..params import LinearParamsPreparer
+
+
+def test_linear():
+    pp = LinearParamsPreparer('?')
+    assert pp.add(1) == '?'
+    assert pp.add('foo') == '?'
+    assert pp.add(1) == '?'
+    assert pp.add(2) == '?'
+    assert pp.add('foo') == '?'
+    assert pp.add('bar') == '?'
+    assert pp.prepare() == lang.Args(
+        1,
+        'foo',
+        1,
+        2,
+        'foo',
+        'bar',
+    )
 
 
 def test_numeric():
