cfdfcfda84704e6ee04afb012165fc726b0727a7
wrmsr <timwilloney@gmail.com>
2024-10-29 16:29:45 -0700

    --

--- ominfra/supervisor/configs.py
+++ ominfra/supervisor/configs.py
@@ -13,60 +13,6 @@ from .datatypes import octal_type
 
 
 @dc.dataclass(frozen=True)
-class ServerConfig:
-    user: ta.Optional[str] = None
-    nodaemon: bool = False
-    umask: int = 0o22
-    directory: ta.Optional[str] = None
-    logfile: str = 'supervisord.log'
-    logfile_maxbytes: int = 50 * 1024 * 1024
-    logfile_backups: int = 10
-    loglevel: int = logging.INFO
-    pidfile: str = 'supervisord.pid'
-    identifier: str = 'supervisor'
-    child_logdir: str = '/dev/null'
-    minfds: int = 1024
-    minprocs: int = 200
-    nocleanup: bool = False
-    strip_ansi: bool = False
-    silent: bool = False
-
-    groups: ta.Optional[ta.Sequence['ProcessGroupConfig']] = None
-
-    @classmethod
-    def new(
-            cls,
-            umask: ta.Union[int, str] = 0o22,
-            directory: ta.Optional[str] = None,
-            logfile: str = 'supervisord.log',
-            logfile_maxbytes: ta.Union[int, str] = 50 * 1024 * 1024,
-            loglevel: ta.Union[int, str] = logging.INFO,
-            pidfile: str = 'supervisord.pid',
-            child_logdir: ta.Optional[str] = None,
-            **kwargs: ta.Any,
-    ) -> 'ServerConfig':
-        return cls(
-            umask=octal_type(umask),
-            directory=existing_directory(directory) if directory is not None else None,
-            logfile=existing_dirpath(logfile),
-            logfile_maxbytes=byte_size(logfile_maxbytes),
-            loglevel=logging_level(loglevel),
-            pidfile=existing_dirpath(pidfile),
-            child_logdir=child_logdir if child_logdir else tempfile.gettempdir(),
-            **kwargs,
-        )
-
-
-@dc.dataclass(frozen=True)
-class ProcessGroupConfig:
-    name: str
-
-    priority: int = 999
-
-    processes: ta.Optional[ta.Sequence['ProcessConfig']] = None
-
-
-@dc.dataclass(frozen=True)
 class ProcessConfig:
     name: str
     command: str
@@ -103,8 +49,62 @@ class ProcessConfig:
 
     killasgroup: bool = False
 
-    exitcodes: ta.Iterable[int] = (0,)
+    exitcodes: ta.Sequence[int] = (0,)
 
     redirect_stderr: bool = False
 
     environment: ta.Optional[ta.Mapping[str, str]] = None
+
+
+@dc.dataclass(frozen=True)
+class ProcessGroupConfig:
+    name: str
+
+    priority: int = 999
+
+    processes: ta.Optional[ta.Sequence[ProcessConfig]] = None
+
+
+@dc.dataclass(frozen=True)
+class ServerConfig:
+    user: ta.Optional[str] = None
+    nodaemon: bool = False
+    umask: int = 0o22
+    directory: ta.Optional[str] = None
+    logfile: str = 'supervisord.log'
+    logfile_maxbytes: int = 50 * 1024 * 1024
+    logfile_backups: int = 10
+    loglevel: int = logging.INFO
+    pidfile: str = 'supervisord.pid'
+    identifier: str = 'supervisor'
+    child_logdir: str = '/dev/null'
+    minfds: int = 1024
+    minprocs: int = 200
+    nocleanup: bool = False
+    strip_ansi: bool = False
+    silent: bool = False
+
+    groups: ta.Optional[ta.Sequence[ProcessGroupConfig]] = None
+
+    @classmethod
+    def new(
+            cls,
+            umask: ta.Union[int, str] = 0o22,
+            directory: ta.Optional[str] = None,
+            logfile: str = 'supervisord.log',
+            logfile_maxbytes: ta.Union[int, str] = 50 * 1024 * 1024,
+            loglevel: ta.Union[int, str] = logging.INFO,
+            pidfile: str = 'supervisord.pid',
+            child_logdir: ta.Optional[str] = None,
+            **kwargs: ta.Any,
+    ) -> 'ServerConfig':
+        return cls(
+            umask=octal_type(umask),
+            directory=existing_directory(directory) if directory is not None else None,
+            logfile=existing_dirpath(logfile),
+            logfile_maxbytes=byte_size(logfile_maxbytes),
+            loglevel=logging_level(loglevel),
+            pidfile=existing_dirpath(pidfile),
+            child_logdir=child_logdir if child_logdir else tempfile.gettempdir(),
+            **kwargs,
+        )
--- ominfra/supervisor/exceptions.py
+++ ominfra/supervisor/exceptions.py
@@ -1,18 +1,20 @@
 class ProcessError(Exception):
-    """ Specialized exceptions used when attempting to start a process """
+    """Specialized exceptions used when attempting to start a process."""
 
 
 class BadCommandError(ProcessError):
-    """ Indicates the command could not be parsed properly. """
+    """Indicates the command could not be parsed properly."""
 
 
 class NotExecutableError(ProcessError):
-    """ Indicates that the filespec cannot be executed because its path
-    resolves to a file which is not executable, or which is a directory. """
+    """
+    Indicates that the filespec cannot be executed because its path resolves to a file which is not executable, or which
+    is a directory.
+    """
 
 
 class NotFoundError(ProcessError):
-    """ Indicates that the filespec cannot be executed because it could not be found """
+    """Indicates that the filespec cannot be executed because it could not be found."""
 
 
 class NoPermissionError(ProcessError):
