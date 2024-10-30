ddd7fb52ee244ef7e7a5d2d093fe9bb4b9d92002
wrmsr <timwilloney@gmail.com>
2024-10-30 16:00:52 -0700

    --

--- omdev/tools/doctools.py
+++ omdev/tools/doctools.py
@@ -4,10 +4,14 @@ TODO:
 """
 import contextlib
 import io
+import os.path
+import subprocess
 import sys
+import tempfile
 import typing as ta
 
 from omlish import argparse as ap
+from omlish import check
 from omlish import lang
 
 from ..cli import CliModule
@@ -38,17 +42,35 @@ def rst2html(rst, report_level=None):
         return html, warning
 
 
+def open_html(src: str, name: str) -> None:
+    check.non_empty_str(name)
+    out_dir = tempfile.mkdtemp()
+    out_file = os.path.join(out_dir, name + '.html')
+    with open(out_file, 'w') as f:
+        f.write(src)
+    subprocess.check_call(['open', out_file])
+
+
 class Cli(ap.Cli):
+    def _read_file(self, input_file: str | None) -> tuple[str, str]:
+        if input_file is not None:
+            with open(input_file) as f:
+                src = f.read()
+            name = os.path.basename(input_file)
+
+        else:
+            src = sys.stdin.read()
+            name = 'stdin'
+
+        return src, name
+
     @ap.command(
         ap.arg('input-file', nargs='?'),
         ap.arg('--report-level', type=int),
+        ap.arg('-O', '--open', action='store_true'),
     )
     def rst(self) -> None:
-        if self.args.input_file is not None:
-            with open(self.args.input_file) as f:
-                src = f.read()
-        else:
-            src = sys.stdin.read()
+        src, name = self._read_file(self.args.input_file)
 
         html, warning = rst2html(
             src,
@@ -57,21 +79,25 @@ class Cli(ap.Cli):
 
         if warning:
             sys.stderr.write(warning)
-        print(html)
+
+        if self.args.open:
+            open_html(html, name)
+        else:
+            print(html)
 
     @ap.command(
         ap.arg('input-file', nargs='?'),
+        ap.arg('-O', '--open', action='store_true'),
     )
     def md(self):
-        if self.args.input_file is not None:
-            with open(self.args.input_file) as f:
-                src = f.read()
-        else:
-            src = sys.stdin.read()
+        src, name = self._read_file(self.args.input_file)
 
         html = markdown.markdown(src)
 
-        print(html)
+        if self.args.open:
+            open_html(html, name)
+        else:
+            print(html)
 
 
 # @omlish-manifest
