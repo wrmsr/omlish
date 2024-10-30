8c659d9f35f5a27aa1d0eb6a942765b2ddba8172
wrmsr <timwilloney@gmail.com>
2024-10-30 11:55:05 -0700

    --

---
+++ x/matviews/old.py
@@ -0,0 +1,73 @@
+import typing as ta
+
+from omlish import dataclasses as dc
+from omlish import dispatch
+from omlish import lang
+
+
+Row: ta.TypeAlias = ta.Mapping[str, ta.Any]
+
+
+##
+
+
+class Node(dc.Frozen, lang.Abstract):
+    pass
+
+
+class Scan(Node, lang.Final):
+    table: str
+    columns: ta.Sequence[str] | None = None
+
+
+##
+
+
+class Driver:
+    def __init__(
+            self,
+            *,
+            tables: ta.Mapping[str, ta.Sequence[Row]] | None = None,
+    ) -> None:
+        super().__init__()
+
+        self._tables = tables
+
+    @dispatch.method
+    def drive(self, n: Node) -> ta.Iterable[Row]:
+        raise TypeError(n)
+
+    @drive.register
+    def drive_scan(self, n: Scan) -> ta.Iterable[Row]:
+        if not (tbls := self._tables):
+            raise KeyError(n.table)
+        tbl = tbls[n.table]
+
+        if n.columns is not None:
+            ks: set[str] | None = set(n.columns)
+        else:
+            ks = None
+
+        for r in tbl:
+            if ks is not None:
+                r = {k: v for k, v in r.items() if k in ks}
+            yield r
+
+
+##
+
+
+def _main() -> None:
+    foo_rows = [
+        {'x': 0, 'y': 1},
+        {'x': 2, 'y': 3},
+        {'x': 4, 'y': 5},
+    ]
+    x: Node = Scan('foo', ['y'])
+    d = Driver(tables={'foo': foo_rows})
+    o = list(d.drive(x))
+    print(o)
+
+
+if __name__ == '__main__':
+    _main()
