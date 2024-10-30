4036050578a68a5fb838aea87947e3628ea09a92
wrmsr <timwilloney@gmail.com>
2024-10-29 22:44:11 -0700

    --

--- ommlx/minichain/backends/openai/embedding.py
+++ ommlx/minichain/backends/openai/embedding.py
@@ -1,7 +1,9 @@
-import typing as ta
+import os
 
 from omlish import check
-from omlish import lang
+from omlish import http
+from omlish.formats import json
+from omlish.secrets import Secret
 
 from ...vectors import EmbeddingModel
 from ...vectors import EmbeddingRequest
@@ -9,27 +11,32 @@ from ...vectors import EmbeddingResponse
 from ...vectors import Vector
 
 
-if ta.TYPE_CHECKING:
-    import openai
-else:
-    openai = lang.proxy_import('openai')
-
-
 class OpenaiEmbeddingModel(EmbeddingModel):
     model = 'text-embedding-3-small'
 
-    def __init__(self, *, api_key: str | None = None) -> None:
+    def __init__(
+            self,
+            *,
+            api_key: Secret | str | None = None,
+    ) -> None:
         super().__init__()
-        self._api_key = api_key
+        self._api_key = Secret.of(api_key if api_key is not None else os.environ['OPENAI_API_KEY'])
 
     def invoke(self, request: EmbeddingRequest) -> EmbeddingResponse:
-        client = openai.OpenAI(
-            api_key=self._api_key,
-        )
-
-        response = client.embeddings.create(
+        raw_request = dict(
             model=self.model,
             input=check.isinstance(request.v, str),
         )
 
-        return EmbeddingResponse(v=Vector(response.data[0].embedding))
+        raw_response = http.request(
+            'https://api.openai.com/v1/embeddings',
+            headers={
+                http.consts.HEADER_CONTENT_TYPE: http.consts.CONTENT_TYPE_JSON,
+                http.consts.HEADER_AUTH: http.consts.format_bearer_auth_header(check.not_none(self._api_key).reveal()),
+            },
+            data=json.dumps(raw_request).encode('utf-8'),
+        )
+
+        response = json.loads(check.not_none(raw_response.data).decode('utf-8'))
+
+        return EmbeddingResponse(v=Vector(response['data'][0]['embedding']))
