c73d3f7466e8d28109cd7574c6ae340a9e45b087
wrmsr <timwilloney@gmail.com>
2024-10-31 14:26:05 -0700

    --

---
+++ x/llm/cwpdf.py
@@ -0,0 +1,146 @@
+# MIT License
+#
+# Copyright (c) LangChain, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import dataclasses as dc
+import io
+import os.path
+import typing as ta
+
+import numpy as np
+import pypdf
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class Doc:
+    content: str
+    metadata: ta.Mapping[str, ta.Any] | None = None
+
+
+##
+
+
+def extract_text_from_page(page: pypdf.PageObject) -> str:
+    return page.extract_text(
+        extraction_mode='plain',
+    )
+
+
+##
+
+
+PDF_FILTER_WITH_LOSS = {
+    'DCTDecode',
+    'DCT',
+    'JPXDecode',
+}
+
+PDF_FILTER_WITHOUT_LOSS = {
+    'LZWDecode',
+    'LZW',
+    'FlateDecode',
+    'Fl',
+    'ASCII85Decode',
+    'A85',
+    'ASCIIHexDecode',
+    'AHx',
+    'RunLengthDecode',
+    'RL',
+    'CCITTFaxDecode',
+    'CCF',
+    'JBIG2Decode',
+}
+
+
+def extract_from_images_with_rapidocr(
+        images: ta.Sequence[ta.Iterable[np.ndarray] | bytes],
+) -> str:
+    from rapidocr_onnxruntime import RapidOCR
+
+    ocr = RapidOCR()
+    text = ''
+    for img in images:
+        result, _ = ocr(img)
+        if result:
+            result = [text[1] for text in result]
+            text += "\n".join(result)
+
+    return text
+
+
+def extract_images_from_page(page: pypdf.PageObject) -> str:
+    if '/XObject' not in page['/Resources'].keys():  # noqa
+        return ''
+
+    xobj = page['/Resources']['/XObject'].get_object()  # type: ignore
+    images = []
+    for obj in xobj:
+        if xobj[obj]['/Subtype'] == '/Image':
+            if xobj[obj]['/Filter'][1:] in PDF_FILTER_WITHOUT_LOSS:
+                height, width = xobj[obj]['/Height'], xobj[obj]['/Width']
+
+                images.append(
+                    np.frombuffer(
+                        xobj[obj].get_data(),
+                        dtype=np.uint8,
+                    ).reshape(
+                        height,
+                        width,
+                        -1,
+                    ),
+                )
+
+            elif xobj[obj]['/Filter'][1:] in PDF_FILTER_WITH_LOSS:
+                images.append(xobj[obj].get_data())
+
+            else:
+                raise Exception('Unknown PDF Filter!')
+
+    return extract_from_images_with_rapidocr(images)
+
+
+##
+
+
+def _main() -> None:
+    pdf_file = os.path.expanduser('~/Downloads/nke-10k-2023.pdf')
+    pdf_reader = pypdf.PdfReader(pdf_file)
+
+    extract_images = False
+
+    docs = [
+        Doc(
+            content=' '.join([
+                extract_text_from_page(page),
+                *([extract_images_from_page(page)] if extract_images else []),
+            ]),
+            metadata={
+                'source': pdf_file,
+                'page': page_number
+            },
+        )
+        for page_number, page in enumerate(pdf_reader.pages)
+    ]
+
+    for doc in docs:
+        print(doc)
+
+
+if __name__ == '__main__':
+    _main()
