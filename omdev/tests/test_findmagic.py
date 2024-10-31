b12b288b7cff3e49126de55e1c2f01e725f9752c
wrmsr <timwilloney@gmail.com>
2024-10-31 11:21:14 -0700

    --

--- omdev/tests/test_findmagic.py
+++ omdev/tests/test_findmagic.py
@@ -166,6 +166,7 @@ def find_magic(
         key = start_line[len(first_prefix):].split()[0]
 
         end = start
+        magic: Magic | None = None
         while end < len(lines):
             block_lines = chop_magic_block(
                 key,
@@ -180,16 +181,24 @@ def find_magic(
             try:
                 prepared = compile(block_src, '<magic>', 'eval')
             except SyntaxError:
+                end += 1
                 continue
 
-            out.append(Magic(
+            magic =Magic(
                 key=key,
                 file=file,
                 start_line=start,
                 end_line=end,
                 raw=block_src,
                 prepared=prepared,
-            ))
+            )
+            break
+
+        if magic is None:
+            raise Exception(f'Failed to find magic block terminator : {file=} {start=} {end=}')
+
+        out.append(magic)
+        start += 1
 
     return out
 
@@ -197,35 +206,16 @@ def find_magic(
 def test_multiline_magic():
     print()
 
-    for tests, first_prefix, rest_prefix in [
-        (PY_TESTS, '# ', '# '),
-        (C_LINE_TESTS, '// ', '// '),
-        (C_BLOCK_TESTS, '/* ', ''),
+    for test_file, first_prefix, rest_prefix in [
+        (PY_TEST_FILE.replace('%', '@'), '# ', '# '),
+        # (C_LINE_TESTS, '// ', '// '),
+        # (C_BLOCK_TESTS, '/* ', ''),
     ]:
-        for s in tests:
-            s = first_prefix + s
-            print(s)
-            print()
-
-            lines = s.splitlines(keepends=True)
-            start = 0
-            for end in range(start, len(lines)):
-                block_lines = unmagic_lines(
-                    TEST_MAGIC,
-                    first_prefix,
-                    rest_prefix,
-                    lines[start:end + 1],
-                )
-                if block_lines is None:
-                    raise Exception('Failed to find magic block terminator')
-
-                block_src = ''.join(['(', *block_lines, ')'])
-                try:
-                    code = compile(block_src, '<magic>', 'eval')
-                except SyntaxError:
-                    continue
-
-                print(block_src)
-                block_val = eval(code)  # noqa
-                print(block_val)
-                break
+        magics = find_magic(
+            MAGIC_KEY_PREFIX,
+            first_prefix,
+            rest_prefix,
+            test_file.splitlines(keepends=True),
+            'test-file',
+        )
+        print(magics)
