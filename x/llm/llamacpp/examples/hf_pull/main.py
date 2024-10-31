552f0a4e7aa052b8cdd2730a18bb08eb7cab1c07
wrmsr <timwilloney@gmail.com>
2024-10-30 17:21:36 -0700

    --

--- x/llm/llamacpp/examples/hf_pull/main.py
+++ x/llm/llamacpp/examples/hf_pull/main.py
@@ -1,36 +1,44 @@
+import contextlib
+
 import llama_cpp
 import llama_cpp.llama_tokenizer
 
 
-llama = llama_cpp.Llama.from_pretrained(
-    repo_id="Qwen/Qwen1.5-0.5B-Chat-GGUF",
-    filename="*q8_0.gguf",
-    tokenizer=llama_cpp.llama_tokenizer.LlamaHFTokenizer.from_pretrained(
-        "Qwen/Qwen1.5-0.5B"
-    ),
-    verbose=False,
-)
+def _main() -> None:
+    with contextlib.ExitStack() as es:
+        llama = es.enter_context(contextlib.closing(llama_cpp.Llama.from_pretrained(
+            repo_id="Qwen/Qwen1.5-0.5B-Chat-GGUF",
+            filename="*q8_0.gguf",
+            tokenizer=llama_cpp.llama_tokenizer.LlamaHFTokenizer.from_pretrained(
+                "Qwen/Qwen1.5-0.5B"
+            ),
+            verbose=False,
+        )))
 
-response = llama.create_chat_completion(
-    messages=[{"role": "user", "content": "What is the capital of France?"}],
-    response_format={
-        "type": "json_object",
-        "schema": {
-            "type": "object",
-            "properties": {
-                "country": {"type": "string"},
-                "capital": {"type": "string"},
+        response = llama.create_chat_completion(
+            messages=[{"role": "user", "content": "What is the capital of France?"}],
+            response_format={
+                "type": "json_object",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "country": {"type": "string"},
+                        "capital": {"type": "string"},
+                    },
+                    "required": ["country", "capital"],
+                },
             },
-            "required": ["country", "capital"],
-        },
-    },
-    stream=True,
-)
+            stream=True,
+        )
 
-for chunk in response:
-    delta = chunk["choices"][0]["delta"]
-    if "content" not in delta:
-        continue
-    print(delta["content"], end="", flush=True)
+        for chunk in response:
+            delta = chunk["choices"][0]["delta"]
+            if "content" not in delta:
+                continue
+            print(delta["content"], end="", flush=True)
 
-print()
+        print()
+
+
+if __name__ == '__main__':
+    _main()
