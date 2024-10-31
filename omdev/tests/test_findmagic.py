079192d53696eb235b373fa9d39a2f2d4aac42ef
wrmsr <timwilloney@gmail.com>
2024-10-31 10:58:11 -0700

    --

--- omdev/tests/test_findmagic.py
+++ omdev/tests/test_findmagic.py
@@ -7,43 +7,74 @@ import typing as ta
 
 
 TEST_MAGIC = '@omlish-magic-test'
-PY_COMMENT_PREFIX = '# '
 
 PY_TESTS = [
-    ' @omlish-magic-test',
+    '@omlish-magic-test',
     '\n'.join([
-        ' @omlish-magic-test',
+        '@omlish-magic-test',
         '"bar"',
     ]),
     '\n'.join([
-        ' @omlish-magic-test',
+        '@omlish-magic-test',
         'bar',
     ]),
 
-    ' @omlish-magic-test "foo"',
+    '@omlish-magic-test "foo"',
     '\n'.join([
-        ' @omlish-magic-test "foo"',
+        '@omlish-magic-test "foo"',
         '"bar"',
     ]),
     '\n'.join([
-        ' @omlish-magic-test "foo"',
+        '@omlish-magic-test "foo"',
         'bar',
     ]),
 
     '\n'.join([
-        ' @omlish-magic-test "foo"',
+        '@omlish-magic-test "foo"',
         '# @omlish-magic-test "bar"',
     ]),
 
-    ' @omlish-magic-test "foo", "bar"',
+    '@omlish-magic-test "foo", "bar"',
 
-    ' @omlish-magic-test {"foo": 1, "bar": 2}',
+    '@omlish-magic-test {"foo": 1, "bar": 2}',
 
     '\n'.join([
-        ' @omlish-magic-test {',
+        '@omlish-magic-test {',
         '#     "foo": 1,',
         '#     "bar": 2',
         '# }',
+    ]),
+]
+
+C_LINE_TESTS = [
+    '@omlish-magic-test',
+
+    '\n'.join([
+        '@omlish-magic-test "foo"',
+        '// @omlish-magic-test "bar"',
+    ]),
+
+    '\n'.join([
+        '@omlish-magic-test {',
+        '//     "foo": 1,',
+        '//     "bar": 2',
+        '// }',
+    ]),
+]
+
+C_BLOCK_TESTS = [
+    '@omlish-magic-test',
+
+    '\n'.join([
+        '@omlish-magic-test "foo"',
+        '@omlish-magic-test "bar"',
+    ]),
+
+    '\n'.join([
+        '@omlish-magic-test {',
+        '    "foo": 1,',
+        '    "bar": 2',
+        '}',
     ]),
 ]
 
@@ -70,31 +101,35 @@ def unmagic_lines(
 def test_multiline_magic():
     print()
 
-    for s in PY_TESTS:
-        s = '#' + s
-        print(s)
-        print()
+    for tests, first_prefix, rest_prefix in [
+        (PY_TESTS, '# ', '# '),
+        (C_LINE_TESTS, '// ', '// '),
+        (C_BLOCK_TESTS, '/* ', ''),
+    ]:
+        for s in tests:
+            s = first_prefix + s
+            print(s)
+            print()
 
-        lines = s.splitlines(keepends=True)
-        start = 0
-        assert lines[start].startswith(PY_COMMENT_PREFIX + TEST_MAGIC)
-        for end in range(start, len(lines)):
-            block_lines = unmagic_lines(
-                TEST_MAGIC,
-                '# ',
-                '# ',
-                lines[start:end + 1],
-            )
-            if block_lines is None:
-                raise Exception('Failed to find magic block terminator')
+            lines = s.splitlines(keepends=True)
+            start = 0
+            for end in range(start, len(lines)):
+                block_lines = unmagic_lines(
+                    TEST_MAGIC,
+                    first_prefix,
+                    rest_prefix,
+                    lines[start:end + 1],
+                )
+                if block_lines is None:
+                    raise Exception('Failed to find magic block terminator')
 
-            block_src = ''.join(['(', *block_lines, ')'])
-            try:
-                code = compile(block_src, '<magic>', 'eval')
-            except SyntaxError:
-                continue
+                block_src = ''.join(['(', *block_lines, ')'])
+                try:
+                    code = compile(block_src, '<magic>', 'eval')
+                except SyntaxError:
+                    continue
 
-            print(block_src)
-            block_val = eval(code)
-            print(block_val)
-            break
+                print(block_src)
+                block_val = eval(code)  # noqa
+                print(block_val)
+                break
