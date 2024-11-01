03675f5aefdee5c9b5a83991ded0d7dcee939b21
wrmsr <timwilloney@gmail.com>
2024-11-01 11:53:21 -0700

    --

--- x/proxy_init.py
+++ x/proxy_init.py
@@ -15,6 +15,20 @@ import typing as ta
 from omlish import lang
 
 
+class _ProxyInit:
+    def __init__(self, package: str) -> None:
+        super().__init__()
+
+        self._package = package
+
+    @property
+    def package(self) -> str:
+        return self._package
+
+    def get(self, attr: str) -> ta.Any:
+        raise NotImplementedError
+
+
 def proxy_init(
         globals: ta.MutableMapping[str, ta.Any],  # noqa
         package: str,
@@ -23,11 +37,24 @@ def proxy_init(
     if isinstance(attrs, str):
         raise TypeError(attrs)
 
-    raise NotImplementedError
+    init_package = globals['__name__']
+
+    pi: _ProxyInit
+    try:
+        pi = globals['__proxy_init__']
+    except KeyError:
+        pi = _ProxyInit(init_package)
+        globals['__proxy_init__'] = pi
+    else:
+        if pi.package != init_package:
+            raise Exception('Wrong init package')
 
 
 def _main() -> None:
-    pass
+    from . import proxy_init_test as pit
+    print(pit)
+    print(pit._ta)
+    print(pit.foo)
 
 
 if __name__ == '__main__':
