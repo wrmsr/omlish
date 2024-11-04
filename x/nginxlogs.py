09b815a6004f90e0721e0a3a7885731f16429853
wrmsr <timwilloney@gmail.com>
2024-11-04 10:14:26 -0800

    --

--- x/nginxlogs.py
+++ x/nginxlogs.py
@@ -1,3 +1,5 @@
+import argparse
+import contextlib
 import itertools
 import json
 import re
@@ -31,25 +33,44 @@ body_bytes_sent
 
 ==
 
-remote_addr
--
-remote_user
-time_local
-request
-status
-body_bytes_sent
-http_referer
-http_user_agent
 
 """
 
 
 def _main() -> None:
-    pat = re.compile(r'("[^"]*")|(\[^\]*\]")|([^ ]+)')
-    for s in sys.stdin:
-        l = list(itertools.batched(pat.split(s), 3))
-        v = [a or b for t in l if len(t) > 1 for _, a, b in [t]]
-        
-        
+    pat = re.compile(r'(\[[^\]]*\])|("[^"]*")|([^ ]+)')
+
+    cols = [
+        'remote_addr',
+        '-',
+        'remote_user',
+        'time_local',
+        'request',
+        'status',
+        'body_bytes_sent',
+        'http_referer',
+        'http_user_agent',
+    ]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', nargs='?')
+    args = parser.parse_args()
+
+    with contextlib.ExitStack() as es:
+        if args.file:
+            f = es.enter_context(open(args.file, 'r'))
+        else:
+            f = sys.stdin
+
+        for s in f:
+            vs = []
+            for t in itertools.batched(pat.split(s.strip()), 4):
+                if len(t) < 2:
+                    continue
+                [v] = filter(None, t[1:])
+                vs.append(v)
+            d = dict(zip(cols, vs, strict=True))
+
+
 if __name__ == '__main__':
     _main()
