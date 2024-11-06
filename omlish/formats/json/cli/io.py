8f621769b29db1726f503f7ef3834f67b04f886b
wrmsr <timwilloney@gmail.com>
2024-11-06 12:19:41 -0800

    --

--- omlish/formats/json/cli/io.py
+++ omlish/formats/json/cli/io.py
@@ -3,8 +3,11 @@ TODO:
  - -I/-Ogz, lz4, etc
   - fnpairs? or not yet just do it
 """
+import abc
 import contextlib
+import dataclasses as dc
 import gzip
+import os
 import typing as ta
 
 from .... import lang
@@ -20,11 +23,49 @@ else:
     _lzma = lang.proxy_import('lzma')
 
 
-IoCodec: ta.TypeAlias = ta.Callable[[ta.IO, str], ta.ContextManager[ta.IO]]
+##
 
 
-# class TrampolineThread:
-#     pass
+class BaseIo(lang.Abstract):
+    def close(self) -> None:
+        raise NotImplementedError
+
+    def fileno(self) -> int | None:
+        return None
+
+
+class Input(BaseIo):
+    @abc.abstractmethod
+    def read(self, sz: int | None = None) -> bytes:
+        raise NotImplementedError
+
+
+class Output(BaseIo):
+    @abc.abstractmethod
+    def write(self, data: bytes) -> int:
+        raise NotImplementedError
+
+
+#
+
+
+DEFAULT_READ_SZ = 0x4000
+
+
+@dc.dataclass(frozen=True)
+class FdIo(Input, Output):
+    fd: int
+
+    default_read_sz: int = DEFAULT_READ_SZ
+
+    def read(self, sz: int | None = None) -> bytes:
+        return os.read(self.fd, sz or self.default_read_sz)
+
+    def write(self, data: bytes) -> int:
+        return os.write(self.fd, data)
+
+
+##
 
 
 @contextlib.contextmanager
