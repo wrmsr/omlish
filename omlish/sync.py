2be4e3ccc9b7ce5a660934daec5443bd26b7371a
wrmsr <timwilloney@gmail.com>
2024-11-06 15:34:18 -0800

    --

--- omlish/sync.py
+++ omlish/sync.py
@@ -3,6 +3,7 @@ TODO:
  - sync (lol) w/ asyncs.anyio
  - atomics
 """
+import collections
 import threading
 import typing as ta
 
@@ -68,3 +69,57 @@ class LazyFn(ta.Generic[T]):
             self._v = lang.just(self._fn())
         self._once.do(do)
         return self._v.must()
+
+
+class ConditionDeque(ta.Generic[T]):
+    def __init__(
+            self,
+            *,
+            cond: threading.Condition | None = None,
+            deque: collections.deque[T] | None = None,
+
+            lock: ta.Optional['threading.RLock'] = None,
+            maxlen: int | None = None,
+            init: ta.Iterable[T] | None = None,
+    ) -> None:
+        super().__init__()
+
+        if cond is None:
+            cond = threading.Condition(lock=lock)
+        if deque is None:
+            deque = collections.deque(maxlen=maxlen)
+        if init:
+            deque.extend(init)
+
+        self._cond = cond
+        self._deque = deque
+
+    @property
+    def cond(self) -> threading.Condition:
+        return self._cond
+
+    @property
+    def deque(self) -> collections.deque[T]:
+        return self._deque
+
+    def push(
+            self,
+            *items: T,
+            n: int = 1,
+    ) -> None:
+        with self.cond:
+            self.deque.extend(items)
+            self.cond.notify(n)
+
+    def pop(
+            self,
+            timeout: float | None = None,
+            *,
+            if_empty: ta.Callable[[], None] | None = None,
+    ) -> T:
+        with self.cond:
+            if not self.deque and if_empty is not None:
+                if_empty()
+            while not self.deque:
+                self.cond.wait(timeout)
+            return self.deque.popleft()
