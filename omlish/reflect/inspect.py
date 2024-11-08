af5ffe00c81d63f248c743f657342a3f1a182c12
wrmsr <timwilloney@gmail.com>
2024-11-08 11:51:23 -0800

    --

---
+++ omlish/reflect/inspect.py
@@ -0,0 +1,23 @@
+"""
+TODO:
+ - vs ta.get_type_hints?
+"""
+import typing as ta
+
+from .. import lang
+
+
+if ta.TYPE_CHECKING:
+    import inspect
+else:
+    inspect = lang.proxy_import('inspect')
+
+
+annotationlib = lang.lazy_import('annotationlib', optional=True, cache_failure=True)
+
+
+def get_annotations(obj: ta.Any) -> ta.Mapping[str, ta.Any]:
+    if al := (annotationlib()) is not None:
+        return al.get_annotations(obj, format=annotationlib.Format.FORWARDREF)  # noqa
+    else:
+        return inspect.get_annotations(obj)
