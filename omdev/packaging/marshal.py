695f37f0dda3d6772db0daf606c4b98060c67deb
wrmsr <timwilloney@gmail.com>
2024-11-08 12:44:43 -0800

    --

--- omdev/packaging/marshal.py
+++ omdev/packaging/marshal.py
@@ -1,50 +1,54 @@
-import typing as ta
-
-from omlish import lang
-from omlish import marshal as msh
-from omlish import reflect as rfl
-
-from .requires import RequiresMarkerList
-from .requires import RequiresNode
-from .requires import RequiresOp
-from .requires import RequiresValue
-from .requires import RequiresVariable
-
-
-##
-
-
-class MarshalRequiresMarkerList(lang.NotInstantiable, lang.Final):
-    pass
-
-
-##
-
-
-@lang.static_init
-def _install_standard_marshalling() -> None:
-    requires_node_poly = msh.Polymorphism(
-        RequiresNode,
-        [
-            msh.Impl(RequiresVariable, 'variable'),
-            msh.Impl(RequiresValue, 'value'),
-            msh.Impl(RequiresOp, 'op'),
-        ],
-    )
-    msh.STANDARD_MARSHALER_FACTORIES[0:0] = [
-        msh.PolymorphismMarshalerFactory(requires_node_poly),
-    ]
-    msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [
-        msh.PolymorphismUnmarshalerFactory(requires_node_poly),
-    ]
-
-    msh.GLOBAL_REGISTRY.register(
-        RequiresMarkerList,
-        msh.ReflectOverride(MarshalRequiresMarkerList),
-        identity=True,
-    )
-
-    RequiresMarkerVar = ta.Union['RequiresVariable', 'RequiresValue']
-    RequiresMarkerItem = ta.Tuple['RequiresMarkerVar', 'RequiresOp', 'RequiresMarkerVar']
-    RequiresMarkerAtom = ta.Union['RequiresMarkerItem', ta.Sequence['RequiresMarkerAtom']]
-    RequiresMarkerList = ta.Sequence[ta.Union['RequiresMarkerList', 'RequiresMarkerAtom', str]]
+"""
+NOTE: This cannot be auto-imported as @omlish-lite usage of other modules in this package requires it be importable on
+8.
+"""
+# import typing as ta
+#
+# from omlish import lang
+# from omlish import marshal as msh
+# from omlish import reflect as rfl
+#
+# from .requires import RequiresMarkerList
+# from .requires import RequiresNode
+# from .requires import RequiresOp
+# from .requires import RequiresValue
+# from .requires import RequiresVariable
+#
+#
+# ##
+#
+#
+# class MarshalRequiresMarkerList(lang.NotInstantiable, lang.Final):
+#     pass
+#
+#
+# ##
+#
+#
+# @lang.static_init
+# def _install_standard_marshalling() -> None:
+#     requires_node_poly = msh.Polymorphism(
+#         RequiresNode,
+#         [
+#             msh.Impl(RequiresVariable, 'variable'),
+#             msh.Impl(RequiresValue, 'value'),
+#             msh.Impl(RequiresOp, 'op'),
+#         ],
+#     )
+#     msh.STANDARD_MARSHALER_FACTORIES[0:0] = [
+#         msh.PolymorphismMarshalerFactory(requires_node_poly),
+#     ]
+#     msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [
+#         msh.PolymorphismUnmarshalerFactory(requires_node_poly),
+#     ]
+#
+#     msh.GLOBAL_REGISTRY.register(
+#         RequiresMarkerList,
+#         msh.ReflectOverride(MarshalRequiresMarkerList),
+#         identity=True,
+#     )
+#
+#     RequiresMarkerVar = ta.Union['RequiresVariable', 'RequiresValue']
+#     RequiresMarkerItem = ta.Tuple['RequiresMarkerVar', 'RequiresOp', 'RequiresMarkerVar']
+#     RequiresMarkerAtom = ta.Union['RequiresMarkerItem', ta.Sequence['RequiresMarkerAtom']]
+#     RequiresMarkerList = ta.Sequence[ta.Union['RequiresMarkerList', 'RequiresMarkerAtom', str]]
--- omlish/reflect/tests/test_types.py
+++ omlish/reflect/tests/test_types.py
@@ -1,3 +1,4 @@
+# ruff: noqa: UP006 UP007
 import collections.abc
 import dataclasses as dc
 import pprint
