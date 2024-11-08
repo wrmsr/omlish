8582789e5669b9be425f468363e693629ff645fa
wrmsr <timwilloney@gmail.com>
2024-11-08 12:22:16 -0800

    --

--- omdev/packaging/marshal.py
+++ omdev/packaging/marshal.py
@@ -1,8 +1,14 @@
+import typing as ta
+
 from omlish import lang
 from omlish import marshal as msh
 from omlish import reflect as rfl
 
 from .requires import RequiresMarkerList
+from .requires import RequiresNode
+from .requires import RequiresOp
+from .requires import RequiresValue
+from .requires import RequiresVariable
 
 
 ##
@@ -17,6 +23,21 @@ class MarshalRequiresMarkerList(lang.NotInstantiable, lang.Final):
 
 @lang.static_init
 def _install_standard_marshalling() -> None:
+    requires_node_poly = msh.Polymorphism(
+        RequiresNode,
+        [
+            msh.Impl(RequiresVariable, 'variable'),
+            msh.Impl(RequiresValue, 'value'),
+            msh.Impl(RequiresOp, 'op'),
+        ],
+    )
+    msh.STANDARD_MARSHALER_FACTORIES[0:0] = [
+        msh.PolymorphismMarshalerFactory(requires_node_poly),
+    ]
+    msh.STANDARD_UNMARSHALER_FACTORIES[0:0] = [
+        msh.PolymorphismUnmarshalerFactory(requires_node_poly),
+    ]
+
     msh.GLOBAL_REGISTRY.register(
         RequiresMarkerList,
         msh.ReflectOverride(MarshalRequiresMarkerList),
