feaa4c4f0ce2361e3a6ab95afcfe64188cf70c24
wrmsr <timwilloney@gmail.com>
2024-11-12 12:27:29 -0800

    --

--- omlish/diag/pycharm/__init__.py
+++
@@ -1,7 +0,0 @@
-from .pycharm import (  # noqa
-    PycharmRemoteDebugger,
-    get_pycharm_version,
-    is_pycharm_hosted,
-    pycharm_remote_debugger_attach,
-    read_darwin_pycharm_info_plist,
-)
