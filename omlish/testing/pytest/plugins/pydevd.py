447074e5a00c28422287ef00a5636712bf13cc0b
wrmsr <timwilloney@gmail.com>
2024-11-12 19:52:13 -0800

    --

--- omlish/testing/pytest/plugins/pydevd.py
+++ omlish/testing/pytest/plugins/pydevd.py
@@ -5,8 +5,20 @@ from ._registry import register
 @register
 class PydevdPlugin:
 
+    def pytest_addoption(self, parser):
+        parser.addoption('--no-pydevd', action='store_true', default=False, help='Disables pydevd debugging')
+
     def pytest_collection(self, session):
-        setup = opd.get_setup()
-        if setup is not None:
+        if opd.get_setup() is not None:
             if hasattr(session.config, '_env_timeout'):
                 session.config._env_timeout = None  # noqa
+
+            # if (dbg := opd.get_global_debugger()) is not None:
+            #     dbg.set_unit_tests_debugging_mode()
+
+    def pytest_exception_interact(self, node, call, report):
+        if opd.get_setup() is not None:
+            if not node.session.config.option.no_pydevd:
+                opd.debug_unhandled_exception(call.excinfo._excinfo)  # noqa
+
+        return report
