c5ca085fa58ab0fd7c759cf03df2602bf511580b
wrmsr <timwilloney@gmail.com>
2024-11-18 12:21:42 -0800

    --

--- ominfra/supervisor/tests/test_supervisor.py
+++ ominfra/supervisor/tests/test_supervisor.py
@@ -1,6 +1,14 @@
+import os.path
 import unittest
+
+from ..supervisor import main
 
 
 class TestSupervisor(unittest.TestCase):
     def test_supervisor(self):
-        pass
+        conf_file = os.path.join(os.path.dirname(__file__), 'demo.json')
+        main(
+            [conf_file],
+            test=True,
+            # no_logging=True,
+        )
