bd570c1ebd40cfbb5222c4375b8050daf96a8ad9
wrmsr <timwilloney@gmail.com>
2024-11-18 12:21:42 -0800

    --

---
+++ x/infra/supervisor/tests/test_confecho.py
@@ -0,0 +1,13 @@
+"""Test suite for supervisor.confecho"""
+
+import unittest
+from supervisor.compat import StringIO
+from supervisor import confecho
+
+class TopLevelFunctionTests(unittest.TestCase):
+    def test_main_writes_data_out_that_looks_like_a_config_file(self):
+        sio = StringIO()
+        confecho.main(out=sio)
+
+        output = sio.getvalue()
+        self.assertTrue("[supervisord]" in output)
