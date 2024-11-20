4995742f2601add1fb334ee4d0b0921698a0d139
wrmsr <timwilloney@gmail.com>
2024-11-19 18:03:48 -0800

    --

--- ominfra/supervisor/types.py
+++ ominfra/supervisor/types.py
@@ -1,9 +1,12 @@
-# ruff: noqa: UP006
+# ruff: noqa: UP006 UP007
 import abc
+import functools
 import typing as ta
 
 from .configs import ProcessConfig
+from .configs import ProcessGroupConfig
 from .configs import ServerConfig
+from .states import ProcessState
 from .states import SupervisorState
 
 
@@ -28,6 +31,7 @@ class AbstractServerContext(abc.ABC):
         raise NotImplementedError
 
 
+@functools.total_ordering
 class AbstractSubprocess(abc.ABC):
     @property
     @abc.abstractmethod
@@ -39,6 +43,12 @@ class AbstractSubprocess(abc.ABC):
     def config(self) -> ProcessConfig:
         raise NotImplementedError
 
+    def __lt__(self, other):
+        return self.config.priority < other.config.priority
+
+    def __eq__(self, other):
+        return self.config.priority == other.config.priority
+
     @property
     @abc.abstractmethod
     def context(self) -> AbstractServerContext:
@@ -47,3 +57,49 @@ class AbstractSubprocess(abc.ABC):
     @abc.abstractmethod
     def finish(self, sts: int) -> None:
         raise NotImplementedError
+
+    @abc.abstractmethod
+    def remove_logs(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def reopen_logs(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def stop(self) -> ta.Optional[str]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def give_up(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def transition(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_state(self) -> ProcessState:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def create_auto_child_logs(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_dispatchers(self) -> ta.Mapping[int, ta.Any]:  # dict[int, Dispatcher]
+        raise NotImplementedError
+
+
+@functools.total_ordering
+class AbstractProcessGroup(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def config(self) -> ProcessGroupConfig:
+        raise NotImplementedError
+
+    def __lt__(self, other):
+        return self.config.priority < other.config.priority
+
+    def __eq__(self, other):
+        return self.config.priority == other.config.priority
