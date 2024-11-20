2d289210bf04b2f54ce323f03b3c90aaead95173
wrmsr <timwilloney@gmail.com>
2024-11-19 16:43:05 -0800

    --

--- ominfra/supervisor/poller.py
+++ ominfra/supervisor/poller.py
@@ -48,30 +48,30 @@ class SelectPoller(BasePoller):
     def __init__(self) -> None:
         super().__init__()
 
-        self._readables: ta.Set[int] = set()
-        self._writables: ta.Set[int] = set()
+        self._readable: ta.Set[int] = set()
+        self._writable: ta.Set[int] = set()
 
     def register_readable(self, fd: int) -> None:
-        self._readables.add(fd)
+        self._readable.add(fd)
 
     def register_writable(self, fd: int) -> None:
-        self._writables.add(fd)
+        self._writable.add(fd)
 
     def unregister_readable(self, fd: int) -> None:
-        self._readables.discard(fd)
+        self._readable.discard(fd)
 
     def unregister_writable(self, fd: int) -> None:
-        self._writables.discard(fd)
+        self._writable.discard(fd)
 
     def unregister_all(self) -> None:
-        self._readables.clear()
-        self._writables.clear()
+        self._readable.clear()
+        self._writable.clear()
 
     def poll(self, timeout: ta.Optional[float]) -> ta.Tuple[ta.List[int], ta.List[int]]:
         try:
             r, w, x = select.select(
-                self._readables,
-                self._writables,
+                self._readable,
+                self._writable,
                 [], timeout,
             )
         except OSError as err:
@@ -94,40 +94,40 @@ class PollPoller(BasePoller):
         super().__init__()
 
         self._poller = select.poll()
-        self._readables: set[int] = set()
-        self._writables: set[int] = set()
+        self._readable: set[int] = set()
+        self._writable: set[int] = set()
 
     def register_readable(self, fd: int) -> None:
         self._poller.register(fd, self._READ)
-        self._readables.add(fd)
+        self._readable.add(fd)
 
     def register_writable(self, fd: int) -> None:
         self._poller.register(fd, self._WRITE)
-        self._writables.add(fd)
+        self._writable.add(fd)
 
     def unregister_readable(self, fd: int) -> None:
-        self._readables.discard(fd)
+        self._readable.discard(fd)
         self._poller.unregister(fd)
-        if fd in self._writables:
+        if fd in self._writable:
             self._poller.register(fd, self._WRITE)
 
     def unregister_writable(self, fd: int) -> None:
-        self._writables.discard(fd)
+        self._writable.discard(fd)
         self._poller.unregister(fd)
-        if fd in self._readables:
+        if fd in self._readable:
             self._poller.register(fd, self._READ)
 
     def poll(self, timeout: ta.Optional[float]) -> ta.Tuple[ta.List[int], ta.List[int]]:
         fds = self._poll_fds(timeout)  # type: ignore
-        readables, writables = [], []
+        readable, writable = [], []
         for fd, eventmask in fds:
             if self._ignore_invalid(fd, eventmask):
                 continue
             if eventmask & self._READ:
-                readables.append(fd)
+                readable.append(fd)
             if eventmask & self._WRITE:
-                writables.append(fd)
-        return readables, writables
+                writable.append(fd)
+        return readable, writable
 
     def _poll_fds(self, timeout: float) -> ta.List[ta.Tuple[int, int]]:
         try:
@@ -143,8 +143,8 @@ class PollPoller(BasePoller):
             # POLLNVAL means `fd` value is invalid, not open. When a process quits it's `fd`s are closed so there is no
             # more reason to keep this `fd` registered If the process restarts it's `fd`s are registered again.
             self._poller.unregister(fd)
-            self._readables.discard(fd)
-            self._writables.discard(fd)
+            self._readable.discard(fd)
+            self._writable.discard(fd)
             return True
         return False
 
@@ -157,27 +157,27 @@ if sys.platform == 'darwin' or sys.platform.startswith('freebsd'):
             super().__init__()
 
             self._kqueue: ta.Optional[ta.Any] = select.kqueue()
-            self._readables: set[int] = set()
-            self._writables: set[int] = set()
+            self._readable: set[int] = set()
+            self._writable: set[int] = set()
 
         def register_readable(self, fd: int) -> None:
-            self._readables.add(fd)
+            self._readable.add(fd)
             kevent = select.kevent(fd, filter=select.KQ_FILTER_READ, flags=select.KQ_EV_ADD)
             self._kqueue_control(fd, kevent)
 
         def register_writable(self, fd: int) -> None:
-            self._writables.add(fd)
+            self._writable.add(fd)
             kevent = select.kevent(fd, filter=select.KQ_FILTER_WRITE, flags=select.KQ_EV_ADD)
             self._kqueue_control(fd, kevent)
 
         def unregister_readable(self, fd: int) -> None:
             kevent = select.kevent(fd, filter=select.KQ_FILTER_READ, flags=select.KQ_EV_DELETE)
-            self._readables.discard(fd)
+            self._readable.discard(fd)
             self._kqueue_control(fd, kevent)
 
         def unregister_writable(self, fd: int) -> None:
             kevent = select.kevent(fd, filter=select.KQ_FILTER_WRITE, flags=select.KQ_EV_DELETE)
-            self._writables.discard(fd)
+            self._writable.discard(fd)
             self._kqueue_control(fd, kevent)
 
         def _kqueue_control(self, fd: int, kevent: 'select.kevent') -> None:
@@ -190,32 +190,32 @@ if sys.platform == 'darwin' or sys.platform.startswith('freebsd'):
                     raise
 
         def poll(self, timeout: ta.Optional[float]) -> ta.Tuple[ta.List[int], ta.List[int]]:
-            readables, writables = [], []  # type: ignore
+            readable, writable = [], []  # type: ignore
 
             try:
                 kevents = self._kqueue.control(None, self.max_events, timeout)  # type: ignore
             except OSError as error:
                 if error.errno == errno.EINTR:
                     log.debug('EINTR encountered in poll')
-                    return readables, writables
+                    return readable, writable
                 raise
 
             for kevent in kevents:
                 if kevent.filter == select.KQ_FILTER_READ:
-                    readables.append(kevent.ident)
+                    readable.append(kevent.ident)
                 if kevent.filter == select.KQ_FILTER_WRITE:
-                    writables.append(kevent.ident)
+                    writable.append(kevent.ident)
 
-            return readables, writables
+            return readable, writable
 
         def before_daemonize(self) -> None:
             self.close()
 
         def after_daemonize(self) -> None:
             self._kqueue = select.kqueue()
-            for fd in self._readables:
+            for fd in self._readable:
                 self.register_readable(fd)
-            for fd in self._writables:
+            for fd in self._writable:
                 self.register_writable(fd)
 
         def close(self) -> None:
