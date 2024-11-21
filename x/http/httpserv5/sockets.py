9d3311af8d6d30913764fe9c38128ef238f241c2
wrmsr <timwilloney@gmail.com>
2024-11-21 12:59:37 -0800

    --

--- x/http/httpserv5/sockets.py
+++ x/http/httpserv5/sockets.py
@@ -12,22 +12,3 @@ from omlish.lite.sockets import SocketAddress
 ##
 
 
-SocketHandlerFactory: ta.TypeAlias = ta.Callable[[SocketAddress, ta.BinaryIO, ta.BinaryIO], 'SocketHandler']  # noqa
-
-
-class SocketHandler(abc.ABC):
-    def __init__(
-            self,
-            client_address: SocketAddress,
-            rfile: ta.BinaryIO,
-            wfile: ta.BinaryIO,
-    ) -> None:
-        super().__init__()
-
-        self.client_address = client_address
-        self.rfile = rfile
-        self.wfile = wfile
-
-    @abc.abstractmethod
-    def handle(self) -> None:
-        raise NotImplementedError
