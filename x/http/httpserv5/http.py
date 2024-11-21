26ce2538b7a7f004d0219505c998ea269606f030
wrmsr <timwilloney@gmail.com>
2024-11-21 11:06:59 -0800

    --

--- x/http/httpserv5/http.py
+++ x/http/httpserv5/http.py
@@ -5,6 +5,7 @@ import html
 import http.client
 import http.server
 import io
+import textwrap
 import time
 import typing as ta
 
@@ -348,21 +349,21 @@ class HttpSocketRequestHandler(SocketRequestHandler):
 
     #
 
-    DEFAULT_ERROR_MESSAGE = """\
-    <!DOCTYPE HTML>
-    <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <title>Error response</title>
-        </head>
-        <body>
-            <h1>Error response</h1>
-            <p>Error code: %(code)d</p>
-            <p>Message: %(message)s.</p>
-            <p>Error code explanation: %(code)s - %(explain)s.</p>
-        </body>
-    </html>
-    """
+    DEFAULT_ERROR_MESSAGE = textwrap.dedent("""\
+        <!DOCTYPE HTML>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title>Error response</title>
+            </head>
+            <body>
+                <h1>Error response</h1>
+                <p>Error code: %(code)d</p>
+                <p>Message: %(message)s.</p>
+                <p>Error code explanation: %(code)s - %(explain)s.</p>
+            </body>
+        </html>
+    """)
 
     DEFAULT_ERROR_CONTENT_TYPE = 'text/html;charset=utf-8'
 
