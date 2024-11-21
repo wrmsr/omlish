f32cabc125a6b3d559b339dc397a6f70db97ea4a
wrmsr <timwilloney@gmail.com>
2024-11-21 11:34:39 -0800

    --

--- omlish/lite/http/parsing.py
+++ omlish/lite/http/parsing.py
@@ -5,29 +5,14 @@ import http.server
 import io
 import typing as ta
 
+from .versions import HttpProtocolVersion
+from .versions import HttpProtocolVersions
+
 
 T = ta.TypeVar('T')
 
 
 HttpHeaders = http.client.HTTPMessage  # ta.TypeAlias
-
-
-##
-
-
-class HttpProtocolVersion(ta.NamedTuple):
-    major: int
-    minor: int
-
-    def __str__(self) -> str:
-        return f'HTTP/{self.major}.{self.minor}'
-
-
-class HttpProtocolVersions:
-    HTTP_0_9 = HttpProtocolVersion(0, 9)
-    HTTP_1_0 = HttpProtocolVersion(1, 0)
-    HTTP_1_1 = HttpProtocolVersion(1, 1)
-    HTTP_2_0 = HttpProtocolVersion(2, 0)
 
 
 ##
