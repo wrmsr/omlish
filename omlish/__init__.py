e4a09f0cce51439893881dfd71ddead2a12ae226
wrmsr <timwilloney@gmail.com>
2024-11-20 20:14:17 -0800

    --

---
+++ omlish/__init__.py
@@ -0,0 +1,3 @@
+# While the library as a whole only supports modern python, the 'lite' package is importable and usable down to python
+# 3.8. As a result this file needs to be kept basically empty as anything it would otherwise do would likely involve
+# non-lite code.
