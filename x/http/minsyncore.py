cdafeb073904c5a4e31174e8807e8139284b5f77
wrmsr <timwilloney@gmail.com>
2024-11-25 11:46:55 -0800

    --

--- x/http/minsyncore.py
+++ x/http/minsyncore.py
@@ -1,10 +1,67 @@
-import functools
 import select
 import socket
 import typing as ta
 
+from omlish.lite.http.coroserver import CoroHttpServer
+from omlish.lite.http.handlers import HttpHandler
+from omlish.lite.http.handlers import HttpHandlerRequest
+from omlish.lite.http.handlers import HttpHandlerResponse
+from omlish.lite.io import DelimitingBuffer
+from omlish.lite.socket import SocketAddress
 
-Addr: ta.TypeAlias = tuple[str, int]
+
+class DelimitingReader:
+    def __init__(self) -> None:
+        super().__init__()
+        self._lst: list[bytes] = []
+
+    def feed(self, d: bytes) -> None:
+        if d:
+            self._lst.append(d)
+
+    def _chop(self, i: int, e: int) -> bytes:
+        lst = self._lst
+        d = lst[i]
+
+        o = b''.join([
+            *lst[:i],
+            d[:e],
+        ])
+
+        self._lst = [
+            *(d[e:] if e < len(d) else []),
+            *lst[i:],
+        ]
+
+        return o
+
+    def read(self, n: int | None = None) -> bytes | None:
+        if n is None:
+            o = b''.join(self._lst)
+            self._lst = []
+            return o
+
+        if not (lst := self._lst):
+            return None
+
+        c = 0
+        for i, d in enumerate(lst):
+            r = n - c
+            if (l := len(d)) >= r:
+                return self._chop(i, r)
+            c += l
+
+        return None
+
+    def readline(self, delim: bytes = b'\n') -> bytes | None:
+        if not (lst := self._lst):
+            return None
+
+        for i, d in enumerate(lst):
+            if (p := d.find(delim)) >= 0:
+                return self._chop(i, p + 1)
+
+        return None
 
 
 class IoManager:
@@ -59,43 +116,124 @@ class IoManager:
             self._write_callbacks[wf]()
 
 
-class EchoServerConnection:
-    def __init__(self, addr: Addr, sock: socket.socket, io: IoManager) -> None:
+class HttpServer:
+    def __init__(
+            self,
+            addr: SocketAddress,
+            handler: HttpHandler,
+            io: IoManager,
+    ) -> None:
         super().__init__()
+
         self._addr = addr
-        self._sock = sock
+        self._handler = handler
         self._io = io
-        io.register(self._on_read, r=[sock.fileno()])
 
-    def _on_read(self) -> None:
-        buf = self._sock.recv(1024)
-        if not buf:
-            self._io.unregister(r=[self._sock.fileno()])
-            self._sock.close()
-            return
-        self._sock.send(buf)
-
-
-class EchoServer:
-    def __init__(self, addr: Addr, io: IoManager) -> None:
-        super().__init__()
-        self._addr = addr
-        self._io = io
         self._sock = socket.create_server(self._addr)
         self._sock.listen(1)
+
         io.register(self._on_conn, r=[self._sock.fileno()])
 
     def _on_conn(self) -> None:
         cli_sock, cli_addr = self._sock.accept()
-        EchoServerConnection(cli_addr, cli_sock, self._io)
+        HttpServerConnection(
+            cli_addr,
+            cli_sock,
+            self._handler,
+            self._io,
+        )
 
+
+class HttpServerConnection:
+    def __init__(
+            self,
+            addr: SocketAddress,
+            sock: socket.socket,
+            handler: HttpHandler,
+            io: IoManager,
+    ) -> None:
+        super().__init__()
+
+        self._addr = addr
+        self._sock = sock
+        self._handler = handler
+        self._io = io
+
+        self._coro_srv = CoroHttpServer(
+            addr,
+            handler=self._handler,
+        )
+        self._srv_coro = self._coro_srv.coro_handle()
+        self._cur_io = self._next_io(None)
+
+        self._line_buf = DelimitingBuffer(keep_ends=True)
+
+        io.register(self._on_read, r=[sock.fileno()])
+
+    def _next_io(self, d: bytes | None) -> CoroHttpServer.Io | None:  # noqa
+        o: CoroHttpServer.Io | None
+        while True:
+            try:
+                if d is not None:
+                    o = self._srv_coro.send(d)
+                    d = None
+                else:
+                    o = next(self._srv_coro)
+            except StopIteration:
+                o = None
+                break
+
+            if isinstance(o, CoroHttpServer.AnyLogIo):
+                print(o)
+                continue
+
+            break
+
+        self._cur_io = o
+        return o
+
+    def _on_read(self) -> None:
+        buf = self._sock.recv(1024)
+
+        for out in self._line_buf.feed(buf):
+            print(out)
+
+        if not buf:
+            self._io.unregister(r=[self._sock.fileno()])
+            self._sock.close()
+            return
+
+
+
+def say_hi_handler(req: HttpHandlerRequest) -> HttpHandlerResponse:
+    resp = '\n'.join([
+        f'method: {req.method}',
+        f'path: {req.path}',
+        f'data: {len(req.data or b"")}',
+        '',
+    ])
+
+    return HttpHandlerResponse(
+        200,
+        data=resp.encode('utf-8'),
+    )
 
 
 def _main() -> None:
+    dr = DelimitingReader()
+    dr.feed(b'abcd\nef')
+    dr.feed(b'ghi\njkl')
+    print(dr.read(5))
+
+
     iom = IoManager()
 
     srv_addr = ('localhost', 9999)
-    srv = EchoServer(srv_addr, iom)
+    HttpServer(
+        srv_addr,
+        say_hi_handler,
+        iom,
+    )
 
     while True:
         iom.poll()
