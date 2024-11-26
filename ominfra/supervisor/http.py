92f567c9479c9904c0f4c794158927f8ca0e8379
wrmsr <timwilloney@gmail.com>
2024-11-25 18:14:30 -0800

    --

--- ominfra/supervisor/http.py
+++ ominfra/supervisor/http.py
@@ -1,3 +1,4 @@
+# ruff: noqa: U006 UP007
 import json
 import socket
 import typing as ta
@@ -5,12 +6,14 @@ import typing as ta
 from omlish.lite.check import check_not_none
 from omlish.lite.fdio.corohttp import CoroHttpServerConnectionFdIoHandler
 from omlish.lite.fdio.handlers import SocketFdIoHandler
+from omlish.lite.http.handlers import HttpHandler
 from omlish.lite.http.handlers import HttpHandlerRequest
 from omlish.lite.http.handlers import HttpHandlerResponse
 from omlish.lite.json import JSON_PRETTY_KWARGS
 from omlish.lite.socket import SocketAddress
 
 from .dispatchers import Dispatchers
+from .groups import ProcessGroupManager
 from .types import HasDispatchers
 
 
@@ -46,13 +49,21 @@ class SocketServerFdIoHandler(SocketFdIoHandler):
 
 
 class HttpServer(HasDispatchers):
+    class Address(ta.NamedTuple):
+        a: SocketAddress
+
+    class Handler(ta.NamedTuple):
+        h: HttpHandler
+
     def __init__(
             self,
-            addr: SocketAddress = ('localhost', 8000),
+            handler: Handler,
+            addr: Address = Address(('localhost', 8000)),
     ) -> None:
         super().__init__()
 
-        self._addr = addr
+        self._handler = handler.h
+        self._addr = addr.a
 
         self._server = SocketServerFdIoHandler(self._addr, self._on_connect)
 
@@ -73,16 +84,41 @@ class HttpServer(HasDispatchers):
         conn = CoroHttpServerConnectionFdIoHandler(
             addr,
             sock,
-            self._http_handler,
+            self._handler,
         )
 
         self._conns.append(conn)
 
-    def _http_handler(self, req: HttpHandlerRequest) -> HttpHandlerResponse:
+
+##
+
+
+class SupervisorHttpHandler:
+    def __init__(
+            self,
+            *,
+            groups: ProcessGroupManager,
+    ) -> None:
+        super().__init__()
+
+        self._groups = groups
+
+    def handle(self, req: HttpHandlerRequest) -> HttpHandlerResponse:
         dct = {
             'method': req.method,
             'path': req.path,
             'data': len(req.data or b''),
+            'groups': {
+                g.name: {
+                    'processes': {
+                        p.name: {
+                            'pid': p.pid,
+                        }
+                        for p in g
+                    },
+                }
+                for g in self._groups
+            },
         }
 
         return HttpHandlerResponse(
@@ -90,5 +126,5 @@ class HttpServer(HasDispatchers):
             data=json.dumps(dct, **JSON_PRETTY_KWARGS).encode('utf-8') + b'\n',
             headers={
                 'Content-Type': 'application/json',
-            }
+            },
         )
