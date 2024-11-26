ee7428c23873e34967418c4e386cc127b0a00358
wrmsr <timwilloney@gmail.com>
2024-11-26 12:04:51 -0800

    --

--- ominfra/scripts/supervisor.py
+++ ominfra/scripts/supervisor.py
@@ -1059,18 +1059,6 @@ def drop_privileges(user: ta.Union[int, str, None]) -> ta.Optional[str]:
 
 
 class ProcessState(enum.IntEnum):
-    """
-    http://supervisord.org/subprocess.html
-
-    STOPPED -> STARTING
-    STARTING -> RUNNING, BACKOFF, STOPPING
-    RUNNING -> STOPPING, EXITED
-    BACKOFF -> STARTING, FATAL
-    STOPPING -> STOPPED
-    EXITED -> STARTING
-    FATAL -> STARTING
-    """
-
     STOPPED = 0
     STARTING = 10
     RUNNING = 20
@@ -1092,6 +1080,17 @@ class ProcessState(enum.IntEnum):
     def signalable(self) -> bool:
         return self in SIGNALABLE_STATES
 
+
+# http://supervisord.org/subprocess.html
+STATE_TRANSITIONS = {
+    ProcessState.STOPPED: (ProcessState.STARTING,),
+    ProcessState.STARTING: (ProcessState.RUNNING, ProcessState.BACKOFF, ProcessState.STOPPING),
+    ProcessState.RUNNING: (ProcessState.STOPPING, ProcessState.EXITED),
+    ProcessState.BACKOFF: (ProcessState.STARTING, ProcessState.FATAL),
+    ProcessState.STOPPING: (ProcessState.STOPPED,),
+    ProcessState.EXITED: (ProcessState.STARTING,),
+    ProcessState.FATAL: (ProcessState.STARTING,),
+}
 
 STOPPED_STATES = (
     ProcessState.STOPPED,
