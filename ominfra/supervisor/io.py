25adee63224a6889a4bb33f234347ac05155de40
wrmsr <timwilloney@gmail.com>
2024-11-25 16:16:21 -0800

    --

--- ominfra/supervisor/io.py
+++ ominfra/supervisor/io.py
@@ -45,7 +45,9 @@ class IoManager(HasDispatchers):
                 self._poller.register_writable(fd)
 
         timeout = 1  # this cannot be fewer than the smallest TickEvent (5)
+        log.info(f'Polling: {timeout=}')
         polled = self._poller.poll(timeout)
+        log.info(f'Polled: {polled=}')
         if polled.msg is not None:
             log.error(polled.msg)
         if polled.exc is not None:
