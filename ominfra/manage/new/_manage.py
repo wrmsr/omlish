4f6b354e6594df3fd4fa55e6e07ee7ff5b39dded
wrmsr <timwilloney@gmail.com>
2024-11-30 16:29:04 -0600

    2024-11-30T22:29:04Z

---
+++ ominfra/manage/new/_manage.py
@@ -0,0 +1,598 @@
+#!/usr/bin/env python3
+# noinspection DuplicatedCode
+# @omlish-lite
+# @omlish-script
+# @omlish-amalg-output main.py
+# ruff: noqa: N802 UP006 UP007 UP036
+import abc
+import contextlib
+import dataclasses as dc
+import datetime
+import functools
+import inspect
+import json
+import logging
+import os
+import shlex
+import subprocess
+import sys
+import threading
+import time
+import typing as ta
+
+
+########################################
+
+
+if sys.version_info < (3, 8):
+    raise OSError(f'Requires python (3, 8), got {sys.version_info} from {sys.executable}')  # noqa
+
+
+########################################
+
+
+# ../../../omlish/lite/cached.py
+T = ta.TypeVar('T')
+
+# main.py
+CommandInputT = ta.TypeVar('CommandInputT', bound='Command.Input')
+CommandOutputT = ta.TypeVar('CommandOutputT', bound='Command.Output')
+
+
+########################################
+# ../../../../omlish/lite/cached.py
+
+
+class _cached_nullary:  # noqa
+    def __init__(self, fn):
+        super().__init__()
+        self._fn = fn
+        self._value = self._missing = object()
+        functools.update_wrapper(self, fn)
+
+    def __call__(self, *args, **kwargs):  # noqa
+        if self._value is self._missing:
+            self._value = self._fn()
+        return self._value
+
+    def __get__(self, instance, owner):  # noqa
+        bound = instance.__dict__[self._fn.__name__] = self.__class__(self._fn.__get__(instance, owner))
+        return bound
+
+
+def cached_nullary(fn):  # ta.Callable[..., T]) -> ta.Callable[..., T]:
+    return _cached_nullary(fn)
+
+
+########################################
+# ../../../../omlish/lite/json.py
+
+
+##
+
+
+JSON_PRETTY_INDENT = 2
+
+JSON_PRETTY_KWARGS: ta.Mapping[str, ta.Any] = dict(
+    indent=JSON_PRETTY_INDENT,
+)
+
+json_dump_pretty: ta.Callable[..., bytes] = functools.partial(json.dump, **JSON_PRETTY_KWARGS)  # type: ignore
+json_dumps_pretty: ta.Callable[..., str] = functools.partial(json.dumps, **JSON_PRETTY_KWARGS)
+
+
+##
+
+
+JSON_COMPACT_SEPARATORS = (',', ':')
+
+JSON_COMPACT_KWARGS: ta.Mapping[str, ta.Any] = dict(
+    indent=None,
+    separators=JSON_COMPACT_SEPARATORS,
+)
+
+json_dump_compact: ta.Callable[..., bytes] = functools.partial(json.dump, **JSON_COMPACT_KWARGS)  # type: ignore
+json_dumps_compact: ta.Callable[..., str] = functools.partial(json.dumps, **JSON_COMPACT_KWARGS)
+
+
+########################################
+# ../../../../omlish/lite/logs.py
+"""
+TODO:
+ - translate json keys
+ - debug
+"""
+
+
+log = logging.getLogger(__name__)
+
+
+##
+
+
+class TidLogFilter(logging.Filter):
+
+    def filter(self, record):
+        record.tid = threading.get_native_id()
+        return True
+
+
+##
+
+
+class JsonLogFormatter(logging.Formatter):
+
+    KEYS: ta.Mapping[str, bool] = {
+        'name': False,
+        'msg': False,
+        'args': False,
+        'levelname': False,
+        'levelno': False,
+        'pathname': False,
+        'filename': False,
+        'module': False,
+        'exc_info': True,
+        'exc_text': True,
+        'stack_info': True,
+        'lineno': False,
+        'funcName': False,
+        'created': False,
+        'msecs': False,
+        'relativeCreated': False,
+        'thread': False,
+        'threadName': False,
+        'processName': False,
+        'process': False,
+    }
+
+    def format(self, record: logging.LogRecord) -> str:
+        dct = {
+            k: v
+            for k, o in self.KEYS.items()
+            for v in [getattr(record, k)]
+            if not (o and v is None)
+        }
+        return json_dumps_compact(dct)
+
+
+##
+
+
+STANDARD_LOG_FORMAT_PARTS = [
+    ('asctime', '%(asctime)-15s'),
+    ('process', 'pid=%(process)-6s'),
+    ('thread', 'tid=%(thread)x'),
+    ('levelname', '%(levelname)s'),
+    ('name', '%(name)s'),
+    ('separator', '::'),
+    ('message', '%(message)s'),
+]
+
+
+class StandardLogFormatter(logging.Formatter):
+
+    @staticmethod
+    def build_log_format(parts: ta.Iterable[ta.Tuple[str, str]]) -> str:
+        return ' '.join(v for k, v in parts)
+
+    converter = datetime.datetime.fromtimestamp  # type: ignore
+
+    def formatTime(self, record, datefmt=None):
+        ct = self.converter(record.created)  # type: ignore
+        if datefmt:
+            return ct.strftime(datefmt)  # noqa
+        else:
+            t = ct.strftime('%Y-%m-%d %H:%M:%S')
+            return '%s.%03d' % (t, record.msecs)  # noqa
+
+
+##
+
+
+class ProxyLogFilterer(logging.Filterer):
+    def __init__(self, underlying: logging.Filterer) -> None:  # noqa
+        self._underlying = underlying
+
+    @property
+    def underlying(self) -> logging.Filterer:
+        return self._underlying
+
+    @property
+    def filters(self):
+        return self._underlying.filters
+
+    @filters.setter
+    def filters(self, filters):
+        self._underlying.filters = filters
+
+    def addFilter(self, filter):  # noqa
+        self._underlying.addFilter(filter)
+
+    def removeFilter(self, filter):  # noqa
+        self._underlying.removeFilter(filter)
+
+    def filter(self, record):
+        return self._underlying.filter(record)
+
+
+class ProxyLogHandler(ProxyLogFilterer, logging.Handler):
+    def __init__(self, underlying: logging.Handler) -> None:  # noqa
+        ProxyLogFilterer.__init__(self, underlying)
+
+    _underlying: logging.Handler
+
+    @property
+    def underlying(self) -> logging.Handler:
+        return self._underlying
+
+    def get_name(self):
+        return self._underlying.get_name()
+
+    def set_name(self, name):
+        self._underlying.set_name(name)
+
+    @property
+    def name(self):
+        return self._underlying.name
+
+    @property
+    def level(self):
+        return self._underlying.level
+
+    @level.setter
+    def level(self, level):
+        self._underlying.level = level
+
+    @property
+    def formatter(self):
+        return self._underlying.formatter
+
+    @formatter.setter
+    def formatter(self, formatter):
+        self._underlying.formatter = formatter
+
+    def createLock(self):
+        self._underlying.createLock()
+
+    def acquire(self):
+        self._underlying.acquire()
+
+    def release(self):
+        self._underlying.release()
+
+    def setLevel(self, level):
+        self._underlying.setLevel(level)
+
+    def format(self, record):
+        return self._underlying.format(record)
+
+    def emit(self, record):
+        self._underlying.emit(record)
+
+    def handle(self, record):
+        return self._underlying.handle(record)
+
+    def setFormatter(self, fmt):
+        self._underlying.setFormatter(fmt)
+
+    def flush(self):
+        self._underlying.flush()
+
+    def close(self):
+        self._underlying.close()
+
+    def handleError(self, record):
+        self._underlying.handleError(record)
+
+
+##
+
+
+class StandardLogHandler(ProxyLogHandler):
+    pass
+
+
+##
+
+
+@contextlib.contextmanager
+def _locking_logging_module_lock() -> ta.Iterator[None]:
+    if hasattr(logging, '_acquireLock'):
+        logging._acquireLock()  # noqa
+        try:
+            yield
+        finally:
+            logging._releaseLock()  # type: ignore  # noqa
+
+    elif hasattr(logging, '_lock'):
+        # https://github.com/python/cpython/commit/74723e11109a320e628898817ab449b3dad9ee96
+        with logging._lock:  # noqa
+            yield
+
+    else:
+        raise Exception("Can't find lock in logging module")
+
+
+def configure_standard_logging(
+        level: ta.Union[int, str] = logging.INFO,
+        *,
+        json: bool = False,
+        target: ta.Optional[logging.Logger] = None,
+        force: bool = False,
+        handler_factory: ta.Optional[ta.Callable[[], logging.Handler]] = None,
+) -> ta.Optional[StandardLogHandler]:
+    with _locking_logging_module_lock():
+        if target is None:
+            target = logging.root
+
+        #
+
+        if not force:
+            if any(isinstance(h, StandardLogHandler) for h in list(target.handlers)):
+                return None
+
+        #
+
+        if handler_factory is not None:
+            handler = handler_factory()
+        else:
+            handler = logging.StreamHandler()
+
+        #
+
+        formatter: logging.Formatter
+        if json:
+            formatter = JsonLogFormatter()
+        else:
+            formatter = StandardLogFormatter(StandardLogFormatter.build_log_format(STANDARD_LOG_FORMAT_PARTS))
+        handler.setFormatter(formatter)
+
+        #
+
+        handler.addFilter(TidLogFilter())
+
+        #
+
+        target.addHandler(handler)
+
+        #
+
+        if level is not None:
+            target.setLevel(level)
+
+        #
+
+        return StandardLogHandler(handler)
+
+
+########################################
+# ../../../../omlish/lite/runtime.py
+
+
+@cached_nullary
+def is_debugger_attached() -> bool:
+    return any(frame[1].endswith('pydevd.py') for frame in inspect.stack())
+
+
+REQUIRED_PYTHON_VERSION = (3, 8)
+
+
+def check_runtime_version() -> None:
+    if sys.version_info < REQUIRED_PYTHON_VERSION:
+        raise OSError(f'Requires python {REQUIRED_PYTHON_VERSION}, got {sys.version_info} from {sys.executable}')  # noqa
+
+
+########################################
+# ../../../../omlish/lite/subprocesses.py
+
+
+##
+
+
+_SUBPROCESS_SHELL_WRAP_EXECS = False
+
+
+def subprocess_shell_wrap_exec(*args: str) -> ta.Tuple[str, ...]:
+    return ('sh', '-c', ' '.join(map(shlex.quote, args)))
+
+
+def subprocess_maybe_shell_wrap_exec(*args: str) -> ta.Tuple[str, ...]:
+    if _SUBPROCESS_SHELL_WRAP_EXECS or is_debugger_attached():
+        return subprocess_shell_wrap_exec(*args)
+    else:
+        return args
+
+
+def _prepare_subprocess_invocation(
+        *args: str,
+        env: ta.Optional[ta.Mapping[str, ta.Any]] = None,
+        extra_env: ta.Optional[ta.Mapping[str, ta.Any]] = None,
+        quiet: bool = False,
+        shell: bool = False,
+        **kwargs: ta.Any,
+) -> ta.Tuple[ta.Tuple[ta.Any, ...], ta.Dict[str, ta.Any]]:
+    log.debug(args)
+    if extra_env:
+        log.debug(extra_env)
+
+    if extra_env:
+        env = {**(env if env is not None else os.environ), **extra_env}
+
+    if quiet and 'stderr' not in kwargs:
+        if not log.isEnabledFor(logging.DEBUG):
+            kwargs['stderr'] = subprocess.DEVNULL
+
+    if not shell:
+        args = subprocess_maybe_shell_wrap_exec(*args)
+
+    return args, dict(
+        env=env,
+        shell=shell,
+        **kwargs,
+    )
+
+
+def subprocess_check_call(*args: str, stdout=sys.stderr, **kwargs: ta.Any) -> None:
+    args, kwargs = _prepare_subprocess_invocation(*args, stdout=stdout, **kwargs)
+    return subprocess.check_call(args, **kwargs)  # type: ignore
+
+
+def subprocess_check_output(*args: str, **kwargs: ta.Any) -> bytes:
+    args, kwargs = _prepare_subprocess_invocation(*args, **kwargs)
+    return subprocess.check_output(args, **kwargs)
+
+
+def subprocess_check_output_str(*args: str, **kwargs: ta.Any) -> str:
+    return subprocess_check_output(*args, **kwargs).decode().strip()
+
+
+##
+
+
+DEFAULT_SUBPROCESS_TRY_EXCEPTIONS: ta.Tuple[ta.Type[Exception], ...] = (
+    FileNotFoundError,
+    subprocess.CalledProcessError,
+)
+
+
+def subprocess_try_call(
+        *args: str,
+        try_exceptions: ta.Tuple[ta.Type[Exception], ...] = DEFAULT_SUBPROCESS_TRY_EXCEPTIONS,
+        **kwargs: ta.Any,
+) -> bool:
+    try:
+        subprocess_check_call(*args, **kwargs)
+    except try_exceptions as e:  # noqa
+        if log.isEnabledFor(logging.DEBUG):
+            log.exception('command failed')
+        return False
+    else:
+        return True
+
+
+def subprocess_try_output(
+        *args: str,
+        try_exceptions: ta.Tuple[ta.Type[Exception], ...] = DEFAULT_SUBPROCESS_TRY_EXCEPTIONS,
+        **kwargs: ta.Any,
+) -> ta.Optional[bytes]:
+    try:
+        return subprocess_check_output(*args, **kwargs)
+    except try_exceptions as e:  # noqa
+        if log.isEnabledFor(logging.DEBUG):
+            log.exception('command failed')
+        return None
+
+
+def subprocess_try_output_str(*args: str, **kwargs: ta.Any) -> ta.Optional[str]:
+    out = subprocess_try_output(*args, **kwargs)
+    return out.decode().strip() if out is not None else None
+
+
+##
+
+
+def subprocess_close(
+        proc: subprocess.Popen,
+        timeout: ta.Optional[float] = None,
+) -> None:
+    # TODO: terminate, sleep, kill
+    if proc.stdout:
+        proc.stdout.close()
+    if proc.stderr:
+        proc.stderr.close()
+    if proc.stdin:
+        proc.stdin.close()
+
+    proc.wait(timeout)
+
+
+########################################
+# main.py
+
+
+class Command(abc.ABC, ta.Generic[CommandInputT, CommandOutputT]):
+    @dc.dataclass(frozen=True)
+    class Input(abc.ABC):  # noqa
+        pass
+
+    @dc.dataclass(frozen=True)
+    class Output(abc.ABC):  # noqa
+        pass
+
+    @abc.abstractmethod
+    def _execute(self, inp: CommandInputT) -> CommandOutputT:
+        raise NotImplementedError
+
+
+class SubprocessCommand(Command['SubprocessCommand.Input', 'SubprocessCommand.Output']):
+    @dc.dataclass(frozen=True)
+    class Input(Command.Input):
+        args: ta.Sequence[str]
+
+        shell: bool = False
+        cwd: ta.Optional[str] = None
+        env: ta.Optional[ta.Mapping[str, str]] = None
+
+        capture_stdout: bool = False
+        capture_stderr: bool = False
+
+        input: ta.Optional[bytes] = None
+        timeout: ta.Optional[float] = None
+
+        def __post_init__(self) -> None:
+            if isinstance(self.args, str):
+                raise TypeError(self.args)
+
+    @dc.dataclass(frozen=True)
+    class Output(Command.Output):
+        rc: int
+        pid: int
+
+        elapsed_s: float
+
+        stdout: ta.Optional[bytes] = None
+        stderr: ta.Optional[bytes] = None
+
+    def _execute(self, inp: Input) -> Output:
+        proc = subprocess.Popen(
+            subprocess_maybe_shell_wrap_exec(*inp.args),
+
+            shell=inp.shell,
+            cwd=inp.cwd,
+            env={**os.environ, **(inp.env or {})},
+
+            stdin=subprocess.PIPE if inp.input is not None else None,
+            stdout=subprocess.PIPE if inp.capture_stdout else None,
+            stderr=subprocess.PIPE if inp.capture_stderr else None,
+        )
+
+        start_time = time.time()
+        stdout, stderr = proc.communicate(
+            input=inp.input,
+            timeout=inp.timeout,
+        )
+        end_time = time.time()
+
+        return SubprocessCommand.Output(
+            rc=proc.returncode,
+            pid=proc.pid,
+
+            elapsed_s=end_time - start_time,
+
+            stdout=stdout,  # noqa
+            stderr=stderr,  # noqa
+        )
+
+
+def _main() -> None:
+    i = SubprocessCommand.Input(
+        args=['python3', '-'],
+        input=b'print(1)\n',
+        capture_stdout=True,
+    )
+    o = SubprocessCommand()._execute(i)  # noqa
+    print(o)
+
+
+if __name__ == '__main__':
+    _main()
--- ominfra/manage/new/main.py
+++ ominfra/manage/new/main.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+# @omlish-amalg ./_manage.py
+# ruff: noqa: UP006 UP007
 import abc
 import dataclasses as dc
+import os
 import subprocess
+import time
 import typing as ta
 
 from omlish.lite.subprocesses import subprocess_maybe_shell_wrap_exec
@@ -29,11 +34,15 @@ class SubprocessCommand(Command['SubprocessCommand.Input', 'SubprocessCommand.Ou
     class Input(Command.Input):
         args: ta.Sequence[str]
 
-        input: ta.Optional[bytes] = None
-        timeout: ta.Optional[float] = None
+        shell: bool = False
+        cwd: ta.Optional[str] = None
+        env: ta.Optional[ta.Mapping[str, str]] = None
 
         capture_stdout: bool = False
         capture_stderr: bool = False
+
+        input: ta.Optional[bytes] = None
+        timeout: ta.Optional[float] = None
 
         def __post_init__(self) -> None:
             if isinstance(self.args, str):
@@ -44,25 +53,36 @@ class SubprocessCommand(Command['SubprocessCommand.Input', 'SubprocessCommand.Ou
         rc: int
         pid: int
 
+        elapsed_s: float
+
         stdout: ta.Optional[bytes] = None
         stderr: ta.Optional[bytes] = None
 
     def _execute(self, inp: Input) -> Output:
         proc = subprocess.Popen(
-            inp.args,
+            subprocess_maybe_shell_wrap_exec(*inp.args),
 
-            **(dict(stdout=subprocess.PIPE) if inp.capture_stdout else {}),
-            **(dict(stderr=subprocess.PIPE) if inp.capture_stderr else {}),
+            shell=inp.shell,
+            cwd=inp.cwd,
+            env={**os.environ, **(inp.env or {})},
+
+            stdin=subprocess.PIPE if inp.input is not None else None,
+            stdout=subprocess.PIPE if inp.capture_stdout else None,
+            stderr=subprocess.PIPE if inp.capture_stderr else None,
         )
 
+        start_time = time.time()
         stdout, stderr = proc.communicate(
             input=inp.input,
             timeout=inp.timeout,
         )
+        end_time = time.time()
 
         return SubprocessCommand.Output(
             rc=proc.returncode,
             pid=proc.pid,
+
+            elapsed_s=end_time - start_time,
 
             stdout=stdout,  # noqa
             stderr=stderr,  # noqa
@@ -71,8 +91,8 @@ class SubprocessCommand(Command['SubprocessCommand.Input', 'SubprocessCommand.Ou
 
 def _main() -> None:
     i = SubprocessCommand.Input(
-        args=['echo'],
-        input=b'hi',
+        args=['python3', '-'],
+        input=b'print(1)\n',
         capture_stdout=True,
     )
     o = SubprocessCommand()._execute(i)  # noqa
