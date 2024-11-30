1700248b997607dabc09b398c7f8b56caa8246a7
wrmsr <timwilloney@gmail.com>
2024-11-30 12:00:38 -0600

    2024-11-30T18:00:38Z

--- ominfra/tests/_runcommands.py
+++ ominfra/tests/_runcommands.py
@@ -276,6 +276,7 @@ _PYREMOTE_BOOTSTRAP_ARGV0_VAR = '_OPYR_ARGV0'
 _PYREMOTE_BOOTSTRAP_ACK0 = b'OPYR000\n'
 _PYREMOTE_BOOTSTRAP_ACK1 = b'OPYR001\n'
 _PYREMOTE_BOOTSTRAP_ACK2 = b'OPYR001\n'
+_PYREMOTE_BOOTSTRAP_ACK3 = b'OPYR001\n'
 
 _PYREMOTE_BOOTSTRAP_PROC_TITLE_FMT = '(pyremote:%s)'
 
@@ -390,6 +391,20 @@ def pyremote_build_bootstrap_cmd(context_name: str) -> str:
 ##
 
 
+@dc.dataclass(frozen=True)
+class PyremoteEnvInfo:
+    platform: str
+
+
+def _get_pyremote_env_info() -> PyremoteEnvInfo:
+    return PyremoteEnvInfo(
+        platform=sys.platform,
+    )
+
+
+##
+
+
 class PyremoteBootstrapDriver:
     def __init__(self, main_src: str) -> None:
         super().__init__()
@@ -397,22 +412,26 @@ class PyremoteBootstrapDriver:
         self._main_src = main_src
         self._main_z = zlib.compress(main_src.encode('utf-8'))
 
-    class Read(ta.NamedTuple):
+    @dc.dataclass(frozen=True)
+    class Read:
         sz: int
 
-    class Write(ta.NamedTuple):
+    @dc.dataclass(frozen=True)
+    class Write:
         d: bytes
 
     class ProtocolError(Exception):
         pass
 
-    class Result(ta.NamedTuple):
+    @dc.dataclass(frozen=True)
+    class Result:
         pid: int
+        env_info: PyremoteEnvInfo
 
     def __call__(self) -> ta.Generator[ta.Union[Read, Write], ta.Optional[bytes], Result]:
         yield from self._expect(_PYREMOTE_BOOTSTRAP_ACK0)
 
-        d = check_isinstance((yield self.Read(8)), bytes)
+        d = yield from self._read(8)
         pid = struct.unpack('<Q', d)[0]
 
         check_none((yield self.Write(struct.pack('<I', len(self._main_z)))))
@@ -421,18 +440,33 @@ class PyremoteBootstrapDriver:
         yield from self._expect(_PYREMOTE_BOOTSTRAP_ACK1)
         yield from self._expect(_PYREMOTE_BOOTSTRAP_ACK2)
 
-        return self.Result(pid)
+        d = yield from self._read(4)
+        env_info_json_len = struct.unpack('<I', d)[0]
+        d = yield from self._read(env_info_json_len)
+        env_info_json = d.decode('utf-8')
+        env_info = PyremoteEnvInfo(**json.loads(env_info_json))
+
+        yield from self._expect(_PYREMOTE_BOOTSTRAP_ACK3)
+
+        return self.Result(
+            pid=pid,
+            env_info=env_info,
+        )
 
     def _expect(self, e: bytes) -> ta.Generator[Read, bytes, None]:
         d = check_isinstance((yield self.Read(len(e))), bytes)
         if d != e:
             raise self.ProtocolError
 
+    def _read(self, sz: int) -> ta.Generator[Read, bytes, bytes]:
+        return check_isinstance((yield self.Read(sz)), bytes)
+
 
 ##
 
 
-class PyremoteBootstrapPayloadRuntime(ta.NamedTuple):
+@dc.dataclass(frozen=True)
+class PyremoteBootstrapPayloadRuntime:
     input: ta.BinaryIO
     main_src: str
 
@@ -452,6 +486,14 @@ def pyremote_bootstrap_finalize() -> PyremoteBootstrapPayloadRuntime:
 
     # Write third ack
     os.write(1, _PYREMOTE_BOOTSTRAP_ACK2)
+
+    env_info = _get_pyremote_env_info()
+    env_info_json = json.dumps(dc.asdict(env_info), indent=None, separators=(',', ':'))
+    os.write(1, struct.pack('<I', len(env_info_json)))
+    os.write(1, env_info_json.encode('utf-8'))
+
+    # Write fourth ack
+    os.write(1, _PYREMOTE_BOOTSTRAP_ACK3)
 
     return PyremoteBootstrapPayloadRuntime(
         input=os.fdopen(_PYREMOTE_BOOTSTRAP_COMM_FD, 'rb', 0),
