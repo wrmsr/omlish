ab32d97491dd0bc8e8ebeac6c68fb7fd39ff0df5
wrmsr <timwilloney@gmail.com>
2024-12-03 14:37:41 -0800

    2024-12-03T22:37:41Z

---
+++ omlish/io/generators.py
@@ -0,0 +1,46 @@
+import typing as ta
+
+
+class PrependableBytesGeneratorReader:
+    def __init__(self) -> None:
+        super().__init__()
+        self._p: list[bytes] = []
+
+    class ReadError(Exception):
+        pass
+
+    def read(self, sz: int | None) -> ta.Generator[int | None, bytes, bytes]:
+        if not self._p:
+            d = yield sz
+            return d
+
+        if sz is None:
+            return self._p.pop(0)
+
+        l: list[bytes] = []
+        r = sz
+        while r > 0 and self._p:
+            c = self._p[0]
+
+            if len(c) > r:
+                l.append(c[:r])
+                self._p[0] = c[r:]
+                return b''.join(l)
+
+            l.append(c)
+            r -= len(c)
+            self._p.pop(0)
+
+        if r:
+            c = yield r
+            if not c:
+                return b''
+            if len(c) != r:
+                raise self.ReadError(f'Reader got {len(c)} bytes, expected {r}')
+            l.append(c)
+
+        return b''.join(l)
+
+    def prepend(self, d: bytes) -> None:
+        if d:
+            self._p.append(d)
