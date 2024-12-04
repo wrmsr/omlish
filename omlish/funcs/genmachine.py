0cc2060a933f0fba69c024cccd200edffddae5a8
wrmsr <timwilloney@gmail.com>
2024-12-04 11:16:11 -0800

    2024-12-04T19:16:11Z

--- omlish/funcs/genmachine.py
+++ omlish/funcs/genmachine.py
@@ -67,8 +67,7 @@ class GenMachine(ta.Generic[I, O]):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type is None:
-            self.close()
+        self.close()
 
     #
 
