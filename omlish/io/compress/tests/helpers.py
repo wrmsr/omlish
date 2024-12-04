f74df70da736661abbd83d88f460ffa54ad5423a
wrmsr <timwilloney@gmail.com>
2024-12-03 16:02:52 -0800

    2024-12-04T00:02:52Z

--- omlish/io/compress/tests/helpers.py
+++ omlish/io/compress/tests/helpers.py
@@ -3,57 +3,50 @@ import typing as ta
 from ..types import IncrementalCompressor
 
 
+def _yield_read_chunks(
+        f: ta.IO,
+        read_size: int = 4096,
+) -> ta.Iterator[bytes]:
+    while True:
+        d = f.read(read_size)
+        yield d
+        if len(d) != read_size:
+            break
+    if d:
+        yield b''
+
+
 def feed_inc_compressor(
         g: IncrementalCompressor,
         f: ta.IO,
         *,
         read_size: int = 4096,
 ) -> ta.Iterator[bytes]:
+    chunks = _yield_read_chunks(f, read_size)
+
     i = None
     while True:
         o = g.send(i)
         i = None
 
         if o is None:
-            i = f.read(read_size)
-            if len(i) != read_size:
+            if not (i := next(chunks, None)):
                 break
         elif not o:
-            raise TypeError(o)
+            raise EOFError
         else:
             yield o
 
-    o = g.send(i)
     while True:
+        o = g.send(i)
+        i = None
+
         if o is None:
-            break
+            raise EOFError
         elif not o:
-            raise TypeError(o)
+            break
         else:
             yield o
 
-        try:
-            o = g.send(None)
-        except StopIteration:
-            if i:
-                raise RuntimeError  # noqa
-
-    if i:
-        try:
-            o = g.send(b'')
-        except StopIteration:
-            pass
-
-        else:
-            while True:
-                if o is None:
-                    raise TypeError(o)
-                elif not o:
-                    break
-                else:
-                    yield o
-
-                try:
-                    o = g.send(None)
-                except StopIteration:
-                    break
+    if o != b'':
+        raise EOFError
