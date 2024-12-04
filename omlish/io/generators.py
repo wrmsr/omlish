1aba857023241aabc5878cef87e0615d32f507e0
wrmsr <timwilloney@gmail.com>
2024-12-04 14:07:12 -0800

    2024-12-04T22:07:12Z

--- omlish/io/generators.py
+++ omlish/io/generators.py
@@ -8,10 +8,36 @@ import typing as ta
 from .. import check
 
 
+T = ta.TypeVar('T')
 AnyT = ta.TypeVar('AnyT', bound=ta.Any)
 
 
-class PrependableGeneratorReader(ta.Generic[AnyT]):
+##
+
+
+class _BytesJoiner:
+    def _join(self, lst: list[bytes]) -> bytes:
+        return b''.join(lst)
+
+
+class _StrJoiner:
+    def _join(self, lst: list[str]) -> str:
+        return ''.join(lst)
+
+
+##
+
+
+class GeneratorReader(abc.ABC, ta.Generic[T]):
+    @abc.abstractmethod
+    def read(self, sz: int | None) -> ta.Generator[int | None, T, T]:
+        raise NotImplementedError
+
+
+##
+
+
+class PrependableGeneratorReader(GeneratorReader[AnyT]):
     def __init__(self) -> None:
         super().__init__()
 
@@ -29,43 +55,71 @@ class PrependableGeneratorReader(ta.Generic[AnyT]):
         if sz is None:
             return self._lst.pop(0)
 
-        l: list[AnyT] = []
-        r = sz
-        while r > 0 and self._lst:
+        lst: list[AnyT] = []
+        rem = sz
+        while rem > 0 and self._lst:
             c = self._lst[0]
 
-            if len(c) > r:
-                l.append(c[:r])
-                self._lst[0] = c[r:]
-                return self._join(l)
+            if len(c) > rem:
+                lst.append(c[:rem])
+                self._lst[0] = c[rem:]
+                return self._join(lst)
 
-            l.append(c)
-            r -= len(c)
+            lst.append(c)
+            rem -= len(c)
             self._lst.pop(0)
 
-        if r:
-            d = check.not_none((yield r))
+        if rem:
+            d = check.not_none((yield rem))
             if not d:
                 return self._join([])  # type: ignore[unreachable]
-            if len(d) != r:  # type: ignore[unreachable]
-                raise EOFError(f'Reader got {len(d)}, expected {r}')
-            l.append(d)
+            if len(d) != rem:  # type: ignore[unreachable]
+                raise EOFError(f'Reader got {len(d)}, expected {rem}')
+            lst.append(d)
 
-        if len(l) == 1:
-            return l[0]
+        if len(lst) == 1:
+            return lst[0]
         else:
-            return self._join(l)
+            return self._join(lst)
 
     def prepend(self, d: AnyT) -> None:
         if d:
             self._lst.append(d)
 
 
-class PrependableBytesGeneratorReader(PrependableGeneratorReader[bytes]):
-    def _join(self, lst: list[bytes]) -> bytes:
-        return b''.join(lst)
+class PrependableBytesGeneratorReader(
+    _BytesJoiner,
+    PrependableGeneratorReader[bytes],
+):
+    pass
 
 
-class PrependableStrGeneratorReader(PrependableGeneratorReader[str]):
-    def _join(self, lst: list[str]) -> str:
-        return ''.join(lst)
+class PrependableStrGeneratorReader(
+    _StrJoiner,
+    PrependableGeneratorReader[str],
+):
+    pass
+
+
+##
+
+
+class BufferedGeneratorReader(PrependableGeneratorReader[AnyT], abc.ABC):
+    def read(self, sz: int | None) -> ta.Generator[int | None, AnyT, AnyT]:
+        return super().read(sz)
+
+
+class BufferedBytesGeneratorReader(
+    _BytesJoiner,
+    BufferedGeneratorReader[bytes],
+    PrependableGeneratorReader[bytes],
+):
+    pass
+
+
+class BufferedStrGeneratorReader(
+    _StrJoiner,
+    BufferedGeneratorReader[str],
+    PrependableGeneratorReader[str],
+):
+    pass
