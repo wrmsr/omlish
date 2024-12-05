cbdb40c76e0188650d7bb456c89302aef20a50b8
wrmsr <timwilloney@gmail.com>
2024-12-04 16:34:14 -0800

    2024-12-05T00:34:14Z

--- ominfra/manage/spawning.py
+++ ominfra/manage/spawning.py
@@ -29,11 +29,30 @@ class PySpawner:
         self._python = python
         self._stderr = stderr
 
-    @dc.dataclass(frozen=True)
-    class Spawned:
-        stdin: ta.IO
-        stdout: ta.IO
-        stderr: ta.Optional[ta.IO]
+    #
+
+    class _PreparedCmd(ta.NamedTuple):
+        cmd: ta.Sequence[str]
+        shell: bool
+
+    def _prepare_cmd(self) -> _PreparedCmd:
+        if self._shell is not None:
+            sh_src = f'{self._python} -c {shlex.quote(self._src)}'
+            if self._shell_quote:
+                sh_src = shlex.quote(sh_src)
+            sh_cmd = f'{self._shell} {sh_src}'
+            return PySpawner._PreparedCmd(
+                cmd=[sh_cmd],
+                shell=True,
+            )
+
+        else:
+            return PySpawner._PreparedCmd(
+                cmd=[self._python, '-c', self._src],
+                shell=False,
+            )
+
+    #
 
     _STDERR_KWARG_MAP: ta.Mapping[str, int] = {
         'pipe': subprocess.PIPE,
@@ -41,29 +60,41 @@ class PySpawner:
         'devnull': subprocess.DEVNULL,
     }
 
+    @dc.dataclass(frozen=True)
+    class Spawned:
+        stdin: ta.IO
+        stdout: ta.IO
+        stderr: ta.Optional[ta.IO]
+
     @contextlib.contextmanager
-    def spawn(self) -> ta.Generator[Spawned, None, None]:
-        if self._shell is not None:
-            sh_src = f'{self._python} -c {shlex.quote(self._src)}'
-            if self._shell_quote:
-                sh_src = shlex.quote(sh_src)
-            sh_cmd = f'{self._shell} {sh_src}'
-            cmd = [sh_cmd]
-            shell = True
-        else:
-            cmd = [self._python, '-c', self._src]
-            shell = False
+    def spawn(
+            self,
+            *,
+            timeout: ta.Optional[float] = None,
+    ) -> ta.Generator[Spawned, None, None]:
+        pc = self._prepare_cmd()
 
         with subprocess.Popen(
-            subprocess_maybe_shell_wrap_exec(*cmd),
-            shell=shell,
+            subprocess_maybe_shell_wrap_exec(*pc.cmd),
+            shell=pc.shell,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=self._STDERR_KWARG_MAP[self._stderr] if self._stderr is not None else None,
         ) as proc:
-            yield PySpawner.Spawned(
-                stdin=check_not_none(proc.stdin),
-                stdout=check_not_none(proc.stdout),
-                stderr=proc.stderr,
-            )
+            stdin = check_not_none(proc.stdin)
+            stdout = check_not_none(proc.stdout)
 
+            try:
+                yield PySpawner.Spawned(
+                    stdin=stdin,
+                    stdout=stdout,
+                    stderr=proc.stderr,
+                )
+
+            finally:
+                try:
+                    stdin.close()
+                except BrokenPipeError:
+                    pass
+
+                proc.wait(timeout)
