c7e7e100e402766763e6b75eea924a68ee5eb108
wrmsr <timwilloney@gmail.com>
2024-12-04 16:20:13 -0800

    2024-12-05T00:20:13Z

--- ominfra/manage/main.py
+++ ominfra/manage/main.py
@@ -5,15 +5,12 @@
 manage.py -s 'docker run -i python:3.12'
 manage.py -qs 'ssh -i foo/bar foo@bar.baz' --python=python3.8
 """
-import inspect
 import json
 import shlex
 import struct
 import subprocess
-import sys
 import typing as ta
 
-from omlish.lite.cached import cached_nullary
 from omlish.lite.cached import static_init
 from omlish.lite.check import check_not_none
 from omlish.lite.json import json_dumps_compact
@@ -31,6 +28,7 @@ from ..pyremote import pyremote_build_bootstrap_cmd
 from .commands.base import Command
 from .commands.subprocess import SubprocessCommand
 from .commands.subprocess import SubprocessCommandExecutor
+from .payload import get_payload_src
 
 
 ##
@@ -110,38 +108,6 @@ def _remote_main() -> None:
 ##
 
 
-@cached_nullary
-def _get_self_src() -> str:
-    return inspect.getsource(sys.modules[__name__])
-
-
-def _is_src_amalg(src: str) -> bool:
-    for l in src.splitlines():  # noqa
-        if l.startswith('# @omlish-amalg-output '):
-            return True
-    return False
-
-
-@cached_nullary
-def _is_self_amalg() -> bool:
-    return _is_src_amalg(_get_self_src())
-
-
-def _get_amalg_src(*, amalg_file: ta.Optional[str]) -> str:
-    if amalg_file is not None:
-        with open(amalg_file) as f:
-            return f.read()
-
-    if _is_self_amalg():
-        return _get_self_src()
-
-    import importlib.resources
-    return importlib.resources.files(__package__.split('.')[0] + '.scripts').joinpath('manage.py').read_text()
-
-
-##
-
-
 def _main() -> None:
     import argparse
 
@@ -150,19 +116,20 @@ def _main() -> None:
     parser.add_argument('-s', '--shell')
     parser.add_argument('-q', '--shell-quote', action='store_true')
     parser.add_argument('--python', default='python3')
-    parser.add_argument('--_amalg-file')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--_payload-file')
 
     args = parser.parse_args()
 
     #
 
-    amalg_src = _get_amalg_src(amalg_file=args._amalg_file)  # noqa
+    payload_src = get_payload_src(file=args._payload_file)  # noqa
 
     #
 
     remote_src = '\n\n'.join([
         '__name__ = "__remote__"',
-        amalg_src,
+        payload_src,
         '_remote_main()',
     ])
 
@@ -181,6 +148,8 @@ def _main() -> None:
         cmd = [args.python, '-c', bs_src]
         shell = False
 
+    #
+
     proc = subprocess.Popen(
         subprocess_maybe_shell_wrap_exec(*cmd),
         shell=shell,
@@ -191,10 +160,12 @@ def _main() -> None:
     stdin = check_not_none(proc.stdin)
     stdout = check_not_none(proc.stdout)
 
+    #
+
     res = PyremoteBootstrapDriver(  # noqa
         remote_src,
         PyremoteBootstrapOptions(
-            # debug=True,
+            debug=args.debug,
         ),
     ).run(stdin, stdout)
     # print(res)
