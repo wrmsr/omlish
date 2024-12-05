0875ff8dafb52db7312aea1ae6b6b15e26636eb4
wrmsr <timwilloney@gmail.com>
2024-12-05 14:36:40 -0800

    2024-12-05T22:36:40Z

--- omlish/io/compress/bz2.py
+++ omlish/io/compress/bz2.py
@@ -4,6 +4,7 @@ import typing as ta
 from ... import lang
 from ..generators import BytesSteppedGenerator
 from ..generators import BytesSteppedReaderGenerator
+from .base import Compression
 from .adapters import CompressorObjectIncrementalAdapter
 from .adapters import DecompressorObjectIncrementalAdapter
 
@@ -14,28 +15,31 @@ else:
     bz2 = lang.proxy_import('bz2')
 
 
-class IncrementalBz2Compressor:
+class Bz2Compression(Compression):
     def __init__(
             self,
             *,
-            compresslevel: int = 9,
+            level: int = 9,
     ) -> None:
         super().__init__()
 
-        self._compresslevel = compresslevel
+        self._level = level
 
-    @lang.autostart
-    def __call__(self) -> BytesSteppedGenerator:
-        return CompressorObjectIncrementalAdapter(
+    def compress(self, d: bytes) -> bytes:
+        return bz2.compress(d, self._level)
+
+    def decompress(self, d: bytes) -> bytes:
+        return bz2.decompress(d, self._level)
+
+    def compress_incremental(self) -> BytesSteppedGenerator[None]:
+        return lang.nextgen(CompressorObjectIncrementalAdapter(
             functools.partial(
                 bz2.BZ2Compressor,  # type: ignore
-                self._compresslevel,
+                self._level,
             ),
-        )()
+        )())
 
-
-class IncrementalBz2Decompressor:
-    def __call__(self) -> BytesSteppedReaderGenerator:
+    def decompress_incremental(self) -> BytesSteppedReaderGenerator[None]:
         return DecompressorObjectIncrementalAdapter(
             bz2.BZ2Decompressor,  # type: ignore
             trailing_error=OSError,
