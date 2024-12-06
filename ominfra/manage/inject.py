9a52312082a1c997e14a6c3ab653f65d49e6e251
wrmsr <timwilloney@gmail.com>
2024-12-06 12:31:53 -0800

    2024-12-06T20:31:53Z

--- ominfra/manage/inject.py
+++ ominfra/manage/inject.py
@@ -1,9 +1,9 @@
 # ruff: noqa: UP006 UP007
 import typing as ta
 
-from omlish.lite.inject import inj
 from omlish.lite.inject import InjectorBindingOrBindings
 from omlish.lite.inject import InjectorBindings
+from omlish.lite.inject import inj
 from omlish.lite.marshal import ObjMarshalerManager
 
 from .commands.inject import bind_commands
