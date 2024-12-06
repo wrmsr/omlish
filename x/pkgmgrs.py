4e164eaf26142e63124ce437bf18d5673c25af80
wrmsr <timwilloney@gmail.com>
2024-12-06 00:25:07 -0800

    2024-12-06T08:25:07Z

--- x/pkgmgrs.py
+++ x/pkgmgrs.py
@@ -20,6 +20,10 @@ class SystemPackageManager(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractmethod
+    def upgrade(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
     def install(self, *packages: SystemPackageOrStr) -> None:
         raise NotImplementedError
 
@@ -32,11 +36,20 @@ class BrewSystemPackageManager(SystemPackageManager):
     def update(self) -> None:
         subprocess.check_call(['brew', 'update'])
 
+    def upgrade(self) -> None:
+        subprocess.check_call(['brew', 'upgrade'])
+
     def install(self, *packages: SystemPackageOrStr) -> None:
-        raise NotImplementedError
+        es: ta.List[str] = []
+        for p in packages:
+            if isinstance(p, SystemPackage):
+                es.append(p.name + (f'{p.version}' if p.version is not None else ''))
+            else:
+                es.append(p)
+        subprocess.check_call(['brew', 'install', *es])
 
     def query(self, *packages: SystemPackageOrStr) -> ta.Mapping[str, SystemPackage]:
-        pns = [p if isinstance(p, str) else p.name for p in packages]
+        pns = [p.name if isinstance(p, SystemPackage) else p for p in packages]
         o = subprocess.check_output(['brew', 'info', '--json', *pns])
         j = json.loads(o.decode())
         d: ta.Dict[str, SystemPackage] = {}
@@ -51,17 +64,37 @@ class BrewSystemPackageManager(SystemPackageManager):
 
 
 class AptSystemPackageManager(SystemPackageManager):
-    pass
+    def update(self) -> None:
+        raise NotImplementedError
+
+    def upgrade(self) -> None:
+        raise NotImplementedError
+
+    def install(self, *packages: SystemPackageOrStr) -> None:
+        raise NotImplementedError
+
+    def query(self, *packages: SystemPackageOrStr) -> ta.Mapping[str, SystemPackage]:
+        raise NotImplementedError
 
 
 class YumSystemPackageManager(SystemPackageManager):
-    pass
+    def update(self) -> None:
+        raise NotImplementedError
+
+    def upgrade(self) -> None:
+        raise NotImplementedError
+
+    def install(self, *packages: SystemPackageOrStr) -> None:
+        raise NotImplementedError
+
+    def query(self, *packages: SystemPackageOrStr) -> ta.Mapping[str, SystemPackage]:
+        raise NotImplementedError
 
 
 def _main() -> None:
     spm = BrewSystemPackageManager()
-    # spm.update()
     print(spm.query('jq', 'gcc', 'gfortran'))
+    spm.install('cowsay')
 
 
 if __name__ == '__main__':
