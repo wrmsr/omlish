1ecc206ef93df8b34e6edbfa83026c6a24e34e84
wrmsr <timwilloney@gmail.com>
2024-12-07 23:56:26 -0800

    2024-12-08T07:56:26Z

--- x/amalg_resources_test.py
+++ x/amalg_resources_test.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # ruff: noqa: UP006 UP007
 # @omlish-amalg ./amalg_resources_test.py
+from omlish.lite.resources import read_package_resource_binary
 from omlish.lite.resources import read_package_resource_text
 
 
 ATOM_SQL = read_package_resource_text(__package__, 'atom.sql')
+
+
+ATOM_SQL_BYTES = read_package_resource_binary(__package__, 'atom.sql')
 
 
 def _main() -> None:
