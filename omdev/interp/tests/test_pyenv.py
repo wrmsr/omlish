2006cfa4532fb1fe89dff0f71020c57100b1b925
wrmsr <timwilloney@gmail.com>
2024-12-09 11:26:06 -0800

    2024-12-09T19:26:06Z

--- omdev/interp/tests/test_pyenv.py
+++ omdev/interp/tests/test_pyenv.py
@@ -4,15 +4,15 @@ import unittest
 from .. import pyenv as pe
 
 
-class TestPyenv(unittest.TestCase):
+class TestPyenv(unittest.IsolatedAsyncioTestCase):
     @unittest.skipUnless(sys.platform == 'darwin', 'requires darwin')
-    def test_darwin(self):
-        print(pe.DarwinPyenvInstallOpts().opts())
+    async def test_darwin(self):
+        print(await pe.DarwinPyenvInstallOpts().opts())
 
     @unittest.skipUnless(sys.platform == 'linux', 'requires linux')
-    def test_linux(self):
-        print(pe.LinuxPyenvInstallOpts().opts())
+    async def test_linux(self):
+        print(await pe.LinuxPyenvInstallOpts().opts())
 
-    def test_pyenv(self):
+    async def test_pyenv(self):
         p = pe.PyenvInterpProvider()
-        print(p.installed())
+        print(await p.installed())
