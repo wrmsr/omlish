57677b7e56d9b31f882f0d6399e49ad53a5ef6e4
wrmsr <timwilloney@gmail.com>
2024-12-09 11:27:05 -0800

    2024-12-09T19:27:05Z

--- omdev/interp/tests/test_base.py
+++ omdev/interp/tests/test_base.py
@@ -7,4 +7,4 @@ from ..types import InterpSpecifier
 class TestBaseProviders(unittest.IsolatedAsyncioTestCase):
     async def test_running(self):
         v = await bp.RunningInterpProvider().get_installed_versions(InterpSpecifier.parse('3.12'))
-        print(bp.RunningInterpProvider().get_installed_version(v[0]))
+        print(await bp.RunningInterpProvider().get_installed_version(v[0]))
