e0a00334faedb5ac580c2e2757a2fa0d6d784c37
wrmsr <timwilloney@gmail.com>
2024-12-09 16:01:48 -0800

    2024-12-10T00:01:48Z

--- ominfra/manage/remote/channel.py
+++ ominfra/manage/remote/channel.py
@@ -2,6 +2,7 @@
 import asyncio
 import json
 import struct
+import sys
 import typing as ta
 
 from omlish.lite.json import json_dumps_compact
@@ -37,9 +38,12 @@ class RemoteChannel:
         j = json_dumps_compact(self._msh.marshal_obj(o, ty))
         d = j.encode('utf-8')
 
+        sys.stderr.write('_send_obj\n')
+        sys.stderr.write(repr(d) + '\n')
         self._output.write(struct.pack('<I', len(d)))
         self._output.write(d)
-        await self._output.drain()  # FIXME: FLUSH UNDERLYING
+        await self._output.drain()
+        sys.stderr.write('-_send_obj\n')
 
     async def send_obj(self, o: ta.Any, ty: ta.Any = None) -> None:
         async with self._lock:
@@ -48,18 +52,23 @@ class RemoteChannel:
     #
 
     async def _recv_obj(self, ty: ta.Type[T]) -> ta.Optional[T]:
+        sys.stderr.write('_recv_obj\n')
         d = await self._input.read(4)
+        sys.stderr.write(repr(d) + '\n')
         if not d:
             return None
         if len(d) != 4:
             raise EOFError
 
         sz = struct.unpack('<I', d)[0]
+        sys.stderr.write(repr(sz) + '\n')
         d = await self._input.read(sz)
+        sys.stderr.write(repr(d) + '\n')
         if len(d) != sz:
             raise EOFError
 
         j = json.loads(d.decode('utf-8'))
+        sys.stderr.write(repr(j) + '\n')
         return self._msh.unmarshal_obj(j, ty)
 
     async def recv_obj(self, ty: ta.Type[T]) -> ta.Optional[T]:
