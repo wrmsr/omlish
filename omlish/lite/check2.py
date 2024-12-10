1feb8fd51165cb2d58df7c122d868e9aa3eb9955
wrmsr <timwilloney@gmail.com>
2024-12-10 13:57:04 -0800

    2024-12-10T21:57:04Z

--- omlish/lite/check2.py
+++ omlish/lite/check2.py
@@ -1,3 +1,4 @@
+# ruff: noqa: UP006 UP007
 """
 TODO:
  - def maybe(v: lang.Maybe[T])
@@ -12,128 +13,92 @@ import typing as ta
 T = ta.TypeVar('T')
 SizedT = ta.TypeVar('SizedT', bound=ta.Sized)
 
-Message: ta.TypeAlias = ta.Union[str, ta.Callable[..., ta.Optional[str]], None]
-
-_NONE_TYPE = type(None)
-
-_isinstance = isinstance
-_issubclass = issubclass
-_callable = callable
-
-
-##
-
-
-_CONFIG_LOCK = threading.RLock()
-
-
-OnRaiseFn: ta.TypeAlias = ta.Callable[[Exception], None]
-_ON_RAISE: ta.Sequence[OnRaiseFn] = []
-
-
-def register_on_raise(fn: OnRaiseFn) -> None:
-    global _ON_RAISE
-    with _CONFIG_LOCK:
-        _ON_RAISE = [*_ON_RAISE, fn]
-
-
-def unregister_on_raise(fn: OnRaiseFn) -> None:
-    global _ON_RAISE
-    with _CONFIG_LOCK:
-        _ON_RAISE = [e for e in _ON_RAISE if e != fn]
-
-
-#
-
-
-_ARGS_RENDERER: ta.Optional[ta.Callable[..., ta.Optional[str]]] = None
-
-
-def _try_enable_args_rendering() -> bool:
-    global _ARGS_RENDERER
-    if _ARGS_RENDERER is not None:
-        return True
-
-    try:
-        from .diag.asts import ArgsRenderer
-
-        ArgsRenderer.smoketest()
-
-    except Exception:  # noqa
-        return False
-
-    def _real_render_args(fmt: str, *args: ta.Any) -> ta.Optional[str]:
-        ra = ArgsRenderer(back=3).render_args(*args)
-        if ra is None:
-            return None
-
-        return fmt % tuple(str(a) for a in ra)
-
-    _ARGS_RENDERER = _real_render_args
-    return True
-
-
-_TRIED_ENABLED_ARGS_RENDERING: ta.Optional[bool] = None
-
-
-def try_enable_args_rendering() -> bool:
-    global _TRIED_ENABLED_ARGS_RENDERING
-    if _TRIED_ENABLED_ARGS_RENDERING is not None:
-        return _TRIED_ENABLED_ARGS_RENDERING
-
-    with _CONFIG_LOCK:
-        if _TRIED_ENABLED_ARGS_RENDERING is None:
-            _TRIED_ENABLED_ARGS_RENDERING = _try_enable_args_rendering()
-
-        return _TRIED_ENABLED_ARGS_RENDERING
-
-
-##
-
-
-def _default_exception_factory(exc_cls: ta.Type[Exception], *args, **kwargs) -> Exception:
-    return exc_cls(*args, **kwargs)  # noqa
-
-
-_EXCEPTION_FACTORY = _default_exception_factory
-
-
-class _ArgsKwargs:
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
+CheckMessage = ta.Union[str, ta.Callable[..., ta.Optional[str]], None]  # ta.TypeAlias
+CheckLateConfigureFn = ta.Callable[['Checks'], None]
+CheckOnRaiseFn = ta.Callable[[Exception], None]  # ta.TypeAlias
+CheckExceptionFactory = ta.Callable[..., Exception]  # ta.TypeAlias
+CheckArgsRenderer = ta.Callable[..., ta.Optional[str]]  # ta.TypeAlias
 
 
 ##
 
 
 class Checks:
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._config_lock = threading.RLock()
+        self._on_raise_fns: ta.Sequence[CheckOnRaiseFn] = []
+        self._exception_factory: CheckExceptionFactory = Checks.default_exception_factory
+        self._args_renderer: ta.Optional[CheckArgsRenderer] = None
+        self._late_configure_fns: ta.Sequence[CheckLateConfigureFn] = []
+
+    @staticmethod
+    def default_exception_factory(exc_cls: ta.Type[Exception], *args, **kwargs) -> Exception:
+        return exc_cls(*args, **kwargs)  # noqa
+
+    #
+
+    def register_on_raise(self, fn: CheckOnRaiseFn) -> None:
+        with self._config_lock:
+            self._on_raise_fns = [*self._on_raise_fns, fn]
+
+    def unregister_on_raise(self, fn: CheckOnRaiseFn) -> None:
+        with self._config_lock:
+            self._on_raise_fns = [e for e in self._on_raise_fns if e != fn]
+
+    #
+
+    def register_late_configure(self, fn: CheckLateConfigureFn) -> None:
+        with self._config_lock:
+            self._late_configure_fns = [*self._late_configure_fns, fn]
+
+    def _late_configure(self) -> None:
+        if not self._late_configure_fns:
+            return
+
+        with self._config_lock:
+            if not (lc := self._late_configure_fns):
+                return
+
+            for fn in lc:
+                fn(self)
+
+            self._late_configure_fns = []
+
+    #
+
+    class _ArgsKwargs:
+        def __init__(self, *args, **kwargs):
+            self.args = args
+            self.kwargs = kwargs
+
     def _raise(
             self,
             exception_type: ta.Type[Exception],
             default_message: str,
-            message: Message,
+            message: CheckMessage,
             ak: _ArgsKwargs = _ArgsKwargs(),
             *,
             render_fmt: ta.Optional[str] = None,
     ) -> ta.NoReturn:
         exc_args = ()
-        if _callable(message):
+        if callable(message):
             message = ta.cast(ta.Callable, message)(*ak.args, **ak.kwargs)
-            if _isinstance(message, tuple):
+            if isinstance(message, tuple):
                 message, *exc_args = message  # type: ignore
 
         if message is None:
             message = default_message
 
-        try_enable_args_rendering()
-
-        if render_fmt is not None and _ARGS_RENDERER is not None:
-            rendered_args = _ARGS_RENDERER(render_fmt, *ak.args)
+        if render_fmt is not None and (af := self._args_renderer) is not None:
+            rendered_args = af(render_fmt, *ak.args)
             if rendered_args is not None:
                 message = f'{message} : {rendered_args}'
 
-        exc = _EXCEPTION_FACTORY(
+        self._late_configure()
+
+        exc = self._exception_factory(
             exception_type,
             message,
             *exc_args,
@@ -141,7 +106,7 @@ class Checks:
             **ak.kwargs,
         )
 
-        for fn in _ON_RAISE:
+        for fn in self._on_raise_fns:
             fn(exc)
 
         raise exc
@@ -154,59 +119,59 @@ class Checks:
         if not isinstance(spec, tuple):
             spec = (spec,)
         if None in spec:
-            spec = tuple(filter(None, spec)) + (_NONE_TYPE,)  # noqa
+            spec = tuple(filter(None, spec)) + (None.__class__,)  # noqa
         if ta.Any in spec:
             spec = (object,)
         return spec
 
-    def isinstance(self, v: ta.Any, spec: ta.Union[ta.Type[T], tuple], msg: Message = None) -> T:  # noqa
+    def isinstance(self, v: ta.Any, spec: ta.Union[ta.Type[T], tuple], msg: CheckMessage = None) -> T:  # noqa
         if not isinstance(v, self._unpack_isinstance_spec(spec)):
             self._raise(
                 TypeError,
                 'Must be instance',
                 msg,
-                _ArgsKwargs(v, spec),
+                Checks._ArgsKwargs(v, spec),
                 render_fmt='not isinstance(%s, %s)',
             )
 
         return v
 
-    def of_isinstance(self, spec: ta.Union[ta.Type[T], tuple], msg: Message = None) -> ta.Callable[[ta.Any], T]:
+    def of_isinstance(self, spec: ta.Union[ta.Type[T], tuple], msg: CheckMessage = None) -> ta.Callable[[ta.Any], T]:
         def inner(v):
-            return isinstance(v, self._unpack_isinstance_spec(spec), msg)
+            return self.isinstance(v, self._unpack_isinstance_spec(spec), msg)
 
         return inner
 
-    def cast(self, v: ta.Any, cls: ta.Type[T], msg: Message = None) -> T:  # noqa
+    def cast(self, v: ta.Any, cls: ta.Type[T], msg: CheckMessage = None) -> T:  # noqa
         if not isinstance(v, cls):
             self._raise(
                 TypeError,
                 'Must be instance',
                 msg,
-                _ArgsKwargs(v, cls),
+                Checks._ArgsKwargs(v, cls),
             )
 
         return v
 
-    def of_cast(self, cls: ta.Type[T], msg: Message = None) -> ta.Callable[[T], T]:
+    def of_cast(self, cls: ta.Type[T], msg: CheckMessage = None) -> ta.Callable[[T], T]:
         def inner(v):
-            return isinstance(v, cls, msg)
+            return self.cast(v, cls, msg)
 
         return inner
 
-    def not_isinstance(self, v: T, spec: ta.Any, msg: Message = None) -> T:  # noqa
-        if _isinstance(v, self._unpack_isinstance_spec(spec)):
+    def not_isinstance(self, v: T, spec: ta.Any, msg: CheckMessage = None) -> T:  # noqa
+        if isinstance(v, self._unpack_isinstance_spec(spec)):
             self._raise(
                 TypeError,
                 'Must not be instance',
                 msg,
-                _ArgsKwargs(v, spec),
+                Checks._ArgsKwargs(v, spec),
                 render_fmt='isinstance(%s, %s)',
             )
 
         return v
 
-    def of_not_isinstance(self, spec: ta.Any, msg: Message = None) -> ta.Callable[[T], T]:
+    def of_not_isinstance(self, spec: ta.Any, msg: CheckMessage = None) -> ta.Callable[[T], T]:
         def inner(v):
             return self.not_isinstance(v, self._unpack_isinstance_spec(spec), msg)
 
@@ -214,69 +179,69 @@ class Checks:
 
     ##
 
-    def issubclass(self, v: ta.Type[T], spec: ta.Any, msg: Message = None) -> ta.Type[T]:  # noqa
+    def issubclass(self, v: ta.Type[T], spec: ta.Any, msg: CheckMessage = None) -> ta.Type[T]:  # noqa
         if not issubclass(v, spec):
             self._raise(
                 TypeError,
                 'Must be subclass',
                 msg,
-                _ArgsKwargs(v, spec),
+                Checks._ArgsKwargs(v, spec),
                 render_fmt='not issubclass(%s, %s)',
             )
 
         return v
 
-    def not_issubclass(self, v: ta.Type[T], spec: ta.Any, msg: Message = None) -> ta.Type[T]:  # noqa
+    def not_issubclass(self, v: ta.Type[T], spec: ta.Any, msg: CheckMessage = None) -> ta.Type[T]:  # noqa
         if issubclass(v, spec):
             self._raise(
                 TypeError,
                 'Must not be subclass',
                 msg,
-                _ArgsKwargs(v, spec),
+                Checks._ArgsKwargs(v, spec),
                 render_fmt='issubclass(%s, %s)',
             )
 
         return v
 
-    ##
+    #
 
-    def in_(self, v: T, c: ta.Container[T], msg: Message = None) -> T:
+    def in_(self, v: T, c: ta.Container[T], msg: CheckMessage = None) -> T:
         if v not in c:
             self._raise(
                 ValueError,
                 'Must be in',
                 msg,
-                _ArgsKwargs(v, c),
+                Checks._ArgsKwargs(v, c),
                 render_fmt='%s not in %s',
             )
 
         return v
 
-    def not_in(self, v: T, c: ta.Container[T], msg: Message = None) -> T:
+    def not_in(self, v: T, c: ta.Container[T], msg: CheckMessage = None) -> T:
         if v in c:
             self._raise(
                 ValueError,
                 'Must not be in',
                 msg,
-                _ArgsKwargs(v, c),
+                Checks._ArgsKwargs(v, c),
                 render_fmt='%s in %s',
             )
 
         return v
 
-    def empty(self, v: SizedT, msg: Message = None) -> SizedT:
+    def empty(self, v: SizedT, msg: CheckMessage = None) -> SizedT:
         if len(v) != 0:
             self._raise(
                 ValueError,
                 'Must be empty',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
 
         return v
 
-    def iterempty(self, v: ta.Iterable[T], msg: Message = None) -> ta.Iterable[T]:
+    def iterempty(self, v: ta.Iterable[T], msg: CheckMessage = None) -> ta.Iterable[T]:
         it = iter(v)
         try:
             next(it)
@@ -287,37 +252,37 @@ class Checks:
                 ValueError,
                 'Must be empty',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
 
         return v
 
-    def not_empty(self, v: SizedT, msg: Message = None) -> SizedT:
+    def not_empty(self, v: SizedT, msg: CheckMessage = None) -> SizedT:
         if len(v) == 0:
             self._raise(
                 ValueError,
                 'Must not be empty',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
 
         return v
 
-    def unique(self, it: ta.Iterable[T], msg: Message = None) -> ta.Iterable[T]:
+    def unique(self, it: ta.Iterable[T], msg: CheckMessage = None) -> ta.Iterable[T]:
         dupes = [e for e, c in collections.Counter(it).items() if c > 1]
         if dupes:
             self._raise(
                 ValueError,
                 'Must be unique',
                 msg,
-                _ArgsKwargs(it, dupes),
+                Checks._ArgsKwargs(it, dupes),
             )
 
         return it
 
-    def single(self, obj: ta.Iterable[T], message: Message = None) -> T:
+    def single(self, obj: ta.Iterable[T], message: CheckMessage = None) -> T:
         try:
             [value] = obj
         except ValueError:
@@ -325,13 +290,13 @@ class Checks:
                 ValueError,
                 'Must be single',
                 message,
-                _ArgsKwargs(obj),
+                Checks._ArgsKwargs(obj),
                 render_fmt='%s',
             )
 
         return value
 
-    def opt_single(self, obj: ta.Iterable[T], message: Message = None) -> ta.Optional[T]:
+    def opt_single(self, obj: ta.Iterable[T], message: CheckMessage = None) -> ta.Optional[T]:
         it = iter(obj)
         try:
             value = next(it)
@@ -347,138 +312,143 @@ class Checks:
             ValueError,
             'Must be empty or single',
             message,
-            _ArgsKwargs(obj),
+            Checks._ArgsKwargs(obj),
             render_fmt='%s',
         )
 
-    ##
+        raise RuntimeError  # noqa
 
-    def none(self, v: ta.Any, msg: Message = None) -> None:
+    #
+
+    def none(self, v: ta.Any, msg: CheckMessage = None) -> None:
         if v is not None:
             self._raise(
                 ValueError,
                 'Must be None',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
 
-    def not_none(self, v: ta.Optional[T], msg: Message = None) -> T:
+    def not_none(self, v: ta.Optional[T], msg: CheckMessage = None) -> T:
         if v is None:
             self._raise(
                 ValueError,
                 'Must not be None',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
 
         return v
 
-    ##
+    #
 
-    def equal(self, v: T, o: ta.Any, msg: Message = None) -> T:
+    def equal(self, v: T, o: ta.Any, msg: CheckMessage = None) -> T:
         if o != v:
             self._raise(
                 ValueError,
                 'Must be equal',
                 msg,
-                _ArgsKwargs(v, o),
+                Checks._ArgsKwargs(v, o),
                 render_fmt='%s != %s',
             )
 
         return v
 
-    def is_(self, v: T, o: ta.Any, msg: Message = None) -> T:
+    def is_(self, v: T, o: ta.Any, msg: CheckMessage = None) -> T:
         if o is not v:
             self._raise(
                 ValueError,
                 'Must be the same',
                 msg,
-                _ArgsKwargs(v, o),
+                Checks._ArgsKwargs(v, o),
                 render_fmt='%s is not %s',
             )
 
         return v
 
-    def is_not(self, v: T, o: ta.Any, msg: Message = None) -> T:
+    def is_not(self, v: T, o: ta.Any, msg: CheckMessage = None) -> T:
         if o is v:
             self._raise(
                 ValueError,
                 'Must not be the same',
                 msg,
-                _ArgsKwargs(v, o),
+                Checks._ArgsKwargs(v, o),
                 render_fmt='%s is %s',
             )
 
         return v
 
-    def callable(self, v: T, msg: Message = None) -> T:  # noqa
-        if not _callable(v):
+    def callable(self, v: T, msg: CheckMessage = None) -> T:  # noqa
+        if not callable(v):
             self._raise(
                 TypeError,
                 'Must be callable',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
 
         return v  # type: ignore
 
-    def non_empty_str(self, v: ta.Optional[str], msg: Message = None) -> str:
+    def non_empty_str(self, v: ta.Optional[str], msg: CheckMessage = None) -> str:
         if not isinstance(v, str) or not v:
             self._raise(
                 ValueError,
                 'Must be non-empty str',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
 
         return v
 
-    def replacing(self, expected: ta.Any, old: ta.Any, new: T, msg: Message = None) -> T:
+    def replacing(self, expected: ta.Any, old: ta.Any, new: T, msg: CheckMessage = None) -> T:
         if old != expected:
             self._raise(
                 ValueError,
                 'Must be replacing',
                 msg,
-                _ArgsKwargs(expected, old, new),
+                Checks._ArgsKwargs(expected, old, new),
                 render_fmt='%s -> %s -> %s',
             )
 
         return new
 
-    def replacing_none(self, old: ta.Any, new: T, msg: Message = None) -> T:
+    def replacing_none(self, old: ta.Any, new: T, msg: CheckMessage = None) -> T:
         if old is not None:
             self._raise(
                 ValueError,
                 'Must be replacing None',
                 msg,
-                _ArgsKwargs(old, new),
+                Checks._ArgsKwargs(old, new),
                 render_fmt='%s -> %s',
             )
 
         return new
 
-    ##
+    #
 
-    def arg(self, v: bool, msg: Message = None) -> None:
+    def arg(self, v: bool, msg: CheckMessage = None) -> None:
         if not v:
             self._raise(
                 RuntimeError,
                 'Argument condition not met',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
 
-    def state(self, v: bool, msg: Message = None) -> None:
+    def state(self, v: bool, msg: CheckMessage = None) -> None:
         if not v:
             self._raise(
                 RuntimeError,
                 'State condition not met',
                 msg,
-                _ArgsKwargs(v),
+                Checks._ArgsKwargs(v),
                 render_fmt='%s',
             )
+
+
+check = Checks()
