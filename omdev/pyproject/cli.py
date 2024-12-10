679a31edf567e7b669f181bcd933de23f73b8d44
wrmsr <timwilloney@gmail.com>
2024-12-10 15:07:40 -0800

    2024-12-10T23:07:40Z

--- omdev/pyproject/cli.py
+++ omdev/pyproject/cli.py
@@ -25,7 +25,6 @@ import asyncio
 import concurrent.futures as cf
 import dataclasses as dc
 import functools
-import glob
 import itertools
 import multiprocessing as mp
 import os.path
@@ -34,23 +33,18 @@ import shutil
 import sys
 import typing as ta
 
-from omlish.lite.cached import async_cached_nullary
 from omlish.lite.cached import cached_nullary
 from omlish.lite.check import check
 from omlish.lite.logs import configure_standard_logging
-from omlish.lite.logs import log
 from omlish.lite.runtime import check_runtime_version
 from omlish.lite.subprocesses import subprocess_check_call
 
-from ..interp.resolvers import DEFAULT_INTERP_RESOLVER
-from ..interp.types import InterpSpecifier
 from ..toml.parser import toml_loads
 from .configs import PyprojectConfig
 from .configs import PyprojectConfigPreparer
-from .configs import VenvConfig
 from .pkg import BasePyprojectPackageGenerator
 from .pkg import PyprojectPackageGenerator
-from .reqs import RequirementsRewriter
+from .venvs import Venv
 
 
 ##
@@ -96,105 +90,6 @@ def _script_rel_path() -> str:
     if not (f := __file__).startswith(cwd):
         raise OSError(f'file {f} not in {cwd}')
     return f[len(cwd):].lstrip(os.sep)
-
-
-##
-
-
-class Venv:
-    def __init__(
-            self,
-            name: str,
-            cfg: VenvConfig,
-    ) -> None:
-        super().__init__()
-        self._name = name
-        self._cfg = cfg
-
-    @property
-    def cfg(self) -> VenvConfig:
-        return self._cfg
-
-    DIR_NAME = '.venvs'
-
-    @property
-    def dir_name(self) -> str:
-        return os.path.join(self.DIR_NAME, self._name)
-
-    @async_cached_nullary
-    async def interp_exe(self) -> str:
-        i = InterpSpecifier.parse(check.not_none(self._cfg.interp))
-        return check.not_none(await DEFAULT_INTERP_RESOLVER.resolve(i, install=True)).exe
-
-    @cached_nullary
-    def exe(self) -> str:
-        ve = os.path.join(self.dir_name, 'bin/python')
-        if not os.path.isfile(ve):
-            raise Exception(f'venv exe {ve} does not exist or is not a file!')
-        return ve
-
-    @async_cached_nullary
-    async def create(self) -> bool:
-        if os.path.exists(dn := self.dir_name):
-            if not os.path.isdir(dn):
-                raise Exception(f'{dn} exists but is not a directory!')
-            return False
-
-        log.info('Using interpreter %s', (ie := await self.interp_exe()))
-        subprocess_check_call(ie, '-m', 'venv', dn)
-
-        ve = self.exe()
-        uv = self._cfg.use_uv
-
-        subprocess_check_call(
-            ve,
-            '-m', 'pip',
-            'install', '-v', '--upgrade',
-            'pip',
-            'setuptools',
-            'wheel',
-            *(['uv'] if uv else []),
-        )
-
-        if sr := self._cfg.requires:
-            rr = RequirementsRewriter(self._name)
-            reqs = [rr.rewrite(req) for req in sr]
-
-            # TODO: automatically try slower uv download when it fails? lol
-            #   Caused by: Failed to download distribution due to network timeout. Try increasing UV_HTTP_TIMEOUT (current value: 30s).  # noqa
-            #   UV_CONCURRENT_DOWNLOADS=4 UV_HTTP_TIMEOUT=3600
-
-            subprocess_check_call(
-                ve,
-                '-m',
-                *(['uv'] if uv else []),
-                'pip',
-                'install',
-                *([] if uv else ['-v']),
-                *reqs,
-            )
-
-        return True
-
-    @staticmethod
-    def _resolve_srcs(raw: ta.List[str]) -> ta.List[str]:
-        out: list[str] = []
-        seen: ta.Set[str] = set()
-        for r in raw:
-            es: list[str]
-            if any(c in r for c in '*?'):
-                es = list(glob.glob(r, recursive=True))
-            else:
-                es = [r]
-            for e in es:
-                if e not in seen:
-                    seen.add(e)
-                    out.append(e)
-        return out
-
-    @cached_nullary
-    def srcs(self) -> ta.Sequence[str]:
-        return self._resolve_srcs(self._cfg.srcs or [])
 
 
 ##
