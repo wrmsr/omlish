f0f8000663fb0d1821e90c64e22a76c360780004
wrmsr <timwilloney@gmail.com>
2024-12-10 17:44:59 -0800

    2024-12-11T01:44:59Z

--- ominfra/manage/system/commands.py
+++ ominfra/manage/system/commands.py
@@ -1,10 +1,14 @@
 # ruff: noqa: UP006 UP007
 import dataclasses as dc
+import typing as ta
 
+from omlish.lite.check import check
 from omlish.lite.logs import log
 
 from ..commands.base import Command
 from ..commands.base import CommandExecutor
+from .packages import SystemPackage
+from .packages import SystemPackageManager
 
 
 ##
@@ -12,13 +16,29 @@ from ..commands.base import CommandExecutor
 
 @dc.dataclass(frozen=True)
 class CheckSystemPackageCommand(Command['CheckSystemPackageCommand.Output']):
+    pkgs: ta.Sequence[str] = ()
+
+    def __post_init__(self) -> None:
+        check.not_isinstance(self.pkgs, str)
+
     @dc.dataclass(frozen=True)
     class Output(Command.Output):
-        pass
+        pkgs: ta.Sequence[SystemPackage]
 
 
 class CheckSystemPackageCommandExecutor(CommandExecutor[CheckSystemPackageCommand, CheckSystemPackageCommand.Output]):
+    def __init__(
+            self,
+            *,
+            mgr: SystemPackageManager,
+    ) -> None:
+        super().__init__()
+
+        self._mgr = mgr
+
     async def execute(self, cmd: CheckSystemPackageCommand) -> CheckSystemPackageCommand.Output:
         log.info('Checking system package!')
 
-        return CheckSystemPackageCommand.Output()
+        ret = await self._mgr.query(*cmd.pkgs)
+
+        return CheckSystemPackageCommand.Output(list(ret.values()))
