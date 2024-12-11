a80631cb0af4758f51d348ac4e10f47ef5bc9721
wrmsr <timwilloney@gmail.com>
2024-12-10 19:28:59 -0800

    2024-12-11T03:28:59Z

--- ominfra/manage/targets/connection.py
+++ ominfra/manage/targets/connection.py
@@ -1,11 +1,34 @@
 # ruff: noqa: UP006 UP007
+import abc
+import contextlib
+import dataclasses as dc
 import typing as ta
 
+from omlish.lite.check import check
+
 from ..commands.base import CommandExecutor
+from ..commands.local import LocalCommandExecutor
+from .targets import DirectManageTarget
 from .targets import ManageTarget
 
 
 ##
 
 
-ManageTargetConnector = ta.Callable[[ManageTarget], ta.AsyncContextManager[CommandExecutor]]  # ta.TypeAlias
+class ManageTargetConnector(abc.ABC):
+    @abc.abstractmethod
+    def connect(self, tgt: ManageTarget) -> ta.AsyncContextManager[CommandExecutor]:
+        raise NotImplementedError
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class DirectManageTargetConnectorImpl(ManageTargetConnector):
+    _local_executor: LocalCommandExecutor
+
+    @contextlib.asynccontextmanager
+    async def connect(self, tgt: ManageTarget) -> ta.AsyncGenerator[CommandExecutor, None]:
+        check.isinstance(tgt, DirectManageTarget)
+        yield self._local_executor
