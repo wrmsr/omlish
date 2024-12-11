d63260b77436a1e2cbcd45c61b5299d9c5db10c5
wrmsr <timwilloney@gmail.com>
2024-12-10 20:06:32 -0800

    2024-12-11T04:06:32Z

--- ominfra/manage/targets/connection.py
+++ ominfra/manage/targets/connection.py
@@ -10,6 +10,8 @@ from ..commands.base import CommandExecutor
 from ..commands.local import LocalCommandExecutor
 from .targets import LocalManageTarget
 from .targets import ManageTarget
+from ..remote.connection import PyremoteRemoteExecutionConnector
+from ..remote.connection import InProcessRemoteExecutionConnector
 
 
 ##
@@ -27,15 +29,22 @@ class ManageTargetConnector(abc.ABC):
 @dc.dataclass(frozen=True)
 class LocalManageTargetConnectorImpl(ManageTargetConnector):
     _local_executor: LocalCommandExecutor
+    _in_process_connector: InProcessRemoteExecutionConnector
+    _pyremote_connector: PyremoteRemoteExecutionConnector
 
     @contextlib.asynccontextmanager
     async def connect(self, tgt: ManageTarget) -> ta.AsyncGenerator[CommandExecutor, None]:
         lmt = check.isinstance(tgt, LocalManageTarget)
+
         if lmt.mode == LocalManageTarget.Mode.DIRECT:
             yield self._local_executor
+
         elif lmt.mode == LocalManageTarget.Mode.SUBPROCESS:
             raise NotImplementedError
+
         elif lmt.mode == LocalManageTarget.Mode.FAKE_REMOTE:
-            raise NotImplementedError
+            with self._in_process_connector.connect() as rce:
+                yield rce
+
         else:
             raise TypeError(lmt.mode)
