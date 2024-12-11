80fa6d087ce5128988d1177b3c4fea667917423d
wrmsr <timwilloney@gmail.com>
2024-12-10 17:37:15 -0800

    2024-12-11T01:37:15Z

--- ominfra/scripts/manage.py
+++ ominfra/scripts/manage.py
@@ -1221,8 +1221,6 @@ def async_cached_nullary(fn):  # ta.Callable[..., T]) -> ta.Callable[..., T]:
 """
 TODO:
  - def maybe(v: lang.Maybe[T])
- - patch / override lite.check ?
-  - checker interface?
 """
 
 
@@ -7413,7 +7411,7 @@ class MainCli(ArgparseCli):
 
                 timebomb_delay_s=self.args.remote_timebomb_delay_s,
 
-                use_in_process_remote_executor=True,
+                # use_in_process_remote_executor=True,
             ),
         )
 
