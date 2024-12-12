2586a7fc5934cfb7747da5789ecb30225ca9f520
wrmsr <timwilloney@gmail.com>
2024-12-12 12:03:13 -0800

    2024-12-12T20:03:13Z

--- ominfra/manage/deploy/tests/test_git.py
+++ ominfra/manage/deploy/tests/test_git.py
@@ -13,7 +13,9 @@ class TestGit(unittest.IsolatedAsyncioTestCase):
     async def test_git(self):
         deploy_home = DeployHome(tempfile.mkdtemp())
 
-        git = DeployGitManager(deploy_home)
+        git = DeployGitManager(
+            deploy_home=deploy_home,
+        )
 
         spec = DeployGitSpec(
             repo=DeployGitRepo(
