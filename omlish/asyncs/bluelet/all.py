6130fe9d8221cde4b8a71dadc177cfa6c59c1a69
wrmsr <timwilloney@gmail.com>
2024-12-13 15:44:45 -0800

    2024-12-13T23:44:45Z

--- omlish/asyncs/bluelet/all.py
+++ omlish/asyncs/bluelet/all.py
@@ -1,35 +1,54 @@
-from .api import bluelet
-from .core import BlueletCoro as Coro  # noqa
-from .core import BlueletExcInfo as ExcInfo  # noqa
-from .core import CoreBlueletEvent as CoreEvent  # noqa
-from .core import DelegationBlueletEvent as DelegationEvent  # noqa
-from .core import ExceptionBlueletEvent as ExceptionEvent  # noqa
-from .core import JoinBlueletEvent as JoinEvent  # noqa
-from .core import KillBlueletEvent as KillEvent  # noqa
-from .core import ReturnBlueletEvent as ReturnEvent  # noqa
-from .core import SleepBlueletEvent as SleepEvent  # noqa
-from .core import SpawnBlueletEvent as SpawnEvent  # noqa
-from .core import ValueBlueletEvent as ValueEvent  # noqa
-from .events import BlueletEvent as Event  # noqa
-from .events import BlueletFuture as Future  # noqa
-from .events import BlueletHasFileno as HasFileno  # noqa
-from .events import BlueletWaitable as Waitable  # noqa
-from .events import BlueletWaitables as Waitables  # noqa
-from .events import WaitableBlueletEvent as WaitableEvent  # noqa
-from .files import FileBlueletEvent as FileEvent  # noqa
-from .files import ReadBlueletEvent as ReadEvent  # noqa
-from .files import WriteBlueletEvent as WriteEvent  # noqa
-from .runner import BlueletCoroException as CoroException  # noqa
-from .sockets import AcceptBlueletEvent as AcceptEvent  # noqa
-from .sockets import BlueletConnection as Connection  # noqa
-from .sockets import BlueletListener as Listener  # noqa
-from .sockets import ReceiveBlueletEvent as ReceiveEvent  # noqa
-from .sockets import SendBlueletEvent as SendEvent  # noqa
-from .sockets import SocketBlueletEvent as SocketEvent  # noqa
-from .sockets import SocketClosedBlueletError as SocketClosedError  # noqa
+# ruff: noqa: I001
+from .api import (  # noqa
+    bluelet,
+)
+
+from .core import (  # noqa
+    BlueletCoro as Coro,
+    BlueletExcInfo as ExcInfo,
+    CoreBlueletEvent as CoreEvent,
+    DelegationBlueletEvent as DelegationEvent,
+    ExceptionBlueletEvent as ExceptionEvent,
+    JoinBlueletEvent as JoinEvent,
+    KillBlueletEvent as KillEvent,
+    ReturnBlueletEvent as ReturnEvent,
+    SleepBlueletEvent as SleepEvent,
+    SpawnBlueletEvent as SpawnEvent,
+    ValueBlueletEvent as ValueEvent,
+)
+
+from .events import (  # noqa
+    BlueletEvent as Event,
+    BlueletFuture as Future,
+    BlueletHasFileno as HasFileno,
+    BlueletWaitable as Waitable,
+    BlueletWaitables as Waitables,
+    WaitableBlueletEvent as WaitableEvent,
+)
+
+from .files import (  # noqa
+    FileBlueletEvent as FileEvent,
+    ReadBlueletEvent as ReadEvent,
+    WriteBlueletEvent as WriteEvent,
+)
+
+from .runner import (  # noqa
+    BlueletCoroException as CoroException,
+)
+
+from .sockets import (  # noqa
+    AcceptBlueletEvent as AcceptEvent,
+    BlueletConnection as Connection,
+    BlueletListener as Listener,
+    ReceiveBlueletEvent as ReceiveEvent,
+    SendBlueletEvent as SendEvent,
+    SocketBlueletEvent as SocketEvent,
+    SocketClosedBlueletError as SocketClosedError,
+)
 
 
 ##
+
 
 call = bluelet.call
 end = bluelet.end
