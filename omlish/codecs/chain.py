6c3ec82df31d99a301e4f369af6f328b9c598e1c
wrmsr <timwilloney@gmail.com>
2024-12-14 21:04:50 -0800

    2024-12-15T05:04:49Z

---
+++ omlish/codecs/chain.py
@@ -0,0 +1,22 @@
+import dataclasses as dc
+import typing as ta
+
+from .base import EagerCodec
+
+
+@dc.dataclass(frozen=True)
+class ChainEagerCodec(EagerCodec[ta.Any, ta.Any]):
+    codecs: ta.Sequence[EagerCodec]
+
+    def encode(self, v: ta.Any) -> ta.Any:
+        for c in self.codecs:
+            v = c.encode(v)
+        return v
+
+    def decode(self, v: ta.Any) -> ta.Any:
+        for c in reversed(self.codecs):
+            v = c.decode(v)
+        return v
+
+
+chain = ChainEagerCodec
