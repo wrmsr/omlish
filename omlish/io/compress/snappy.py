df18fa43db816c36fab54eb17a683f21672955fa
wrmsr <timwilloney@gmail.com>
2024-12-14 18:59:50 -0800

    2024-12-15T02:59:50Z

--- omlish/io/compress/snappy.py
+++ omlish/io/compress/snappy.py
@@ -3,12 +3,17 @@ import typing as ta
 
 from ... import lang
 from .base import Compression
+from .codecs import make_compression_codec
+from .codecs import make_compression_lazy_loaded_codec
 
 
 if ta.TYPE_CHECKING:
     import snappy
 else:
     snappy = lang.proxy_import('snappy')
+
+
+##
 
 
 @dc.dataclass(frozen=True)
@@ -18,3 +23,12 @@ class SnappyCompression(Compression):
 
     def decompress(self, d: bytes) -> bytes:
         return snappy.decompress(d)
+
+
+##
+
+
+SNAPPY_CODEC = make_compression_codec('snappy', SnappyCompression)
+
+# @omlish-manifest
+_SNAPPY_LAZY_CODEC = make_compression_lazy_loaded_codec(__name__, 'SNAPPY_CODEC', SNAPPY_CODEC)
