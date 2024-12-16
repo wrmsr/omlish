087e8a1401985e917b1c26f6903d55bfc588f40f
wrmsr <timwilloney@gmail.com>
2024-12-16 13:45:19 -0800

    2024-12-16T21:45:19Z

--- omlish/io/compress/tests/test_codecs.py
+++ omlish/io/compress/tests/test_codecs.py
@@ -1,7 +1,15 @@
+import io
+import typing as ta
+
 import pytest
 
+from .... import check
 from .... import codecs
+from .... import lang
 from ....testing import pytest as ptu
+
+
+##
 
 
 def _test_compression(name: str) -> None:
@@ -34,3 +42,49 @@ def test_compression_snappy() -> None:
 @ptu.skip.if_cant_import('zstandard')
 def test_compression_zstd() -> None:
     _test_compression('zstd')
+
+
+##
+
+
+def _run_incremental_codec(g: ta.Generator[bytes | None, bytes | None, None], i: bytes) -> bytes:
+    cg = lang.corogen(g)
+    o = io.BytesIO()
+
+    for b in i:
+        r = cg.send(bytes([b]))
+        assert not r.is_return
+
+        while r.v is not None:
+            o.write(r.v)
+            r = cg.send(None)
+            assert not r.is_return
+
+    r = cg.send(b'')
+    assert not r.is_return
+
+    while r.v is not None:
+        o.write(r.v)
+        r = cg.send(None)
+
+    # assert r.is_return  # FIXME: ??
+    cg.close()
+
+    return o.getvalue()
+
+
+def _test_incremental_compression(name: str) -> None:
+    co = check.not_none(codecs.lookup(name).new_incremental)()
+    o = b'abcd1234'
+    c = _run_incremental_codec(co.encode_incremental(), o)
+    u = _run_incremental_codec(co.decode_incremental(), c)
+    assert o == u
+
+
+@pytest.mark.parametrize('name', [
+    'bz2',
+    'gzip',
+    'lzma',
+])
+def test_incremental_compression_codec(name: str) -> None:
+    _test_incremental_compression(name)
