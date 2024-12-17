0517b89a6faba82005252cfbc5ca8dc7f87df885
wrmsr <timwilloney@gmail.com>
2024-12-17 11:16:35 -0800

    2024-12-17T19:16:35Z

--- omlish/sql/queries/idents.py
+++ omlish/sql/queries/idents.py
@@ -1,5 +1,7 @@
+import functools
 import typing as ta
 
+from ... import check
 from ... import lang
 from .base import Builder
 from .base import Node
@@ -12,17 +14,43 @@ class Ident(Node, lang.Final):
     s: str
 
 
+##
+
+
+@functools.singledispatch
+def as_ident(o: ta.Any) -> Ident:
+    raise TypeError(o)
+
+
+@as_ident.register
+def _(i: Ident) -> Ident:
+    return i
+
+
+@as_ident.register
+def _(s: str) -> Ident:
+    return Ident(s)
+
+
+##
+
+
+class IdentAccessor:
+    def __getattr__(self, s: str) -> Ident:
+        check.not_in('.', s)
+        check.arg(not s.startswith('__'))
+        return Ident(s)
+
+
+##
+
+
 CanIdent: ta.TypeAlias = Ident | str
 
 
 class IdentBuilder(Builder):
     def ident(self, o: CanIdent) -> Ident:
-        if isinstance(o, Ident):
-            return o
-        elif isinstance(o, str):
-            return Ident(o)
-        else:
-            raise TypeError(o)
+        return as_ident(o)
 
     @ta.final
     def i(self, o: CanIdent) -> Ident:
--- omlish/sql/queries/names.py
+++ omlish/sql/queries/names.py
@@ -1,33 +1,83 @@
+import functools
 import typing as ta
 
+from ... import check
 from ... import dataclasses as dc
 from ... import lang
 from .base import Node
 from .idents import CanIdent
 from .idents import Ident
 from .idents import IdentBuilder
+from .idents import as_ident
 
 
 ##
 
 
+def _coerce_name_parts(o: ta.Iterable[Ident]) -> ta.Sequence[Ident]:
+    check.not_isinstance(o, str)
+    return check.not_empty(tuple(check.isinstance(e, Ident) for e in o))
+
+
 class Name(Node, lang.Final):
-    ps: ta.Sequence[Ident] = dc.xfield(coerce=tuple, validate=bool)
+    ps: ta.Sequence[Ident] = dc.xfield(coerce=_coerce_name_parts)
 
 
-CanName: ta.TypeAlias = Name | CanIdent | ta.Sequence[CanIdent]
+##
+
+
+@functools.singledispatch
+def as_name(o: ta.Any) -> Name:
+    if isinstance(o, ta.Sequence):
+        return Name([as_ident(p) for p in o])
+    else:
+        raise TypeError(o)
+
+
+@as_name.register
+def _(n: Name) -> Name:
+    return n
+
+
+@as_name.register
+def _(o: Ident | str) -> Name:
+    return Name([as_ident(o)])
+
+
+##
+
+
+class NameAccessor:
+    def __init__(self, n: Name | None = None) -> None:
+        super().__init__()
+        self.__query_name__ = n
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.__query_name__!r})'
+
+    def __getattr__(self, s: str) -> 'NameAccessor':
+        check.not_in('.', s)
+        check.arg(not s.startswith('__'))
+        return NameAccessor(Name([
+            *(sn.ps if (sn := self.__query_name__) is not None else []),
+            Ident(s),
+        ]))
+
+
+@as_name.register
+def _(a: NameAccessor) -> Name:
+    return check.not_none(a.__query_name__)
+
+
+##
+
+
+CanName: ta.TypeAlias = Name | CanIdent | ta.Sequence[CanIdent] | NameAccessor
 
 
 class NameBuilder(IdentBuilder):
     def name(self, o: CanName) -> Name:
-        if isinstance(o, Name):
-            return o
-        elif isinstance(o, (Ident, str)):
-            return Name([self.ident(o)])
-        elif isinstance(o, ta.Sequence):
-            return Name([self.ident(p) for p in o])
-        else:
-            raise TypeError(o)
+        return as_name(o)
 
     @ta.final
     def n(self, o: CanName) -> Name:
