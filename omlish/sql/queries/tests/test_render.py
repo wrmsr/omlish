84a0e962c42a848fc79aa7743ec30e727af401db
wrmsr <timwilloney@gmail.com>
2024-12-17 11:59:10 -0800

    2024-12-17T19:59:10Z

--- omlish/sql/queries/tests/test_render.py
+++ omlish/sql/queries/tests/test_render.py
@@ -8,18 +8,18 @@ def test_render():
             [
                 1,
             ],
-            'foo',
+            Q.n.foo,
             Q.and_(
-                Q.eq(Q.i('foo'), 1),
-                Q.ne(Q.i('bar'), Q.add(Q.i('baz'), 2)),
+                Q.eq(Q.i.foo, 1),
+                Q.ne(Q.i.bar, Q.add(Q.i.baz, 2)),
             ),
         ),
         Q.insert(
             ['foo', 'bar'],
-            'some_table',
+            Q.n.some_schema.some_table,
             [123, 'abc'],
         ),
-        Q.select([Q.p('foo'), Q.p(), Q.p('bar'), Q.p('foo')]),
+        Q.select([Q.p.foo, Q.p(), Q.p.bar, Q.p.foo]),
     ]:
         print(query)
         print(render(query))
