f6cd2c42a409762a515e5338cc1b6702fa18562f
wrmsr <timwilloney@gmail.com>
2024-12-17 14:38:01 -0800

    2024-12-17T22:38:01Z

---
+++ ominfra/manage/deploy/paths/manager.py
@@ -0,0 +1,36 @@
+# ruff: noqa: UP006 UP007
+import typing as ta
+
+from omlish.lite.cached import cached_nullary
+
+from ..types import DeployHome
+from .owners import DeployPathOwner
+from .owners import DeployPathOwners
+from .paths import DeployPath
+from .paths import DeployPathError
+
+
+class DeployPathsManager:
+    def __init__(
+            self,
+            *,
+            deploy_home: ta.Optional[DeployHome],
+            deploy_path_owners: DeployPathOwners,
+    ) -> None:
+        super().__init__()
+
+        self._deploy_home = deploy_home
+        self._deploy_path_owners = deploy_path_owners
+
+    @cached_nullary
+    def owners_by_path(self) -> ta.Mapping[DeployPath, DeployPathOwner]:
+        dct: ta.Dict[DeployPath, DeployPathOwner] = {}
+        for o in self._deploy_path_owners:
+            for p in o.get_owned_deploy_paths():
+                if p in dct:
+                    raise DeployPathError(f'Duplicate deploy path owner: {p}')
+                dct[p] = o
+        return dct
+
+    def validate_deploy_paths(self) -> None:
+        self.owners_by_path()
