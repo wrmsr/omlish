ca47c969d7cb56ebe011fa05ab4b9bdf35a7699e
wrmsr <timwilloney@gmail.com>
2024-12-16 16:51:36 -0800

    2024-12-17T00:51:36Z

--- ominfra/manage/deploy/conf.py
+++ ominfra/manage/deploy/conf.py
@@ -1,4 +1,20 @@
 # ruff: noqa: UP006 UP007
+"""
+TODO:
+ - post-deploy: remove any dir_links not present in new spec
+  - * only if succeeded * - otherwise, remove any dir_links present in new spec but not previously present?
+   - no such thing as 'previously present'.. build a 'deploy state' and pass it back?
+ - ** whole thing can be atomic **
+  - 1) new atomic temp dir
+  - 2) for each subdir not needing modification, hardlink into temp dir
+  - 3) for each subdir needing modification, new subdir, hardlink all files not needing modification
+  - 4) write (or if deleting, omit) new files
+  - 5) swap top level
+ - ** whole deploy can be atomic(-ish) - do this for everything **
+  - just a '/deploy/current' dir
+  - some things (venvs) cannot be moved, thus the /deploy/venvs dir
+  - ** ensure (enforce) equivalent relpath nesting
+"""
 import os.path
 import typing as ta
 
@@ -7,6 +23,7 @@ from omlish.os.paths import is_path_in_dir
 
 from .paths import SingleDirDeployPathOwner
 from .specs import DeployConfSpec
+from .types import DeployAppTag
 from .types import DeployHome
 
 
@@ -25,6 +42,8 @@ class DeployConfManager(SingleDirDeployPathOwner):
             self,
             spec: DeployConfSpec,
             conf_dir: str,
+            app_tag: DeployAppTag,
+            link_dir: str,
     ) -> None:
         conf_dir = os.path.abspath(conf_dir)
         os.makedirs(conf_dir)
@@ -39,4 +58,10 @@ class DeployConfManager(SingleDirDeployPathOwner):
                 f.write(cf.body)
 
         for dl in spec.dir_links:
-            raise NotImplementedError
+            cdd = os.path.join(self._make_dir(), dl)
+            check.arg(is_path_in_dir(self._make_dir(), cdd))
+            os.makedirs(cdd, exist_ok=True)
+
+            link_src = os.path.join(link_dir, dl)
+            link_dst = os.path.join(cdd, app_tag.app)
+            os.symlink(link_src, link_dst)
