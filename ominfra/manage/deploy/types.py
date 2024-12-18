548764f4dd30bd39a2836b1973292261e218ea1b
wrmsr <timwilloney@gmail.com>
2024-12-18 13:06:11 -0800

    2024-12-18T21:06:10Z

--- ominfra/manage/deploy/types.py
+++ ominfra/manage/deploy/types.py
@@ -1,12 +1,4 @@
-# ruff: noqa: UP006 UP007
-import dataclasses as dc
 import typing as ta
-
-from omlish.lite.check import check
-
-
-DeployPathKind = ta.Literal['dir', 'file']  # ta.TypeAlias
-DeployPathPlaceholder = ta.Literal['app', 'tag', 'conf', 'rev']  # ta.TypeAlias
 
 
 ##
@@ -15,38 +7,7 @@ DeployPathPlaceholder = ta.Literal['app', 'tag', 'conf', 'rev']  # ta.TypeAlias
 DeployHome = ta.NewType('DeployHome', str)
 
 DeployApp = ta.NewType('DeployApp', str)
-DeployTag = ta.NewType('DeployTag', str)
-DeployRev = ta.NewType('DeployRev', str)
+DeployConf = ta.NewType('DeployConf', str)
+
 DeployKey = ta.NewType('DeployKey', str)
-
-
-##
-
-
-# Sync with DeployPathPlaceholder
-DEPLOY_PATH_PLACEHOLDERS: ta.FrozenSet[str] = frozenset([
-    'app',
-    'tag',
-    'conf',
-    'rev',
-])
-
-
-##
-
-
-@dc.dataclass(frozen=True)
-class DeployAppTag:
-    app: DeployApp
-    tag: DeployTag
-
-    def __post_init__(self) -> None:
-        for s in [self.app, self.tag]:
-            check.non_empty_str(s)
-            check.equal(s, s.strip())
-
-    def placeholders(self) -> ta.Mapping[DeployPathPlaceholder, str]:
-        return {
-            'app': self.app,
-            'tag': self.tag,
-        }
+DeployRev = ta.NewType('DeployRev', str)
