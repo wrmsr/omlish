77e60b82be0908346a9e1e8636d3c465233648c0
wrmsr <timwilloney@gmail.com>
2024-12-18 13:53:39 -0800

    2024-12-18T21:53:39Z

--- ominfra/manage/deploy/paths/types.py
+++ ominfra/manage/deploy/paths/types.py
@@ -3,11 +3,3 @@ import typing as ta
 
 
 DeployPathKind = ta.Literal['dir', 'file']  # ta.TypeAlias
-
-DeployPathPlaceholderMap = ta.Mapping['DeployPathPlaceholder', str]  # ta.TypeAlias
-
-
-##
-
-
-DeployPathPlaceholder = ta.NewType('DeployPathPlaceholder', str)
