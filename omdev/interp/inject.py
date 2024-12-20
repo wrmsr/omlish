3d83303ac7f5036556f740b717af6ae55b7f47ea
wrmsr <timwilloney@gmail.com>
2024-12-19 16:08:45 -0800

    2024-12-20T00:08:45Z

--- omdev/interp/inject.py
+++ omdev/interp/inject.py
@@ -1,6 +1,7 @@
 # ruff: noqa: UP006 UP007
 import typing as ta
 
+from omlish.lite.inject import Injector
 from omlish.lite.inject import InjectorBindingOrBindings
 from omlish.lite.inject import InjectorBindings
 from omlish.lite.inject import inj
@@ -8,6 +9,8 @@ from omlish.lite.inject import inj
 from .inspect import InterpInspector
 from .providers.inject import bind_interp_providers
 from .pyenv.inject import bind_interp_pyenv
+from .resolvers import InterpResolver
+from .resolvers import InterpResolverProviders
 from .uv.inject import bind_interp_uv
 
 
@@ -21,5 +24,26 @@ def bind_interp() -> InjectorBindings:
 
         inj.bind(InterpInspector, singleton=True),
     ]
+
+    #
+
+    def provide_interp_resolver_providers(injector: Injector) -> InterpResolverProviders:
+        from .providers.running import RunningInterpProvider
+        from .providers.system import SystemInterpProvider
+        from .pyenv.pyenv import PyenvInterpProvider
+
+        return InterpResolverProviders([((p := injector[c]).name, p) for c in [
+            PyenvInterpProvider,
+            RunningInterpProvider,
+            SystemInterpProvider,
+        ]])
+
+    lst.append(inj.bind(provide_interp_resolver_providers, singleton=True))
+
+    lst.extend([
+        inj.bind(InterpResolver, singleton=True),
+    ])
+
+    #
 
     return inj.as_bindings(*lst)
