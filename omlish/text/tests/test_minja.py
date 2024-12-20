13eebcff087f660bd7b4990652ceed9783291306
wrmsr <timwilloney@gmail.com>
2024-12-20 12:00:20 -0800

    2024-12-20T20:00:20Z

--- omlish/text/tests/test_minja.py
+++ omlish/text/tests/test_minja.py
@@ -1,13 +1,13 @@
 # ruff: noqa: UP006 UP007
-import jinja2
+import typing as ta
+import unittest
 
 from ..minja import render_minja_template
 
 
-def test_minja() -> None:
-    for tmpl, ctx in [
-        (
-            """\
+TEMPLATE_TEST_CASES = [
+    (
+        """\
 Hello {{ name }}!
 {# This is a comment and should not appear #}
 {% for item in items %}
@@ -15,15 +15,15 @@ Hello {{ name }}!
 {% endfor %}
 Some function call: {{ len(items) }} items total.
 """,
-            {
-                'name': 'World',
-                'items': ['apple', 'banana', 'cherry'],
-                'len': len,
-            },
-        ),
+        {
+            'name': 'World',
+            'items': ['apple', 'banana', 'cherry'],
+            'len': len,
+        },
+    ),
 
-        (
-            """\
+    (
+        """\
 Hello {{ name }}!
 {# This is a comment and should not appear #}
 {% for item in items %}
@@ -38,15 +38,15 @@ it's something else!
 {% endfor %}
 Some function call: {{ len(items) }} items total.
 """,
-            {
-                'name': 'World',
-                'items': ['apple', 'banana', 'cherry'],
-                'len': len,
-            },
-        ),
+        {
+            'name': 'World',
+            'items': ['apple', 'banana', 'cherry'],
+            'len': len,
+        },
+    ),
 
-        (
-            """\
+    (
+        """\
 Hello {{ name }}!
 {#- This is a comment and should not appear -#}
 {% for item in items %}
@@ -61,20 +61,32 @@ it's something else!
 {% endfor %}
 Some function call: {{ len(items) }} items total.
 """,
-            {
-                'name': 'World',
-                'items': ['apple', 'banana', 'cherry'],
-                'len': len,
-            },
-        ),
+        {
+            'name': 'World',
+            'items': ['apple', 'banana', 'cherry'],
+            'len': len,
+        },
+    ),
 
-    ]:
-        print('=== jinja ===')
-        print(jinja2.Template(tmpl).render(**ctx))
-        print('=== end jinja ===')
-        print()
+]
 
-        print('=== minja ===')
-        print(render_minja_template(tmpl, **ctx))
-        print('=== end minja ===')
-        print()
+
+class TestMinja(unittest.TestCase):
+    def test_minja(self) -> None:
+        jinja2: ta.Any
+        try:
+            import jinja2
+        except ImportError:
+            jinja2 = None
+
+        for tmpl, ctx in TEMPLATE_TEST_CASES:
+            if jinja2 is not None:
+                print('=== jinja ===')
+                print(jinja2.Template(tmpl).render(**ctx))
+                print('=== end jinja ===')
+                print()
+
+            print('=== minja ===')
+            print(render_minja_template(tmpl, **ctx))
+            print('=== end minja ===')
+            print()
