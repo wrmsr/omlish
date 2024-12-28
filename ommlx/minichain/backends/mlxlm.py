761cfc3aea0341b9ef3580fe160f52b3480998da
wrmsr <timwilloney@gmail.com>
2024-12-28 13:55:13 -0500

    2024-12-28T18:55:13Z

---
+++ ommlx/minichain/backends/mlxlm.py
@@ -0,0 +1,98 @@
+import contextlib
+import typing as ta
+
+from omlish import check
+from omlish import lang
+
+from ..chat import AiChoice
+from ..chat import AiMessage
+from ..chat import ChatModel
+from ..chat import ChatRequest
+from ..chat import ChatResponse
+from ..chat import Message
+from ..chat import SystemMessage
+from ..chat import UserMessage
+
+
+if ta.TYPE_CHECKING:
+    import mlx_lm
+else:
+    mlx_lm = lang.proxy_import('mlx_lm')
+
+
+class MlxlmChatModel(ChatModel):
+    DEFAULT_MODEL = (
+        # 'mlx-community/DeepSeek-Coder-V2-Lite-Instruct-8bit'
+        'mlx-community/Llama-3.3-70B-Instruct-4bit'
+        # 'mlx-community/Llama-3.3-70B-Instruct-6bit'
+        # 'mlx-community/Llama-3.3-70B-Instruct-8bit'
+        # 'mlx-community/Mixtral-8x7B-Instruct-v0.1'
+        # 'mlx-community/QwQ-32B-Preview-8bit'
+        # 'mlx-community/QwQ-32B-Preview-bf16'
+        # 'mlx-community/Qwen2.5-32B-Instruct-8bit'
+        # 'mlx-community/Qwen2.5-Coder-32B-Instruct-8bit'
+        # 'mlx-community/mamba-2.8b-hf-f16'
+    )
+    def __init__(
+            self,
+            model: str = DEFAULT_MODEL,
+    ) -> None:
+        super().__init__()
+        self._model = model
+
+    ROLES_MAP: ta.ClassVar[ta.Mapping[type[Message], str]] = {
+        SystemMessage: 'system',
+        UserMessage: 'user',
+        AiMessage: 'assistant',
+    }
+
+    def _get_msg_content(self, m: Message) -> str | None:
+        if isinstance(m, (SystemMessage, AiMessage)):
+            return m.s
+
+        elif isinstance(m, UserMessage):
+            return check.isinstance(m.c, str)
+
+        else:
+            raise TypeError(m)
+
+    def invoke(self, request: ChatRequest) -> ChatResponse:
+        model, tokenizer = mlx_lm.load(self._model)
+
+        if not (
+                hasattr(tokenizer, 'apply_chat_template') and
+                tokenizer.chat_template is not None
+        ):
+            raise RuntimeError(tokenizer)
+
+        messages = [{'role': 'user', 'content': request.v}]
+
+        prompt = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+
+        output = llm.create_chat_completion(
+            messages=[  # noqa
+                dict(  # type: ignore
+                    role=self.ROLES_MAP[type(m)],
+                    content=self._get_msg_content(m),
+                )
+                for m in request.v
+            ],
+            max_tokens=1024,
+            # stop=['\n'],
+        )
+
+        response = mlx_lm.generate(
+            model,
+            tokenizer,
+            prompt=prompt,
+            verbose=True,
+        )
+
+        return ChatResponse(v=[
+            AiChoice(AiMessage(c['message']['content']))  # noqa
+            for c in output['choices']  # type: ignore
+        ])
---
+++ ommlx/minichain/backends/tests/test_mlxlm.py
@@ -0,0 +1,35 @@
+import pytest
+
+from omlish import check
+from omlish.formats import json
+from omlish.secrets.tests.harness import HarnessSecrets
+from omlish.testing import pytest as ptu
+
+from ...backends.llamacpp import LlamacppPromptModel
+from ...backends.openai import OpenaiChatModel
+from ...backends.mlxlm import MlxlmChatModel
+from ...chat import Message
+from ...chat import SystemMessage
+from ...chat import Tool
+from ...chat import ToolExecResultMessage
+from ...chat import ToolParam
+from ...chat import ToolSpec
+from ...chat import UserMessage
+from ...generative import MaxTokens
+from ...generative import Temperature
+from ...prompts import PromptRequest
+
+
+@pytest.mark.not_docker_guest
+@ptu.skip.if_cant_import('mlx_lm')
+def test_mlxlm():
+    llm = MlxlmChatModel('mlx-community/mamba-2.8b-hf-f16')
+
+    resp = llm([UserMessage('Is water dry?')])
+    print(resp)
+    assert resp.v
+
+    resp = llm('Is water dry?')
+    print(resp)
+    assert resp.v
+
