10e3708c2d12284469fbc0cf641cf3abf1017130
wrmsr <timwilloney@gmail.com>
2025-01-21 11:08:36 -0800

    2025-01-21T19:08:36Z

---
+++ omlish/asyncs/ioproxy/all.py
@@ -0,0 +1,33 @@
+# ruff: noqa: I001
+from .io import (  # noqa
+    BufferedIOBase_AsyncIoProxy as BufferedIOBase,
+    BufferedRandom_AsyncIoProxy as BufferedRandom,
+    BufferedReader_AsyncIoProxy as BufferedReader,
+    BufferedRWPair_AsyncIoProxy as BufferedRWPair,
+    BufferedWriter_AsyncIoProxy as BufferedWriter,
+    BytesIO_AsyncIoProxy as BytesIO,
+    FileIO_AsyncIoProxy as FileIO,
+    IOBase_AsyncIoProxy as IOBase,
+    RawIOBase_AsyncIoProxy as RawIOBase,
+    StringIO_AsyncIoProxy as StringIO,
+    TextIOBase_AsyncIoProxy as TextIOBase,
+    TextIOWrapper_AsyncIoProxy as TextIOWrapper,
+)
+
+from .proxier import (  # noqa
+    AsyncIoProxier as Proxier,
+)
+
+from .proxy import (  # noqa
+    AsyncIoProxy as Proxy,
+    AsyncIoProxyRunner as Runer,
+    AsyncIoProxyTarget as Target,
+    async_io_proxy_cls_for as proxy_cls_for,
+    async_io_proxy_fn as proxy_fn,
+)
+
+from .typing import (  # noqa
+    TypingBinaryIO_AsyncIoProxy as TypingBinaryIO,
+    TypingIO_AsyncIoProxy as TypingIO,
+    TypingTextIO_AsyncIoProxy as TypingTextIO,
+)
