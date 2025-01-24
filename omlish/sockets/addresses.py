6498eaf8191800fee18a8756e917b0d539d56b75
wrmsr <timwilloney@gmail.com>
2025-01-24 11:05:30 -0800

    2025-01-24T19:05:30Z

--- omlish/sockets/addresses.py
+++ omlish/sockets/addresses.py
@@ -9,8 +9,78 @@ import dataclasses as dc
 import socket
 import typing as ta
 
+from ..lite.check import check
+
 
 SocketAddress = ta.Any
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class SocketAddressFamily:
+    i: int
+    name: str
+
+    def __post_init__(self) -> None:
+        check.isinstance(self.i, int)
+        check.non_empty_str(self.name)
+
+
+class _SocketAddressFamilies:
+    def __init__(self, *sfs: SocketAddressFamily) -> None:
+        super().__init__()
+
+        self._sfs = sfs
+
+        by_i: ta.Dict[int, SocketAddressFamily] = {}
+        by_name: ta.Dict[str, SocketAddressFamily] = {}
+        for sf in sfs:
+            check.not_in(sf.i, by_i)
+            check.not_in(sf.name, by_name)
+            by_i[sf.i] = sf
+            by_name[sf.name] = sf
+        self._by_i = by_i
+        self._by_name = by_name
+
+    def __iter__(self) -> ta.Iterator[SocketAddressFamily]:
+        return iter(self._sfs)
+
+    def __len__(self) -> int:
+        return len(self._sfs)
+
+    def __getitem__(self, key: ta.Union[int, str]) -> SocketAddressFamily:
+        if isinstance(key, int):
+            return self._by_i[key]
+        elif isinstance(key, str):
+            return self._by_name[key]
+        else:
+            raise TypeError(key)
+
+    def get(self, key: ta.Union[int, str]) -> ta.Optional[SocketAddressFamily]:
+        try:
+            return self[key]
+        except KeyError:
+            return None
+
+
+#
+
+
+_SOCKET_ADDRESS_FAMILIES: ta.Sequence[SocketAddressFamily] = [
+    SocketAddressFamily(
+        i=i,
+        name=name,
+    )
+    for name in dir(socket)
+    if name.startswith('AF_')
+    for i in [getattr(socket, name)]
+    if isinstance(i, int)
+]
+
+
+SOCKET_ADDRESS_FAMILIES = _SocketAddressFamilies(*_SOCKET_ADDRESS_FAMILIES)
 
 
 ##
