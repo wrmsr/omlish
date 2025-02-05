0aada59cb57032b0d73714fabe0d4f3ad298108a
wrmsr <timwilloney@gmail.com>
2025-02-05 13:01:00 -0800

    2025-02-05T21:01:00Z

--- omdev/ci/tests/test_ci.py
+++ omdev/ci/tests/test_ci.py
@@ -136,6 +136,9 @@ class CiHarness(AsyncExitStacked):
 
 class TestCi(unittest.IsolatedAsyncioTestCase):
     async def test_ci(self):
+        if not shutil.which('docker'):
+            self.skipTest('no docker')
+
         async with CiHarness() as ci_harness:
             async with ci_harness.make_ci() as ci:
                 await ci.run()
