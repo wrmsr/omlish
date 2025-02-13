4ee4b3b8a11ed0696df99888ca995740a17e85f7
wrmsr <timwilloney@gmail.com>
2025-02-13 11:40:46 -0800

    2025-02-13T19:40:46Z

--- omlish/os/pidfiles/manager.py
+++ omlish/os/pidfiles/manager.py
@@ -7,71 +7,38 @@ import typing as ta
 import weakref
 
 from ...lite.check import check
+from ..forkhooks import ForkHook
 from .pidfile import Pidfile
 
 
 ##
 
 
-class _PidfileManager:
+class _PidfileManager(ForkHook):
     """
     Manager for controlled inheritance of Pidfiles across forks in the presence of multiple threads. There is of course
     no safe or correct way to mix the use of fork and multiple active threads, and one should never write code which
     does so, but in the Real World one may still find oneself in such a situation outside of their control (such as when
     running under Pycharm's debugger which forces the use of forked multiprocessing).
-
-    Not implemented as an instantiated class as there is no way to unregister at_fork listeners, and because Pidfiles
-    may be pickled and there must be no possibility of accidentally unpickling and instantiating a new instance of the
-    manager.
     """
 
-    def __new__(cls, *args, **kwargs):  # noqa
-        raise TypeError
-
     _lock: ta.ClassVar[threading.Lock] = threading.Lock()
-    _installed: ta.ClassVar[bool] = False
     _pidfile_threads: ta.ClassVar[ta.MutableMapping[Pidfile, threading.Thread]] = weakref.WeakKeyDictionary()
+    _num_kills: ta.ClassVar[int] = 0
 
     @classmethod
-    def _before_fork(cls) -> None:
-        cls._lock.acquire()
-
-    @classmethod
-    def _after_fork_in_parent(cls) -> None:
-        cls._lock.release()
+    def num_kills(cls) -> int:
+        return cls._num_kills
 
     @classmethod
     def _after_fork_in_child(cls) -> None:
-        th = threading.current_thread()
-        for pf, pf_th in list(cls._pidfile_threads.items()):
-            if pf_th is not th:
-                pf.close()
-                del cls._pidfile_threads[pf]
-
-        cls._lock.release()
-
-    #
-
-    @classmethod
-    def _install(cls) -> None:
-        check.state(not cls._installed)
-
-        os.register_at_fork(
-            before=cls._before_fork,
-            after_in_parent=cls._after_fork_in_parent,
-            after_in_child=cls._after_fork_in_child,
-        )
-
-        cls._installed = True
-
-    @classmethod
-    def install(cls) -> bool:
         with cls._lock:
-            if cls._installed:
-                return False
-
-            cls._install()
-            return True
+            th = threading.current_thread()
+            for pf, pf_th in list(cls._pidfile_threads.items()):
+                if pf_th is not th:
+                    pf.close()
+                    del cls._pidfile_threads[pf]
+                    cls._num_kills += 1
 
     @classmethod
     @contextlib.contextmanager
