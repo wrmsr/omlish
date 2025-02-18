d8be494f274a622374f179325047f225341d8cd9
wrmsr <timwilloney@gmail.com>
2025-02-17 19:05:25 -0800

    Refactor Service class to inherit from Configurable and streamline configuration handling.

--- omlish/daemons/services.py
+++ omlish/daemons/services.py
@@ -1,9 +1,9 @@
 import abc
-import dataclasses as dc
 import typing as ta
 
 from .. import check
 from .. import lang
+from ..configs.classes import Configurable
 
 
 ServiceConfigT = ta.TypeVar('ServiceConfigT', bound='Service.Config')
@@ -12,30 +12,15 @@ ServiceConfigT = ta.TypeVar('ServiceConfigT', bound='Service.Config')
 ##
 
 
-class Service(lang.Abstract, ta.Generic[ServiceConfigT]):
-    @dc.dataclass(frozen=True, kw_only=True)
-    class Config:
-        service_cls: ta.ClassVar[type['Service']]
-
-    def __init_subclass__(cls, **kwargs: ta.Any) -> None:
-        super().__init_subclass__(**kwargs)
-
-        if not lang.is_abstract_class(cls):
-            check.in_('Config', cls.__dict__)
-            cfg_cls = check.issubclass(cls.Config, Service.Config)
-            check.not_in('service_cls', cfg_cls.__dict__)
-            cfg_cls.service_cls = cls
+class Service(Configurable[ServiceConfigT], lang.Abstract):
+    class Config(Configurable.Config):
+        pass
 
     #
 
-    def __init__(self, config: ServiceConfigT) -> None:
-        super().__init__()
-
-        self._config: ServiceConfigT = check.isinstance(config, self.Config)  # type: ignore[assignment]
-
     @classmethod
     def from_config(cls, config: Config) -> 'Service':
-        return config.service_cls(config)
+        return check.isinstance(config.configurable_cls(config), cls)
 
     #
 
