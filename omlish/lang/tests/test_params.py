363fadc840f50b94bf13877302c253c3a231bd84
wrmsr <timwilloney@gmail.com>
2025-03-13 15:13:43 -0700

    Add comprehensive tests for parameter specifications in function signatures
    Introduce multiple test cases to validate the behavior of `ParamSpec` with various parameter types, including positional-only, keyword-only, and mixed parameters. Each test checks the expected output and the correct handling of parameter separators.

--- omlish/lang/tests/test_params.py
+++ omlish/lang/tests/test_params.py
@@ -1,12 +1,95 @@
 import inspect
 
+from ..params import ArgsParam
+from ..params import KwargsParam
+from ..params import KwOnlyParam
+from ..params import ParamSeparator
 from ..params import ParamSpec
+from ..params import PosOnlyParam
+from ..params import ValueParam
 
 
-def test_params() -> None:
+def test_params_simple() -> None:
     def foo(a, b, *c, **kw):
         pass
 
     ps = ParamSpec.of_signature(inspect.signature(foo))
-    print(ps)
-    print(ps.with_seps)
+
+    assert ps == ParamSpec(
+        ValueParam('a'),
+        ValueParam('b'),
+        ArgsParam('c'),
+        KwargsParam('kw'),
+    )
+
+    assert list(ps.with_seps) == [
+        ValueParam('a'),
+        ValueParam('b'),
+        ArgsParam('c'),
+        KwargsParam('kw'),
+    ]
+
+
+def test_params_pos_only() -> None:
+    def foo(a, /, b, *c, **kw):
+        pass
+
+    ps = ParamSpec.of_signature(inspect.signature(foo))
+
+    assert ps == ParamSpec(
+        PosOnlyParam('a'),
+        ValueParam('b'),
+        ArgsParam('c'),
+        KwargsParam('kw'),
+    )
+
+    assert list(ps.with_seps) == [
+        PosOnlyParam('a'),
+        ParamSeparator.POS_ONLY,
+        ValueParam('b'),
+        ArgsParam('c'),
+        KwargsParam('kw'),
+    ]
+
+
+def test_params_kw_only() -> None:
+    def foo(a, *, b, **kw):
+        pass
+
+    ps = ParamSpec.of_signature(inspect.signature(foo))
+
+    assert ps == ParamSpec(
+        ValueParam('a'),
+        KwOnlyParam('b'),
+        KwargsParam('kw'),
+    )
+
+    assert list(ps.with_seps) == [
+        ValueParam('a'),
+        ParamSeparator.KW_ONLY,
+        KwOnlyParam('b'),
+        KwargsParam('kw'),
+    ]
+
+
+def test_params_pos_only_and_kw_only() -> None:
+    def foo(a, /, b, *, c, **kw):
+        pass
+
+    ps = ParamSpec.of_signature(inspect.signature(foo))
+
+    assert ps == ParamSpec(
+        PosOnlyParam('a'),
+        ValueParam('b'),
+        KwOnlyParam('c'),
+        KwargsParam('kw'),
+    )
+
+    assert list(ps.with_seps) == [
+        PosOnlyParam('a'),
+        ParamSeparator.POS_ONLY,
+        ValueParam('b'),
+        ParamSeparator.KW_ONLY,
+        KwOnlyParam('c'),
+        KwargsParam('kw'),
+    ]
