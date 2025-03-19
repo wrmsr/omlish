753f9ffe894c2954a22121f33134b2d50c666da2
wrmsr <timwilloney@gmail.com>
2025-03-19 10:54:54 -0700

    Refactor typed lambda handling and improve pickling support
    - Update the way typed lambda functions are called and reduced, enhancing clarity and maintainability.
    - Replace direct access to function attributes with local variables for better performance and readability.
    - Modify pickling methods to use private `_dumps` and `_loads` for improved compatibility.

--- x/typing2.py
+++ x/typing2.py
@@ -17,6 +17,53 @@ In [6]: %timeit exec('def foo(*a, x=0, **k): return (a, x, k)')
 
 In [7]: %timeit exec('\n'.join(['class C:', '  def foo(self, *a, x=0, **k): return (a, x, k)']))
 12.9 μs ± 48.9 ns per loop (mean ± std. dev. of 7 runs, 100,000 loops each)
+
+==
+
+  0: \x80 PROTO      4
+  2: \x95 FRAME      121
+ 11: \x8c SHORT_BINUNICODE 'builtins'
+ 21: \x94 MEMOIZE    (as 0)
+ 22: \x8c SHORT_BINUNICODE 'getattr'
+ 31: \x94 MEMOIZE    (as 1)
+ 32: \x93 STACK_GLOBAL
+ 33: \x94 MEMOIZE    (as 2)
+ 34: \x8c SHORT_BINUNICODE '__main__'
+ 44: \x94 MEMOIZE    (as 3)
+ 45: \x8c SHORT_BINUNICODE '_make_typed_lambda_cls_inst'
+ 74: \x94 MEMOIZE    (as 4)
+ 75: \x93 STACK_GLOBAL
+ 76: \x94 MEMOIZE    (as 5)
+ 77: h    BINGET     3
+ 79: \x8c SHORT_BINUNICODE '_foo'
+ 85: \x94 MEMOIZE    (as 6)
+ 86: \x93 STACK_GLOBAL
+ 87: \x94 MEMOIZE    (as 7)
+ 88: }    EMPTY_DICT
+ 89: \x94 MEMOIZE    (as 8)
+ 90: (    MARK
+ 91: \x8c     SHORT_BINUNICODE 'x'
+ 94: \x94     MEMOIZE    (as 9)
+ 95: \x8c     SHORT_BINUNICODE 'builtins'
+105: \x94     MEMOIZE    (as 10)
+106: \x8c     SHORT_BINUNICODE 'int'
+111: \x94     MEMOIZE    (as 11)
+112: \x93     STACK_GLOBAL
+113: \x94     MEMOIZE    (as 12)
+114: \x8c     SHORT_BINUNICODE 'y'
+117: \x94     MEMOIZE    (as 13)
+118: h        BINGET     12
+120: u        SETITEMS   (MARK at 90)
+121: \x86 TUPLE2
+122: \x94 MEMOIZE    (as 14)
+123: R    REDUCE
+124: \x94 MEMOIZE    (as 15)
+125: h    BINGET     6
+127: \x86 TUPLE2
+128: \x94 MEMOIZE    (as 16)
+129: R    REDUCE
+130: \x94 MEMOIZE    (as 17)
+131: .    STOP
 """
 import functools
 import inspect
@@ -115,11 +162,14 @@ class _TypedLambda:
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__()
 
-        cls.__call__ = _make_typed_lambda_fn(cls.__dict__['fn'], _TYPED_LAMBDA_SELF_ARG, **cls.kw)
+        fn = cls.__dict__['fn']
+        cls.__call__ = _make_typed_lambda_fn(fn, _TYPED_LAMBDA_SELF_ARG, **cls.kw)
+        _update_wrapper_no_anns(cls.__call__, fn)
 
     def __reduce__(self):
         cls = self.__class__
-        return (_make_typed_lambda_cls_inst, (cls.__dict__['fn'], cls.kw))
+        fn = cls.__dict__['fn']
+        return (_make_typed_lambda_cls_inst, (fn, cls.kw))
 
 
 def _make_typed_lambda_cls(fn, kw):
@@ -230,8 +280,8 @@ def _main() -> None:
     assert ta.get_type_hints(l) == {'x': int, 'y': int}
 
     import pickle
-    lp = pickle.dumps(l)
-    l2 = pickle.loads(lp)
+    lp = pickle._dumps(l)
+    l2 = pickle._loads(lp)
     assert l2(x=3, y=4) == 7
     assert ta.get_type_hints(l2) == {'x': int, 'y': int}
 
