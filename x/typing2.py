ce8b27dbe0d0568b6d00dbcd2c9e7dd6872d5b38
wrmsr <timwilloney@gmail.com>
2025-03-19 11:03:36 -0700

    Refactor _TypedLambda to improve method unpickling handling
    Add logic to ensure that the method unpickling machinery correctly retrieves the proxy by setting the method's name attribute after updating the wrapper. This change addresses potential issues with name conflicts during unpickling.

--- x/typing2.py
+++ x/typing2.py
@@ -17,53 +17,6 @@ In [6]: %timeit exec('def foo(*a, x=0, **k): return (a, x, k)')
 
 In [7]: %timeit exec('\n'.join(['class C:', '  def foo(self, *a, x=0, **k): return (a, x, k)']))
 12.9 μs ± 48.9 ns per loop (mean ± std. dev. of 7 runs, 100,000 loops each)
-
-==
-
-  0: \x80 PROTO      4
-  2: \x95 FRAME      121
- 11: \x8c SHORT_BINUNICODE 'builtins'
- 21: \x94 MEMOIZE    (as 0)
- 22: \x8c SHORT_BINUNICODE 'getattr'
- 31: \x94 MEMOIZE    (as 1)
- 32: \x93 STACK_GLOBAL
- 33: \x94 MEMOIZE    (as 2)
- 34: \x8c SHORT_BINUNICODE '__main__'
- 44: \x94 MEMOIZE    (as 3)
- 45: \x8c SHORT_BINUNICODE '_make_typed_lambda_cls_inst'
- 74: \x94 MEMOIZE    (as 4)
- 75: \x93 STACK_GLOBAL
- 76: \x94 MEMOIZE    (as 5)
- 77: h    BINGET     3
- 79: \x8c SHORT_BINUNICODE '_foo'
- 85: \x94 MEMOIZE    (as 6)
- 86: \x93 STACK_GLOBAL
- 87: \x94 MEMOIZE    (as 7)
- 88: }    EMPTY_DICT
- 89: \x94 MEMOIZE    (as 8)
- 90: (    MARK
- 91: \x8c     SHORT_BINUNICODE 'x'
- 94: \x94     MEMOIZE    (as 9)
- 95: \x8c     SHORT_BINUNICODE 'builtins'
-105: \x94     MEMOIZE    (as 10)
-106: \x8c     SHORT_BINUNICODE 'int'
-111: \x94     MEMOIZE    (as 11)
-112: \x93     STACK_GLOBAL
-113: \x94     MEMOIZE    (as 12)
-114: \x8c     SHORT_BINUNICODE 'y'
-117: \x94     MEMOIZE    (as 13)
-118: h        BINGET     12
-120: u        SETITEMS   (MARK at 90)
-121: \x86 TUPLE2
-122: \x94 MEMOIZE    (as 14)
-123: R    REDUCE
-124: \x94 MEMOIZE    (as 15)
-125: h    BINGET     6
-127: \x86 TUPLE2
-128: \x94 MEMOIZE    (as 16)
-129: R    REDUCE
-130: \x94 MEMOIZE    (as 17)
-131: .    STOP
 """
 import functools
 import inspect
@@ -165,6 +118,11 @@ class _TypedLambda:
         fn = cls.__dict__['fn']
         cls.__call__ = _make_typed_lambda_fn(fn, _TYPED_LAMBDA_SELF_ARG, **cls.kw)
         _update_wrapper_no_anns(cls.__call__, fn)
+
+        # update_wrapper sets __call__.__name__, and method unpickling machinery getattr's by __name__ after
+        # instantiating __self__, so that needs to return the proxy. this might blow up with bad names, but we'll burn
+        # that bridge if we come to it.
+        setattr(cls, cls.__call__.__name__, cls.__call__)
 
     def __reduce__(self):
         cls = self.__class__
