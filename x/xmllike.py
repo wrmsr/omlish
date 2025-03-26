f0b74ddd789901ce0ca49e17f70d7f36d36511de
wrmsr <timwilloney@gmail.com>
2025-03-25 20:10:55 -0700

    Add XML-like tag parsing logic to XmlLikeParser
    Introduce methods to handle opening and closing tags, utilizing a character input validation function. Update main function to parse nested tags.

--- x/xmllike.py
+++ x/xmllike.py
@@ -1,3 +1,4 @@
+import io
 import typing as ta
 
 from omlish import lang
@@ -17,14 +18,45 @@ class XmlLikeParser(GenMachine[str, 'XmlLikeParser.Output']):
     def __init__(self) -> None:
         super().__init__(self._do_main())
 
+        self._stack: list[dict[str, str]] = []
+
+    def _char_in(self, c: str) -> str:
+        if c and len(c) != 1:
+            raise ValueError(c)
+
+        return c
+
     def _do_main(self):
         while True:
-            c = yield None  # noqa
+            c = self._char_in((yield None))  # noqa
 
-            if c and len(c) != 1:
-                raise ValueError(c)
+            if c == '<':
+                c = self._char_in((yield None))  # noqa
 
-            yield [self.IdleOutput()]
+                if c == '/':
+                    return self._do_tag('close')
+                else:
+                    return self._do_tag('open', c)
+
+    def _do_tag(
+            self,
+            mode: ta.Literal['open', 'close'],
+            prefix: str = '',
+    ):
+        sb = io.StringIO()
+        if prefix:
+            sb.write(prefix)
+
+        while True:
+            c = self._char_in((yield None))  # noqa
+
+            if c == '>':
+                break
+
+            sb.write(c)
+
+        t = sb.getvalue()
+        return self._do_main()
 
 
 ##
@@ -32,7 +64,7 @@ class XmlLikeParser(GenMachine[str, 'XmlLikeParser.Output']):
 
 def _main() -> None:
     p = XmlLikeParser()
-    for c in 'abcd<tool>efg</tool>':
+    for c in 'abcd<tools><tool>efg</tool><tool>hij</tool></tools>':
         for o in p(c):
             print(o)
 
