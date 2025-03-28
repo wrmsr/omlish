b6c48d028db613b64a1ad8a25503504ed2d17612
wrmsr <timwilloney@gmail.com>
2025-03-28 15:24:06 -0700

    Add KeyTransformedKv and ValueTransformedKv classes for key and value transformation
    Introduce two new classes, KeyTransformedKv and ValueTransformedKv, to enable transformation of keys and values in a key-value store. These classes utilize callable functions for transforming keys and values, enhancing flexibility in data handling.

---
+++ x/kv2/transformed.py
@@ -0,0 +1,89 @@
+import typing as ta
+
+from omlish import check
+
+from .interfaces import Kv
+from .shrinkwraps import ShrinkwrapFullKv
+
+
+K = ta.TypeVar('K')
+V = ta.TypeVar('V')
+
+# 'Above' the wrapper
+KA = ta.TypeVar('KA')
+VA = ta.TypeVar('VA')
+
+# 'Below' the wrapper
+KB = ta.TypeVar('KB')
+VB = ta.TypeVar('VB')
+
+
+##
+
+
+class KeyTransformedKv(ShrinkwrapFullKv[KA, V], ta.Generic[KA, KB, V]):
+    def __init__(
+            self,
+            u: Kv[KB, V],
+            *,
+            a_to_b: ta.Callable[[KA], KB] | None = None,
+            b_to_a: ta.Callable[[KB], KA] | None = None,
+    ) -> None:
+        super().__init__(u)
+
+        self._a_to_b = a_to_b
+        self._b_to_a = b_to_a
+
+    def __getitem__(self, k: KA, /) -> V:
+        fn = check.not_none(self._a_to_b)
+        return self._u[fn(k)]
+
+    def __len__(self) -> int:
+        return len(self._u)
+
+    def items(self) -> ta.Iterator[tuple[KA, V]]:
+        fn = check.not_none(self._b_to_a)
+        return ((fn(k), v) for k, v in self._u.items())
+
+    def __setitem__(self, k: KA, v: V, /) -> None:
+        fn = check.not_none(self._a_to_b)
+        self._u[fn(k)] = v
+
+    def __delitem__(self, k: KA, /) -> None:
+        fn = check.not_none(self._a_to_b)
+        del self._u[fn(k)]
+
+
+##
+
+
+class ValueTransformedKv(ShrinkwrapFullKv[K, VA], ta.Generic[K, VA, VB]):
+    def __init__(
+            self,
+            u: Kv[K, VB],
+            *,
+            a_to_b: ta.Callable[[VA], VB] | None = None,
+            b_to_a: ta.Callable[[VB], VA] | None = None,
+    ) -> None:
+        super().__init__(u)
+
+        self._a_to_b = a_to_b
+        self._b_to_a = b_to_a
+
+    def __getitem__(self, k: K, /) -> VA:
+        fn = check.not_none(self._b_to_a)
+        return fn(self._u[k])
+
+    def __len__(self) -> int:
+        return len(self._u)
+
+    def items(self) -> ta.Iterator[tuple[K, VA]]:
+        fn = check.not_none(self._b_to_a)
+        return ((k, fn(v)) for k, v in self._u.items())
+
+    def __setitem__(self, k: K, v: VA, /) -> None:
+        fn = check.not_none(self._a_to_b)
+        self._u[k] = fn(v)
+
+    def __delitem__(self, k: K, /) -> None:
+        del self._u[k]
