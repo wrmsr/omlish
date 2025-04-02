34f0b3737af9b28887d7dc9d9466167454144685
wrmsr <timwilloney@gmail.com>
2025-04-02 10:04:03 -0700

    Add reference links to llama3 model initialization for context

--- ommlx/tinygrad/models/llama3/__init__.py
+++ ommlx/tinygrad/models/llama3/__init__.py
@@ -1,3 +1,7 @@
+# https://github.com/tinygrad/tinygrad/blob/2d6d8b735506464367b0315f9a2f424e0d19f66e/examples/llama3.py
+# https://github.com/tinygrad/tinygrad/blob/2d6d8b735506464367b0315f9a2f424e0d19f66e/extra/models/llama.py
+
+
 from .llm import Llama3Llm  # noqa
 from .tokenization import Tokenizer  # noqa
 from .transformer import Transformer  # noqa
