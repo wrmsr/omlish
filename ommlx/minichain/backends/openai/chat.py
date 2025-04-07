7f3dd7cd183be8d561151fcf7025fa9957b541c8
wrmsr <timwilloney@gmail.com>
2025-04-07 14:37:53 -0700

    Refactor OpenaiChatService to use OpenaiChatRequestHandler
    Separate request handling logic into OpenaiChatRequestHandler for better modularity and maintainability. Add caching for option processing to improve performance.

--- ommlx/minichain/backends/openai/chat.py
+++ ommlx/minichain/backends/openai/chat.py
@@ -14,6 +14,7 @@ TODO:
 import os
 import typing as ta
 
+from omlish import cached
 from omlish import check
 from omlish import lang
 from omlish import typedvalues as tv
@@ -45,6 +46,126 @@ from .format import render_tool_spec
 ##
 
 
+class OpenaiChatRequestHandler:
+    def __init__(
+            self,
+            *,
+            request: ChatRequest,
+            model: str,
+    ) -> None:
+        super().__init__()
+
+        self._request = request
+        self._model = model
+
+    ROLES_MAP: ta.ClassVar[ta.Mapping[type[Message], str]] = {
+        SystemMessage: 'system',
+        UserMessage: 'user',
+        AiMessage: 'assistant',
+        ToolExecResultMessage: 'tool',
+    }
+
+    DEFAULT_OPTIONS: ta.ClassVar[tv.TypedValues[RequestOption]] = tv.TypedValues(
+        Temperature(0.),
+        MaxTokens(1024),
+    )
+
+    _OPTION_KWARG_NAMES_MAP: ta.Mapping[type[tv.ScalarTypedValue], str] = {
+        Temperature: 'temperature',
+        MaxTokens: 'max_tokens',
+    }
+
+    class _ProcessedOptions(ta.NamedTuple):
+        kwargs: dict[str, ta.Any]
+        tools_by_name: dict[str, ToolSpec]
+
+    @cached.function
+    def _process_options(self) -> _ProcessedOptions:
+        kwargs: dict = dict(
+            temperature=0,
+            max_tokens=1024,
+        )
+
+        tools_by_name: dict[str, ToolSpec] = {}
+
+        for opt in self._request.options:
+            if (
+                    isinstance(opt, tv.ScalarTypedValue) and
+                    (kwn := self._OPTION_KWARG_NAMES_MAP.get(type(opt))) is not None
+            ):
+                kwargs[kwn] = opt.v
+
+            elif isinstance(opt, Tool):
+                if opt.spec.name in tools_by_name:
+                    raise NameError(opt.spec.name)
+                tools_by_name[opt.spec.name] = opt.spec
+
+            else:
+                raise TypeError(opt)
+
+        return self._ProcessedOptions(
+            kwargs=kwargs,
+            tools_by_name=tools_by_name,
+        )
+
+    @cached.function
+    def raw_request(self) -> ta.Mapping[str, ta.Any]:
+        po = self._process_options()
+
+        tools = [
+            dict(
+                type='function',
+                function=render_tool_spec(ts),
+            )
+            for ts in po.tools_by_name.values()
+        ]
+
+        return dict(
+            model=self._model,
+            messages=[
+                build_request_message(m)
+                for m in self._request.chat
+            ],
+            top_p=1,
+            **lang.opt_kw(tools=tools),
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stream=False,
+            **po.kwargs,
+        )
+
+    def build_response(self, raw_response: ta.Mapping[str, ta.Any]) -> ChatResponse:
+        po = self._process_options()
+
+        return ChatResponse(
+            [
+                AiChoice(AiMessage(
+                    choice['message']['content'],
+                    tool_exec_requests=[
+                        ToolExecRequest(
+                            id=tc['id'],
+                            spec=po.tools_by_name[tc['function']['name']],
+                            args=tc['function']['arguments'],
+                        )
+                        for tc in choice['message'].get('tool_calls', [])
+                    ],
+                ))
+                for choice in raw_response['choices']
+            ],
+
+            outputs=tv.TypedValues(
+                *([TokenUsageOutput(TokenUsage(
+                    input=tu['prompt_tokens'],
+                    output=tu['completion_tokens'],
+                    total=tu['total_tokens'],
+                ))] if (tu := raw_response.get('usage')) is not None else []),
+            ),
+        )
+
+
+##
+
+
 # @omlish-manifest ommlx.minichain.backends.manifests.BackendManifest(name='openai', type='ChatService')
 class OpenaiChatService(
     ChatService[
@@ -59,18 +180,6 @@ class OpenaiChatService(
         # 'gpt-4o-mini'
     )
 
-    ROLES_MAP: ta.ClassVar[ta.Mapping[type[Message], str]] = {
-        SystemMessage: 'system',
-        UserMessage: 'user',
-        AiMessage: 'assistant',
-        ToolExecResultMessage: 'tool',
-    }
-
-    DEFAULT_OPTIONS: ta.ClassVar[tv.TypedValues[RequestOption]] = tv.TypedValues(
-        Temperature(0.),
-        MaxTokens(1024),
-    )
-
     def __init__(
             self,
             *,
@@ -82,59 +191,17 @@ class OpenaiChatService(
         self._model = model or self.DEFAULT_MODEL
         self._api_key = Secret.of(api_key if api_key is not None else os.environ['OPENAI_API_KEY'])
 
-    _OPTION_KWARG_NAMES_MAP: ta.Mapping[type[tv.ScalarTypedValue], str] = {
-        Temperature: 'temperature',
-        MaxTokens: 'max_tokens',
-    }
-
     def invoke(self, request: ChatRequest) -> ChatResponse:
         check.isinstance(request, ChatRequest)
 
-        kw: dict = dict(
-            temperature=0,
-            max_tokens=1024,
-        )
-
-        tools_by_name: dict[str, ToolSpec] = {}
-
-        for opt in request.options:
-            if (
-                    isinstance(opt, tv.ScalarTypedValue) and
-                    (kwn := self._OPTION_KWARG_NAMES_MAP.get(type(opt))) is not None
-            ):
-                kw[kwn] = opt.v
-
-            elif isinstance(opt, Tool):
-                if opt.spec.name in tools_by_name:
-                    raise NameError(opt.spec.name)
-                tools_by_name[opt.spec.name] = opt.spec
-
-            else:
-                raise TypeError(opt)
-
-        tools = [
-            dict(
-                type='function',
-                function=render_tool_spec(ts),
-            )
-            for ts in tools_by_name.values()
-        ]
-
-        raw_request = dict(
+        rh = OpenaiChatRequestHandler(
+            request=request,
             model=self._model,
-            messages=[
-                build_request_message(m)
-                for m in request.chat
-            ],
-            top_p=1,
-            **lang.opt_kw(tools=tools),
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-            stream=False,
-            **kw,
         )
 
-        raw_response = http.request(
+        raw_request = rh.raw_request()
+
+        http_response = http.request(
             'https://api.openai.com/v1/chat/completions',
             headers={
                 http.consts.HEADER_CONTENT_TYPE: http.consts.CONTENT_TYPE_JSON,
@@ -143,28 +210,6 @@ class OpenaiChatService(
             data=json.dumps(raw_request).encode('utf-8'),
         )
 
-        response = json.loads(check.not_none(raw_response.data).decode('utf-8'))
+        raw_response = json.loads(check.not_none(http_response.data).decode('utf-8'))
 
-        return ChatResponse(
-            [
-                AiChoice(AiMessage(
-                    choice['message']['content'],
-                    tool_exec_requests=[
-                        ToolExecRequest(
-                            id=tc['id'],
-                            spec=tools_by_name[tc['function']['name']],
-                            args=tc['function']['arguments'],
-                        )
-                        for tc in choice['message'].get('tool_calls', [])
-                    ],
-                ))
-                for choice in response['choices']
-            ],
-            outputs=tv.TypedValues(
-                *([TokenUsageOutput(TokenUsage(
-                    input=tu['prompt_tokens'],
-                    output=tu['completion_tokens'],
-                    total=tu['total_tokens'],
-                ))] if (tu := response.get('usage')) is not None else []),
-            ),
-        )
+        return rh.build_response(raw_response)
