9af21a6f3ac21cb342bd5020e55449ac2e98ea7c
wrmsr <timwilloney@gmail.com>
2025-04-10 17:51:21 -0700

    Refactor tests by moving generic tests to a new file
    Moved generic-related test classes and functions from test_dcb.py to a new test_generic.py file for better organization and clarity. Removed unused imports and redundant code.

---
+++ x/dcb/tests/test_generic.py
@@ -0,0 +1,51 @@
+import inspect
+import pprint
+import typing as ta
+
+from .. import api
+
+
+T = ta.TypeVar('T')
+K = ta.TypeVar('K')
+V = ta.TypeVar('V')
+
+
+##
+
+
+@api.dataclass(frozen=True, generic_init=True)
+class Thing(ta.Generic[T]):
+    s: set[T]
+    # mk: ta.Mapping[K, T]
+    # mv: ta.Mapping[T, V]
+    # mfk: ta.Mapping[ta.FrozenSet[K], T]
+    # mfv: ta.Mapping[T, ta.FrozenSet[V]]
+    mk: ta.Mapping[str, T]
+    mv: ta.Mapping[T, str]
+    mfk: ta.Mapping[frozenset[str], T]
+    mfv: ta.Mapping[T, frozenset[str]]
+
+
+@api.dataclass(frozen=True, generic_init=True)
+class IntThing(Thing[int]):
+    pass
+
+
+@api.dataclass(frozen=True, generic_init=True)
+class Thing2(Thing[K]):
+    pass
+
+
+@api.dataclass(frozen=True, generic_init=True)
+class IntThing2(Thing2[int]):
+    pass
+
+
+def test_generics2():
+    print()
+
+    pprint.pprint(dict(inspect.signature(Thing).parameters))
+
+    pprint.pprint(dict(inspect.signature(IntThing).parameters))
+
+    pprint.pprint(dict(inspect.signature(IntThing2).parameters))
