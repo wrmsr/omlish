0e692023036cf63f071c167d60ca64ef1ba1ed1b
wrmsr <timwilloney@gmail.com>
2025-04-15 11:34:41 -0700

    Refactor LazyModule to _LazyDef with simplified initialization
    Replaced LazyModule with _LazyDef, removing unused methods and attributes, and introduced lang.LazyGlobals for initialization.

--- x/dataclasses/tests/test_lazydef.py
+++ x/dataclasses/tests/test_lazydef.py
@@ -1,59 +1,16 @@
 import typing as ta
 
+from omlish import lang
+
 
 ##
 
 
-class LazyModule:
-    def __init__(
-            self,
-            *,
-            globals: ta.MutableMapping[str, ta.Any] | None = None,  # noqa
-            update_globals: bool = False,
-    ) -> None:
+class _LazyDef:
+    def __init__(self, lazy_globals: lang.LazyGlobals) -> None:
         super().__init__()
 
-        self._globals = globals
-        self._update_globals = update_globals
-
-        self._attr_fns: dict[str, ta.Callable[[], ta.Any]] = {}
-
-    @classmethod
-    def install(cls, globals: ta.MutableMapping[str, ta.Any] | None = None) -> 'LazyModule':  # noqa
-        try:
-            xga = globals['__getattr__']
-        except KeyError:
-            pass
-        else:
-            if not isinstance(xga, cls):
-                raise RuntimeError(f'Module already has __getattr__ hook: {xga}')
-            return xga
-
-        lm = cls(
-            globals=globals,
-            update_globals=True,
-        )
-
-        globals['__getattr__'] = lm
-
-        return lm
-
-    def add(self, attr: str, fn: ta.Callable[[], ta.Any]) -> 'LazyModule':
-        self._attr_fns[attr] = fn
-        return self
+        self._lazy_globals = lazy_globals
 
     def get(self, attr: str) -> ta.Any:
-        try:
-            fn = self._attr_fns[attr]
-        except KeyError:
-            raise AttributeError(attr)
-
-        val = fn()
-
-        if self._update_globals and self._globals is not None:
-            self._globals[attr] = val
-
-        return val
-
-    def __call__(self, attr: str) -> ta.Any:
-        return self.get(attr)
+        raise NotImplementedError
