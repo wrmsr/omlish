bb7b01ac263f5763d38c9905d9fda254985f11a6
wrmsr <timwilloney@gmail.com>
2025-04-15 16:53:24 -0700

    Update dependencies and submodule
    Upgrade various dependencies in requirements files to their latest versions, including mysql-connector-python, beautifulsoup4, contourpy, and sqlglot. Update the tinygrad submodule to the latest commit.

--- omlish/__about__.py
+++ omlish/__about__.py
@@ -92,7 +92,7 @@ class Project(ProjectBase):
             # 'psycopg ~= 3.2',
 
             'pymysql ~= 1.1',
-            # 'mysql-connector-python ~= 9.1',
+            # 'mysql-connector-python ~= 9.3',
             # 'mysqlclient ~= 2.2',
 
             'aiomysql ~= 0.2',
