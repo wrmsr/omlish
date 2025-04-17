c12a83b82097695e09ae7ffa8c8226c2f54d1c8a
wrmsr <timwilloney@gmail.com>
2025-04-17 09:31:00 -0700

    Add TODO comments for future enhancements in processing config

---
+++ omlish/configs/processing/__init__.py
@@ -0,0 +1,7 @@
+"""
+TODO:
+ - inline polymorph: 'ami: foo' -> 'image: {'ami': {'name': 'foo'}}
+ - parse duration and size settings, "512k" or "10 seconds"
+ - includes
+ - substitutions ("foo" : ${bar}, "foo" : Hello ${who})
+"""
