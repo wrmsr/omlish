9751afe599a1cbc0fe516cd4870660294393ec83
wrmsr <timwilloney@gmail.com>
2025-04-29 15:27:55 -0700

    Enhance `translate_name` function with input validation and prefix/suffix handling
    Add checks for non-empty strings and strings not composed entirely of underscores. Preserve leading and trailing underscores in the output.

--- omlish/marshal/naming.py
+++ omlish/marshal/naming.py
@@ -5,6 +5,7 @@ TODO:
 """
 import enum
 
+from .. import check
 from .. import lang
 from .base import Option
 
@@ -16,10 +17,22 @@ class Naming(Option, enum.Enum):
 
 
 def translate_name(n: str, e: Naming) -> str:
+    check.non_empty_str(n)
+    check.not_equal(set(n), {'_'})
+
+    n1 = n.lstrip('_')
+    pfx = '_' * (len(n) - len(n1))
+    n2 = n1.rstrip('_')
+    sfx = '_' * (len(n1) - len(n2))
+    ps = lang.split_string_casing(n2)
+
     if e is Naming.SNAKE:
-        return lang.snake_case(*lang.split_string_casing(n))
-    if e is Naming.CAMEL:
-        return lang.camel_case(*lang.split_string_casing(n))
-    if e is Naming.LOW_CAMEL:
-        return lang.low_camel_case(*lang.split_string_casing(n))
-    raise ValueError(e)
+        r = lang.snake_case(*ps)
+    elif e is Naming.CAMEL:
+        r = lang.camel_case(*ps)
+    elif e is Naming.LOW_CAMEL:
+        r = lang.low_camel_case(*ps)
+    else:
+        raise ValueError(e)
+
+    return pfx + r + sfx
