57e0370a70b488095d82a9018b024fe44fedca2f
wrmsr <timwilloney@gmail.com>
2025-05-13 18:29:44 -0700

    Refactor test_mlx and add multiply function
    Refactored the test_mlx function to improve clarity and maintainability. Introduced a standalone multiply function and updated the test to use it. Also updated the submodule reference for tinygrad.

--- ommlx/minichain/text/toolparsing/tests/test_mlx.py
+++ ommlx/minichain/text/toolparsing/tests/test_mlx.py
@@ -1,3 +1,16 @@
+"""
+mlx-community/Qwen2.5-7B-Instruct-8bit
+'<tool_call>\n{{"name": "multiply", "arguments": {"a": 12234585, "b": 48838483920}}}\n</tool_call>'
+
+mlx-community/Qwen2.5-Coder-7B-Instruct-8bit
+'```xml\n<function_call>\n  {"name": "multiply", "arguments": {"a": 12234585, "b": 48838483920}}\n</function_call>\n```<|im_end|>\n'
+
+mlx-community/Qwen2.5-32B-Instruct-4bit
+'<tool_call>\n{"name": "multiply", "arguments": {"a": 12234585, "b": 48838483920}}\n</tool_call>'
+
+mlx-community/Qwen2.5-Coder-32B-Instruct-8bit
+'<tools>\n{"name": "multiply", "arguments": {"a": 12234585, "b": 48838483920}}\n</tools>'
+"""  # noqa
 # Copyright © 2025 Apple Inc.
 # MIT License
 #
@@ -26,58 +39,48 @@ from ....tools import ToolSpec
 from ....tools import build_tool_spec_json_schema
 
 
-@pytest.mark.not_docker_guest
-@ptu.skip.if_cant_import('mlx_lm')
-def test_mlx():
-    import mlx.nn  # noqa
-    import mlx_lm.models.cache
-    import mlx_lm.utils
+##
 
-    # checkpoint = 'mlx-community/Qwen2.5-7B-Instruct-8bit'
-    # checkpoint = 'mlx-community/Qwen2.5-Coder-7B-Instruct-8bit'
-    # checkpoint = 'mlx-community/Qwen2.5-32B-Instruct-4bit'
-    checkpoint = 'mlx-community/Qwen2.5-Coder-32B-Instruct-8bit'
 
-    model: mlx.nn.Module
-    tokenizer: mlx_lm.utils.TokenizerWrapper
-    model, tokenizer = mlx_lm.load(path_or_hf_repo=checkpoint)
+def multiply(a: float, b: float) -> float:
+    """
+    A function that multiplies two numbers
 
-    def multiply(a: float, b: float):
-        """
-        A function that multiplies two numbers
+    Args:
+        a: The first number to multiply
+        b: The second number to multiply
+    """
 
-        Args:
-            a: The first number to multiply
-            b: The second number to multiply
-        """
+    return a * b
 
-        return a * b
 
-    multiply_tool = ToolSpec(
-        'multiply',
-        [
-            ToolParam(
-                'a',
-                'number',
-                desc='The first number to multiply',
-                required=True,
-            ),
-            ToolParam(
-                'b',
-                'number',
-                desc='The second number to multiply',
-                required=True,
-            ),
-        ],
-        desc='A function that multiplies two numbers',
-    )
+MULTIPLY_TOOL = ToolSpec(
+    'multiply',
+    [
+        ToolParam(
+            'a',
+            'number',
+            desc='The first number to multiply',
+            required=True,
+        ),
+        ToolParam(
+            'b',
+            'number',
+            desc='The second number to multiply',
+            required=True,
+        ),
+    ],
+    desc='A function that multiplies two numbers',
+)
 
-    multiply_tool_json_schema = build_tool_spec_json_schema(
-        multiply_tool,
-        omit_additional_properties_keyword=True,
-    )
+MULTIPLY_TOOL_JSON_SCHEMA = build_tool_spec_json_schema(
+    MULTIPLY_TOOL,
+    omit_additional_properties_keyword=True,
+)
 
-    assert multiply_tool_json_schema == {
+
+def test_multiply_tool_json_schema():
+    assert MULTIPLY_TOOL_JSON_SCHEMA == {
         'description': 'A function that multiplies two numbers',
         'name': 'multiply',
         'parameters': {
@@ -96,9 +99,65 @@ def test_mlx():
         },
     }
 
+
+##
+
+
+# def test_barf():
+#     tools = {'multiply': multiply}
+#
+#     messages = [{'role': 'user', 'content': 'Multiply 12234585 and 48838483920.'}]
+#
+#     import mlx.nn  # noqa
+#     import mlx_lm.utils
+#
+#     for checkpoint in [
+#         'mlx-community/Qwen2.5-7B-Instruct-8bit',
+#         'mlx-community/Qwen2.5-Coder-7B-Instruct-8bit',
+#         'mlx-community/Qwen2.5-32B-Instruct-4bit',
+#         'mlx-community/Qwen2.5-Coder-32B-Instruct-8bit',
+#     ]:
+#         model: mlx.nn.Module
+#         tokenizer: mlx_lm.utils.TokenizerWrapper
+#         model, tokenizer = mlx_lm.load(path_or_hf_repo=checkpoint)
+#
+#         prompt = tokenizer.apply_chat_template(
+#             messages,
+#             add_generation_prompt=True,
+#             tools=list(tools.values()),
+#         )
+#
+#         response = mlx_lm.generate(
+#             model=model,
+#             tokenizer=tokenizer,
+#             prompt=prompt,
+#             max_tokens=2048,
+#         )
+#
+#         print(checkpoint)
+#         print(repr(response))
+#         print()
+
+
+@pytest.mark.not_docker_guest
+@ptu.skip.if_cant_import('mlx_lm')
+def test_mlx():
+    import mlx.nn  # noqa
+    import mlx_lm.models.cache
+    import mlx_lm.utils
+
     tools = {'multiply': multiply}
 
-    messages = [{'role': 'user', 'content': 'Multiply 12234585 and 48838483920.'}]
+    messages: list[dict] = [{'role': 'user', 'content': 'Multiply 12234585 and 48838483920.'}]
+
+    # checkpoint = 'mlx-community/Qwen2.5-7B-Instruct-8bit'
+    # checkpoint = 'mlx-community/Qwen2.5-Coder-7B-Instruct-8bit'
+    # checkpoint = 'mlx-community/Qwen2.5-32B-Instruct-4bit'
+    checkpoint = 'mlx-community/Qwen2.5-Coder-32B-Instruct-8bit'
+
+    model: mlx.nn.Module
+    tokenizer: mlx_lm.utils.TokenizerWrapper
+    model, tokenizer = mlx_lm.load(path_or_hf_repo=checkpoint)
 
     prompt = tokenizer.apply_chat_template(
         messages,
