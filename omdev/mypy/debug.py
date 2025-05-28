38fb990aa6724f363a1d16dbac5fdac00f33ec29
wrmsr <timwilloney@gmail.com>
2025-05-28 11:04:18 -0700

    Add mypy report module and refactor debug functions
    Introduce a new report module to handle build result reporting in mypy. Refactor debug functions for better clarity and separation of concerns. Update redis and fonttools dependencies to newer versions. Update tinygrad submodule to latest commit.

--- omdev/mypy/debug.py
+++ omdev/mypy/debug.py
@@ -3,6 +3,9 @@ import re
 import sys
 
 
+##
+
+
 def _is_instance_or_subclass(obj, cls):
     return (isinstance(obj, type) and issubclass(obj, cls)) or isinstance(obj, cls)
 
@@ -11,44 +14,57 @@ class MypyDebugPathFinder(importlib.machinery.PathFinder):
     @classmethod
     def _get_spec(cls, fullname, path, target=None):  # noqa
         namespace_path = []
+
         for entry in path:
             if not isinstance(entry, (str, bytes)):
                 continue
+
             finder = cls._path_importer_cache(entry)  # type: ignore  # noqa
             if finder is not None:
                 if isinstance(finder, importlib.machinery.FileFinder):
                     finder = importlib.machinery.FileFinder(
                         finder.path,
                         *[
-                            (i, [s]) for s, i in finder._loaders  # type: ignore  # noqa
+                            (i, [s])
+                            for s, i in finder._loaders  # type: ignore  # noqa
                             if not _is_instance_or_subclass(i, importlib.machinery.ExtensionFileLoader)
                         ],
                     )
+
                 if hasattr(finder, 'find_spec'):
                     spec = finder.find_spec(fullname, target)
                 else:
                     spec = cls._legacy_get_spec(fullname, finder)  # type: ignore  # noqa
+
                 if spec is None:
                     continue
+
                 if spec.loader is not None:
                     return spec
+
                 portions = spec.submodule_search_locations
                 if portions is None:
                     raise ImportError('spec missing loader')
+
                 namespace_path.extend(portions)
+
         return None
 
     @classmethod
     def find_spec(cls, fullname, path=None, target=None):  # noqa
         if not fullname.startswith('mypy.') and fullname != 'mypy':
             return None
+
         if path is None:
             path = sys.path
+
         spec = cls._get_spec(fullname, path, target)
         if spec is None:
             return None
+
         elif spec.loader is None:
             namespace_path = spec.submodule_search_locations
+
             if namespace_path:
                 spec.origin = None
                 spec.submodule_search_locations = importlib.machinery._NamespacePath(  # type: ignore  # noqa
@@ -57,28 +73,49 @@ class MypyDebugPathFinder(importlib.machinery.PathFinder):
                     cls._get_spec,
                 )
                 return spec
+
             else:
                 return None
+
         else:
             return spec
 
 
-def _main():
+def install_debug_path_finder() -> None:
     for i, e in enumerate(sys.meta_path):  # noqa
         if _is_instance_or_subclass(e, importlib.machinery.PathFinder):
             break
+
     sys.meta_path.insert(i, MypyDebugPathFinder)  # noqa
     sys.path_importer_cache.clear()
     importlib.invalidate_caches()
 
+
+##
+
+
+def run_mypy_main(
+        *,
+        show_traceback: bool = False,
+) -> None:
     from mypy.__main__ import console_entry  # noqa
 
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
 
-    if not any(k in sys.argv[1:] for k in ['--show-traceback', '--tb']):
-        sys.argv.insert(1, '--show-traceback')
+    if show_traceback:
+        if not any(k in sys.argv[1:] for k in ['--show-traceback', '--tb']):
+            sys.argv.insert(1, '--show-traceback')
 
     console_entry()
+
+
+##
+
+
+def _main() -> None:
+    install_debug_path_finder()
+
+    run_mypy_main(show_traceback=True)
 
 
 if __name__ == '__main__':
---
+++ omdev/mypy/report.py
@@ -0,0 +1,62 @@
+import collections
+import typing as ta
+
+from .debug import install_debug_path_finder
+from .debug import run_mypy_main
+
+
+if ta.TYPE_CHECKING:
+    import mypy.build
+    import mypy.errors
+
+
+##
+
+
+def _report_build_result(
+        result: 'mypy.build.BuildResult',
+) -> None:
+    errors: ta.Sequence[mypy.errors.ErrorInfo] = [
+        e
+        for es in result.manager.errors.error_info_map.values()
+        for e in es
+    ]
+    if errors:
+        count_by_code = collections.Counter(
+            e.code.code
+            for e in errors
+            if e.code is not None
+        )
+
+        print()
+        max_code_len = max(map(len, count_by_code))
+        for code, count in sorted(count_by_code.items(), key=lambda kv: -kv[1]):
+            print(f'{code.rjust(max_code_len)} : {count}')
+
+
+##
+
+
+def _main() -> None:
+    install_debug_path_finder()
+
+    ##
+
+    import mypy.main
+
+    old_run_build = mypy.main.run_build
+
+    def new_run_build(*args, **kwargs):
+        ret = old_run_build(*args, **kwargs)
+        _report_build_result(ta.cast('mypy.build.BuildResult', ret[0]))
+        return ret
+
+    mypy.main.run_build = new_run_build
+
+    ##
+
+    run_mypy_main()
+
+
+if __name__ == '__main__':
+    _main()
