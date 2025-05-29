0822cac6280422b63e327458ee8b5416ff293206
wrmsr <timwilloney@gmail.com>
2025-05-29 16:05:00 -0700

    Refactor completion and embedding services to use proxy imports
    Switch to using proxy imports for backend services to improve modularity and type checking. Adjust service instantiation to use lambda functions for deferred execution.

--- ommlds/cli/sessions/completion.py
+++ ommlds/cli/sessions/completion.py
@@ -5,10 +5,17 @@ from omlish import check
 from omlish import lang
 
 from ... import minichain as mc
-from ...minichain.backends.llamacpp.completion import LlamacppCompletionService
-from ...minichain.backends.openai.completion import OpenaiCompletionService
-from ...minichain.backends.transformers.transformers import TransformersCompletionService
 from .base import Session
+
+
+if ta.TYPE_CHECKING:
+    from ...minichain.backends.llamacpp import completion as mc_lcc_completion
+    from ...minichain.backends.openai import completion as mc_openai_completion
+    from ...minichain.backends.transformers import transformers as mc_tfm
+else:
+    mc_lcc_completion = lang.proxy_import('...minichain.backends.llamacpp.completion', __package__)
+    mc_openai_completion = lang.proxy_import('...minichain.backends.openai.completion', __package__)
+    mc_tfm = lang.proxy_import('...minichain.backends.transformers.transformers', __package__)
 
 
 ##
@@ -16,10 +23,10 @@ from .base import Session
 
 DEFAULT_COMPLETION_MODEL_BACKEND = 'openai'
 
-COMPLETION_MODEL_BACKENDS: ta.Mapping[str, type[mc.CompletionService]] = {
-    'llamacpp': LlamacppCompletionService,
-    'openai': OpenaiCompletionService,
-    'transformers': TransformersCompletionService,
+COMPLETION_MODEL_BACKENDS: ta.Mapping[str, ta.Callable[[], type[mc.CompletionService]]] = {
+    'llamacpp': lambda: mc_lcc_completion.LlamacppCompletionService,
+    'openai': lambda: mc_openai_completion.OpenaiCompletionService,
+    'transformers': lambda: mc_tfm.TransformersCompletionService,
 }
 
 
@@ -41,7 +48,7 @@ class CompletionSession(Session['CompletionSession.Config']):
     def run(self) -> None:
         prompt = check.isinstance(self._config.content, str)
         with lang.maybe_managing(
-                COMPLETION_MODEL_BACKENDS[self._config.backend or DEFAULT_COMPLETION_MODEL_BACKEND](),
+                COMPLETION_MODEL_BACKENDS[self._config.backend or DEFAULT_COMPLETION_MODEL_BACKEND]()(),
         ) as mdl:
             response = mdl.invoke(mc.CompletionRequest.new(prompt))
             print(response.text.strip())
--- ommlds/cli/sessions/embedding.py
+++ ommlds/cli/sessions/embedding.py
@@ -5,19 +5,24 @@ from omlish import lang
 from omlish.formats import json
 
 from ... import minichain as mc
-from ...minichain.backends.openai.embedding import OpenaiEmbeddingService
-from ...minichain.backends.transformers.sentence import SentenceTransformersEmbeddingService
 from .base import Session
 
+
+if ta.TYPE_CHECKING:
+    from ...minichain.backends.openai import embedding as mc_openai_embedding
+    from ...minichain.backends.transformers import sentence as mc_stfm
+else:
+    mc_openai_embedding = lang.proxy_import('...minichain.backends.openai.embedding', __package__)
+    mc_stfm = lang.proxy_import('...minichain.backends.transformers.sentence', __package__)
 
 ##
 
 
 DEFAULT_EMBEDDING_MODEL_BACKEND = 'openai'
 
-EMBEDDING_MODEL_BACKENDS: ta.Mapping[str, type[mc.EmbeddingService]] = {
-    'openai': OpenaiEmbeddingService,
-    'stfm': SentenceTransformersEmbeddingService,
+EMBEDDING_MODEL_BACKENDS: ta.Mapping[str, ta.Callable[[], type[mc.EmbeddingService]]] = {
+    'openai': lambda: mc_openai_embedding.OpenaiEmbeddingService,
+    'stfm': lambda: mc_stfm.SentenceTransformersEmbeddingService,
 }
 
 
@@ -38,7 +43,7 @@ class EmbeddingSession(Session['EmbeddingSession.Config']):
 
     def run(self) -> None:
         with lang.maybe_managing(
-                EMBEDDING_MODEL_BACKENDS[self._config.backend or DEFAULT_EMBEDDING_MODEL_BACKEND](),
+                EMBEDDING_MODEL_BACKENDS[self._config.backend or DEFAULT_EMBEDDING_MODEL_BACKEND]()(),
         ) as mdl:
             response = mdl.invoke(mc.EmbeddingRequest.new(self._config.content))
             print(json.dumps_compact(response.vector))
