735d13c7ae2ba542d490b72a443ebcf36f1d5119
wrmsr <timwilloney@gmail.com>
2025-06-04 15:08:36 -0700

    Update dependencies and refactor test factories
    Bump versions for uv, boto3, botocore, and cattrs in requirements files. Refactor test factories by renaming functions, adding type variables, and introducing TypedServiceFactory class for improved type handling.

--- x/mc3/services/tests/test_factories.py
+++ x/mc3/services/tests/test_factories.py
@@ -1,3 +1,4 @@
+import dataclasses as dc
 import typing as ta
 
 from omlish import lang
@@ -12,7 +13,7 @@ SelectorT_contra = ta.TypeVar('SelectorT_contra', contravariant=True)
 
 ServiceT = ta.TypeVar('ServiceT', bound=Service)
 ServiceT_co = ta.TypeVar('ServiceT_co', bound=Service, covariant=True)
-
+ServiceU = ta.TypeVar('ServiceU', bound=Service)
 
 ##
 
@@ -57,17 +58,44 @@ def test_factory():
 ##
 
 
-def test_factory2():
-    def fn(ty: type[ServiceT], name: str) -> ServiceT:
-        if ty == ChatService:
-            if name == 'local':
-                return ta.cast(ServiceT, LocalChatServiceImpl())
-            elif name == 'remote':
-                return ta.cast(ServiceT, RemoteChatServiceImpl())
-            else:
-                raise ValueError(name)
+def new_service_from_name(ty: type[ServiceT], name: str) -> ServiceT:
+    if ty == ChatService:
+        if name == 'local':
+            return ta.cast(ServiceT, LocalChatServiceImpl())
+        elif name == 'remote':
+            return ta.cast(ServiceT, RemoteChatServiceImpl())
         else:
-            raise TypeError(ty)
+            raise ValueError(name)
+    else:
+        raise TypeError(ty)
 
-    cs: ChatService = fn(ChatService, 'local')  # type: ignore[type-abstract]
+
+def test_factory2():
+    cs: ChatService = new_service_from_name(ChatService, 'local')  # type: ignore[type-abstract]
+    assert isinstance(cs, LocalChatServiceImpl)
+
+
+##
+
+
+class TypedServiceFactory(ta.Generic[ServiceT]):
+    @dc.dataclass(frozen=True)
+    class _Bound(ta.Generic[ServiceU]):  # noqa
+        cls: type[ServiceU]
+
+        def new(self, name: str, *args: ta.Any, **kwargs: ta.Any) -> ServiceU:
+            return new_service_from_name(self.cls, name, *args, **kwargs)
+
+    def __class_getitem__(cls, *args, **kwargs):
+        [bind_cls] = args
+        return TypedServiceFactory._Bound(bind_cls)
+
+    @classmethod
+    def new(cls, name: str, *args: ta.Any, **kwargs: ta.Any) -> ServiceT:  # noqa
+        raise TypeError
+
+
+def test_factory3():
+    cs: ChatService = TypedServiceFactory[ChatService].new('local')
+    lang.static_check_isinstance[ChatService](cs)
     assert isinstance(cs, LocalChatServiceImpl)
