2b8a5312c4852300a00c51287fab443cf9f64f60
wrmsr <timwilloney@gmail.com>
2025-07-25 14:46:41 -0700

    Add new tool context and executor classes
    Introduce ToolContext and ToolExecutor classes to manage tool execution contexts and inputs/outputs. This includes context binding and validation for dataclass inputs and outputs.

---
+++ ommlds/minichain/lib/fs/ls/tests/newtools.py
@@ -0,0 +1,90 @@
+import contextlib
+import contextvars
+import typing as ta
+
+from omlish import check
+from omlish import dataclasses as dc
+from omlish import lang
+
+
+D = ta.TypeVar('D')
+R = ta.TypeVar('R')
+T = ta.TypeVar('T')
+
+
+##
+
+
+class ToolContext(lang.Final):
+    pass
+
+
+_TOOL_CONTEXT: contextvars.ContextVar[ToolContext] = contextvars.ContextVar(f'{__name__}._TOOL_CONTEXT')
+
+
+@contextlib.contextmanager
+def bind_tool_context(ctx: ToolContext) -> ta.Generator[ToolContext]:
+    try:
+        cur = _TOOL_CONTEXT.get()
+    except LookupError:
+        pass
+    else:
+        raise RuntimeError(f'Already bound: {cur}')
+
+    tok = _TOOL_CONTEXT.set(ctx)
+    try:
+        yield ctx
+    finally:
+        _TOOL_CONTEXT.reset(tok)
+
+
+def tool_context() -> ToolContext:
+    return _TOOL_CONTEXT.get()
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class ToolExecutor(lang.Final):
+    fn: ta.Callable
+
+    #
+
+    @dc.dataclass(frozen=True)
+    class Input(lang.Sealed, lang.Abstract):
+        pass
+
+    @dc.dataclass(frozen=True)
+    class DataclassInput(Input, lang.Final, ta.Generic[D]):
+        cls: type[D]
+
+        def __post_init__(self) -> None:
+            check.isinstance(self.cls, type)
+            check.arg(dc.is_dataclass(self.cls))
+
+    @dc.dataclass(frozen=True)
+    class KwargsInput(Input, lang.Final):
+        pass
+
+    input: Input
+
+    #
+
+    @dc.dataclass(frozen=True)
+    class Output(lang.Sealed, lang.Abstract):
+        pass
+
+    @dc.dataclass(frozen=True)
+    class DataclassOutput(Output, lang.Final, ta.Generic[D]):
+        cls: type[D]
+
+        def __post_init__(self) -> None:
+            check.isinstance(self.cls, type)
+            check.arg(dc.is_dataclass(self.cls))
+
+    @dc.dataclass(frozen=True)
+    class RawStringOutput(lang.Sealed, lang.Abstract):
+        pass
+
+    output: Output
