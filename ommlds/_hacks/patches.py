3b29e6acb4dbc2fa38be6032e7cc4b92615f2941
wrmsr <timwilloney@gmail.com>
2025-08-04 12:14:25 -0700

    Rename 'hacks' directory to '_hacks' and update comments
    Renamed the 'hacks' directory to '_hacks' for better organization and added comments to clarify the purpose of the codebase interactions. Updated TODOs in goyaml to include exceptions and amalg-safe considerations.

--- ommlds/_hacks/patches.py
+++ ommlds/_hacks/patches.py
@@ -1,4 +1,7 @@
 """
+Currently, and ideally, the only thing this codebase will ever interact with requiring these kinds of awful hacks is ML
+code - thus these 'helpers' are kept here, not in the core library.
+
 TODO:
  - patch lock
  - thread / context local gating
