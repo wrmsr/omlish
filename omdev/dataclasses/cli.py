e25ce8411a9e496783732cb0d57416d79bfabb9a
wrmsr <timwilloney@gmail.com>
2025-08-13 20:30:00 -0700

    Add code generation support with configuration
    Integrate logging and argument parsing for code generation. Implement configuration file detection and processing to enable conditional code generation based on '.dataclasses.toml'.

--- omdev/dataclasses/cli.py
+++ omdev/dataclasses/cli.py
@@ -1,4 +1,5 @@
 from omlish.argparse import all as ap
+from omlish.logs.standard import configure_standard_logging
 
 from .codegen import DataclassCodeGen
 
@@ -7,15 +8,19 @@ from .codegen import DataclassCodeGen
 
 
 class Cli(ap.Cli):
-    @ap.cmd()
+    @ap.cmd(
+        ap.arg('roots', metavar='root', nargs='+'),
+    )
     def codegen(self) -> None:
-        DataclassCodeGen().run()
+        DataclassCodeGen().run(self.args.roots)
 
 
 ##
 
 
 def _main() -> None:
+    configure_standard_logging()
+
     Cli().cli_run_and_exit()
 
 
--- omdev/dataclasses/codegen.py
+++ omdev/dataclasses/codegen.py
@@ -1,3 +1,58 @@
+import logging
+import os.path
+import typing as ta
+
+from omlish import check
+from omlish import lang
+from omlish.formats.toml.parser import toml_loads
+
+
+log = logging.getLogger(__name__)
+
+
+##
+
+
+CONFIG_FILE_NAME = '.dataclasses.toml'
+
+
 class DataclassCodeGen:
-    def run(self) -> None:
-        raise NotImplementedError
+    def __init__(self) -> None:
+        super().__init__()
+
+    def run_config_file(self, config_file: str) -> None:
+        with open(config_file) as f:
+            config_toml = f.read()
+        config_obj = toml_loads(config_toml)
+
+        should_codegen = check.isinstance(config_obj.get('codegen', False), bool)
+        if not should_codegen:
+            return
+
+        pkg_root = os.path.dirname(config_file).replace(os.sep, '.')
+
+        log.info('Running codegen on package: %s', pkg_root)
+
+        sub_pkgs = sorted(lang.yield_importable(
+            pkg_root,
+            recursive=True,
+            include_special=True,
+        ))
+
+        for sub_pkg in sub_pkgs:
+            print(f'{sub_pkg=}')
+
+    def run(
+            self,
+            root_dirs: ta.Iterable[str],
+    ) -> None:
+        check.not_isinstance(root_dirs, str)
+
+        config_files: set[str] = set()
+        for root_dir in root_dirs:
+            for dp, _, fns in os.walk(root_dir):
+                if CONFIG_FILE_NAME in fns:
+                    config_files.add(os.path.join(dp, CONFIG_FILE_NAME))
+
+        for config_file in sorted(config_files):
+            self.run_config_file(config_file)
