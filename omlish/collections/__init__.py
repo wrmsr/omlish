4e9cb702d1d048bb8211070b471983d3b4d7bb6d
wrmsr <timwilloney@gmail.com>
2025-08-15 15:13:01 -0700

    Refactor import structure to use auto_proxy_init
    Simplify the import logic by replacing conditional type checking with _lang.auto_proxy_init, reducing redundancy and improving maintainability.

--- omlish/collections/__init__.py
+++ omlish/collections/__init__.py
@@ -1,165 +1,151 @@
 # ruff: noqa: I001
-import typing as _ta
-
 from .. import lang as _lang
 
 
-from .bimap import (  # noqa
-    BiMap,
+with _lang.auto_proxy_init(globals()):
+    ##
 
-    make_bi_map,
-)
+    from .bimap import (  # noqa
+        BiMap,
 
-from .coerce import (  # noqa
-    abs_set,
-    abs_set_of,
-    abs_set_of_or_none,
-    abs_set_or_none,
-    frozenset_,
-    frozenset_of,
-    frozenset_of_or_none,
-    frozenset_or_none,
-    map,  # noqa
-    map_of,
-    map_of_or_none,
-    map_or_none,
-    opt_abs_set,
-    opt_abs_set_of,
-    opt_frozenset,
-    opt_frozenset_of,
-    opt_map,
-    opt_map_of,
-    opt_seq,
-    opt_seq_of,
-    seq,
-    seq_of,
-    seq_of_or_none,
-    seq_or_none,
-)
+        make_bi_map,
+    )
 
-from .frozen import (  # noqa
-    Frozen,
-    FrozenDict,
-    FrozenList,
-    frozendict,
-    frozenlist,
-)
+    from .coerce import (  # noqa
+        abs_set,
+        abs_set_of,
+        abs_set_of_or_none,
+        abs_set_or_none,
+        frozenset_,
+        frozenset_of,
+        frozenset_of_or_none,
+        frozenset_or_none,
+        map,  # noqa
+        map_of,
+        map_of_or_none,
+        map_or_none,
+        opt_abs_set,
+        opt_abs_set_of,
+        opt_frozenset,
+        opt_frozenset_of,
+        opt_map,
+        opt_map_of,
+        opt_seq,
+        opt_seq_of,
+        seq,
+        seq_of,
+        seq_of_or_none,
+        seq_or_none,
+    )
 
-from .hasheq import (  # noqa
-    HashEq,
-    HashEqMap,
-    HashEq_,
-    hash_eq,
-)
+    from .frozen import (  # noqa
+        Frozen,
+        FrozenDict,
+        FrozenList,
+        frozendict,
+        frozenlist,
+    )
 
-from .identity import (  # noqa
-    IdentityKeyDict,
-    IdentitySet,
-    IdentityWeakKeyDictionary,
-    IdentityWeakSet,
-)
+    from .hasheq import (  # noqa
+        HashEq,
+        HashEqMap,
+        HashEq_,
+        hash_eq,
+    )
 
-if _ta.TYPE_CHECKING:
-    from . import kv
-else:
-    kv = _lang.proxy_import('.kv', __package__)
+    from .identity import (  # noqa
+        IdentityKeyDict,
+        IdentitySet,
+        IdentityWeakKeyDictionary,
+        IdentityWeakSet,
+    )
 
-from .mappings import (  # noqa
-    MissingDict,
-    TypeMap,
-    DynamicTypeMap,
-    guarded_map_update,
-    multikey_dict,
-)
+    from . import kv  # noqa
 
-from .multimaps import (  # noqa
-    MultiMap,
+    from .mappings import (  # noqa
+        MissingDict,
+        TypeMap,
+        DynamicTypeMap,
+        guarded_map_update,
+        multikey_dict,
+    )
 
-    SequenceMultiMap,
-    AbstractSetMultiMap,
+    from .multimaps import (  # noqa
+        MultiMap,
 
-    BiMultiMap,
-    InverseBiMultiMap,
+        SequenceMultiMap,
+        AbstractSetMultiMap,
 
-    SequenceBiMultiMap,
-    AbstractSetBiMultiMap,
+        BiMultiMap,
+        InverseBiMultiMap,
 
-    TupleBiMultiMap,
-    seq_bi_multi_map,
+        SequenceBiMultiMap,
+        AbstractSetBiMultiMap,
 
-    FrozensetBiMultiMap,
-    abs_set_bi_multi_map,
-)
+        TupleBiMultiMap,
+        seq_bi_multi_map,
 
-from .ordered import (  # noqa
-    OrderedFrozenSet,
-    OrderedSet,
-)
+        FrozensetBiMultiMap,
+        abs_set_bi_multi_map,
+    )
 
-from .persistent.persistent import (  # noqa
-    PersistentMap,
-    PersistentMapping,
-)
+    from .ordered import (  # noqa
+        OrderedFrozenSet,
+        OrderedSet,
+    )
 
-if _ta.TYPE_CHECKING:
+    from .persistent.persistent import (  # noqa
+        PersistentMap,
+        PersistentMapping,
+    )
+
     from .persistent.treapmap import (  # noqa
         TreapDict,
         TreapMap,
         new_treap_dict,
         new_treap_map,
     )
-else:
-    _lang.proxy_init(globals(), '.persistent.treapmap', [
-        'TreapMap',
-        'new_treap_map',
-    ])
 
-from .ranked import (  # noqa
-    RankedSeq,
-    RankedSetSeq,
-)
+    from .ranked import (  # noqa
+        RankedSeq,
+        RankedSetSeq,
+    )
 
-if _ta.TYPE_CHECKING:
     from .sorted.skiplist import (  # noqa
         SkipList,
         SkipListDict,
     )
-else:
-    _lang.proxy_init(globals(), '.sorted.skiplist', [
-        'SkipList',
-        'SkipListDict',
-    ])
 
-from .sorted.sorted import (  # noqa
-    SortedCollection,
-    SortedItems,
-    SortedIter,
-    SortedListDict,
-    SortedMapping,
-    SortedMutableMapping,
-)
+    from .sorted.sorted import (  # noqa
+        SortedCollection,
+        SortedItems,
+        SortedIter,
+        SortedListDict,
+        SortedMapping,
+        SortedMutableMapping,
+    )
 
-from .trie import (  # noqa
-    Trie,
-)
+    from .trie import (  # noqa
+        Trie,
+    )
 
-from .unmodifiable import (  # noqa
-    Unmodifiable,
-    UnmodifiableMapping,
-    UnmodifiableSequence,
-    UnmodifiableSet,
-)
+    from .unmodifiable import (  # noqa
+        Unmodifiable,
+        UnmodifiableMapping,
+        UnmodifiableSequence,
+        UnmodifiableSet,
+    )
 
-from .utils import (  # noqa
-    PartitionResult,
-    all_equal,
-    all_not_equal,
-    indexes,
-    key_cmp,
-    make_map,
-    make_map_by,
-    multi_map,
-    multi_map_by,
-    partition,
-    unique,
-)
+    from .utils import (  # noqa
+        PartitionResult,
+        all_equal,
+        all_not_equal,
+        indexes,
+        key_cmp,
+        make_map,
+        make_map_by,
+        multi_map,
+        multi_map_by,
+        partition,
+        unique,
+    )
