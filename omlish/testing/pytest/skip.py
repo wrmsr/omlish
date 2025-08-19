4a09204b541c81197eefc642ec2dbb0873934fb9
wrmsr <timwilloney@gmail.com>
2025-08-19 14:44:52 -0700

    Refactor skip condition functions for readability
    Reformat the return statements in skip condition functions to improve code readability by using multi-line formatting.

--- omlish/testing/pytest/skip.py
+++ omlish/testing/pytest/skip.py
@@ -17,20 +17,35 @@ from ... import lang
 
 def if_cant_import(*modules: str, **kwargs):
     missing = [m for m in modules if not lang.can_import(m, **kwargs)]
-    return pytest.mark.skipif(bool(missing), reason=f'requires import {", ".join(missing)}')
+    return pytest.mark.skipif(
+        bool(missing),
+        reason=f'requires import {", ".join(missing)}',
+    )
 
 
 def if_not_on_path(exe: str):
-    return pytest.mark.skipif(shutil.which(exe) is None, reason=f'requires exe on path {exe}')
+    return pytest.mark.skipif(
+        shutil.which(exe) is None,
+        reason=f'requires exe on path {exe}',
+    )
 
 
 def if_python_version_less_than(num: ta.Sequence[int]):
-    return pytest.mark.skipif(sys.version_info < tuple(num), reason=f'python version {tuple(sys.version_info)} < {tuple(num)}')  # noqa
+    return pytest.mark.skipif(
+        sys.version_info < tuple(num),
+        reason=f'python version {tuple(sys.version_info)} < {tuple(num)}',
+    )
 
 
 def if_not_platform(*platforms: str):
-    return pytest.mark.skipif(sys.platform not in platforms, reason=f'requires platform in {platforms}')
+    return pytest.mark.skipif(
+        sys.platform not in platforms,
+        reason=f'requires platform in {platforms}',
+    )
 
 
 def if_nogil():
-    return pytest.mark.skipif(sysconfig.get_config_var('Py_GIL_DISABLED'), reason='requires gil build')
+    return pytest.mark.skipif(
+        sysconfig.get_config_var('Py_GIL_DISABLED'),
+        reason='requires gil build',
+    )
