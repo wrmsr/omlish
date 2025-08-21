f43511b4ca17740a0ccc06c5d5e9742c7913060a
wrmsr <timwilloney@gmail.com>
2025-08-21 16:51:30 -0700

    Refactor backend string resolver logic
    Simplify the backend string resolution by integrating resolver initialization into BackendStringBackendCatalog and removing redundant ManifestBackendStringResolver class.

--- ommlds/cli/backends/strings.py
+++ ommlds/cli/backends/strings.py
@@ -1,112 +1,31 @@
-"""
-TODO:
- - ModelNamePackCollectionThing
-"""
-import abc
-import dataclasses as dc
 import typing as ta
 
 from omlish import check
-from omlish import lang
-from omlish.manifests.globals import GlobalManifestLoader
 
 from ... import minichain as mc
-from ...minichain.backends.strings.manifests import BackendStringsManifest
-from ...minichain.backends.strings.parsing import ParsedBackendString
 from ...minichain.backends.strings.parsing import parse_backend_string
+from ...minichain.backends.strings.resolving import BackendStringResolver
+from ...minichain.backends.strings.resolving import build_manifest_backend_string_resolver
 from .catalog import BackendCatalog
-
-
-T = ta.TypeVar('T')
-
-
-##
-
-
-@dc.dataclass(frozen=True)
-class ResolvedBackend:
-    backend_name: str
-
-    args: ta.Sequence[ta.Any] | None = None
-
-
-class BackendStringResolver(lang.Abstract):
-    @abc.abstractmethod
-    def resolve_backend_string(self, ps: ParsedBackendString) -> ResolvedBackend | None:
-        raise NotImplementedError
-
-
-##
-
-
-class ManifestBackendStringResolver(BackendStringResolver):
-    def __init__(self, manifests: ta.Iterable[BackendStringsManifest]) -> None:
-        super().__init__()
-
-        self._manifests = list(manifests)
-
-    def _model_name_args(self, s: str | None) -> ta.Sequence[ta.Any]:
-        if s is not None:
-            return [mc.ModelName(s)]
-        else:
-            return []
-
-    def resolve_backend_string(self, ps: ParsedBackendString) -> ResolvedBackend | None:
-        if ps.backend is not None and isinstance(ps.model, ParsedBackendString.NameModel):
-            return ResolvedBackend(
-                ps.model.name,
-                self._model_name_args(ps.model.name),
-            )
-
-        for m in self._manifests:
-            if ps.backend is not None:
-                if ps.backend == m.backend_name:
-                    if isinstance(ps.model, ParsedBackendString.NameModel):
-                        return ResolvedBackend(
-                            ps.backend,
-                            self._model_name_args(ps.model.name),
-                        )
-
-                    else:
-                        raise NotImplementedError
-
-                else:
-                    continue
-
-            if isinstance(ps.model, ParsedBackendString.NameModel):
-                if ps.model.name in m.backend_name:
-                    return ResolvedBackend(
-                        m.backend_name,
-                        self._model_name_args(m.model_names.resolved_default if m.model_names is not None else None),
-                    )
-
-                if m.model_names is not None:
-                    if ps.model.name == m.model_names.default:
-                        return ResolvedBackend(
-                            m.backend_name,
-                            self._model_name_args(m.model_names.resolved_default),
-                        )
-
-                    elif ps.model.name in m.model_names.alias_map:
-                        return ResolvedBackend(
-                            m.backend_name,
-                            self._model_name_args(m.model_names.alias_map[ps.model.name]),
-                        )
-
-            else:
-                raise NotImplementedError
-
-        return None
 
 
 ##
 
 
 class BackendStringBackendCatalog(BackendCatalog):
+    def __init__(
+            self,
+            resolver: BackendStringResolver | None = None,
+    ) -> None:
+        super().__init__()
+
+        if resolver is None:
+            resolver = build_manifest_backend_string_resolver()
+        self._resolver = resolver
+
     def get_backend(self, service_cls: ta.Any, name: str, *args: ta.Any, **kwargs: ta.Any) -> ta.Any:
         ps = parse_backend_string(name)
-        rv = ManifestBackendStringResolver(GlobalManifestLoader.load_values_of(BackendStringsManifest))
-        rs = check.not_none(rv.resolve_backend_string(ps))
+        rs = check.not_none(self._resolver.resolve_backend_string(ps))
 
         return mc.registry_new(
             service_cls,
--- ommlds/minichain/backends/strings/parsing.py
+++ ommlds/minichain/backends/strings/parsing.py
@@ -11,7 +11,7 @@ from omlish import lang
 
 @dc.dataclass(frozen=True, kw_only=True)
 class ParsedBackendString:
-    backend: str | None = None
+    backend: str | None = dc.xfield(None, repr_fn=dc.opt_repr)
 
     #
 
