b1bd5f57f115dfe8ebad17c4d89cb82b68abd147
wrmsr <timwilloney@gmail.com>
2025-08-22 17:46:43 -0700

    Add model path resolution for backend strings
    Introduce logic to resolve model paths when the first argument is a ModelRepo instance, using HuggingfaceModelRepoResolver.

--- ommlds/cli/backends/strings.py
+++ ommlds/cli/backends/strings.py
@@ -6,6 +6,8 @@ from ... import minichain as mc
 from ...minichain.backends.strings.parsing import parse_backend_string
 from ...minichain.backends.strings.resolving import BackendStringResolver
 from ...minichain.backends.strings.resolving import build_manifest_backend_string_resolver
+from ...minichain.models.configs import ModelPath
+from ...minichain.models.configs import ModelRepo
 from .catalog import BackendCatalog
 
 
@@ -27,10 +29,20 @@ class BackendStringBackendCatalog(BackendCatalog):
         ps = parse_backend_string(name)
         rs = check.not_none(self._resolver.resolve_backend_string(ps))
 
+        al = list(rs.args or [])
+
+        # FIXME: lol
+        if al and isinstance(al[0], ModelRepo):
+            [mr] = al
+            from ...minichain.backends.impls.huggingface.repos import HuggingfaceModelRepoResolver
+            mrr = HuggingfaceModelRepoResolver()
+            mrp = check.not_none(mrr.resolve(mr))
+            al = [ModelPath(mrp.path), *al[1:]]
+
         return mc.registry_new(
             service_cls,
             rs.backend_name,
-            *(rs.args or []),
+            *al,
             *args,
             **kwargs,
         )
