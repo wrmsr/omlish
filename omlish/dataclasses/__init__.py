79bb2b1f7af2d9c344f8471cd6e785914768a72f
wrmsr <timwilloney@gmail.com>
2025-08-28 22:33:03 -0700

    Refactor import structure and apply auto_proxy_init for dataclasses module
    Reorganize imports for better readability and maintainability. Introduce _lang.auto_proxy_init to manage global proxies for dataclass functions, ensuring consistent behavior across the module.

--- omlish/dataclasses/__init__.py
+++ omlish/dataclasses/__init__.py
@@ -1,52 +1,161 @@
 # ruff: noqa: I001
 import sys as _sys
 
+from .. import lang as _lang
 
-##
-# stdlib interface
 
-from dataclasses import (  # noqa
-    FrozenInstanceError,
+with _lang.auto_proxy_init(globals()):
+    ##
 
-    MISSING,
-    KW_ONLY,
+    ##
+    # stdlib interface
 
-    InitVar,
-    Field,
+    from dataclasses import (  # noqa
+        FrozenInstanceError,
 
-    field,
+        MISSING,
+        KW_ONLY,
 
-    dataclass,
-    make_dataclass,
+        InitVar,
+        Field,
 
-    fields,
+        field,
 
-    is_dataclass,
+        dataclass,
+        make_dataclass,
 
-    replace,
-)
+        fields,
 
-from .impl.api import (  # noqa
-    dataclass as xdataclass,
+        is_dataclass,
 
-    make_dataclass as xmake_dataclass,
+        replace,
+    )
 
-    field as xfield,
-)
+    from .impl.api import (  # noqa
+        dataclass as xdataclass,
 
-from .impl.concerns.replace import (  # noqa
-    replace as xreplace,
-)
+        make_dataclass as xmake_dataclass,
 
-from .tools.as_ import (  # noqa
-    asdict,
-    astuple,
-)
+        field as xfield,
+    )
+
+    from .impl.concerns.replace import (  # noqa
+        replace as xreplace,
+    )
+
+    from .tools.as_ import (  # noqa
+        asdict,
+        astuple,
+    )
+
+    ##
+    # additional interface
+
+    from .impl.api import (  # noqa
+        append_class_metadata,
+        extra_class_params,
+        init,
+        metadata,
+        validate,
+
+        extra_field_params,
+        set_field_metadata,
+        update_extra_field_params,
+        with_extra_field_params,
+    )
+
+    from .errors import (  # noqa
+        FieldFnValidationError,
+        FieldTypeValidationError,
+        FieldValidationError,
+        FnValidationError,
+        TypeValidationError,
+        ValidationError,
+    )
+
+    from .metaclass.bases import (  # noqa
+        Box,
+        Case,
+        Data,
+        Frozen,
+    )
+
+    from .metaclass.meta import (  # noqa
+        DataMeta,
+    )
+
+    from .metaclass.specs import (  # noqa
+        get_metaclass_spec,
+    )
+
+    from .reflection import (  # noqa
+        reflect,
+    )
+
+    from .specs import (  # noqa
+        CoerceFn,
+        ValidateFn,
+        ReprFn,
+
+        InitFn,
+        ClassValidateFn,
+
+        DefaultFactory,
+
+        FieldType,
+
+        FieldSpec,
+
+        ClassSpec,
+    )
+
+    from .tools.as_ import (  # noqa
+        shallow_asdict,
+        shallow_astuple,
+    )
+
+    from .tools.iter import (  # noqa
+        fields_dict,
+
+        iter_items,
+        iter_keys,
+        iter_values,
+    )
+
+    from .tools.modifiers import (  # noqa
+        field_modifier,
+        update_fields,
+    )
+
+    from .tools.only_ import (  # noqa
+        only,
+    )
+
+    from .tools.replace import (  # noqa
+        deep_replace,
+    )
+
+    from .tools.repr import (  # noqa
+        opt_repr,
+        truthy_repr,
+    )
+
+    from .tools.static import (  # noqa
+        Static,
+    )
+
+    ##
+    # lite imports
+
+    from ..lite.dataclasses import (  # noqa
+        is_immediate_dataclass,
+
+        dataclass_maybe_post_init as maybe_post_init,
+    )
 
 
 ##
 # globals hack
-
 
 def _self_patching_global_proxy(l, r, ak_fn=None):
     def inner(*args, **kwargs):
@@ -69,110 +178,3 @@ globals()['make_dataclass'] = _self_patching_global_proxy(
 )
 
 globals()['replace'] = _self_patching_global_proxy('replace', 'xreplace')
-
-
-##
-# additional interface
-
-from .impl.api import (  # noqa
-    append_class_metadata,
-    extra_class_params,
-    init,
-    metadata,
-    validate,
-
-    extra_field_params,
-    set_field_metadata,
-    update_extra_field_params,
-    with_extra_field_params,
-)
-
-from .errors import (  # noqa
-    FieldFnValidationError,
-    FieldTypeValidationError,
-    FieldValidationError,
-    FnValidationError,
-    TypeValidationError,
-    ValidationError,
-)
-
-from .metaclass.bases import (  # noqa
-    Box,
-    Case,
-    Data,
-    Frozen,
-)
-
-from .metaclass.meta import (  # noqa
-    DataMeta,
-)
-
-from .metaclass.specs import (  # noqa
-    get_metaclass_spec,
-)
-
-from .reflection import (  # noqa
-    reflect,
-)
-
-from .specs import (  # noqa
-    CoerceFn,
-    ValidateFn,
-    ReprFn,
-
-    InitFn,
-    ClassValidateFn,
-
-    DefaultFactory,
-
-    FieldType,
-
-    FieldSpec,
-
-    ClassSpec,
-)
-
-from .tools.as_ import (  # noqa
-    shallow_asdict,
-    shallow_astuple,
-)
-
-from .tools.iter import (  # noqa
-    fields_dict,
-
-    iter_items,
-    iter_keys,
-    iter_values,
-)
-
-from .tools.modifiers import (  # noqa
-    field_modifier,
-    update_fields,
-)
-
-from .tools.only_ import (  # noqa
-    only,
-)
-
-from .tools.replace import (  # noqa
-    deep_replace,
-)
-
-from .tools.repr import (  # noqa
-    opt_repr,
-    truthy_repr,
-)
-
-from .tools.static import (  # noqa
-    Static,
-)
-
-
-##
-# lite imports
-
-from ..lite.dataclasses import (  # noqa
-    is_immediate_dataclass,
-
-    dataclass_maybe_post_init as maybe_post_init,
-)
