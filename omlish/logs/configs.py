40ce1202e99446bd00bc1e82c9cd1528bf133de2
wrmsr <timwilloney@gmail.com>
2025-09-01 14:17:08 -0700

    Refactor type annotations for logging config
    Switch from PEP 585 style to typing module for type annotations and add noqa directive for ruff.

--- omlish/logs/configs.py
+++ omlish/logs/configs.py
@@ -1,17 +1,20 @@
+# ruff: noqa: UP006 UP045
+# @omlish-lite
 """
 https://docs.python.org/3/howto/logging.html#configuring-logging
+https://docs.python.org/3/library/logging.config.html#logging-config-dictschema
 """
 import dataclasses as dc
 import typing as ta
 
 
+FilterConfig = ta.Dict[str, ta.Any]  # ta.TypeAlias
+FormatterConfig = ta.Dict[str, ta.Any]  # ta.TypeAlias
+HandlerConfig = ta.Dict[str, ta.Any]  # ta.TypeAlias
+LoggerConfig = ta.Dict[str, ta.Any]  # ta.TypeAlias
+
+
 ##
-
-
-FilterConfig: ta.TypeAlias = dict[str, ta.Any]
-FormatterConfig: ta.TypeAlias = dict[str, ta.Any]
-HandlerConfig: ta.TypeAlias = dict[str, ta.Any]
-LoggerConfig: ta.TypeAlias = dict[str, ta.Any]
 
 
 @dc.dataclass()
@@ -19,8 +22,8 @@ class DictConfig:
     version: int = 1
     incremental: bool = False
     disable_existing_loggers: bool = False
-    filters: dict[str, FilterConfig] = dc.field(default_factory=dict)
-    formatters: dict[str, FormatterConfig] = dc.field(default_factory=dict)
-    handlers: dict[str, HandlerConfig] = dc.field(default_factory=dict)
-    loggers: dict[str, LoggerConfig] = dc.field(default_factory=dict)
-    root: LoggerConfig | None = None
+    filters: ta.Dict[str, FilterConfig] = dc.field(default_factory=dict)
+    formatters: ta.Dict[str, FormatterConfig] = dc.field(default_factory=dict)
+    handlers: ta.Dict[str, HandlerConfig] = dc.field(default_factory=dict)
+    loggers: ta.Dict[str, LoggerConfig] = dc.field(default_factory=dict)
+    root: ta.Optional[LoggerConfig] = None
