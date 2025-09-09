19341fce0c30dc5a49c8c9dd457fa4a3be1f14d3
wrmsr <timwilloney@gmail.com>
2025-09-09 15:06:36 -0700

    Refactor file path handling in test_import_hook
    Improve clarity by using variable assignment for file paths in the loop.

--- omdev/cexts/tests/test_import_hook.py
+++ omdev/cexts/tests/test_import_hook.py
@@ -7,9 +7,9 @@ def test_import_hook():
     if not os.path.isfile(os.path.join(os.getcwd(), 'pyproject.toml')):
         raise RuntimeError('run in project root')
 
-    for fn in os.listdir(os.path.dirname(__file__)):
-        if os.path.isfile(fn) and fn.endswith('.so'):
-            os.unlink(fn)
+    for fn in os.listdir(bp := os.path.dirname(__file__)):
+        if os.path.isfile(fp := os.path.join(bp, fn)) and fn.endswith('.so'):
+            os.unlink(fp)
 
     was_installed = importhook.is_installed()
     try:
