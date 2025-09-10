97bde773ba1e7d8a5c4245f8d7ed6b0492842e64
wrmsr <timwilloney@gmail.com>
2025-09-10 10:46:11 -0700

    Add subprocess utility functions and update dependencies
    Introduce utility functions for subprocess management in `all.py`. Update `tavily-python` and `httplib2` dependencies to latest versions. Update submodule `tinygrad_` to latest commit.

--- omlish/subprocesses/all.py
+++ omlish/subprocesses/all.py
@@ -1,3 +1,6 @@
+import sys as _sys
+import typing as _ta
+
 from .. import lang as _lang  # noqa
 
 
@@ -40,4 +43,93 @@ with _lang.auto_proxy_init(globals()):
     from .wrap import (  # noqa
         subprocess_shell_wrap_exec as shell_wrap_exec,
         subprocess_maybe_shell_wrap_exec as maybe_shell_wrap_exec,
+    )
+
+
+##
+
+
+if _ta.TYPE_CHECKING:
+    from . import sync as _sync  # noqa
+else:
+    _sync = _lang.proxy_import('.sync', __package__)
+
+
+def run(
+        *cmd: str,
+        input: _ta.Any = None,  # noqa
+        timeout: '_lang.TimeoutLike' = None,
+        check: bool = False,
+        capture_output: bool | None = None,
+        **kwargs: _ta.Any,
+) -> 'SubprocessRunOutput':
+    return _sync.subprocesses.run(
+        *cmd,
+        input=input,
+        timeout=timeout,
+        check=check,
+        capture_output=capture_output,
+        **kwargs,
+    )
+
+
+def check_call(
+        *cmd: str,
+        stdout: _ta.Any = _sys.stderr,
+        **kwargs: _ta.Any,
+) -> None:
+    return _sync.subprocesses.check_call(
+        *cmd,
+        stdout=stdout,
+        **kwargs,
+    )
+
+
+def check_output(
+        *cmd: str,
+        **kwargs: _ta.Any,
+) -> bytes:
+    return _sync.subprocesses.check_output(
+        *cmd,
+        **kwargs,
+    )
+
+
+def check_output_str(
+        *cmd: str,
+        **kwargs: _ta.Any,
+) -> str:
+    return _sync.subprocesses.check_output_str(
+        *cmd,
+        **kwargs,
+    )
+
+
+def try_call(
+        *cmd: str,
+        **kwargs: _ta.Any,
+) -> bool:
+    return _sync.subprocesses.try_call(
+        *cmd,
+        **kwargs,
+    )
+
+
+def try_output(
+        *cmd: str,
+        **kwargs: _ta.Any,
+) -> bytes | None:
+    return _sync.subprocesses.try_output(
+        *cmd,
+        **kwargs,
+    )
+
+
+def try_output_str(
+        *cmd: str,
+        **kwargs: _ta.Any,
+) -> str | None:
+    return _sync.subprocesses.try_output_str(
+        *cmd,
+        **kwargs,
     )
