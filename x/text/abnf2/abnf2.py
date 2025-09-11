4348c2a9e41f048c29fd6a1df7d78ce4a70d454e
wrmsr <timwilloney@gmail.com>
2025-09-10 18:05:48 -0700

    Refactor Node classes and update parsing logic
    Remove unused 'name' property from Node classes and adjust Literal class to handle case-insensitive parsing. Update parse method to use Context and yield matches.

--- x/text/abnf2/abnf2.py
+++ x/text/abnf2/abnf2.py
@@ -11,11 +11,6 @@ from omlish.lite.dataclasses import dataclass_cache_hash
 
 @dc.dataclass(frozen=True)
 class Node(lang.Abstract, lang.Sealed):
-    # @property
-    # @abc.abstractmethod
-    # def name(self) -> str:
-    #     raise NotImplementedError
-
     pass
 
 
@@ -23,20 +18,17 @@ class Node(lang.Abstract, lang.Sealed):
 @dataclass_cache_hash()
 @dc.dataclass(frozen=True)
 class ConcatenateNode(Node, lang.Final):
-    name: str
-    children: tuple['Node', ...] | None = None
+    parser: 'Parser'
+    children: tuple['Node', ...]
 
 
 @ta.final
 @dataclass_cache_hash()
 @dc.dataclass(frozen=True)
 class LiteralNode(Node, lang.Final):
+    parser: 'Literal'
     start: int
     length: int
-
-    @property
-    def name(self) -> str:
-        return 'literal'
 
 
 @ta.final
@@ -47,46 +39,63 @@ class Match(lang.Final):
     nodes: tuple[Node, ...]
 
 
-Matches: ta.TypeAlias = frozenset[Match]
-
-
 ##
 
 
 @dc.dataclass(frozen=True)
-class ParseContext:
+class Context:
     source: str
 
 
 class Parser(lang.Abstract):
     @abc.abstractmethod
-    def parse(self, ctx: ParseContext, start: int) -> Matches:
+    def parse(self, ctx: Context, start: int) -> ta.Iterator[Match]:
         raise NotImplementedError
 
 
 ##
 
 
-class Literal(Parser):
+class Literal(Parser, lang.Abstract):
     class Range(ta.NamedTuple):
         lo: int
         hi: int
 
     def __init__(
             self,
-            value:
-    ):
+            value: str | Range,
+            *,
+            case_insensitive: bool | None = None,
+    ) -> None:
+        super().__init__()
 
+        if isinstance(value, str):
+            if case_insensitive:
+                value = value.casefold()
+        elif case_insensitive is not None:
+            raise TypeError('Should not set case_insensitive with non-string literal')
+        self._value = value
+        self._case_insensitive = case_insensitive
 
+    def parse(self, ctx: Context, start: int) -> ta.Iterator[Match]:
+        if not isinstance(value := self._value, str):
+            raise NotImplementedError
 
-
+        if start < len(ctx.source):
+            source = ctx.source[start : start + len(self._value)]
+            if self._case_insensitive:
+                source = source.casefold()
+            if source == value:
+                yield Match(start, (LiteralNode(self, start, len(source)),))
 
 
 ##
 
 
 def _main() -> None:
-    hash(Node('abc', (Node('def'),)))
+    ctx = Context('foo')
+    parser = Literal('foo')
+    print(parser.parse(ctx, 0))
 
 
 if __name__ == '__main__':
