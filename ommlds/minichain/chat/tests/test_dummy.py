8097f30dccd7fec1fd22b5518958b5bda6fd79a3
wrmsr <timwilloney@gmail.com>
2025-09-30 15:07:52 -0700

    Refactor test to use check for type validation
    Replaced direct assertion with check.isinstance to ensure response type is AiMessage, improving code clarity and robustness.

--- ommlds/minichain/chat/tests/test_dummy.py
+++ ommlds/minichain/chat/tests/test_dummy.py
@@ -1,9 +1,12 @@
+from omlish import check
 from omlish import lang
 
+from ..messages import AiMessage
 from ..messages import UserMessage
 from ..services import ChatRequest
 from .dummy import DummyChatService
 
 
 def test_dummy():
-    assert lang.sync_await(DummyChatService.simple(lambda s: s + '!').invoke(ChatRequest([UserMessage('hi')]))).v.c == 'hi!'  # noqa
+    resp = lang.sync_await(DummyChatService.simple(lambda s: s + '!').invoke(ChatRequest([UserMessage('hi')])))
+    assert check.isinstance(check.single(resp.v), AiMessage).c == 'hi!'  # noqa
