6e279bf23d17980b38b4104a260e97506c7cedd2
wrmsr <timwilloney@gmail.com>
2025-10-01 16:35:11 -0700

    ┬─┬ノ(º _ ºノ)

--- omlish/marshal/base/registries.py
+++ omlish/marshal/base/registries.py
@@ -2,13 +2,11 @@
 TODO:
  - col.TypeMap?
   - at least get_any
- - sheesh threadsafe this junk
 """
 import dataclasses as dc
 import threading
 import typing as ta
 
-from ... import collections as col
 from ... import lang
 
 
@@ -21,27 +19,6 @@ class RegistryItem(lang.Abstract):
 
 RegistryItemT = ta.TypeVar('RegistryItemT', bound=RegistryItem)
 RegistryItemU = ta.TypeVar('RegistryItemU', bound=RegistryItem)
-
-
-@dc.dataclass(frozen=True)
-class _KeyRegistryItems(ta.Generic[RegistryItemT]):
-    key: ta.Any
-    items: ta.Sequence[RegistryItemT] = ()
-    item_lists_by_ty: ta.Mapping[type[RegistryItemT], ta.Sequence[RegistryItemT]] = dc.field(default_factory=dict)
-
-    def add(self, *items: RegistryItemT) -> '_KeyRegistryItems':
-        item_lists_by_ty: dict[type[RegistryItemT], list[RegistryItemT]] = {}
-        for i in items:
-            try:
-                l = item_lists_by_ty[type(i)]
-            except KeyError:
-                l = item_lists_by_ty[type(i)] = list(self.item_lists_by_ty.get(type(i), ()))
-            l.append(i)
-        return _KeyRegistryItems(
-            self.key,
-            (*self.items, *items),
-            {**self.item_lists_by_ty, **item_lists_by_ty},
-        )
 
 
 class RegistrySealedError(Exception):
@@ -70,11 +47,35 @@ class Registry(ta.Generic[RegistryItemT]):
 
     #
 
+    @dc.dataclass(frozen=True)
+    class _KeyItems(ta.Generic[RegistryItemU]):
+        key: ta.Any
+        items: ta.Sequence[RegistryItemU] = ()
+        item_lists_by_ty: ta.Mapping[type[RegistryItemU], ta.Sequence[RegistryItemU]] = dc.field(default_factory=dict)
+
+        def add(self, *items: RegistryItemU) -> 'Registry._KeyItems[RegistryItemU]':
+            item_lists_by_ty: dict[type[RegistryItemU], list[RegistryItemU]] = {}
+
+            for i in items:
+                try:
+                    l = item_lists_by_ty[type(i)]
+                except KeyError:
+                    l = item_lists_by_ty[type(i)] = list(self.item_lists_by_ty.get(type(i), ()))
+                l.append(i)
+
+            return Registry._KeyItems(
+                self.key,
+                (*self.items, *items),
+                {**self.item_lists_by_ty, **item_lists_by_ty},
+            )
+
     @dc.dataclass(frozen=True, kw_only=True)
     class _State(ta.Generic[RegistryItemU]):
-        dct: ta.Mapping[ta.Any, _KeyRegistryItems[RegistryItemU]]
-        id_dct: ta.Mapping[ta.Any, _KeyRegistryItems[RegistryItemU]]
+        dct: ta.Mapping[ta.Any, 'Registry._KeyItems[RegistryItemU]']
+        id_dct: ta.Mapping[ta.Any, 'Registry._KeyItems[RegistryItemU]']
         version: int
+
+        #
 
         def register(
                 self,
@@ -85,16 +86,19 @@ class Registry(ta.Generic[RegistryItemT]):
             if not items:
                 return self
 
-            dct: ta.Any = self.dct if not identity else self.id_dct
-            if (sr := dct.get(key)) is None:
-                sr = _KeyRegistryItems(key)
-            sr.add(*items)
+            sr_dct: ta.Any = self.dct if not identity else self.id_dct
+            if (sr := sr_dct.get(key)) is None:
+                sr = Registry._KeyItems(key)
+            sr = sr.add(*items)
+            new = {key: sr}
 
             return Registry._State(
-                dct={**self.dct, **dct} if not identity else self.dct,
-                id_dct={**self.id_dct, **dct} if identity else self.id_dct,
+                dct={**self.dct, **new} if not identity else self.dct,
+                id_dct={**self.id_dct, **new} if identity else self.id_dct,
                 version=self.version + 1,
             )
+
+        #
 
         _get_cache: dict[ta.Any, ta.Sequence[RegistryItem]] = dc.field(default_factory=dict)
 
@@ -209,4 +213,4 @@ class Registry(ta.Generic[RegistryItemT]):
             *,
             identity: bool | None = None,
     ) -> ta.Sequence[RegistryItemU]:
-        return self._state.get_of(key, item_ty, identity=identity)
+        return self._state.get_of(key, item_ty, identity=identity)  # type: ignore[return-value]
