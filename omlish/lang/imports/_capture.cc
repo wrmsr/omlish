96e160faa8fae168c93fb6b5259629ce67306b16
wrmsr <timwilloney@gmail.com>
2025-10-14 11:47:43 -0700

    Update interpreter GIL support flag in capture module
    Change the flag to indicate per-interpreter GIL support, aligning with updated interpreter capabilities.

--- omlish/lang/imports/_capture.cc
+++ omlish/lang/imports/_capture.cc
@@ -76,7 +76,7 @@ static PyMethodDef capture_methods[] = {
 
 static struct PyModuleDef_Slot capture_slots[] = {
     {Py_mod_gil, Py_MOD_GIL_NOT_USED},
-    {Py_mod_multiple_interpreters, Py_MOD_MULTIPLE_INTERPRETERS_SUPPORTED},
+    {Py_mod_multiple_interpreters, Py_MOD_PER_INTERPRETER_GIL_SUPPORTED},
     {0, NULL}
 };
 
