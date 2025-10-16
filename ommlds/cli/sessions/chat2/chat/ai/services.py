9922092110ca47372022f709c1d0b0893b77841f
wrmsr <timwilloney@gmail.com>
2025-10-16 11:39:34 -0700

    Add options parameter to ChatChoicesService classes
    Introduce an optional 'options' parameter to both ChatChoicesServiceAiChatGenerator and ChatChoicesStreamServiceAiChatGenerator classes, allowing customization of chat choices requests.

--- ommlds/cli/sessions/chat2/chat/ai/services.py
+++ ommlds/cli/sessions/chat2/chat/ai/services.py
@@ -1,3 +1,5 @@
+import typing as ta
+
 from omlish import check
 
 from ...... import minichain as mc
@@ -8,14 +10,26 @@ from .types import AiChoiceDeltaCallback
 ##
 
 
+ChatChoicesServiceOptions = ta.NewType('ChatChoicesServiceOptions', ta.Sequence[mc.ChatChoicesOptions])
+
+
+##
+
+
 class ChatChoicesServiceAiChatGenerator(AiChatGenerator):
-    def __init__(self, service: mc.ChatChoicesService) -> None:
+    def __init__(
+            self,
+            service: mc.ChatChoicesService,
+            *,
+            options: ChatChoicesServiceOptions | None = None,
+    ) -> None:
         super().__init__()
 
         self._service = service
+        self._options = options
 
     async def get_next_ai_messages(self, chat: mc.Chat) -> mc.AiChat:
-        resp = await self._service.invoke(mc.ChatChoicesRequest(chat))
+        resp = await self._service.invoke(mc.ChatChoicesRequest(chat, self._options or []))
 
         return check.single(resp.v).ms
 
@@ -25,17 +39,19 @@ class ChatChoicesStreamServiceAiChatGenerator(AiChatGenerator):
             self,
             service: mc.ChatChoicesStreamService,
             *,
+            options: ChatChoicesServiceOptions | None = None,
             on_delta: AiChoiceDeltaCallback | None = None,
     ) -> None:
         super().__init__()
 
         self._service = service
+        self._options = options
         self._on_delta = on_delta
 
     async def get_next_ai_messages(self, chat: mc.Chat) -> mc.AiChat:
         lst: list[str] = []
 
-        async with (await self._service.invoke(mc.ChatChoicesStreamRequest(chat))).v as st_resp:
+        async with (await self._service.invoke(mc.ChatChoicesStreamRequest(chat, self._options or []))).v as st_resp:
             async for o in st_resp:
                 choice = check.single(o.choices)
 
