f5baf5acb2bc27599d009c3cfc992abef5b78434
wrmsr <timwilloney@gmail.com>
2025-10-16 14:45:38 -0700

    Add test for cyclic dependency exception
    Introduce a test to verify that a cyclic dependency raises the expected CyclicDependencyError. Include a placeholder for a future test to handle cyclic proxies.

---
+++ omlish/inject/tests/test_proxy.py
@@ -0,0 +1,47 @@
+"""
+TODO:
+ - no config, explicit opt-in allowing a cyclic proxy for a specific type, bind a CyclicProxyProvider or smth
+"""
+import dataclasses as dc
+
+import pytest
+
+from ... import inject as inj
+
+
+@dc.dataclass()
+class CycA:
+    b: 'CycB'
+
+
+@dc.dataclass()
+class CycB:
+    a: CycA
+
+
+def test_cyclic_exc():
+    injector = inj.create_injector(
+        inj.bind(CycA, singleton=True),
+        inj.bind(CycB, singleton=True),
+    )
+    with pytest.raises(inj.CyclicDependencyError):
+        a = injector[CycA]  # noqa
+        b = injector[CycB]  # noqa
+
+
+# FIXME:
+# def test_cyclic_prox():
+#     binder = bind_.create_binder()
+#     binder.bind_class(CycA, as_singleton=True)
+#     binder.bind_class(CycB, as_singleton=True)
+#     injector = inj.create_injector(binder, config=inj.InjectorConfig(enable_cyclic_proxies=True))
+#     a = injector[CycA]
+#     b = injector[CycB]
+#     a.x = 5
+#     b.y = 10
+#     assert a.b is b
+#     assert b.a is not a
+#     assert b.a.b is b
+#     assert a.b.a is not a
+#     assert a.b.y == 10
+#     assert b.a.x == 5
