52ec1d114aaed04959468fbfdfef9cd01cf34bb9
wrmsr <timwilloney@gmail.com>
2025-10-16 13:41:51 -0700

    Correct test assertion and update dependencies
    Fix the test assertion in test_helpers.py to check for the correct file name. Update development and frozen requirements to newer versions for ruff, boto3, botocore, and jupyter_core to ensure compatibility and security.

--- omlish/inject/tests/test_helpers.py
+++ omlish/inject/tests/test_helpers.py
@@ -6,6 +6,6 @@ def test_const_fn():
     assert cf() == 420
 
     assert cf.origins.lst
-    assert 'test_utils.py' in repr(cf.origins.lst[0])
+    assert 'test_helpers.py' in repr(cf.origins.lst[0])
 
     assert inj.create_injector(inj.bind(int, to_fn=inj.ConstFn(421)))[int] == 421
