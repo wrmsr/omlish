a4d3e537893c5997be33ca0adbf4fe9fa9a855d6
wrmsr <timwilloney@gmail.com>
2025-10-16 15:57:36 -0700

    Add test for wrappers injection with multiple bindings
    Introduce a new test to verify the behavior of the injector with multiple bindings and private scopes, ensuring correct provision of tagged instances.

--- omlish/inject/tests/test_wrappers.py
+++ omlish/inject/tests/test_wrappers.py
@@ -54,3 +54,17 @@ def test_wrappers() -> None:
         item='foo',
     )
     assert list(thing_doer.do_thing()) == ['3', '2', '1', 'foo']
+
+
+def test_wrappers_injection():
+    injector = inj.create_injector(
+        inj.bind(ThingDoer, tag=3, to_const=ConstThingDoer('2', '1', '3')),
+
+        inj.private(
+            inj.bind(ThingDoer, to_key=inj.Key(ThingDoer, tag=3)),
+            inj.bind(ThingDoer, tag=2, to_ctor=SortingThingDoer, expose=True),
+        ),
+    )
+    assert list(injector.provide(inj.Key(ThingDoer, tag=3)).do_thing()) == ['2', '1', '3']
+    assert list(injector.provide(inj.Key(ThingDoer, tag=2)).do_thing()) == ['1', '2', '3']
+    # assert list(injector[ThingDoer].do_thing()) == ['3', '2', '1', 'foo']
