e4334e54317302fd4f8082dba0049dc66d9ba173
wrmsr <timwilloney@gmail.com>
2025-10-16 10:43:40 -0700

    Add in-memory and storage-based chat state management
    Introduce `InMemoryChatStateManager` and `StateStorageChatStateManager` to handle chat state management. These classes provide methods to get, clear, and extend chat states, supporting both in-memory and persistent storage solutions.

---
+++ x/minichain/mc5/chat/state/storage.py
@@ -0,0 +1,53 @@
+import dataclasses as dc
+
+from omlish import check
+from omlish import lang
+from ommlds import minichain as mc
+from ommlds.cli.state import StateStorage
+
+from .types import ChatState
+from .types import ChatStateManager
+
+
+##
+
+
+class StateStorageChatStateManager(ChatStateManager):
+    def __init__(
+            self,
+            *,
+            storage: StateStorage,
+            key: str = 'chat',
+    ) -> None:
+        super().__init__()
+
+        self._storage = storage
+        self._key = check.non_empty_str(key)
+
+        self._state: ChatState | None = None
+
+    async def get_state(self) -> ChatState:
+        if self._state is not None:
+            return self._state
+        state: ChatState | None = self._storage.load_state(self._key, ChatState)
+        if state is None:
+            state = ChatState()
+        self._state = state
+        return state
+
+    async def clear_state(self) -> ChatState:
+        state = ChatState()
+        self._storage.save_state(self._key, state, ChatState)
+        self._state = state
+        return state
+
+    async def extend_chat(self, chat_additions: mc.Chat) -> ChatState:
+        state = await self.get_state()
+        state = dc.replace(
+            state,
+            chat=[*state.chat, *chat_additions],
+            updated_at=lang.utcnow(),
+        )
+        self._storage.save_state(self._key, state, ChatState)
+        self._state = state
+        return state
