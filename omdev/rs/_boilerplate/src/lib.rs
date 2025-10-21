e7b6df2cf1d6b4d77f8ad17693d10449b7dbc750
wrmsr <timwilloney@gmail.com>
2025-10-21 09:44:40 -0700

    2025-10-21T16:44:40Z

---
+++ omdev/rs/_boilerplate/src/lib.rs
@@ -0,0 +1,11 @@
+use pyo3::prelude::*;
+
+#[pymodule]
+mod _boilerplate {
+    use pyo3::prelude::*;
+
+    #[pyfunction]
+    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+        Ok((a + b).to_string())
+    }
+}
