11eb16731c415a7fb6f56683e364bbcae94c12d1
wrmsr <timwilloney@gmail.com>
2025-10-24 16:33:53 -0700

    Add tests for Ollama chat services
    Introduce test cases for OllamaChatChoicesService and OllamaChatChoicesStreamService to ensure proper functionality and response handling.

---
+++ ommlds/minichain/backends/impls/ollama/tests/test_chat.py
@@ -0,0 +1,43 @@
+import pytest
+
+from omlish import lang
+from omlish.http import all as http
+
+from .....chat.messages import UserMessage
+from .....services import Request
+from ..chat import OllamaChatChoicesService
+from ..chat import OllamaChatChoicesStreamService
+
+
+@pytest.mark.not_docker_guest
+@pytest.mark.high_mem
+@pytest.mark.asyncs('asyncio')
+async def test_chat():
+    try:
+        http.request(OllamaChatChoicesService.DEFAULT_API_URL.v)
+    except http.HttpClientError:
+        pytest.skip('No ollama server')
+
+    with lang.maybe_managing(OllamaChatChoicesService()) as llm:
+        q = 'Is a bird a mammal?'
+
+        resp = await llm.invoke(Request([UserMessage(q)]))
+        print(resp)
+        assert resp.v
+
+
+@pytest.mark.not_docker_guest
+@pytest.mark.high_mem
+@pytest.mark.asyncs('asyncio')
+async def test_chat_stream():
+    try:
+        http.request(OllamaChatChoicesService.DEFAULT_API_URL.v)
+    except http.HttpClientError:
+        pytest.skip('No ollama server')
+
+    with lang.maybe_managing(OllamaChatChoicesStreamService()) as llm:
+        q = 'Is a bird a mammal?'
+
+        async with (resp := await llm.invoke(Request([UserMessage(q)]))).v as it:  # noqa
+            async for o in it:
+                print(o)
