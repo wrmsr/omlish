24d2c2516aff178be6acb3095c250b9fdb5d40f2
wrmsr <timwilloney@gmail.com>
2025-11-04 23:18:46 -0800

    Refactor import structure with auto_proxy_init
    Utilize _lang.auto_proxy_init to streamline imports and add fetch_model import.

--- ommlds/backends/tinygrad/models/llama3/__init__.py
+++ ommlds/backends/tinygrad/models/llama3/__init__.py
@@ -4,17 +4,25 @@
 # https://github.com/tinygrad/tinygrad/blob/ef17af85c6d3f84d1e1cc084d6dee8ced3d1a33e/extra/models/llama.py
 
 
-from .llm import (  # noqa
-    Llama3Llm,
-    run_llm,
-    run_llm_to_stop,
-    RunLlmToStopResult,
-)
+from omlish import lang as _lang
 
-from .tokenization import (  # noqa
-    Tokenizer,
-)
 
-from .transformer import (  # noqa
-    Transformer,
-)
+with _lang.auto_proxy_init(globals()):
+    from .fetch import (  # noqa
+        fetch_model,
+    )
+
+    from .llm import (  # noqa
+        Llama3Llm,
+        run_llm,
+        run_llm_to_stop,
+        RunLlmToStopResult,
+    )
+
+    from .tokenization import (  # noqa
+        Tokenizer,
+    )
+
+    from .transformer import (  # noqa
+        Transformer,
+    )
