f62bf9b36b753139cd9ad2e960a2253052dd1081
wrmsr <timwilloney@gmail.com>
2025-11-10 14:15:47 -0800

    Refactor CLI to use profile-based configuration
    Consolidated CLI logic into a single file and introduced a profile-based system for handling different session configurations. Removed redundant main2.py file. This change improves maintainability and scalability of the CLI tool.

--- ommlds/cli/main.py
+++ ommlds/cli/main.py
@@ -1,182 +1,332 @@
 """
-See:
- - https://github.com/simonw/llm
- - https://github.com/TheR1D/shell_gpt
- - https://github.com/paul-gauthier/aider
+TODO:
+ - bootstrap lol
 """
+import abc
 import functools
 import typing as ta
 
 import anyio
 
 from omlish import check
+from omlish import dataclasses as dc
 from omlish import inject as inj
 from omlish import lang
 from omlish.argparse import all as ap
 from omlish.logs import all as logs
-from omlish.subprocesses.editor import edit_text_with_user_editor
-from omlish.subprocesses.sync import subprocesses
 
-from .. import minichain as mc
 from .inject import bind_main
-from .rendering.configs import RenderingConfig
 from .secrets import install_secrets
 from .sessions.base import Session
-from .sessions.chat.backends.configs import BackendConfig
-from .sessions.chat.chat.ai.configs import AiConfig
-from .sessions.chat.chat.state.configs import StateConfig
-from .sessions.chat.chat.user.configs import UserConfig
 from .sessions.chat.configs import ChatConfig
-from .sessions.chat.tools.configs import ToolsConfig
 from .sessions.completion.configs import CompletionConfig
 from .sessions.embedding.configs import EmbeddingConfig
-
-
-if ta.TYPE_CHECKING:
-    import PIL.Image as pimg  # noqa
-else:
-    pimg = lang.proxy_import('PIL.Image')
 
 
 ##
 
 
-async def _a_main(args: ta.Any = None) -> None:
-    parser = ap.ArgumentParser()
-    parser.add_argument('prompt', nargs='*')
+MAIN_EXTRA_ARGS: ta.Sequence[ap.Arg] = [
+    ap.arg('-v', '--verbose', action='store_true'),
+]
 
-    parser.add_argument('-b', '--backend', default='openai')
 
-    parser.add_argument('-m', '--model-name')
-
-    parser.add_argument('-C', '--completion', action='store_true')
-
-    parser.add_argument('-n', '--new', action='store_true')
-    parser.add_argument('--ephemeral', action='store_true')
-
-    parser.add_argument('-e', '--editor', action='store_true')
-    parser.add_argument('-i', '--interactive', action='store_true')
-    parser.add_argument('-c', '--code', action='store_true')
-    parser.add_argument('-s', '--stream', action='store_true')
-    parser.add_argument('-M', '--markdown', action='store_true')
-
-    parser.add_argument('-E', '--embed', action='store_true')
-    parser.add_argument('-j', '--image', action='store_true')
-
-    parser.add_argument('-v', '--verbose', action='store_true')
-
-    parser.add_argument('--enable-fs-tools', action='store_true')
-    parser.add_argument('--enable-todo-tools', action='store_true')
-    parser.add_argument('--enable-unsafe-tools-do-not-use-lol', action='store_true')
-    parser.add_argument('--enable-test-weather-tool', action='store_true')
-    parser.add_argument('--dangerous-no-tool-confirmation', action='store_true')
-
-    args = parser.parse_args(args)
-
-    #
-
+def _process_main_extra_args(args: ap.Namespace) -> None:
     if args.verbose:
         logs.configure_standard_logging('DEBUG')
     else:
         logs.configure_standard_logging('INFO')
         logs.silence_noisy_loggers()
 
+
+##
+
+
+class Profile(lang.Abstract):
+    @abc.abstractmethod
+    def run(self, argv: ta.Sequence[str]) -> ta.Awaitable[None]:
+        raise NotImplementedError
+
+
+##
+
+
+# class ChatAspect(lang.Abstract):
+#     def get_parser_args(self) -> ta.Sequence[ap.Arg]: ...
+#     def set_args(self, args: ap.Namespace) -> None: ...
+#     def configure(self, cfg: ChatConfig) -> ChatConfig: ...
+
+
+class ChatProfile(Profile):
+    _args: ap.Namespace
+
     #
 
-    content: mc.Content | None
+    BACKEND_ARGS: ta.ClassVar[ta.Sequence[ap.Arg]] = [
+        ap.arg('-b', '--backend', group='backend'),
+    ]
 
-    if args.image:
-        content = mc.ImageContent(pimg.open(check.non_empty_str(check.single(args.prompt))))
+    def configure_backend(self, cfg: ChatConfig) -> ChatConfig:
+        return dc.replace(
+            cfg,
+            backend=dc.replace(
+                cfg.backend,
+                backend=self._args.backend,
+            ),
+        )
 
-    elif args.editor:
-        check.arg(not args.prompt)
-        if (ec := edit_text_with_user_editor('', subprocesses)) is None:
-            return
-        content = ec
+    #
 
-    elif args.interactive:
-        if args.prompt:
-            raise ValueError('Must not provide prompt')
-        content = None
+    INPUT_ARGS: ta.ClassVar[ta.Sequence[ap.Arg]] = [
+        ap.arg('message', nargs='*', group='input'),
+        ap.arg('-i', '--interactive', action='store_true', group='input'),
+        ap.arg('-e', '--editor', action='store_true', group='input'),
+    ]
 
-    elif args.code:
-        if args.prompt:
-            content = ' '.join(args.prompt)
+    def configure_input(self, cfg: ChatConfig) -> ChatConfig:
+        if self._args.editor:
+            check.arg(not self._args.interactive)
+            check.arg(not self._args.message)
+            raise NotImplementedError
+
+        elif self._args.interactive:
+            check.arg(not self._args.message)
+            return dc.replace(
+                cfg,
+                user=dc.replace(
+                    cfg.user,
+                    interactive=True,
+                ),
+            )
+
+        elif self._args.message:
+            # TODO: '-' -> stdin
+            return dc.replace(
+                cfg,
+                user=dc.replace(
+                    cfg.user,
+                    initial_user_content=' '.join(self._args.message),
+                ),
+            )
+
         else:
-            content = None
-
-    elif not args.prompt:
-        raise ValueError('Must provide prompt')
+            raise ValueError('Must specify input')
 
     #
 
-    install_secrets()
+    STATE_ARGS: ta.ClassVar[ta.Sequence[ap.Arg]] = [
+        ap.arg('-n', '--new', action='store_true', group='state'),
+        ap.arg('--ephemeral', action='store_true', group='state'),
+    ]
+
+    def configure_state(self, cfg: ChatConfig) -> ChatConfig:
+        return dc.replace(
+            cfg,
+            state=dc.replace(
+                cfg.state,
+                state='ephemeral' if self._args.ephemeral else 'new' if self._args.new else 'continue',
+            ),
+        )
 
     #
 
-    session_cfg: ta.Any
+    OUTPUT_ARGS: ta.ClassVar[ta.Sequence[ap.Arg]] = [
+        ap.arg('-s', '--stream', action='store_true', group='output'),
+        ap.arg('-M', '--markdown', action='store_true', group='output'),
+    ]
 
-    if args.embed:
-        session_cfg = EmbeddingConfig(
-            check.not_none(content),  # noqa
-            backend=args.backend,
+    def configure_output(self, cfg: ChatConfig) -> ChatConfig:
+        return dc.replace(
+            cfg,
+            ai=dc.replace(
+                cfg.ai,
+                stream=bool(self._args.stream),
+            ),
+            rendering=dc.replace(
+                cfg.rendering,
+                markdown=bool(self._args.markdown),
+            ),
         )
 
-    elif args.completion:
-        session_cfg = CompletionConfig(
-            check.not_none(content),  # noqa
-            backend=args.backend,
+    #
+
+    TOOLS_ARGS: ta.ClassVar[ta.Sequence[ap.Arg]] = [
+        ap.arg('--enable-fs-tools', action='store_true', group='tools'),
+        ap.arg('--enable-todo-tools', action='store_true', group='tools'),
+        # ap.arg('--enable-unsafe-tools-do-not-use-lol', action='store_true', group='tools'),
+        ap.arg('--enable-test-weather-tool', action='store_true', group='tools'),
+    ]
+
+    def configure_tools(self, cfg: ChatConfig) -> ChatConfig:
+        return dc.replace(
+            cfg,
+            ai=dc.replace(
+                cfg.ai,
+                enable_tools=(
+                    self._args.enable_fs_tools or
+                    self._args.enable_todo_tools or
+                    # self._args.enable_unsafe_tools_do_not_use_lol or
+                    self._args.enable_test_weather_tool or
+                    self._args.code
+                ),
+            ),
+            tools=dc.replace(
+                cfg.tools,
+                enabled_tools={  # noqa
+                    *(cfg.tools.enabled_tools or []),
+                    *(['fs'] if self._args.enable_fs_tools else []),
+                    *(['todo'] if self._args.enable_todo_tools else []),
+                    *(['weather'] if self._args.enable_test_weather_tool else []),
+                },
+            ),
         )
 
-    else:
-        system_content: mc.Content | None = None
-        if (args.new or args.ephemeral) and args.code:
+    #
+
+    CODE_CONFIG: ta.ClassVar[ta.Sequence[ap.Arg]] = [
+        ap.arg('-c', '--code', action='store_true', group='code'),
+    ]
+
+    def configure_code(self, cfg: ChatConfig) -> ChatConfig:
+        if not self._args.code:
+            return cfg
+
+        cfg = dc.replace(
+            cfg,
+            ai=dc.replace(
+                cfg.ai,
+                enable_tools=True,
+            ),
+        )
+
+        if self._args.new or self._args.ephemeral:
             from ..minichain.lib.code.prompts import CODE_AGENT_SYSTEM_PROMPT
             system_content = CODE_AGENT_SYSTEM_PROMPT
 
-        session_cfg = ChatConfig(
-            backend=BackendConfig(
-                backend=args.backend,
-            ),
-            ai=AiConfig(
-                stream=bool(args.stream),
-                enable_tools=(
-                    args.enable_fs_tools or
-                    args.enable_todo_tools or
-                    args.enable_unsafe_tools_do_not_use_lol or
-                    args.enable_test_weather_tool or
-                    args.code
+            cfg = dc.replace(
+                cfg,
+                user=dc.replace(
+                    cfg.user,
+                    initial_system_content=system_content,
                 ),
-            ),
-            state=StateConfig(
-                state='ephemeral' if args.ephemeral else 'new' if args.new else 'continue',
-            ),
-            user=UserConfig(
-                initial_system_content=system_content,
-                initial_user_content=content,  # noqa
-                interactive=bool(args.interactive),
-            ),
-            rendering=RenderingConfig(
-                markdown=bool(args.markdown),
-            ),
-            tools=ToolsConfig(
-                enabled_tools={  # noqa
-                    *(['fs'] if args.enable_fs_tools else []),
-                    *(['todo'] if args.enable_todo_tools else []),
-                    *(['weather'] if args.enable_test_weather_tool else []),
-                    # FIXME: enable_unsafe_tools_do_not_use_lol
-                },
-                dangerous_no_confirmation=bool(args.dangerous_no_tool_confirmation),
-            ),
-        )
+            )
+
+        return cfg
 
     #
 
-    with inj.create_managed_injector(bind_main(
-            session_cfg=session_cfg,
-            enable_backend_strings=isinstance(session_cfg, ChatConfig),
-    )) as injector:
-        await injector[Session].run()
+    async def run(self, argv: ta.Sequence[str]) -> None:
+        parser = ap.ArgumentParser()
+
+        for grp_name, grp_args in [
+            ('backend', self.BACKEND_ARGS),
+            ('input', self.INPUT_ARGS),
+            ('state', self.STATE_ARGS),
+            ('output', self.OUTPUT_ARGS),
+            ('tools', self.TOOLS_ARGS),
+            ('code', self.CODE_CONFIG),
+        ]:
+            grp = parser.add_argument_group(grp_name)
+            for a in grp_args:
+                grp.add_argument(*a.args, **a.kwargs)
+
+        self._args = parser.parse_args(argv)
+
+        cfg = ChatConfig()
+        cfg = self.configure_backend(cfg)
+        cfg = self.configure_input(cfg)
+        cfg = self.configure_state(cfg)
+        cfg = self.configure_output(cfg)
+        cfg = self.configure_tools(cfg)
+        cfg = self.configure_code(cfg)
+
+        with inj.create_managed_injector(bind_main(
+                session_cfg=cfg,
+                enable_backend_strings=True,
+        )) as injector:
+            await injector[Session].run()
+
+
+##
+
+
+class CompletionProfile(Profile):
+    async def run(self, argv: ta.Sequence[str]) -> None:
+        parser = ap.ArgumentParser()
+        parser.add_argument('prompt', nargs='*')
+        parser.add_argument('-b', '--backend', default='openai')
+        args = parser.parse_args(argv)
+
+        content = ' '.join(args.prompt)
+
+        cfg = CompletionConfig(
+            check.non_empty_str(content),
+            backend=args.backend,
+        )
+
+        with inj.create_managed_injector(bind_main(
+                session_cfg=cfg,
+        )) as injector:
+            await injector[Session].run()
+
+
+##
+
+
+class EmbedProfile(Profile):
+    async def run(self, argv: ta.Sequence[str]) -> None:
+        parser = ap.ArgumentParser()
+        parser.add_argument('prompt', nargs='*')
+        parser.add_argument('-b', '--backend', default='openai')
+        args = parser.parse_args(argv)
+
+        content = ' '.join(args.prompt)
+
+        cfg = EmbeddingConfig(
+            check.non_empty_str(content),
+            backend=args.backend,
+        )
+
+        with inj.create_managed_injector(bind_main(
+                session_cfg=cfg,
+        )) as injector:
+            await injector[Session].run()
+
+
+##
+
+
+PROFILE_TYPES: ta.Mapping[str, type[Profile]] = {
+    'chat': ChatProfile,
+    'complete': CompletionProfile,
+    'embed': EmbedProfile,
+}
+
+
+##
+
+
+MAIN_PROFILE_ARGS: ta.Sequence[ap.Arg] = [
+    ap.arg('-p', '--profile', default='chat'),
+    ap.arg('args', nargs=ap.REMAINDER),
+]
+
+
+async def _a_main(argv: ta.Any = None) -> None:
+    parser = ap.ArgumentParser()
+
+    for a in [*MAIN_PROFILE_ARGS, *MAIN_EXTRA_ARGS]:
+        parser.add_argument(*a.args, **a.kwargs)
+
+    args, unk_args = parser.parse_known_args(argv)
+
+    _process_main_extra_args(args)
+
+    install_secrets()
+
+    profile_cls = PROFILE_TYPES[args.profile]
+    profile = profile_cls()
+    await profile.run([*unk_args, *args.args])
 
 
 def _main(args: ta.Any = None) -> None:
@@ -185,7 +335,6 @@ def _main(args: ta.Any = None) -> None:
             _a_main,
             args,
         ),
-        # backend='trio',
     )  # noqa
 
 
