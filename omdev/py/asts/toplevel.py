8867d1378a27e2ea45f25c1db2d4793e7a06d13d
wrmsr <timwilloney@gmail.com>
2025-11-12 10:33:05 -0800

    Add TopLevelCallFinder for AST analysis and update dependencies
    Introduce a new module for finding top-level calls in ASTs, enabling basic static analysis. Update several dependencies to their latest versions for improved stability and performance.

---
+++ omdev/py/asts/toplevel.py
@@ -0,0 +1,114 @@
+import ast
+import dataclasses as dc
+
+from omlish import check
+
+
+##
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class TopLevelImport:
+    module: str
+    name: str
+    alias: str | None
+    node: ast.Import | ast.ImportFrom
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class TopLevelCall:
+    node: ast.Call
+    imp: TopLevelImport
+
+
+class _TopLevelCallFinderVisitor(ast.NodeVisitor):
+    """
+    Quite, quite lame, but sufficient for immediate needs. Accurate static symbol resolution is in development but
+    can't block other work.
+    """
+
+    def __init__(self, module_name: str) -> None:
+        super().__init__()
+
+        self.module_name = module_name
+        self.module_name_parts = module_name.split('.')
+
+        self.imports: dict[str, TopLevelImport] = {}
+        self.calls: list[TopLevelCall] = []
+
+    #
+
+    def visit_Import(self, node: ast.Import) -> None:
+        for alias in node.names:
+            name = alias.asname if alias.asname else alias.name
+            self.imports[name] = TopLevelImport(
+                module=alias.name,
+                name=alias.name,
+                alias=alias.asname,
+                node=node,
+            )
+
+        self.generic_visit(node)
+
+    def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
+        for alias in node.names:
+            if node.level:
+                level = node.level - 1
+                check.state(level < len(self.module_name_parts))
+                module = '.'.join([
+                    *self.module_name_parts[:level],
+                    *([node.module] if node.module else []),
+                ])
+            else:
+                module = check.not_none(node.module)
+            name = alias.asname if alias.asname else alias.name
+            self.imports[name] = TopLevelImport(
+                module=module,
+                name=alias.name,
+                alias=alias.asname,
+                node=node,
+            )
+
+        self.generic_visit(node)
+
+    #
+
+    def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> None:
+        pass
+
+    def visit_ClassDef(self, node: ast.ClassDef) -> None:
+        pass
+
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
+        pass
+
+    #
+
+    def handle_call(self, node: ast.Call) -> None:
+        if not (
+                isinstance(attr := node.func, ast.Attribute) and
+                isinstance(attr.ctx, ast.Load) and
+                isinstance(name := attr.value, ast.Name) and
+                isinstance(name.ctx, ast.Load)
+        ):
+            return
+
+        if (imp := self.imports.get(name.id)) is None:
+            return
+
+        self.calls.append(TopLevelCall(
+            node=node,
+            imp=imp,
+        ))
+
+    def visit_Call(self, node: ast.Call) -> None:
+        self.handle_call(node)
+
+
+def find_module_top_level_calls(
+        module: ast.Module,
+        module_name: str,
+) -> list[TopLevelCall]:
+    visitor = _TopLevelCallFinderVisitor(module_name)
+    visitor.visit(module)
+    return visitor.calls
