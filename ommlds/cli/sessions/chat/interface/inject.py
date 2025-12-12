1c3761eb4f7be47ebd79b63cd3810d9a5c51b2bd
wrmsr <timwilloney@gmail.com>
2025-12-11 18:35:54 -0800

    (╯°□°)╯︵ ┻━┻

--- ommlds/cli/sessions/chat/interface/inject.py
+++ ommlds/cli/sessions/chat/interface/inject.py
@@ -11,5 +11,35 @@ with lang.auto_proxy_import(globals()):
 
 
 def bind_interface(cfg: InterfaceConfig = InterfaceConfig()) -> inj.Elements:
-    _textual.bind_textual()
-    raise NotImplementedError
+    els: list[inj.Elemental] = []
+
+    _textual.bind_textual()  # FIXME lol
+
+    if cfg.interactive:
+        if cfg.initial_user_content is not None:
+            async def add_initial_user_content(cm: '_state.ChatStateManager') -> None:
+                await cm.extend_chat([mc.UserMessage(cfg.initial_user_content)])
+
+            els.append(phase_callbacks().bind_item(to_fn=lang.typed_lambda(cm=_state.ChatStateManager)(
+                lambda cm: ChatPhaseCallback(ChatPhase.STARTED, lambda: add_initial_user_content(cm)),
+            )))
+
+            raise NotImplementedError
+
+        els.append(inj.bind(_types.UserChatInput, to_ctor=_interactive.InteractiveUserChatInput, singleton=True))
+
+        els.extend([
+            inj.bind(_inputs_sync.SyncStringInput, to_const=_inputs_sync.InputSyncStringInput(use_readline=cfg.use_readline)),  # noqa
+            inj.bind(_inputs_asyncs.AsyncStringInput, to_ctor=_inputs_asyncs.ThreadAsyncStringInput, singleton=True),
+        ])
+
+    else:
+        if cfg.initial_user_content is None:
+            raise ValueError('Initial user content is required for non-interactive chat')
+
+        els.extend([
+            inj.bind(_oneshot.OneshotUserChatInputInitialChat, to_const=[mc.UserMessage(cfg.initial_user_content)]),
+            inj.bind(_types.UserChatInput, to_ctor=_oneshot.OneshotUserChatInput, singleton=True),
+        ])
+
+    return inj.as_elements(*els)
