01d43b7fada20e03e56594d132d8094bf705d822
wrmsr <timwilloney@gmail.com>
2025-12-30 16:26:59 -0800

    Add RetryService wrapper with configurable max retries and retry count output
    Implemented a new retry wrapper that invokes the underlying service, automatically retrying on exceptions up to a default (or supplied) limit. Introduced `RetryServiceMaxRetriesExceededError` for exhausted retries and `RetryServiceOutput` to expose the number of attempts. Removed the previously commented‑out FirstInWins service scaffolding.

--- ommlds/minichain/_dataclasses.py
+++ ommlds/minichain/_dataclasses.py
@@ -1157,6 +1157,7 @@ def _process_dataclass__b4a9277b8c9559fb4e325a8c35bc504f30ab2294():
         ('ommlds.minichain.chat.tools.execution', 'ToolFn.RawKwargsInput'),
         ('ommlds.minichain.chat.tools.execution', 'ToolFn.RawStringOutput'),
         ('ommlds.minichain.metadata', 'MetadataContainerDataclass'),
+        ('ommlds.minichain.wrappers.retry', 'RetryServiceMaxRetriesExceededError'),
     ),
 )
 def _process_dataclass__e1f7edfe11f2b721d6a656c46e698fedc95461bb():
@@ -19889,6 +19890,153 @@ def _process_dataclass__338ef6c7e8d95c07d43101aa3fb2e5001c70b0ab():
             parts.append(f"first_in_wins_service={self.first_in_wins_service!r}")
             parts.append(f"response_service={self.response_service!r}")
             parts.append(f"service_exceptions={self.service_exceptions!r}")
+            return (
+                f"{self.__class__.__qualname__}("
+                f"{', '.join(parts)}"
+                f")"
+            )
+
+        __repr__.__qualname__ = f"{__dataclass__cls.__qualname__}.__repr__"
+        if '__repr__' in __dataclass__cls.__dict__:
+            raise __dataclass__TypeError(f"Cannot overwrite attribute __repr__ in class {__dataclass__cls.__name__}")
+        setattr(__dataclass__cls, '__repr__', __repr__)
+
+    return _process_dataclass
+
+
+@_register(
+    plan_repr=(
+        "Plans(tup=(CopyPlan(fields=('retry_service', 'num_retries')), EqPlan(fields=('retry_service', 'num_retries')),"
+        " FrozenPlan(fields=('retry_service', 'num_retries'), allow_dynamic_dunder_attrs=False), HashPlan(action='add',"
+        " fields=('retry_service', 'num_retries'), cache=False), InitPlan(fields=(InitPlan.Field(name='retry_service', "
+        "annotation=OpRef(name='init.fields.0.annotation'), default=None, default_factory=None, init=True, override=Fal"
+        "se, field_type=FieldType.INSTANCE, coerce=None, validate=None, check_type=None), InitPlan.Field(name='num_retr"
+        "ies', annotation=OpRef(name='init.fields.1.annotation'), default=None, default_factory=None, init=True, overri"
+        "de=False, field_type=FieldType.INSTANCE, coerce=None, validate=None, check_type=None)), self_param='self', std"
+        "_params=('retry_service', 'num_retries'), kw_only_params=(), frozen=True, slots=False, post_init_params=None, "
+        "init_fns=(), validate_fns=()), ReprPlan(fields=(ReprPlan.Field(name='retry_service', kw_only=False, fn=None), "
+        "ReprPlan.Field(name='num_retries', kw_only=False, fn=None)), id=False, terse=False, default_fn=None)))"
+    ),
+    plan_repr_sha1='ef4a3cb4e4ef425f9b346f3232271cb4f7f5e97e',
+    op_ref_idents=(
+        '__dataclass__init__fields__0__annotation',
+        '__dataclass__init__fields__1__annotation',
+    ),
+    cls_names=(
+        ('ommlds.minichain.wrappers.retry', 'RetryServiceOutput'),
+    ),
+)
+def _process_dataclass__ef4a3cb4e4ef425f9b346f3232271cb4f7f5e97e():
+    def _process_dataclass(
+        *,
+        __dataclass__cls,
+        __dataclass__init__fields__0__annotation,
+        __dataclass__init__fields__1__annotation,
+        __dataclass__FieldFnValidationError,  # noqa
+        __dataclass__FieldTypeValidationError,  # noqa
+        __dataclass__FnValidationError,  # noqa
+        __dataclass__FrozenInstanceError=dataclasses.FrozenInstanceError,  # noqa
+        __dataclass__FunctionType=types.FunctionType,  # noqa
+        __dataclass__HAS_DEFAULT_FACTORY=dataclasses._HAS_DEFAULT_FACTORY,  # noqa
+        __dataclass__MISSING=dataclasses.MISSING,  # noqa
+        __dataclass__None=None,  # noqa
+        __dataclass__TypeError=TypeError,  # noqa
+        __dataclass___recursive_repr=reprlib.recursive_repr,  # noqa
+        __dataclass__isinstance=isinstance,  # noqa
+        __dataclass__object_setattr=object.__setattr__,  # noqa
+        __dataclass__property=property,  # noqa
+    ):
+        def __copy__(self):
+            if self.__class__ is not __dataclass__cls:
+                raise TypeError(self)
+            return __dataclass__cls(  # noqa
+                retry_service=self.retry_service,
+                num_retries=self.num_retries,
+            )
+
+        __copy__.__qualname__ = f"{__dataclass__cls.__qualname__}.__copy__"
+        if '__copy__' in __dataclass__cls.__dict__:
+            raise __dataclass__TypeError(f"Cannot overwrite attribute __copy__ in class {__dataclass__cls.__name__}")
+        setattr(__dataclass__cls, '__copy__', __copy__)
+
+        def __eq__(self, other):
+            if self is other:
+                return True
+            if self.__class__ is not other.__class__:
+                return NotImplemented
+            return (
+                self.retry_service == other.retry_service and
+                self.num_retries == other.num_retries
+            )
+
+        __eq__.__qualname__ = f"{__dataclass__cls.__qualname__}.__eq__"
+        if '__eq__' in __dataclass__cls.__dict__:
+            raise __dataclass__TypeError(f"Cannot overwrite attribute __eq__ in class {__dataclass__cls.__name__}")
+        setattr(__dataclass__cls, '__eq__', __eq__)
+
+        __dataclass___setattr_frozen_fields = {
+            'retry_service',
+            'num_retries',
+        }
+
+        def __setattr__(self, name, value):
+            if (
+                type(self) is __dataclass__cls
+                 or name in __dataclass___setattr_frozen_fields
+            ):
+                raise __dataclass__FrozenInstanceError(f"cannot assign to field {name!r}")
+            super(__dataclass__cls, self).__setattr__(name, value)
+
+        __setattr__.__qualname__ = f"{__dataclass__cls.__qualname__}.__setattr__"
+        if '__setattr__' in __dataclass__cls.__dict__:
+            raise __dataclass__TypeError(f"Cannot overwrite attribute __setattr__ in class {__dataclass__cls.__name__}")
+        setattr(__dataclass__cls, '__setattr__', __setattr__)
+
+        __dataclass___delattr_frozen_fields = {
+            'retry_service',
+            'num_retries',
+        }
+
+        def __delattr__(self, name):
+            if (
+                type(self) is __dataclass__cls
+                 or name in __dataclass___delattr_frozen_fields
+            ):
+                raise __dataclass__FrozenInstanceError(f"cannot delete field {name!r}")
+            super(__dataclass__cls, self).__delattr__(name)
+
+        __delattr__.__qualname__ = f"{__dataclass__cls.__qualname__}.__delattr__"
+        if '__delattr__' in __dataclass__cls.__dict__:
+            raise __dataclass__TypeError(f"Cannot overwrite attribute __delattr__ in class {__dataclass__cls.__name__}")
+        setattr(__dataclass__cls, '__delattr__', __delattr__)
+
+        def __hash__(self):
+            return hash((
+                self.retry_service,
+                self.num_retries,
+            ))
+
+        __hash__.__qualname__ = f"{__dataclass__cls.__qualname__}.__hash__"
+        setattr(__dataclass__cls, '__hash__', __hash__)
+
+        def __init__(
+            self,
+            retry_service: __dataclass__init__fields__0__annotation,
+            num_retries: __dataclass__init__fields__1__annotation,
+        ) -> __dataclass__None:
+            __dataclass__object_setattr(self, 'retry_service', retry_service)
+            __dataclass__object_setattr(self, 'num_retries', num_retries)
+
+        __init__.__qualname__ = f"{__dataclass__cls.__qualname__}.__init__"
+        if '__init__' in __dataclass__cls.__dict__:
+            raise __dataclass__TypeError(f"Cannot overwrite attribute __init__ in class {__dataclass__cls.__name__}")
+        setattr(__dataclass__cls, '__init__', __init__)
+
+        @__dataclass___recursive_repr()
+        def __repr__(self):
+            parts = []
+            parts.append(f"retry_service={self.retry_service!r}")
+            parts.append(f"num_retries={self.num_retries!r}")
             return (
                 f"{self.__class__.__qualname__}("
                 f"{', '.join(parts)}"
--- ommlds/minichain/wrappers/retry.py
+++ ommlds/minichain/wrappers/retry.py
@@ -1,64 +1,76 @@
-# import typing as ta
-#
-# from omlish import check
-# from omlish import dataclasses as dc
-# from omlish import lang
-#
-# from ..services.requests import Request
-# from ..services.responses import Response
-# from ..services.services import Service
-# from ..types import Option
-# from ..types import Output
-#
-#
-# RequestV = ta.TypeVar('RequestV')
-# OptionT = ta.TypeVar('OptionT', bound=Option)
-#
-# ResponseV = ta.TypeVar('ResponseV')
-# OutputT = ta.TypeVar('OutputT', bound=Output)
-#
-#
-# ##
-#
-#
-# @dc.dataclass(frozen=True)
-# class FirstInWinsServiceCancelledError(Exception):
-#     e: BaseException
-#
-#
-# class FirstInWinsServiceExceptionGroup(ExceptionGroup):
-#     pass
-#
-#
-# @dc.dataclass(frozen=True)
-# class FirstInWinsServiceOutput(Output):
-#     first_in_wins_service: 'FirstInWinsService'
-#     response_service: Service
-#     service_exceptions: ta.Mapping[Service, Exception] | None = None
-#
-#
-# class FirstInWinsService(
-#     lang.Abstract,
-#     ta.Generic[
-#         RequestV,
-#         OptionT,
-#         ResponseV,
-#         OutputT,
-#     ],
-# ):
-#     def __init__(
-#             self,
-#             *services: Service[
-#                 Request[
-#                     RequestV,
-#                     OptionT,
-#                 ],
-#                 Response[
-#                     ResponseV,
-#                     OutputT,
-#                 ],
-#             ],
-#     ) -> None:
-#         super().__init__()
-#
-#         self._services = check.not_empty(services)
+"""
+TODO:
+ - tenacity shit
+  - exception filter
+"""
+import typing as ta
+
+from omlish import dataclasses as dc
+
+from ..types import Output
+from .services import WrappedOptionT
+from .services import WrappedOutputT
+from .services import WrappedRequest
+from .services import WrappedRequestV
+from .services import WrappedResponse
+from .services import WrappedResponseV
+from .services import WrappedService
+from .services import WrapperService
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class RetryServiceMaxRetriesExceededError(Exception):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class RetryServiceOutput(Output):
+    retry_service: 'RetryService'
+    num_retries: int
+
+
+class RetryService(
+    WrapperService[
+        WrappedRequestV,
+        WrappedOptionT,
+        WrappedResponseV,
+        WrappedOutputT,
+    ],
+):
+    DEFAULT_MAX_RETRIES: ta.ClassVar[int] = 3
+
+    def __init__(
+            self,
+            service: WrappedService,
+            *,
+            max_retries: int | None = None,
+    ) -> None:
+        super().__init__(service)
+
+        if max_retries is None:
+            max_retries = self.DEFAULT_MAX_RETRIES
+        self._max_retries = max_retries
+
+    async def invoke(self, request: WrappedRequest) -> WrappedResponse:
+        n = 0
+
+        while True:
+            try:
+                resp = await self._service.invoke(request)
+
+            except Exception as e:  # noqa
+                if n < self._max_retries:
+                    n += 1
+                    continue
+
+                raise RetryServiceMaxRetriesExceededError from e
+
+            return resp.with_outputs(RetryServiceOutput(
+                retry_service=self,
+                num_retries=n,
+            ))
+
+        raise RuntimeError  # unreachable
