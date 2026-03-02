8475a6e1bb9186d6fcdb8c57aff37f34eef11c8b
wrmsr <timwilloney@gmail.com>
2026-03-02 09:29:38 -0800

    Add ASGI and WSGI server app support
    Introduce AsgiHandler, AsgiSpec, WsgiHandler, and WsgiSpec in the server.apps package, provide the underlying ASGI driver logic, and refactor the demo pipelines to import these new handlers instead of containing duplicated implementations. Also add an empty package initializer.

--- omxtra/io/pipelines/http/tests/demos/http_server_wsgi.py
+++ omxtra/io/pipelines/http/tests/demos/http_server_wsgi.py
@@ -1,102 +1,21 @@
 # ruff: noqa: UP006 UP045
 # @omlish-lite
 import asyncio
-import dataclasses as dc
 import typing as ta
 
-from omlish.http.headers import HttpHeaders
-from omlish.lite.check import check
-
-from ....core import ChannelPipelineHandler
-from ....core import ChannelPipelineHandlerContext
 from ....core import ChannelPipelineMessages
 from ....core import PipelineChannel
 from ....drivers.asyncio import SimpleAsyncioStreamPipelineChannelDriver
 from ....flow.types import ChannelPipelineFlowMessages
 from ....handlers.flatmap import FlatMapChannelPipelineHandlers
-from ...requests import FullPipelineHttpRequest
-from ...responses import FullPipelineHttpResponse
-from ...responses import PipelineHttpResponseHead
+from ...server.apps.wsgi import WsgiHandler
+from ...server.apps.wsgi import WsgiSpec
 from ...server.requests import PipelineHttpRequestBodyAggregator
 from ...server.requests import PipelineHttpRequestHeadDecoder
 from ...server.responses import PipelineHttpResponseEncoder
 
 
 ##
-
-
-@dc.dataclass(frozen=True)
-class WsgiSpec:
-    app: ta.Any
-    host: str = '127.0.0.1'
-    port: int = 8087
-
-
-##
-
-
-class WsgiHandler(ChannelPipelineHandler):
-    def __init__(self, app: ta.Any) -> None:
-        super().__init__()
-
-        self._app = app
-
-    def inbound(self, ctx: ChannelPipelineHandlerContext, msg: ta.Any) -> None:
-        if not isinstance(msg, FullPipelineHttpRequest):
-            ctx.feed_in(msg)
-            return
-
-        #
-
-        environ = {
-            'REQUEST_METHOD': msg.head.method,
-            'PATH_INFO': msg.head.target,
-        }
-
-        #
-
-        started_response: ta.Optional[ta.Tuple[ta.Any, ta.Any]] = None
-
-        def start_response(status, headers):  # noqa
-            nonlocal started_response
-            check.none(started_response)
-            started_response = (status, headers)
-
-        #
-
-        ret = self._app(environ, start_response)
-
-        #
-
-        status, headers = check.not_none(started_response)
-        status_code_str, _, status_reason = status.partition(' ')
-        status_code = int(status_code_str)
-
-        #
-
-        body: bytes
-        if isinstance(ret, bytes):
-            body = ret
-        elif isinstance(ret, list):
-            body = b''.join(ret)
-        else:
-            raise TypeError(ret)
-
-        #
-
-        resp = FullPipelineHttpResponse(
-            head=PipelineHttpResponseHead(
-                status=status_code,
-                reason=status_reason,
-                headers=HttpHeaders(headers),
-            ),
-            body=body,
-        )
-
-        #
-
-        ctx.feed_out(resp)
-        ctx.feed_out(ChannelPipelineMessages.FinalOutput())
 
 
 def build_wsgi_channel(app: ta.Any) -> PipelineChannel.Spec:
