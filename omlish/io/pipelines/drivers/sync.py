847443458097c6b8e7e4c20d86617fe421fa21d4
wrmsr <timwilloney@gmail.com>
2026-03-09 09:23:13 -0700

    Refactor sync socket pipeline driver to use lazy `_pipeline_` attribute and rename accesses
    Fix WSGI demo to reference the new attribute and introduce an `IterSyncSocketIoPipelineDriver` placeholder class.

--- omlish/io/pipelines/drivers/sync.py
+++ omlish/io/pipelines/drivers/sync.py
@@ -4,6 +4,7 @@
 TODO:
  - can implement sched w/ select
 """
+import collections
 import dataclasses as dc
 import typing as ta
 
@@ -50,7 +51,40 @@ class BaseSyncSocketIoPipelineDriver(ta.Generic[BaseSyncSocketIoPipelineDriverCo
     def config(self) -> BaseSyncSocketIoPipelineDriverConfigT:
         return self._config
 
-    _pipeline: IoPipeline
+    #
+
+    _pipeline_: IoPipeline
+
+    def _pipeline(self) -> ta.Optional[IoPipeline]:
+        try:
+            return self._pipeline_
+        except AttributeError:
+            return None
+
+    def _ensure_pipeline(self) -> IoPipeline:
+        try:
+            return self._pipeline_
+        except AttributeError:
+            pass
+        self._pipeline_ = pipeline = self._make_pipeline()
+        return pipeline
+
+    def _make_pipeline(self) -> IoPipeline:
+        return IoPipeline(dc.replace(
+            self._spec,
+            metadata=(*self._spec.metadata, DriverIoPipelineMetadata(self)),
+        ))
+
+    #
+
+    def __enter__(self) -> 'BaseSyncSocketIoPipelineDriver':  # noqa
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if (pipeline := self._pipeline()) is not None:
+            pipeline.destroy()
+
+    #
 
     def _handle_output(self, msg: ta.Any) -> bool:
         """Returns whether or not to continue running."""
@@ -64,7 +98,7 @@ class BaseSyncSocketIoPipelineDriver(ta.Generic[BaseSyncSocketIoPipelineDriverCo
             return False
 
         elif isinstance(msg, IoPipelineMessages.Defer):
-            self._pipeline.run_deferred(msg)
+            self._pipeline_.run_deferred(msg)
             return True
 
         else:
@@ -89,38 +123,34 @@ class LoopSyncSocketIoPipelineDriver(BaseSyncSocketIoPipelineDriver['LoopSyncSoc
             sock: ta.Any,
             config: ta.Optional[Config] = None,
     ) -> None:
-        super().__init__(
-            spec,
-            sock,
-            config or LoopSyncSocketIoPipelineDriver.Config.DEFAULT,
-        )
+        super().__init__(spec, sock, config or LoopSyncSocketIoPipelineDriver.Config.DEFAULT)
+
+    #
+
+    def __enter__(self) -> 'LoopSyncSocketIoPipelineDriver':  # noqa
+        return self
+
+    #
 
     def _run(self, in_msgs: ta.List[ta.Any]) -> None:
-        try:
-            self._pipeline  # noqa
-        except AttributeError:
-            pass
-        else:
+        if self._pipeline() is not None:
             raise RuntimeError('Already running')
 
-        self._pipeline = IoPipeline(dc.replace(
-            self._spec,
-            metadata=(*self._spec.metadata, DriverIoPipelineMetadata(self)),
-        ))
+        pipeline = self._ensure_pipeline()
 
         try:
-            self._pipeline.feed_initial_input()
+            pipeline.feed_initial_input()
 
             while True:
                 if in_msgs:
-                    self._pipeline.feed_in(*in_msgs)
+                    pipeline.feed_in(*in_msgs)
                     in_msgs.clear()
 
-                while (msg := self._pipeline.output.poll()) is not None:
+                while (msg := pipeline.output.poll()) is not None:
                     if not self._handle_output(msg):
                         return
 
-                if not self._pipeline.saw_final_input:
+                if not pipeline.saw_final_input:
                     b = self._sock.recv(self._config.read_chunk_size)
                     if not b:
                         in_msgs.append(IoPipelineMessages.FinalInput())
@@ -128,7 +158,44 @@ class LoopSyncSocketIoPipelineDriver(BaseSyncSocketIoPipelineDriver['LoopSyncSoc
                         in_msgs.append(b)
 
         finally:
-            self._pipeline.destroy()
+            pipeline.destroy()
 
     def run(self, *in_msgs: ta.Any) -> None:
         self._run(list(in_msgs))
+
+
+##
+
+
+class IterSyncSocketIoPipelineDriver(BaseSyncSocketIoPipelineDriver['IterSyncSocketIoPipelineDriver.Config']):
+    @dc.dataclass(frozen=True)
+    class Config(BaseSyncSocketIoPipelineDriver.Config):
+        DEFAULT: ta.ClassVar['IterSyncSocketIoPipelineDriver.Config']
+
+    #
+
+    def __init__(
+            self,
+            spec: IoPipeline.Spec,
+            sock: ta.Any,
+            config: ta.Optional[Config] = None,
+    ) -> None:
+        super().__init__(spec, sock, config or self.Config.DEFAULT)
+
+        self._input_q: collections.deque[ta.Any] = collections.deque()
+        self._input_q.append(IoPipelineMessages.InitialInput())
+
+    #
+
+    def __enter__(self) -> 'IterSyncSocketIoPipelineDriver':  # noqa
+        return self
+
+    #
+
+    def enqueue(self, *in_msgs: ta.Any) -> None:
+        self._input_q.extend(in_msgs)
+
+    def next(self) -> ta.Optional[ta.Any]:
+        pipeline = self._ensure_pipeline()  # noqa
+
+        raise NotImplementedError
