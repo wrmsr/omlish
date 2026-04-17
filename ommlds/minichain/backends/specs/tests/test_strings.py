37e6c0578f6c92efc5007668de6c89ab261d4aca
wrmsr <timwilloney@gmail.com>
2026-04-16 18:54:37 -0700

    Add skeleton test file for BackendStringsManifest strings handling.

---
+++ ommlds/minichain/backends/specs/tests/test_strings.py
@@ -0,0 +1,8 @@
+# from omlish.manifests.globals import GlobalManifestLoader
+#
+# from ...strings.manifests import BackendStringsManifest
+#
+#
+# def test_strings():
+#     bsms = GlobalManifestLoader.load_values_of(BackendStringsManifest)
+#     print(bsms)
