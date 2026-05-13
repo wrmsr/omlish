4b430e8b2978ecb756afd1ad651bbdc5a0654d38
wrmsr <timwilloney@gmail.com>
2026-05-13 12:18:09 -0700

    Empty commit

---
+++ ommlds/backends/google/protocol/_dataclasses.py
@@ -0,0 +1,23 @@
+# @omlish-generated
+# type: ignore
+# ruff: noqa
+# flake8: noqa
+import dataclasses
+import reprlib
+import types
+
+
+##
+
+
+REGISTRY = {}
+
+
+def _register(**kwargs):
+    def inner(fn):
+        REGISTRY[kwargs['plan_repr']] = (kwargs, fn)
+        return fn
+    return inner
+
+
+##
