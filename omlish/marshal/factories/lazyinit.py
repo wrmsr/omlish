5584ea82296308f5309c9e2e6780ffc1532f5471
wrmsr <timwilloney@gmail.com>
2026-05-17 14:27:46 -0700

    Introduce thread‑safe internal state for LazyInitRunningFactory
    Replace the previous _AlreadyRunLazyInits config with an InternalState entry that holds a lock, the last seen LazyInit sequence, and the set of already‑run lazy inits. Adjust run logic to use this state, remove unused imports, and simplify lazy‑init execution checks.

--- omlish/marshal/factories/lazyinit.py
+++ omlish/marshal/factories/lazyinit.py
@@ -1,16 +1,15 @@
+import threading
 import typing as ta
 
 from ... import check
-from ... import dataclasses as dc
 from ... import lang
 from ... import reflect as rfl
-from ... import typedvalues as tv
-from ..api.configs import Config
 from ..api.configs import ConfigRegistry
 from ..api.contexts import BaseContext
 from ..api.contexts import MarshalFactoryContext
 from ..api.contexts import UnmarshalFactoryContext
 from ..api.contexts import _PreReflectFactory
+from ..api.internalstate import InternalState
 from ..api.types import Marshaler
 from ..api.types import MarshalerFactory
 from ..api.types import Unmarshaler
@@ -24,11 +23,6 @@ FactoryT = ta.TypeVar('FactoryT', bound=MarshalerFactory | UnmarshalerFactory)
 ##
 
 
-@dc.dataclass(frozen=True, eq=False)
-class _AlreadyRunLazyInits(Config, tv.UniqueTypedValue, lang.Final):
-    lis: frozenset[LazyInit]
-
-
 class _LazyInitRunningFactory(_PreReflectFactory, ta.Generic[FactoryT]):
     def __init__(
             self,
@@ -40,14 +34,19 @@ class _LazyInitRunningFactory(_PreReflectFactory, ta.Generic[FactoryT]):
         self._fac = fac
         self._callback = callback
 
-        self._last_lis: ta.Any = None
+    class _State(InternalState.ByConfig.Entry, lang.Final):
+        def __init__(self) -> None:
+            self.lock = threading.RLock()
 
-    def _do_run(self, cfgs: ConfigRegistry, lis: ta.Sequence[LazyInit]) -> None:
-        ars = cfgs.get().get(_AlreadyRunLazyInits)
+            self.last_lazy_inits: ta.Any | None = None
+            self.already_run_lazy_inits: frozenset[LazyInit] | None = None
+
+    def _do_run(self, st: _State, cfgs: ConfigRegistry, lis: ta.Sequence[LazyInit]) -> None:
+        ars = st.already_run_lazy_inits
 
         lst: list[LazyInit] = []
         for li in lis:
-            if ars is not None and li in ars.lis:
+            if ars is not None and li in ars:
                 continue
             li.fn(cfgs)
             lst.append(li)
@@ -55,32 +54,33 @@ class _LazyInitRunningFactory(_PreReflectFactory, ta.Generic[FactoryT]):
         if not lst:
             return
 
-        if ars is not None and ars.lis:
-            lst.extend(ars.lis)
-        ars = _AlreadyRunLazyInits(frozenset(lst))
-        cfgs.update(None, ars, mode='override')
+        if ars is not None and ars:
+            lst.extend(ars)
+        st.already_run_lazy_inits = frozenset(lst)
 
         if self._callback is not None:
             self._callback()
 
-    def _run_if_necessary(self, cfgs: ConfigRegistry) -> None:
-        if (lis := cfgs.get().get(LazyInit)) and lis is not self._last_lis:
-            with cfgs._lock:  # noqa
-                if (lis := cfgs.get().get(LazyInit)) and lis is not self._last_lis:
-                    self._do_run(cfgs, lis)
-                    self._last_lis = lis
+    def _run_if_necessary(self, ctx: BaseContext) -> None:
+        cfgs = ctx.configs
+        st = ctx.internal_state_by_config.get(_LazyInitRunningFactory._State)
+        if (lis := cfgs.get().get(LazyInit)) and lis is not st.last_lazy_inits:
+            with st.lock:
+                if (lis := cfgs.get().get(LazyInit)) and lis is not st.last_lazy_inits:
+                    self._do_run(st, check.isinstance(cfgs, ConfigRegistry), lis)
+                    st.last_lazy_inits = lis
 
     def _pre_reflect(self, ctx: BaseContext) -> None:
-        self._run_if_necessary(check.isinstance(ctx.configs, ConfigRegistry))
+        self._run_if_necessary(ctx)
 
 
 class LazyInitRunningMarshalerFactory(_LazyInitRunningFactory[MarshalerFactory], MarshalerFactory):
     def make_marshaler(self, ctx: MarshalFactoryContext, rty: rfl.Type) -> ta.Callable[[], Marshaler] | None:
-        self._run_if_necessary(check.isinstance(ctx.configs, ConfigRegistry))
+        self._run_if_necessary(ctx)
         return self._fac.make_marshaler(ctx, rty)
 
 
 class LazyInitRunningUnmarshalerFactory(_LazyInitRunningFactory[UnmarshalerFactory], UnmarshalerFactory):
     def make_unmarshaler(self, ctx: UnmarshalFactoryContext, rty: rfl.Type) -> ta.Callable[[], Unmarshaler] | None:
-        self._run_if_necessary(check.isinstance(ctx.configs, ConfigRegistry))
+        self._run_if_necessary(ctx)
         return self._fac.make_unmarshaler(ctx, rty)
