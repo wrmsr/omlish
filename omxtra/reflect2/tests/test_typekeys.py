7e1b029ae1ffc0cd320f463bce9f9affafde2923
wrmsr <timwilloney@gmail.com>
2026-06-17 12:33:17 -0700

    Use TypeKeys for type key generation and cache verification in tests.

--- omxtra/reflect2/tests/test_typekeys.py
+++ omxtra/reflect2/tests/test_typekeys.py
@@ -23,64 +23,67 @@ from ..errors import UnreflectableTypeError
 from ..reflector import TypeReflector
 from ..reflector import reflect_type
 from ..universe import TypeUniverse
+from ..typekeys import TypeKeys
 
 
 def test_new_type_literal_reflection_and_key_are_cache_friendly() -> None:
     reflector = TypeReflector(universe=TypeUniverse())
+    type_keys = TypeKeys()
     mode = ta.NewType('Mode', ta.Literal['a', 'b'])  # type: ignore
 
     typ = reflector.reflect_type(mode)
-    key = reflector.type_key(typ)
+    key = type_keys.type_key(typ)
 
     assert reflector.reflect_type(mode) is typ
-    assert reflector.type_key(typ) is key
+    assert type_keys.type_key(typ) is key
     assert reflector._type_cache[mode] is typ
-    assert reflector._type_key_cache[(TYPE_KEY, typ)] is key
+    assert type_keys._type_key_cache[(TYPE_KEY, typ)] is key
 
 
 def test_type_alias_reflection_and_key_are_cache_friendly() -> None:
     reflector = TypeReflector(universe=TypeUniverse())
+    type_keys = TypeKeys()
     mode = ta.NewType('Mode', ta.Literal['a', 'b'])  # type: ignore
     mode_list = ta.TypeAliasType('ModeList', list[mode])  # type: ignore
 
     typ = reflector.reflect_type(mode_list)
-    key = reflector.type_key(typ)
+    key = type_keys.type_key(typ)
 
     assert reflector.reflect_type(mode_list) is typ
-    assert reflector.type_key(typ) is key
+    assert type_keys.type_key(typ) is key
     assert reflector._type_cache[mode_list] is typ
-    assert reflector._type_key_cache[(TYPE_KEY, typ)] is key
+    assert type_keys._type_key_cache[(TYPE_KEY, typ)] is key
 
 
 def test_subscripted_type_alias_reflection_and_key_are_cache_friendly() -> None:
     reflector = TypeReflector(universe=TypeUniverse())
+    type_keys = TypeKeys()
     t_var = ta.TypeVar('T')  # type: ignore
     mode = ta.NewType('Mode', ta.Literal['a', 'b'])  # type: ignore
     alias = ta.TypeAliasType('Alias', dict[str, t_var], type_params=(t_var,))  # type: ignore
     form = alias[mode]
 
     typ = reflector.reflect_type(form)
-    key = reflector.type_key(typ)
+    key = type_keys.type_key(typ)
 
     assert reflector.reflect_type(form) is typ
-    assert reflector.type_key(typ) is key
+    assert type_keys.type_key(typ) is key
     assert reflector._type_cache[form] is typ
-    assert reflector._type_key_cache[(TYPE_KEY, typ)] is key
+    assert type_keys._type_key_cache[(TYPE_KEY, typ)] is key
 
 
 def test_subscripted_variadic_type_alias_reflection_and_key_are_cache_friendly() -> None:
     reflector = TypeReflector(universe=TypeUniverse())
+    type_keys = TypeKeys()
     ts_var = ta.TypeVarTuple('Ts')  # type: ignore
     alias = ta.TypeAliasType('Alias', tuple[*ts_var], type_params=(ts_var,))  # type: ignore
     form = alias[int, str]
 
     typ = reflector.reflect_type(form)
-    key = reflector.type_key(typ)
+    key = type_keys.type_key(typ)
 
     assert isinstance(typ, types.TypeAliasType)
     assert reflector.reflect_type(form) is typ
-    assert reflector.type_key(typ) is key
+    assert type_keys.type_key(typ) is key
     assert reflector._type_cache[form] is typ
-    assert reflector._type_key_cache[(TYPE_KEY, typ)] is key
-
-
+    assert type_keys._type_key_cache[(TYPE_KEY, typ)] is key
