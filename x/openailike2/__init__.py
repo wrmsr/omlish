6d09e02a7da97232fbcdfaa941bae38d77ded87d
wrmsr <timwilloney@gmail.com>
2026-07-01 10:25:18 -0700

    Expose package API by re-exporting stream, unary, content, json, request, response, tool, and typetag symbols in the top‑level __init__.py.

---
+++ x/openailike2/__init__.py
@@ -0,0 +1,85 @@
+from .stream.requests import (  # noqa
+    StreamOptions,
+    StreamRequest,
+)
+
+from .stream.responses import (  # noqa
+    StreamDeltaToolCall,
+    FunctionStreamDeltaToolCallFunction,
+    FunctionStreamDeltaToolCall,
+
+    StreamDelta,
+    AssistantStreamDelta,
+
+    StreamChoice,
+
+    StreamChunk,
+)
+
+from .unary.requests import (  # noqa
+    Request,
+)
+
+from .unary.responses import (  # noqa
+    ResponseMessage,
+    AssistantResponseMessage,
+
+    Choice,
+
+    Response,
+)
+
+from .content import (  # noqa
+    ContentPart,
+    TextContentPart,
+    ImageUrlContentPart,
+)
+
+from .json import (  # noqa
+    JsonValue,
+    JsonObject,
+)
+
+from .requests import (  # noqa
+    RequestMessage,
+    SystemRequestMessage,
+    UserRequestMessage,
+    AssistantRequestMessage,
+    ToolRequestMessage,
+
+    RequestBase,
+
+    HasRequestTools,
+    HasRequestParallelToolCalls,
+
+    HasRequestLogprobs,
+
+    ResponseFormat,
+    HasRequestResponseFormat,
+
+    SamplingStop,
+    HasRequestSampling,
+)
+
+from .responses import (  # noqa
+    Usage,
+
+    TopLogprob,
+    Logprob,
+    Logprobs,
+)
+
+from .tools import (  # noqa
+    Tool,
+
+    FunctionToolFunction,
+    FunctionTool,
+
+    ToolCall,
+    FunctionToolCallFunction,
+    FunctionToolCall,
+)
+
+from .typetags import (  # noqa
+    TypeTagged,
+)
