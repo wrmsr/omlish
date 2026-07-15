f3670163c307b0074bf23af5eac3dd77a0a70c3b
wrmsr <timwilloney@gmail.com>
2026-07-15 11:51:58 -0700

    Add noninteractive flag to apt install in JDK setup

--- omdev/dockerdev/resources/fragments/jdk.sh
+++ omdev/dockerdev/resources/fragments/jdk.sh
@@ -1,4 +1,4 @@
-sudo apt install gnupg ca-certificates curl ;
+sudo apt install -y gnupg ca-certificates curl ;
 curl -s 'https://repos.azul.com/azul-repo.key' | sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg ;
 echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list ;
 
